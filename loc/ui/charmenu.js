39441bf1884a8bd3a81d3c565e326a7b4a752939
korvnisse <SSandstrom95@gmail.com>
2019-04-07 20:17:09 +0200

    Update charmenu.js

--- loc/ui/charmenu.js
+++ loc/ui/charmenu.js
@@ -3,38 +3,48 @@ export default {
   // Character selection
   charSelect: {
     select: {
-      en: 'Select Character'
+      en: 'Select Character',
+      se: 'Välj Karaktär'
     },
     create: {
-      en: 'Click to create character'
+      en: 'Click to create character',
+      se: 'Klicka för att skapa karaktär'
     },
     enterWorld: {
-      en: 'Enter World'
+      en: 'Enter World',
+      se: 'Starta Värld'
     },
     emptySlot: {
-      en: 'Empty'
+      en: 'Empty',
+      se: 'Tomt'
     }
   },
 
   // Character creation
   charCreate: {
     title: {
-      en: 'Character creation'
+      en: 'Character creation',
+      se: 'Skapa karaktär'
     },
     pressIcon: {
-      en: 'Press an icon for a brief description.'
+      en: 'Press an icon for a brief description.',
+      se: 'Klicka en ikon för beskrivning.'
     },
     selectClass: {
-      en: 'Select a class'
+      en: 'Select a class',
+      se: 'Välj en klass'
     },
     selectFaction: {
-      en: 'Select a faction'
+      en: 'Select a faction',
+      se: 'Väljg en sida'
     },
     enterName: {
-      en: 'Enter a name'
+      en: 'Enter a name',
+      se: 'Välj ett namn'
     },
     nameReq: {
-      en: 'Your name can only contain characters a-Z, and numbers 0-9, without any spaces. Up to 16 characters.'
+      en: 'Your name can only contain characters a-Z, and numbers 0-9, without any spaces. Up to 16 characters.',
+      se: 'Ditt namn kan bara använda sig karaktärerna a-Z, och nummer 0-9, utan mellanslag. Max 16 karaktärer.'
     }
   },
 
@@ -42,10 +52,12 @@ export default {
   charDelete: {
     // It places the characters name after the string, e.g. 'Delete Battlebubbles'
     title: {
-      en: 'Delete'
+      en: 'Delete',
+      se: 'Ta bort'
     },
     info: {
-      en: 'To delete, type the characters name. Your character can not be restored.'
+      en: 'To delete, type the characters name. Your character can not be restored.',
+      se: 'För att ta bort, skriv in karaktärens namn. Din karaktär tas bort för alltid.'
     }
   }
 
