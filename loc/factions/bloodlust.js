07456d7f1371b97bdc66f21e853adb944ef49fb9
korvnisse <SSandstrom95@gmail.com>
2019-04-07 20:07:22 +0200

    Update bloodlust.js

--- loc/factions/bloodlust.js
+++ loc/factions/bloodlust.js
@@ -1,8 +1,11 @@
 export default {
   name: {
-    en: 'Bloodlust'
+    en: 'Bloodlust',
+    se: 'Blodtörstiga'
   },
   description: {
-    en: 'The Bloodlust horde values freedom and loyalty, offers a place for individualism or even chaos. Their Defenses are located in a desert realm called Headless Landing.'
+    en: 'The Bloodlust horde values freedom and loyalty, offers a place for individualism or even chaos. Their Defenses are located in a desert realm called Headless Landing.',
+    se: 'De blodtörstiga värderar frihet och lojalitet, erbjunder en plats för individualism men också kaos. Deras försvar hittas i ett öken rike vid namn Headless Landing.'
+    
   }
 }
