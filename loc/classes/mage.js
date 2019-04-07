c1401979a581ab7d6bc6f89d97a0f48034af02cf
korvnisse <SSandstrom95@gmail.com>
2019-04-07 20:53:49 +0200

    Update mage.js

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -3,11 +3,11 @@ export default {
   name: {
     en: 'Mage',
     de: 'Magier',
-	se: 'Magiker'
+    se: 'Magiker'
   },
   // Class description, visible during character creation
   description: {
     en: 'Mages deal very heavy large scale Area of Effect damage while also providing utility through slowing targets with their ice based magic.',
-	se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.'
+    se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.'
   }
 }
