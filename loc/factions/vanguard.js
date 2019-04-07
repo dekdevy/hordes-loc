8ad039240b686c8c3a80a0e90fe80a12583c2a2e
korvnisse <SSandstrom95@gmail.com>
2019-04-07 20:11:09 +0200

    Update vanguard.js

--- loc/factions/vanguard.js
+++ loc/factions/vanguard.js
@@ -1,8 +1,10 @@
 export default {
   name: {
-    en: 'Vanguard'
+    en: 'Vanguard',
+    se: 'Vanguard'
   },
   description: {
-    en: 'The Vanguard horde seeks tradition, structure and order in society. Their Castle can be found in a lush green area around Guardstone.'
+    en: 'The Vanguard horde seeks tradition, structure and order in society. Their Castle can be found in a lush green area around Guardstone.',
+    se: 'Vanguarden söker tradition, Struktur och ordning i samhället. Deras slott kan hittas i ett extravagant grönt område runt Guardstone.'
   }
 }
