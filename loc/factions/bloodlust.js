df7039447b61101cb333d60fc3d27dcce0a3c790
Nuliax <49404566+Nuliax@users.noreply.github.com>
2019-04-08 17:21:50 +0200

    Update bloodlust.js

--- loc/factions/bloodlust.js
+++ loc/factions/bloodlust.js
@@ -1,11 +1,12 @@
 export default {
   name: {
     en: 'Bloodlust',
-    se: 'Bloodlust'
+    se: 'Bloodlust',
+    ru: 'Bloodlust'
   },
   description: {
     en: 'The Bloodlust horde values freedom and loyalty, offers a place for individualism or even chaos. Their Defenses are located in a desert realm called Headless Landing.',
     se: 'Bloodlust värderar frihet och lojalitet, erbjunder en plats för individualism men också kaos. Deras försvar hittas i ett öken kungarike vid namn Headless Landing.'
-    
+    ru: 'Орда Bloodlust славится свободой и лояльностью, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном мире, названном Headless Landing.'
   }
 }
