64be3ad4f604efae0b126e6497a7d56c7feba0ec
dek <polydek@googlemail.com>
2019-04-10 23:02:30 +0200

    added item files

---
+++ loc/items/talisman.js
@@ -0,0 +1,26 @@
+export default [
+  { // T1 item name. names are english only!
+    name       : { en: 'Trashy Thing' },
+    // Item description, visible during character creation
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+
+  { // T2 item. names are english only!
+    name       : { en: 'Wooden Thing' },
+    // Item description, visible during character creation
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+
+  { // T3 item. and so on...
+    name       : { en: 'Elven Thing' },
+    description: {
+      en: 'Interesting item description'
+    }
+  }
+
+  // keep adding objects here for t4,t5, etc..
+]
