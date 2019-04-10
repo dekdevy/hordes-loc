e157462394c90ce3da34fbe6c7568ab947acabc9
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-10 22:29:54 +0100

    Update sword.js

--- loc/items/sword.js
+++ loc/items/sword.js
@@ -1,25 +1,119 @@
 export default [
   { // T1 item name. names are english only!
-    name       : { en: 'Trashy Thing' },
+    name       : { en: 'Wooden Sword' },
     // Item description
     description: {
       en: 'Interesting item description'
     }
   },
 
-  { // T2 item. names are english only!
-    name       : { en: 'Wooden Thing' },
+  { // T2 item
+    name       : { en: 'Rusty Ironsword' },
     description: {
       en: 'Interesting item description'
     }
   },
 
-  { // T3 item. and so on...
-    name       : { en: 'Elven Thing' },
+  { // T3 item
+    name       : { en: 'Troll Blade' },
     description: {
       en: 'Interesting item description'
     }
-  }
+  },
 
-  // keep adding objects here for t4,t5, etc..
+  { // T4 item
+    name       : { en: 'Broadsword' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T5 item. names are english only!
+    name       : { en: 'Longsword' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  { // T6 item
+    name       : { en: 'Carved Bonesword' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  { // T7 item
+    name       : { en: 'Greatsword' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  { // T8 item
+    name       : { en: 'Ghastly Scimitar' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T9 item
+    name       : { en: 'Nullfire Sword' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+ 
+  { // T10 item
+    name       : { en: "Knight's Greatsword" },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T11 item
+    name       : { en: "King's Glaive" },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T12 item
+    name       : { en: 'Void Blade' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T13 item
+    name       : { en: 'Zerstörer' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T14 item
+    name       : { en: 'Hearteater' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T15 item
+    name       : { en: 'Demonedge' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T16 item
+    name       : { en: 'Excalibur' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T17 item
+    name       : { en: 'Harbringer' },
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+ 
 ]
