6ba14567a32c8e49de7a19b957cd16991849f6e3
chambtheminerva <49500705+chambtheminerva@users.noreply.github.com>
2019-04-11 17:04:35 -0400

    Update charmenu.js

--- loc/ui/charmenu.js
+++ loc/ui/charmenu.js
@@ -6,27 +6,32 @@ export default {
     title: {
       en: 'Select Character',
       se: 'Välj Karaktär',
-      ru: 'Выберите Персонажа'
+      ru: 'Выберите Персонажа',
+      fr: 'Sélectionner un personnage'
     },
     create: {
       en: 'Click to create character',
       se: 'Klicka för att skapa karaktär',
-      ru: 'Нажмите, чтобы создать персонажа'
+      ru: 'Нажмите, чтобы создать персонажа',
+      fr: 'Cliquer pour créer le personnage'
     },
     enterWorld: {
       en: 'Enter World',
       se: 'Starta Värld',
-      ru: 'Войти в мир'
+      ru: 'Войти в мир',
+      fr: 'Entrer le monde'
     },
     emptySlot: {
       en: 'Empty',
       se: 'Ingen',
-      ru: 'Пусто'
+      ru: 'Пусто',
+      fr: 'Vide'
     },
     error: {
       en: 'Network error',
       se: 'Nätverks problem',
-      ru: 'Проблема с сетью'
+      ru: 'Проблема с сетью',
+      fr: 'Problème de réseau' 
     }
   },
 
@@ -35,32 +40,38 @@ export default {
     title: {
       en: 'Character creation',
       se: 'Skapa karaktär',
-      ru: 'Создание персонажа'
+      ru: 'Создание персонажа',
+      fr: 'Création du personnage'
     },
     pressIcon: {
       en: 'Press an icon for a brief description.',
       se: 'Klicka en ikon för beskrivning.',
-      ru: 'Нажмите на иконку, чтобы увидеть краткое описание.'
+      ru: 'Нажмите на иконку, чтобы увидеть краткое описание.',
+      fr: 'Cliquer sur une icône pour une brève description'
     },
     selectClass: {
       en: 'Select a class',
       se: 'Välj en klass',
-      ru: 'Выберите класс'
+      ru: 'Выберите класс',
+      fr: 'Sélectionner une classe'
     },
     selectFaction: {
       en: 'Select a faction',
       se: 'Välj en sida',
-      ru: 'Выберите фракцию'
+      ru: 'Выберите фракцию',
+      fr: 'Sélectionner une faction'
     },
     enterName: {
       en: 'Enter a name',
       se: 'Välj ett namn',
-      ru: 'Введите имя'
+      ru: 'Введите имя',
+      fr: 'Entrer un nom'
     },
     nameReq: {
       en: 'Your name can only contain characters a-Z, and numbers 0-9, without any spaces. Up to 16 characters.',
       se: 'Ditt namn kan bara använda sig karaktärerna a-Z, och nummer 0-9, utan mellanslag. Max 16 karaktärer.',
-      ru: 'Ваше имя может состоять только из латинского алфавита a-Z и цифр 0-9, без пробелов. До 16 символов.'
+      ru: 'Ваше имя может состоять только из латинского алфавита a-Z и цифр 0-9, без пробелов. До 16 символов.',
+      fr: 'Votre nom ne peut contenir que des caractères de a-Z, des nombres de 0-9, sans aucune espace. Jusqu\'à 16 charactères.'
     }
   },
 
@@ -70,17 +81,20 @@ export default {
     title: {
       en: 'Delete',
       se: 'Ta bort',
-      ru: 'Удалить'
+      ru: 'Удалить',
+      fr: 'Supprimer'
     },
     info: {
       en: 'To delete, type the characters name. Your character can not be restored.',
       se: 'För att ta bort, skriv in karaktärens namn. Din karaktär tas bort för alltid.',
-      ru: 'Чтобы удалить, введите имя персонажа. Ваш персонаж не может быть восстановлен.'
+      ru: 'Чтобы удалить, введите имя персонажа. Ваш персонаж не может быть восстановлен.',
+      fr: 'Écrire le nom du personnage à supprimer. Le personnage ne pourra pas être restauré.'
     },
     placeholder: {
       en: 'Are you sure?',
       se: 'Är du säker?',
-      ru: 'Вы уверены?'
+      ru: 'Вы уверены?',
+      fr: 'Êtes-vous sûr?'
     }
   }
 
