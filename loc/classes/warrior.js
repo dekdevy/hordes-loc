b956ede016934814d01fb6ee85eccf8070cd191c
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-11 14:02:56 -0400

    Update warrior.js

--- loc/classes/warrior.js
+++ loc/classes/warrior.js
@@ -11,5 +11,6 @@ export default {
     en: 'Warriors are built to tank more monsters than any other class by having high defense through equipment (Shields) and strong defensive buffs.',
     se: 'Krigare är byggda för att tåla mer skada än alla andra klasser genom högt försvar från sin utrustning (Sköld) och starka försvars Buffar.',
     ru: 'Воины построены для того, чтобы танковать больше монстров чем любой другой класс, имея хорошую защиту с помощью снаряжения (Щита) и сильных защитных усилений.'
+    de: 'Krieger sind so gebaut, dass sie mehr Monster als jede andere Klasse besiegen, indem sie eine hohe Verteidigung durch Ausrüstung (Schilde) und starke Verteidigungszauber haben.'
   }
 }
