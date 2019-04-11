747358929b4556a6b33ff45aaa8402830af21b2c
ValdusShadowmask <49405227+ValdusShadowmask@users.noreply.github.com>
2019-04-10 20:15:04 -0400

    Update archer.js

--- loc/classes/archer.js
+++ loc/classes/archer.js
@@ -4,13 +4,15 @@ export default {
     en: 'Archer',
     de: 'Jäger',
     se: 'Bågskytt',
-    ru: 'Лучник'
+    ru: 'Лучник',
+    ko: '궁수',
   },
   // Class description, visible during character creation
   description: {
     en: 'Archers deal high single target damage and bursty Area of Effect damage. A good archer will always keep their distance and snipe enemies from afar.',
     de: 'Jäger richten großen Schaden bei einzelnen Zielen, und schubweise Flächenschaden an. Ein guter Jäger wird immer auf distanz bleiben.',
     se: 'Bågskytt gör hög skada mot enskilda personer och har även hög Areal skada. En bra bågskytt attackerar alltid från en distans.',
-    ru: 'Лучники наносят высокий урон по одиночной цели и взрывной урон по области. Хороший лучник всегда будет держать дистанцию и отстреливать врагов издалека.'
+    ru: 'Лучники наносят высокий урон по одиночной цели и взрывной урон по области. Хороший лучник всегда будет держать дистанцию и отстреливать врагов издалека.',
+    ko: '궁수는 높은 단일 타겟 데미지와 버스트 에어리어 피해를줍니다. 좋은 궁수는 먼 거리에서 항상 적을 공격합니다.'
   }
 }
