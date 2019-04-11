1df8b8e0090a52deef2aa7d8246b3722a9297453
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-11 18:20:54 +0100

    Update bow.js

--- loc/items/bow.js
+++ loc/items/bow.js
@@ -17,15 +17,15 @@ export default [
   },
 
   { // T3 item
-    name       : { en: "Adventurer's Shortbow" },
+    name       : { en: 'Adventurers Shortbow' },
     description: {
-      en: "Whoever left this on the ground probably didn't meet a nice end.",
+      en: 'Whoever left this on the ground probably did not meet a nice end.',
       ru: 'Кто то оставил его на земле, вероятно, не встретил хороший конец'
     }
   },
   
   { // T3 item
-    name       : { en: "Long Bow" },
+    name       : { en: 'Long Bow' },
     description: {
       en: 'A more advanced weapon. Quite nice to look at.',
       ru: 'Более продвинутое оружие. Довольно приятно смотреть на эго.'
@@ -33,7 +33,7 @@ export default [
   },
   
  { // T4 item
-    name       : { en: "Bone Bow" },
+    name       : { en: 'Bone Bow' },
     description: {
       en: 'Put together with the bones of... somebody?',
       ru: 'Cобран из костей ... кого-нибудь?'
@@ -41,7 +41,7 @@ export default [
   },
   
  { // T5 item
-    name       : { en: "Elven Bow" },
+    name       : { en: 'Elven Bow' },
     description: {
       en: 'An expert bow of a forgotten race.',
       ru: 'Экспертный лук забытой расы'
@@ -49,7 +49,7 @@ export default [
   },
   
  { // T6 item
-    name       : { en: "Ancient Bow" },
+    name       : { en: 'Ancient Bow' },
     description: {
       en: 'Crude, but effective. Draws its power from the tiny leaf attached to it.',
       ru: 'Грубый, но эффективный. получает силу из крошечного листа, прикрепленного к нему.'
@@ -57,7 +57,7 @@ export default [
   },
   
  { // T7 item
-    name       : { en: "Iron Piercer" },
+    name       : { en: 'Iron Piercer' },
     description: {
       en: 'Burns away the enemy with the spiciest of red coloring.',
       ru: 'Сжигает противника с пряностями красной окраскаи.'
@@ -65,7 +65,7 @@ export default [
   },
   
  { // T8 item
-    name       : { en: "Silver Recurve" },
+    name       : { en: 'Silver Recurve' },
     description: {
       en: 'A bow commonly used by low-ranked royal archers. Blends in well with vegetation.',
       ru: 'Лук, обычно используемый низкими королевскими лучниками. Хорошо сочетается с растительностью.'
@@ -73,7 +73,7 @@ export default [
   },
   
  { // T9 item
-    name       : { en: "Assassin's Bow" },
+    name       : { en: 'Assassins Bow' },
     description: {
       en: 'Masterfully crafted by the bandits of the capitals.',
       ru: 'Мастерски созданный бандитами столиц.'
@@ -81,7 +81,7 @@ export default [
   },
   
  { // T10 item
-    name       : { en: "Skyfire Warbow" },
+    name       : { en: 'Skyfire Warbow' },
     description: {
       en: 'A rare bow used in the greatest of wars. Shooting it makes you feel like the heavens will collapse.',
       ru: 'Редкий лук, используемый в величайших войнах. Bыстрел заставляет вас чувствовать, что небеса рухнут.'
@@ -89,34 +89,34 @@ export default [
   },
   
   { // T11 item
-    name       : { en: "Hellfire Warbow" },
+    name       : { en: 'Hellfire Warbow' },
     description: {
       en: 'What is it? Is it ice? The world may never know...',
       ru: 'Что это? Это лед? Мир может никогда не узнать ...'
     }
   },{ // T13 item
-    name       : { en: "Widowmaker" },
+    name       : { en: 'Widowmaker' },
     description: {
       en: 'Kills as beautifully as it looks.',
       ru: 'Убивает так же красиво, как и выглядит.'
     }
     
   },{ // T3 item
-    name       : { en: "Stormsong" },
+    name       : { en: 'Stormsong' },
     description: {
       en: 'Surrounded by a ghastly aura. The wind blows in the direction you aim it.',
       ru: 'Окруженный страшной аурой. Ветер дует в том направлении, куда вы нацеливаетесь.'
     }
     
   },{ // T14 item
-    name       : { en: "Scarebow" },
+    name       : { en: 'Scarebow' },
     description: {
       en: 'So scary that few have the guts to hold it.',
       ru: 'Настолько страшно, что немногие имеют мужество держать его'
     }
     
   },{ // T15 item
-    name       : { en: "Fury" },
+    name       : { en: 'Fury' },
     description: {
       en: 'Embedded with a primal rage, your foes tremble at the sight of this weapon. Pew pew is the last thing they will ever hear.',
       ru: 'Окрыленные яростью, ваши противники дрожат при виде этого оружия. Пью Пью - последнее, что они услышат.'
