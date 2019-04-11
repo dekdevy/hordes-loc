db9904ecee31697912ffa9942d53aaa7bfa3b93a
chambtheminerva <49500705+chambtheminerva@users.noreply.github.com>
2019-04-11 17:29:34 -0400

    Update mage.js

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -10,7 +10,7 @@ export default {
   description: {
     en: 'Mages deal very heavy large scale Area of Effect damage while also providing utility through slowing targets with their ice based magic.',
     se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.',
-    fr: 'Les mages infligent des dégats de zone à grande échelle tout en ralentissant leurs ennemis grâce à leur magie à base de glace.'
+    fr: 'Les mages infligent des dégats de zone à grande échelle tout en ralentissant leurs ennemis grâce à leur magie basée sur la glace.'
   }
   }
 }
