8a0e926ac324b531fa9b01a18deee1b2a423de7e
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-11 21:46:14 +0100

    Add files via upload

---
+++ loc/items/staff.js
@@ -0,0 +1,121 @@
+export default [
+  {
+    name       : {en: 'Broken Twig' },
+    description: {
+      en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.'
+    }
+  },
+
+  {
+    name       : {en: 'Cracked Stick' },
+    description: {
+      en: 'Desperate situations call for desperate measures.'
+    }
+  },
+
+  {
+    name       : {en: 'Gnarled Broomstick' },
+    description: {
+      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.'
+    }
+  },
+
+  {
+    name       : {en: 'Oak Stave' },
+    description: {
+      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.'
+    }
+  },
+
+  {
+    name       : {en: 'Mystic Wand' },
+    description: {
+      en: 'Mystical weapon glowing with a mysterious essence.'
+    }
+  },
+
+  {
+    name       : {en: 'Bone Stave' },
+    description: {
+      en: 'Nothing fancy about this weapon, just the femur of a lich.'
+    }
+  },
+
+  {
+    name       : {en: 'Encrusted Wand' },
+    description: {
+      en: 'Long wand, with a blue shimmering gem at its crown.'
+    }
+  },
+
+  {
+    name       : {en: 'Imbued Staff' },
+    description: {
+      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.'
+    }
+  },
+
+  {
+    name       : {en: 'Emerald Staff' },
+    description: {
+      en: 'Contructed from emeralds stolen from the mountain dwarves.'
+    }
+  },
+
+  {
+    name       : {en: 'Sapphire Staff' },
+    description: {
+      en: 'Strange......the staff seems.........triangular.....'
+    }
+  },
+
+  {
+    name       : {en: 'Frozen Greatstaff' },
+    description: {
+      en: 'Eccentric rod with a pale blue stone embedded inside....'
+    }
+  },
+
+  {
+    name       : {en: 'Infernal Staff' },
+    description: {
+      en: 'A gift from the almight lava god, Erlik.'
+    }
+  },
+
+  {
+    name       : {en: 'Hellfire Greatstaff' },
+    description: {
+      en: 'Its beauty calls out to you.....to kill.....to conquer....'
+    }
+  },
+
+  {
+    name       : {en: 'Staff of Angelness' },
+    description: {
+      en: 'A staff stolen from the angels. Theyre still hunting for it.'
+    }
+  },
+
+  {
+    name       : {en: 'Crystal Core' },
+    description: {
+      en: 'Crude in its looks, yet mighty at its core......'
+    }
+  },
+
+  {
+    name       : {en: 'Witchs Heart' },
+    description: {
+      en: 'We are told its core is from the heart of a witch. No one has found the corpse. Or the staff itself.'
+    }
+  },
+
+  {
+    name       : {en: 'Deathweaver' },
+    description: {
+      en: 'This weapon is so secretive that the few who wield it have killed all that know of it.'
+    }
+  }
+
+]
