90aa7baec8af64948cec3549d2eb817ac02f7ced
chambtheminerva <49500705+chambtheminerva@users.noreply.github.com>
2019-04-10 20:39:43 -0400

    Update mage.js

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -3,11 +3,14 @@ export default {
   name: {
     en: 'Mage',
     de: 'Magier',
-    se: 'Magiker'
+    se: 'Magiker',
+    fr: 'Mage'
   },
   // Class description, visible during character creation
   description: {
     en: 'Mages deal very heavy large scale Area of Effect damage while also providing utility through slowing targets with their ice based magic.',
-    se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.'
+    se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.',
+    fr: 'Les mages infligent des dégats de zone à grande échelle tout en ralentissant leurs ennemis grâce à leur magie à base de glace.'
+  }
   }
 }
