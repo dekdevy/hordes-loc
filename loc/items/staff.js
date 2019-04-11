60c2bf3a91893f0236fa6c84978dfc06ba33c901
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-11 21:20:39 +0100

    Update staff.js

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -1,120 +1,137 @@
 export default [ 
   {
-    name      : {en: 'Broken Twig' }, 
+    name      : {en: 'Broken Twig' },
+    // Item description
     description: {
       en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.' 
     }
   },
 
   {
-    name      : {en: 'Cracked Stick' }, 
+    name      : {en: 'Cracked Stick' },
+    // Item description
     description: {
-      en: 'Desperate situations call for desperate measures.' 
+      en: 'Desperate situations call for desperate measures.'
     }
   },
 
   {
-    name      : {en: 'Gnarled Broomstick' }, 
+    name      : {en: 'Gnarled Broomstick' },
+    // Item description
     description: {
-      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.' 
+      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.'
     }
   },
 
   {
-    name      : {en: 'Oak Stave' }, 
+    name      : {en: 'Oak Stave' },
+    // Item description
     description: {
-      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.' 
+      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.'
     }
   },
 
   {
-    name      : {en: 'Mystic Wand' }, 
+    name      : {en: 'Mystic Wand' },
+    // Item description
     description: {
-      en: 'Mystical weapon glowing with a mysterious essence.' 
+      en: 'Mystical weapon glowing with a mysterious essence.'
     }
   },
 
   {
-    name      : {en: 'Bone Stave' }, 
+    name      : {en: 'Bone Stave' },
+    // Item description
     description: {
-      en: 'Nothing fancy about this weapon, just the femur of a lich.' 
+      en: 'Nothing fancy about this weapon, just the femur of a lich.'
     }
   },
 
   {
-    name      : {en: 'Encrusted Wand' }, 
+    name      : {en: 'Encrusted Wand' },
+    // Item description
     description: {
-      en: 'Long wand, with a blue shimmering gem at its crown.' 
+      en: 'Long wand, with a blue shimmering gem at its crown.'
     }
   },
 
   {
-    name      : {en: 'Imbued Staff' }, 
+    name      : {en: 'Imbued Staff' },
+    // Item description
     description: {
-      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.' 
+      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.'
     }
   },
 
   {
-    name      : {en: 'Emerald Staff' }, 
+    name      : {en: 'Emerald Staff' },
+    // Item description
     description: {
-      en: 'Contructed from emeralds stolen from the mountain dwarves.' 
+      en: 'Contructed from emeralds stolen from the mountain dwarves.'
     }
   },
 
   {
-    name      : {en: 'Sapphire Staff' }, 
+    name      : {en: 'Sapphire Staff' },
+    // Item description
     description: {
-      en: 'Strange......the staff seems.........triangular.....' 
+      en: 'Strange......the staff seems.........triangular.....'
     }
   },
 
   {
-    name      : {en: 'Frozen Greatstaff' }, 
+    name      : {en: 'Frozen Greatstaff' },
+    // Item description
     description: {
-      en: 'Eccentric rod with a pale blue stone embedded inside....' 
+      en: 'Eccentric rod with a pale blue stone embedded inside....'
     }
   },
 
   {
-    name      : {en: 'Infernal Staff' }, 
+    name      : {en: 'Infernal Staff' },
+    // Item description
     description: {
-      en: 'A gift from the almight lava god, Erlik.' 
+      en: 'A gift from the almight lava god, Erlik.'
     }
   },
 
   {
-    name      : {en: 'Hellfire Greatstaff' }, 
+    name      : {en: 'Hellfire Greatstaff' },
+    // Item description
     description: {
-      en: 'Its beauty calls out to you.....to kill.....to conquer....' 
+      en: 'Its beauty calls out to you.....to kill.....to conquer....'
     }
   },
 
   {
-    name      : {en: 'Staff of Angelness' }, 
+    name      : {en: 'Staff of Angelness' },
+    // Item description
     description: {
-      en: 'A staff stolen from the angels. Theyre still hunting for it.' 
+      en: 'A staff stolen from the angels. Theyre still hunting for it.'
     }
   },
 
   {
-    name      : {en: 'Crystal Core' }, 
+    name      : {en: 'Crystal Core' },
+    // Item description
     description: {
-      en: 'Crude in its looks, yet mighty at its core......' 
+      en: 'Crude in its looks, yet mighty at its core......'
     }
   },
 
   {
-    name      : {en: 'Witchs Heart' }, 
+    name      : {en: 'Witchs Heart' },
+    // Item description
     description: {
-      en: 'We are told its core is from the heart of a witch. No one has found the corpse. Or the staff itself.' 
+      en: 'We are told its core is from the heart of a witch. No one has found the corpse. Or the staff itself.'
     }
   },
 
   {
-    name      : {en: 'Deathweaver' }, 
+    name      : {en: 'Deathweaver' },
+    // Item description
     description: {
-      en: 'This weapon is so secretive that the few who wield it have killed all that know of it.' 
+      en: 'This weapon is so secretive that the few who wield it have killed all that know of it.'
     }
   }
 ]
