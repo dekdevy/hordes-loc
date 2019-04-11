4683439f3e1d6d955916c30e854540431f904158
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-11 14:26:36 -0400

    Update shaman.js

--- loc/classes/shaman.js
+++ loc/classes/shaman.js
@@ -10,6 +10,7 @@ export default {
   description: {
     en: 'The Shamans role is to support the group by healing the Tank and offering offensive Buffs or crippling Debuffs.',
     se: 'Schamanens roll är att hjälpa sin grupp med att hela upp Tanken och använda sig utav sina offensiva Buffs eller förlamande Debuffs.',
-    ru: 'Шаманы играют роль поддержки группы, излечивая Танка и преподнося усиливающие бонусы или наносящие урон отрицательные эффекты.'
+    ru: 'Шаманы играют роль поддержки группы, излечивая Танка и преподнося усиливающие бонусы или наносящие урон отрицательные эффекты.',
+    de: 'Die Rolle der Schamanen besteht darin, die Gruppe zu unterstützen, indem sie den Panzer heilen und beleidigende Buffs anbieten oder Debuffs verkrüppeln.'
   }
 }
