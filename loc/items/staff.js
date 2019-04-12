f8e178b9259fc784772727830539a68d90670b8d
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 18:17:49 +0100

    Update staff.js

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -2,112 +2,128 @@ export default [
   {
     name       : {en: 'Broken Twig' },
     description: {
-      en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.'
+      en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.',
+      ru: 'Оружие, выкованное величайшим кузнецом из всей земли. По крайней мере, так мы сказали маленькому Тимми.'
     }
   },
 
   {
     name       : {en: 'Cracked Stick' },
     description: {
-      en: 'Desperate situations call for desperate measures.'
+      en: 'Desperate situations call for desperate measures.',
+      ru: 'Отчаянные ситуации требуют отчаянных мер.'
     }
   },
 
   {
     name       : {en: 'Gnarled Broomstick' },
     description: {
-      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.'
+      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.',
+      ru: 'Потрепанная метла украдена у мага. Корявый и костлявый.'
     }
   },
 
   {
     name       : {en: 'Oak Stave' },
     description: {
-      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.'
+      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.',
+      ru: 'Палочка, вырезанная из ветвей древнего дуба. Обладает глубокой силой.'
     }
   },
 
   {
     name       : {en: 'Mystic Wand' },
     description: {
-      en: 'Mystical weapon glowing with a mysterious essence.'
+      en: 'Mystical weapon glowing with a mysterious essence.',
+      ru: 'Мистическое оружие, светящееся загадочной сущностью.'
     }
   },
 
   {
     name       : {en: 'Bone Stave' },
     description: {
-      en: 'Nothing fancy about this weapon, just the femur of a lich.'
+      en: 'Nothing fancy about this weapon, just the femur of a lich.',
+      ru: 'Ничего особенного в этом оружии, только бедро лича.'
     }
   },
 
   {
     name       : {en: 'Encrusted Wand' },
     description: {
-      en: 'Long wand, with a blue shimmering gem at its crown.'
+      en: 'Long wand, with a blue shimmering gem at its crown.',
+      ru: 'Длинная палочка с синим мерцающим драгоценным камнем на короне.'
     }
   },
 
   {
     name       : {en: 'Imbued Staff' },
     description: {
-      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.'
+      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.',
+      ru: 'Темно-фиолетовый посох. Совершенно таинственно в его создании, но приятно смотреть на него.'
     }
   },
 
   {
     name       : {en: 'Emerald Staff' },
     description: {
-      en: 'Contructed from emeralds stolen from the mountain dwarves.'
+      en: 'Constructed from emeralds stolen from the mountain dwarves.',
+      ru: 'Создан из изумрудов украденных у горных гномов.'
     }
   },
 
   {
     name       : {en: 'Sapphire Staff' },
     description: {
-      en: 'Strange......the staff seems.........triangular.....'
+      en: 'Strange......the staff seems.........triangular.....',
+      ru: 'Странно ...... посох кажется ......... треугольным .....'
     }
   },
 
   {
     name       : {en: 'Frozen Greatstaff' },
     description: {
-      en: 'Eccentric rod with a pale blue stone embedded inside....'
+      en: 'Eccentric rod with a pale blue stone embedded inside....',
+      ru: 'Эксцентриковый стержень с бледно-голубым камнем, встроенным внутрь ....'
     }
   },
 
   {
     name       : {en: 'Infernal Staff' },
     description: {
-      en: 'A gift from the almighty lava god, Erlik.'
+      en: 'A gift from the almighty lava god, Erlik.',
+      ru: 'Дар всемогущего лавового бога Эрлика.'
     }
   },
 
   {
     name       : {en: 'Hellfire Greatstaff' },
     description: {
-      en: 'Its beauty calls out to you.....to kill.....to conquer....'
+      en: 'Its beauty calls out to you.....to kill.....to conquer....',
+      ru: 'Его красота призывает тебя ..... убивать ..... завоевывать ....'
     }
   },
 
   {
     name       : {en: 'Staff of Angelness' },
     description: {
-      en: 'A staff stolen from the angels. Theyre still hunting for it.'
+      en: 'A staff stolen from the angels. They\'re still hunting for it.',
+      ru: 'Посох, украденный у ангелов. Они все еще охотятся за этим.'
     }
   },
 
   {
     name       : {en: 'Crystal Core' },
     description: {
-      en: 'Crude in its looks, yet mighty at its core......'
+      en: 'Crude in its looks, yet mighty at its core......',
+      ru: 'Грубый в своей внешности, но могущественный по его ядро ......'
     }
   },
 
   {
     name       : {en: 'Witchs Heart' },
     description: {
-      en: 'We are told its core is from the heart of a witch. No one has found the corpse. Or the staff itself.'
+      en: 'We are told its core is from the heart of a witch. No one has found the corpse. Or the staff itself.',
+      ru: 'Нам говорят, что его ядро ​​от сердца ведьмы. Никто не нашел труп. Или сам посох.'
     }
   },
 
@@ -115,6 +131,7 @@ export default [
     name       : {en: 'Deathweaver' },
     description: {
       en: 'This weapon is so secretive that the few who wield it have killed all that know of it.',
+      ru: 'Это оружие настолько скрытно, что те, кто владеют им, убили всех, кто о нем знает.',
       es: 'Se dicen que nadie se queda vivo para contar de este arco, pero... de donde viene las historias?'
     }
   }
