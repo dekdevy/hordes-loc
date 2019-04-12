edf974ffc2248f7db945e1c146ca144a922624c5
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 18:28:35 +0100

    Update quiver.js

--- loc/items/quiver.js
+++ loc/items/quiver.js
@@ -2,35 +2,40 @@ export default [
   {
     name       : {en: 'Linen Quiver' },
     description: {
-      en: 'Soft, full of holes. Arrows keep falling out.'
+      en: 'Soft, full of holes. Arrows keep falling out.',
+      ru: 'Мягкий, дырявый. Стрелки продолжают выпадать.'
     }
   },
 
   {
     name       : {en: 'Reinforced Quiver' },
     description: {
-      en: 'Linen sack covered in stiff leather.'
+      en: 'Linen sack covered in stiff leather.',
+      ru: 'Льняной мешочек обтянут жесткой кожей.'
     }
   },
 
   {
     name       : {en: 'Last Reserves' },
     description: {
-      en: 'Used for when all else fails.'
+      en: 'Used for when all else fails.',
+      ru: 'Используется, когда ничего не помогает.'
     }
   },
 
   {
     name       : {en: 'Lotharien' },
     description: {
-      en: 'Elven quiver coated in shimmering diamond.'
+      en: 'Elven quiver coated in shimmering diamond.',
+      ru: 'Эльфийский колчан, покрытый мерцающим бриллиантом.'
     }
   },
 
   {
     name       : {en: 'Snake Quiver' },
     description: {
-      en: 'Every time you pull an arrow out, it hisses at you'
+      en: 'Every time you pull an arrow out, it hisses at you.',
+      ru: 'Каждый раз, когда вы вытаскиваете стрелу, она шипит на вас.'
     }
   }
 
