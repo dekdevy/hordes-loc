245165a5d1582cd5f589e98ab9ac5eed18a98bc4
Vedant Bang <33593544+VedantBang@users.noreply.github.com>
2019-04-12 22:10:03 +0530

    Added हिंदी

--- loc/factions/vanguard.js
+++ loc/factions/vanguard.js
@@ -3,12 +3,14 @@ export default {
     en: 'Vanguard',
     se: 'Vanguard',
     ru: 'Vanguard',
-    es: 'Vanguard'
+    es: 'Vanguard',
+    hi: 'वैंगार्ड'
   },
   description: {
     en: 'The Vanguard horde seeks tradition, structure and order in society. Their Castle can be found in a lush green area around Guardstone.',
     se: 'Vanguard söker tradition, struktur och ordning i samhället. Deras slott kan hittas i ett vackert grönt område omkring Guardstone.',
     ru: 'Орда Vanguard соблюдает традиции, структуру и порядок в обществе. Их крепость может быть найдена в пышной зеленой зоне около Guardstone.',
-    es: 'Los valores del Vanguard son la tradición, la estructura y el orden de sociedad. Su Castillo se halla en el bosque exuberante de Guardstone.'
+    es: 'Los valores del Vanguard son la tradición, la estructura y el orden de sociedad. Su Castillo se halla en el bosque exuberante de Guardstone.',
+    hi: 'वैंगार्ड वंश समाज में परम्परा, संरचना और अनुक्रम चाहते है। उनका क़िला गुआर्डस्टोने के चारों ओर हरे भरे ईलाके में स्थित है।'
   }
 }
