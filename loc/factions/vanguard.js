f9c248a0897bfce9b783c9128f729c38ad149e11
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-11 20:36:07 -0400

    Update vanguard.js

--- loc/factions/vanguard.js
+++ loc/factions/vanguard.js
@@ -2,11 +2,15 @@ export default {
   name: {
     en: 'Vanguard',
     se: 'Vanguard',
-    ru: 'Vanguard'
+    ru: 'Vanguard',
+    de: 'Vorhut',
+    es: 'Vanguardia',
   },
   description: {
     en: 'The Vanguard horde seeks tradition, structure and order in society. Their Castle can be found in a lush green area around Guardstone.',
     se: 'Vanguard söker tradition, struktur och ordning i samhället. Deras slott kan hittas i ett vackert grönt område omkring Guardstone.',
-    ru: 'Орда Vanguard соблюдает традиции, структуру и порядок в обществе. Их крепость может быть найдена в пышной зеленой зоне около Guardstone.'
+    ru: 'Орда Vanguard соблюдает традиции, структуру и порядок в обществе. Их крепость может быть найдена в пышной зеленой зоне около Guardstone.',
+    de: 'Die Vanguard Horde sucht Tradition, Struktur und Ordnung in der Gesellschaft. Ihre Burg befindet sich in einer üppigen grünen Umgebung rund um Guardstone.',
+    es: 'La horda de vanguardia busca la tradición, la estructura y el orden en la sociedad. Su castillo se encuentra en una exuberante zona verde alrededor de Guardstone'
   }
 }
