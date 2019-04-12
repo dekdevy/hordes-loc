f83238061141656c1b3df053ed6a837ad1e0026c
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 19:50:35 +0100

    Update armlet.js

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -2,91 +2,104 @@ export default [
   {
     name       : {en: 'Simple Bracelet' },
     description: {
-      en: 'Primitive leather and wooden band. Weak and feeble.'
+      en: 'Primitive leather and wooden band. Weak and feeble.',
+      ru: 'Примитивная кожа и деревянная полоса. Cлабоя.'
     }
   },
 
   {
     name       : {en: 'Makeshift Brace' },
     description: {
-      en: 'A little more advanced, not by much though.'
+      en: 'A little more advanced, not by much though.',
+      ru: 'Немного более продвинутый, но не намного.'
     }
   },
 
   {
     name       : {en: 'Leather Armlet' },
     description: {
-      en: 'Stiff, makes your arm sore.'
+      en: 'Stiff, makes your arm sore.',
+      ru: 'Жосткая, болит рука.'
     }
   },
 
   {
     name       : {en: 'Ivory Bracelet' },
     description: {
-      en: 'Elegant cuff made from the tusks of the last mammonth.'
+      en: 'Elegant cuff made from the tusks of the last mammonth.',
+      ru: 'Элегантная манжета из бивней последнего мамонта.'
     }
   },
 
   {
     name       : {en: 'Iron Vambrace' },
     description: {
-      en: 'Heavy on your arm.....worth the protection.'
+      en: 'Heavy on your arm.....worth the protection.',
+      ru: 'Тяжело на твоей руке ..... стоит защиты.'
     }
   },
 
   {
     name       : {en: 'Imbued Bracers' },
     description: {
-      en: 'Dull metal bracer, handy for nightime raidings.'
+      en: 'Dull metal bracer, handy for nightime raidings.',
+      ru: 'Унылый металлический браслет, удобный для ночных рейдов.'
     }
   },
 
   {
     name       : {en: 'Ember Cuffs' },
     description: {
-      en: 'Scaled bracer, makes your arms resemble that of a dragon.'
+      en: 'Scaled bracer, makes your arms resemble that of a dragon.',
+      ru: 'Чешуйчатый наруч, делает ваши руки похожими на руки дракона.'
     }
   },
 
   {
     name       : {en: 'Mirrored Armlet' },
     description: {
-      en: 'Blinds your foes with the shine of Ra.'
+      en: 'Blinds your foes with the shine of Ra.',
+      ru: 'Ослепляет ваших противников сиянием Ра.'
     }
   },
 
   {
     name       : {en: 'Golem Fragment' },
     description: {
-      en: 'The archaic guardians left remnants of their passing, which you unscrupulously have decided to wear on your arm.'
+      en: 'The archaic guardians left remnants of their passing, which you unscrupulously have decided to wear on your arm.',
+      ru: 'Архаичные стражи оставили остатки своего прохождения, которые вы бессовестно решили надеть на руку.'
     }
   },
 
   {
     name       : {en: 'Coldforged Bracer' },
     description: {
-      en: 'The deceptively light steel offers chilling protection against sneaky blows'
+      en: 'The deceptively light steel offers chilling protection against sneaky blows',
+      ru: 'Обманчиво легкая сталь предлагает охлаждающую защиту от подлых ударов'
     }
   },
 
   {
     name       : {en: 'Blackstar Gem' },
     description: {
-      en: 'Stars are not often black, yet this fragment has a.......deep.......deep darkness.'
+      en: 'Stars are not often black, yet this fragment has a.......deep.......deep darkness.',
+      ru: 'Звезды не часто черные, но этот фрагмент имеет ....... глубокую ....... глубокую тьму.'
     }
   },
 
   {
     name       : {en: 'Eternal Vambraces' },
     description: {
-      en: 'Reforged, lost, found, stolen, hidden for eras without end.'
+      en: 'Reforged, lost, found, stolen, hidden for eras without end.',
+      ru: 'Перекован, потерян, найден, украден, спрятан на века без конца.'
     }
   },
 
   {
     name       : {en: 'Lost Guardian' },
     description: {
-      en: 'This item was forged on a full moon when mysterious incantations were heard in the whispers of the dark. Some say that it was Anubis himself speaking with the dead.'
+      en: 'This item was forged on a full moon when mysterious incantations were heard in the whispers of the dark. Some say that it was Anubis himself speaking with the dead.',
+      ru: 'Этот предмет был выкован в полнолуние, когда в шепот темноты раздавались таинственные заклинания. Некоторые говорят, что это был сам Анубис, говорящий с мертвыми.'
     }
   }
 
