fb531d82f2565cde6a05d31cb4c0dce347ab565e
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-12 10:02:42 -0400

    Update bow.js

--- loc/items/bow.js
+++ loc/items/bow.js
@@ -4,7 +4,10 @@ export default [
     // Item description
     description: {
       en: 'Almost lightweight. You can probably find something better around.',
-      ru: 'Легкий лук. Возможно, вы можете найти что-то лучшее вокруг.'
+      ru: 'Легкий лук. Возможно, вы можете найти что-то лучшее вокруг.',
+      de: 'Fast leicht. Du kannst wahrscheinlich etwas Besseres finden.',
+      es: 'Casi ligero. Probablemente puedas encontrar algo mejor por aquí.',
+      fr: 'Presque léger. Vous pouvez probablement trouver quelque chose de mieux dans le coin.'
     }
   },
 
@@ -12,7 +15,10 @@ export default [
     name       : { en: 'Novice Shortbow' },
     description: {
       en: 'You feel less skilled just by holding this in your hands.',
-      ru: 'Вы чувствуете себя менее опытным, просто держа его в своих руках'
+      ru: 'Вы чувствуете себя менее опытным, просто держа его в своих руках',
+      de: 'Du fühlst dich weniger geschickt, nur weil du das in deinen Händen hältst.',
+      es: 'Te sientes menos hábil con sólo sostener esto en tus manos.',
+      fr: 'Vous vous sentez moins doué rien qu\'en le tenant dans vos mains.',
     }
   },
 
@@ -20,7 +26,10 @@ export default [
     name       : { en: 'Adventurer\'s Shortbow' },
     description: {
       en: 'Whoever left this on the ground probably didn\'t meet a nice end.',
-      ru: 'Кто то оставил его на земле, вероятно, не встретил хороший конец'
+      ru: 'Кто то оставил его на земле, вероятно, не встретил хороший конец',
+      de: 'Wer das auf dem Boden gelassen hat, hat wahrscheinlich kein schönes Ende gefunden.',
+      es: 'Quienquiera que haya dejado esto en el suelo probablemente no tuvo un buen final.',
+      fr: 'Celui qui a laissé ça sur le sol n\'a probablement pas trouvé une fin heureuse.',
     }
   },
 
@@ -28,7 +37,10 @@ export default [
     name       : { en: 'Long Bow' },
     description: {
       en: 'A more advanced weapon. Quite nice to look at.',
-      ru: 'Более продвинутое оружие. Довольно приятно смотреть на эго.'
+      ru: 'Более продвинутое оружие. Довольно приятно смотреть на эго.',
+      de: 'Eine fortschrittlichere Waffe. Sehr schön anzusehen.',
+      es: 'Un arma más avanzada. Bastante bonito de ver.',
+      fr: 'Une arme plus avancée. Plutôt agréable à regarder.',
     }
   },
 
@@ -36,7 +48,10 @@ export default [
     name       : { en: 'Bone Bow' },
     description: {
       en: 'Put together with the bones of... somebody?',
-      ru: 'Cобран из костей ... кого-нибудь?'
+      ru: 'Cобран из костей ... кого-нибудь?',
+      de: 'Zusammen mit den Knochen von.... jemandem?',
+      es: '¿Colocado con los huesos de... alguien?',
+      fr: 'Avec les os de... quelqu\'un?'
     }
   },
 
@@ -44,7 +59,10 @@ export default [
     name       : { en: 'Elven Bow' },
     description: {
       en: 'An expert bow of a forgotten race.',
-      ru: 'Экспертный лук забытой расы'
+      ru: 'Экспертный лук забытой расы',
+      de: 'Ein Expertenbogen einer vergessenen Rasse.',
+      es: 'Una reverencia experta de una raza olvidada.',
+      fr: 'Un arc expert d\'une race oubliée.'
     }
   },
 
@@ -52,7 +70,10 @@ export default [
     name       : { en: 'Ancient Bow' },
     description: {
       en: 'Crude, but effective. Draws its power from the tiny leaf attached to it.',
-      ru: 'Грубый, но эффективный. получает силу из крошечного листа, прикрепленного к нему.'
+      ru: 'Грубый, но эффективный. получает силу из крошечного листа, прикрепленного к нему.',
+      de: 'Grob, aber effektiv. Seine Kraft bezieht er aus dem kleinen Blatt, das daran befestigt ist.',
+      es: 'Crudo, pero efectivo. Extrae su poder de la diminuta hoja adherida a él.',
+      fr: 'Grossier, mais efficace. Tire son énergie de la petite feuille qui lui est attachée.'
     }
   },
 
@@ -60,7 +81,10 @@ export default [
     name       : { en: 'Iron Piercer' },
     description: {
       en: 'Burns away the enemy with the spiciest of red coloring.',
-      ru: 'Сжигает противника с пряностями красной окраскаи.'
+      ru: 'Сжигает противника с пряностями красной окраскаи.',
+      de: 'Verbrennt den Feind mit der schärfsten roten Farbe.',
+      es: 'Quema al enemigo con la coloración roja más picante.',
+      fr: 'Brûle l\'ennemi avec la coloration rouge la plus épicée.'
     }
   },
 
@@ -68,7 +92,10 @@ export default [
     name       : { en: 'Silver Recurve' },
     description: {
       en: 'A bow commonly used by low-ranked royal archers. Blends in well with vegetation.',
-      ru: 'Лук, обычно используемый низкими королевскими лучниками. Хорошо сочетается с растительностью.'
+      ru: 'Лук, обычно используемый низкими королевскими лучниками. Хорошо сочетается с растительностью.',
+      de: 'Ein Bogen, der häufig von niederrangigen königlichen Bogenschützen verwendet wird. Passt sich gut der Vegetation an.',
+      es: 'Un arco comúnmente usado por arqueros reales de bajo rango. Se integra bien con la vegetación.',
+      fr: 'Un arc couramment utilisé par les archers royaux de bas rang. S\'intègre bien à la végétation.'
     }
   },
 
@@ -76,15 +103,20 @@ export default [
     name       : { en: 'Assassin\'s Bow' },
     description: {
       en: 'Masterfully crafted by the bandits of the capitals.',
-      ru: 'Мастерски созданный бандитами столиц.'
-    }
+      ru: 'Мастерски созданный бандитами столиц.',
+      de: 'Meisterhaft gefertigt von den Banditen der Hauptstädte.',
+      es: 'Hecho con maestría por los bandidos de las capitales.',
+      fr: 'Fabriqué avec brio par les bandits des capitales.'
   },
 
   { // T11 item
     name       : { en: 'Skyfire Warbow' },
     description: {
       en: 'A rare bow used in the greatest of wars. Shooting it makes you feel like the heavens will collapse.',
-      ru: 'Редкий лук, используемый в величайших войнах. Bыстрел заставляет вас чувствовать, что небеса рухнут.'
+      ru: 'Редкий лук, используемый в величайших войнах. Bыстрел заставляет вас чувствовать, что небеса рухнут.',
+      de: 'Ein seltener Bogen, der in den größten Kriegen verwendet wird. Beim Schießen hat man das Gefühl, dass der Himmel zusammenbrechen wird.',
+      es: 'Un arco raro usado en la mayor de las guerras. Disparar te hace sentir como si el cielo se fuera a derrumbar.',
+      fr: 'Un arc rare utilisé dans la plus grande des guerres. Tirer dessus vous donne l\'impression que le ciel va s\'effondrer.'
     }
   },
 
@@ -92,7 +124,10 @@ export default [
     name       : { en: 'Hellfire Warbow' },
     description: {
       en: 'What is it? Is it ice? The world may never know...',
-      ru: 'Что это? Это лед? Мир может никогда не узнать ...'
+      ru: 'Что это? Это лед? Мир может никогда не узнать ...',
+      de: 'Was ist das? Was ist das? Ist es Eis? Die Welt wird es vielleicht nie erfahren.....',
+      es: '¿Qué es esto? ¿Es hielo? El mundo tal vez nunca lo sepa...',
+      fr: 'Qu\'est-ce que c\'est ? C\'est de la glace ? Le monde ne le saura peut-être jamais....'
     }
   },
 
@@ -100,7 +135,10 @@ export default [
     name       : { en: 'Widowmaker' },
     description: {
       en: 'Kills as beautifully as it looks.',
-      ru: 'Убивает так же красиво, как и выглядит.'
+      ru: 'Убивает так же красиво, как и выглядит.',
+      de: 'Tötet so schön, wie es aussieht.',
+      es: 'Mata tan maravillosamente como parece.',
+      fr: 'Ça tue aussi joliment que ça en a l\'air.'
     }
   },
 
@@ -108,7 +146,10 @@ export default [
     name       : { en: 'Stormsong' },
     description: {
       en: 'Surrounded by a ghastly aura. The wind blows in the direction you aim it.',
-      ru: 'Окруженный страшной аурой. Ветер дует в том направлении, куда вы нацеливаетесь.'
+      ru: 'Окруженный страшной аурой. Ветер дует в том направлении, куда вы нацеливаетесь.',
+      de: 'Umgeben von einer grässlichen Aura. Der Wind weht in die Richtung, in die du ihn zielst.',
+      es: 'Rodeado de un aura espantosa. El viento sopla en la dirección en que lo apuntas.',
+      fr: 'Entouré d\'une aura affreuse. Le vent souffle dans la direction que vous visez.'
     }
   },
 
@@ -116,7 +157,10 @@ export default [
     name       : { en: 'Scarebow' },
     description: {
       en: 'So scary that few have the guts to hold it.',
-      ru: 'Настолько страшно, что немногие имеют мужество держать его.'
+      ru: 'Настолько страшно, что немногие имеют мужество держать его.',
+      de: 'So beängstigend, dass nur wenige den Mut haben, es zu halten.',
+      es: 'Tan aterrador que pocos tienen las agallas para sostenerlo.',
+      fr: 'Si effrayant que peu de gens ont le courage de le tenir.'
     }
   },
 
@@ -124,7 +168,10 @@ export default [
     name       : { en: 'Fury' },
     description: {
       en: 'Embedded with a primal rage, your foes tremble at the sight of this weapon. Pew pew is the last thing they will ever hear.',
-      ru: 'Окрыленные яростью, ваши противники дрожат при виде этого оружия. Пью Пью - последнее, что они услышат.'
+      ru: 'Окрыленные яростью, ваши противники дрожат при виде этого оружия. Пью Пью - последнее, что они услышат.',
+      de: 'Eingebettet in eine urtümliche Wut, zittern deine Feinde beim Anblick dieser Waffe. Kirchenbank ist das Letzte, was sie je hören werden.',
+      es: 'Envueltos en una furia primigenia, tus enemigos tiemblan al ver esta arma. Pew Pew Pew es lo último que oirán.',
+      fr: 'Encastrés dans une rage primitive, vos ennemis tremblent à la vue de cette arme. Pew pew pew est la dernière chose qu\'ils n\'entendront jamais.'
     }
   }
 
