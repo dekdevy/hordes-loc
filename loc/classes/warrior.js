7697c6d74a2e15d9f6540ee7184c7e7fc0aa38b6
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-11 20:49:25 -0400

    Update warrior.js

--- loc/classes/warrior.js
+++ loc/classes/warrior.js
@@ -4,13 +4,17 @@ export default {
     en: 'Warrior',
     de: 'Krieger',
     se: 'Krigare',
-    ru: 'Воин'
+    ru: 'Воин',
+    es: 'Guerrero',
+    fr: 'Guerrier'
   },
   // Class description, visible during character creation
   description: {
     en: 'Warriors are built to tank more monsters than any other class by having high defense through equipment (Shields) and strong defensive buffs.',
     se: 'Krigare är byggda för att tåla mer skada än alla andra klasser genom högt försvar från sin utrustning (Sköld) och starka försvars Buffar.',
     ru: 'Воины построены для того, чтобы танковать больше монстров чем любой другой класс, имея хорошую защиту с помощью снаряжения (Щита) и сильных защитных усилений.',
-    de: 'Krieger sind so gebaut, dass sie mehr Monster als jede andere Klasse besiegen, indem sie eine hohe Verteidigung durch Ausrüstung (Schilde) und starke Verteidigungszauber haben.'
+    de: 'Krieger sind so gebaut, dass sie mehr Monster als jede andere Klasse besiegen, indem sie eine hohe Verteidigung durch Ausrüstung (Schilde) und starke Verteidigungszauber haben.',
+    es: 'Los guerreros están construidos para lanzar más monstruos que cualquier otra clase al tener una alta defensa a través del equipo (Escudos) y fuertes aficionados defensivos.',
+    fr: 'Les guerriers sont construits pour chasser plus de monstres que n'importe quelle autre classe en ayant une haute défense à travers l'équipement (boucliers) et des buffs défensifs forts.'
   }
 }
