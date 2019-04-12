a9ec6e18b0bff0092cdf869d911a8e9399b95575
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-11 20:43:35 -0400

    Update mage.js

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -3,12 +3,16 @@ export default {
   name: {
     en: 'Mage',
     de: 'Magier',
-    se: 'Magiker'
+    se: 'Magiker',
+    es: 'Mago',
+    fr: 'Mage'
   },
   // Class description, visible during character creation
   description: {
     en: 'Mages deal very heavy large scale Area of Effect damage while also providing utility through slowing targets with their ice based magic.',
     se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.',
-    de: 'Magier verursachen sehr großen Flächenschaden und sind gleichzeitig nützlich, da sie Ziele mit ihrer auf Eis basierenden Magie verlangsamen.'
+    de: 'Magier verursachen sehr großen Flächenschaden und sind gleichzeitig nützlich, da sie Ziele mit ihrer auf Eis basierenden Magie verlangsamen.',
+    es: 'Los magos infligen daños a gran escala a gran escala en el área de efecto, a la vez que proporcionan utilidad a través de la ralentización de los objetivos con su magia basada en el hielo.',
+    fr: 'Les Mages infligent de très lourds dégâts à grande échelle de la Zone d'Effet tout en offrant de l'utilité en ralentissant les cibles grâce à leur magie de glace.'
   }
 }
