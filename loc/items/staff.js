f5bcfa6cf4242c0a60960a7e42fa786b4a71f4b7
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-12 15:32:25 -0400

    Update staff.js

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -3,7 +3,9 @@ export default [
     name       : {en: 'Broken Twig' },
     description: {
       en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.',
-      es: 'La horda de sed de sangre valora la libertad y la lealtad, ofrece un lugar para el individualismo o incluso el caos. Sus defensas están ubicadas en un reino desértico llamado Headless Landing'
+      es: 'Arma forjada por el más grande herrero de toda la tierra. Al menos eso es lo que le dijimos al pequeño Timmy.',
+      de: 'Die Waffe wurde vom größten Schmied des Landes geschmiedet. Zumindest haben wir dem kleinen Timmy das gesagt.',
+      fr: 'Une arme forgée par le plus grand forgeron du pays. Du moins, c\'est ce qu\'on a dit au petit Timmy.'
     }
   },
 
