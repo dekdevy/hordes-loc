c63cc19cc5c9977814395c17c1654446afc05cb4
Nate Johnston <46363319+nateo221@users.noreply.github.com>
2019-04-12 10:27:34 -0600

    termos de varitas
    hice los descripciones por las varitas

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -2,112 +2,128 @@ export default [
   {
     name       : {en: 'Broken Twig' },
     description: {
-      en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.'
+      en: 'Weapon forged by the greatest blacksmith of all the land. At least that\'s what we told little Timmy.',
+      es: 'Este es un palo normal. Pero ¿estás seguro que solo es un palo normal?'
     }
   },
 
   {
     name       : {en: 'Cracked Stick' },
     description: {
-      en: 'Desperate situations call for desperate measures.'
+      en: 'Desperate situations call for desperate measures.',
+      es: 'Has roto su palo. Está bien, todavía puedes usarlo'
     }
   },
 
   {
     name       : {en: 'Gnarled Broomstick' },
     description: {
-      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.'
+      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.',
+      es: 'Blandiendo su nueva escoba, se siente más y más como un mago verdadero.'
     }
   },
 
   {
     name       : {en: 'Oak Stave' },
     description: {
-      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.'
+      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.',
+      es: 'Un bastón hecho de las ramas de un arce anciano. Tiene el gran poder canadiense.'
     }
   },
 
   {
     name       : {en: 'Mystic Wand' },
     description: {
-      en: 'Mystical weapon glowing with a mysterious essence.'
+      en: 'Mystical weapon glowing with a mysterious essence.',
+      es: 'Una varita mágica irradiando con una luz misteriosa.'
     }
   },
 
   {
     name       : {en: 'Bone Stave' },
     description: {
-      en: 'Nothing fancy about this weapon, just the femur of a lich.'
+      en: 'Nothing fancy about this weapon, just the femur of a lich.',
+      es: 'El rey liche no necesitaba su fémur, así que lo usé para hacer una varita.'
     }
   },
 
   {
     name       : {en: 'Encrusted Wand' },
     description: {
-      en: 'Long wand, with a blue shimmering gem at its crown.'
+      en: 'Long wand, with a blue shimmering gem at its crown.',
+      es: 'Una varita larga, con un diamante grande por su corona.'
     }
   },
 
   {
     name       : {en: 'Imbued Staff' },
     description: {
-      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.'
+      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.',
+      es: 'El cayado de un pastor. Solo costó como noventa cobres y una gallina.'
     }
   },
 
   {
     name       : {en: 'Emerald Staff' },
     description: {
-      en: 'Contructed from emeralds stolen from the mountain dwarves.'
+      en: 'Contructed from emeralds stolen from the mountain dwarves.', 
+      es: 'Hay una herrera en las montañas, se llama Esmeralda. Ella solo hace varitas de esmeralda por algún razón.'
     }
   },
 
   {
     name       : {en: 'Sapphire Staff' },
     description: {
-      en: 'Strange......the staff seems.........triangular.....'
+      en: 'Strange......the staff seems.........triangular.....',
+      es: 'Este parece como un triángulo. ¿Cuál tipo de varita es un triángulo?'
     }
   },
 
   {
     name       : {en: 'Frozen Greatstaff' },
     description: {
-      en: 'Eccentric rod with a pale blue stone embedded inside....'
+      en: 'Eccentric rod with a pale blue stone embedded inside....',
+      es: 'Finalmente, ¡una varita de nieve para el mago de invierno!'
     }
   },
 
   {
     name       : {en: 'Infernal Staff' },
     description: {
-      en: 'A gift from the almighty lava god, Erlik.'
+      en: 'A gift from the almighty lava god, Erlik.',
+      es: 'Un regalo del ser magmático Erlik. Es interesante que el Dios del infierno te dio un bastón del invierno.'
     }
   },
 
   {
     name       : {en: 'Hellfire Greatstaff' },
     description: {
-      en: 'Its beauty calls out to you.....to kill.....to conquer....'
+      en: 'Its beauty calls out to you.....to kill.....to conquer....',
+      es: '¿Qué dice este bastón? Dice, \"...mátalos....conquistar el mundo....será divertido......\"'
     }
   },
 
   {
     name       : {en: 'Staff of Angelness' },
     description: {
-      en: 'A staff stolen from the angels. Theyre still hunting for it.'
+      en: 'A staff stolen from the angels. Theyre still hunting for it.',
+      es: 'Robaste el obispo y tomaste su báculo de oro. Ahora los ángeles te cazan.'
     }
   },
 
   {
     name       : {en: 'Crystal Core' },
     description: {
-      en: 'Crude in its looks, yet mighty at its core......'
+      en: 'Crude in its looks, yet mighty at its core......',
+      es: 'No se ve bien, pero ciertamente lanza bien.'
     }
   },
 
   {
     name       : {en: 'Witchs Heart' },
     description: {
-      en: 'We are told its core is from the heart of a witch. No one has found the corpse. Or the staff itself.'
+      en: 'We are told it\'s core is from the heart of a witch. No one has found the corpse. Or the staff itself.',
+      es: 'Nadie lo ha encontrado, pero los mitos dicen que la varita tomes su poder de la corazón de la brujona.'
     }
   },
 
