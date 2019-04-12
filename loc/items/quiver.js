1087645193cb0c78f6bf889e04b819525485714d
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 18:25:15 +0100

    Add files via upload

---
+++ loc/items/quiver.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    name       : {en: 'Linen Quiver' },
+    description: {
+      en: 'Soft, full of holes. Arrows keep falling out.'
+    }
+  },
+
+  {
+    name       : {en: 'Reinforced Quiver' },
+    description: {
+      en: 'Linen sack covered in stiff leather.'
+    }
+  },
+
+  {
+    name       : {en: 'Last Reserves' },
+    description: {
+      en: 'Used for when all else fails.'
+    }
+  },
+
+  {
+    name       : {en: 'Lotharien' },
+    description: {
+      en: 'Elven quiver coated in shimmering diamond.'
+    }
+  },
+
+  {
+    name       : {en: 'Snake Quiver' },
+    description: {
+      en: 'Every time you pull an arrow out, it hisses at you'
+    }
+  }
+
+]
