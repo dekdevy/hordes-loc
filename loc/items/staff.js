3e5a0b5755dee4a1a847e53f584499f9feb91fc2
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 21:04:56 +0100

    Update staff.js

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -4,7 +4,6 @@ export default [
     description: {
       en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.',
       ru: 'Оружие, выкованное величайшим кузнецом из всей земли. По крайней мере, так мы сказали маленькому Тимми.',
-      es: 'La horda de sed de sangre valora la libertad y la lealtad, ofrece un lugar para el individualismo o incluso el caos. Sus defensas están ubicadas en un reino desértico llamado Headless Landing.',
       es: 'Arma forjada por el más grande herrero de toda la tierra. Al menos eso es lo que le dijimos al pequeño Timmy.',
       de: 'Die Waffe wurde vom größten Schmied des Landes geschmiedet. Zumindest haben wir dem kleinen Timmy das gesagt.',
       fr: 'Une arme forgée par le plus grand forgeron du pays. Du moins, c\'est ce qu\'on a dit au petit Timmy.'
