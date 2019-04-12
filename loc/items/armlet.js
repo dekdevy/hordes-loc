eebec8ea26b278c9d335e100945308c2b9dbd565
dArNSiEs <49496964+dArNSiEs@users.noreply.github.com>
2019-04-12 19:50:03 -0400

    Update armlet.js

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -31,6 +31,7 @@ export default [
     description: {
       en: 'Elegant cuff made from the tusks of the last mammoth.',
       ru: 'Элегантная манжета из бивней последнего мамонта.'
+      fr: 'Menottes élégant fait à partir des défenses de mammouths'
     }
   },
 
@@ -48,7 +49,7 @@ export default [
     description: {
       en: 'Dull metal bracer, handy for nightime raidings.',
       ru: 'Унылый металлический браслет, удобный для ночных рейдов.'
-      fr: '
+      fr: 'Terne métal remontant, maniable pour les raids nocturnes'
     }
   },
 
@@ -57,6 +58,7 @@ export default [
     description: {
       en: 'Scaled bracer, makes your arms resemble that of a dragon.',
       ru: 'Чешуйчатый наруч, делает ваши руки похожими на руки дракона.'
+      fr: 'Remontant en écailles, cela fait ressembler vos bras à ceux d`un dragon.
     }
   },
 
@@ -65,6 +67,7 @@ export default [
     description: {
       en: 'Blinds your foes with a magical shine off a devious magicians mirror......',
       ru: 'Ослепляет ваших врагов волшебным блеском с коварного зеркалoм мага ......'
+      fr: 'Aveugle vos ennemis avec un éclat magique sur un miroir de magiciens sournois.....'
     }
   },
 
@@ -73,6 +76,7 @@ export default [
     description: {
       en: 'The archaic guardians left remnants of their passing, which you unscrupulously have decided to wear on your arm.',
       ru: 'Архаичные стражи оставили остатки своего прохождения, которые вы бессовестно решили надеть на руку.'
+      fr: 'Les gardiens archaïques ont laissé des restes de leur passage, que vous avez sans scrupule décidé de porter sur votre bras.'
     }
   },
 
@@ -81,6 +85,7 @@ export default [
     description: {
       en: 'The deceptively light steel offers chilling protection against sneaky blows',
       ru: 'Обманчиво легкая сталь предлагает охлаждающую защиту от подлых ударов'
+      fr: 'L`acier léger trompeur offre la protection contre les coups sournois'
     }
   },
 
@@ -89,6 +94,7 @@ export default [
     description: {
       en: 'Stars are not often black, yet this fragment has a.......deep.......deep darkness.',
       ru: 'Звезды не часто черные, но этот фрагмент имеет ....... глубокую ....... глубокую тьму.'
+      fr: 'Les étoiles ne sont pas souvent noires, mais ce fragment a une.......profund....... obscurité profonde.'
     }
   },
 
@@ -97,6 +103,7 @@ export default [
     description: {
       en: 'Reforged, lost, found, stolen, hidden for eras without end.',
       ru: 'Перекован, потерян, найден, украден, спрятан на века без конца.'
+      fr: 'Reforgé, perdu, trouvé, volé, caché pour des ères sans fin.' 
     }
   },
 
@@ -105,6 +112,7 @@ export default [
     description: {
       en: 'This item was forged on a full moon when mysterious incantations were heard in the whispers of the dark. Some say that it was Anubis himself speaking with the dead.',
       ru: 'Этот предмет был выкован в полнолуние, когда в шепот темноты раздавались таинственные заклинания. Некоторые говорят, что это был сам Анубис, говорящий с мертвыми.'
+      fr: 'Cet article a été forgé à la pleine lune quand des incantations mystérieuses ont été entendues dans les murmures de l`obscurité. Certains disent que c`était Anubis lui-même parlant avec les morts'
     }
   }
 
