ccdf91bfc9943d3806c5783d480bf2df4efdbe75
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 19:39:31 +0100

    Add files via upload

---
+++ loc/items/ring.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    name       : {en: 'Woven Band' },
+    description: {
+      en: 'Dont look at it or it will fall apart'
+    }
+  },
+
+  {
+    name       : {en: 'Ironbark Circlet' },
+    description: {
+      en: 'Not sure about the bark, but its definitely made of iron.'
+    }
+  },
+
+  {
+    name       : {en: 'Brass Ringlet' },
+    description: {
+      en: 'Finally, a ring that is shiny.'
+    }
+  },
+
+  {
+    name       : {en: 'Hollowed Bone' },
+    description: {
+      en: 'A ring made of last nights leftovers'
+    }
+  },
+
+  {
+    name       : {en: 'Elven Band' },
+    description: {
+      en: 'The elves made this ring to protect themselves from danger and suceeded in giving it away.'
+    }
+  },
+
+  {
+    name       : {en: 'Imbued Circlet' },
+    description: {
+      en: 'Fits nicely, though the effects it gives is marginal.'
+    }
+  },
+
+  {
+    name       : {en: 'Arcane Ring' },
+    description: {
+      en: 'No one knows if it is worth putting on.'
+    }
+  },
+
+  {
+    name       : {en: 'Emerald Band' },
+    description: {
+      en: 'Emeralds on all sides to make you feel rich'
+    }
+  },
+
+  {
+    name       : {en: 'Infernal Ring' },
+    description: {
+      en: 'If it could speak, it would probably complain about being triangular.'
+    }
+  },
+
+  {
+    name       : {en: 'Ancient Talisman' },
+    description: {
+      en: 'No written records exist to tell us about its origin......'
+    }
+  },
+
+  {
+    name       : {en: 'Hallowed Relic' },
+    description: {
+      en: 'Sacred artifact lost by holy druids.'
+    }
+  },
+
+  {
+    name       : {en: 'Peacekeeper' },
+    description: {
+      en: 'Keeper of peace.........not sure about that, but just might keep you alive.'
+    }
+  }
+
+]
