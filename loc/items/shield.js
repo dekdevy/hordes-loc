af0f6a16d623c4f865e6262e1e5512cca502c900
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-12 14:29:00 -0400

    Update shield.js

--- loc/items/shield.js
+++ loc/items/shield.js
@@ -1,21 +1,37 @@
 export default [
   { // T1 item name. names are english only!
-    name       : { en: 'Trashy Thing' },
+    name       : { en: 'Smelly Talisman' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T2 item name. names are english only!
+    name       : { en: 'Guided Talisman' },
     // Item description
     description: {
       en: 'Interesting item description'
     }
   },
 
-  { // T2 item. names are english only!
-    name       : { en: 'Wooden Thing' },
+  { // T3 item name. names are english only!
+    name       : { en: 'Ocean Talisman' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+  { // T4 item. names are english only!
+    name       : { en: 'Qui\'ton' },
     description: {
       en: 'Interesting item description'
     }
   },
 
-  { // T3 item. and so on...
-    name       : { en: 'Elven Thing' },
+  { // T5 item. and so on...
+    name       : { en: 'Dimension Talisman' },
     description: {
       en: 'Interesting item description'
     }
