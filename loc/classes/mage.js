907fc9b0081e8866ea26dd901be43e800677c16d
Vedant Bang <33593544+VedantBang@users.noreply.github.com>
2019-04-13 00:09:52 +0530

    Added हिंदी

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -4,13 +4,15 @@ export default {
     en: 'Mage',
     de: 'Magier',
     se: 'Magiker',
-    es: 'Mago'
+    es: 'Mago',
+    hi: 'जादूगर' 
   },
   // Class description, visible during character creation
   description: {
     en: 'Mages deal very heavy large scale Area of Effect damage while also providing utility through slowing targets with their ice based magic.',
     se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.',
     de: 'Magier verursachen sehr großen Flächenschaden und sind gleichzeitig nützlich, da sie Ziele mit ihrer auf Eis basierenden Magie verlangsamen.',
-    es: 'Los magos usan los poderes del invierno para ralentizar y hacer daño massivo a sus enemigos.'
+    es: 'Los magos usan los poderes del invierno para ralentizar y hacer daño massivo a sus enemigos.',
+    hi: 'जादूगर बड़े स्तर पर एक ईलाके में नुक़सान पहुँचा सकते है और साथ ही साथ अपें बर्फ़ीले जादू से दुश्मनो को धीमे भी कर सकते है।'
   }
 }
