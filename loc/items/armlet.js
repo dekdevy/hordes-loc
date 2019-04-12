29896f5bbeda216f0ec7a3f771e0d5bb525e5e8d
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 19:28:40 +0100

    Add files via upload

---
+++ loc/items/armlet.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    name       : {en: 'Simple Bracelet' },
+    description: {
+      en: 'Primitive leather and wooden band. Weak and feeble.'
+    }
+  },
+
+  {
+    name       : {en: 'Makeshift Brace' },
+    description: {
+      en: 'A little more advanced, not by much though.'
+    }
+  },
+
+  {
+    name       : {en: 'Leather Armlet' },
+    description: {
+      en: 'Stiff, makes your arm sore.'
+    }
+  },
+
+  {
+    name       : {en: 'Ivory Bracelet' },
+    description: {
+      en: 'Elegant cuff made from the tusks of the last mammonth.'
+    }
+  },
+
+  {
+    name       : {en: 'Iron Vambrace' },
+    description: {
+      en: 'Heavy on your arm.....worth the protection.'
+    }
+  },
+
+  {
+    name       : {en: 'Imbued Bracers' },
+    description: {
+      en: 'Dull metal bracer, handy for nightime raidings.'
+    }
+  },
+
+  {
+    name       : {en: 'Ember Cuffs' },
+    description: {
+      en: 'Scaled bracer, makes your arms resemble that of a dragon.'
+    }
+  },
+
+  {
+    name       : {en: 'Mirrored Armlet' },
+    description: {
+      en: 'Blinds your foes with the shine of Ra.'
+    }
+  },
+
+  {
+    name       : {en: 'Golem Fragment' },
+    description: {
+      en: 'The archaic guardians left remnants of their passing, which you unscrupulously have decided to wear on your arm.'
+    }
+  },
+
+  {
+    name       : {en: 'Coldforged Bracer' },
+    description: {
+      en: 'The deceptively light steel offers chilling protection against sneaky blows'
+    }
+  },
+
+  {
+    name       : {en: 'Blackstar Gem' },
+    description: {
+      en: 'Stars are not often black, yet this fragment has a.......deep.......deep darkness.'
+    }
+  },
+
+  {
+    name       : {en: 'Eternal Vambraces' },
+    description: {
+      en: 'Reforged, lost, found, stolen, hidden for eras without end.'
+    }
+  },
+
+  {
+    name       : {en: 'Lost Guardian' },
+    description: {
+      en: 'This item was forged on a full moon when mysterious incantations were heard in the whispers of the dark. Some say that it was Anubis himself speaking with the dead.'
+    }
+  }
+
+]
