0fc53769b0816018bc8d34dded4c1973c0af0dbc
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 21:49:31 +0100

    Add files via upload

---
+++ loc/items/gloves.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    name       : {en: 'Hand Wraps' },
+    description: {
+      en: 'Straps of leather covering your feeble hands. Pulling bowstring is much easier for you now'
+    }
+  },
+
+  {
+    name       : {en: 'Cloth Mitts' },
+    description: {
+      en: 'Spun yarn from stolen sheep makes these warm gloves a pleasure to wear'
+    }
+  },
+
+  {
+    name       : {en: 'Leather Gloves' },
+    description: {
+      en: 'Your fingers can barely move in these stiff leather mitts'
+    }
+  },
+
+  {
+    name       : {en: 'Bone Grips' },
+    description: {
+      en: 'Clacking sounds travel with you as you wear these'
+    }
+  },
+
+  {
+    name       : {en: 'Iron Gauntlets' },
+    description: {
+      en: 'Soft iron gloves provide a little more protection, although your hands are cold now'
+    }
+  },
+
+  {
+    name       : {en: 'Imbued Gloves' },
+    description: {
+      en: 'Mitts made out of unknown material....it shines.....'
+    }
+  },
+
+  {
+    name       : {en: 'Wartorn Mitts' },
+    description: {
+      en: 'Battle-hard war general must have dropped these.....probably unintentionally too'
+    }
+  },
+
+  {
+    name       : {en: 'Fiery Handguards' },
+    description: {
+      en: 'Fire is no match for your hands as you wear these green mitts'
+    }
+  },
+
+  {
+    name       : {en: 'Gloves of Empowerment' },
+    description: {
+      en: 'Gives a false sense of power'
+    }
+  },
+
+  {
+    name       : {en: 'Coldforged Fists' },
+    description: {
+      en: 'Forged without heat, worn without fear'
+    }
+  },
+
+  {
+    name       : {en: 'Eternal Gauntlets' },
+    description: {
+      en: 'Gloves sought by warriors for eras on end...at last....it is found'
+    }
+  },
+
+  {
+    name       : {en: 'Gloves of Midas' },
+    description: {
+      en: 'Rumours say that a touch from these gloves turns your foes into gold......your hand is strangely heavy....'
+    }
+  },
+
+  {
+    name       : {en: 'Titans Mitts' },
+    description: {
+      en: 'Gloves so powerful in their nature that the very air shimmers as you wave your hand.'
+    }
+  }
+
+]
