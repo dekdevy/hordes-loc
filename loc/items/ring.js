e15a3c97b1b8c4e8224d507876b892b06f76d454
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-12 13:46:05 -0400

    Update ring.js

--- loc/items/ring.js
+++ loc/items/ring.js
@@ -1,6 +1,6 @@
 export default [
   { // T1 item name. names are english only!
-    name       : { en: 'Trashy Thing' },
+    name       : { en: 'Woven Band' },
     // Item description
     description: {
       en: 'Interesting item description'
@@ -8,18 +8,96 @@ export default [
   },
 
   { // T2 item. names are english only!
-    name       : { en: 'Wooden Thing' },
+    name       : { en: 'Ironbark Circlet' },
     description: {
       en: 'Interesting item description'
     }
   },
 
   { // T3 item. and so on...
-    name       : { en: 'Elven Thing' },
+    name       : { en: 'Brass Ringlet' },
     description: {
       en: 'Interesting item description'
     }
-  }
+  },
 
-  // keep adding objects here for t4,t5, etc..
-]
+  // T4 item. 
+ { // T1 item name. names are english only!
+    name       : { en: 'Hollowed Bone' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T5 item name. names are english only!
+    name       : { en: 'Elven Band' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T6 item name. names are english only!
+    name       : { en: 'Imbued Circlet' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T7 item name. names are english only!
+    name       : { en: 'Arcane Ring' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T8 item name. names are english only!
+    name       : { en: 'Emerald Band' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T9 item name. names are english only!
+    name       : { en: 'Infernal Ring' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T10 item name. names are english only!
+    name       : { en: 'Hallowed Relic' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T11 item name. names are english only!
+    name       : { en: 'Cantor\'s Trinket' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T12 item name. names are english only!
+    name       : { en: 'Peacekeeper' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T13 item name. names are english only!
+    name       : { en: '' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
