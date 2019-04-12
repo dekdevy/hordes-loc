85fde9ab8ba0cae1861fed5211e55ae515fcecb7
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-12 10:55:35 -0400

    Update staff.js

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -2,112 +2,158 @@ export default [
   {
     name       : {en: 'Broken Twig' },
     description: {
-      en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.'
+      en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.',
+      es: 'La horda de sed de sangre valora la libertad y la lealtad, ofrece un lugar para el individualismo o incluso el caos. Sus defensas están ubicadas en un reino desértico llamado Headless Landing'
     }
   },
 
   {
     name       : {en: 'Cracked Stick' },
     description: {
-      en: 'Desperate situations call for desperate measures.'
+      en: 'Desperate situations call for desperate measures.',
+      es: 'Las situaciones desesperadas requieren medidas desesperadas.',
+      de: 'Verzweifelte Situationen erfordern verzweifelte Maßnahmen.',
+      fr: 'Les situations désespérées exigent des mesures désespérées.'
     }
   },
 
   {
     name       : {en: 'Gnarled Broomstick' },
     description: {
-      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.'
+      en: 'Scruffy broomstick stolen from a mage. Gnarled and bony.',
+      es: 'Un palo de escoba desaliñado robado de un mago. Nudoso y huesudo.',
+      de: 'Dreckiger Besenstiel, der einem Magier gestohlen wurde. Knurrig und knöchern.',
+      fr: 'Un balai débraillé volé à un mage. Gnarled et osseux.'
     }
   },
 
   {
     name       : {en: 'Oak Stave' },
     description: {
-      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.'
+      en: 'A wand cut from the branches of an anceint oak. Infused with deep power.',
+      es: 'Una varita cortada de las ramas de un roble anceint. Infundido con un profundo poder.',
+      de: 'Ein Zauberstab, der aus den Zweigen einer kastanienbraunen Eiche geschnitten wurde. Durchdrungen von tiefer Kraft.',
+      fr: 'Baguette coupée dans les branches d\'un chêne d\'anceint. Doté d\'une puissance profonde.'
     }
   },
 
   {
     name       : {en: 'Mystic Wand' },
     description: {
-      en: 'Mystical weapon glowing with a mysterious essence.'
+      en: 'Mystical weapon glowing with a mysterious essence.',
+      es: 'Arma mística que brilla con una esencia misteriosa.',
+      de: 'Mystische Waffe, die mit einer mysteriösen Essenz angestrahlt wird.',
+      fr: 'Une arme mystique qui brille \'une essence mystérieuse.'
     }
   },
 
   {
     name       : {en: 'Bone Stave' },
     description: {
-      en: 'Nothing fancy about this weapon, just the femur of a lich.'
+      en: 'Nothing fancy about this weapon, just the femur of a lich.',
+      es: 'Esta arma no tiene nada de elegante, sólo el fémur de un liquen.',
+      de: 'Nichts Besonderes an dieser Waffe, nur der Oberschenkelknochen eines Lichs.',
+      fr: 'Cette arme n\'a rien d\'extravagant, juste le fémur d\'un lich.'
     }
   },
 
   {
     name       : {en: 'Encrusted Wand' },
     description: {
-      en: 'Long wand, with a blue shimmering gem at its crown.'
+      en: 'Long wand, with a blue shimmering gem at its crown.',
+      es: 'Varita larga, con una joya azul brillante en su corona.',
+      de: 'Langer Stab, mit einem blau schimmernden Edelstein an der Krone.',
+      fr: 'Longue baguette, avec une gemme bleue chatoyante à sa couronne.'
     }
   },
 
   {
     name       : {en: 'Imbued Staff' },
     description: {
-      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.'
+      en: 'Dark purple staff. Quite mysterious in its making, yet nice to look at.',
+      es: 'Bastón de color púrpura oscuro. Bastante misterioso en su fabricación, pero agradable de ver.',
+      de: 'Dunkelvioletter Stab. Ziemlich mysteriös in seiner Entstehung, aber schön anzusehen.',
+      fr: 'Bâton violet foncé. Assez mystérieux dans sa fabrication, mais agréable à regarder.'
     }
   },
 
   {
     name       : {en: 'Emerald Staff' },
     description: {
-      en: 'Contructed from emeralds stolen from the mountain dwarves.'
+      en: 'Constructed from emeralds stolen from the mountain dwarves.',
+      es: 'Construido con esmeraldas robadas a los enanos de la montaña.',
+      de: 'Gebaut aus Smaragden, die den Bergweibchen gestohlen wurden.',
+      fr: 'Construit à partir d\'émeraudes volées aux nains des montagnes.'
     }
   },
 
   {
     name       : {en: 'Sapphire Staff' },
     description: {
-      en: 'Strange......the staff seems.........triangular.....'
+      en: 'Strange......the staff seems.........triangular.....',
+      es: 'Extraño, el bastón parece, triangular...',
+      de: 'Seltsam, der Stab scheint, dreieckig.....',
+      fr: 'Bizarre, le bâton semble triangulaire....'
     }
   },
 
   {
     name       : {en: 'Frozen Greatstaff' },
     description: {
-      en: 'Eccentric rod with a pale blue stone embedded inside....'
+      en: 'Eccentric rod with a pale blue stone embedded inside....',
+      es: 'Varilla excéntrica con una piedra de color azul pálido incrustada en el interior......',
+      de: 'Exzentrische Stange mit einem hellblauen Stein, der innen eingebettet ist.......',
+      fr: 'Tige excentrique avec une pierre bleu pâle à l\'intérieur......'
     }
   },
 
   {
     name       : {en: 'Infernal Staff' },
     description: {
-      en: 'A gift from the almighty lava god, Erlik.'
+      en: 'A gift from the almighty lava god, Erlik.',
+      es: 'Un regalo del todopoderoso dios de la lava, Erlik.',
+      de: 'Ein Geschenk des allmächtigen Lavagottes Erlik.',
+      fr: 'Un cadeau du dieu tout-puissant de la lave, Erlik.'
     }
   },
 
   {
     name       : {en: 'Hellfire Greatstaff' },
     description: {
-      en: 'Its beauty calls out to you.....to kill.....to conquer....'
+      en: 'Its beauty calls out to you.....to kill.....to conquer...',
+      es: 'Su belleza te llama a ti, a matar, a conquistar...',
+      de: 'Seine Schönheit ruft nach dir, zu töten, zu erobern...',
+      fr: 'Sa beauté vous interpelle, pour tuer, pour conquérir...'
     }
   },
 
   {
     name       : {en: 'Staff of Angelness' },
     description: {
-      en: 'A staff stolen from the angels. Theyre still hunting for it.'
+      en: 'A staff stolen from the angels. Theyre still hunting for it.',
+      es: 'Un bastón robado a los ángeles. Todavía lo están buscando.',
+      de: 'Ein Stab, der von den Engeln gestohlen wurde. Sie sind immer noch auf der Jagd danach.',
+      fr: 'Un bâton volé aux anges. Ils sont toujours à sa recherche.'
     }
   },
 
   {
     name       : {en: 'Crystal Core' },
     description: {
-      en: 'Crude in its looks, yet mighty at its core......'
+      en: 'Crude in its looks, yet mighty at its core...',
+      es: 'Crudo en su aspecto, pero poderoso en su núcleo....',
+      de: 'Grob in seinem Aussehen, aber mächtig in seinem Kern.....',
+      fr: 'Brut dans son apparence, mais puissant dans son essence....'
     }
   },
 
   {
     name       : {en: 'Witchs Heart' },
     description: {
-      en: 'We are told its core is from the heart of a witch. No one has found the corpse. Or the staff itself.'
+      en: 'We are told its core is from the heart of a witch. No one has found the corpse. Or the staff itself.',
+      es: 'Se nos dice que su núcleo es del corazón de una bruja. Nadie ha encontrado el cadáver. O el propio personal.',
+      de: 'Uns wurde gesagt, dass sein Kern aus dem Herzen einer Hexe stammt. Niemand hat die Leiche gefunden. Oder das Personal selbst.',
+      fr: 'On nous dit que son coeur est celui d\'une sorcière. Personne n\'a trouvé le cadavre. Ou le personnel lui-même.'
     }
   },
 
@@ -115,7 +161,9 @@ export default [
     name       : {en: 'Deathweaver' },
     description: {
       en: 'This weapon is so secretive that the few who wield it have killed all that know of it.',
-      es: 'Se dicen que nadie se queda vivo para contar de este arco, pero... de donde viene las historias?'
+      es: 'Esta arma es tan secreta que los pocos que la manejan han matado a todos los que la conocen.',
+      de: 'Diese Waffe ist so geheimnisvoll, dass die wenigen, die sie führen, alle getötet haben, die von ihr wissen.',
+      fr: 'Cette arme est si secrète que les quelques personnes qui l\'utilisent ont tué tous ceux qui la connaissent.'
     }
   }
 
