27b03aee6266b7f32dca13310c33535074ae48bf
Vedant Bang <33593544+VedantBang@users.noreply.github.com>
2019-04-12 21:57:44 +0530

    Added हिंदी

--- loc/ui/charmenu.js
+++ loc/ui/charmenu.js
@@ -6,27 +6,32 @@ export default {
     title: {
       en: 'Select Character',
       se: 'Välj Karaktär',
-      ru: 'Выберите Персонажа'
+      ru: 'Выберите Персонажа',
+      hi: 'स्वरूप चुने'
     },
     create: {
       en: 'Click to create character',
       se: 'Klicka för att skapa karaktär',
-      ru: 'Нажмите, чтобы создать персонажа'
+      ru: 'Нажмите, чтобы создать персонажа',
+      hi: 'स्वरूप बानाने के लिए क्लिक करे'
     },
     enterWorld: {
       en: 'Enter World',
       se: 'Starta Värld',
-      ru: 'Войти в мир'
+      ru: 'Войти в мир',
+      hi: 'प्रवेश करे'
     },
     emptySlot: {
       en: 'Empty',
       se: 'Ingen',
-      ru: 'Пусто'
+      ru: 'Пусто',
+      hi: 'रिक्त'
     },
     error: {
       en: 'Network error',
       se: 'Nätverks problem',
-      ru: 'Проблема с сетью'
+      ru: 'Проблема с сетью',
+      hi: 'नेट्वर्क में त्रुटि'
     }
   },
 
@@ -35,32 +40,38 @@ export default {
     title: {
       en: 'Character creation',
       se: 'Skapa karaktär',
-      ru: 'Создание персонажа'
+      ru: 'Создание персонажа',
+      hi: 'स्वरूप निर्माण'
     },
     pressIcon: {
       en: 'Press an icon for a brief description.',
       se: 'Klicka en ikon för beskrivning.',
-      ru: 'Нажмите на иконку, чтобы увидеть краткое описание.'
+      ru: 'Нажмите на иконку, чтобы увидеть краткое описание.',
+      hi: 'संक्षिप्त विवरण के लिए तस्वीर पर क्लिक करे।'
     },
     selectClass: {
       en: 'Select a class',
       se: 'Välj en klass',
-      ru: 'Выберите класс'
+      ru: 'Выберите класс',
+      hi: 'श्रेणी चुने'
     },
     selectFaction: {
       en: 'Select a faction',
       se: 'Välj en sida',
-      ru: 'Выберите фракцию'
+      ru: 'Выберите фракцию',
+      hi: 'दल चुने'
     },
     enterName: {
       en: 'Enter a name',
       se: 'Välj ett namn',
-      ru: 'Введите имя'
+      ru: 'Введите имя',
+      hi: 'नाम डाले'
     },
     nameReq: {
       en: 'Your name can only contain characters a-Z, and numbers 0-9, without any spaces. Up to 16 characters.',
       se: 'Ditt namn kan bara använda sig karaktärerna a-Z, och nummer 0-9, utan mellanslag. Max 16 karaktärer.',
-      ru: 'Ваше имя может состоять только из латинского алфавита a-Z и цифр 0-9, без пробелов. До 16 символов.'
+      ru: 'Ваше имя может состоять только из латинского алфавита a-Z и цифр 0-9, без пробелов. До 16 символов.',
+      hi: 'आपके नाम में केवल a-Z अक्षर और 0-9 अंक हो सकते है| अधिकतम 16 अक्षर।'
     }
   },
 
@@ -70,17 +81,20 @@ export default {
     title: {
       en: 'Delete',
       se: 'Ta bort',
-      ru: 'Удалить'
+      ru: 'Удалить',
+      hi: 'हटा दे'
     },
     info: {
       en: 'To delete, type the characters name. Your character can not be restored.',
       se: 'För att ta bort, skriv in karaktärens namn. Din karaktär tas bort för alltid.',
-      ru: 'Чтобы удалить, введите имя персонажа. Ваш персонаж не может быть восстановлен.'
+      ru: 'Чтобы удалить, введите имя персонажа. Ваш персонаж не может быть восстановлен.',
+      hi: 'हटाने के लिए नाम डाले आपका स्वरूप पुनर नहीं मिल पाएगा।'
     },
     placeholder: {
       en: 'Are you sure?',
       se: 'Är du säker?',
-      ru: 'Вы уверены?'
+      ru: 'Вы уверены?',
+      hi: 'पक्की तौर पर?'
     }
   }
 
