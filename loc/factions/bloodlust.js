964f33b884ac9771583fa30b6b411cdee86b0d2e
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-11 20:33:41 -0400

    Update bloodlust.js

--- loc/factions/bloodlust.js
+++ loc/factions/bloodlust.js
@@ -2,11 +2,16 @@ export default {
   name: {
     en: 'Bloodlust',
     se: 'Bloodlust',
-    ru: 'Bloodlust'
+    ru: 'Bloodlust',
+    de: 'Blutgier',
+    es: 'Sed de sangre'
   },
   description: {
     en: 'The Bloodlust horde values freedom and loyalty, offers a place for individualism or even chaos. Their Defenses are located in a desert realm called Headless Landing.',
     se: 'Bloodlust värderar frihet och lojalitet, erbjunder en plats för individualism men också kaos. Deras försvar hittas i ett öken kungarike vid namn Headless Landing.',
-    ru: 'Орда Bloodlust славится свободой и лояльностью, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном мире, названном Headless Landing.'
+    ru: 'Орда Bloodlust славится свободой и лояльностью, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном мире, названном Headless Landing.',
+    de: 'Die Horde der Blutgier schätzt Freiheit und Loyalität, bietet einen Ort für Individualismus oder gar Chaos. Ihre Verteidigung befindet sich in einem Wüstenreich namens Headless Landing.',
+    es: 'La horda de sed de sangre valora la libertad y la lealtad, ofrece un lugar para el individualismo o incluso el caos. Sus defensas están ubicadas en un reino desértico llamado Headless Landing.'
   }
+  
 }
