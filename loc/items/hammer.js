12c35c015db7666f6dec233626d1de7419067086
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 17:47:31 +0100

    Update hammer.js

--- loc/items/hammer.js
+++ loc/items/hammer.js
@@ -2,119 +2,137 @@ export default [
   {
     name       : {en: 'Splintered Club' },
     description: {
-      en: 'Its already half broken and you are still using it?'
+      en: 'Its already half broken and you are still using it?',
+      ru: 'Это уже наполовину сломано, и вы все еще используете его?'
     }
   },
 
   {
     name       : {en: 'Wooden Mallet' },
     description: {
-      en: 'This should be hammering nails, not skulls. '
+      en: 'This should be hammering nails, not skulls.',
+      ru: 'Это должны быть гвозди, а не черепа.'
     }
   },
 
   {
     name       : {en: 'Rusty Flail' },
     description: {
-      en: 'Rusty old hammer, on the verge of breaking.'
+      en: 'Rusty old hammer, on the verge of breaking.',
+      ru: 'Ржавый старый молот, на грани поломки.'
     }
   },
 
   {
     name       : {en: 'Orcish Bludgeon' },
     description: {
-      en: 'Orcs follow the wielder of this meager weapon. Its pretty useless, but they seem to like it......'
+      en: 'Orcs follow the wielder of this meager weapon. Its pretty useless, but they seem to like it......',
+      ru: 'Орки следуют за обладателем этого оружия. Oн довольно бесполезно, но им похоже нравится ......'
     }
   },
 
   {
     name       : {en: 'Heavy Mace' },
     description: {
-      en: 'Large weapon used in the ancient times to cave in skulls.'
+      en: 'Large weapon used in the ancient times to cave in skulls.',
+      ru: 'Большое оружие, используемое в древние времена для разрушенея черепаx.'
     }
   },
 
   {
     name       : {en: 'Iron Basher' },
     description: {
-      en: 'Slender rod-like weapon. Lightweight and fairly useless.'
+      en: 'Slender rod-like weapon. Lightweight and fairly useless.',
+      ru: 'Тонкое прутообразное оружие. Легкая и довольно бесполезное.'
     }
   },
 
   {
     name       : {en: 'Darkmetal Maul' },
     description: {
-      en: 'Ancient dwarves forged this mighty structure. Dark and deadly.'
+      en: 'Ancient dwarves forged this mighty structure. Dark and deadly.',
+      ru: 'Древние гномы создали эту могущественную структуру. Темноя и смертельно.'
     }
   },
 
   {
     name       : {en: 'Divine Gavel' },
     description: {
-      en: 'Bronze hammer, triangular shaped. Sharp corners make deep indents in heads.'
+      en: 'Bronze hammer, triangular shaped. Sharp corners make deep indents in heads.',
+      ru: 'Бронзовый молот, треугольной формы. Острые углы делают глубокие выемки в головах.'
     }
   },
 
   {
     name       : {en: 'Hallowed Hammer' },
     description: {
-      en: 'Holy tool used by druids to make sacrifices to the great god, Yggdrasil.'
+      en: 'Holy tool used by druids to make sacrifices to the great god, Yggdrasil.',
+      ru: 'Святой инструмент, используемый друидами, чтобы приносить жертвы великому богу Иггдрасилю.'
     }
   },
 
   {
     name       : {en: 'Dwarven Maul' },
     description: {
-      en: 'Heavy basher used by dwarf warriors. Made of an unknown metal.'
+      en: 'Heavy basher used by dwarf warriors. Made of an unknown metal.',
+      ru: 'Тяжелый башер, используемый гномами-воинами. Изготовлен из неизвестного металла.'
     }
   },
 
   {
     name       : {en: 'Coldforged Gavel' },
     description: {
-      en: 'An ancient headstone transformed into a deadly hammer. Cold to touch.'
+      en: 'An ancient headstone transformed into a deadly hammer. Cold to touch.',
+      ru: 'Древний надгробие превращается в смертельный молот. Xалодный на ощупь.'
     }
   },
 
   {
     name       : {en: 'Amboss' },
     description: {
-      en: 'Topped with the skull of an enemy, this weapon inspires terror in all who stand against it. '
+      en: 'Topped with the skull of an enemy, this weapon inspires terror in all who stand against it.',
+      ru: 'Это оружие, увенчанное черепом врага, внушает ужас всем, кто противостоит ему.'
     }
   },
 
   {
     name       : {en: 'Skullshatterer' },
     description: {
-      en: 'Spiky. Can penetrate your feeble skull quite easily. Or your foes. Probably yours though.'
+      en: 'Can penetrate your feeble skull quite easily. Or your foes. Probably yours though.',
+      ru: 'Может легко проникнуть в ваш слабый череп. Или твои враги. Вероятно, ваш, хотя.'
     }
   },
 
   {
     name       : {en: 'Benevolence' },
     description: {
-      en: 'Nothing kind about this weapon.'
+      en: 'Nothing kind about this weapon.',
+      ru: 'Ничего доброго в этом оружии.'
     }
   },
 
   {
     name       : {en: 'Hammer of Gaia' },
     description: {
-      en: 'Simply a wooden rod with a mass of iron on top. Crude, yet effective.'
+      en: 'Simply a wooden rod with a mass of iron on top. Crude, yet effective.',
+      ru: 'Просто деревянный стержень с массой железа на вершине. Грубая, но эффективная.'
     }
   },
 
   {
     name       : {en: 'Worldender' },
     description: {
-      en: 'Does it have the power to end worlds? Few can say. If it does, we will not live long enough to learn of it.'
+      en: 'Does it have the power to end worlds? Few can say. If it does, we will not live long enough to learn of it.',
+      ru: 'Есть ли у него сила, чтобы положить конец мирам? Мало кто может сказать. Если это произойдет, мы не будем жить достаточно долго, чтобы узнать об этом.
+'
     }
   },
 
   {
     name       : {en: 'Nightmare' },
     description: {
-      en: 'Be afraid. Be very, very afraid.'
+      en: 'Be afraid. Be very, very afraid.',
+      ru: 'Бойтесь. Очень oчень бойтесь.'
     }
   }
 
