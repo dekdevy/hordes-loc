fe5b03c6bd199407f946c182af067a705433c1b2
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 17:58:40 +0100

    Update talisman.js

--- loc/items/talisman.js
+++ loc/items/talisman.js
@@ -2,35 +2,40 @@ export default [
   {
     name       : {en: 'Smelly Talisman' },
     description: {
-      en: 'Smells so bad, I wonder why you even picked it up.'
+      en: 'Smells so bad, I wonder why you even picked it up.',
+      ru: 'Пахнет так плохо, я удивляюсь, что ты даже поднял его.'
     }
   },
 
   {
     name       : {en: 'Guided Talisman' },
     description: {
-      en: 'Throw it at the sun and it will tell you where to go.'
+      en: 'Throw it at the sun and it will tell you where to go.',
+      ru: 'Брось его на солнце, и он скажет тебе, куда идти.'
     }
   },
 
   {
     name       : {en: 'Ocean Talisman' },
     description: {
-      en: 'Surprisingly, it doesnt smell like fish. Just dont show it freshwater.'
+      en: 'Surprisingly, it doesnt smell like fish. Just dont show it freshwater.',
+      ru: 'Удивительно, но он не пахнет рыбой. Просто не показывай ему пресную воду.'
     }
   },
 
   {
     name       : {en: 'Quiton' },
     description: {
-      en: 'Cant pronounce it, cant use it.'
+      en: 'Cant pronounce it, cant use it.',
+      ru: 'Не могу произнести, не могу использовать.'
     }
   },
 
   {
     name       : {en: 'Dimension Talisman' },
     description: {
-      en: 'Ripped from between worlds, this talisman allows for transition between realms.'
+      en: 'Ripped from between worlds, this talisman allows for transition between realms.',
+      ru: 'Разорванный между мирами, этот талисман позволяет переходить между вселенные.'
     }
   }
 
