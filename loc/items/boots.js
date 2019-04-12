82a8fedf73340f67be6110513b58626c0e702c60
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 21:28:55 +0100

    Add files via upload

---
+++ loc/items/boots.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    name       : {en: 'Sandals' },
+    description: {
+      en: 'At least you are wearing something on those feet. '
+    }
+  },
+
+  {
+    name       : {en: 'Cloth Footpads' },
+    description: {
+      en: 'Soles made from cloth, will probably tear after a few hours.'
+    }
+  },
+
+  {
+    name       : {en: 'Leather Boots' },
+    description: {
+      en: 'The soft leather keeps your feet warm while providing a little protection from '
+    }
+  },
+
+  {
+    name       : {en: 'Bone Greaves' },
+    description: {
+      en: 'Clearly lacking in imagination, the maker made boots from the bones of a very large creature...'
+    }
+  },
+
+  {
+    name       : {en: 'Scaled Treads' },
+    description: {
+      en: 'Scales line line the outside of your footwear, cathing glares in the sun'
+    }
+  },
+
+  {
+    name       : {en: 'Patterned Greaves' },
+    description: {
+      en: 'A pleasant pattern made to paralyze your foes....wonder if it works'
+    }
+  },
+
+  {
+    name       : {en: 'Wartorn Boots' },
+    description: {
+      en: 'Greaves taken from the battle-hardened war veterans'
+    }
+  },
+
+  {
+    name       : {en: 'Imbued Treads' },
+    description: {
+      en: 'Blocky in its looks, probably very uncomfortable'
+    }
+  },
+
+  {
+    name       : {en: 'Skyswift Boots' },
+    description: {
+      en: 'Run as quick as the wind....or close to it'
+    }
+  },
+
+  {
+    name       : {en: 'Coldforged Greaves' },
+    description: {
+      en: 'Forged without heat, runs without flaw'
+    }
+  },
+
+  {
+    name       : {en: 'Cloudrunner Treads' },
+    description: {
+      en: 'Some say you can run on clouds with these. The few that have tried have already ascended.'
+    }
+  },
+
+  {
+    name       : {en: 'Boots of Hermes' },
+    description: {
+      en: 'Ancient boots rumoured to be stolen from the feet of the Roman messenger god....'
+    }
+  },
+
+  {
+    name       : {en: 'Starshard Greaves' },
+    description: {
+      en: 'Stars were always considered to be all-mighty, now theyre yours'
+    }
+  }
+
+]
