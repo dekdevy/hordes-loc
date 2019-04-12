d9c726b1bec2c808c28bc6353de41128b8fb7b4d
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 17:52:16 +0100

    Update shield.js

--- loc/items/shield.js
+++ loc/items/shield.js
@@ -2,35 +2,40 @@ export default [
   {
     name       : {en: 'Wooden Shield' },
     description: {
-      en: 'Literally a square plank of wood. '
+      en: 'Literally a square plank of wood.',
+      ru: 'Буквально квадратная доска из дерева.'
     }
   },
 
   {
     name       : {en: 'Buckler' },
     description: {
-      en: 'Smelted iron ring filled with bronze. Flimsy in its making.'
+      en: 'Smelted iron ring filled with bronze. Flimsy in its making.',
+      ru: 'Плавленое железное кольцо заполнено бронзой. Хлипкий в его создании.'
     }
   },
 
   {
     name       : {en: 'Old Bulwark' },
     description: {
-      en: 'Some farmer has managed to contruct a cute little shield.'
+      en: 'Some farmer has managed to contruct a cute little shield.',
+      ru: 'Некоторому фермеру удалось создать милый маленький щит.'
     }
   },
 
   {
     name       : {en: 'Metal Guard' },
     description: {
-      en: 'Intricate shield made out of 3 solid metal pieces. Sturdy.'
+      en: 'Intricate shield made out of three solid metal pieces. Sturdy.',
+      ru: 'Замысловатый щит из трех металлических частей. Прочная'
     }
   },
 
   {
     name       : {en: 'Protectron' },
     description: {
-      en: 'Made from obsidian and steel. Someone sloppily attempted to paint it yellow. They failed. Miserably.'
+      en: 'Made from obsidian and steel. Someone sloppily attempted to paint it yellow. They failed. Miserably.',
+      ru: 'Изготовлен из обсидиана и стали. Кто-то небрежно попытался покрасить его в желтый цвет. Hе удалось.'
     }
   }
 
