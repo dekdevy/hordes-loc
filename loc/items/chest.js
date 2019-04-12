4edce3467b55994090169ee39e72b658e6039e3b
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 18:42:31 +0100

    Add files via upload

---
+++ loc/items/chest.js
@@ -0,0 +1,79 @@
+export default [
+  {
+    name       : {en: 'Potato Sack' },
+    description: {
+      en: 'Made from the skins of starch, provides weak protection against the forces of carbohydrates.'
+    }
+  },
+
+  {
+    name       : {en: 'Faded Garment' },
+    description: {
+      en: 'Leather shirt, faded from wear and tear.'
+    }
+  },
+
+  {
+    name       : {en: 'Adventurers Tunic' },
+    description: {
+      en: 'Knobbly soft iron tunic, passed on from many generations of adventurers.'
+    }
+  },
+
+  {
+    name       : {en: 'Quilted Leather' },
+    description: {
+      en: 'This fabric isnt much, but it might stop an arrow or two.'
+    }
+  },
+
+  {
+    name       : {en: 'Scaled Chestguard' },
+    description: {
+      en: 'Hard suit made from scales taken from the mighty dragon, Cantor.'
+    }
+  },
+
+  {
+    name       : {en: 'Sky Cloak' },
+    description: {
+      en: 'Rumours say its made from the clouds.....'
+    }
+  },
+
+  {
+    name       : {en: 'Shadow Cloak' },
+    description: {
+      en: 'The wearer blends nicely within the shadows.....or so the thieves think'
+    }
+  },
+
+  {
+    name       : {en: 'Obsidian Cloak' },
+    description: {
+      en: 'Matches Dhwtys Obsidian Crown.'
+    }
+  },
+
+  {
+    name       : {en: 'Hellfire Cloak' },
+    description: {
+      en: 'Forged from the depths of hell, it provides protection for your weak, fleshy body.'
+    }
+  },
+
+  {
+    name       : {en: 'Soulkeeper' },
+    description: {
+      en: 'When enemies are slain, their souls are added to the wielders collection.'
+    }
+  },
+
+  {
+    name       : {en: 'Deathless' },
+    description: {
+      en: 'Rumours say that the wearer is immortal. Unfortunately this armour can only be found on corpses of the slain.'
+    }
+  }
+
+]
