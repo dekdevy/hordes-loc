96feda4e1ca769e2aca5f41df0fb5e837bdc5c60
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 17:23:43 +0100

    Add files via upload

---
+++ loc/items/shield.js
@@ -0,0 +1,120 @@
+export default [
+  {
+    name       : {en: 'Wooden Shield' },
+    description: {
+      en: 'Literally a square plank of wood. '
+    }
+  },
+
+  {
+    name       : {en: 'Buckler' },
+    description: {
+      en: 'Smelted iron ring filled with bronze. Flimsy in its making.'
+    }
+  },
+
+  {
+    name       : {en: 'Old Bulwark' },
+    description: {
+      en: 'Some farmer has managed to contruct a cute little shield.'
+    }
+  },
+
+  {
+    name       : {en: 'Metal Guard' },
+    description: {
+      en: 'Intricate shield made out of 3 solid metal pieces. Sturdy.'
+    }
+  },
+
+  {
+    name       : {en: 'Protectron' },
+    description: {
+      en: 'Made from obsidian and steel. Someone sloppily attempted to paint it yellow. They failed. Miserably.'
+    }
+  },
+
+]   name       : {en: 'Iron Basher' },
+    description: {
+      en: 'Slender rod-like weapon. Lightweight and fairly useless.'
+    }
+  },
+
+  {
+    name       : {en: 'Darkmetal Maul' },
+    description: {
+      en: 'Ancient dwarves forged this mighty structure. Dark and deadly.'
+    }
+  },
+
+  {
+    name       : {en: 'Divine Gavel' },
+    description: {
+      en: 'Bronze hammer, triangular shaped. Sharp corners make deep indents in heads.'
+    }
+  },
+
+  {
+    name       : {en: 'Hallowed Hammer' },
+    description: {
+      en: 'Holy tool used by druids to make sacrifices to the great god, Yggdrasil.'
+    }
+  },
+
+  {
+    name       : {en: 'Dwarven Maul' },
+    description: {
+      en: 'Heavy basher used by dwarf warriors. Made of an unknown metal.'
+    }
+  },
+
+  {
+    name       : {en: 'Coldforged Gavel' },
+    description: {
+      en: 'An ancient headstone transformed into a deadly hammer. Cold to touch.'
+    }
+  },
+
+  {
+    name       : {en: 'Amboss' },
+    description: {
+      en: 'Topped with the skull of an enemy, this weapon inspires terror in all who stand against it. '
+    }
+  },
+
+  {
+    name       : {en: 'Skullshatterer' },
+    description: {
+      en: 'Spiky. Can penetrate your feeble skull quite easily. Or your foes. Probably yours though.'
+    }
+  },
+
+  {
+    name       : {en: 'Benevolence' },
+    description: {
+      en: 'Nothing kind about this weapon.'
+    }
+  },
+
+  {
+    name       : {en: 'Hammer of Gaia' },
+    description: {
+      en: 'Simply a wooden rod with a mass of iron on top. Crude, yet effective.'
+    }
+  },
+
+  {
+    name       : {en: 'Worldender' },
+    description: {
+      en: 'Does it have the power to end worlds? Few can say. If it does, we will not live long enough to learn of it.'
+    }
+  },
+
+  {
+    name       : {en: 'Nightmare' },
+    description: {
+      en: 'Be afraid. Be very, very afraid.'
+    }
+  }
+
+]
