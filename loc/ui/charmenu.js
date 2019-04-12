a36ba1ff82004959700011ed8ba39adabf632215
YoJoGaming <49464653+YoJoGaming@users.noreply.github.com>
2019-04-11 20:28:02 -0400

    ES, DE language update charmenu.js

--- loc/ui/charmenu.js
+++ loc/ui/charmenu.js
@@ -6,27 +6,37 @@ export default {
     title: {
       en: 'Select Character',
       se: 'Välj Karaktär',
-      ru: 'Выберите Персонажа'
+      ru: 'Выберите Персонажа',
+      de: 'Charakter auswählen,
+      es: 'Seleccione Carácter'
     },
     create: {
       en: 'Click to create character',
       se: 'Klicka för att skapa karaktär',
-      ru: 'Нажмите, чтобы создать персонажа'
+      ru: 'Нажмите, чтобы создать персонажа',
+      de: 'Klicken Sie hier, um einen Charakter zu erstellen.',
+      es: 'Haga clic para crear un personaje'
     },
     enterWorld: {
       en: 'Enter World',
       se: 'Starta Värld',
-      ru: 'Войти в мир'
+      ru: 'Войти в мир',
+      de: 'Tritt ein in die Welt',
+      es: 'Entrar en el mundo'
     },
     emptySlot: {
       en: 'Empty',
       se: 'Ingen',
-      ru: 'Пусто'
+      ru: 'Пусто',
+      de: 'Leer',
+      es: 'Vacío'
     },
     error: {
       en: 'Network error',
       se: 'Nätverks problem',
-      ru: 'Проблема с сетью'
+      ru: 'Проблема с сетью',
+      de: 'Netzwerkfehler',
+      es: 'Error de red'
     }
   },
 
@@ -35,32 +45,44 @@ export default {
     title: {
       en: 'Character creation',
       se: 'Skapa karaktär',
-      ru: 'Создание персонажа'
+      ru: 'Создание персонажа',
+      de: 'Charaktererstellung',
+      es: 'Creación de personajes'
     },
     pressIcon: {
       en: 'Press an icon for a brief description.',
       se: 'Klicka en ikon för beskrivning.',
-      ru: 'Нажмите на иконку, чтобы увидеть краткое описание.'
+      ru: 'Нажмите на иконку, чтобы увидеть краткое описание.',
+      de: 'Drücken Sie ein Symbol für eine Kurzbeschreibung.',
+      es: 'Pulse un icono para obtener una breve descripción.'
     },
     selectClass: {
       en: 'Select a class',
       se: 'Välj en klass',
-      ru: 'Выберите класс'
+      ru: 'Выберите класс',
+      de: 'Wählen Sie eine Klasse',
+      es: 'Seleccione una clase'
     },
     selectFaction: {
       en: 'Select a faction',
       se: 'Välj en sida',
-      ru: 'Выберите фракцию'
+      ru: 'Выберите фракцию',
+      de: 'Wählen Sie eine Fraktion aus',
+      es: 'Seleccione una facción'
     },
     enterName: {
       en: 'Enter a name',
       se: 'Välj ett namn',
-      ru: 'Введите имя'
+      ru: 'Введите имя',
+      de: 'Geben Sie einen Namen ein',
+      es: 'Introduzca un nombre'
     },
     nameReq: {
       en: 'Your name can only contain characters a-Z, and numbers 0-9, without any spaces. Up to 16 characters.',
       se: 'Ditt namn kan bara använda sig karaktärerna a-Z, och nummer 0-9, utan mellanslag. Max 16 karaktärer.',
-      ru: 'Ваше имя может состоять только из латинского алфавита a-Z и цифр 0-9, без пробелов. До 16 символов.'
+      ru: 'Ваше имя может состоять только из латинского алфавита a-Z и цифр 0-9, без пробелов. До 16 символов.',
+      de: 'Dein Name darf nur die Zeichen a-Z und die Zahlen 0-9 ohne Leerzeichen enthalten. Bis zu 16 Zeichen.',
+      es: 'Su nombre sólo puede contener los caracteres de la a-Z, y los números del 0 al 9, sin espacios. Hasta 16 caracteres.'
     }
   },
 
@@ -70,17 +92,23 @@ export default {
     title: {
       en: 'Delete',
       se: 'Ta bort',
-      ru: 'Удалить'
+      ru: 'Удалить',
+      de: 'Löschen',
+      es: 'Borrar'
     },
     info: {
       en: 'To delete, type the characters name. Your character can not be restored.',
       se: 'För att ta bort, skriv in karaktärens namn. Din karaktär tas bort för alltid.',
-      ru: 'Чтобы удалить, введите имя персонажа. Ваш персонаж не может быть восстановлен.'
+      ru: 'Чтобы удалить, введите имя персонажа. Ваш персонаж не может быть восстановлен.',
+      de: 'Geben Sie zum Löschen den Namen des Zeichens ein. Dein Charakter kann nicht wiederhergestellt werden.',
+      es: 'Para eliminar, escriba el nombre de los caracteres. Tu carácter no puede ser restaurado.'
     },
     placeholder: {
       en: 'Are you sure?',
       se: 'Är du säker?',
-      ru: 'Вы уверены?'
+      ru: 'Вы уверены?',
+      de: 'Bist du sicher?',
+      es: '¿Estás seguro de eso?'
     }
   }
 
