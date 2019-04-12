89417e2ffd9384b29ab42f715518998496734539
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-12 17:29:46 +0100

    Add files via upload

---
+++ loc/items/talisman.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    name       : {en: 'Smelly Talisman' },
+    description: {
+      en: 'Smells so bad, I wonder why you even picked it up.'
+    }
+  },
+
+  {
+    name       : {en: 'Guided Talisman' },
+    description: {
+      en: 'Throw it at the sun and it will tell you where to go.'
+    }
+  },
+
+  {
+    name       : {en: 'Ocean Talisman' },
+    description: {
+      en: 'Surprisingly, it doesnt smell like fish. Just dont show it freshwater.'
+    }
+  },
+
+  {
+    name       : {en: 'Quiton' },
+    description: {
+      en: 'Cant pronounce it, cant use it.'
+    }
+  },
+
+  {
+    name       : {en: 'Dimension Talisman' },
+    description: {
+      en: 'Ripped from between worlds, this talisman allows for transition between realms.'
+    }
+  }
+
+]
