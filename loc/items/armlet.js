31e1ee9ab32dfa9cc419f7db1bd22c13aa128f6e
Nate Johnston <46363319+nateo221@users.noreply.github.com>
2019-04-12 20:54:33 -0600

    Pulseras (armlets, braces, oh my)

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -1,105 +1,118 @@
 export default [
-  {
+  {//T1
     name       : {en: 'Simple Bracelet' },
     description: {
       en: 'Cheap leather band bought by an ambitious farmer',
-      ru: 'Дешевый кожаный ремешок, купленный амбициозным фермером'
+      ru: 'Дешевый кожаный ремешок, купленный амбициозным фермером',
+      es: 'Pulsera de cuero hecho por unos racheros.'
     }
   },
 
-  {
+  {//T2
     name       : {en: 'Makeshift Brace' },
     description: {
       en: 'A little more advanced, not by much though.',
-      ru: 'Немного более продвинутый, но не намного.'
+      ru: 'Немного более продвинутый, но не намного.',
+      es: 'Pulsera avanzada de cuero.'
     }
   },
 
-  {
+  {//T3
     name       : {en: 'Leather Armlet' },
     description: {
       en: 'Stiff, makes your arm sore.',
-      ru: 'Жосткая, болит рука.'
+      ru: 'Жосткая, болит рука.',
+      es: 'Mas cuero. Todo es cuero. Nesecitas una chaqueta de cuero para ser completo.'
     }
   },
 
-  {
+  {//T4
     name       : {en: 'Ivory Bracelet' },
     description: {
       en: 'Elegant cuff made from the tusks of the last mammonth.',
-      ru: 'Элегантная манжета из бивней последнего мамонта.'
+      ru: 'Элегантная манжета из бивней последнего мамонта.',
+      es: 'Un pulsera elegante creado con el comillo del elefante final.'
     }
   },
 
-  {
+  {//T5
     name       : {en: 'Iron Vambrace' },
     description: {
       en: 'Heavy on your arm.....worth the protection.',
-      ru: 'Тяжело на твоей руке ..... стоит защиты.'
+      ru: 'Тяжело на твоей руке ..... стоит защиты.',
+      es: 'Pesado, pero vale la pena.'
     }
   },
 
-  {
+  {//T6
     name       : {en: 'Imbued Bracers' },
     description: {
       en: 'Dull metal bracer, handy for nightime raidings.',
-      ru: 'Унылый металлический браслет, удобный для ночных рейдов.'
+      ru: 'Унылый металлический браслет, удобный для ночных рейдов.',
+      es: 'Perfecto para correr en la noche, el color del metal está apagado.'
     }
   },
 
-  {
+  {//T7
     name       : {en: 'Ember Cuffs' },
     description: {
       en: 'Scaled bracer, makes your arms resemble that of a dragon.',
-      ru: 'Чешуйчатый наруч, делает ваши руки похожими на руки дракона.'
+      ru: 'Чешуйчатый наруч, делает ваши руки похожими на руки дракона.',
+      es: 'Con armas de dragones, puedes resistir todo.'
     }
   },
 
-  {
+  {//T8
     name       : {en: 'Mirrored Armlet' },
     description: {
       en: 'Blinds your foes with a magical shine off a devious magicians mirror......',
-      ru: 'Ослепляет ваших врагов волшебным блеском с коварного зеркалoм мага ......'
+      ru: 'Ослепляет ваших врагов волшебным блеском с коварного зеркалoм мага ......',
+      es: 'Un espejo por su muñeca. Lo usas para cegar sus enemigos.'
     }
   },
 
-  {
+  {//T9
     name       : {en: 'Golem Fragment' },
     description: {
       en: 'The archaic guardians left remnants of their passing, which you unscrupulously have decided to wear on your arm.',
-      ru: 'Архаичные стражи оставили остатки своего прохождения, которые вы бессовестно решили надеть на руку.'
+      ru: 'Архаичные стражи оставили остатки своего прохождения, которые вы бессовестно решили надеть на руку.',
+      es: 'Un fragmento de los servientes de los archimagos de la era pasada. Has decido pónertelo porque... razones.'
     }
   },
 
-  {
+  {//T10
     name       : {en: 'Coldforged Bracer' },
     description: {
-      en: 'The deceptively light steel offers chilling protection against sneaky blows',
-      ru: 'Обманчиво легкая сталь предлагает охлаждающую защиту от подлых ударов'
+      en: 'The deceptively light steel offers chilling protection against sneaky blows.',
+      ru: 'Обманчиво легкая сталь предлагает охлаждающую защиту от подлых ударов.',
+      es: 'El acero es engañosamente ligero y te ofrece protección frígido contra golpes furtivos.'
     }
   },
 
-  {
+  {//T11
     name       : {en: 'Blackstar Gem' },
     description: {
       en: 'Stars are not often black, yet this fragment has a.......deep.......deep darkness.',
-      ru: 'Звезды не часто черные, но этот фрагмент имеет ....... глубокую ....... глубокую тьму.'
+      ru: 'Звезды не часто черные, но этот фрагмент имеет ....... глубокую ....... глубокую тьму.',
+      es: 'Completamente hecho con piezas de estrellas de neutrón: denso, duro, poderoso.'
     }
   },
 
-  {
+  {//T12
     name       : {en: 'Eternal Vambraces' },
     description: {
       en: 'Reforged, lost, found, stolen, hidden for eras without end.',
-      ru: 'Перекован, потерян, найден, украден, спрятан на века без конца.'
+      ru: 'Перекован, потерян, найден, украден, спрятан на века без конца.',
+      es: 'Roto, rehecho, recubierto, robado, perdido por eras sin fin.'
     }
   },
 
-  {
+  {//T13
     name       : {en: 'Lost Guardian' },
     description: {
       en: 'This item was forged on a full moon when mysterious incantations were heard in the whispers of the dark. Some say that it was Anubis himself speaking with the dead.',
-      ru: 'Этот предмет был выкован в полнолуние, когда в шепот темноты раздавались таинственные заклинания. Некоторые говорят, что это был сам Анубис, говорящий с мертвыми.'
+      ru: 'Этот предмет был выкован в полнолуние, когда в шепот темноты раздавались таинственные заклинания. Некоторые говорят, что это был сам Анубис, говорящий с мертвыми.',
+      es: 'Forjado durante la plenilunia cuando incantaciones misteriosas fueron oído en los susurros de la oscurridad.'
     }
   }
 
