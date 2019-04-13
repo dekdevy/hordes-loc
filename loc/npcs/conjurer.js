9a6196e8fbf65921166a539ca49744f02ebf1b84
dek <polydek@googlemail.com>
2019-04-13 23:39:54 +0200

    Update conjurer.js

--- loc/npcs/conjurer.js
+++ loc/npcs/conjurer.js
@@ -3,7 +3,7 @@ export default {
     text: {
       // This is not a final text, just a placeholder, probably not worth translating yet
       // someone write a good text for this lol
-      en: 'Hello, I will bring you to another realm. Where would you like to go?'
+      en: 'Hello, I can create a portal to other conjurers. Where would you like to go?'
     },
     choices: [{
       en: 'Please bring me to $1.'
