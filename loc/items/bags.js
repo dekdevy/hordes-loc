8614520aca84bf007dc07807ae34817bc0a6b07d
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-13 11:25:36 +0100

    Add files via upload

---
+++ loc/items/bags.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    name       : {en: 'Linen Pouch' },
+    description: {
+      en: 'Small little purse used by merchants wives.'
+    }
+  },
+
+  {
+    name       : {en: 'Adventurers Rucksack' },
+    description: {
+      en: 'The buckle is worth more than the rest of it.'
+    }
+  },
+
+  {
+    name       : {en: 'Purpur Duffel' },
+    description: {
+      en: 'Nice purple rucksack for all your travelling needs. '
+    }
+  },
+
+  {
+    name       : {en: 'Elven Saddlebag' },
+    description: {
+      en: 'Elegant bag used by elven archers on horseback'
+    }
+  },
+
+  {
+    name       : {en: 'Moss Enigma' },
+    description: {
+      en: 'Woven from threads of lichen, the construction of this bag remains a mystery'
+    }
+  }
+
+]
