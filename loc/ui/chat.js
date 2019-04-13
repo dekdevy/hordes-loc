30054056c95f11254bd77badfc73e1fa13398e94
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-13 17:29:12 +0100

    Create chat.js

---
+++ loc/ui/chat.js
@@ -0,0 +1,43 @@
+export default {
+
+  showall: {
+    en: 'Show All',
+    ru: 'Показать все'
+  },
+
+  showclan: {
+    en: 'Show Clan',
+    ru: 'Показать клан'
+  },
+
+  showsystem: {
+    en: 'Show System',
+    ru: 'Показать систему'
+  },
+
+  loot: {
+    en: 'Loot',
+    ru: 'Добыча'
+  },
+
+  exp: {
+    en: 'exp',
+    ru: 'опыт'
+  },
+
+  pvp: {
+    en: 'pvp',
+    ru: 'ипи'
+  },
+
+  // translations not needed
+  twitter: {
+    en: 'tw'
+  },
+
+  // translations not needed
+  discord: {
+    en: 'Discord'
+  }
+
+}
