3dc270d0559afbf5999d80335387be0cc374344b
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-13 11:45:41 +0100

    Create armor.js

---
+++ loc/items/armor.js
@@ -0,0 +1,25 @@
+export default [	
+  { // T1 item name. names are english only!	
+    name       : { en: 'Trashy Thing' },	
+    // Item description	
+    description: {	
+      en: 'Interesting item description'	
+    }	
+  },	
+
+   { // T2 item. names are english only!	
+    name       : { en: 'Wooden Thing' },	
+    description: {	
+      en: 'Interesting item description'	
+    }	
+  },	
+
+   { // T3 item. and so on...	
+    name       : { en: 'Elven Thing' },	
+    description: {	
+      en: 'Interesting item description'	
+    }	
+  }	
+
+   // keep adding objects here for t4,t5, etc..	
+] 
