dfecbcd357c6226af9b9ddd5b5f8a9e4c4710f3b
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-13 15:00:00 +0100

    Create navbar.js

---
+++ loc/ui/navbar.js
@@ -0,0 +1,21 @@
+export default {
+  players: {
+    en: 'Players',
+    ru: 'Игроки'
+  },
+
+  clans: {
+    en: 'Clans',
+    ru: 'Кланы'
+  },
+
+  guide: {
+    en: 'Guide',
+    ru: 'Проводник'
+  },
+
+  blog: {
+    en: 'Blog',
+    ru: 'Блог'
+  },
+}
