9c97c579f9f12b70ba152c0b86460af9f26cc309
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-13 18:07:30 +0100

    Update clan.js

--- loc/ui/clan.js
+++ loc/ui/clan.js
@@ -10,7 +10,33 @@ export default {
     ru: 'Покинуть Клан'
   },
 
-  create: {
+  clan: {
+    en: 'Clan',
+    ru: 'Клан'
+  },
+
+  createheading: {
+    en: 'Create Clan',
+    ru: 'Создать Клан'
+  },
+
+  createinfo: {
+    en: 'To create a clan you need',
+    ru: 'Для создания клана вам нужно'
+  },
+
+  clanname: {
+    en: 'Clan name',
+    ru: 'Название клана'
+  },
+
+  clantag: {
+    en: 'Clan tag',
+    ru: 'Тег клана'
+  },
+
+  createbutton: {
     en: 'Create',
     ru: 'Создать'
-}
+  },
+} 
