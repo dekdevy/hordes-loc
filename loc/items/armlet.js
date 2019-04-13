233afca31e5022780a5b3187d72deeffa82f64d9
dArNSiEs <49496964+dArNSiEs@users.noreply.github.com>
2019-04-13 08:05:56 -0400

    Update armlet.js

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -3,7 +3,7 @@ export default [
     name       : {en: 'Simple Bracelet' },
     description: {
       en: 'Cheap leather band bought by an ambitious farmer',
-      ru: 'Дешевый кожаный ремешок, купленный амбициозным фермером'
+      ru: 'Дешевый кожаный ремешок, купленный амбициозным фермером',
       fr: 'Bracelet en cuir bon marché acheté par un agriculteur ambitieux'
     }
   },
@@ -12,7 +12,7 @@ export default [
     name       : {en: 'Makeshift Brace' },
     description: {
       en: 'A little more advanced, not by much though.',
-      ru: 'Немного более продвинутый, но не намного.'
+      ru: 'Немного более продвинутый, но не намного.',
       fr: 'Un peu plus avancé, pas beaucoup cependant'
     }
   },
@@ -21,7 +21,7 @@ export default [
     name       : {en: 'Leather Armlet' },
     description: {
       en: 'Stiff, makes your arm sore.',
-      ru: 'Жосткая, болит рука.'
+      ru: 'Жосткая, болит рука.',
       fr: 'Rigide, rend le bras douloureux'
     }
   },
@@ -30,7 +30,7 @@ export default [
     name       : {en: 'Ivory Bracelet' },
     description: {
       en: 'Elegant cuff made from the tusks of the last mammoth.',
-      ru: 'Элегантная манжета из бивней последнего мамонта.'
+      ru: 'Элегантная манжета из бивней последнего мамонта.',
       fr: 'Menottes élégant fait à partir des défenses de mammouths'
     }
   },
@@ -39,7 +39,7 @@ export default [
     name       : {en: 'Iron Vambrace' },
     description: {
       en: 'Heavy on your arm.....worth the protection.',
-      ru: 'Тяжело на твоей руке ..... стоит защиты.'
+      ru: 'Тяжело на твоей руке ..... стоит защиты.',
       fr: 'Lourd sur votre bras ..... vaut la protection'
     }
   },
@@ -48,7 +48,7 @@ export default [
     name       : {en: 'Imbued Bracers' },
     description: {
       en: 'Dull metal bracer, handy for nightime raidings.',
-      ru: 'Унылый металлический браслет, удобный для ночных рейдов.'
+      ru: 'Унылый металлический браслет, удобный для ночных рейдов.',
       fr: 'Terne métal remontant, maniable pour les raids nocturnes'
     }
   },
@@ -57,7 +57,7 @@ export default [
     name       : {en: 'Ember Cuffs' },
     description: {
       en: 'Scaled bracer, makes your arms resemble that of a dragon.',
-      ru: 'Чешуйчатый наруч, делает ваши руки похожими на руки дракона.'
+      ru: 'Чешуйчатый наруч, делает ваши руки похожими на руки дракона.',
       fr: 'Remontant en écailles, cela fait ressembler vos bras à ceux d`un dragon.
     }
   },
@@ -66,7 +66,7 @@ export default [
     name       : {en: 'Mirrored Armlet' },
     description: {
       en: 'Blinds your foes with a magical shine off a devious magicians mirror......',
-      ru: 'Ослепляет ваших врагов волшебным блеском с коварного зеркалoм мага ......'
+      ru: 'Ослепляет ваших врагов волшебным блеском с коварного зеркалoм мага ......',
       fr: 'Aveugle vos ennemis avec un éclat magique sur un miroir de magiciens sournois.....'
     }
   },
@@ -75,7 +75,7 @@ export default [
     name       : {en: 'Golem Fragment' },
     description: {
       en: 'The archaic guardians left remnants of their passing, which you unscrupulously have decided to wear on your arm.',
-      ru: 'Архаичные стражи оставили остатки своего прохождения, которые вы бессовестно решили надеть на руку.'
+      ru: 'Архаичные стражи оставили остатки своего прохождения, которые вы бессовестно решили надеть на руку.',
       fr: 'Les gardiens archaïques ont laissé des restes de leur passage, que vous avez sans scrupule décidé de porter sur votre bras.'
     }
   },
@@ -84,7 +84,7 @@ export default [
     name       : {en: 'Coldforged Bracer' },
     description: {
       en: 'The deceptively light steel offers chilling protection against sneaky blows',
-      ru: 'Обманчиво легкая сталь предлагает охлаждающую защиту от подлых ударов'
+      ru: 'Обманчиво легкая сталь предлагает охлаждающую защиту от подлых ударов',
       fr: 'L`acier léger trompeur offre la protection contre les coups sournois'
     }
   },
@@ -93,7 +93,7 @@ export default [
     name       : {en: 'Blackstar Gem' },
     description: {
       en: 'Stars are not often black, yet this fragment has a.......deep.......deep darkness.',
-      ru: 'Звезды не часто черные, но этот фрагмент имеет ....... глубокую ....... глубокую тьму.'
+      ru: 'Звезды не часто черные, но этот фрагмент имеет ....... глубокую ....... глубокую тьму.',
       fr: 'Les étoiles ne sont pas souvent noires, mais ce fragment a une.......profund....... obscurité profonde.'
     }
   },
@@ -102,7 +102,7 @@ export default [
     name       : {en: 'Eternal Vambraces' },
     description: {
       en: 'Reforged, lost, found, stolen, hidden for eras without end.',
-      ru: 'Перекован, потерян, найден, украден, спрятан на века без конца.'
+      ru: 'Перекован, потерян, найден, украден, спрятан на века без конца.',
       fr: 'Reforgé, perdu, trouvé, volé, caché pour des ères sans fin.' 
     }
   },
@@ -111,7 +111,7 @@ export default [
     name       : {en: 'Lost Guardian' },
     description: {
       en: 'This item was forged on a full moon when mysterious incantations were heard in the whispers of the dark. Some say that it was Anubis himself speaking with the dead.',
-      ru: 'Этот предмет был выкован в полнолуние, когда в шепот темноты раздавались таинственные заклинания. Некоторые говорят, что это был сам Анубис, говорящий с мертвыми.'
+      ru: 'Этот предмет был выкован в полнолуние, когда в шепот темноты раздавались таинственные заклинания. Некоторые говорят, что это был сам Анубис, говорящий с мертвыми.',
       fr: 'Cet article a été forgé à la pleine lune quand des incantations mystérieuses ont été entendues dans les murmures de l`obscurité. Certains disent que c`était Anubis lui-même parlant avec les morts'
     }
   }
