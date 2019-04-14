e9f439271f37bd8e6773b0640c45f0a7a579c78c
chambtheminerva <49500705+chambtheminerva@users.noreply.github.com>
2019-04-14 11:54:27 -0400

    Update bloodlust.js

--- loc/factions/bloodlust.js
+++ loc/factions/bloodlust.js
@@ -4,7 +4,7 @@ export default {
     se: 'Bloodlust',
     ru: 'Bloodlust',
     es: 'Bloodlust',
-    fr: 'La soif de sang',
+    fr: 'Bloodlust',
     de: 'Blutgier'
   },
   description: {
@@ -12,7 +12,7 @@ export default {
     se: 'Bloodlust värderar frihet och lojalitet, erbjunder en plats för individualism men också kaos. Deras försvar hittas i ett öken kungarike vid namn Headless Landing.',
     ru: 'Орда Bloodlust славится свободой и лояльностью, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном мире, названном Headless Landing.',
     es: 'A la horda Bloodlust le importan la libertad y lealtad. Con su guarida situado en un desierto llamado Headless Landing, ofrecen el lugar perfecto para expresión y caos.',
-    fr: 'La horde Bloodlust valorise la liberté et la loyauté, offre une place à l\'individualisme ou même au chaos. Leurs défenses sont situées dans un royaume désertique appelé l\'atterrissage sans tête.',
+    fr: 'La faction des Bloodlust valorise la liberté et la loyauté. Elle offre une place à l\'individualisme ou même au chaos. Leurs défenses sont situées dans un royaume désertique appelé Headless Landing.',
     de: 'Die Horde der Blutgier schätzt Freiheit und Loyalität, bietet einen Ort für Individualismus oder gar Chaos. Ihre Verteidigung befindet sich in einem Wüstenreich namens Headless Landing.'
   }
 }
