a97aa7852d11f26a74f7b521262d331af9e99ab5
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-14 19:28:34 -0400

    Update chat.js

--- loc/ui/chat.js
+++ loc/ui/chat.js
@@ -3,25 +3,37 @@ export default {
   showall: {
     en: 'Show All',
     ru: 'Показать Все',
-    hi: 'सब दिखाए'
+    hi: 'सब दिखाए',
+    es: 'inwentaryzacja',
+    de: 'Alle anzeigen',
+    fr: 'Montrer tous/toutes'
   },
 
   showclan: {
     en: 'Show Clan',
     ru: 'Показать Клан',
-    hi: 'दल से दिखाए'
+    hi: 'दल से दिखाए',
+    es: 'Mostrar Clan',
+    de: 'Show-Clan',
+    fr: 'Afficher le clan'
   },
 
   showsystem: {
     en: 'Show System',
     ru: 'Показать Систему',
-    hi: 'सिस्टम से दिखाए'
+    hi: 'सिस्टम से दिखाए',
+    es: 'Mostrar sistema',
+    de: 'System anzeigen',
+    fr: 'Afficher le système'
   },
 
   loot: {
     en: 'Loot',
     ru: 'Добыча',
-    hi: 'लूट'
+    hi: 'लूट',
+    es: 'Saqueo',
+    de: 'Beute',
+    fr: 'Pillage'
   },
 
   exp: {
