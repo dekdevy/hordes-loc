3bd8f24ebf2fcfe8a3ec3859171f212092a8356d
chambtheminerva <49500705+chambtheminerva@users.noreply.github.com>
2019-04-14 14:42:14 -0400

    Update conjurer.js

--- loc/npcs/conjurer.js
+++ loc/npcs/conjurer.js
@@ -3,10 +3,12 @@ export default {
     text: {
       // This is not a final text, just a placeholder, probably not worth translating yet
       // someone write a good text for this lol
-      en: 'Hello, I can create a portal to other conjurers. Where would you like to go?'
+      en: 'Hello, I can create a portal to other conjurers. Where would you like to go?',
+      fr: 'Bonjour, je peux faire apparaître un portail vers les autres conjurers. Où voudriez-vous aller?'
     },
     choices: [{
-      en: 'Please bring me to $1.'
+      en: 'Please bring me to $1.',
+      fr: 'Apportez-moi vers le 1$, s\'il-vous plaît.'
     }]
   }]
 }
