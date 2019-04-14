4edf31badd94b815fc19b69e6d57836816913181
chambtheminerva <49500705+chambtheminerva@users.noreply.github.com>
2019-04-14 14:33:44 -0400

    Update clan.js

--- loc/ui/clan.js
+++ loc/ui/clan.js
@@ -2,41 +2,49 @@ export default {
 
   manager: {
     en: 'Clan Manager',
-    ru: 'Клан Менеджер'
+    ru: 'Клан Менеджер',
+    fr: 'Gestionnaire du clan'
   },
 
   leave: {
     en: 'Leave Clan',
-    ru: 'Покинуть Клан'
+    ru: 'Покинуть Клан',
+    fr: 'Quitter le clan'
   },
 
   heading: {
     en: 'Clan',
-    ru: 'Клан'
+    ru: 'Клан',
+    fr: 'Clan'
   },
 
   createheading: {
     en: 'Create Clan',
-    ru: 'Создать Клан'
+    ru: 'Создать Клан',
+    fr: 'Créer un clan'
   },
 
   createinfo: {
     en: 'To create a clan you need',
-    ru: 'Для создания клана вам нужно'
+    ru: 'Для создания клана вам нужно',
+    fr: 'Pour créer un clan, on requiert'
   },
 
   createname: {
     en: 'Clan name',
-    ru: 'Название клана'
+    ru: 'Название клана',
+    fr: 'Nom du clan'
   },
 
   createtag: {
     en: 'Clan tag',
-    ru: 'Тег клана'
+    ru: 'Тег клана',
+    fr: 'Tag du clan'
   },
 
   createbutton: {
     en: 'Create',
-    ru: 'Создать'
+    ru: 'Создать',
+    fr: 'Créer'
   }
 }
