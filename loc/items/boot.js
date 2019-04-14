499bd6944ab467344b0e877942738de223fe790f
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-14 13:40:08 +0100

    Update boot.js

--- loc/items/boot.js
+++ loc/items/boot.js
@@ -2,8 +2,9 @@ export default [
   {
     name       : {en: 'Sandals' },
     description: {
-      en: 'At least you are wearing something on those feet. ',
+      en: 'At least you are wearing something on those feet.',
       ru: 'По крайней мере, вы носите что-то на этих ногах.',
+      de: 'Zumindest trägst du etwas auf diesen Füßen.',
       es: 'A lo menos puede tirarlo para hacer daño significativo.'
     }
   },
@@ -13,6 +14,7 @@ export default [
     description: {
       en: 'Soles made from cloth, will probably tear after a few hours.',
       ru: 'Подошвы сделанные из ткани, вероятно, порвутся через несколько часов.',
+      de: 'Sohlen aus Stoff, werden wahrscheinlich nach ein paar Stunden reißen.',
       es: 'OI. Estos son calcetines. ¡Dame mis chanclas!'
     }
   },
@@ -20,9 +22,10 @@ export default [
   {
     name       : {en: 'Leather Boots' },
     description: {
-      en: 'The soft leather keeps your feet warm while providing a little protection from low blows.',
-      ru: 'Мягкая кожа сохраняет ваши ноги в тепле, обеспечивая небольшую защиту от низких ударов.',
-      es: 'Con el cuero de un toro fuerte, estás protegido por algunas más días.'
+      en: 'The soft leather is said to be stripped off wild boars roaming the eerie woods.',
+      ru: 'Говорят, что мягкую кожу стерли с кабанов, бродящих по жутким лесам.',
+      de: 'Das weiche Leder soll von Wildschweinen befreit worden sein, die durch die unheimlichen Wälder streifen.',
+      es: 'Se dice que el cuero suave es despojado de los jabalíes que deambulan por los espeluznantes bosques.'
     }
   },
 
@@ -31,6 +34,7 @@ export default [
     description: {
       en: 'Clearly lacking in imagination, the maker made boots from the bones of a very large creature...',
       ru: 'Очевидно, что не хватает воображения, создатель сделал сапоги из костей очень крупного существа ...',
+      de: 'Offensichtlich phantasielos, machte der Hersteller Stiefel aus den Knochen einer sehr großen Kreatur.....',
       es: 'Claramente sin imaginación, el zapatero uso los huesos de una criatura grande...'
     }
   },
@@ -38,8 +42,8 @@ export default [
   {
     name       : {en: 'Scaled Treads' },
     description: {
-      en: 'Scales line line the outside of your footwear, catching glares in the sun',
-      ru: 'Чешуя выравнивают внешнюю сторону вашей обуви, ловя блики на солнце',
+      en: 'Scales cover the outside of your footwear, catching glares in the sun',
+      ru: 'Чешуя покрывает внешнюю сторону вашей обуви, ловя блики на солнце',
       es: 'Las escamas de una lagartija te da botas impermeables.'
     }
   },
@@ -49,6 +53,7 @@ export default [
     description: {
       en: 'A pleasant pattern made to paralyze your foes... wonder if it works',
       ru: 'Приятный узор, созданный, чтобы парализовать ваших врагов... интересно, работает ли он',
+      de: 'Ein angenehmes Muster, das gemacht wurde, um deine Feinde zu lähmen... frage dich, ob es funktioniert.',
       es: '¡Eyyy, ahora puedes correr sin desmayar con zapatos reales!'
     }
   },
@@ -58,6 +63,7 @@ export default [
     description: {
       en: 'Taken from war veterans who clearly didn\'t find enough protection from these greaves.',
       ru: 'Взято у ветеранов войны, которые явно не нашли достаточной защиты от этих наголенников.',
+      de: 'Entnommen von Kriegsveteranen, die offensichtlich nicht genug Schutz vor diesen Grieben gefunden haben.',
       es: 'No protegieron sus dueños anteriores tan bien, tal vez funcionan mejor ahora.'
     }
   },
@@ -67,6 +73,7 @@ export default [
     description: {
       en: 'Chunky in its looks, probably very uncomfortable',
       ru: 'Коренастый во внешности, наверное, очень неудобный',
+      de: 'Grob in seinem Aussehen, wahrscheinlich sehr unangenehm.',
       es: 'Macizo y fornido, prbablamente incómodo.'
     }
   },
@@ -76,6 +83,7 @@ export default [
     description: {
       en: 'Run as quick as the wind... or close to it',
       ru: 'Беги так быстро, как ветер... ну или близко к нему',
+      de: 'Laufen Sie so schnell wie der Wind.... oder in der Nähe davon.',
       es: 'Con estos zapatos puedes ver el viento y llevarlo contigo.'
     }
   },
@@ -85,6 +93,7 @@ export default [
     description: {
       en: 'Forged without heat, runs without stumble',
       ru: 'Кованые без тепла, беги без спотыкания',
+      de: 'Geschmiedet ohne Hitze, läuft ohne Stolpern.',
       es: 'Forjado sin calor, te dejan correr sin tropezar.'
     }
   },
@@ -94,6 +103,7 @@ export default [
     description: {
       en: 'Some say you can run on clouds with these. The few that have tried have already ascended.',
       ru: 'Некоторые говорят, что с этими ботинками можно бегать на облаках. Те немногие, которые пытались, уже вознеслись.',
+      de: 'Einige sagen, dass man damit auf Wolken laufen kann. Die wenigen, die es versucht haben, sind bereits aufgestiegen.',
       es: '¡Corre por las nubes con las nuevas Air Miguels!'
     }
   },
@@ -103,6 +113,7 @@ export default [
     description: {
       en: 'Ancient boots rumoured to be stolen from the feet of the Roman messenger god...',
       ru: 'По слухам, древние сапоги были украдены у ног римского бога-вестника ...',
+      de: 'Alte Stiefel sollen aus den Füßen des römischen Kuriergottes gestohlen worden sein...',
       es: 'Botas del mesajero mentiroso, del espía nocturno, del dios griego de apoyar y asesinar.'
     }
   },
@@ -112,6 +123,7 @@ export default [
     description: {
       en: 'Stars were always considered to be all-mighty, now the\'re yours',
       ru: 'Звезды всегда считались всемогущими, теперь они твои',
+      de: 'Sterne wurden immer als allmächtig angesehen, jetzt gehören sie dir.',
       es: 'Las armas mas peligrosas del universo, chanclas imbuido con la ira y furia de una madre cariñosa.'
     }
   }
