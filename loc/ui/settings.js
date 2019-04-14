bee491070e864212f0cd7c4854ac532f6a97d4d2
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-14 19:58:56 -0400

    Update settings.js

--- loc/ui/settings.js
+++ loc/ui/settings.js
@@ -3,80 +3,119 @@ export default {
   settings: {
     en: 'settings',
     ru: 'настройки',
-    hi: 'सेटिंग्स'
+    hi: 'सेटिंग्स',
+    es: 'escenarios',
+    de: 'Einstellungen',
+    fr: 'paramétrage'
   },
 
   // controls section of settings menu
   controls: {
     en: 'controls',
     ru: 'управления',
-    hi: 'कंट्रोल्स'
+    hi: 'कंट्रोल्स',
+    es: 'mandos',
+    de: 'Kontrollen',
+    fr: 'commandes'
   },
 
   lockedcamera: {
     en: 'Locked camera',
     ru: 'Заблокированная камера',
-    hi: 'कैमरा लॉक'
+    hi: 'कैमरा लॉक',
+    es: 'Cámara bloqueada',
+    de: 'Gesperrte Kamera',
+    fr: 'Caméra verrouillée'
   },
 
   graphics: {
     en: 'graphics',
     ru: 'графика',
-    hi: 'ग्राफ़िक्स'
+    hi: 'ग्राफ़िक्स',
+    es: 'dibujos',
+    de: 'Grafiken',
+    fr: 'schémas'
   },
 
   resolution: {
     en: 'Resolution',
     ru: 'Pезолюция',
-    hi: 'रेसोलूशन'
+    hi: 'रेसोलूशन',
+    es: 'Resolución',
+    de: 'Auflösung',
+    fr: 'Résolution'
   },
 
   skilleffects: {
     en: 'Skill effects',
     ru: 'Эффекты навык',
-    hi: 'स्किल दिखावट'
+    hi: 'स्किल दिखावट',
+    es: 'Efectos de habilidad',
+    de: 'Fertigkeitseffekte',
+    fr: 'Effets de compétences'
   },
 
   shadow: {
     en: 'Shadow quality',
     ru: 'Shadow quality',
-    hi: 'परछाईं क्वालिटी'
+    hi: 'परछाईं क्वालिटी',
+    es: 'Calidad de sombra',
+    de: 'Schattenqualität',
+    fr: 'Qualité de l\'ombre'
   },
 
   grassamount: {
     en: 'Grass amount',
     ru: 'Количество травы',
-    hi: 'घास की मात्रा'
+    hi: 'घास की मात्रा',
+    es: 'Cantidad de césped',
+    de: 'Grasmenge',
+    fr: 'Quantité d\'herbe'
   },
 
   chat: {
     en: 'chat',
     ru: 'чат',
-    hi: 'चैट'
+    hi: 'चैट',
+    es: 'charla',
+    de: 'Chat',
+    fr: 'clavardage'
   },
 
   chatwidth: {
     en: 'Chat width',
     ru: 'Ширина чата',
-    hi: 'चैट की चौडाई'
+    hi: 'चैट की चौडाई',
+    es: 'Ancho del chat',
+    de: 'Chatbreite',
+    fr: 'Largeur du chat'
   },
 
   chatheight: {
     en: 'Chat height',
     ru: 'Высота чата',
-    hi: 'चैट की लम्बाई'
+    hi: 'चैट की लम्बाई',
+    es: 'Altura del chat',
+    de: 'Chathöhe',
+    fr: 'Hauteur de clavardage'
   },
 
   tutorial: {
     en: 'tutorial',
     ru: 'учебник',
-    hi: 'शिक्षण'
+    hi: 'शिक्षण',
+    es: 'tutorial',
+    de: 'Tutorial',
+    fr: 'didacticiel'
   },
 
   reset: {
     en: 'Reset tutorial',
     ru: 'Сбросить учебник',
-    hi: 'शिक्षण रीसेट'
+    hi: 'शिक्षण रीसेट',
+    es: 'Restablecer tutorial',
+    de: 'Tutorial zurücksetzen',
+    fr: 'Réinitialiser le tutoriel'
   }
 
 }
