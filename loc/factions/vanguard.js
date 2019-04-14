238f545250f010474f280dc6c6deb6c4977c78d1
chambtheminerva <49500705+chambtheminerva@users.noreply.github.com>
2019-04-14 11:56:39 -0400

    Update vanguard.js

--- loc/factions/vanguard.js
+++ loc/factions/vanguard.js
@@ -5,7 +5,7 @@ export default {
     ru: 'Vanguard',
     es: 'Vanguard',
     de: 'Vorhut',
-    fr: 'Avant-garde'
+    fr: 'Vanguard'
   },
   description: {
     en: 'The Vanguard horde seeks tradition, structure and order in society. Their Castle can be found in a lush green area around Guardstone.',
@@ -13,6 +13,6 @@ export default {
     ru: 'Орда Vanguard соблюдает традиции, структуру и порядок в обществе. Их крепость может быть найдена в пышной зеленой зоне около Guardstone.',
     es: 'Los valores del Vanguard son la tradición, la estructura y el orden de sociedad. Su Castillo se halla en el bosque exuberante de Guardstone.',
     de: 'Die Vanguard Horde sucht Tradition, Struktur und Ordnung in der Gesellschaft. Ihre Burg befindet sich in einer üppigen grünen Umgebung rund um Guardstone.',
-    fr: 'La horde Vanguard cherche la tradition, la structure et l\'ordre dans la société. Leur château se trouve dans une zone verdoyante autour de Guardstone.'
+    fr: 'La faction des Vanguards respecte la tradition, la structure et l\'ordre dans la société. Leur palais se trouve dans une zone verdoyante autour de Guardstone.'
   }
 }
