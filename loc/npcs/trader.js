e54966435bf229f59504c172eb403c68e2be500e
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-14 12:39:24 +0100

    Update trader.js

--- loc/npcs/trader.js
+++ loc/npcs/trader.js
@@ -4,7 +4,8 @@ export default {
       // This is not a final text, just a placeholder, probably not worth translating yet
       en: 'Greetings wandering traveler! Would you like to sell something?'
     },
-    choices: [{
-      en: 'Yes, of .'
-    }]
+    choices: {
+      en: 'Yes, of course.'
+    }
   }]
+}
