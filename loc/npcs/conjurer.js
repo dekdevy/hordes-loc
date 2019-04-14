57d0196471fc3d0022e8e4d47f76f3b351a3d3fa
Vedant Bang <33593544+VedantBang@users.noreply.github.com>
2019-04-14 09:55:13 +0530

    Added हिंदी

--- loc/npcs/conjurer.js
+++ loc/npcs/conjurer.js
@@ -3,10 +3,12 @@ export default {
     text: {
       // This is not a final text, just a placeholder, probably not worth translating yet
       // someone write a good text for this lol
-      en: 'Hello, I can create a portal to other conjurers. Where would you like to go?'
+      en: 'Hello, I can create a portal to other conjurers. Where would you like to go?',
+      hi: 'हम दूसरे स्थानो का द्वार खोल सकते। कहा जाना चाहते हो तुम?'
     },
     choices: [{
-      en: 'Please bring me to $1.'
+      en: 'Please bring me to $1.',
+      hi: 'हमें $1 लाकर दीजिए।'
     }]
   }]
 }
