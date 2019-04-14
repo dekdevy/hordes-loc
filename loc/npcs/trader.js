70cfdae3d988057998939bb63c81641414d639e9
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-14 18:16:50 -0400

    Update trader.js

--- loc/npcs/trader.js
+++ loc/npcs/trader.js
@@ -5,7 +5,11 @@ export default {
       en: 'Greetings wandering traveler! Would you like to sell something?'
     },
     choices: {
-      en: 'Yes, of course.'
+      en: 'Yes, of course.',
+      en: 'No, I will come back later.',
+      es: 'No, volveré más tarde.',
+      de: 'Nein, ich komme später wieder.',
+      fr: 'Non, je reviendrai plus tard.'
     }
   }]
 }
