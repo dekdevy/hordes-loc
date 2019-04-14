1deb89a0ca8fe21744e5a909517ac86e9f94734f
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-14 12:31:33 +0100

    Update conjurer.js

--- loc/npcs/conjurer.js
+++ loc/npcs/conjurer.js
@@ -2,7 +2,6 @@ export default {
   interactions: [{
     text: {
       // This is not a final text, just a placeholder, probably not worth translating yet
-      // someone write a good text for this lol
       en: 'Ah, I see you traveler. Weary and famished you look. Us conjurers ,too, roam these fine lands. Would you like me to take you to one?'
     },
     choices: [{
