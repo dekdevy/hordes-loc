4fc7c97f8d3a7a052d55b08e078309d4c45ec13c
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-14 19:31:00 -0400

    Update characterswitch.js

--- loc/ui/characterswitch.js
+++ loc/ui/characterswitch.js
@@ -3,12 +3,18 @@ export default {
   logout: {
     en: 'Log Out',
     ru: 'Выйти',
-    hi: 'बाहर आए'
+    hi: 'बाहर आए',
+    es: 'Cerrar sesión',
+    de: 'Abmeldung',
+    fr: 'Déconnexion'
   },
 
   home: {
     en: 'Home',
     ru: 'Домой',
-    hi: 'मुख्य स्क्रीन'
+    hi: 'मुख्य स्क्रीन',
+    es: 'Inicio',
+    de: 'Startseite',
+    fr: 'Accueil'
   }
 }
