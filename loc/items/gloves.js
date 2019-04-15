4aed85c9c29b3a244c1223cc3e3e96720a47601c
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-15 11:26:29 +0100

    Update gloves.js

--- loc/items/gloves.js
+++ loc/items/gloves.js
@@ -5,7 +5,7 @@ export default [
       en: 'Straps of leather covering your feeble hands. Pulling the bowstring is much easier for you now.',
       ru: 'Ремешки из кожи покрывают ваши слабые руки. Вытягивать тетиву теперь намного проще.',
       de: 'Lederbänder, die Ihre schwachen Hände bedecken. Das Ziehen der Bogensehne ist für dich jetzt viel einfacher.',
-      es: ''
+      es: 'Correas de cuero cubriendo tus débiles manos. Tirar de la cuerda es mucho más fácil para ti ahora.'
     }
   },
 
@@ -15,7 +15,7 @@ export default [
       en: 'Spun yarn from stolen sheep makes these warm gloves a pleasure to wear.',
       ru: 'Пряжа из украденных овец делает эти теплые перчатки приятно носить.',
       de: 'Gesponnenes Garn aus gestohlenen Schafen macht diese warmen Handschuhe zu einem wahren Vergnügen.',
-      es: ''
+      es: 'El hilo hilado de oveja robada hace que estos guantes cálidos sean un placer de llevar.'
     }
   },
 
@@ -35,7 +35,7 @@ export default [
       en: 'Clacking sounds travel with you as you wear these.',
       ru: 'щелкающие звуки путешествуют с вами когда вы носите эти перчатки.',
       de: 'Klackende Geräusche reisen mit, während Sie diese tragen.',
-      es: '',
+      es: 'Los sonidos de agarrotamiento viajan con usted mientras los usa.',
     }
   },
 
@@ -45,7 +45,7 @@ export default [
       en: 'Soft-iron gloves provide a little more protection, although your hands are cold now.',
       ru: 'Железные перчатки обеспечивают немного больше защиты, хотя ваши руки теперь холодные.',
       de: 'Eisenhandschuhe bieten etwas mehr Schutz, obwohl deine Hände jetzt kalt sind.',
-      es: ''
+      es: 'Los guantes de hierro proporcionan un poco más de protección, aunque ahora sus manos están frías.'
     }
   },
 
@@ -55,7 +55,7 @@ export default [
       en: 'Mitts made out of unknown material... it shines...',
       ru: 'Рукавицы, сделанные из неизвестного материала ... оно сияет ...',
       de: 'Handschuhe aus unbekanntem Material... sie leuchten...',
-      es: ''
+      es: 'Guantes de material desconocido.... brilla...'
     }
   },
 
@@ -65,7 +65,7 @@ export default [
       en: 'Some war general must have dropped these...probably unintentionally too.',
       ru: 'какой-то военный генерал, должно быть, уронил их... возможно, нечаянно.',
       de: 'Ein Kriegsgeneral muss diese.... wahrscheinlich auch unbeabsichtigt fallen gelassen haben.',
-      es: ''
+      es: 'Algún general de guerra debe haberlos tirado... probablemente sin querer.'
     }
   },
 
@@ -75,7 +75,7 @@ export default [
       en: 'Fire is no match for your hands as you wear these emerald mitts.',
       ru: 'Огонь не враг для ваших рук когда вы носите эти изумрудные перчатки.',
       de: 'Feuer hat keinen Einfluss auf deine Hände, wenn du diese Smaragdhandschuhe trägst.',
-      es: ''
+      es: 'El fuego no es rival para tus manos cuando usas estos guantes esmeralda.'
     }
   },
 
@@ -85,7 +85,7 @@ export default [
       en: 'Gives a false sense of power.',
       ru: 'Дает ложное чувство силы.',
       de: 'Gibt ein falsches Gefühl von Macht.',
-      es: ''
+      es: 'Da una falsa sensación de poder.'
     }
   },
 
@@ -95,7 +95,7 @@ export default [
       en: 'Forged without heat, worn without fear.',
       ru: 'Кованые без тепла, носите без страха.',
       de: 'Ohne Hitze geschmiedet, ohne Angst getragen.',
-      es: ''
+      es: 'Forjados sin calor, llevados sin miedo.'
     }
   },
 
@@ -105,17 +105,17 @@ export default [
       en: 'Gloves sought by warriors for eras on end...at last...it is found.',
       ru: 'Перчатки разыскиваются воинами на протяжении эпох... наконец... нашлись.',
       de: 'Handschuhe, die von Kriegern für Epochen am Ende gesucht wurden... endlich... es wird gefunden.',
-      es: '',
+      es: 'Guantes buscados por los guerreros para las eras....al fin...se encuentra.',
     }
   },
 
   {
     name       : {en: 'Gloves of Midas' },
     description: {
-      en: 'Rumours say that a touch from these gloves turns your foes into gold...your hand is strangely heavy....',
-      ru: 'Слухи говорят, что прикосновение этих перчаток превращает твоих врагов в золото... твоя рука странно тяжелая ....',
+      en: 'Rumours say that a touch from these gloves turns your foes into gold...your hand is strangely heavy...',
+      ru: 'Слухи говорят, что прикосновение этих перчаток превращает твоих врагов в золото... твоя рука странно тяжелая ...',
       de: 'Gerüchten zufolge verwandelt eine Berührung dieser Handschuhe deine Feinde in Gold... deine Hand ist seltsam schwer...',
-      es: ''
+      es: 'Los rumores dicen que un toque de estos guantes convierte a tus enemigos en oro... tu mano es extrañamente pesada...'
     }
   },
 
@@ -125,7 +125,7 @@ export default [
       en: 'Gloves so powerful in their nature that the very air shimmers as you wave your hand.',
       ru: 'Перчатки настолько мощные по своей природе, что сам воздух мерцает, когда вы машете рукой.',
       de: 'Handschuhe, die in ihrer Natur so kraftvoll sind, dass die Luft schimmert, wenn man mit der Hand wedelt.',
-      es: ''
+      es: 'Guantes tan poderosos en su naturaleza que el mismo aire brilla al agitar la mano.'
     }
   }
 
