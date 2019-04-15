e0fc7a0d2865880155d3aa6571cd0436f2d8027b
ValdusShadowmask <49405227+ValdusShadowmask@users.noreply.github.com>
2019-04-15 14:22:21 -0400

    Update vanguard.js

--- loc/factions/vanguard.js
+++ loc/factions/vanguard.js
@@ -10,6 +10,7 @@ export default {
     es: 'Los valores del Vanguard son la tradición, la estructura y el orden de sociedad. Su Castillo se halla en el bosque exuberante de Guardstone.',
     hi: 'वैंगार्ड वंश समाज में परम्परा, संरचना और अनुक्रम चाहते है। उनका क़िला गुआर्डस्टोने के चारों ओर हरे भरे ईलाके में स्थित है।',
     de: 'Die Vanguard Horde sucht Tradition, Struktur und Ordnung in der Gesellschaft. Ihre Burg befindet sich in einer üppigen grünen Umgebung rund um Guardstone.',
-    fr: 'La faction des Vanguards respecte la tradition, la structure et l\'ordre dans la société. Leur palais se trouve dans une zone verdoyante autour de Guardstone.'
+    fr: 'La faction des Vanguards respecte la tradition, la structure et l\'ordre dans la société. Leur palais se trouve dans une zone verdoyante autour de Guardstone.',
+    ko: '뱅가드 호드는 전통, 구조 및 질서를 추구합니다. 그들의 성곽은 “가드 스톤” 주변의 무성한 녹색 지역에서 발견 할 수 있습니다.'
   }
 }
