9f8005da7ffefd0fba5e8dd82140fe18ec8889f6
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-15 11:03:38 +0100

    Update trader.js

--- loc/npcs/trader.js
+++ loc/npcs/trader.js
@@ -2,10 +2,12 @@ export default {
   interactions: [{
     text: {
       // This is not a final text, just a placeholder, probably not worth translating yet
-      en: 'Greetings wandering traveler! Would you like to sell something?'
+      en: 'Greetings wandering traveler! Would you like to sell something?',
+      ru: 'Приветствую брадащий путешественник! Хотите продать что-нибудь?'
     },
     choices: {
       en: 'Yes, of course.',
+      ru: 'Да, конечно.',
       es: 'Sí, por supuesto.',
       de: 'Ja, natürlich.',
       fr: 'Oui, bien sûr.'
