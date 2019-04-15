c529bc054c1e24bf3bcb80938a4fbba697a9ca1f
FarqAndraith <49627616+FarqAndraith@users.noreply.github.com>
2019-04-15 16:18:09 -0500

    Update bow.js
    Added Portuguese

--- loc/items/bow.js
+++ loc/items/bow.js
@@ -7,7 +7,8 @@ export default [
       ru: 'Легкий лук. Возможно, вы можете найти что-то лучшее вокруг.',
       fr: 'Un vrai poid plume. Il y en à sûrement de meilleurs dans le coin.',
       de: 'Fast leicht. Du kannst wahrscheinlich etwas Besseres finden.',
-      es: 'Casi ligero. Probablemente puedas encontrar algo mejor por aquí.'
+      es: 'Casi ligero. Probablemente puedas encontrar algo mejor por aquí.',
+      pt: 'Quase leve. Você pode provavelmente encontrar algo melhor ao redor.'
     }
   },
 
@@ -18,7 +19,8 @@ export default [
       ru: 'Вы чувствуете себя менее опытным, просто держа его в своих руках',
       fr: 'On se sent moins doué rien qu\'en tenant cet arc.',
       de: 'Du fühlst dich weniger geschickt, nur weil du das in deinen Händen hältst.',
-      es: 'Te sientes menos hábil con sólo sostener esto en tus manos.'
+      es: 'Te sientes menos hábil con sólo sostener esto en tus manos.',
+      pt: 'Sentes-te menos hábil só por teres isto nas mãos.'
     }
   },
 
@@ -29,7 +31,8 @@ export default [
       ru: 'Кто то оставил его на земле, вероятно, не встретил хороший конец',
       fr: 'Quiconque a échappé ceci par terre n\'a sûrement pas rencontré une fin heureuse.',
       de: 'Wer das auf dem Boden gelassen hat, hat wahrscheinlich kein schönes Ende gefunden.',
-      es: 'Quienquiera que haya dejado esto en el suelo probablemente no tuvo un buen final.'
+      es: 'Quienquiera que haya dejado esto en el suelo probablemente no tuvo un buen final.',
+      pt: 'Quem quer que tenha deixado isto no chão, provavelmente não teve um final agradável.'
     }
   },
 
@@ -40,7 +43,8 @@ export default [
       ru: 'Более продвинутое оружие. Довольно приятно смотреть на эго.',
       de: 'Eine fortschrittlichere Waffe. Sehr schön anzusehen.',
       es: 'Un arma más avanzada. Bastante bonito de ver.',
-      fr: 'Une arme plus avancée. Plutôt agréable à regarder.'
+      fr: 'Une arme plus avancée. Plutôt agréable à regarder.',
+      pt: 'Uma arma mais avançada. Bastante agradável de se ver.'
     }
   },
 
@@ -51,7 +55,8 @@ export default [
       ru: 'Cобран из костей ... кого-нибудь?',
       fr: 'Assemblé avec les os de... quelqu\'un?',
       de: 'Zusammen mit den Knochen von.... jemandem?',
-      es: '¿Colocado con los huesos de... alguien?'
+      es: '¿Colocado con los huesos de... alguien?',
+      pt: 'Juntos com os ossos de... alguém?'
     }
   },
 
@@ -62,7 +67,8 @@ export default [
       ru: 'Экспертный лук забытой расы',
       fr: 'Un arc d\'expert originant d\'une race oublié.',
       de: 'Ein Expertenbogen einer vergessenen Rasse.',
-      es: 'Una reverencia experta de una raza olvidada.'
+      es: 'Una reverencia experta de una raza olvidada.',
+      pt: 'Um arco perito de uma raça esquecida.'
     }
   },
 
@@ -73,7 +79,8 @@ export default [
       ru: 'Грубый, но эффективный. получает силу из крошечного листа, прикрепленного к нему.',
       de: 'Grob, aber effektiv. Seine Kraft bezieht er aus dem kleinen Blatt, das daran befestigt ist.',
       es: 'Crudo, pero efectivo. Extrae su poder de la diminuta hoja adherida a él.',
-      fr: 'Grossier, mais efficace. Tire son énergie de la petite feuille qui lui est attachée.'
+      fr: 'Grossier, mais efficace. Tire son énergie de la petite feuille qui lui est attachée.',
+      pt: 'Bruto, mas eficaz. Retira o seu poder da pequena folha que lhe está ligada.'
     }
   },
 
@@ -84,7 +91,8 @@ export default [
       ru: 'Сжигает противника с пряностями красной окраскаи.',
       de: 'Verbrennt den Feind mit der schärfsten roten Farbe.',
       es: 'Quema al enemigo con la coloración roja más picante.',
-      fr: 'Brûle ses ennemis avec la coloration rouge la plus épicée.'
+      fr: 'Brûle ses ennemis avec la coloration rouge la plus épicée.',
+      pt: 'Queima o inimigo com a mais picante das cores vermelhas.'
     }
   },
 
@@ -95,7 +103,8 @@ export default [
       ru: 'Лук, обычно используемый низкими королевскими лучниками. Хорошо сочетается с растительностью.',
       de: 'Ein Bogen, der häufig von niederrangigen königlichen Bogenschützen verwendet wird. Passt sich gut der Vegetation an.',
       es: 'Un arco comúnmente usado por arqueros reales de bajo rango. Se integra bien con la vegetación.',
-      fr: 'Un arc couramment utilisé par les archers royaux de bas rang. S\'intègre bien à la végétation.'
+      fr: 'Un arc couramment utilisé par les archers royaux de bas rang. S\'intègre bien à la végétation.',
+      pt: 'Um arco comumente usado por arqueiros reais de baixo escalão. Mistura-se bem com a vegetação.'
     }
   },
 
@@ -106,7 +115,8 @@ export default [
       ru: 'Мастерски созданный бандитами столиц.',
       de: 'Meisterhaft gefertigt von den Banditen der Hauptstädte.',
       es: 'Hecho con maestría por los bandidos de las capitales.',
-      fr: 'Fabriqué avec brio par les bandits des capitales.'
+      fr: 'Fabriqué avec brio par les bandits des capitales.',
+      pt: 'Magistralmente trabalhado pelos bandidos das capitais.'
     }
   },
 
@@ -117,7 +127,8 @@ export default [
       ru: 'Редкий лук, используемый в величайших войнах. Bыстрел заставляет вас чувствовать, что небеса рухнут.',
       fr: 'Un arc rare utilisé dans les plus grandes batailles. Tirer une flèche avec cet arme vous donne l\'impression que les cieux vont s\'effondrer.',
       de: 'Ein seltener Bogen, der in den größten Kriegen verwendet wird. Beim Schießen hat man das Gefühl, dass der Himmel zusammenbrechen wird.',
-      es: 'Un arco raro usado en la mayor de las guerras. Disparar te hace sentir como si el cielo se fuera a derrumbar.'
+      es: 'Un arco raro usado en la mayor de las guerras. Disparar te hace sentir como si el cielo se fuera a derrumbar.',
+      pt: 'Um arco raro usado na maior das guerras. Atirar nele faz você sentir que os céus vão entrar em colapso.'
     }
   },
 
@@ -128,7 +139,8 @@ export default [
       ru: 'Что это? Это лед? Мир может никогда не узнать ...',
       fr: 'Qu\'est-ce cela? De la glace? Le monde ne le saura peut-être jamais...',
       de: 'Was ist das? Was ist das? Ist es Eis? Die Welt wird es vielleicht nie erfahren....',
-      es: '¿Qué es esto? ¿Es hielo? El mundo tal vez nunca lo sepa....'
+      es: '¿Qué es esto? ¿Es hielo? El mundo tal vez nunca lo sepa....',
+      pt: 'O que é isso? É gelo? O mundo pode nunca saber...'
     }
   },
 
@@ -139,7 +151,8 @@ export default [
       ru: 'Убивает так же красиво, как и выглядит.',
       de: 'Tötet so schön, wie es aussieht.',
       es: 'Mata tan maravillosamente como parece.',
-      fr: 'Ça tue aussi joliment que ça en a l\'air.'
+      fr: 'Ça tue aussi joliment que ça en a l\'air.',
+      pt: 'Mata tão bem como parece.'
     }
   },
 
@@ -150,7 +163,8 @@ export default [
       ru: 'Окруженный страшной аурой. Ветер дует в том направлении, куда вы нацеливаетесь.',
       de: 'Umgeben von einer grässlichen Aura. Der Wind weht in die Richtung, in die du ihn zielst.',
       es: 'Rodeado de un aura espantosa. El viento sopla en la dirección en que lo apuntas.',
-      fr: 'Entouré d\'une aura affreuse. Le vent souffle dans la direction que vous visez.'
+      fr: 'Entouré d\'une aura affreuse. Le vent souffle dans la direction que vous visez.',
+      pt: 'Rodeado por uma aura horrível. O vento sopra na direção que você aponta.'
     }
   },
 
@@ -161,7 +175,8 @@ export default [
       ru: 'Настолько страшно, что немногие имеют мужество держать его.',
       de: 'So beängstigend, dass nur wenige den Mut haben, es zu halten.',
       es: 'Tan aterrador que pocos tienen la coraje para sostenerlo.',
-      fr: 'Une arme si effrayante que peu de gens ont le courage de la brandir.'
+      fr: 'Une arme si effrayante que peu de gens ont le courage de la brandir.',
+      pt: 'Tão assustador que poucos têm a coragem de o segurar.'
     }
   },
 
@@ -172,7 +187,8 @@ export default [
       ru: 'Окрыленные яростью, ваши противники дрожат при виде этого оружия. Пью Пью - последнее, что они услышат.',
       de: 'Eingebettet in eine urtümliche Wut, zittern deine Feinde beim Anblick dieser Waffe. Kirchenbank ist das Letzte, was sie je hören werden.',
       es: 'Envueltos en una furia primigenia, tus enemigos tiemblan al ver esta arma. Pew Pew Pew es lo último que oirán.',
-      fr: 'Encastrés dans une rage primitive, vos ennemis tremblent à la vue de cette arme. Pew pew pew est la dernière chose qu\'ils n\'entendront jamais.'
+      fr: 'Encastrés dans une rage primitive, vos ennemis tremblent à la vue de cette arme. Pew pew pew est la dernière chose qu\'ils n\'entendront jamais.',
+      pt: 'Embebidos em uma raiva primária, seus inimigos tremem ao ver esta arma. O banco de bancos é a última coisa que eles vão ouvir.'
     }
   }
 
