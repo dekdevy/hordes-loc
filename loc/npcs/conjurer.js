145aba7d1be61c2cc2eae10211b0ea397ff5a19f
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-15 10:59:35 +0100

    Update conjurer.js

--- loc/npcs/conjurer.js
+++ loc/npcs/conjurer.js
@@ -3,12 +3,14 @@ export default {
     text: {
       // This is not a final text, just a placeholder, probably not worth translating yet
       en: 'Ah, I see you traveler. Weary and famished you look. Us conjurers, too, roam these fine lands. Would you like me to take you to one?',
+      ru: 'А, вижу, путешественник. Ты выглядишь уставшим и голодным. Мы, волшебники, тоже бродим по этим прекрасным землям. Хочешь, я отвезу тебя k одному?',
       fr: 'Bonjour, je peux faire apparaître un portail vers les autres conjurers. Où voudriez-vous aller?',
       es: 'Ah, te veo viajero. Pareces cansado y hambriento. Nosotros, los hechiceros, también, deambulamos por estas bellas tierras. ¿Quieres que te lleve a uno?',
       de: 'Ah, ich sehe dich als Reisenden. Müd und ausgehungert siehst du aus. Auch wir Zauberer durchstreifen diese schönen Ländereien. Möchtest du, dass ich dich zu einem bringe?'
     },
     choices: [{
       en: 'Please bring me to $1.',
+      ru: 'Пожалуйста, принеси мне k $1.',
       fr: 'Apportez-moi vers le 1$, s\'il-vous plaît.',
       hi: 'हम दूसरे स्थानो का द्वार खोल सकते। कहा जाना चाहते हो तुम?',
       de: 'Bitte bring mich auf $1.',
