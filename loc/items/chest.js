1df7d343ee17ac8d8c7395d3a321ab3394f95b49
FarqAndraith <49627616+FarqAndraith@users.noreply.github.com>
2019-04-15 16:25:33 -0500

    Update chest.js
    Added Portuguese

--- loc/items/chest.js
+++ loc/items/chest.js
@@ -6,7 +6,8 @@ export default [
       ru: 'Мешок жесткий с крахмалом заставляет чувствовать вас грязным и взъерошенным.',
       es: 'Saco rígido con almidón que te hace sentir sucio y despeinado.',
       de: 'Sack steif mit Stärke, so dass Sie sich schmutzig und zerzaust fühlen.',
-      fr: 'Sac rigide avec de l\'amidon qui vous donne l\'impression d\'être sale et ébouriffé.'
+      fr: 'Sac rigide avec de l\'amidon qui vous donne l\'impression d\'être sale et ébouriffé.',
+      pt: 'Saco rígido com amido que te faz sentir sujo e desgrenhado.'
     }
   },
 
@@ -17,7 +18,8 @@ export default [
       ru: 'Кожаная рубашка, выцветшая от износа.',
       es: 'Camisa de cuero, desteñida por el desgaste.',
       de: 'Lederhemd, verblasst durch Abnutzung.',
-      fr: 'Chemise en cuir, décolorée par l\'usure.'
+      fr: 'Chemise en cuir, décolorée par l\'usure.',
+      pt: 'Camisa de couro, desbotada de desgaste.'
     }
   },
 
@@ -28,7 +30,8 @@ export default [
       ru: 'Изогнутая мягкая железная туника, переданная от многих поколений искателей приключений, проходящих по этим землям.',
       es: 'Túnica de hierro blando y nudoso, transmitida por muchas generaciones de aventureros que pasan por estas tierras.',
       de: 'Knorrige Weicheisen-Tunika, die von vielen Generationen von Abenteurern überliefert wurde, die diese Länder durchquerten.',
-      fr: 'Tunique en fer doux et noueux, transmise de génération en génération par les aventuriers de passage sur ces terres.'
+      fr: 'Tunique en fer doux et noueux, transmise de génération en génération par les aventuriers de passage sur ces terres.',
+      pt: 'Uma túnica de ferro macio e manhoso, passada de muitas gerações de aventureiros passando por essas terras.'
     }
   },
 
@@ -39,7 +42,8 @@ export default [
       ru: 'Эта ткань не очень, но она может остановить стрелу или две.',
       es: 'Este tejido no es mucho, pero puede detener una flecha o dos.',
       de: 'Dieser Stoff ist nicht viel, aber er könnte einen oder zwei Pfeile aufhalten.',
-      fr: 'Ce tissu n\'est pas grand-chose, mais il pourrait arrêter une flèche ou deux.'
+      fr: 'Ce tissu n\'est pas grand-chose, mais il pourrait arrêter une flèche ou deux.',
+      pt: 'Este tecido não é muito, mas pode parar uma flecha ou duas.'
     }
   },
 
@@ -50,7 +54,8 @@ export default [
       ru: 'Жесткий костюм из чешуи, взятой у могучего дракона, когда-то бродившего по этим землям.',
       es: 'Traje duro hecho de escamas tomadas de un poderoso dragón que una vez deambulaba por estas tierras.',
       de: 'Ein Hardkostüm aus Schuppen eines mächtigen Drachens, der einst durch dieses Land streift.',
-      fr: 'Costume dur fait d\'écailles prises d\'un puissant dragon qui errait autrefois sur ces terres.'
+      fr: 'Costume dur fait d\'écailles prises d\'un puissant dragon qui errait autrefois sur ces terres.',
+      pt: 'Um fato duro feito de escamas tiradas de um poderoso dragão que percorreu estas terras.'
     }
   },
 
@@ -61,7 +66,8 @@ export default [
       ru: 'Слухи говорят, что они сделаны из облаков...',
       es: 'Los rumores dicen que está hecho de las nubes...',
       de: 'Gerüchte besagen, dass es aus den Wolken gemacht wurde...',
-      fr: 'Les rumeurs disent que c\'est fait à partir des nuages...'
+      fr: 'Les rumeurs disent que c\'est fait à partir des nuages...',
+      pt: 'Os rumores dizem que é feito de nuvens...
     }
   },
 
@@ -72,7 +78,8 @@ export default [
       ru: 'Носитель прекрасно сочетается в тени... или так думают воры.',
       es: 'El portador se mezcla bien entre las sombras... o eso piensan los ladrones.',
       de: 'Der Träger fügt sich gut in den Schatten ein.... so denken die Diebe.',
-      fr: 'Le porteur se fond bien dans l\'ombre... c\'est du moins ce que pensent les voleurs.'
+      fr: 'Le porteur se fond bien dans l\'ombre... c\'est du moins ce que pensent les voleurs.',
+      pt: 'O utilizador mistura-se bem com as sombras... ou assim pensam os ladrões.'
     }
   },
 
@@ -83,7 +90,8 @@ export default [
       ru: 'Выкованный неизвестными магами, вы носите глубокий, темный и мощный нагрудник.',
       es: 'Forjado por magos desconocidos, llevas una placa de pecho profunda, oscura y poderosa.',
       de: 'Geschmiedet von unbekannten Magiern, trägst du einen tiefen, dunklen und kraftvollen Brustschild.',
-      fr: 'Forgé par des magiciens inconnus, vous portez une plaque de poitrine profonde, sombre et puissante.'
+      fr: 'Forgé par des magiciens inconnus, vous portez une plaque de poitrine profonde, sombre et puissante.',
+      pt: 'Forjado por mágicos desconhecidos, você usa uma couraça profunda, escura e poderosa.'
     }
   },
 
@@ -94,7 +102,8 @@ export default [
       ru: 'Выкованный из глубин ада, он обеспечивает защиту вашему слабому, мясистому телу.',
       es: 'Forjada desde las profundidades del infierno, proporciona protección para tu cuerpo débil y carnoso.',
       de: 'Aus den Tiefen der Hölle geschmiedet, bietet es Schutz für deinen schwachen, fleischigen Körper.',
-      fr: 'Forgé des profondeurs de l\'enfer, il protège votre corps faible et charnu.'
+      fr: 'Forgé des profondeurs de l\'enfer, il protège votre corps faible et charnu.',
+      pt: 'Forjado das profundezas do inferno, ele fornece proteção para seu corpo fraco e carnudo.'
     }
   },
 
@@ -105,7 +114,8 @@ export default [
       ru: 'Когда враги убиты, их души добавляются в коллекцию владельца.',
       es: 'Cuando los enemigos son asesinados, sus almas se añaden a la colección del poseedor.',
       de: 'Wenn Feinde getötet werden, werden ihre Seelen der Sammlung des Führers hinzugefügt.',
-      fr: 'Quand des ennemis sont tués, leurs âmes sont ajoutées à la collection du maniaque.'
+      fr: 'Quand des ennemis sont tués, leurs âmes sont ajoutées à la collection du maniaque.',
+      pt: 'Quando os inimigos são mortos, suas almas são adicionadas à coleção do portador.'
     }
   },
 
@@ -116,7 +126,8 @@ export default [
       ru: 'Слухи говорят, что владелец бессмертен. К сожалению, эту броню можно найти только на трупах убитых.',
       es: 'Los rumores dicen que el portador es inmortal. Desafortunadamente, esta armadura sólo se encuentra en los cadáveres de los muertos.',
       de: 'Gerüchten zufolge ist der Träger unsterblich. Leider ist diese Rüstung nur auf den Leichen der Erschlagenen zu finden.',
-      fr: 'Les rumeurs disent que le porteur est immortel. Malheureusement, cette armure ne peut être trouvée que sur les cadavres des victimes.'
+      fr: 'Les rumeurs disent que le porteur est immortel. Malheureusement, cette armure ne peut être trouvée que sur les cadavres des victimes.',
+      pt: 'Rumores dizem que o portador é imortal. Infelizmente esta armadura só pode ser encontrada nos cadáveres dos mortos.'
     }
   }
 
