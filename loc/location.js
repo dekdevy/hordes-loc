078494fd15d2d47235a9c272dce0c8a3fd5c3a12
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-16 10:34:56 +0100

    Update location.js

--- loc/location.js
+++ loc/location.js
@@ -5,7 +5,7 @@ export default {
   },
 
   headlesslanding: {
-    en: 'Barren desert, once the bed of a mighty sea, Headless Landing is a lifeless wasteland home to the Bloodlust lair, one of few civilized locations.'
+    en: 'Barren desert, once the bed of a mighty sea, Headless Landing is a lifeless wasteland home to the Bloodlust lair, one of few civilized locations in this area.'
   }
 
 }
