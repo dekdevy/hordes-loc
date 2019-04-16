5ab3ba8461eb3901d3806f1f7bc65edf0483544c
Aydoğan <aydogan.gokberk@gmail.com>
2019-04-16 23:14:36 +0300

    tr added

--- loc/classes/archer.js
+++ loc/classes/archer.js
@@ -22,7 +22,7 @@ export default {
     fr: 'Les archers infligent des dégâts élevés sur une cible précise et des dégâts de zone importants. Un bon archer garde toujours ses distances et élimine ses ennemis de loin.',
     pt: 'Arqueiros causam danos altos a um único alvo e danos na Área de Efeito. Um bom arqueiro sempre manterá sua distância e snipe inimigos de longe.',
     de: 'Bogenschützen verursachen hohen einzelzielschaden und Berstenden Wirkungsbereich. Ein guter Bogenschütze wird immer Abstand halten und Feinde aus der Ferne schnippen.',
-    nl: 'Boogschutters groot enkel doelschade en barstenschade aan het gebied van het effect toe. Een goede boogschutter zal altijd afstand houden en vijanden uit de verte vernietigen.'
+    nl: 'Boogschutters groot enkel doelschade en barstenschade aan het gebied van het effect toe. Een goede boogschutter zal altijd afstand houden en vijanden uit de verte vernietigen.',
     tr: 'Okçular tekli hedefe yüksek zarar verir ve aralıklı alan etkili hasar verir. İyi bir okçu her zaman mesafesini korur ve hedeflerini uzaktan avlar.'
   }
 }
--- loc/classes/shaman.js
+++ loc/classes/shaman.js
@@ -10,6 +10,7 @@ export default {
     ko: '샤먼',
     pt: 'Xamã',
     nl: 'Sjamaan'
+    tr: 'Şaman'
   },
   // Class description, visible during character creation
   description: {
@@ -21,6 +22,7 @@ export default {
     fr: 'Le rôle des chamans est de soutenir le groupe en guérissant le char et en offrant des Buffs offensifs ou des Débuffs ralentissants.',
     ko: '샤먼의 역할은 탱크를 치유하고 공세적 인 버프 또는 맹독적인 디버프를 제공하여 그룹을 지원하는 것입니다.',
     pt: 'O papel dos xamãs é apoiar o grupo, curando o Tanque e oferecendo Buffs ofensivos ou Debuffs incapacitantes.',
-    nl: 'De rol van de Sjamaan is om de groep te steunen door de Tank te helen en aanvallende Buffs of verlammende Debuffs aan te bieden.'
+    nl: 'De rol van de Sjamaan is om de groep te steunen door de Tank te helen en aanvallende Buffs of verlammende Debuffs aan te bieden.',
+    tr: 'Şamanın rolü, Tankı iyileştirerek ve saldırgan Bufflar veya sakatlayan Debufflar sunarak grubu desteklemektir.'
   }
 }
