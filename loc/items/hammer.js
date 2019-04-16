10f916bada7a8477bbfd68d2f67be36f1cf56496
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-16 14:53:37 +0100

    Update hammer.js

--- loc/items/hammer.js
+++ loc/items/hammer.js
@@ -4,6 +4,7 @@ export default [
     description: {
       en: 'It\'s already half broken and you are still using it?',
       ru: 'Это уже наполовину сломано, и вы все еще используете его?',
+      hi: '',
       es: '¿Ya está medio roto y todavía lo estás usando?',
       de: 'Es ist bereits halb gebrochen und du benutzt es immer noch?',
       fr: 'Il est déjà à moitié cassé et vous l\'utilisez toujours ?',
@@ -16,6 +17,7 @@ export default [
     description: {
       en: 'This should be hammering nails, not skulls.',
       ru: 'Это должны быть гвозди, а не черепа.',
+      hi: '',
       es: 'Esto debería estar clavando clavos, no cráneos.',
       de: 'Das sollten hämmernde Nägel sein, nicht Schädel.',
       fr: 'Ça devrait être des clous martelés, pas des crânes.',
@@ -27,7 +29,8 @@ export default [
     name       : {en: 'Rusty Flail' },
     description: {
       en: 'Rusty old hammer, on the verge of breaking.',
-      ru: 'Ржавый старый молот, на грани поломки.',
+      ru: 'Ржавый старый молот, на грани поломки.',,
+      hi: '',
       es: 'Un viejo martillo oxidado, a punto de romperse.',
       de: 'Rostiger alter Hammer, kurz davor, zu brechen.',
       fr: 'Un vieux marteau rouillé, sur le point de se briser.',
@@ -40,6 +43,7 @@ export default [
     description: {
       en: 'Orcs follow the wielder of this meager weapon. It\'s pretty useless, but they seem to like it...',
       ru: 'Орки следуют за обладателем этого оружия. Oн довольно бесполезно, но им похоже нравится ...',
+      hi: '',
       es: 'Los Orcos siguen al portador de esta magra arma. Es bastante inútil, pero parece que les gusta...',
       de: 'Orks folgen dem Besitzer dieser mageren Waffe. Es ist ziemlich nutzlos, aber sie scheinen es zu mögen...',
       fr: 'Les Orques suivent le maître de cette maigre arme. C\'est plutôt inutile, mais ils ont l\'air d\'aimer ça...',
@@ -52,6 +56,7 @@ export default [
     description: {
       en: 'Large weapon used in the ancient times to cave in skulls.',
       ru: 'Большое оружие, используемое в древние времена для разрушенея черепаx.',
+      hi: '',
       es: 'Gran arma utilizada en la antigüedad para derribar cráneos.',
       de: 'Große Waffe, die in der Antike benutzt wurde, um in Schädeln einzugreifen.',
       fr: 'Grande arme utilisée dans l\'Antiquité pour céder des crânes.',
@@ -64,6 +69,7 @@ export default [
     description: {
       en: 'Strange rod-like basher. Seems crude and emits a faint smell of druid oils...',
       ru: 'Странный стержнеобразный башер. Кажется грубый и испускает слабый запах друидских масел...',
+      hi: '',
       es: 'Extraño golpeador en forma de varilla. Parece crudo y emite un ligero olor a aceite de druida...',
       de: 'Seltsamer stabförmiger Schläger. Scheint roh zu sein und verströmt einen schwachen Geruch von Druidenölen...',
       fr: 'Étrange batteur en forme de baguette. Semble brut et émet une légère odeur d\'huile de druide...',
@@ -76,6 +82,7 @@ export default [
     description: {
       en: 'Ancient dwarves forged this mighty structure. Dark and deadly.',
       ru: 'Древние гномы создали эту могущественную структуру. Темноя и смертельно.',
+      hi: '',
       es: 'Los enanos antiguos forjaron esta poderosa estructura. Oscura y mortal.',
       de: 'Alte Zwerge schmiedeten diese mächtige Struktur. Dunkel und tödlich.',
       fr: 'Les anciens nains ont forgé cette puissante structure. Sombre et mortel.',
@@ -88,6 +95,7 @@ export default [
     description: {
       en: 'Bronze hammer, triangular shaped. Sharp corners make deep indents in heads.',
       ru: 'Бронзовый молот, треугольной формы. Острые углы делают глубокие выемки в головах.',
+      hi: '',
       es: 'Martillo de bronce, forma triangular. Las esquinas afiladas hacen profundas muescas en las cabezas.',
       de: 'Bronzehammer, dreieckig geformt. Scharfe Ecken machen tiefe Einschnitte in den Kopf.',
       fr: 'Marteau en bronze de forme triangulaire. Les coins tranchants creusent de profondes entailles dans la tête.',
@@ -100,6 +108,7 @@ export default [
     description: {
       en: 'Holy tool used by druids to make sacrifices to the ancient nature gods.',
       ru: 'Святой инструмент, используемый друидами, чтобы приносить жертвы древним богам природы.',
+      hi: '',
       es: 'Herramienta sagrada usada por los druidas para hacer sacrificios al gran dios, Yggdrasil.',
       de: 'Heiliges Werkzeug, das von Druiden benutzt wird, um dem großen Gott Yggdrasil Opfer zu bringen.',
       fr: 'Outil sacré utilisé par les druides pour faire des sacrifices au grand dieu, Yggdrasil.',
@@ -112,6 +121,7 @@ export default [
     description: {
       en: 'Heavy basher used by dwarven warriors. Made of an unknown metal, dark and heavy.',
       ru: 'Тяжелый башер, используемый гномами-воинами. Изготовлен из неизвестного металла, темный и тяжелый.',
+      hi: '',
       es: 'Golpeador pesado usado por los guerreros enanos. Hecho de un metal desconocido, oscuro y pesado.',
       de: 'Schwerer Schläger, der von Zwergenkrieger benutzt wird. Hergestellt aus einem unbekannten Metall, dunkel und schwer.',
       fr: 'Une arme lourde utilisée par les guerriers nains. Fait d\'un métal inconnu, sombre et lourd.',
@@ -124,6 +134,7 @@ export default [
     description: {
       en: 'Forged without heat, kills without mercy.',
       ru: 'Выкован без тепла, убивает без пощады.',
+      hi: '',
       es: 'Forjado sin calor, mata sin misericordia.',
       de: 'Ohne Hitze geschmiedet, ohne Gnade getötet.',
       fr: 'Forgé sans chaleur, tue sans pitié.',
@@ -136,6 +147,10 @@ export default [
     description: {
       en: 'Topped with the skull of a slain behemoth enemy, this weapon inspires terror in all who stand against it.',
       ru: 'Это оружие, увенчанное черепом бегемота, внушает ужас всем, кто противостоит ему.',
+      hi: '',
+      es: '',
+      de: '',
+      fr: '',
       pt: 'Encimada com o crânio de um inimigo beemote morto, esta arma inspira terror em todos os que estão contra ela.'
     }
   },
@@ -145,6 +160,7 @@ export default [
     description: {
       en: 'Can penetrate your feeble skull quite easily. Or your foes. Probably yours though.',
       ru: 'Может легко проникнуть в ваш слабый череп. Или твои враги. Вероятно, ваш, хотя.',
+      hi: '',
       es: 'Cubierta con el cráneo de un enemigo gigante asesinado, esta arma inspira terror en todos los que se oponen a ella.',
       de: 'Mit dem Schädel eines getöteten Riesenfeindes überzogen, inspiriert diese Waffe Schrecken bei allen, die sich dagegen stellen.',
       fr: 'Surmontée du crâne d\'un ennemi géant assassiné, cette arme inspire la terreur à tous ceux qui s\'y opposent.',
@@ -157,6 +173,7 @@ export default [
     description: {
       en: 'Nothing kind about this cruel weapon.',
       ru: 'Ничего доброго в этом жестоком  оружии.',
+      hi: '',
       es: 'Esta arma cruel no tiene nada de amable.',
       de: 'Nichts Besonderes an dieser grausamen Waffe.',
       fr: 'Cette arme cruelle n\'a rien de bon.',
@@ -169,6 +186,7 @@ export default [
     description: {
       en: 'Some say its made by dwarves, others say by elves... the truth is yet to be uncovered.',
       ru: 'Некоторые говорят, что он сделано дварфами, другие говорят эльфами... правда еще не раскрыта.',
+      hi: '',
       es: 'Madre Tierra da, y Madre Tierra quita. En este caso, dio.',
       de: 'Einige sagen, dass es von Zwergen gemacht wurde, andere sagen, dass es von Elfen gemacht wurde... die Wahrheit ist noch nicht enthüllt.',
       fr: 'Certains disent que c\'est fait par des nains, d\'autres par des elfes... la vérité na pas encore été découverte.',
@@ -181,6 +199,7 @@ export default [
     description: {
       en: 'Does it have the power to end worlds? Few can say. If it does, we will not live long enough to learn of it.',
       ru: 'Есть ли у него сила, чтобы положить конец мирам? Мало кто может сказать. Если это произойдет, мы не будем жить достаточно долго, чтобы узнать об этом.',
+      hi: '',
       es: '¿Tiene el poder de acabar con los mundos? Pocos pueden decirlo. Si lo hace, no viviremos lo suficiente para aprenderlo.',
       de: 'Hat sie die Macht, Welten zu beenden? Nur wenige können das sagen. Wenn dies der Fall ist, werden wir nicht lange genug leben, um davon zu erfahren.',
       fr: 'A-t-il le pouvoir de mettre fin aux mondes ? Peu de gens peuvent le dire. Si c\'est le cas, nous ne vivrons pas assez longtemps pour l\'apprendre.',
@@ -193,6 +212,7 @@ export default [
     description: {
       en: 'A mysterious magician soaked many intricate charms into this weapon, bringing ghastly thoughts and dreams to its wielder.',
       ru: 'Таинственный волшебник впитал множество замысловатых чар на этом оружии, принося ужасные мысли и мечты его обладателю.',
+      hi: '',
       es: 'Un mago misterioso empapó muchos encantos intrincados en esta arma, trayendo pensamientos y sueños espantosos a su portador.',
       de: 'Mysteriöser Magier, der mit vielen komplizierten Reizen auf dieser Waffe getränkt ist und schreckliche Gedanken und Träume zu seinem Verwalter bringt.',
       fr: 'Mystérieux magicien trempé dans de nombreux charmes complexes sur cette arme, apportant des pensées et des rêves épouvantables à celui qui l\'utilise.',
