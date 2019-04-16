b5f3ec841b513b23a0850263b7bafb5363dde29d
Safe870 <49463377+Safe870@users.noreply.github.com>
2019-04-16 17:06:29 -0400

    Update charmenu.js
    More languages.

--- loc/ui/charmenu.js
+++ loc/ui/charmenu.js
@@ -11,7 +11,16 @@ export default {
       hi: 'स्वरूप चुने',
       es: 'Seleccione Carácter',
       de: 'Charakter auswählen',
-      ko: '캐릭터 선택'
+      ko: '캐릭터 선택',
+      bg: 'Изберете Символ',
+      cs: 'Zvolte znak',
+      el: 'Επιλέξτε χαρακτήρας',
+      ja: 'キャラクターを選択',
+      ph: 'Piliin ang Character',
+      it: 'Seleziona carattere',
+      pl: 'Wybierz postać',
+      pt: 'Selecione o personagem',
+
     },
     create: {
       en: 'Click to create character',
@@ -21,37 +30,74 @@ export default {
       es: 'Haga clic para crear un personaje',
       de: 'Klicken Sie hier, um einen Charakter zu erstellen.',
       fr: 'Cliquez pour créer le personnage',
-      ko: '캐릭터를 만들려면 클릭하십시오'
+      ko: '캐릭터를 만들려면 클릭하십시오',
+      bg: 'Кликнете, за да създадете знак',
+      cs: 'Kliknutím vytvoříte znak',
+      el: 'Κάντε κλικ για να δημιουργήσετε χαρακτήρα',
+      ja: 'クリックしてキャラクターを作成',
+      ph: 'I-click upang lumikha ng character',
+      it: 'Clicca per creare il personaggio',
+      pl: 'Kliknij, aby utworzyć postać',
+      pt: 'Clique para criar personagem',
+
     },
     enterWorld: {
       en: 'Enter World',
-      se: 'Starta Värld',
+      se: 'Ange världen',
       ru: 'Войти в мир',
       fr: 'Entrer le monde',
       hi: 'प्रवेश करे',
       es: 'Entrar en el mundo',
       de: 'Tritt ein in die Welt',
-      ko: '세계에 들어가기'
+      ko: '세계에 들어가기',
+      bg: 'Bлезте в света',
+      cs: 'Vstupte do světa',
+      el: 'Εισάγετε τον κόσμο',
+      ja: '世界に入る',
+      ph: 'Ipasok ang World',
+      it: 'Entra nel mondo',
+      pl: 'Wejdź do świata',
+      pt: 'Entre no mundo',
+
+
     },
     emptySlot: {
       en: 'Empty',
-      se: 'Ingen',
+      se: 'Tom plats',
       ru: 'Пусто',
       hi: 'रिक्त',
       es: 'Vacío',
       de: 'Leer',
       fr: 'Vide',
-      ko: '빈 슬롯'
+      ko: '빈 슬롯',
+      bg: 'Празен слот',
+      cs: 'Prázdné místo',
+      el: 'Κενή υποδοχή',
+      ja: '空きスロット',
+      ph: 'Bakanteng pwesto',
+      it: 'Slot vuoto',
+      pl: 'Puste miejsce',
+      pt: 'Slot vazio',
+
     },
     error: {
       en: 'Network error',
-      se: 'Nätverks problem',
+      se: 'Nätverksfel',
       ru: 'Проблема с сетью',
       fr: 'Problème de réseau',
       hi: 'नेट्वर्क में त्रुटि',
       es: 'Error de red',
       de: 'Netzwerkfehler',
-      ko: '네트워크 오류'
+      ko: '네트워크 오류',
+      bg: 'Мрежова грешка',
+      cs: 'Chyba sítě',
+      el: 'Σφάλμα δικτύου',
+      ja: 'ネットワークエラー',
+      ph: 'Error sa network',
+      it: 'Errore di rete',
+      pl: 'Błąd sieci',
+      pt: 'Erro de rede',
+
     }
   },
 
@@ -65,17 +111,35 @@ export default {
       hi: 'स्वरूप निर्माण',
       es: 'Creación de personajes',
       de: 'Charaktererstellung',
-      ko: '캐릭터 생성'
+      ko: '캐릭터 생성',
+      bg: 'Създаване на символи',
+      cs: 'Tvorba znaků',
+      el: 'Δημιουργία χαρακτήρων',
+      ja: 'キャラクター作成',
+      ph: 'Paglikha ng karakter',
+      it: 'Creazione del personaggio',
+      pl: 'Tworzenie postaci',
+      pt: 'Criação de personagens',
+
     },
     pressIcon: {
       en: 'Press an icon for a brief description.',
-      se: 'Klicka en ikon för beskrivning.',
+      se: 'Tryck på en ikon för en kort beskrivning.',
       ru: 'Нажмите на иконку, чтобы увидеть краткое описание.',
       fr: 'Cliquez sur une icône pour une brève description',
       hi: 'संक्षिप्त विवरण के लिए तस्वीर पर क्लिक करे।',
       es: 'Pulse un icono para obtener una breve descripción.',
       de: 'Drücken Sie ein Symbol für eine Kurzbeschreibung.',
-      ko: '간단한 설명을 보려면 아이콘을 누르십시오'
+      ko: '간단한 설명을 보려면 아이콘을 누르십시오',
+      bg: 'Натиснете икона за кратко описание',
+      cs: 'Pro stručný popis stiskněte ikonu',
+      el: 'Πατήστε ένα εικονίδιο για μια σύντομη περιγραφή',
+      ja: 'アイコンを押すと簡単な説明が表示されます',
+      ph: 'Pindutin ang isang icon para sa maikling paglalarawan',
+      it: 'Premi unicona per una breve descrizione',
+      pl: 'Naciśnij ikonę, aby uzyskać krótki opis',
+      pt: 'Pressione um ícone para uma breve descrição',
+
     },
     selectClass: {
       en: 'Select a class',
@@ -85,27 +149,54 @@ export default {
       es: 'Seleccione una clase',
       de: 'Wählen Sie eine Klasse',
       fr: 'Sélectionnez une classe',
-      ko: '클래스 선택'
+      ko: '클래스 선택',
+      bg: 'Изберете клас',
+      cs: 'Vyberte třídu',
+      el: 'Επιλέξτε μια τάξη',
+      ja: 'クラスを選択',
+      ph: 'Pumili ng klase',
+      it: 'Seleziona una classe',
+      pl: 'Wybierz klasę',
+      pt: 'Selecione uma turma',
+
     },
     selectFaction: {
       en: 'Select a faction',
-      se: 'Välj en sida',
+      se: 'Välj en fraktion',
       ru: 'Выберите фракцию',
       fr: 'Sélectionnez une faction',
       hi: 'वंश चुने',
       es: 'Seleccione una facción',
       de: 'Wählen Sie eine Fraktion aus',
-      ko: '진영을 선택하십시오'
+      ko: '진영을 선택하십시오',
+      bg: 'Изберете фракция',
+      cs: 'Vyberte frakci',
+      el: 'Επιλέξτε μια παράταξη',
+      ja: '派閥を選択',
+      ph: 'Pumili ng isang pangkatin',
+      it: 'Seleziona una fazione',
+      pl: 'Wybierz frakcję',
+      pt: 'Selecione uma facção',
+
     },
     enterName: {
       en: 'Enter a name',
-      se: 'Välj ett namn',
+      se: 'Ange ett namn',
       ru: 'Введите имя',
       fr: 'Entrez un nom',
       hi: 'नाम डाले',
       es: 'Introduzca un nombre',
       de: 'Geben Sie einen Namen ein',
-      ko: '이름 입력'
+      ko: '이름 입력',
+      bg: 'Въведете име',
+      cs: 'Zadejte název',
+      el: 'Εισαγάγετε ένα όνομα',
+      ja: '名前を入力してください',
+      ph: 'Magpasok ng isang pangalan',
+      it: 'Inserisci un nome',
+      pl: 'Wpisz imię',
+      pt: 'Insira o nome',
+
     },
     nameReq: {
       en: 'Your name can only contain characters a-Z, and numbers 0-9, without any spaces. Up to 16 characters.',
@@ -115,7 +206,16 @@ export default {
       hi: 'आपके नाम में केवल a-Z अक्षर और 0-9 अंक हो सकते है| अधिकतम 16 अक्षर।',
       es: 'Su nombre sólo puede contener los caracteres de la a-Z, y los números del 0 al 9, sin espacios. Hasta 16 caracteres.',
       de: 'Dein Name darf nur die Zeichen a-Z und die Zahlen 0-9 ohne Leerzeichen enthalten. Bis zu 16 Zeichen.',
-      ko: '이름에는 공백없이 a-Z와 0-9의 문자 만 사용할 수 있습니다. 최대 16 자.'
+      ko: '이름에는 공백없이 a-Z와 0-9의 문자 만 사용할 수 있습니다. 최대 16 자.',
+      bg: 'Вашето име може да съдържа само знаци a-Z и цифри 0-9, без никакви интервали. До 16 знака.',
+      cs: 'Vaše jméno může obsahovat pouze znaky a-Z a čísla 0-9, bez mezer. Až 16 znaků.',
+      el: 'To όνομά σας μπορεί να περιέχει μόνο χαρακτήρες a-Z και αριθμούς 0-9, χωρίς κενά. Έως 16 χαρακτήρες.',
+      ja: 'あなたの名前はスペースなしで文字a-Zと数字0-9を含むことができます。 16文字まで',
+      ph: 'Ang iyong pangalan ay maaari lamang maglaman ng mga character a-Z, at numero 0-9, nang walang anumang mga puwang. Hanggang sa 16 na mga character.',
+      it: '  Il tuo nome può contenere solo caratteri a-Z e numeri da 0 a 9, senza spazi. Fino a 16 caratteri.',
+      pl: 'Twoje imię może zawierać tylko znaki a-Z i cyfry 0-9, bez spacji. Do 16 znaków.',
+      pt: 'Seu nome pode conter apenas caracteres a-Z e números de 0 a 9, sem espaços. Até 16 caracteres.',
+
     }
   },
 
@@ -124,13 +224,22 @@ export default {
     // It places the characters name after the string, e.g. 'Delete Battlebubbles'
     title: {
       en: 'Delete',
-      se: 'Ta bort',
+      se: 'Radera',
       ru: 'Удалить',
       hi: 'हटा दे',
       es: 'Borrar',
       de: 'Löschen',
       fr: 'Supprimer',
-      ko: '캐릭터 삭제'
+      ko: '캐릭터 삭제',
+      bg: 'Изтрий',
+      cs: 'Smazat',
+      el: 'Διαγράφω',
+      ja: '削除する',
+      ph: 'Tanggalin',
+      it: 'Elimina',
+      pl: 'Kasować',
+      pt: 'Excluir',
+
     },
     info: {
       en: 'To delete, type the character\'s name. Your character can not be restored.',
@@ -140,7 +249,16 @@ export default {
       es: 'Para eliminar, escriba el nombre del caracter. Tu carácter no puede ser restaurado.',
       de: 'Geben Sie zum Löschen den Namen des Zeichens ein. Dein Charakter kann nicht wiederhergestellt werden.',
       fr: 'Pour supprimer, saisissez le nom du personnage. Votre personnage ne pourra pas être restauré.',
-      ko: '삭제하려면 캐릭터의 이름을 입력하십시오. 당신의 캐릭터는 회복 될 수 없습니다.'
+      ko: '삭제하려면 캐릭터의 이름을 입력하십시오. 당신의 캐릭터는 회복 될 수 없습니다.',
+      bg: 'За да изтриете, въведете името на символа. Вашият герой не може да бъде възстановен.',
+      cs: 'Chcete-li odstranit, zadejte název znaku. Vaše postava nemůže být obnovena.',
+      el: 'Για να διαγράψετε, πληκτρολογήστε το όνομα του χαρακτήρα. Ο χαρακτήρας σας δεν μπορεί να αποκατασταθεί.',
+      ja: '削除するには、文字\の名前を入力します。あなたの性格は回復できません。',
+      ph: 'Upang tanggalin, i-type ang pangalan ng character. Ang iyong karakter ay hindi maibabalik.',
+      it: 'Per cancellare, digita il nome del personaggio. Il tuo personaggio non può essere ripristinato.',
+      pl: 'Aby usunąć, wpisz nazwę postaci. Twoja postać nie może zostać przywrócona.',
+      pt: 'Para excluir, digite o nome do caractere. Seu personagem não pode ser restaurado.',
+
     },
     placeholder: {
       en: 'Are you sure?',
@@ -150,7 +268,16 @@ export default {
       hi: 'पक्की तौर पर?',
       es: '¿Estás seguro de eso?',
       de: 'Bist du sicher?',
-      ko: '확실합니까?'
+      ko: '확실합니까?',
+      bg: 'Сигурен ли си?',
+      cs: 'Jsi si jistá?',
+      el: 'Είσαι σίγουρος?',
+      ja: '本気ですか？',
+      ph: 'Sigurado ka ba?',
+      it: 'Sei sicuro?',
+      pl: 'Jesteś pewny?',
+      pt: 'Você tem certeza?',
+
     }
   }
 }
