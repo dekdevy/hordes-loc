90e67df7d5ca2cad6179bf0a2e4168d3accaf602
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-16 09:01:27 +0100

    Update gem.js

--- loc/items/gem.js
+++ loc/items/gem.js
@@ -2,77 +2,88 @@ export default [
   {
     name       : {en: 'Lucid' },
     description: {
-      en: 'Small and sharp, this pendant leaves shallow scrapes on your skin during battle.'
+      en: 'Small and sharp, this pendant leaves shallow scrapes on your skin during battle.',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Melant' },
     description: {
-      en: 'Rough disc-shaped gray stone with strange druidical engravings encasing it.'
+      en: 'Rough disc-shaped gray stone with strange druidical engravings encasing it.',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Turim' },
     description: {
-      en: 'Powerful transparent glass talisman, faint purple tint shows you the world in a melancholic light '
+      en: 'Powerful transparent glass talisman, faint purple tint shows you the world in a melancholic light.',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Fundo' },
     description: {
-      en: 'Brown desaturated gem, emitting putrid scents as you wear it around your neck.'
+      en: 'Brown desaturated gem, emitting putrid scents as you wear it around your neck.',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Amary' },
     description: {
-      en: 'Lustrous blue artifact, exposed wispy white veins running along its spherical surface.'
+      en: 'Lustrous blue artifact, exposed wispy white veins running along its spherical surface.',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Purum' },
     description: {
-      en: 'Radiant titian glass-like gem revealing your foes hidden secrets.'
+      en: 'Radiant titian glass-like gem revealing your foes hidden secrets.',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Royal' },
     description: {
-      en: 'Grand red gem enclosed in abyss dark fissures, granting your foes a sense of jealousy.'
+      en: 'Grand red gem enclosed in abyss dark fissures, granting your foes a sense of jealousy.',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Tara' },
     description: {
-      en: 'The sharp green edges catch sparkling light in the sun, making a beautiful and deadly sight.'
+      en: 'The sharp green edges catch sparkling light in the sun, making a beautiful and deadly sight.',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Gloomy' },
     description: {
-      en: 'Small, smooth and dark, this gem is possesses treacherous traits as it slips from its wielders hands...'
+      en: 'Small, smooth and dark, this gem is possesses treacherous traits as it slips from its wielders hands...',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Pluris' },
     description: {
-      en: 'Spectacular saturated pink plumes rise from this faceted prismical gem. Your foes to stare at you with awe...'
+      en: 'Spectacular saturated pink plumes rise from this faceted prismical gem. Your foes to stare at you with awe...',
+      ru: ''
     }
   },
 
   {
     name       : {en: 'Aeter' },
     description: {
-      en: 'Bright green lights occasionally span out of this glass gem, casting projections of the wearers future.'
+      en: 'Bright green lights occasionally span out of this glass gem, casting projections of the wearers future.',
+      ru: ''
     }
   }
 ]
