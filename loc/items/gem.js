7a46b07051dd01547c7ee4039bce417c55322af4
Cozmicc <49627827+Cozmicc@users.noreply.github.com>
2019-04-15 22:28:32 -0400

    Update gem.js
    Spanish

--- loc/items/gem.js
+++ loc/items/gem.js
@@ -2,77 +2,88 @@ export default [
   {
     name       : {en: 'Lucid' },
     description: {
-      en: 'Small and sharp, this pendant leaves shallow scrapes on your skin during battle.'
+      en: 'Small and sharp, this pendant leaves shallow scrapes on your skin during battle.',
+      es: 'Pequeño y con punta afilada, este pendiente deja somero rasguños en la piel durante de la guerra.'
     }
   },
 
   {
     name       : {en: 'Melant' },
     description: {
-      en: 'Rough disc-shaped gray stone with strange druidical engravings encasing it.'
+      en: 'Rough disc-shaped gray stone with strange druidical engravings encasing it.',
+      es: 'Rugoso con forma de disco gris pierda con extraños inscripción druídicos que lo cerrar.'
     }
   },
 
   {
     name       : {en: 'Turim' },
     description: {
-      en: 'Powerful transparent glass talisman, faint purple tint shows you the world in a melancholic light '
+      en: 'Powerful transparent glass talisman, faint purple tint shows you the world in a melancholic light ',
+      es: 'Poderoso talismán de cristal transparente, de color púrpura pálido que muestra el mundo en una luz melancólica.'
     }
   },
 
   {
     name       : {en: 'Fundo' },
     description: {
-      en: 'Brown desaturated gem, emitting putrid scents as you wear it around your neck.'
+      en: 'Brown desaturated gem, emitting putrid scents as you wear it around your neck.',
+      es: 'Gema desaturada marrón, que emite olores pútridos al llevarlo alrededor del cuello.'
     }
   },
 
   {
     name       : {en: 'Amary' },
     description: {
-      en: 'Lustrous blue artifact, exposed wispy white veins running along its spherical surface.'
+      en: 'Lustrous blue artifact, exposed wispy white veins running along its spherical surface.',
+      es: 'Lustroso artefacto azul, vetas blancas y delgadas expuestas que corren a lo largo de su superficie esférica.'
     }
   },
 
   {
     name       : {en: 'Purum' },
     description: {
-      en: 'Radiant titian glass-like gem revealing your foes hidden secrets.'
+      en: 'Radiant titian glass-like gem revealing your foes hidden secrets.',
+      es: 'Radiante gema en forma de vidrio que revela los secretos ocultos de tus enemigos.'
     }
   },
 
   {
     name       : {en: 'Royal' },
     description: {
-      en: 'Grand red gem enclosed in abyss dark fissures, granting your foes a sense of jealousy.'
+      en: 'Grand red gem enclosed in abyss dark fissures, granting your foes a sense of jealousy.',
+      es: 'Gran gema roja encerrada en las oscuras fisuras del abismo, dando a tus enemigos una sensación de celos.'
     }
   },
 
   {
     name       : {en: 'Tara' },
     description: {
-      en: 'The sharp green edges catch sparkling light in the sun, making a beautiful and deadly sight.'
+      en: 'The sharp green edges catch sparkling light in the sun, making a beautiful and deadly sight.',
+      es: 'Los bordes afilados de color verde captan una luz brillante en el sol, lo que hace que la vista sea hermosa y mortal.'
     }
   },
 
   {
     name       : {en: 'Gloomy' },
     description: {
-      en: 'Small, smooth and dark, this gem is possesses treacherous traits as it slips from its wielders hands...'
+      en: 'Small, smooth and dark, this gem is possesses treacherous traits as it slips from its wielders hands...',
+      es: 'Pequeña, lisa y oscura, esta gema posee rasgos traicioneros ya que se desliza de las manos de sus dueños...'
     }
   },
 
   {
     name       : {en: 'Pluris' },
     description: {
-      en: 'Spectacular saturated pink plumes rise from this faceted prismical gem. Your foes to stare at you with awe...'
+      en: 'Spectacular saturated pink plumes rise from this faceted prismical gem. Your foes to stare at you with awe...',
+      es: 'Espectaculares plumas rosas saturadas surgen de esta gema prismática facetada. Tus enemigos te mirarán con asombro...'
     }
   },
 
   {
     name       : {en: 'Aeter' },
     description: {
-      en: 'Bright green lights occasionally span out of this glass gem, casting projections of the wearers future.'
+      en: 'Bright green lights occasionally span out of this glass gem, casting projections of the wearers future.',
+      es: 'Las luces verdes vivo extienden a veces fuera de esta gema de cristal, proyecciones de fundición del futuro de los usuarios.'
     }
   }
 ]
