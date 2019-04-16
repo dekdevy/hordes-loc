19412bcbad8c52f1a9a1699c6db861b132d3af6c
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-16 10:17:12 +0100

    Update location.js

--- loc/location.js
+++ loc/location.js
@@ -1,13 +1,11 @@
 export default {
 
   guardstone: {
-    en: ''
+    en: 'Built around the Vanguard Castle, Guardstone grants its residents lush green valleys and radiant views while being guarded by the valiant Vangaurd horde.'
   },
 
   headlesslanding: {
-    en: ''
+    en: 'Barren desert, once the bed of a mighty sea, Headless Landing is a lifeless wasteland home to the Bloodlust lair, one of few civilized locations.'
   }
-
-
 
 }
