782d5c400ecf8041da10269c1e24ef6afaca403a
Vedant Bang <33593544+VedantBang@users.noreply.github.com>
2019-04-16 09:10:37 +0530

    Added हिंदी

--- loc/items/ring.js
+++ loc/items/ring.js
@@ -6,7 +6,8 @@ export default [
       ru: 'Не смотрите на ево, а то он развалится.',
       es: 'No lo mires o se caerá a pedazos.',
       de: 'Sieh es dir nicht an, sonst fällt es auseinander.',
-      fr: 'Ne la regardez pas ou elle va s\'effondrer.'
+      fr: 'Ne la regardez pas ou elle va s\'effondrer.',
+      hi: 'इसकी तरफ़ देखना मत। टूट जाएगा।'
     }
   },
 
@@ -16,7 +17,8 @@ export default [
       en: 'Not sure about the bark, but its definitely made of iron.',
       ru: 'Не уверен насчет коры, но оно определена зделана из железы.',
       es: 'No estoy seguro de la corteza, pero definitivamente es de hierro.',
-      de: 'Nicht sicher über die Rinde, aber sно оно определена зделана из железы\'est du fer.'
+      de: 'Nicht sicher über die Rinde, aber sно оно определена зделана из железы\'est du fer.',
+      hi: 'चाल का तो पता नहीं पर ये लोहे का ज़रूर बना है।'
     }
   },
 
@@ -27,7 +29,8 @@ export default [
       ru: 'Наконец-то блисташия колто.',
       es: 'Finalmente, un anillo que brilla.',
       de: 'Endlich ein Ring, der glänzt.',
-      fr: 'Enfin, un anneau qui brille.'
+      fr: 'Enfin, un anneau qui brille.',
+      hi: 'एक चमकीली अँगूठी।'
     }
   },
 
@@ -38,7 +41,8 @@ export default [
       ru: 'Колто из остатков прошлова ужина.',
       es: 'Un anillo hecho con las sobras de anoche.',
       de: 'Ein Ring aus den Überresten der letzten Nächte.',
-      fr: 'Une bague faite des restes des dernières nuits.'
+      fr: 'Une bague faite des restes des dernières nuits.',
+      hi: 'कल के बचे खुचे खाने से बनी अँगूठी।'
     }
   },
 
@@ -49,7 +53,8 @@ export default [
       ru: 'Эльфы сделали это кольцо, чтобы защитить себя от опасности, и удалось отдать его',
       es: 'Los elfos hicieron este anillo para protegerse del peligro y lograron regalarlo.',
       de: 'Die Elfen bauten diesen Ring, um sich vor Gefahren zu schützen, und es gelang ihnen, ihn wegzugeben.',
-      fr: 'Les elfes ont fabriqué cet anneau pour se protéger du danger et ont réussi à le donner.'
+      fr: 'Les elfes ont fabriqué cet anneau pour se protéger du danger et ont réussi à le donner.',
+      hi: 'एलव लोगों ने ख़ुद को ख़तरे से बचाने के लिए इसे बनाया था, और इसे हमें देने में सफल रहे।'
     }
   },
 
@@ -60,7 +65,8 @@ export default [
       ru: 'Хорошо вписывается, хотя эффекты, которые он дает, незначительны.',
       es: 'Encaja bien, aunque los efectos que produce son marginales.',
       de: 'Passt gut, auch wenn die Wirkung gering ist.',
-      fr: 'Elle convient bien, bien que les effets qu\'elle produit soient marginaux.'
+      fr: 'Elle convient bien, bien que les effets qu\'elle produit soient marginaux.',
+      hi: 'सही बैठती है, भले ही इसकी शक्तियाँ कुछ ख़ास नहीं।'
     }
   },
 
@@ -71,7 +77,8 @@ export default [
       ru: 'Никто не знает, стоит ли ево надевать.',
       es: 'Nadie sabe si vale la pena ponerse esto.',
       de: 'Niemand weiß, ob es sich lohnt, sie anzuziehen.',
-      fr: 'Personne ne sait si cela vaut la peine d\'être mis en place.'
+      fr: 'Personne ne sait si cela vaut la peine d\'être mis en place.',
+      hi: 'इसे पहना या ना पहना, कोई नहीं जानते।'
     }
   },
 
@@ -82,7 +89,8 @@ export default [
       ru: 'Изумруды со всех сторон, чтобы вы чувствовали себя богатыми.',
       es: 'Esmeraldas por todos lados para que te sientas rico.',
       de: 'Smaragde auf allen Seiten, damit Sie sich reich fühlen.',
-      fr: 'Des émeraudes de tous les côtés pour vous faire sentir riche.'
+      fr: 'Des émeraudes de tous les côtés pour vous faire sentir riche.',
+      hi: 'इसके चारों ओर क़ीमती पत्थर जड़े है, जिनसे आप धनी महसूस करे।'
     }
   },
 
@@ -93,7 +101,8 @@ export default [
       es: 'Si pudiera hablar, probablemente se quejaría de ser triangular.',
       de: 'Gefühl zu geben, wenn es sprechen könnte, würde es sich wahrscheinlich darüber beschweren, dreieckig zu sein.',
       fr: 'Si elle pouvait parler, elle se plaindrait probablement d\'être triangulaire.',
-      ru: 'Если он мог говорить, он вероятно, пожаловался что он треугольный.'
+      ru: 'Если он мог говорить, он вероятно, пожаловался что он треугольный.',
+      hi: 'अगर ये अँगूठी बोल पति तो अपने त्रिकोण होने की शिकायत करती।'
     }
   },
 
@@ -104,7 +113,8 @@ export default [
       ru: 'Не существует никаких письменных отчетов о его происхождении ....',
       es: 'No existen registros escritos que nos indiquen su origen....',
       de: 'Es gibt keine schriftlichen Aufzeichnungen, um uns über die Herkunft zu informieren....',
-      fr: 'Il n\'existe aucune trace écrite de son origine....'
+      fr: 'Il n\'existe aucune trace écrite de son origine....',
+      hi: 'इसके बारे में कोई जानकारी उपलब्ध नहीं है।'
     }
   },
 
@@ -115,7 +125,8 @@ export default [
       ru: 'Священный артефакт, потерянный святыми друидами.',
       es: 'Artefacto sagrado perdido por druidas sagrados.',
       de: 'Heiliges Artefakt, das von heiligen Druiden verloren wurde.',
-      fr: 'Artefact sacré perdu par les druides sacrés.'
+      fr: 'Artefact sacré perdu par les druides sacrés.',
+      hi: 'पवित्र द्रुइड ने इस धार्मिक अँगूठी को खो दिया था।'
     }
   },
 
@@ -126,7 +137,8 @@ export default [
       ru: 'Хранитель мира .... не уверен в этом, но он может подержать тебя в живых ....',
       es: 'Guardián de la paz.... no estoy seguro de eso, pero podría mantenerte vivo.',
       de: 'Hüter des Friedens.... nicht sicher, aber vielleicht hält dich das am Leben.',
-      fr: 'Gardien de la paix.... pas sûr de ça, mais ça pourrait vous garder en vie.'
+      fr: 'Gardien de la paix.... pas sûr de ça, mais ça pourrait vous garder en vie.',
+      hi: 'शांति की रखवाली....इसकी गैरंटी तो नहीं, पर आपको ज़िंदा रख पाएगी शायद....'
     }
   }
 
