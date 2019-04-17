0b97ed5821c1ba97d6f3686881b89f8ca228fa69
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-17 16:51:37 +0100

    Update link.js

--- random/link.js
+++ random/link.js
@@ -11,7 +11,7 @@ const adjectives = ['epic', 'rare', 'common', 'trashy', 'magical', 'thunderous',
   'ignorant', 'divine', 'eccentric', 'gloomy', 'funny', 'safe', 'insured', 'cultist'. 'washed', 'mighty', 'depressed',
   'wild', 'hallowed', 'heavy', 'ghostly', 'sly', 'sneaky', 'flooded', 'waterlogged', 'stretchy', 'dodgy', 'slippery',
   'electrical', 'mature', 'immature', 'old', 'new', 'young', 'ancient', 'elderly', 'pythonic', 'poetic', 'oily', 'ageing',
-  'growing', 'communist', 'socialist']
+  'growing', 'communist', 'socialist', 'democratic', 'vile', 'disgusting', 'lovely', 'swampy', 'holy']
 
 const nouns = ['sword', 'axe', 'potion', 'hammer', 'mace', 'coin', 'bag', 'archer', 'warrior', 'mage', 'unicorn',
   'deer', 'dog', 'cat', 'staff', 'bear', 'banana', 'apple', 'boots', 'armor', 'helmet', 'tower', 'forge', 'castle',
