7648957b25d9de9cb2cf1ae44c3042745d192624
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-17 13:12:58 +0100

    Update name.js

--- random/name.js
+++ random/name.js
@@ -32,7 +32,7 @@ const syllables = ['heim', 'tuk', 'ta', 'lor', 'bok', 'le', 'me', 'ka', 'na', 'p
   'rads', 'stim', 'pak', 'scorp', 'mire', 'spart', 'mutt', 'io', 'cans', 'ole', 'ian', 'reek', 'off', 'guard', 'van', 'scrub', 'sign',
   'road', 'awe', 'way', 'ant', 'woe', 'grief', 'active', 'ultra', 'mega', 'multi', 'mill', 'cent', 'nano', 'micro', 'fore', 'foe', 'ever',
   'zel', 'away', 'rio', 'orion', 'sec', 'cas', 'mor', 'vank', 'vakh', 'ject', 'rej', 're', 'xyl', 'phlo', 'flow', 'len', 'size', 'small',
-  'big', 'large', 'tiny', 'massive', 'minute', 'scope', 'signed', 'harp']
+  'big', 'large', 'tiny', 'massive', 'minute', 'scope', 'signed', 'harp', 'beth', 'esda', 'arq', 'nubis']
 
 export const generate = (minSyl = 2, maxSyl = 6, maxLen = 8)=>{
   // generates a random name with a variable max length / syllable amount
