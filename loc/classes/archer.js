beb1436431d07f6f860561e62b00fab8528aaad8
KaiserCheezel <49390953+KaiserCheezel@users.noreply.github.com>
2019-04-18 20:46:29 +1200

    test

--- loc/classes/archer.js
+++ loc/classes/archer.js
@@ -19,6 +19,7 @@ export default {
   // Class description, visible during character creation
   description: {
     en: 'Archers deal high single target damage and bursty Area of Effect damage. A good archer will always keep their distance and snipe enemies from afar.',
+    en-UK: 'Archers deal high single target damage and bursty Area of Effect damage. A good archer will always keep their distance and snipe enemies from afar.'
     se: 'Bågskytt gör hög skada mot enskilda personer och har även hög Areal skada. En bra bågskytt attackerar alltid från en distans.',
     ru: 'Лучники наносят высокий урон по одиночной цели и взрывной урон по области. Хороший лучник всегда будет держать дистанцию и отстреливать врагов издалека.',
     ko: '궁수는 높은 단일 타겟 데미지와 버스트 에어리어 피해를줍니다. 좋은 궁수는 먼 거리에서 항상 적을 공격합니다.',
