f9d12de0f3400546c409d8a0dceb75ba9d61499e
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-18 10:07:10 +0100

    Update warrior.js

--- loc/classes/warrior.js
+++ loc/classes/warrior.js
@@ -3,7 +3,7 @@ export default {
   name: {
     en: 'Warrior',
     de: 'Krieger',
-    se: 'Krigare',
+    sv: 'Krigare',
     ru: 'Воин',
     es: 'Soldado',
     fr: 'Guerrier',
@@ -19,7 +19,7 @@ export default {
   // Class description, visible during character creation
   description: {
     en: 'Warriors are built to tank more monsters than any other class by having high defense through equipment (Shields) and strong defensive buffs.',
-    se: 'Krigare är byggda för att tåla mer skada än alla andra klasser genom högt försvar från sin utrustning (Sköld) och starka försvars Buffar.',
+    sv: 'Krigare är byggda för att tåla mer skada än alla andra klasser genom högt försvar från sin utrustning (Sköld) och starka försvars Buffar.',
     ru: 'Воины построены для того, чтобы танковать больше монстров чем любой другой класс, имея хорошую защиту с помощью снаряжения (Щита) и сильных защитных усилений.',
     de: 'Krieger sind so gebaut, dass sie mehr Monster als jede andere Klasse besiegen, indem sie eine hohe Verteidigung durch Ausrüstung (Schilde) und starke Verteidigungszauber haben.',
     es: 'Soldados son los tanques de la guerra, soldados luchan para el honor y gloria de la batalla. Ellos pueden aguantar un gran cantidad de daño de todo los otro clases con sus escudos y buffs de defensa.',
