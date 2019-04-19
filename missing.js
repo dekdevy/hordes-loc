5f170bea51996dd2b5bc77393011a36adf1b2770
zbot473 <13140490+zbot473@users.noreply.github.com>
2019-04-19 12:54:01 -0400

    Update missing.js

--- missing.js
+++ missing.js
@@ -24,7 +24,7 @@ const findmissing = (obj, currentType, path)=>{
   Object.entries(obj).forEach( ([ key, value ]) => {
     if(key == '$$') return
     // check whether value is object or string
-    const next = path !== undefined ? `${path} ${key}` : key
+    const next = path !== undefined ? `${path} → ${key}` : key
     if(typeof value  === 'object') {
       // if object, check whether object has strings as children
       if(Object.entries(value).some(v => typeof v[1] === 'string')) {
