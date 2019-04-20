435c9341426fdb5d78b34abe7f8f4824e26d6fc8
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-20 20:22:22 +0100

    Add files via upload

---
+++ loc/items/pendant.js
@@ -0,0 +1,107 @@
+export default [
+  {
+    name       : {$$: 'Smelly Talisman' },
+    description: {
+      en: 'Smells so bad, I wonder why you even picked it up.'
+    }
+  },
+
+  {
+    name       : {$$: 'Bone Plate' },
+    description: {
+      en: 'The shoulder plate of some creature fares well as a portable eating utensil.'
+    }
+  },
+
+  {
+    name       : {$$: 'Polished Slab' },
+    description: {
+      en: 'Heavy necklace, smoothed and polished to blind you during battle'
+    }
+  },
+
+  {
+    name       : {$$: 'Apple Slice' },
+    description: {
+      en: 'A putrid morsel from last years harvest.'
+    }
+  },
+
+  {
+    name       : {$$: 'Skimmer' },
+    description: {
+      en: 'Thin slice of granite, perfect for bouncing across the ripples of the lake.'
+    }
+  },
+
+  {
+    name       : {$$: 'Silk Adornment' },
+    description: {
+      en: 'Shimmering silver fabric said to be stolen from Aphrodite herself.'
+    }
+  },
+
+  {
+    name       : {$$: 'Frosted Necklace' },
+    description: {
+      en: 'Cold and blue, the pendant steals your warmth as a payment for its effects...'
+    }
+  },
+
+  {
+    name       : {$$: 'Ocean Lavalier' },
+    description: {
+      en: 'Surprisingly, it doesn't smell like fish. Just don't show it freshwater.'
+    }
+  },
+
+  {
+    name       : {$$: 'Verstärker' },
+    description: {
+      en: 'Armies squandered over this trinket yet it seems worthless to you...'
+    }
+  },
+
+  {
+    name       : {$$: 'Golden Brooch' },
+    description: {
+      en: 'Humans, dwarves and elves owned this artifact, passing it on to generations of hunter, warriors and great battle leaders.'
+    }
+  },
+
+  {
+    name       : {$$: 'Myrso' },
+    description: {
+      en: 'The colour of sunset, it swarms images and projections into your conscience, telling you your next misfortune'
+    }
+  },
+
+  {
+    name       : {$$: 'Lozlo' },
+    description: {
+      en: 'Intricate weaves of mithril bound to powerful magic, filling you with a mysterious energy felt by few of those living on this world'
+    }
+  },
+
+  {
+    name       : {$$: 'Blodhgai' },
+    description: {
+      en: 'Demonic shadows evade the blades of reality, granting you leave into your foes monstrous dreams.'
+    }
+  },
+
+  {
+    name       : {$$: 'Delirious' },
+    description: {
+      en: 'Your foes hallucinatory state of mind brought upon by this trinket permits your cunning blows to strike home'
+    }
+  },
+
+  {
+    name       : {$$: 'Livid' },
+    description: {
+      en: 'Deep dark blue talisman, tracing patterns in your soul, reinforcing, rebuilding and renovating for the world of immortality.'
+    }
+  }
+
+]
