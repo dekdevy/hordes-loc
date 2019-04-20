40679c7a4e41e317de22480eba8770dac62bd8a9
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-20 20:09:59 +0100

    Create party.js

---
+++ loc/ui/party.js
@@ -0,0 +1,24 @@
+export default {
+
+  /* ! Note: !
+    Do not translate the word "Party", to ensure consistent player communication
+  */
+
+  invite: {
+    en: 'Party invite',
+    ru: ' Пригласить в party'
+  },
+
+  kick: {
+    en: 'Party kick',
+    ru: 'Выгнать'
+  },
+
+  leave: {
+    en: 'Leave party'
+  },
+
+  onInvite: {
+    en: '$1 has invited you to join their party.'
+  }
+}
