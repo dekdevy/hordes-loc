c53e2a22bbba3bf7cb108a45dd94029659104749
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-20 20:38:09 +0100

    Update talisman.js

--- loc/items/talisman.js
+++ loc/items/talisman.js
@@ -2,105 +2,120 @@ export default [
   {
     name       : {$$: 'Smelly Talisman' },
     description: {
-      en: 'Smells so bad, I wonder why you even picked it up.'
+      en: 'Smells so bad, I wonder why you even picked it up.',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Bone Plate' },
     description: {
-      en: 'The shoulder plate of some creature fares well as a portable eating utensil.'
+      en: 'The shoulder plate of some creature fares well as a portable eating utensil.',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Polished Slab' },
     description: {
-      en: 'Heavy necklace, smoothed and polished to blind you during battle'
+      en: 'Heavy necklace, smoothed and polished to blind you during battle',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Apple Slice' },
     description: {
-      en: 'A putrid morsel from last years harvest.'
+      en: 'A putrid morsel from last years harvest.',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Skimmer' },
     description: {
-      en: 'Thin slice of granite, perfect for bouncing across the ripples of the lake.'
+      en: 'Thin slice of granite, perfect for bouncing across the ripples of the lake.',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Silk Adornment' },
     description: {
-      en: 'Shimmering silver fabric said to be stolen from Aphrodite herself.'
+      en: 'Shimmering silver fabric said to be stolen from Aphrodite herself.',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Frosted Necklace' },
     description: {
-      en: 'Cold and blue, the pendant steals your warmth as a payment for its effects...'
+      en: 'Cold and blue, the pendant steals your warmth as a payment for its effects...',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Ocean Lavalier' },
     description: {
-      en: 'Surprisingly, it doesn't smell like fish. Just don't show it freshwater.'
+      en: 'Surprisingly, it doesn\'t smell like fish. Just don\'t show it freshwater.',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Verstärker' },
     description: {
-      en: 'Armies squandered over this trinket yet it seems worthless to you...'
+      en: 'Armies squandered over this trinket yet it seems worthless to you...',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Golden Brooch' },
     description: {
-      en: 'Humans, dwarves and elves owned this artifact, passing it on to generations of hunter, warriors and great battle leaders.'
+      en: 'Humans, dwarves and elves owned this artifact, passing it on to generations of hunter, warriors and great battle leaders.',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Myrso' },
     description: {
-      en: 'The colour of sunset, it swarms images and projections into your conscience, telling you your next misfortune'
+      en: 'The colour of sunset, it swarms images and projections into your conscience, telling you your next misfortune',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Lozlo' },
     description: {
-      en: 'Intricate weaves of mithril bound to powerful magic, filling you with a mysterious energy felt by few of those living on this world'
+      en: 'Intricate weaves of mithril bound to powerful magic, filling you with a mysterious energy felt by few of those living on this world',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Blodhgai' },
     description: {
-      en: 'Demonic shadows evade the blades of reality, granting you leave into your foes monstrous dreams.'
+      en: 'Demonic shadows evade the blades of reality, granting you leave into your foes monstrous dreams.',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Delirious' },
     description: {
-      en: 'Your foes hallucinatory state of mind brought upon by this trinket permits your cunning blows to strike home'
+      en: 'Your foes hallucinatory state of mind brought upon by this trinket permits your cunning blows to strike home',
+      ru: ''
     }
   },
 
   {
     name       : {$$: 'Livid' },
     description: {
-      en: 'Deep dark blue talisman, tracing patterns in your soul, reinforcing, rebuilding and renovating for the world of immortality.'
+      en: 'Deep dark blue talisman, tracing patterns in your soul, reinforcing, rebuilding and renovating for the world of immortality.',
+      ru: ''
     }
   }
 
