745a24b89bc48b27b51acd567828aa4aec523e56
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-21 18:38:11 +0100

    Update orb.js

--- loc/items/orb.js
+++ loc/items/orb.js
@@ -4,7 +4,8 @@ export default [
     description: {
       en: 'Moist ball of earth, leaving behind tiny critters in your hand.',
       de: 'Feuchter Erdball, der winzige Lebewesen in der Hand hinterlässt.',
-      fr: 'Boule de terre humide, laissant de minuscules créatures dans votre main.'
+      fr: 'Boule de terre humide, laissant de minuscules créatures dans votre main.',
+      nl: 'Vochtige bal van de aarde, met achterlating van kleine beestjes in je hand.'
     }
   },
 
@@ -13,7 +14,8 @@ export default [
     description: {
       en: 'A grotesque mass of bone bringing luck to the superstitious.',
       de: 'Eine groteske Knochenmasse, die den Abergläubischen Glück bringt.',
-      fr: 'Une masse osseuse grotesque qui porte chance aux superstitieux.'
+      fr: 'Une masse osseuse grotesque qui porte chance aux superstitieux.',
+      nl: 'Een groteske bottenmassa die geluk brengt aan de bijgelovigen.'
     }
   },
 
@@ -22,7 +24,8 @@ export default [
     description: {
       en: 'Buzzing sticky hive, delivering a couple of stings now and then.',
       de: 'Summender, klebriger Bienenstock, der ab und zu ein paar Stiche liefert.',
-      fr: 'Une ruche bourdonnante et collante, avec quelques piqûres de temps en temps.'
+      fr: 'Une ruche bourdonnante et collante, avec quelques piqûres de temps en temps.',
+      nl: 'Zoemende kleverige hicky hive, die af en toe een paar steken levert.'
     }
   },
 
@@ -31,7 +34,8 @@ export default [
     description: {
       en: 'After a tough fight while robbing a bone of a dog, you decided to keep this as a souvenir reminding you of your struggles.',
       de: 'Nach einem harten Kampf beim Raub eines Hundeknochens hast du dich entschieden, diesen als Souvenier aufzubewahren, der dich an deine Kämpfe erinnert.',
-      fr: 'Après une rude bataille alors que vous voliez l\'os d\'un chien, vous avez décidé de garder ceci en souvenir de vos luttes.'
+      fr: 'Après une rude bataille alors que vous voliez l\'os d\'un chien, vous avez décidé de garder ceci en souvenir de vos luttes.',
+      nl: 'Na een zware ruzie tijdens het beroven van een bot van een hond, besloot u dit te bewaren als een souvenir dat u herinnert aan uw strijd.'
     }
   },
 
@@ -40,7 +44,8 @@ export default [
     description: {
       en: 'Leftovers from a couple of nights ago have made quite a stink.',
       de: 'Überreste von vor ein paar Nächten haben einen ziemlichen Gestank verursacht.',
-      fr: 'Les restes d\'il y a quelques nuits ont fait une sacrée puanteur.'
+      fr: 'Les restes d\'il y a quelques nuits ont fait une sacrée puanteur.',
+      nl: 'De restjes van een paar nachten geleden hebben een behoorlijke stank veroorzaakt.'
     }
   },
 
@@ -49,7 +54,8 @@ export default [
     description: {
       en: 'Small metal ball, shiny and smooth with some dents on its surface.',
       de: 'Kleine Metallkugel, glänzend und glatt, mit einigen Beulen auf der Oberfläche.',
-      fr: 'Petite boule métallique, brillante et lisse avec quelques bosses sur sa surface.'
+      fr: 'Petite boule métallique, brillante et lisse avec quelques bosses sur sa surface.',
+      nl: 'Kleine metalen bal, glanzend en glad met enkele deuken op het oppervlak.'
     }
   },
 
@@ -58,7 +64,8 @@ export default [
     description: {
       en: 'Rays of light get caught in this sphere, casting fine rainbows around you.',
       de: 'Lichtstrahlen werden in dieser Sphäre gefangen und werfen feine Regenbögen um sich herum.',
-      fr: 'Des rayons de lumière s\'accrochent dans cette sphère, projetant de fins arcs-en-ciel autour de vous.'
+      fr: 'Des rayons de lumière s\'accrochent dans cette sphère, projetant de fins arcs-en-ciel autour de vous.',
+      nl: 'Stralen van licht raken verstrikt in deze bol en werpen fijne regenbogen om je heen.'
     }
   },
 
@@ -67,7 +74,8 @@ export default [
     description: {
       en: 'The ancient cyclopes terrorizing these lands was finally killed, allowing you to scavenge this grim souvenir.',
       de: 'Der alte Zyklop, der diese Länder terrorisiert hat, wurde schließlich getötet, so dass Sie dieses düstere Souvenir entsorgen können.',
-      fr: 'Les anciens cyclopes qui terrorisaient ces terres ont finalement été tués, ce qui vous a permis de récupérer ce sinistre souvenir.'
+      fr: 'Les anciens cyclopes qui terrorisaient ces terres ont finalement été tués, ce qui vous a permis de récupérer ce sinistre souvenir.',
+      nl: 'De oude cyclopen die deze landen terroriseerden, werden uiteindelijk gedood, waardoor je dit grimmige souvenir kon wegvreten.'
     }
   },
 
@@ -76,7 +84,8 @@ export default [
     description: {
       en: 'Some mages use staffs, some books, you decided to use a frozen clump of snow.',
       de: 'Einige Magier benutzen Stäbe, einige Bücher, du hast dich entschieden, ein gefrorenes Schneebrett zu benutzen.',
-      fr: 'Certains mages utilisent des bâtons, des livres, vous avez décidé d\'utiliser un amas de neige gelée.'
+      fr: 'Certains mages utilisent des bâtons, des livres, vous avez décidé d\'utiliser un amas de neige gelée.',
+      nl: 'Sommige magiërs gebruiken personeel, sommige boeken, sommige boeken, je hebt besloten om een bevroren klomp sneeuw te gebruiken.'
     }
   },
 
@@ -85,7 +94,8 @@ export default [
     description: {
       en: 'Lustrous and charming, this mysterious sphere makes the air around it ice-cold.',
       de: 'Schimmernd und charmant, lässt diese mysteriöse Kugel die Luft um sie herum eiskalt werden.',
-      fr: 'Lustrée et charmante, cette sphère mystérieuse rend l\'air autour d\'elle glacial.'
+      fr: 'Lustrée et charmante, cette sphère mystérieuse rend l\'air autour d\'elle glacial.',
+      nl: 'Deze mysterieuze en charmante sfeer maakt de lucht er omheen ijskoud.'
     }
   },
 
@@ -94,7 +104,8 @@ export default [
     description: {
       en: 'Enigmatic emerald green sphere, enveloped in fine engravings of serpentine prophecies, enticing fame and serendipity to its naive bearer.',
       de: 'Enigmatische smaragdgrüne Kugel, umhüllt von feinen Gravuren serpentinischer Prophezeiungen, die Ruhm und Glücksfall zu ihrem naiven Träger verleiten.',
-      fr: ''
+      fr: 'Sphère énigmatique vert émeraude, enveloppée de fines gravures de prophéties serpentines, séduisant gloire et sérendipité à son porteur naïf.',
+      nl: 'Enigmatische smaragdgroene bol, gehuld in fijne gravures van kronkelige profetieën, die roem en serendipiteit verleidt tot zijn naïeve drager.'
     }
   },
 
@@ -103,7 +114,8 @@ export default [
     description: {
       en: 'Transparent yellow tinged orb, revealing trapped infernal plumes of paranormal aura, engulfing the minds of the weak with plans of poison and murder.',
       de: 'Transparente, gelb gefärbte Kugel, die gefangene infernalische Federn paranormaler Aura enthüllt, die den Geist der Schwachen mit Plänen von Gift und Mord umhüllen.',
-      fr: 'Sphère énigmatique vert émeraude, enveloppée de fines gravures de prophéties serpentines, séduisant gloire et sérendipité à son porteur naïf.'
+      fr: 'Sphère énigmatique vert émeraude, enveloppée de fines gravures de prophéties serpentines, séduisant gloire et sérendipité à son porteur naïf.',
+      nl: 'Transparante geel getinte bol, die de gevangen helse pluimen van paranormale aura onthult en de geesten van de zwakken overspoelt met plannen van vergif en moord.'
     }
   },
 
@@ -112,7 +124,8 @@ export default [
     description: {
       en: 'Pale blue faceted artifact, radiating pulsing waves of light, banishing the dark shadows of demonic souls from your path to glory.',
       de: 'Hellblaues facettiertes Artefakt, das pulsierende Lichtwellen ausstrahlt und die dunklen Schatten dämonischer Seelen von deinem Weg zur Herrlichkeit vertreibt.',
-      fr: 'Artefact bleu pâle à facettes, rayonnant des ondes pulsantes de lumière, bannissant les ombres sombres des âmes démoniaques de votre chemin vers la gloire.'
+      fr: 'Artefact bleu pâle à facettes, rayonnant des ondes pulsantes de lumière, bannissant les ombres sombres des âmes démoniaques de votre chemin vers la gloire.',
+      nl: 'Lichtblauw gefacetteerde artefacten, die pulserende lichtgolven uitstralen en de donkere schaduwen van demonische zielen van je pad naar glorie verbannen.'
     }
   },
 
@@ -121,7 +134,8 @@ export default [
     description: {
       en: 'Flesh and blood infused this orb with the souls of the slain, creating a dark red essence embedded in itself, granting you unprecedented lust for blood and battle.',
       de: 'Fleisch und Blut durchdrangen diese Kugel mit den Seelen der Erschlagenen und schufen eine dunkelrote Essenz, die in sich selbst eingebettet ist und Ihnen eine beispiellose Lust auf Blut und Kampf schenkt.',
-      fr: 'La chair et le sang ont infusé cet orbe avec les âmes des morts, créant une essence rouge foncé incrustée en elle-même, vous accordant une convoitise sans précédent pour le sang et la bataille.'
+      fr: 'La chair et le sang ont infusé cet orbe avec les âmes des morts, créant une essence rouge foncé incrustée en elle-même, vous accordant une convoitise sans précédent pour le sang et la bataille.',
+      nl: 'Vlees en bloed heeft deze bol doordrenkt met de zielen van de gedode mensen, waardoor een donkerrode essentie in zichzelf is ingebed, waardoor je een ongekende lust voor bloed en strijd krijgt.'
     }
   },
 
@@ -130,7 +144,8 @@ export default [
     description: {
       en: 'Dark, deadly and demonic, an orb encased in small craters filled with blood-red gems, weaving sheets of terror into those who oppose you.',
       de: 'Dunkel, tödlich und dämonisch, eine Kugel, die von kleinen Kratern voller blutroter Edelsteine umgeben ist, die Schreckensblätter in diejenigen einweben, die sich dir widersetzen.',
-      fr: 'Sombre, mortel et démoniaque, un globe encastré dans de petits cratères remplis de pierres précieuses rouge sang, tissant des feuilles de terreur en ceux qui vous opposent.'
+      fr: 'Sombre, mortel et démoniaque, un globe encastré dans de petits cratères remplis de pierres précieuses rouge sang, tissant des feuilles de terreur en ceux qui vous opposent.',
+      nl: 'Donker, dodelijk en duivels, een orb, omgeven door kleine kraters gevuld met bloedrode edelstenen, die met schrik weven vellen van terreur in degenen die zich tegen je verzetten.'
     }
   }
 
