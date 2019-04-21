e99ffd63bdceaab4e10c81ef65785aad536b83e1
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-20 21:24:29 -0400

    Update party.js

--- loc/ui/party.js
+++ loc/ui/party.js
@@ -6,19 +6,35 @@ export default {
 
   invite: {
     en: 'Party invite',
-    ru: ' Пригласить в party'
+    ru: ' Пригласить в party',
+    es: 'Invitación a la fiesta',
+    de: 'Partyeinladung',
+    fr: 'Invitation à une fête',
+    ht: 'Pati envite'
   },
 
   kick: {
     en: 'Party kick',
-    ru: 'Выгнать'
+    ru: 'Выгнать',
+    es: 'Patada',
+    de: 'Treten',
+    fr: 'Coup de pied',
+    ht: 'Choute'
   },
 
   leave: {
-    en: 'Leave party'
+    en: 'Leave party',
+    es: 'Fiesta de despedida',
+    de: 'Verlassene Partei',
+    fr: 'Fête de départ',
+    ht: 'Kite pati'
   },
 
   onInvite: {
-    en: '$1 has invited you to join their party.'
+    en: '$1 has invited you to join their party.',
+    es: '$1 te ha invitado a unirte a su fiesta.',
+    de: '$1 hat dich eingeladen, an ihrer Party teilzunehmen.',
+    fr: '$1 vous invite à vous joindre à leur fête.',
+    ht: '$ 1 envite ou nan pati yo.'
   }
 }
