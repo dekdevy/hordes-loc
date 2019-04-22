c5734dc6e517fefbeebadf7c18453d9f54af4e76
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-22 10:20:02 +0100

    Update link.js

--- random/link.js
+++ random/link.js
@@ -45,7 +45,8 @@ const adjectives = ['epic', 'rare', 'common', 'trashy', 'magical', 'thunderous',
   'relentless', 'callous', 'remorseless', 'shrinking', 'expanding', 'scruffy', 'suptuous', 'gorgeous', 'opulent', 'flourishing',
   'exaggerated', 'evasive', 'shifty', 'appealing', 'cheap', 'expensive', 'benevolent', 'liberal', 'virtuous', 'frustrating',
   'ruling', 'controlling', 'supreme', 'inferior', 'superior', 'flammable', 'practical', 'frenzied', 'bloodhungry', 'starving',
-  ',malnourished', 'frightening', 'alarming', 'startling', 'horrifying', 'menacing']
+  'malnourished', 'frightening', 'alarming', 'startling', 'horrifying', 'menacing', 'scary', 'dazzling', 'glaring', 'flagrant',
+  'luxurious', 'diamond', 'splendid', 'plush', 'magnificent', 'ragged']
 
 const nouns = ['sword', 'axe', 'potion', 'hammer', 'mace', 'coin', 'bag', 'archer', 'warrior', 'mage', 'unicorn',
   'deer', 'dog', 'cat', 'staff', 'bear', 'banana', 'apple', 'boots', 'armor', 'helmet', 'tower', 'forge', 'castle',
