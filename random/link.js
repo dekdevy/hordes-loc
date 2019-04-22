64d65b129a1c9648d35cb87a1830bbafb58dd4ca
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-22 18:37:32 +0100

    Update link.js

--- random/link.js
+++ random/link.js
@@ -48,7 +48,12 @@ const adjectives = ['epic', 'rare', 'common', 'trashy', 'magical', 'thunderous',
   'malnourished', 'frightening', 'alarming', 'startling', 'horrifying', 'menacing', 'scary', 'dazzling', 'glaring', 'flagrant',
   'luxurious', 'diamond', 'splendid', 'plush', 'magnificent', 'ragged', 'tatty', 'rundown', 'weedy', 'jagged', 'serrated',
   'rupturing', 'tearing', 'bursting', 'breaching', 'seperating', 'severing', 'sinister', 'dusty', 'wartorn', 'utopian',
-  'striking', 'loathing', 'officious', 'gilded', 'unbelievable']
+  'striking', 'loathing', 'officious', 'gilded', 'unbelievable', 'solemn', 'effective', 'eliminating', 'furry', 'fuzzy',
+  'fluffy', 'fizzy', 'glinting', 'glistening', 'glittery', 'gloating', 'joyless', 'depressing', 'powerful', 'incurable',
+  'indecipherable', 'indecorous', 'inappropiate', 'melodic', 'stlited', 'artificial', 'dangerous', 'awkward', 'awful',
+  'marauding', 'spectacular', 'beatific', 'beautiful', 'gullible', 'highbrow', 'lowbrow', 'hibernating', 'sleeping', 'hideous',
+  'humble', 'humid', 'itchy', 'jerky', 'jesting', 'jejune', 'jeeming', 'kidnapping', 'abducting', 'motionless', 'motley',
+  'grim', 'poised', 'rudimentary', 'emerald', 'eminent', 'famous', 'innocuous']
 
 const nouns = ['sword', 'axe', 'potion', 'hammer', 'mace', 'coin', 'bag', 'archer', 'warrior', 'mage', 'unicorn',
   'deer', 'dog', 'cat', 'staff', 'bear', 'banana', 'apple', 'boots', 'armor', 'helmet', 'tower', 'forge', 'castle',
