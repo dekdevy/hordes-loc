ecaca41f28603e8a5c01bb8fa0cb19123208efe9
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-22 10:13:27 +0100

    Update link.js

--- random/link.js
+++ random/link.js
@@ -39,7 +39,8 @@ const adjectives = ['epic', 'rare', 'common', 'trashy', 'magical', 'thunderous',
   'thieving', 'farcical', 'falsifying', 'azure', 'albino', 'alien', 'agitating', 'galant', 'glossy', 'globular', 'searing',
   'flaming', 'frosty', 'herculean', 'irksome', 'vulnerable', 'meritiorious', 'optimistic', 'pessemistic', 'possessive',
   'slapdash', 'slav', 'terrifying', 'tender', 'raw', 'weedy', 'ironic', 'fake', 'lavish', 'abandoned', 'emetic', 'mocking',
-  'surreal', 'arctic', 'poignant', 'stalking', 'empowering', 'coldforged', 'tattered']
+  'surreal', 'arctic', 'poignant', 'stalking', 'empowering', 'coldforged', 'tattered', 'predestined', 'enigmatic', 'wrathful',
+  'hyperdimensional', 'corrosive', 'bootleg', 'concilatory', 'benificial', 'atrocious', 'laughable', 'luscious', 'faceted']
 
 const nouns = ['sword', 'axe', 'potion', 'hammer', 'mace', 'coin', 'bag', 'archer', 'warrior', 'mage', 'unicorn',
   'deer', 'dog', 'cat', 'staff', 'bear', 'banana', 'apple', 'boots', 'armor', 'helmet', 'tower', 'forge', 'castle',
