75c7bdb596946ca159dc0d5ab63bae1095633f05
Cozmicc <49627827+Cozmicc@users.noreply.github.com>
2019-04-22 15:05:33 -0400

    Update orb.js
    Fixed Spanish Translations

--- loc/items/orb.js
+++ loc/items/orb.js
@@ -10,8 +10,7 @@ export default [
       es: 'Una bola de tierra húmeda, dejando pequeñas criaturas en tu mano.',
       hi: 'पृथ्वी के नम गेंद, अपने हाथ में छोटे critters अकेली रह गई।',
       ht: 'Mouye boul sou latè, kite dèyè yon ti ensèk nan men ou.',
-      it: 'Una palla di terra umida, lasciando piccole creature in mano.',
-      pt: 'Bola húmida de terra, deixando para trás pequenas criaturas na mão.'
+      it: 'Una palla di terra umida, lasciando piccole creature in mano.'
     }
   },
 
@@ -23,27 +22,25 @@ export default [
       de: 'Eine groteske Knochenmasse, die den Abergläubischen Glück bringt.',
       fr: 'Une masse osseuse grotesque qui porte chance aux superstitieux.',
       nl: 'Een groteske bottenmassa die geluk brengt aan de bijgelovigen.',
-      es: 'Una masa ósea grotesca que trae suerte a los supersticiosos.',
+      es: 'Se dice que la masa ósea grotesca trae suerte a los supersticiosos.',
       hi: 'हड्डी का एक विचित्र बड़े पैमाने पर अंधविश्वासी के लिए किस्मत लाने।',
       ht: 'Yon mas grotèsk nan zo ki pote chans sipèstisye.',
-      it: 'Una massa grottesca di ossa che porta fortuna ai superstiziosi.',
-      pt: 'Uma massa grotesca de ossos trazendo sorte aos supersticiosos.'
+      it: 'Una massa grottesca di ossa che porta fortuna ai superstiziosi.'
     }
   },
 
   {
     name       : {$$: 'Beehive' },
     description: {
-      en: 'Buzzing sticky hive, delivering a couple of stings every now and then.',
+      en: 'Una colmena pegajosa que zumba, que da unas cuantas picaduras de vez en cuando..',
       ru: 'Жужжащий липкий улей, периодически жалящий вас',
       de: 'Summender, klebriger Bienenstock, der ab und zu ein paar Stiche liefert.',
       fr: 'Une ruche bourdonnante et collante, avec quelques piqûres de temps en temps.',
       nl: 'Zoemende kleverige hicky hive, die af en toe een paar steken levert.',
-      es: 'Colmena pegajosa zumbando, dando un par de picaduras de vez en cuando.',
+      es: 'Una colmena pegajosa que zumba, pica varias veces de vez en cuando.',
       hi: 'चिपचिपा छत्ता गूंज, अब और फिर डंक के एक जोड़े दे रहे थे।',
       ht: 'Bee itikè ruch kolan, fournir yon koup la pokes kounye a ak lè sa a.',
-      it: 'Alveare appiccicoso e ronzante, con un paio di punture ogni tanto.',
-      pt: 'Uma colmeia pegajosa a zumbir, com duas picadas de vez em quando.'
+      it: 'Alveare appiccicoso e ronzante, con un paio di punture ogni tanto.'
     }
   },
 
@@ -58,8 +55,7 @@ export default [
       es: 'Después de una dura pelea mientras robabas un hueso de un perro, decidiste guardarlo como recuerdo de tus luchas.',
       hi: 'जबकि एक कुत्ते की एक हड्डी चोरी एक कठिन लड़ाई के बाद, आप अपने संघर्ष की याद दिलाने एक स्मारिका के रूप में इस रखने का फैसला किया।',
       ht: 'After a tough fight while robbing a bone of a dog, you decided to keep this as a souvenir reminding you of your struggles.',
-      it: 'Dopo una dura lotta mentre derubava un osso di un cane, avete deciso di tenere questo come ricordo che vi ricorda le vostre lotte.',
-      pt: 'Depois de uma dura luta enquanto roubava um osso de um cão, você decidiu guardar isso como uma lembrança lembrando-o de suas lutas.'
+      it: 'Dopo una dura lotta mentre derubava un osso di un cane, avete deciso di tenere questo come ricordo che vi ricorda le vostre lotte.'
     }
   },
 
@@ -71,11 +67,10 @@ export default [
       de: 'Überreste von vor ein paar Nächten haben einen ziemlichen Gestank verursacht.',
       fr: 'Les restes d\'il y a quelques nuits ont fait une sacrée puanteur.',
       nl: 'De restjes van een paar nachten geleden hebben een behoorlijke stank veroorzaakt.',
-      es: 'Las sobras de hace un par de noches han hecho un gran escándalo.',
+      es: 'Las sobras de un par de noches pasadas han hecho un olor horrible.',
       hi: 'रातों की एक जोड़ी से बचा पहले काफी बदबू बना दिया है।',
       ht: 'Rès manje ki sòti nan yon koup nan nwit sa pibliye depi yo te fè byen yon Stink.',
-      it: 'Gli avanzi di un paio di notti fa hanno fatto un bel po\' di puzza.',
-      pt: 'Os restos de algumas noites atrás fizeram um grande fedor.'
+      it: 'Gli avanzi di un paio di notti fa hanno fatto un bel po\' di puzza.'
     }
   },
 
@@ -90,8 +85,7 @@ export default [
       es: 'Pequeña bola de metal, brillante y lisa con algunas abolladuras en su superficie.',
       hi: 'छोटे धातु गेंद, चमकदार और इसकी सतह पर कुछ छात्रों के साथ चिकनी।',
       ht: 'Ti metal boul, klere ak lis ak kèk étidyan sou sifas li yo.',
-      it: 'Piccola sfera metallica, lucida e liscia con alcune ammaccature sulla sua superficie.',
-      pt: 'Pequena bola de metal, brilhante e lisa, com algumas amolgadelas na superfície.'
+      it: 'Piccola sfera metallica, lucida e liscia con alcune ammaccature sulla sua superficie.'
     }
   },
 
@@ -106,8 +100,7 @@ export default [
       es: 'Los rayos de luz quedan atrapados en esta esfera, lanzando finos arco iris a tu alrededor.',
       hi: 'प्रकाश की किरणें इस क्षेत्र में पकड़े, आप के आसपास ठीक इंद्रधनुष कास्टिंग।',
       ht: 'Reyon limyè kenbe nan esfè sa a, depoze bon lakansyèl bò kote ou.',
-      it: 'Raggi di luce si impigliano in questa sfera, gettando intorno a voi sottili arcobaleni.',
-      pt: 'Raios de luz são apanhados nesta esfera, lançando finos arco-íris à tua volta.'
+      it: 'Raggi di luce si impigliano in questa sfera, gettando intorno a voi sottili arcobaleni.'
     }
   },
 
@@ -122,8 +115,7 @@ export default [
       es: 'Los antiguos cíclopes que aterrorizaban estas tierras fueron finalmente asesinados, permitiéndole a usted recoger este sombrío recuerdo.',
       hi: 'इन जमीनों को आतंकित प्राचीन राक्षस अंत में मारा गया था, तो आप इस गंभीर स्मारिका साफ़-सफाई की अनुमति देता है।',
       ht: 'Ansyen siklop yo teworize tè sa yo finalman te tiye, sa ki te pèmèt ou tire souvni souvni sa a.',
-      it: 'Gli antichi ciclopi che terrorizzavano queste terre sono stati finalmente uccisi, permettendovi di cercare questo brutto souvenir.',
-      pt: 'Os antigos ciclopes que aterrorizavam estas terras foram finalmente mortos, permitindo que você vasculhasse esta lembrança sombria.'
+      it: 'Gli antichi ciclopi che terrorizzavano queste terre sono stati finalmente uccisi, permettendovi di cercare questo brutto souvenir.'
     }
   },
 
@@ -138,8 +130,7 @@ export default [
       es: 'Algunos magos usan bastones, algunos libros, decidiste usar un montón de nieve congelada.',
       hi: 'कुछ जादूगरों कर्मचारी, कुछ किताबों का उपयोग, आप बर्फ की एक जमे हुए पुंज का उपयोग करने का फैसला किया।',
       ht: 'Gen kèk maj sèvi ak baton, kèk liv, ou deside sèvi ak yon touf nan frizè nan nèj.',
-      it: 'Alcuni maghi usano bastoni, alcuni libri, tu hai deciso di usare un ciuffo di neve ghiacciata.',
-      pt: 'Alguns magos usam cajados, alguns livros, você decidiu usar um monte de neve congelada.'
+      it: 'Alcuni maghi usano bastoni, alcuni libri, tu hai deciso di usare un ciuffo di neve ghiacciata.'
     }
   },
 
@@ -154,8 +145,7 @@ export default [
       es: 'Brillante y encantadora, esta misteriosa esfera hace que el aire que la rodea se enfríe.',
       hi: 'चमकदार और आकर्षक, इस रहस्यमय क्षेत्र के चारों ओर हवा ठंडा बना देता है।',
       ht: 'Klere ak bon, esfè sa a misterye fè lè a bò kote l \'glas-frèt.',
-      it: 'Lustrosa e affascinante, questa misteriosa sfera rende l\'aria intorno ad essa gelida.',
-      pt: 'Lustrosa e encantadora, esta esfera misteriosa faz com que o ar à sua volta fique gelado.'
+      it: 'Lustrosa e affascinante, questa misteriosa sfera rende l\'aria intorno ad essa gelida.'
     }
   },
 
@@ -170,8 +160,7 @@ export default [
       es: 'Enigmática esfera verde esmeralda, envuelta en finos grabados de profecías serpenteantes, atrayendo fama y serendipia a su ingenuo portador.',
       hi: 'रहस्यपूर्ण पन्ना हरी क्षेत्र, सर्पिल भविष्यवाणी, अपने अनुभवहीन वाहक करने के लिए प्रसिद्धि और नसीब आकर्षक के ठीक नक्काशी में छा।',
       ht: 'Enigm sifas vèt emwòd, anvlòpki nan grave amann nan pèrfid pwofesi, atiran t\'ap nonmen non ak serendipity pote l \'naif.',
-      it: 'Enigmatica sfera verde smeraldo, avvolta da raffinate incisioni di serpentine profezie, attirando fama e serendipità al suo ingenuo portatore.',
-      pt: 'Esfera verde esmeralda enigmática, envolvida em gravuras finas de profecias serpentinas, atraindo fama e serendipidade ao seu portador ingênuo.'
+      it: 'Enigmatica sfera verde smeraldo, avvolta da raffinate incisioni di serpentine profezie, attirando fama e serendipità al suo ingenuo portatore.'
     }
   },
 
@@ -186,8 +175,7 @@ export default [
       es: 'Órbita teñida de amarillo transparente, revelando plumas infernales atrapadas de aura paranormal, envolviendo las mentes de los débiles con planes de veneno y asesinato.',
       hi: 'पारदर्शी पीले ओर्ब, असाधारण चमक के राक्षसी पंखों फंस प्रकट करने, जहर और हत्या की योजना के साथ कमजोर के मन छा।',
       ht: 'Transparan jòn tente orb, revele plume bloke dyabolik nan aura paranormal, anglouti lespri yo nan fèb la ak plan nan pwazon ak touye moun.',
-      it: 'Globo giallo trasparente tinto di giallo, rivelando pennacchi infernali intrappolati di aura paranormale, inghiottendo le menti dei deboli con piani di veleno e omicidio.',
-      pt: 'Orb transparente amarelo tingido, revelando plumas infernais aprisionadas de aura paranormal, envolvendo as mentes dos fracos com planos de veneno e assassinato.'
+      it: 'Globo giallo trasparente tinto di giallo, rivelando pennacchi infernali intrappolati di aura paranormale, inghiottendo le menti dei deboli con piani di veleno e omicidio.'
     }
   },
 
@@ -202,8 +190,7 @@ export default [
       es: 'Artefacto de color azul pálido con facetas, que irradia pulsantes ondas de luz, desterrando las sombras oscuras de las almas demoníacas de tu camino hacia la gloria.',
       hi: 'पीला नीला आयामी विरूपण साक्ष्य, प्रकाश की तरंगों का उत्सर्जन, गौरव को अपने पथ से राक्षसी आत्माओं के काले साये को नष्ट करने।',
       ht: 'Pale ble fatra asosye, gaye vag enpilsyon nan limyè, banan lonbraj yo fè nwa nan nanm dyabolik soti nan chemen ou nan tout bèl pouvwa.',
-      it: 'Artefatto sfaccettato blu pallido, che irradia onde pulsanti di luce, allontanando le ombre scure delle anime demoniache dal tuo cammino verso la gloria.',
-      pt: 'Artefato facetado azul pálido, irradiando ondas pulsantes de luz, banindo as sombras escuras das almas demoníacas do seu caminho para a glória. '
+      it: 'Artefatto sfaccettato blu pallido, che irradia onde pulsanti di luce, allontanando le ombre scure delle anime demoniache dal tuo cammino verso la gloria.'
     }
   },
 
@@ -218,8 +205,7 @@ export default [
       es: 'La carne y la sangre infundieron en este orbe las almas de los muertos, creando una esencia roja oscura incrustada en sí misma, otorgándote un deseo sin precedentes de sangre y batalla.',
       hi: 'मांस और रक्त मारे हुए की आत्माओं के साथ इस ओर्ब संचार, एक गहरे लाल सार अपने आप में एम्बेडेड बनाते समय, आप रक्त और लड़ाई के लिए अभूतपूर्व वासना देने।',
       ht: 'Kò ak san enfuze orb sa a ak nanm yo nan touye yo, kreye yon sans wouj fonse entegre nan tèt li, ki akòde ou lanvi san parèy pou san ak batay.',
-      it: 'La carne e il sangue hanno infuso questo globo con le anime degli uccisi, creando un\'essenza rosso scuro incastonata in se stessa, che ti garantisce una lussuria senza precedenti per il sangue e la battaglia.',
-      pt: 'Carne e sangue infundiram este globo com as almas dos mortos, criando uma essência vermelha escura embutida em si mesma, concedendo a você uma luxúria sem precedentes por sangue e batalha.'
+      it: 'La carne e il sangue hanno infuso questo globo con le anime degli uccisi, creando un\'essenza rosso scuro incastonata in se stessa, che ti garantisce una lussuria senza precedenti per il sangue e la battaglia.'
     }
   },
 
@@ -231,11 +217,10 @@ export default [
       de: 'Dunkel, tödlich und dämonisch, eine Kugel, die von kleinen Kratern voller blutroter Edelsteine umgeben ist, die Schreckensblätter in diejenigen einweben, die sich dir widersetzen.',
       fr: 'Sombre, mortel et démoniaque, un globe encastré dans de petits cratères remplis de pierres précieuses rouge sang, tissant des feuilles de terreur en ceux qui vous opposent.',
       nl: 'Donker, dodelijk en duivels, een orb, omgeven door kleine kraters gevuld met bloedrode edelstenen, die met schrik weven vellen van terreur in degenen die zich tegen je verzetten.',
-      es: 'Oscura, mortal y demoníaca, un orbe encajonado en pequeños cráteres llenos de gemas rojas de sangre, entretejiendo hojas de terror en aquellos que se oponen a ti.',
+      es: 'Oscura, mortífera y demoníaca, un orbe encajonado en pequeños cráteres llenos de gemas rojas de sangre, tejiendo superficie de terror en aquellos que se oponen a ti.',
       hi: 'डार्क घातक और राक्षसी, एक गोला छोटे जो लोग आप का विरोध में खून से लाल रत्न, आतंक की बुनाई चादरों से भरा खड्ड में छा।',
       ht: 'Nwa, mòtèl ak dyabolik, yon orb antoure nan kratèr ti plen ak Meservey san-wouj, resi dra sou pè nan moun ki opoze ou.',
-      it: 'Oscuro, mortale e demoniaco, un globo racchiuso in piccoli crateri pieni di gemme rosso sangue, intrecciando fogli di terrore in coloro che vi si oppongono.',
-      pt: 'Escuro, mortal e demoníaco, um globo envolto em pequenas crateras cheias de gemas vermelhas de sangue, tecendo folhas de terror naqueles que se opõem a ti.'
+      it: 'Oscuro, mortale e demoniaco, un globo racchiuso in piccoli crateri pieni di gemme rosso sangue, intrecciando fogli di terrore in coloro che vi si oppongono.'
     }
   }
 
