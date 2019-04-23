3115c71e6f05e57379ad0778a3a9b773104fa8b7
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-23 20:39:03 +0100

    Update ring.js

--- loc/items/ring.js
+++ loc/items/ring.js
@@ -12,7 +12,8 @@ export default [
       nl: 'Kijk er niet naar, anders valt het uit elkaar.',
       pl: 'Nie patrz na to, bo się rozpadnie.',
       it: 'Non guardarlo o cadrà a pezzi.',
-      cs: 'Nedívej se na to, nebo se rozpadne.'
+      cs: 'Nedívej se na to, nebo se rozpadne.',
+      ja: 'それを見てはいけないか、それはバラバラになります。'
     }
   },
 
@@ -29,7 +30,8 @@ export default [
       nl: 'Niet zeker van de schors, maar het is zeker van ijzer.',
       pl: 'Nie jestem pewien co do kory, ale na pewno jest zrobiona z żelaza.',
       it: 'Non sono sicuro della corteccia, ma è sicuramente di ferro.',
-      cs: 'Nejste si jisti o kůře, ale rozhodně ze železa.'
+      cs: 'Nejste si jisti o kůře, ale rozhodně ze železa.',
+      ja: '樹皮についてはわからないが、それは確かに鉄でできている。'
     }
   },
 
@@ -46,7 +48,8 @@ export default [
       nl: 'Eindelijk, een ring die glanst.',
       pl: 'W końcu pierścionek, który jest błyszczący.',
       it: 'Finalmente, un anello che è lucido.',
-      cs: 'Konečně, prsten, který je lesklý.'
+      cs: 'Konečně, prsten, který je lesklý.',
+      ja: 'つやのあるリング。'
     }
   },
 
@@ -63,7 +66,8 @@ export default [
       nl: 'Een ring gemaakt van de restjes van gisteravond.',
       pl: 'Pierścionek wykonany z resztek z ostatniej nocy.',
       it: 'Un anello fatto con gli avanzi di ieri sera.',
-      cs: 'Prsten ze zbytků minulé noci.'
+      cs: 'Prsten ze zbytků minulé noci.',
+      ja: '最後の夜の残り物で作られた指輪。'
     }
   },
 
@@ -80,7 +84,8 @@ export default [
       nl: 'De elfen maakten deze ring om zich te beschermen tegen gevaar en slaagden erin deze weg te geven.',
       pl: 'Elfy stworzyły ten pierścień, aby chronić się przed niebezpieczeństwem i udało im się go oddać.',
       it: 'Gli elfi fecero questo anello per proteggersi dal pericolo e riuscirono a regalarlo.',
-      cs: 'Elfové tento prsten vyzbrojili, aby se chránili před nebezpečím a podařilo se mu to vzdát.'
+      cs: 'Elfové tento prsten vyzbrojili, aby se chránili před nebezpečím a podařilo se mu to vzdát.',
+      ja: 'エルフは危険から身を守るためにこの指輪を作り、それを譲ることに成功した。'
     }
   },
 
@@ -97,7 +102,8 @@ export default [
       nl: 'Past mooi, maar de effecten zijn marginaal.',
       pl: 'Pasuje ładnie, chociaż efekty, jakie daje, są marginalne.',
       it: 'Si adatta bene, anche se gli effetti che dà sono marginali.',
-      cs: 'Dobře padne, i když účinky, které dává, jsou okrajové.'
+      cs: 'Dobře padne, i když účinky, které dává, jsou okrajové.',
+      ja: 'それが与える効果はわずかですが、うまく収まります。'
     }
   },
 
@@ -114,7 +120,8 @@ export default [
       nl: 'Niemand weet of het de moeite waard is om aan te trekken.',
       pl: 'Nikt nie wie, czy warto to założyć.',
       it: 'Nessuno sa se vale la pena di indossarlo.',
-      cs: 'Nikdo neví, jestli to stojí za to.'
+      cs: 'Nikdo neví, jestli to stojí za to.',
+      ja: 'それを着る価値があるかどうか誰も知りません。'
     }
   },
 
@@ -131,7 +138,8 @@ export default [
       nl: 'Smaragden aan alle kanten om je rijk te laten voelen.',
       pl: 'Szmaragdy po wszystkich stronach, żebyś poczuł się bogaty.',
       it: 'Nessuno sa se vale la pena di indossarlo.',
-      cs: 'Smaragdy ze všech stran, abys se cítil bohatý.'
+      cs: 'Smaragdy ze všech stran, abys se cítil bohatý.',
+      ja: 'あなたを豊かな気分にさせるためのあらゆる面のエメラルド。'
     }
   },
 
@@ -148,7 +156,8 @@ export default [
       nl: 'Als het kon spreken, zou het waarschijnlijk klagen over het feit dat het driehoekig is.',
       pl: 'Gdyby mógł mówić, prawdopodobnie skarżyłby się na bycie trójkątnym.',
       it: 'Se potesse parlare, probabilmente si lamenterebbe di essere triangolare.',
-      cs: 'Kdyby mohla mluvit, pravděpodobně by si stěžovala na to, že je trojúhelníková.'
+      cs: 'Kdyby mohla mluvit, pravděpodobně by si stěžovala na to, že je trojúhelníková.',
+      ja: 'それが話すことができればそれはおそらく三角形であることについて不平を言うでしょう。'
     }
   },
 
@@ -165,7 +174,8 @@ export default [
       nl: 'Er bestaan geen schriftelijke gegevens om ons te vertellen over de oorsprong ervan...',
       pl: 'Nie ma żadnych pisemnych zapisów, które powiedziałyby nam o jego pochodzeniu...',
       it: 'Non esistono documenti scritti che ci dicono la sua origine...',
-      cs: 'Neexistují žádné písemné záznamy, které by nám sdělily svůj původ ...'
+      cs: 'Neexistují žádné písemné záznamy, které by nám sdělily svůj původ ...',
+      ja: 'その起源について私たちに伝えるための書面による記録はありません。'
     }
   },
 
@@ -182,7 +192,8 @@ export default [
       nl: 'Heilig artefact verloren door heilige druïden.',
       pl: 'Święty artefakt utracony przez świętych druidów.',
       it: 'Artefatto sacro perso dai santi druidi.',
-      cs: 'Posvátný artefakt ztracený svatými druidy.'
+      cs: 'Posvátný artefakt ztracený svatými druidy.',
+      ja: '聖ドルイドによって失われた神聖なアーティファクト。'
     }
   },
 
@@ -199,7 +210,8 @@ export default [
       nl: 'Bewaarder van de vrede....niet zeker, maar het kan je in leven houden...',
       pl: 'Strażnik spokoju...nie jestem tego pewien, ale może to utrzyma cię przy życiu...',
       it: 'Custode di pace...non ne sono sicuro, ma potrebbe tenervi in vita...',
-      cs: 'Strážce míru ... není si tím jistý, ale může vás udržet naživu.'
+      cs: 'Strážce míru ... není si tím jistý, ale může vás udržet naživu.',
+      ja: '平和の番人....それについてはよくわからないが、それはあなたを生かし続けるかもしれない……'
     }
   }
 
