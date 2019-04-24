feb0c6aeee7c6450adf0aa2d295ac6a9f454a037
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-24 13:47:29 +0100

    Update talisman.js

--- loc/items/talisman.js
+++ loc/items/talisman.js
@@ -147,7 +147,8 @@ export default [
       pl: 'Zaskakujšco, nie pachnie jak ryba. Po prostu nie pokazuj jej słodkiej wody.',
       nl: 'Verrassend genoeg ruikt het niet naar vis. Laat het gewoon niet zoetwater zien.',
       cs: 'Překvapivě to necítí jako ryby. Jen to neukazuj sladkovodní.',
-      ja: '驚いたことにそれは魚のような匂いはしません。淡水を見せないでください。'
+      ja: '驚いたことにそれは魚のような匂いはしません。淡水を見せないでください。',
+      ht: 'Surprenante, li pa pran sant tankou pwason. Jis pa montre li dlo dous.'
     }
   },
 
