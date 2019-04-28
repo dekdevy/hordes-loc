e30a671fb26bd95ef2e8114d051edc5ee04298ed
dek <polydek@googlemail.com>
2019-04-28 18:23:47 +0200

    added some more stat comments

--- loc/ui/stats.js
+++ loc/ui/stats.js
@@ -1,12 +1,4 @@
-// STAT_HP = 0
-// STAT_MP = 1
-// STAT_HP_REG5 = 2
-// STAT_MP_REG5 = 3
-// STAT_DMG_MIN = 4
-// STAT_DMG_MAX = 5
-
 // Dont translate any of these for now, i need to figure out best lengths first
-
 export default {
 
   misc: {
@@ -41,3 +33,23 @@ export default {
     }
   ]
 }
+
+// _STAT_HP = 0
+// _STAT_MP = 1
+// _STAT_HP_REG5 = 2
+// _STAT_MP_REG5 = 3
+// _STAT_DMG_MIN = 4
+// _STAT_DMG_MAX = 6
+// _STAT_DEFENSE = 7
+// _STAT_BLOCK = 8
+// _STAT_EVADE = 9
+// _STAT_ACCURACY = 10
+// _STAT_CRITICAL = 11
+// _STAT_MOVESPEED = 12
+// _STAT_HASTE = 13
+// _STAT_DEXTERITY = 14
+// _STAT_STRENGTH = 15
+// _STAT_INTELLIGENCE = 16
+// _STAT_WISDOM = 17
+// _STAT_STAMINA = 18
+// _STAT_LUCK = 19
