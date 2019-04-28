6958bc58a652c1d0f9df69662a80a8c80faba225
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-04-28 08:16:23 +0100

    Update misc.js

--- loc/misc.js
+++ loc/misc.js
@@ -13,5 +13,206 @@ export default [
     description: {
       en: 'An apple a day keeps the monsters away....is that what they say?'
     }
+  },
+
+  {
+    name: {$$: 'Ogre\'s Eye' },
+
+    description: {
+      en: 'Sticky grizzly eyeball smelling of swampland.'
+    }
+  },
+
+  {
+    name: {$$: 'Broken Crown' },
+
+    description: {
+      en: 'A mighty bejewelled crown belonging to the ancient kings of these lands.'
+    }
+  },
+
+  {
+    name: {$$: 'Wispy Reagent' },
+
+    description: {
+      en: 'Deeo dark purple wisp taken from the cursed soul of a malevolent phantom.'
+    }
+  },
+
+
+  {
+    name: {$$: 'Iron Spearhead' },
+
+    description: {
+      en: 'Serrated and rusty, you wonder if it's any good as a weapon.'
+    }
+  },
+
+  {
+    name: {$$: 'Blood Ruby' },
+
+    description: {
+      en: 'Dsrk red mesmerising stone leaving you in a bloodhungry trance should you stare too deeply into its grim core.'
+    }
+  },
+
+  {
+    name: {$$: 'Goose Feather' },
+
+    description: {
+      en: 'Beware of the feral goose. It doesn't like pokes.'
+    }
+  },
+
+  {
+    name: {$$: 'Elegant Bowstring' },
+
+    description: {
+      en: 'Powerful silky string worth more than all your gear.'
+    }
+  },
+
+  {
+    name: {$$: 'Emerald' },
+
+    description: {
+      en: 'Lustrous green stone, might but you a few beers.'
+    }
+  },
+
+  {
+    name: {$$: 'Wolf Fang' },
+
+    description: {
+      en: 'Sharp and white... you cut yourself already.'
+    }
+  },
+
+  {
+    name: {$$: 'Sticky Slime' },
+
+    description: {
+      en: 'Gooey slop, taken from God knows where.'
+    }
+  },
+
+  {
+    name: {$$: 'Golden Leaf' },
+
+    description: {
+      en: 'Finely engraved leaf of gold representing high royalty.'
+    }
+  },
+
+  {
+    name: {$$: 'Rabbit\'s Foot' },
+
+    description: {
+      en: 'The clsssic ingredient to a witch\'s broth.'
+    }
+  },
+
+  {
+    name: {$$: 'Chewed Bone' },
+
+    description: {
+      en: 'Splinters of bone and marrow glued together with stale saliva.'
+    }
+  },
+
+  {
+    name: {$$: 'Wooden Bowl' },
+
+    description: {
+      en: 'Small chipped stew bowl. Careful, it's flammable.'
+    }
+  },
+
+  {
+    name: {$$: 'Golden Eyepiece' },
+
+    description: {
+      en: 'Mages always want to seem wise, perhaps they too made this mystical and elegant eyepiece.'
+    }
+  },
+
+  {
+    name: {$$: 'Cactus Spike' },
+
+    description: {
+      en: 'Ouch, sharp.'
+    }
+  },
+
+  {
+    name: {$$: 'Fine Sand' },
+
+    description: {
+      en: 'A small pile of extremely fine sand. Where do you out this...'
+    }
+  },
+
+  {
+    name: {$$: 'Lich Hair' },
+
+    description: {
+      en: 'A tuft of putrid matted hair. T\'was a tough battle to get hold of this '
+    }
+  },
+
+  {
+    name: {$$: ' Pulpous Maggot' },
+
+    description: {
+      en: 'Disgusting larvae of an unknown insect. Hopefully a small one...'
+    }
+  },
+
+  {
+    name: {$$: 'Obsidian Shard' },
+
+    description: {
+      en: 'Strange triangular fragement of obsidian. Perhaps it's part of a larger structure...'
+    }
+  },
+
+  {
+    name: {$$: 'Frozen Teardrop' },
+
+    description: {
+      en: 'Mesmerising crystalline droplet refracting light intricately casting tiny balls of light onto you as you hold it up to the sun.'
+    }
+  },
+
+  {
+    name: {$$: 'Darkmetal Arrowhead' },
+
+    description: {
+      en: 'Sharo gothic arrowhead, made from a mystical metal which seeps dear into your bones.'
+    }
+  },
+  {
+    name: {$$: 'Gunpowder' },
+
+    description: {
+      en: 'Throw some into a campfire and you're in for a nasty explosive surprise.'
+    }
+  },
+
+  {
+    name: {$$: 'Kindling' },
+
+    description: {
+      en: 'Thin dry twigs, will burn nicely in a campfire.'
+    }
+  },
+
+  {
+    name: {$$: 'Pale Snakeskin' },
+
+    description: {
+      en: 'The shed leathery skin from an albino python slithering its way through the forests of these lands.'
+    }
   }
+
 ]
