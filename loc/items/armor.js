1503250fb5fa84ddfc85ef297dd3d93532133184
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-07 17:24:25 +0100

    Update armor.js

--- loc/items/armor.js
+++ loc/items/armor.js
@@ -29,7 +29,7 @@ export default [
     name       : {$$: 'Faded Garment' },
     description: {
       en: 'Leather shirt, faded from wear and tear.',
-      ru: 'Кожаная рубашка, порванная и выцветшая из-за износа.',
+      ru: 'Кожаная рубашка, выцветшая из-за износа.',
       es: 'Camisa de cuero, desteñida por el desgaste.',
       de: 'Lederhemd, verblasst durch Abnutzung.',
       fr: 'Chemise en cuir, décolorée par l\'usure.',
