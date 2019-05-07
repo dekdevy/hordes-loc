64c567b0ff682bb43823a04c43c57525d50cb1f0
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-07 17:01:20 +0100

    Update conjurer.js

--- loc/npcs/conjurer.js
+++ loc/npcs/conjurer.js
@@ -2,7 +2,7 @@ export default {
   interactions: [{
     text: {
       en: 'Ah, I see you traveler. Weary and famished you look. We conjurers, too, roam these fine lands. Would you like me to take you to one?',
-      ru: 'А, вижу тебя путешественник.Уставшим и голодным выглядишь ты. Мы, волшебники, тоже бродим по этим прекрасным землям. Хочешь, я отведу тебя к одному из нас?',
+      ru: 'А, вижу тебя путешественник. Ты выглядишь уставшим и голодным. Мы, волшебники, тоже путешествуем по этим прекрасным землям. Хочешь, я отведу тебя к одному из нас?',
       fr: 'Bonjour, je peux faire apparaître un portail vers les autres conjurers. Où voudriez-vous aller?',
       hi: 'आह, मैं तुम्हें यात्री देखते हैं। थके हुए और भूखा तुम देखो। हम conjurers, भी, इन ठीक भूमि घूमने। तुम मुझे एक के पास ले करना चाहते हैं?',
       es: 'Ah, te veo viajero. Pareces cansado y hambriento. Nosotros, los hechiceros, también, deambulamos por estas bellas tierras. ¿Quieres que te lleve a uno?',
