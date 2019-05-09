6602b77f5f1a1c1c2945749dc0dc7407f50bcf7f
Ramyx353 <37324815+Ramyx353@users.noreply.github.com>
2019-05-09 18:29:36 +0300

    Update auctioneer.js
    Added Romanian

--- loc/npcs/auctioneer.js
+++ loc/npcs/auctioneer.js
@@ -4,7 +4,8 @@ export default {
       en: 'Auctioneer greeting text'
     },
     choices: [{
-      en: 'Yes, show me the auctions for sale.'
+      en: 'Yes, show me the auctions for sale.',
+      ro: 'Da, arată-mi ce lucruri sunt de vânzare.'
     }]
   }]
 }
