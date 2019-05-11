85a3ba42b4da01f147eeeb4174d16abb594c2e37
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-11 18:00:15 +0100

    Update auctioneer.js

--- loc/npcs/auctioneer.js
+++ loc/npcs/auctioneer.js
@@ -12,7 +12,12 @@ export default {
       pl: 'Tak, pokaż mi aukcje na sprzedaż.',
       es: 'Sí, muéstrame las subastas en venta.',
       nl: 'Ja, laat me de veilingen zien die te koop zijn',
-      fr: 'Oui, montrez-moi les enchères à vendre.'
+      fr: 'Oui, montrez-moi les enchères à vendre.',
+      pl: 'Tak, pokaż mi aukcje na sprzedaż.',
+      ru: 'Да, покажите мне аукционы на продажу.',
+      el: 'Ναι, δείξτε μου τις δημοπρασίες προς πώληση.',
+      tl: 'Oo, ipakita sa akin ang mga auction para mabili.',
+      de: 'Ja, zeigen Sie mir die zu verkaufenden Auktionen.'
     }]
   }]
 }
