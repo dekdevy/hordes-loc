600432e5bd62f76643d76d0f26059cdd682116ae
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-12 20:05:52 +0100

    Add files via upload

---
+++ loc/items/quiver.js
@@ -0,0 +1,144 @@
+export default [
+  {
+    name       : {$$: 'Linen Quiver' },
+    description: {
+      en: 'Soft, full of holes. Arrows keep falling out'
+      ru: 'Мягкий, дырявый. Стрелы постоянно выпадают.',
+      de: 'Weich, voller Löcher. Pfeile fallen immer wieder heraus.',
+      es: 'Si, los calcetines también pueden ser una aljaba.',
+      fr: 'Un carquois en linge dont les flèches n\'arrêtent pas de tomber.',
+      it: 'Morbido, pieno di buchi. Le frecce continuano a cadere.',
+      pt: 'Suave, cheio de buracos. As setas continuam caindo.',
+      tl: 'Malambot na puno ng mga butas. Ang mga arrow ay patuloy na bumagsak.',
+      bg: 'Мека, пълна с дупки. Стрелките продължават да падат.',
+      el: 'Μαλακό γεμάτο τρύπες. Τα βέλη συνεχίζουν να πέφτουν.',
+      hi: 'मुलायम और गद्दों से भारी हुई। सारे तीर गिरते रहते है।',
+      nl: 'Zacht, vol gaten. Pijlen blijven uitvallen.',
+      pl: 'Miękka, pełna dziur. Strzałki wciąż wypadają.',
+      ht: 'Mou, plen twou. Flèch yo kontinye tonbe.',
+      cs: 'Měkká, plná děr. Šipky stále vypadávají.',
+      ja: '柔らかい穴だらけ。矢印は抜け続けます。',
+      sv: 'Mjuk, full av hål. Pilarna fortsätter att falla ut.',
+      ko: '부드러운, 구멍 가득. 화살표는 계속 떨어지고 있습니다.',
+      ro: 'Moale, plină de găuri. Săgețile continuă să cadă.'  
+    }
+  },
+
+  {
+    name       : {$$: 'Silken Pouch' },
+    description: {
+      en: ' Soft and smooth to the touch, yet sturdy enough for your arrows.'
+    }
+  },
+
+  {
+    name       : {$$: 'Bone Rod' },
+    description: {
+      en: 'Hollowed out bone, serving well as a makeshift quiver.'
+    }
+  },
+
+  {
+    name       : {$$: 'Reinforced Quiver' },
+    description: {
+      en: 'Linen sack covered in stiff leather.'
+      ru: 'Льняной мешочек обтянутый жесткой кожей.',
+      de: 'Leinensack mit steifem Leder bezogen.',
+      es: 'Una bolsa de lana con cuero.',
+      fr: 'Sac en lin recouvert de cuir rigide.',
+      tl: 'Linyang lino na sakop sa matigas na katad.',
+      bg: 'Спално бельо, покрито с твърда кожа.',
+      el: 'Σάκος από λινάρι που καλύπτεται από σκληρό δέρμα.',
+      it: 'Sacco in lino rivestito in pelle rigida.',
+      pt: 'Saco de linho coberto de couro rígido.',
+      hi: 'सख़्त चमड़े से ढकी लिनन की पोती।',
+      nl: 'Linnenzak bedekt met stijf leer.',
+      pl: 'Lniany worek pokryty sztywną skórą.',
+      ht: 'Sak twal fin blan kouvri nan kwi rèd.',
+      cs: 'Plátěný pytel pokrytý tuhou kůží.',
+      ja: 'リネンの袋は硬い革で覆われています。',
+      sv: 'Linnesäck täckt av styvt läder.',
+      ko: '리넨 자루는 뻣뻣한 가죽으로 덮여 있습니다.',
+      ro: 'Sac din in acoperit cu piele rigidă.'    
+    }
+  },
+
+  {
+    name       : {$$: 'Mithril Weavings' },
+    description: {
+      en: 'Finely crafted quiver made from a mysterious shimmering material. '
+    }
+  },
+
+  {
+    name       : {$$: 'Snake Quiver' },
+    description: {
+      en: 'Every time you pull an arrow out, it hisses at you.'
+      ru: 'Каждый раз, когда вы вытаскиваете стрелу, она шипит на вас.',
+      de: 'Jedes Mal, wenn du einen Pfeil herausziehst, zischt er dich an.',
+      es: '¡Serpientes! ¿¡Por qué tenía que ser serpientes!?',
+      tl: 'Sa bawat oras na hilahin mo ang isang arrow out ito heses sa iyo.',
+      bg: 'Всеки път, когато издърпате стрела, тя изсъхва.',
+      el: 'Κάθε φορά που τραβάς ένα βέλος, σου σου σου λέει.',
+      fr: 'Un carquois fait en peau de serpent, pouvant supporter les plus puissantes flèches.',
+      it: 'Ogni volta che tiri fuori una freccia, ti sibila addosso.',
+      pt: 'Sempre que puxas uma flecha para fora, ela sibila para ti.',
+      hi: 'तीर निकालने पर साँप की तरह फूफकारती है।',
+      nl: 'Elke keer als je een pijl eruit trekt, sist het naar je.',
+      pl: 'Za każdym razem, gdy wyciągasz strzałę, syczysz na siebie.',
+      ht: 'Chak fwa ou rale yon flèch soti, li sifle nan ou.',
+      cs: 'Pokaždé, když vytáhnete šipku, zasyče na vás.',
+      ja: 'あなたが矢を引き出すたびにそれはあなたに向かっている。',
+      sv: 'Varje gång du drar en pil ut hissar den på dig.',
+      ko: '당신이 화살을 꺼낼 때마다, 그것은 당신에게 겁을줍니다.',
+      ro: 'De fiecare dată când scoți o săgeată, te sâsâie.'    
+    }
+  },
+
+  {
+    name       : {$$: 'Lotharien' },
+    description: {
+      en: 'Elven quiver coated in shimmering diamond.'
+      ru: 'Эльфийский колчан, покрытый мерцающим бриллиантом.',
+      de: 'Elfenköcher, beschichtet mit schimmerndem Diamanten.',
+      es: 'Un carcaj cubrierto en diamante creado por los duendes.',
+      fr: 'Carquois elfique décoré de diamants brillants.',
+      tl: 'Si Elven quiver pinahiran sa shimmering diamond.',
+      bg: 'Елфическия колчан, покрит с блестящ диамант.',
+      el: 'Ελβετικό τρίχας επικαλυμμένο με λαμπερό διαμάντι.',
+      it: 'Faretra elfica rivestita di diamante luccicante.',
+      pt: 'Aljava elfica revestida com diamante cintilante.',
+      hi: 'एल्वेन पोटली, हीरों से जड़ी।',
+      nl: 'Elven koker gecoat in glinsterende diamant.',
+      pl: 'Elficki kołczan pokryty błyszczącym diamentem.',
+      ht: 'Flè Elven kouvwi ak dyaman chatwayant.',
+      cs: 'Elven potápěč potažený třpytivým diamantem.',
+      ja: 'きらめくダイヤモンドでコーティングされたエルフの矢筒。',
+      sv: 'Koger av älvor belagda i skimrande diamant.',
+      ko: '엘프 떨림이 반짝이는 다이아몬드로 코팅되었습니다.',
+      ro: 'Tolbă a elfilor acoperită cu diamante sclipitoare.'    
+    }
+  },
+
+  {
+    name       : {$$: 'Cataclysm' },
+    description: {
+      en: 'A spectacular emerald green quiver filling your foes with guilt as they prepare to attack you.'
+    }
+  },
+
+  {
+    name       : {$$: 'Cain' },
+    description: {
+      en: 'A treacherous black quiver which often lets hold of your arrows in the most dire of moments.'
+    }
+  },
+
+  {
+    name       : {$$: 'Nefarious' },
+    description: {
+      en: 'A dark quiver coated in shimmering deep navy throws you into a chasm of peace as you pull out another cherished arrow...'
+    }
+  }
+
+]
