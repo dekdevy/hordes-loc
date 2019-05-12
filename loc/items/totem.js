2f3463e0360d765916b0cc9981c6db4063872ee8
FarqAndraith <49627616+FarqAndraith@users.noreply.github.com>
2019-05-12 17:03:33 -0500

    Update totem.js
    Added Polish

--- loc/items/totem.js
+++ loc/items/totem.js
@@ -5,7 +5,8 @@ export default [
       en: 'A wad of herbs tied together by a thin piece of yarn.',
       ro: 'O mână de iarbă legată împreună cu un fir subțire de ață',
       pt: 'Um maço de ervas atadas por um fino pedaço de fio.',
-      it: 'Un batuffolo di erbe legate insieme da un sottile pezzo di filato.'
+      it: 'Un batuffolo di erbe legate insieme da un sottile pezzo di filato.',
+      pl: 'Zwitek ziół związany cienkim kawałkiem przędzy.'
     }
   },
 
@@ -15,7 +16,8 @@ export default [
       en: 'Tangles thin twigs scratch your hands as you carry it around.',
       ro: 'Crenguțe subțiri încurcate te zgârie pe mâini în timp ce le cari cu tine.',
       pt: 'Os galhos finos coçam as mãos enquanto a carregas.',
-      it: 'Tangoli sottili ramoscelli sottili si graffiano le mani mentre lo portate in giro.'
+      it: 'Tangoli sottili ramoscelli sottili si graffiano le mani mentre lo portate in giro.',
+      pl: 'Plącze cienkie gałązki drapiące ręce, gdy się je nosi.'
     }
   },
 
@@ -25,7 +27,8 @@ export default [
       en: 'A peculiar stone carving, smooth from the poundings of time..',
       ro: 'O sculptură de piatră ciudată, netezită de trecerea timpului..',
       pt: 'Uma peculiar escultura em pedra, suave das batidas do tempo...',
-      it: 'Una particolare scultura in pietra, liscia dai colpi del tempo....'
+      it: 'Una particolare scultura in pietra, liscia dai colpi del tempo....',
+      pl: 'Osobliwe rzeźbienie w kamieniu, gładkie od funtów czasu...'
     }
   },
 
@@ -35,7 +38,8 @@ export default [
       en: 'An ancient bone, etched with scenes of battle and hunting.',
       ro: 'Un os străvechi, gravat cu scene de luptă și vânătoare.',
       pt: 'Um osso antigo, gravado com cenas de batalha e caça.',
-      it: 'Un osso antico, inciso con scene di battaglia e caccia.'
+      it: 'Un osso antico, inciso con scene di battaglia e caccia.',
+      pl: 'Starożytna kość, wyryta scenami bitwy i polowań.'
     }
   },
 
@@ -45,7 +49,8 @@ export default [
       en: 'Pure white marble rod encased in a soft yellow glow filling your hands with a divine spiritual light.',
       ro: 'O tijă de marmură albă, pură, încastrată într-o licărire blândă și galbenă, îți umple mâinile cu o lumină spirituală divină.',
       pt: 'Puro bastão de mármore branco envolto em um suave brilho amarelo que enche suas mãos com uma luz espiritual divina.',
-      it: 'Asta di marmo bianco puro racchiusa in una morbida luce gialla che riempie le mani di una luce spirituale divina.'
+      it: 'Asta di marmo bianco puro racchiusa in una morbida luce gialla che riempie le mani di una luce spirituale divina.',
+      pl: 'Czysty, biały marmurowy pręt otoczony miękką, żółtą poświatą wypełniający dłonie boskim, duchowym światłem.'
     }
   },
 
@@ -55,7 +60,8 @@ export default [
       en: 'An eagle, noble and valiant, carved from the holy wood of the ancient trees.',
       ro: 'Un vultur, nobil și viteaz, sculptat din lemnul Sfânt al copacilor antici.',
       pt: 'Uma águia, nobre e valente, esculpida no bosque sagrado das árvores antigas',
-      it: 'Un\'aquila, nobile e coraggiosa, scolpita nel legno sacro degli alberi secolari.'
+      it: 'Un\'aquila, nobile e coraggiosa, scolpita nel legno sacro degli alberi secolari.',
+      pl: 'Orzeł, szlachetny i odważny, wyrzeźbiony w świętym drewnie starożytnych drzew.'
     }
   },
 
@@ -65,7 +71,8 @@ export default [
       en: 'The power of the mysterious aqueous demons follows your footsteps as you lead the way along the dark path of the unknown wonders.',
       ro: 'Puterea demonilor misterioși ai apei îți urmează pașii în timp ce tu mergi pe calea întunecată a minunilor necunoscute.',
       pt: 'O poder dos misteriosos demônios aquosos segue seus passos enquanto você lidera o caminho ao longo do caminho escuro das maravilhas desconhecidas.',
-      it: 'Il potere dei misteriosi demoni acquatici segue le tue orme mentre ti guidi lungo il sentiero buio delle meraviglie sconosciute.'
+      it: 'Il potere dei misteriosi demoni acquatici segue le tue orme mentre ti guidi lungo il sentiero buio delle meraviglie sconosciute.',
+      pl: 'Moc tajemniczych wodnych demonów podąża Twoimi śladami, prowadząc Cię po ciemnej ścieżce nieznanych cudów.'
     }
   },
 
@@ -75,7 +82,8 @@ export default [
       en: 'Torn from the patterns of the dimension, this demonic totem submerges the frail minds of your foes under a cloud of ghastly hallucinations.',
       ro: 'Smuls din tiparele dimensiunii, acest totem demonic scufundă mințile fragile ale dușmanilor tăi într-un un nor de halucinații îngrozitoare.',
       pt: 'Arrancado dos padrões da dimensão, este totem demoníaco submerge as mentes frágeis de seus inimigos sob uma nuvem de alucinações horríveis.',
-      it: 'Strappato dai modelli della dimensione, questo totem demoniaco sommerge le menti fragili dei vostri nemici sotto una nuvola di allucinazioni spettrali.'
+      it: 'Strappato dai modelli della dimensione, questo totem demoniaco sommerge le menti fragili dei vostri nemici sotto una nuvola di allucinazioni spettrali.',
+      pl: 'Wyrwany ze wzorów wymiaru, ten demoniczny totem zanurza wątłe umysły twoich wrogów pod chmurą upiornych halucynacji.'
     }
   },
 
@@ -85,7 +93,8 @@ export default [
       en: 'An eerie carving of a grim wolf sometimes casting a soft howl-like whisper into the murmurs of the dark night.',
       ro: 'O sculptură sinistră a unui lup necruțător, ce uneori scoate un urlet moale ca o șoaptă în murmurul nopții întunecate.',
       pt: 'Uma escultura assustadora de um lobo sombrio às vezes lançando um sussurro suave como um uivo nos murmúrios da noite escura.',
-      it: 'Un\'inquietante scultura di un lupo cupo che a volte getta un soffice ululato nei mormorii della notte oscura.'
+      it: 'Un\'inquietante scultura di un lupo cupo che a volte getta un soffice ululato nei mormorii della notte oscura.',
+      pl: 'Niesamowite wyrzeźbienie ponurego wilka, który czasem rzuca miękki, jak gdyby szept w szmery ciemnej nocy.'
     }
   },
 
@@ -95,7 +104,8 @@ export default [
       en: 'Wind and spirit go hand in hand, trapping you in their swift path through the deep valleys of divinity and enlightenment.',
       ro: 'Vântul și spiritul merg mână în mână, prinzându-te în calea lor rapidă prin văile adânci ale divinității și iluminismului.',
       pt: 'Vento e espírito andam de mãos dadas, prendendo-o no seu caminho rápido através dos vales profundos da divindade e da iluminação.',
-      it: 'Vento e spirito vanno di pari passo, intrappolandovi nel loro rapido percorso attraverso le profonde valli della divinità e dell\'illuminazione.'
+      it: 'Vento e spirito vanno di pari passo, intrappolandovi nel loro rapido percorso attraverso le profonde valli della divinità e dell\'illuminazione.',
+      pl: 'Wiatr i duch idą ręka w rękę, uwięzienia w ich szybkiej drodze przez głębokie doliny boskości i oświecenia.'
     }
   }
 
