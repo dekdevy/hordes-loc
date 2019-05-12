7a56edbe31cc2b326b3a6488ed63531ca5197444
FarqAndraith <49627616+FarqAndraith@users.noreply.github.com>
2019-05-12 16:33:21 -0500

    Update totem.js
    Added Portuguese

--- loc/items/totem.js
+++ loc/items/totem.js
@@ -3,7 +3,8 @@ export default [
     name       : {$$: 'Herb Bundle' },
     description: {
       en: 'A wad of herbs tied together by a thin piece of yarn.',
-      ro: 'O mână de iarbă legată împreună cu un fir subțire de ață'
+      ro: 'O mână de iarbă legată împreună cu un fir subțire de ață',
+      pt: 'Um maço de ervas atadas por um fino pedaço de fio.'
     }
   },
 
@@ -11,7 +12,8 @@ export default [
     name       : {$$: 'Twigs of Earth' },
     description: {
       en: 'Tangles thin twigs scratch your hands as you carry it around.',
-      ro: 'Crenguțe subțiri încurcate te zgârie pe mâini în timp ce le cari cu tine.'
+      ro: 'Crenguțe subțiri încurcate te zgârie pe mâini în timp ce le cari cu tine.',
+      pt: 'Os galhos finos coçam as mãos enquanto a carregas.'
     }
   },
 
@@ -19,7 +21,8 @@ export default [
     name       : {$$: 'Spiritual Carving' },
     description: {
       en: 'A peculiar stone carving, smooth from the poundings of time..',
-      ro: 'O sculptură de piatră ciudată, netezită de trecerea timpului..'
+      ro: 'O sculptură de piatră ciudată, netezită de trecerea timpului..',
+      pt: 'Uma peculiar escultura em pedra, suave das batidas do tempo...'
     }
   },
 
@@ -27,7 +30,8 @@ export default [
     name       : {$$: 'Divine Bone' },
     description: {
       en: 'An ancient bone, etched with scenes of battle and hunting.',
-      ro: 'Un os străvechi, gravat cu scene de luptă și vânătoare.'
+      ro: 'Un os străvechi, gravat cu scene de luptă și vânătoare.',
+      pt: 'Um osso antigo, gravado com cenas de batalha e caça.'
     }
   },
 
@@ -35,15 +39,17 @@ export default [
     name       : {$$: 'Celestial Fetish' },
     description: {
       en: 'Pure white marble rod encased in a soft yellow glow filling your hands with a divine spiritual light.',
-      ro: 'O tijă de marmură albă, pură, încastrată într-o licărire blândă și galbenă, îți umple mâinile cu o lumină spirituală divină.'
+      ro: 'O tijă de marmură albă, pură, încastrată într-o licărire blândă și galbenă, îți umple mâinile cu o lumină spirituală divină.',
+      pt: 'Puro bastão de mármore branco envolto em um suave brilho amarelo que enche suas mãos com uma luz espiritual divina.'
     }
   },
 
   {
     name       : {$$: 'Glade' },
     description: {
-      en: 'An eagle, noble and valiant, carved from the holy wood of the ancient trees',
-      ro: 'Un vultur, nobil și viteaz, sculptat din lemnul Sfânt al copacilor antici.'
+      en: 'An eagle, noble and valiant, carved from the holy wood of the ancient trees.',
+      ro: 'Un vultur, nobil și viteaz, sculptat din lemnul Sfânt al copacilor antici.',
+      pt: 'Uma águia, nobre e valente, esculpida no bosque sagrado das árvores antigas'
     }
   },
 
@@ -51,7 +57,8 @@ export default [
     name       : {$$: 'Aquatus' },
     description: {
       en: 'The power of the mysterious aqueous demons follows your footsteps as you lead the way along the dark path of the unknown wonders.',
-      ro: 'Puterea demonilor misterioși ai apei îți urmează pașii în timp ce tu mergi pe calea întunecată a minunilor necunoscute.'
+      ro: 'Puterea demonilor misterioși ai apei îți urmează pașii în timp ce tu mergi pe calea întunecată a minunilor necunoscute.',
+      pt: 'O poder dos misteriosos demônios aquosos segue seus passos enquanto você lidera o caminho ao longo do caminho escuro das maravilhas desconhecidas.'
     }
   },
 
@@ -59,7 +66,8 @@ export default [
     name       : {$$: 'Carving of Dimensions' },
     description: {
       en: 'Torn from the patterns of the dimension, this demonic totem submerges the frail minds of your foes under a cloud of ghastly hallucinations.',
-      ro: 'Smuls din tiparele dimensiunii, acest totem demonic scufundă mințile fragile ale dușmanilor tăi într-un un nor de halucinații îngrozitoare.'
+      ro: 'Smuls din tiparele dimensiunii, acest totem demonic scufundă mințile fragile ale dușmanilor tăi într-un un nor de halucinații îngrozitoare.',
+      pt: 'Arrancado dos padrões da dimensão, este totem demoníaco submerge as mentes frágeis de seus inimigos sob uma nuvem de alucinações horríveis.'
     }
   },
 
@@ -67,7 +75,8 @@ export default [
     name       : {$$: 'Howl' },
     description: {
       en: 'An eerie carving of a grim wolf sometimes casting a soft howl-like whisper into the murmurs of the dark night.',
-      ro: 'O sculptură sinistră a unui lup necruțător, ce uneori scoate un urlet moale ca o șoaptă în murmurul nopții întunecate.'
+      ro: 'O sculptură sinistră a unui lup necruțător, ce uneori scoate un urlet moale ca o șoaptă în murmurul nopții întunecate.',
+      pt: 'Uma escultura assustadora de um lobo sombrio às vezes lançando um sussurro suave como um uivo nos murmúrios da noite escura.'
     }
   },
 
@@ -75,7 +84,8 @@ export default [
     name       : {$$: 'Brethiza' },
     description: {
       en: 'Wind and spirit go hand in hand, trapping you in their swift path through the deep valleys of divinity and enlightenment.',
-      ro: 'Vântul și spiritul merg mână în mână, prinzându-te în calea lor rapidă prin văile adânci ale divinității și iluminismului.'
+      ro: 'Vântul și spiritul merg mână în mână, prinzându-te în calea lor rapidă prin văile adânci ale divinității și iluminismului.',
+      pt: 'Vento e espírito andam de mãos dadas, prendendo-o no seu caminho rápido através dos vales profundos da divindade e da iluminação.'
     }
   }
 
