0451574f185b11c8deeaff9f0940f769e390f0bd
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-12 21:11:30 +0100

    Totems

---
+++ loc/items/totem.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    name       : {$$: 'Herb Bundle' },
+    description: {
+      en: 'A wad of herbs tied together by a thin piece of yarn.'
+    }
+  },
+
+  {
+    name       : {$$: 'Twigs of Earth' },
+    description: {
+      en: 'Tangles thin twigs stratch your hands as you carry it around.'
+    }
+  },
+
+  {
+    name       : {$$: 'Spiritual Carving' },
+    description: {
+      en: 'A peculiar stone carving, smooth from the poundings of time..'
+    }
+  },
+
+  {
+    name       : {$$: 'Divine Bone' },
+    description: {
+      en: 'An ancient bone, etched with scenes of battle and hunting.'
+    }
+  },
+
+  {
+    name       : {$$: 'Celestial Fetish' },
+    description: {
+      en: 'Pure white marble rod encased in a soft yellow glow filling your hands with a divine spiritual light.'
+    }
+  },
+
+  {
+    name       : {$$: 'Glade' },
+    description: {
+      en: 'An eagle, noble and valiant, carved from the holy wood of the ancient trees'
+    }
+  },
+
+  {
+    name       : {$$: 'Aquatus' },
+    description: {
+      en: 'The power of the mysterious aqueous demons follows your footsteps as you lead the way along the dark path of the unknown wonders.'
+    }
+  },
+
+  {
+    name       : {$$: 'Carving of Dimensions' },
+    description: {
+      en: 'Torn from the patterns of the dimension, this demonic totem submerges the frail minds of your foes under a cloud of ghastly hallucinations.'
+    }
+  },
+
+  {
+    name       : {$$: 'Howl' },
+    description: {
+      en: 'An eerie carving of a grim wolf sometimes casting a soft howl-like whisper into the murmurs of the dark night.'
+    }
+  },
+
+  {
+    name       : {$$: 'Brethiza' },
+    description: {
+      en: 'Wind and spirit go hand in hand, trapping you in their swift path through the deep valleys of divinity and enlightment.'
+    }
+  }
+
+]
