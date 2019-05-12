33dbd8963464b3c8ab491f2f43e1ec824ec39dd8
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-12 05:38:44 +0100

    Translations added

--- loc/npcs/stash.js
+++ loc/npcs/stash.js
@@ -4,8 +4,26 @@ export default {
       en: 'Stash explanation text'
     },
     choices: [{
-      en: 'Yes, open my stash',
-      pt: 'Sim, abre o meu esconderijo.'
+      en: 'Yes, open my chest',
+      ru: 'Да, открой мой сундук',
+      pt: 'Sim, abre o meu esconderijo',
+      es: 'Si, abre mi alijo',
+      fr: 'Oui, ouvre ma cachette',
+      de: 'Ja, öffne mein Versteck',
+      ko: '네, 내 숨긴 장을여십시오.',
+      nl: 'Ja, open mijn voorraad',
+      pl: 'Tak, otwórz mój schowek',
+      sv: 'Ja, öppna min stash',
+      tr: 'Evet, eşyalarımı aç',
+      ht: 'Wi, louvri magazen mwen an',
+      it: 'Sì, apri la mia scorta',
+      hi: 'हां, मेरी कलई खोल दो',
+      tl: 'Oo, buksan mo ang aking itago',
+      el: 'Ναι, ανοίξτε το δοχείο μου',
+      bg: 'Да, отворете контейнера',
+      cs: 'Ano, otevřete můj kontejner',
+      ja: 'はい、コンテナを開けます',
+      ro: 'Da, deschide-mi containerul'
     }]
   }]
 }
