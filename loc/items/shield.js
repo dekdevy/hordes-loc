72520b70e9ef2fbd7f0faab68c49eca76243d1d4
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-12 20:03:28 +0100

    Add files via upload

---
+++ loc/items/shield.js
@@ -0,0 +1,162 @@
+export default [
+  {
+    name       : {$$: 'Wooden Shield' },
+    description: {
+      en: 'Literally a square plank of wood.'
+      ru: 'Буквально квадратная доска из дерева.',
+      es: 'Literalmente una tabla cuadrada de madera.',
+      de: 'Buchstäblich eine quadratische Diele aus Holz.',
+      fr: 'Littéralement une planche de bois carrée.',
+      hi: 'केवल एक लकड़ी का टुकड़ा।',
+      it: 'Letteralmente una tavola quadrata di legno.',
+      tl: 'Literal na isang parisukat na tabla ng kahoy.',
+      bg: 'Буквално квадратна дъска от дърво.',
+      el: 'Κυριολεκτικά μια τετράγωνη ξύλινη σανίδα.',
+      pt: 'Literalmente uma tábua quadrada de madeira.',
+      nl: 'Letterlijk een vierkante plank van hout.',
+      pl: 'Dosłownie kwadratowa deska z drewna.',
+      ht: 'Literalman yon gwo bout bwa kare nan bwa.',
+      cs: 'Tavený železný kroužek naplněný bronzem. Flimsy v jeho tvorbě.',
+      ja: '文字通り木の正方形の板。',
+      sv: 'Bokstavligen en fyrkantig planka av trä.',
+      ko: '말 그대로 나무의 정연한 판자.',
+      ro: 'Literalmente o scândură pătrată de lemn.' 
+    }
+  },
+
+  {
+    name       : {$$: 'Old Bulwark' },
+    description: {
+      en: 'Some farmer has managed to construct a cute little shield.'
+      ru: 'Какому-то фермеру удалось создать милый маленький щит.',
+      es: 'Algún granjero ha conseguido construir un bonito escudo.',
+      de: 'Einem Landwirt ist es gelungen, einen süßen kleinen Schild zu bauen.',
+      fr: 'Un fermier a réussi à construire un joli petit bouclier.',
+      hi: 'किसी किसान के एक छोटी सी ढाल बना ही ली।',
+      it: 'Qualche contadino è riuscito a costruire un piccolo scudo carino.',
+      pt: 'Algum agricultor conseguiu construir um pequeno e bonito escudo.',
+      nl: 'Sommige boeren zijn erin geslaagd om een schattig schildje te bouwen.',
+      tl: 'Ang ilang magsasaka ay nakapagtatag ng isang nakatutuwang maliit na kalasag.',
+      bg: 'Някои фермери са успели да построят сладък малък щит.',
+      el: 'Κάποιος αγρότης κατάφερε να κατασκευάσει μια χαριτωμένη μικρή ασπίδα.',
+      pl: 'Jednemu z rolników udało się zbudować ładną małą tarczę.',
+      ht: 'Gen kèk kiltivatè jere yo konstwi yon bèl plak pwotèj ti kras.',
+      cs: 'Složitý štít vyrobený ze tří kovových kusů. Solidní.',
+      ja: '何人かの農夫はかわいい小さな盾を建設することに成功した。',
+      sv: 'En del bonde har lyckats konstruera en söt liten sköld.',
+      ko: '어떤 농부는 귀여운 작은 방패를 만들 수있었습니다.',
+      ro: 'Un fermier a reușit să construiască un scut mic drăguț.'
+    }
+  },
+
+  {
+    name       : {$$: 'Buckler' },
+    description: {
+      en: 'Smelted iron ring filled with bronze. Flimsy in its making.'
+      ru: 'Плавленый железный круг залитый бронзой. Довольно хлипкий.',
+      es: 'Anillo de hierro fundido relleno de bronce. Frágil en su fabricación.',
+      de: 'Geschmolzener Eisenring mit Bronzefüllung. Flimsy in seiner Entstehung.',
+      fr: 'Bague en fonte de fer remplie de bronze. Frêle dans sa fabrication.',
+      tl: 'Nalikom na singsing na bakal na puno ng tanso. Mahina sa paggawa nito.',
+      bg: 'Излята желязна пръстен с бронз. Слаб в своето създаване.',
+      el: 'Σφυρήλατο σίδερο δαχτυλίδι γεμάτο με χαλκό. Αδύνατο να το κάνει.',
+      it: 'Anello in ferro fuso riempito di bronzo. Flimsy nella sua realizzazione.',
+      pt: 'Anel de ferro fundido cheio de bronze. Frágil na sua confecção.',
+      hi: 'लोहा पिघलकर पीतल से भरी हुई। ठीक से नहीं बनी है।',
+      nl: 'Gesmolten ijzeren ring gevuld met brons. Mager in zijn samenstelling.',
+      pl: 'Wytopiony żelazny pierścień wypełniony brązem. Cienki w wykonaniu.',
+      ht: 'Boule bag fè plen ak kwiv. Kannfeuy nan fè li yo.',
+      cs: 'Některému farmáři se podařilo postavit roztomilý malý štít.',
+      ja: '青銅で満たされた製錬鉄の輪。その作成にぎこちない。',
+      sv: 'Smältjärnsring fylld med brons. Spetsig i sin framställning.',
+      ko: '철 고리 청동 가득합니다. 어색한 제작.',
+      ro: 'Inel de fier topit umplut cu bronz. Făcut superficial.'
+    }
+  },
+
+  {
+    name       : {$$: 'Metal Guard' },
+    description: {
+      en: 'Intricate shield made out of three solid metal pieces. Sturdy.'
+      ru: 'Замысловатый щит из трех металлических частей. Прочный',
+      es: 'Intrincado escudo hecho de tres piezas de metal macizo. Robusto.',
+      de: 'Aufwendiger Schild aus drei massiven Metallteilen. Robust.',
+      fr: 'Bouclier complexe composé de trois pièces en métal massif. Robuste.',
+      hi: 'लोहे के तीन ठोस टुकड़ों से बनी जटील ढाल। तगड़ी है।',
+      it: 'Intricato scudo composto da tre pezzi di metallo massiccio. Robusto.',
+      pt: 'Escudo intrincado feito de três peças metálicas sólidas. Resistente.',
+      nl: 'Geïntegreerd schild gemaakt van drie massieve metalen delen. Stevig.',
+      tl: 'Malakas na kalasag na ginawa sa labas ng tatlong solidong piraso ng metal. Matigas.',
+      bg: 'Сложен щит, изработен от три твърди метални парчета. Здрава.',
+      el: 'Συγκριτική ασπίδα κατασκευασμένη από τρία στερεά μεταλλικά κομμάτια. Ισχυρός.',
+      pl: 'Intrygująca osłona wykonana z trzech litych metalowych kawałków. Wytrzymała.',
+      ht: 'Pwoteksyon konplike te fè soti nan twa moso metal solid. Solid.',
+      cs: 'Složitý štít vyrobený ze tří kovových kusů. Solidní.',
+      ja: '3つの頑丈な金属片で作られた複雑なシールド。頑丈。',
+      sv: 'Invecklad sköld gjord av tre solida metallstycken. Kraftig.',
+      ko: '세 개의 단단한 금속 조각으로 만들어진 복잡한 방패. 견고한.',
+      ro: 'Scut complicat făcut din trei piese de metal solid. Robust.'
+    }
+  },
+
+  {
+    name       : {$$: 'Darkmetal Pavis' },
+    description: {
+      en: 'Tall shield covering your entire body. Only the most accurate of arrows can evade its protection.'
+    }
+  },
+
+  {
+    name       : {$$: 'Royal Guard' },
+    description: {
+      en: 'Gold and yellow patterns coat this shield, displaying your authority and power.'
+    }
+  },
+
+  {
+    name       : {$$: 'Protectron' },
+    description: {
+      en: 'Made from obsidian and steal. Someone sloppily attempted to paint it yellow.'
+      ru: 'Изготовлен из обсидиана и стали. Кто-то небрежно попытался покрасить его в желтый цвет. Hе удалось.',
+      es: 'Hecho de obsidiana y acero. Alguien intentó descuidadamente pintarlo de amarillo. Fracasaron. Miserablemente.',
+      de: 'Hergestellt aus Obsidian und Stahl. Jemand versuchte schlampig, es gelb zu malen. Sie haben versagt. Elenderweise.',
+      fr: 'Fabriqué en obsidienne et en acier. Quelqu\'un a tenté de le peindre en jaune. Ils ont échoué. Misérablement.',
+      hi: 'ओब्सिदीयन और इस्पात से बनी ढाल। किसी ने इसे पीला रंगने की कोशिश की पर उनसे सही से हो न पाया।',
+      it: 'Questo è fatto di ossidiana e acciaio. Qualcuno ha tentato disordinatamente di dipingerlo di giallo. Hanno fallito. Miseramente.',
+      pt: 'Feito de obsidiana e aço. Alguém descuidadamente tentou pintá-lo de amarelo. Eles falharam. Miseravelmente.',
+      nl: 'Gemaakt van obsidiaan en staal. Iemand heeft slordig geprobeerd het geel te schilderen. Ze hebben gefaald. Ellendig.',
+      tl: 'Ginawa mula sa obsidian at steel. Sinubukan ng isang tao na pintura itong dilaw. Nabigo sila. Malungkot.',
+      bg: 'Изработен от обсидиан и стомана. Някой небрежно се опита да го нарисува жълто. Те се провалиха. Мизерно.',
+      el: 'Κατασκευασμένο από οψιανό και χάλυβα. Κάποιος προσπαθούσε επιπόλαια να το κηλιδώσει κίτρινο. Αποτυχία. Αθλιως.',
+      pl: 'Zrobione z obsydianu i stali. Ktoś niechlujnie próbował namalować go na żółto. Nie udało im się. Nędznie.',
+      ht: 'Te fè soti nan obsidya ak asye. Yon moun t ap eseye penti li jòn. Yo echwe. Miserableman.',
+      cs: 'Vyrobeno z obsidiánu a oceli. Někdo se ho pokusil namalovat žlutě. Neuspěli. Bídně.',
+      ja: '黒曜石とスチールから作られました。誰かがそれを黄色く塗ろうとしました。彼らは失敗しました。悲惨なことに。',
+      sv: 'Tillverkad av obsidian och stål. Någon försökte att måla den gul. De misslyckades. Eländigt.',
+      ko: '흑요석과 강철로 만들었습니다. 누군가는 그것을 황당하게 노란 색으로 칠하려고했습니다. 그들은 실패했다.',
+      ro: 'Făcut din obsidian și oțel. Cineva neglijent a încercat să-l vopsească în galben. A eșuat. Mizerabil.'    
+    }
+  },
+
+  {
+    name       : {$$: 'Guardian' },
+    description: {
+      en: 'Tall, dark and heavy shield enveloping your unprotected body with the power of ancient guardians once residing in these valleys.'
+    }
+  },
+
+  {
+    name       : {$$: 'Vilzorg' },
+    description: {
+      en: 'A sinister shield casting vile strands of shadows past the barriers of your foes mind to tarnish their memories.'
+    }
+  },
+
+  {
+    name       : {$$: 'Rubio' },
+    description: {
+      en: 'A mighty shield radiating divine light into your noble allies to engulf them in waves of valor.'
+    }
+  }
+
+]
