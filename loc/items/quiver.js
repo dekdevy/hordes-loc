81f867371ec0e9862c5673e81379d0816c4717f4
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-13 20:17:58 +0100

    Update quiver.js

--- loc/items/quiver.js
+++ loc/items/quiver.js
@@ -204,7 +204,7 @@ export default [
   { // no translations needed at the moment
     name       : {$$: 'Nefarious' },
     description: {
-      en: 'A dark quiver coated in shimmering deep navy throws you into a chasm of peace as you pull out another cherished arrow...',
+      en: 'A dark quiver coated in deep, shimmering navy throws you into a chasm of peace as you pull out another cherished arrow...',
     }
   }
 
