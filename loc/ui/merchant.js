c199667cd18b76cf1269233628032c9bf3cfbdf9
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-15 21:27:09 +0100

    merchant moved out of inventory.js

--- loc/ui/merchant.js
+++ loc/ui/merchant.js
@@ -54,6 +54,85 @@ export default {
     de: 'Zieh ein Item hier rein',
     ru: 'Перетащите предмет в слот',
     tl: 'I-drag ang item sa puwang'
+  },
+
+  spend: {
+    en: 'You have spent $1 coins',
+    ru: 'Вы потратили $1 монеты',
+    es: 'Has gastado 2 monedas',
+    fr: 'Vous avez dépensé $1 pièces',
+    de: 'Du hast $1 Münzen ausgegeben',
+    pt: 'Você gastou $1 moedas',
+    ko: '$1 동전을 보냈습니다',
+    nl: 'U heeft $1 munten uitgegeven',
+    pl: 'Wydałeś $1 monety',
+    sv: 'Du har spenderat $1 mynt',
+    tr: '$1 madeni para harcadınız',
+    ht: 'Ou te pase $1 pyès monnen',
+    it: 'Hai speso $1 monete',
+    hi: 'आपने $1 सिक्के खर्च कर दिए हैं',
+    tl: 'Ginugol mo ang $1 barya',
+    el: 'Έχετε ξοδέψει $1 νομίσματα',
+    bg: 'Прекарали сте $1 монети',
+    cs: 'Strávili jste $1 mincí',
+    ja: 'あなたは $1 コインを使った',
+    ro: 'Ai cheltuit $1 de monede'
+  },
+
+  auctionpost: {
+    en: 'You have posted $1 for sale',
+    es: 'Ha contabilizado $1 para la venta',
+    fr: 'Vous avez mis $1 en vente',
+    de: 'Sie haben $1 zum Verkauf angeboten',
+    pt: 'Você postou $1 para venda',
+    ru: 'Вы выставили $1 на продажу',
+    ko: '판매용으로 $1 를 게시했습니다',
+    nl: 'U heeft $1 te koop aangeboden',
+    pl: 'Wysłałeś $1 na sprzedaż',
+    sv: 'Du har skrivit $1 till salu',
+    tr: 'Satılık $1 gönderdiniz',
+    ht: 'Ou te afiche $1 pou vann',
+    it: 'Hai postato $1 in vendita',
+    hi: 'आपने $1 को विक्रय के लिए उपलब्ध कर दिया है',
+    tl: 'Nag-post ka ng $1 para sa pagbebenta',
+    el: 'Έχετε δημοσιεύσει $1 προς πώληση',
+    bg: 'Публикували сте $1 за продажба',
+    cs: 'Zveřejnili jste $1 na prodej',
+    ja: 'あなたは販売のために $1 を投稿しました',
+    ro: 'Ați postat $1 de vânzare'
+  },
+
+  auctioncancel: {
+    en: 'Item delisted and sent to stash',
+    nl: 'Item verwijderd en naar de stash verzonden',
+    de: 'Verkauf abgebrochen und Item in den Speicher geschickt',
+    pt: 'Item retirado da lista e enviado para o esconderijo',
+    it: 'Articolo cancellato e inviato a magazzino',
+    fr: 'L\'article a été retiré de la liste et envoyé à la planque.',
+    pl: 'Przedmiot usunięty z wykazu i wysłany do przechowania.',
+    es: 'Artículo retirado de la lista y enviado a esconder',
+    tl: 'Inalis ang item at ipinadala sa itago'
+  },
+
+  auctionbuy: {
+    en: 'Item purchased for $1 coins and sent to your Stash',
+    ru: 'Предмет, купленный за $1, отправлен в ваш сундук',
+    es: 'Artículo comprado por $1 monedas y enviado a su Stash',
+    fr: 'Article acheté pour $1 pièces et envoyé à votre Stash',
+    de: 'Artikel, der für $1 Münzen gekauft und an Ihr Versteck geschickt wurde',
+    pt: 'Item comprado por $1 moedas e enviado para o seu Cesto',
+    nl: 'Item gekocht voor $1 munten en verzonden naar uw Stash',
+    pl: 'Przedmiot zakupiony za $1 monet i wysłany do Twojego magazynu',
+    sv: 'Produkt som köpts för $1 mynt och skickas till din Stash',
+    ht: 'Atik achte pou pyès monnen $1 epi li voye bay Stash ou',
+    it: 'Oggetto acquistato per $1 monete e inviato al tuo Stash',
+    hi: '$1 के लिए अपने वस्तु खरीद लिया है और उसे आपके स्टैश में भेज दिया गया है',
+    tl: 'Ang item na binili para sa $1 barya at ipinadala sa iyong Stash',
+    el: 'Στοιχείο που αγοράσατε για $1 νομίσματα και απεστάλητε στο Stash σας',
+    bg: 'Позиция, закупена за $1 монети и изпратена до вашия Stash',
+    cs: 'Zboží zakoupené za $1 mincí a zaslané do vašeho Stash',
+    ja: '$1 コインを購入し、Stash に送った商品'
   }
+
 
 }
