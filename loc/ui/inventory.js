28af4678b10bdf178e068032f5c15b59367b9ddf
dek <polydek@googlemail.com>
2019-05-15 20:49:17 +0200

    some corrections, de

--- loc/ui/inventory.js
+++ loc/ui/inventory.js
@@ -29,10 +29,10 @@ export default {
     es: 'Usted ha recogido $1',
     pt: 'Você pegou $1',
     it: 'Hai preso $1',
-    pl: 'Odebrałeś $1.',
-    de: 'Du hast $1 gesammelt.',
+    pl: 'Odebrałeś $1',
+    de: 'Du hast $1 gesammelt',
     ro: 'Ai luat $1',
-    fr: 'Vous avez ramassé $1.',
+    fr: 'Vous avez ramassé $1',
     ru: 'Вы подобрали $1',
     hi: 'आपने $1 उठा लिया',
     bg: 'Взехте $1',
@@ -41,7 +41,7 @@ export default {
     ja: '$1 を拾った',
     tl: 'Kinuha mo na $1',
     ht: 'Ou ranmase $1',
-    ko: '당신은 $1 를 선택했습니다.',
+    ko: '당신은 $1 를 선택했습니다',
     sv: 'Du har plockat upp $1'
   },
 
@@ -51,10 +51,10 @@ export default {
     ro: 'Ai aruncat $1',
     es: 'Tiraste a la basura el $1',
     fr: 'Vous avez jeté $1',
-    de: 'Du hast $1 weggeworfen.',
+    de: 'Du hast $1 weggeworfen',
     pt: 'Deitaste fora a $1',
     ko: '너는 버렸어 $1',
-    nl: 'Je hebt $1 weggegooid.',
+    nl: 'Je hebt $1 weggegooid',
     pl: 'Wyrzuciłeś $1',
     sv: 'Du kastade bort $1',
     tr: '$1 \'i attın',
@@ -70,23 +70,23 @@ export default {
 
   full: {
     en: 'Your inventory is full',
-    nl: 'Uw inventaris is vol.',
+    nl: 'Uw inventaris is vol',
     es: 'Su inventario está lleno',
     pt: 'O seu inventário está cheio',
     it: 'Il tuo inventario è pieno',
-    pl: 'Twój inwentarz jest pełny.',
-    de: 'Dein Inventar ist voll.',
+    pl: 'Twój inwentarz jest pełny',
+    de: 'Dein Inventar ist voll',
     ro: 'Inventarul tău este plin',
     fr: 'Votre inventaire est complet',
     ru: 'В инвентаре нет места',
     hi: 'आपकी वस्तुसूचि भर गयी है',
-    bg: 'Инвентара е пълен.',
+    bg: 'Инвентара е пълен',
     cs: 'Váš inventář je plný',
     el: 'Το απόθεμά σας είναι γεμάτο',
     ja: '在庫がいっぱいです',
     tl: 'Ang iyong imbentaryo ay puno',
     ht: 'Envantè ou plen',
-    ko: '인벤토리가 가득 찼습니다.',
+    ko: '인벤토리가 가득 찼습니다',
     sv: 'Din inventering är full'
   },
 
@@ -96,10 +96,10 @@ export default {
     es: 'Usted ha vendido $1 por $2',
     pt: 'Você vendeu $1 por $2',
     it: 'Hai venduto $1 per $2',
-    pl: 'Sprzedałeś $1 za $2.',
-    de: 'Du hast $1 für $2 verkauft.',
+    pl: 'Sprzedałeś $1 za $2',
+    de: 'Du hast $1 für $2 verkauft',
     ro: 'Ai vândut $1 pentru $2',
-    fr: 'Vous avez vendu $1 pour $2.',
+    fr: 'Vous avez vendu $1 pour $2',
     ru: 'Вы продали $1 за $2',
     hi: 'आपने $1 के लिए $2 बेच दिया है',
     bg: 'Продадохте $1 за $2',
@@ -108,7 +108,7 @@ export default {
     ja: 'あなたは売りました $1 の間 $2',
     tl: 'Binebenta mo na $1 Para sa $2',
     ht: 'Ou te vann $1 pou $2',
-    ko: '$1 에 $1 를 판매했습니다.',
+    ko: '$1 에 $1 를 판매했습니다',
     sv: 'Du har sålt $1 för $2'
   },
 
@@ -117,7 +117,7 @@ export default {
     ru: 'выбросить',
     hi: 'वस्तु गिरा दे',
     es: 'Posición de entrega',
-    de: 'Gegenstand ablegen',
+    de: 'Item ablegen',
     fr: 'Déposer un article',
     nl: 'Laat vallen',
     pt: 'Item de gota',
@@ -142,7 +142,7 @@ export default {
     ro: 'Utilizează',
     es: 'Utilizar posición',
     fr: 'Utiliser l\'article',
-    de: 'Element verwenden',
+    de: 'Item verwenden',
     pt: 'Usar item',
     ko: '항목 사용',
     nl: 'Gebruik item',
@@ -164,11 +164,11 @@ export default {
     ru: 'Положить в сундук',
     pt: 'Mover para o esconderijo',
     it: 'Spostare a nascondiglio',
-    pl: 'Przenieś się do skrytki.',
+    pl: 'Przenieś się do skrytki',
     fr: 'Aller à la cachette',
     es: 'Moverse a escondite',
     nl: 'Verhuizen naar opslagplaats',
-    ko: '은신처로 이동하십시오.',
+    ko: '은신처로 이동하십시오',
     sv: 'Flytta till stash',
     ht: 'Deplase nan catcher',
     hi: 'स्टैश में भेजें',
@@ -181,13 +181,14 @@ export default {
 
   stashed: {
     en: '$1 has been moved to your stash',
+    de: '$1 wurde in deinen Speicher verschoben',
     ru: '$1 Было перенесено в сундук',
     pt: '$1 foi transferido para o seu esconderijo',
-    it: '$1 e\' stato spostato nella tua scorta.',
-    pl: '$1 został przeniesiony do twojego magazynu.',
+    it: '$1 e\' stato spostato nella tua scorta',
+    pl: '$1 został przeniesiony do twojego magazynu',
     fr: '$1 a été transféré dans votre planque',
     es: '$1 ha sido movido a tu escondite',
-    nl: '$1 is verplaatst naar je voorraad.',
+    nl: '$1 is verplaatst naar je voorraad',
     hi: '$1 को स्टैश में भेज दिया गया है'
   },
 
@@ -196,11 +197,11 @@ export default {
     ru: 'Вы потратили $1 монеты',
     es: 'Has gastado 2 monedas',
     fr: 'Vous avez dépensé $1 pièces',
-    de: 'Du hast $1 Münzen ausgegeben.',
+    de: 'Du hast $1 Münzen ausgegeben',
     pt: 'Você gastou $1 moedas',
-    ko: '$1 동전을 보냈습니다.',
+    ko: '$1 동전을 보냈습니다',
     nl: 'U heeft $1 munten uitgegeven',
-    pl: 'Wydałeś $1 monety.',
+    pl: 'Wydałeś $1 monety',
     sv: 'Du har spenderat $1 mynt',
     tr: '$1 madeni para harcadınız',
     ht: 'Ou te pase $1 pyès monnen',
@@ -218,12 +219,12 @@ export default {
     en: 'You have posted $1 for sale',
     es: 'Ha contabilizado $1 para la venta',
     fr: 'Vous avez mis $1 en vente',
-    de: 'Sie haben die $1 zum Verkauf angeboten.',
+    de: 'Sie haben $1 zum Verkauf angeboten',
     pt: 'Você postou $1 para venda',
-    ru: 'Вы выставили $1 на продажу.',
-    ko: '판매용으로 $1 를 게시했습니다.',
+    ru: 'Вы выставили $1 на продажу',
+    ko: '판매용으로 $1 를 게시했습니다',
     nl: 'U heeft $1 te koop aangeboden',
-    pl: 'Wysłałeś $1 na sprzedaż.',
+    pl: 'Wysłałeś $1 na sprzedaż',
     sv: 'Du har skrivit $1 till salu',
     tr: 'Satılık $1 gönderdiniz',
     ht: 'Ou te afiche $1 pou vann',
@@ -239,7 +240,8 @@ export default {
 
   auctioncancel: {
     en: 'Item delisted and sent to stash',
-    nl: 'Item verwijderd en naar de inventaris verzonden'
+    nl: 'Item verwijderd en naar de stash verzonden',
+    de: 'Verkauf abgebrochen und Item in den Speicher geschickt'
   },
 
   auctionbuy: {
@@ -247,10 +249,10 @@ export default {
     ru: 'Предмет, купленный за $1, отправлен в ваш сундук',
     es: 'Artículo comprado por $1 monedas y enviado a su Stash',
     fr: 'Article acheté pour $1 pièces et envoyé à votre Stash',
-    de: 'Artikel, der für $1 Münzen gekauft und an Ihr Versteck geschickt wurde.',
+    de: 'Artikel, der für $1 Münzen gekauft und an Ihr Versteck geschickt wurde',
     pt: 'Item comprado por $1 moedas e enviado para o seu Cesto',
     nl: 'Item gekocht voor $1 munten en verzonden naar uw Stash',
-    pl: 'Przedmiot zakupiony za $1 monet i wysłany do Twojego magazynu.',
+    pl: 'Przedmiot zakupiony za $1 monet i wysłany do Twojego magazynu',
     sv: 'Produkt som köpts för $1 mynt och skickas till din Stash',
     ht: 'Atik achte pou pyès monnen $1 epi li voye bay Stash ou',
     it: 'Oggetto acquistato per $1 monete e inviato al tuo Stash',
@@ -263,7 +265,13 @@ export default {
   },
 
   waitunstash: {
-    en: 'You have to wait until you can retreive this item'
+    en: 'You have to wait until you can retreive this item',
+    de: 'Du musst noch etwas warten, bis du dieses Item nehmen kannst'
+  },
+
+  withdraw: {
+    en: 'Withdraw',
+    de: 'Abheben'
   },
 
   search: {
@@ -273,30 +281,36 @@ export default {
 
   filtermine: {
     en: 'My items only',
-    nl: 'Alleen mijn items'
+    nl: 'Alleen mijn items',
+    de: 'Nur meine Items'
   },
 
   buy: {
     en: 'Buy Item',
-    nl: 'Product Kopen'
+    nl: 'Item Kopen',
+    de: 'Item kaufen'
   },
 
   delist: {
     en: 'Delist Item',
-    nl: 'Verwijderd Item'
+    nl: 'Verwijderd Item',
+    de: 'Verkauf abbrechen'
   },
 
   price: {
     en: 'Price: ', // include the space and punctuation
-    nl: 'Prijs: '
+    nl: 'Prijs: ',
+    de: 'Preis: '
   },
 
   fee: {
-    en: 'Fee: ' // include the space and punctuation
+    en: 'Fee: ', // include the space and punctuation
+    de: 'Gebühr: '
   },
 
   dragitem: {
     en: 'Drag item into slot',
-    nl: 'Slepen het item in de sleuf'
+    nl: 'Slepen het item in de sleuf',
+    de: 'Zieh ein Item in herein'
   }
 }
