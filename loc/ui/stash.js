cb962fa014090e0a8b020aa9bdbfdb34db3002af
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-15 21:22:31 +0100

    Update stash.js

--- loc/ui/stash.js
+++ loc/ui/stash.js
@@ -1,1 +1,17 @@
-//
+export default {
+
+  waitunstash: {
+    en: 'You have to wait until you can retreive this item',
+    de: 'Du musst noch etwas warten, bis du dieses Item nehmen kannst',
+    ru: 'вы должны подождать перед тем как вы можете получить этот предмет',
+    tl: 'Kailangan mong maghintay hanggang sa maari mong ma-retreive ang item na ito'
+  },
+
+  withdraw: {
+    en: 'Withdraw',
+    nl: 'Terugtrekken',
+    de: 'Abheben',
+    ru: 'Забрать',
+    tl: 'Mag-withdraw'
+  }
+}
