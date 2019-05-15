cba3381ea0101142ba284a29b74c470676056420
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-15 21:22:00 +0100

    Update merchant.js

--- loc/ui/merchant.js
+++ loc/ui/merchant.js
@@ -1,1 +1,59 @@
-//
+export default {
+
+  search: {
+    en: 'Search',
+    nl: 'Zoeken',
+    ru: 'Поиск',
+    de: 'Suche',
+    tl: 'Paghahanap'
+  },
+
+  filtermine: {
+    en: 'My items only',
+    nl: 'Alleen mijn items',
+    de: 'Nur meine Items',
+    ru: 'Только мои предметы',
+    tl: 'Aking mga item lamang'
+  },
+
+  buy: {
+    en: 'Buy Item',
+    nl: 'Item Kopen',
+    de: 'Item kaufen',
+    ru: 'Купить предмет',
+    tl: 'Bumili ng Item'
+  },
+
+  delist: {
+    en: 'Delist Item',
+    nl: 'Verwijderd Item',
+    de: 'Verkauf abbrechen',
+    ru: 'исключить предмет',
+    tl: 'ibukod ang item'
+  },
+
+  price: {
+    en: 'Price: ', // include the space and punctuation
+    nl: 'Prijs: ',
+    de: 'Preis: ',
+    ru: 'Цена: ',
+    tl: 'Presyo: '
+  },
+
+  fee: {
+    en: 'Fee: ', // include the space and punctuation
+    de: 'Gebühr: ',
+    nl: 'Honorarium: ',
+    ru: 'Плата: ',
+    tl: 'Bayad: '
+  },
+
+  dragitem: {
+    en: 'Drag item into slot',
+    nl: 'Slepen het item in de sleuf',
+    de: 'Zieh ein Item hier rein',
+    ru: 'Перетащите предмет в слот',
+    tl: 'I-drag ang item sa puwang'
+  }
+
+}
