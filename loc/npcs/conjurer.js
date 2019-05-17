e0e4a30d834db1eedc2296329a0f76a988bfb03f
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-17 16:01:12 +0100

    Update conjurer.js

--- loc/npcs/conjurer.js
+++ loc/npcs/conjurer.js
@@ -10,28 +10,6 @@ export default {
            The shapes are now jagged letters, forming some words. You can barely make out what they say. "Where....wood"?\
            The mysterious fellow  seems to be impatient. He points at his freshly made work and stares at you with an angry flash in his lustrous eyes.\
            "Where would...you like... to go?'
-
-
-      en: 'Ah, I see you traveler. Weary and famished you look. We conjurers, too, roam these fine lands. Would you like me to take you to one?',
-      ru: 'А, вижу тебя путешественник. Ты выглядишь  уставшим и голодным . Мы, волшебники, тоже путешествуем по этим прекрасным землям. Хочешь, я отведу тебя к одному из нас?',
-      fr: 'Bonjour, je peux faire apparaître un portail vers les autres conjurers. Où voudriez-vous aller?',
-      hi: 'आह, मैं आपको देख सकता हूं यात्री। भूखे और थके हारे लग रहे हैं आप। हम मायाविक भी इन सुंदर देशों की यात्रा करते है। क्या मैं आपको कहीं ले चलूं?',
-      es: 'Ah, te veo viajero. Pareces cansado y hambriento. Nosotros, los hechiceros, también, deambulamos por estas bellas tierras. ¿Quieres que te lleve a uno?',
-      de: 'Ah, ich sehe dich als Reisenden. Müd und ausgehungert siehst du aus. Auch wir Zauberer durchstreifen diese schönen Ländereien. Möchtest du, dass ich dich zu einem bringe?',
-      it: 'Ah, vi vedo viaggiatore. Stancato e affamato voi sguardo. Prestigiatori, anche, vaghiamo queste terre fini. voi mi gradiscono prendervi ad uno?',
-      ht: 'O, mwen wè ou vwayajè. Ou gade fatige ak grangou. Nou asistan tou Roaming sa yo bèl tè. Ou vle m \'mennen ou nan yon sèl?',
-      pl: 'Ach, widzę, że jesteś podróżnikiem. Zmęczony i głodny wyglądasz. My, czarnoksiężnicy, również przemierzamy te piękne ziemie. Chcesz, żebym zabrał cię do jednego?',
-      nl: 'Ah, ik zie je reiziger. Moe en uitgehongerd zie je eruit. Wij goochelaars, te, zwerven door dit mooie land. Zou je willen dat ik je naar een van die breng?',
-      pt: 'Ah, vejo-te viajante. Cansado e faminto você olha. Nós, mágico, também andamos por essas terras nobres. Queres que te leve a uma?',
-      sv: 'Ah, jag ser dig resande.  Weary och famished du ser.  Vi medför också förföljare i dessa fina länder.  Vill du att jag ska ta dig till en?',
-      cs: 'Ach, vidím vás cestovatele.  Vzteklý a hladový pohled.  Také se uchylujeme k těmto jemným zemím.  Chcete, abych vás vzal na jednoho?',
-      ja: 'ああ、旅人に会いましょう。疲れきってあなたは見ている。私たちもまた、この素晴らしい土地を歩き回っています。私を連れて行ってもらえますか。',
-      tr: 'Ah, seni görüyorum gezgin. Yorgun ve kurt gibi aç gözüküyorsun. Bu güzel topraklarda dolaşan biz büyücüler de öyleyiz. Seni bir tanesine götürmemi ister misin?',
-      ko: '아, 여행자 본다. 지치고 캄캄 해 보이는 군. 우리의 연인도이 좋은 땅을 돌아 다닌다. 내가 너를 하나에 데려 갈까?',
-      bg: 'Ах, виждам те пътешественико. Изглеждаш гладен и уморен. Ние, Създтелите, също обикаляме по тези прекрасни земи. Искаш ли да те заведа на едно?',
-      el: 'Αξ, σε βλέπω ταξιδιώτη.  Κόκκινος και φτωχός κοιτάζετε.  Και εμείς οι περιπλανώμενοι βόλτα αυτά τα ωραία εδάφη.  Θα θέλατε να σας μεταφέρω σε ένα;',
-      ro: 'Ah, te văd călătorule. Obosit și înfometat arăți. Noi iluzioniștii, de asemenea, cutreierăm aceste pământuri strașnice. Ți-ar plăcea să te duc să vezi unul?',
-      tl: 'Ah, nakikita kita mong biyahero. Nagagalit at gutom na tinitingnan mo. Ang mga conjurer natin, rin, ay naglalakbay sa mga pinong lupain. Gusto mo bang dalhin ka sa isa?'
     },
     choices: [{
       en: 'Please bring me to $1.',
