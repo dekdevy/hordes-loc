44bf10dedff92396cb0c6ea0e201d3614df63373
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-19 13:50:58 +0100

    Added runes

--- loc/loc.js
+++ loc/loc.js
@@ -15,13 +15,13 @@ import armor from './items/armor.js'
 import bag from './items/bag.js'
 import boot from './items/boot.js'
 import bow from './items/bow.js'
-import gem from './items/gem.js'
 import glove from './items/glove.js'
 import hammer from './items/hammer.js'
 import misc from './misc.js'
 import orb from './items/orb.js'
 import quiver from './items/quiver.js'
 import ring from './items/ring.js'
+import rune from './items/rune.js'
 import shield from './items/shield.js'
 import staff from './items/staff.js'
 import sword from './items/sword.js'
@@ -33,13 +33,13 @@ const items = {
   boot,
   bow,
   armor,
-  gem,
   glove,
   hammer,
   misc,
   orb,
   quiver,
   ring,
+  rune,
   shield,
   staff,
   sword,
