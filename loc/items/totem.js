6acdd5955a1ebcc6dcb346d416206dcb0e6d9133
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-20 20:43:15 +0100

    New Totems

--- loc/items/totem.js
+++ loc/items/totem.js
@@ -1,211 +1,71 @@
 export default [
   {
-    name       : {$$: 'Herb Bundle' },
+    name       : {$$: 'Dream Feathers'},
     description: {
-      en: 'A wad of herbs tied together by a thin piece of yarn.',
-      ro: 'O mână de iarbă legată împreună cu un fir subțire de ață',
-      pt: 'Um maço de ervas atadas por um fino pedaço de fio.',
-      it: 'Un batuffolo di erbe legate insieme da un sottile pezzo di filato.',
-      pl: 'Zwitek ziół związany cienkim kawałkiem przędzy.',
-      fr: 'Une poignée de brins d\'herbes liés entre eux par un mince morceau de laine.',
-      es: 'Un fajo de hierbas atado con un fino trozo de hilo.',
-      nl: 'Een propje kruiden samengebonden door een dun stukje garen.',
-      hi: 'पतले धागे से बांधा हुआ जड़ी-बूटियों का गुच्छा।',
-      ru: 'Травяная куча, связанная тонкой нитью.',
-      bg: 'Пачка билки, вързани с тънка прежда.',
-      cs: 'Hromada bylinek svázaných tenkým kouskem příze.',
-      tl: 'Isang balumbon ng mga damo na tinalian ng isang manipis na piraso ng sinulid.',
-      sv: 'En lappa av örter bundet samman med en tunn bit garn.',
-      de: 'Ein Bündel von Kräutern, die durch ein dünnes Stück Garn miteinander verbunden sind.'
+      en: 'Item Description'
     }
   },
 
   {
-    name       : {$$: 'Twigs of Earth' },
+    name       : {$$: 'Voodoo Doll'},
     description: {
-      en: 'Tangled thin twigs scratch your hands as you carry it around.',
-      ro: 'Crenguțe subțiri încurcate te zgârie pe mâini în timp ce le cari cu tine.',
-      pt: 'Os galhos finos coçam as mãos enquanto a carregas.',
-      it: 'Tangoli sottili ramoscelli sottili si graffiano le mani mentre lo portate in giro.',
-      pl: 'Plącze cienkie gałązki drapiące ręce, gdy się je nosi.',
-      fr: 'Des brindilles fines et enchevêtrées vous griffent les mains pendant que vous les transportez avec vous.',
-      es: 'Las delgadas ramitas enredadas raspan tus manos mientras las llevas a todas partes.',
-      nl: 'De klitten van dunne twijgen krabben aan je handen terwijl je het rond draagt.',
-      hi: 'उलझे हुए तिनके जो अपने हाथ में पकडने से आपके हाथों को खरोंचेंए।',
-      ru: 'Запутанные тонкие веточки царапают ваши руки когда вы носите их с собой.',
-      bg: 'Заплетени тънки клонки почесват ръцете ви, докато го носите наоколо.',
-      cs: 'Zamotané tenké větvičky poškrábat ruce, jak si nesete kolem.',
-      tl: 'Gusot ang manipis na mga sanga scratch ang iyong mga kamay bilang dalhin mo ito sa paligid.',
-      sv: 'Tunna tunna tiggar kliar dina händer när du bär den runt.',
-      de: 'Verworrene dünne Zweige kratzen an den Händen, während du sie herumträgst.'
+      en: 'Item Description'
     }
   },
 
   {
-    name       : {$$: 'Spiritual Carving' },
+    name       : {$$: 'Glowing Chicken'},
     description: {
-      en: 'A peculiar stone carving, smooth from the poundings of time.',
-      ro: 'O sculptură de piatră ciudată, netezită de trecerea timpului.',
-      pt: 'Uma peculiar escultura em pedra, suave das batidas do tempo.',
-      it: 'Una particolare scultura in pietra, liscia dai colpi del tempo.',
-      pl: 'Osobliwe rzeźbienie w kamieniu, gładkie od funtów czasu.',
-      fr: 'Une sculpture en pierre particulière, lisse à cause des martèlements du temps.',
-      es: 'Una peculiar talla en piedra, lisa por los golpes del tiempo.',
-      nl: 'Een eigenaardig stenen snijwerk, glad van het bonzen van de tijd.',
-      hi: 'पत्थर पर की गई अजीब नक्काशी, जो समय के साथ चिकनी हो गई है।',
-      ru: 'Странный, обточенный камень, гладкий из-за истечения времени.',
-      bg: 'Особена каменна дърворезба, гладка от ударите на времето.',
-      cs: 'Zvláštní kamenné řezbářství, hladké z bušení času.',
-      tl: 'Ang isang kakaibang bato larawang inukit, makinis mula sa mga poundings ng oras.',
-      sv: 'En märklig stenhuggning, slät från tidens bultar',
-      de: 'Eine eigentümliche Steinmetzarbeit, glatt von den Schlägen der Zeit.'
+      en: 'Item Description'
     }
   },
 
   {
-    name       : {$$: 'Divine Bone' },
+    name       : {$$: 'Divine Beads'},
     description: {
-      en: 'An ancient bone, etched with scenes of battle and hunting.',
-      ro: 'Un os străvechi, gravat cu scene de luptă și vânătoare.',
-      pt: 'Um osso antigo, gravado com cenas de batalha e caça.',
-      it: 'Un osso antico, inciso con scene di battaglia e caccia.',
-      pl: 'Starożytna kość, wyryta scenami bitwy i polowań.',
-      fr: 'Un os ancien, gravé de scènes de bataille et de chasse.',
-      es: 'Un hueso antiguo, grabado con escenas de batalla y caza.',
-      nl: 'Een oud bot, geëtst met scènes van strijd en jacht.',
-      hi: 'एक प्राचीन हड्डी, जिस पर युद्ध के दृश्य की नक्काशी है।',
-      ru: 'Древняя кость, выгравированная сценами сражений и охоты.',
-      bg: 'Древна кост, гравирана с сцени на битка и лов.',
-      cs: 'Starobylá kost, vyrytá scénami bitvy a lovu.',
-      tl: 'Isang sinaunang buto, na may mga tanawin ng labanan at pangangaso.',
-      sv: 'Ett gammalt ben, etsat med scener av strid och jakt.',
-      de: 'Ein alter Knochen, geätzt mit Szenen von Kampf und Jagd.'
+      en: 'Item Description'
     }
   },
 
   {
-    name       : {$$: 'Celestial Fetish' },
+    name       : {$$: 'Templar\'s Resolve'},
     description: {
-      en: 'Pure white marble rod encased in a soft yellow glow filling your hands with a divine spiritual light.',
-      ro: 'O tijă de marmură albă, pură, încastrată într-o licărire blândă și galbenă, îți umple mâinile cu o lumină spirituală divină.',
-      pt: 'Puro bastão de mármore branco envolto em um suave brilho amarelo que enche suas mãos com uma luz espiritual divina.',
-      it: 'Asta di marmo bianco puro racchiusa in una morbida luce gialla che riempie le mani di una luce spirituale divina.',
-      pl: 'Czysty, biały marmurowy pręt otoczony miękką, żółtą poświatą wypełniający dłonie boskim, duchowym światłem.',
-      fr: 'Bâton de marbre blanc pur enveloppé d\'une douce lueur jaune remplissant vos mains d\'une lumière spirituelle divine.',
-      es: 'Vara de mármol blanco puro envuelta en un suave resplandor amarillo que llena tus manos de una luz espiritual divina.',
-      nl: 'Puur wit marmeren staafje in een zachte gele gloed die je handen vult met een goddelijk spiritueel licht.',
-      hi: 'मुलायम पीले सोने में बंद शुद्ध सफेद संगमरमर की छड़ी जो आपके हाथों में दिव्य रौशनी डालती है।',
-      ru: 'Палочка из чистого белого мрамора, заключенная в мягкое желтое сияние, наполняет ваши руки божественным душевным светом.',
-      bg: 'Чиста бяла мраморна пръчка, обгърната с мека жълта светлина, изпълваща ръцете ви с божествена духовна светлина.',
-      cs: 'Čistě bílá mramorová tyč zapouzdřená do měkké žluté záře vyplňující vaše ruce božským duchovním světlem.',
-      tl: 'Purong puting marmol baras encased sa isang malambot na dilaw glow pagpuno ng iyong mga kamay sa isang banal na espirituwal na liwanag.',
-      sv: 'Ren vit marmorstång omslagen i en mjuk gul glöd som fyller dina händer med ett gudomligt andligt ljus.',
-      de: 'Reinweißer Marmorstab, umhüllt von einem weichen gelben Licht, das Ihre Hände mit einem göttlichen spirituellen Licht erfüllt.'
+      en: 'Item Description'
     }
   },
 
   {
-    name       : {$$: 'Glade' },
+    name       : {$$: 'Holy Cricket'},
     description: {
-      en: 'An eagle, noble and valiant, carved from the holy wood of an ancient tree.',
-      ro: 'Un vultur, nobil și viteaz, sculptat din lemnul Sfânt al copacilor antici.',
-      pt: 'Uma águia, nobre e valente, esculpida no bosque sagrado das árvores antigas',
-      it: 'Un\'aquila, nobile e coraggiosa, scolpita nel legno sacro degli alberi secolari.',
-      pl: 'Orzeł, szlachetny i odważny, wyrzeźbiony w świętym drewnie starożytnych drzew.',
-      fr: 'Un aigle, noble et vaillant, sculpté dans le bois sacré des arbres anciens.',
-      es: 'Un águila, noble y valiente, tallada en la madera sagrada de los árboles antiguos.',
-      nl: 'Een adelaar, edel en dapper, gesneden uit het heilige bos van de oude bomen.',
-      hi: 'पौराणिक पेड़ों की पवित्र लकड़ी से बनाया गया गरुड़, बहादुर और महान।',
-      ru: 'Благородный и доблестный орёл, вырезанный из священного дерева древних лесов.',
-      bg: 'Орел, благороден и храбър, издълбан от святото дърво на древните дървета.',
-      cs: 'Orel, vznešený a statečný, vytesaný ze svatého dřeva starých stromů.',
-      tl: 'Isang agila, marangal at magiting, na inukit mula sa banal na kahoy ng sinaunang mga puno.',
-      sv: 'En örn, ädla och tapper, snidade från ett gammalt träds heliga trä.',
-      de: 'Ein Adler, edel und tapfer, geschnitzt aus dem heiligen Holz eines alten Baumes.'
+      en: 'Item Description'
     }
   },
 
   {
-    name       : {$$: 'Aqua' },
+    name       : {$$: 'Laughing Skulls'},
     description: {
-      en: 'The power of mysterious aquatic demons follows your footsteps as you lead the way along the dark path of the unknown wonders.',
-      ro: 'Puterea demonilor misterioși ai apei îți urmează pașii în timp ce tu mergi pe calea întunecată a minunilor necunoscute.',
-      pt: 'O poder dos misteriosos demônios aquosos segue seus passos enquanto você lidera o caminho ao longo do caminho escuro das maravilhas desconhecidas.',
-      it: 'Il potere dei misteriosi demoni acquatici segue le tue orme mentre ti guidi lungo il sentiero buio delle meraviglie sconosciute.',
-      pl: 'Moc tajemniczych wodnych demonów podąża Twoimi śladami, prowadząc Cię po ciemnej ścieżce nieznanych cudów.',
-      fr: 'Le pouvoir des mystérieux démons aqueux suit vos traces alors que vous vous engagez sur le chemin sombre des merveilles inconnues.',
-      es: 'El poder de los misteriosos demonios acuosos sigue tus pasos mientras guías el camino por el oscuro sendero de las maravillas desconocidas.',
-      nl: 'De kracht van de mysterieuze waterige demonen volgt je in je voetsporen en leidt je langs het donkere pad van de onbekende wonderen.',
-      ru: 'Сила таинственных водных демонов следует по вашим стопам, как вы направляетесь по темному пути неизвестных чудес.',
-      bg: 'Силата на тайнствените водни демони върви по стъпките ти, докато водиш пътя по тъмния път на непознатите чудеса.',
-      cs: 'Síla tajemných vodních démonů následuje vaše kroky, když se vydáte cestou temnou cestou neznámých divů.',
-      tl: 'Ang kapangyarihan ng mahiwagang aquatic demons ay sumusunod sa iyong mga yapak habang pinapatnubayan mo ang daan sa madilim na landas ng di kilalang mga kababalaghan.',
-      sv: 'Kraften i mystiska vattenlevande demoner följer dina fotspår när du leder vägen längs den mörka vägen för de okända underverken.',
-      hi: 'अंजाने खज़ाने के काले रास्ते पर चलते हुए रहस्यमई दरियाई राक्षसों की ताकतें आपका साथ देंगी।',
-      de: 'Die Macht der mysteriösen Wasserdämonen folgt deinen Schritten, während du den Weg auf dem dunklen Pfad der unbekannten Wunder gehst.'
+      en: 'Item Description'
     }
   },
 
   {
-    name       : {$$: 'Carving of Dimensions' },
+    name       : {$$: 'Hive Mind'},
     description: {
-      en: 'Torn from the patterns of a dimension, this demonic totem submerges the frail minds of your foes under a cloud of ghastly confusion.',
-      ro: 'Smuls din tiparele dimensiunii, acest totem demonic scufundă mințile fragile ale dușmanilor tăi într-un un nor de halucinații îngrozitoare.',
-      pt: 'Arrancado dos padrões da dimensão, este totem demoníaco submerge as mentes frágeis de seus inimigos sob uma nuvem de confusão horríveis.',
-      it: 'Strappato dai modelli della dimensione, questo totem demoniaco sommerge le menti fragili dei vostri nemici sotto una nuvola di confusione spettrali.',
-      pl: 'Wyrwany ze wzorów wymiaru, ten demoniczny totem zanurza wątłe umysły twoich wrogów pod chmurą upiornych dezorientacja.',
-      fr: 'Déchiré des motifs d\'une dimension, ce totem démoniaque submerge les esprits frêles de vos ennemis sous un nuage de désarrois atroces.',
-      es: 'Arrancado de los patrones de la dimensión, este tótem demoníaco sumerge las frágiles mentes de tus enemigos bajo una nube de horribles desconcierto.',
-      nl: 'Verscheurd uit de patronen van de dimensie, dompelt deze demonische totem de zwakke geesten van je vijanden onder in een wolk van afschuwelijke verwarring.',
-      ru: 'Оторванный от узора измерения, этот демонический тотем поглощает слабые умы ваших врагов под облаком страшных галлюцинаций.',
-      bg: 'Изтръгнат от моделите на измерението, този демоничен тотем потапя слабите умове на вашите врагове под облак от ужасни объркване.',
-      cs: 'Tento démonický totem, roztrhaný ze vzorů dimenze, ponoří křehké mysli svých nepřátel pod mrak hrozných zmatek.',
-      tl: 'Napunit mula sa mga pattern ng isang dimensyon, sinasakop ng demonyo totem na ito ang mga mahina ang isip ng iyong mga kaaway sa ilalim ng isang ulap ng malungkot na pagkalito.',
-      sv: 'Sliten från mönstren av en dimension, dämpar denna demoniska totem svåra sinnen hos dina fiender under ett moln av hemsk förvirring.',
-      hi: 'यथार्थ के पहलु से फाड कर निकाला हुआ, यह राक्षसी टोटैम आपके दुश्मनों के कमज़ोर मन को उलझन के भयंकर बादल में डुबा देता है।',
-      de: 'Dieses dämonische Totem, das aus den Mustern einer Dimension gerissen wurde, taucht den zerbrechlichen Verstand deiner Feinde in eine Wolke schrecklicher Verwirrung.'
+      en: 'Item Description'
     }
   },
 
   {
-    name       : {$$: 'Howl' },
+    name       : {$$: 'Nganga\'s Serpent'},
     description: {
-      en: 'An eerie carving of a grim wolf sometimes casting a soft howl-like whisper into the murmurs of the night.',
-      ro: 'O sculptură sinistră a unui lup necruțător, ce uneori scoate un urlet moale ca o șoaptă în murmurul nopții întunecate.',
-      pt: 'Uma escultura assustadora de um lobo sombrio às vezes lançando um sussurro suave como um uivo nos murmúrios da noite escura.',
-      it: 'Un\'inquietante scultura di un lupo cupo che a volte getta un soffice ululato nei mormorii della notte oscura.',
-      pl: 'Niesamowite wyrzeźbienie ponurego wilka, który czasem rzuca miękki, jak gdyby szept w szmery ciemnej nocy.',
-      fr: 'Sculpture sinistre d\'un loup lugubre qui murmure parfois à voix basse dans les murmures de la nuit sombre.',
-      es: 'Una espeluznante escultura de un lobo lúgubre que a veces lanza un suave susurro parecido a un aullido entre los murmullos de la noche oscura.',
-      nl: 'Een griezelig houtsnijwerk van een grimmige wolf die soms een zacht huilend gefluister werpt in het geruis van de donkere nacht.',
-      ru: 'Жуткая вырезка мрачного волка, иногда шепчущего тихим воем в шорох ночи.',
-      bg: 'Зловеща резба на мрачен вълк, който понякога хвърляше мек шепот в шепота на тъмната нощ.',
-      cs: 'Strašidelné řezbářství ponurého vlka, který někdy vrhal do šeru temné noci měkké vytí.',
-      tl: 'Isang nakatatakot na larawang inukit ng isang mabangis na lobo kung minsan ay naghahain ng isang malambot na alulong-tulad ng ibulong sa mga murmurs ng madilim na gabi.',
-      sv: 'En kuslig snurra av en dyster varg som ibland kastar en mjuk, skurkliknande viskning i nattens murmurs.',
-      hi: 'एक भयंकर भेड़िए की आत्माविषयक नक्काशी जो कभी कभी रात के घने अंधेरे में चीखने की आवाजें निकालता है।',
-      de: 'Eine unheimliche Schnitzerei eines grimmigen Wolfes, der manchmal ein leises heulendes Flüstern in die Murmeln der Nacht wirft.'
+      en: ''
     }
   },
 
   {
-    name       : {$$: 'Brethiza' },
+    name       : {$$: 'Sangoma\'s Bones'},
     description: {
-      en: 'Wind and spirit go hand in hand, trapping you in their swift path through the deep valleys of divinity and enlightenment.',
-      ro: 'Vântul și spiritul merg mână în mână, prinzându-te în calea lor rapidă prin văile adânci ale divinității și iluminismului.',
-      pt: 'Vento e espírito andam de mãos dadas, prendendo-o no seu caminho rápido através dos vales profundos da divindade e da iluminação.',
-      it: 'Vento e spirito vanno di pari passo, intrappolandovi nel loro rapido percorso attraverso le profonde valli della divinità e dell\'illuminazione.',
-      pl: 'Wiatr i duch idą ręka w rękę, uwięzienia w ich szybkiej drodze przez głębokie doliny boskości i oświecenia.',
-      fr: 'Le vent et l\'esprit vont de pair, vous piégeant dans leur rapide route à travers les profondes vallées de la divinité et de l\'illumination.',
-      es: 'El viento y el espíritu van de la mano, atrapándolos en su rápido camino a través de los profundos valles de la divinidad y la iluminación.',
-      nl: 'Wind en geest gaan hand in hand en houden je gevangen in hun snelle pad door de diepe valleien van goddelijkheid en verlichting.',
-      ru: 'Ветер и духи движутся вместе, захватывая вас на своем стремительном пути через глубокие долины божественности и просветления.',
-      bg: 'Вятърът и духът вървят ръка за ръка, за да ви уловят по бързия път през дълбоките долини на божествеността и просветлението.',
-      cs: 'Vítr a duch jdou ruku v ruce, chytí vás do jejich rychlé cesty hlubokými údolími božství a osvícení.',
-      tl: 'Ang hangin at espiritu ay nagpapatuloy, na tinutulak ka sa kanilang mabilis na landas sa pamamagitan ng malalim na mga lambak ng kabanalan at paliwanag.',
-      sv: 'Vind och ande går hand i hand och fångar dig i sin snabba väg genom gudomlighetens och upplysningens djupa dalar.',
-      hi: 'प्राण और वायु हाथ में हाथ दे कर चलते हैं, मिलकर देवत्व और प्रबोधन की गहरी घाटियों के रास्तों में आपको फसा देते हैं।',
-      de: 'Wind und Geist gehen Hand in Hand und fangen dich auf ihrem schnellen Weg durch die tiefen Täler der Göttlichkeit und Erleuchtung ein.'
+      en: ''
     }
   }
 
