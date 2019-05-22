ce8ecbfd6e48c2017337704c2995ba04d85f0deb
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-22 22:27:14 +0100

    Update mage.js

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -22,6 +22,7 @@ export default {
     ja: 'メイジ',
     ro: 'Magician'
   },
+
   // Class description, visible during character creation
   description: {
     en: 'Mages deal very heavy large scale Area of Effect damage, while also providing utility through slowing targets with their ice based magic.',
