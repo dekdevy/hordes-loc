ca0a064ebcced2eecbc7c6f3548d7715823a4fa8
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-23 00:21:14 +0100

    Danish update

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -20,7 +20,8 @@ export default {
     el: 'Mage',
     bg: 'Магьосник',
     ja: 'メイジ',
-    ro: 'Magician'
+    ro: 'Magician',
+    da: 'Troldmænd'
   },
 
   // Class description, visible during character creation
@@ -44,6 +45,7 @@ export default {
     el: 'Οι Mages ασχολούνται με πολύ βαρύς μεγάλης κλίμακας ζημιές Περιοχή Εφέ ενώ παράλληλα παρέχουν χρησιμότητα μέσω της επιβράδυνσης των στόχων με τη μαγεία που βασίζεται στον πάγο.',
     bg: 'Маговете се занимават с много силни магии за щета по многобройни цели, като същевременно осигуряват помощ чрез забавяне на целите с помощта на ледената си магия.',
     ja: '魔術師は、非常に重い大規模な効果領域ダメージを与えながら、氷をベースにした魔法でターゲットを減速させることで有用性も提供します。',
-    ro: 'Magicienii cauzează daune considerabile la scară largă, fiind în același timp folositori prin încetinirea țintelor cu ajutorul magiei lor pe bază de gheață.'
+    ro: 'Magicienii cauzează daune considerabile la scară largă, fiind în același timp folositori prin încetinirea țintelor cu ajutorul magiei lor pe bază de gheață.',
+    da: 'Troldmænd skader i stor grad meget areal skade mens de også gør nytte ved at sænke farten på monstre ved at bruge is-baseret magi.'
   }
 }
