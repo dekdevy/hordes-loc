c0cce3bb653a08dfb07049e6af794b83c22983f5
dek <polydek@googlemail.com>
2019-05-24 17:00:57 +0200

    fix line breaks

--- loc/items/amulet.js
+++ loc/items/amulet.js
@@ -2,16 +2,14 @@ export default [
   {
     name       : {$$: 'Rotten Talisman'},
     description: {
-      en: 'Burned ingredients have been twisted and tied together to form an amulet of meager 
-protection. Creating simple amulets like this has been a common survival tactic since ancient times.'
+      en: 'Burned ingredients have been twisted and tied together to form an amulet of meager protection. Creating simple amulets like this has been a common survival tactic since ancient times.'
     }
   },
 
   {
     name       : {$$: 'Wolf\'s Collar'},
     description: {
-      en: 'Collars to keep wolves in servitude are made from a thick leather. Some of them still 
-have chain links connected, but all wolf collars are sturdy. Some collars are imbued with spiritual 
+      en: 'Collars to keep wolves in servitude are made from a thick leather. Some of them still have chain links connected, but all wolf collars are sturdy. Some collars are imbued with spiritual 
 protection'
     }
   },
@@ -19,114 +17,91 @@ protection'
   {
     name       : {$$: 'Bone Necklace'},
     description: {
-      en: 'This necklace consists of bone fragments from a variety of strong creatures. Each bone
-necklace varies in protection based on the origin of the bones.'
+      en: 'This necklace consists of bone fragments from a variety of strong creatures. Each bone necklace varies in protection based on the origin of the bones.'
     }
   },
 
   {
     name       : {$$: 'Werewolf\'s Claw'},
     description: {
-      en: 'Fearsome werewolf claws that were left behind on the bodies of victims were 
-anointed with mystic oils and instilled with spiritual fortitude through meditation. The creators of these 
-amulets hoped that these spiritual protections would one day be used for revenge.
-Eagle’s Talon: When a talon was found, it was considered a gift from the sacred birds'
+      en: 'Fearsome werewolf claws that were left behind on the bodies of victims were anointed with mystic oils and instilled with spiritual fortitude through meditation. The creators of these amulets hoped that these spiritual protections would one day be used for revenge. Eagle’s Talon: When a talon was found, it was considered a gift from the sacred birds'
     }
   },
 
   {
     name       : {$$: 'Eagle\'s Talon'},
     description: {
-      en: 'When a talon was found, it was considered a gift from the sacred birds in the mountain. 
-The monks in the mountains used the talons to produce spiritual amulets to protect themselves from 
-invaders during battle.'
+      en: 'When a talon was found, it was considered a gift from the sacred birds in the mountain. The monks in the mountains used the talons to produce spiritual amulets to protect themselves from invaders during battle.'
     }
   },
 
   {
     name       : {$$: 'Shadow Scarab'},
     description: {
-      en: 'Ancient desert people collected special stones and shaped them into scarabs with 
-strange, dark magic. Many important people in that ancient civilization wore these amulets for stature 
-and protection'
+      en: 'Ancient desert people collected special stones and shaped them into scarabs with strange, dark magic. Many important people in that ancient civilization wore these amulets for stature and protection'
     }
   },
 
   {
     name       : {$$: 'Mystic Shard'},
     description: {
-      en: 'Strange shards mined from the mountains were enhanced by mystics and formed into 
-amulets in order to provide protection and cause fear.'
+      en: 'Strange shards mined from the mountains were enhanced by mystics and formed into amulets in order to provide protection and cause fear.'
     }
   },
 
   {
     name       : {$$: 'Omega Medallion'},
     description: {
-      en: 'Warring theocracies created divine omega symbols as a means for protection. These
-symbols were used to deter attacks, given as protective gifts, and presented to potential religious 
-followers'
+      en: 'Warring theocracies created divine omega symbols as a means for protection. These symbols were used to deter attacks, given as protective gifts, and presented to potential religious followers'
     }
   },
 
   {
     name       : {$$: 'Verstärker'},
     description: {
-      en: 'This amulet has a strange set of overlapping discs that taps into natural energies and 
-amplifies the natural protection of the wearer. Many magic users strive to learn the secrets of these 
-amulets so that they can be reproduced.'
+      en: 'This amulet has a strange set of overlapping discs that taps into natural energies and amplifies the natural protection of the wearer. Many magic users strive to learn the secrets of these amulets so that they can be reproduced.'
     }
   },
 
   {
     name       : {$$: 'Tempest Carcanet'},
     description: {
-      en: 'Metal strands weave in and out of each other, producing electric arcs and giving the 
-appearance of a living amulet. Scholars speculate that these amulets came from a realm that 
-disappeared, but no one seems sure.'
+      en: 'Metal strands weave in and out of each other, producing electric arcs and giving the appearance of a living amulet. Scholars speculate that these amulets came from a realm that disappeared, but no one seems sure.'
     }
   },
 
   {
     name       : {$$: 'Ankh'},
     description: {
-      en: 'Ancient kings had powerful ankh symbols made to protect them in the afterlife. Grave robbers 
-have stolen many of these amulets and sold them to both people and monsters. Each ankh has an eerie 
-glow that belies its true power.'
+      en: 'Ancient kings had powerful ankh symbols made to protect them in the afterlife. Grave robbers have stolen many of these amulets and sold them to both people and monsters. Each ankh has an eerie glow that belies its true power.'
     }
   },
 
   {
     name       : {$$: 'Yggdrasil Pendant'},
     description: {
-      en: 'Twisted pieces of the enormous World Tree, Yggdrasil, were made into amulets of 
-the most sacred kind. Sap seeps out of the amulet and is absorbed by the body for extra protection.'
+      en: 'Twisted pieces of the enormous World Tree, Yggdrasil, were made into amulets of the most sacred kind. Sap seeps out of the amulet and is absorbed by the body for extra protection.'
     }
   },
 
   {
     name       : {$$: 'Dragon\'s Periapt'},
     description: {
-      en: 'This amulet contains elder dragon magic that was bestowed upon young dragons; 
-these amulets gave young dragons a better chance of survival into adolescence. Once the young 
-dragons became old and large enough to survive on their own, the chain broke and the amulet was lost.'
+      en: 'This amulet contains elder dragon magic that was bestowed upon young dragons; these amulets gave young dragons a better chance of survival into adolescence. Once the young dragons became old and large enough to survive on their own, the chain broke and the amulet was lost.'
     }
   },
 
   {
     name       : {$$: 'Lionheart'},
     description: {
-      en: 'This amulet contains a gem from the crown worn by a famous king. The edges of this 
-amulet are ruthlessly sharp and partially smeared with a mysterious substance'
+      en: 'This amulet contains a gem from the crown worn by a famous king. The edges of this amulet are ruthlessly sharp and partially smeared with a mysterious substance'
     }
   },
 
   {
     name       : {$$: 'Phoenix'},
     description: {
-      en: 'The center of this amulet of phoenix feathers contains a small amount of ashes from all of the 
-fallen adventurers that have ever owned it. Without exception, every adventurer to use such an amulet 
-has transcended into legend.'
+      en: 'The center of this amulet of phoenix feathers contains a small amount of ashes from all of the fallen adventurers that have ever owned it. Without exception, every adventurer to use such an amulet has transcended into legend.'
     }
   }
 ]
