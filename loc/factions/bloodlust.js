e14807e0439c4c91a829ba3ed3d41c6f9869ce62
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-25 16:47:11 +0100

    Update bloodlust.js

--- loc/factions/bloodlust.js
+++ loc/factions/bloodlust.js
@@ -5,7 +5,7 @@ export default {
   description: {
     en: 'The Bloodlust horde values freedom and loyalty, offers a place for individualism or even chaos. Their defenses are located in a desert realm called Headless Landing.',
     sv: 'Bloodlust-horden värdesätter frihet och lojalitet och erbjuder utrymme för individualism eller t.o.m. kaos. Deras fäste ligger i ett ökenrike kallat Headless Landing.',
-    ru: 'Орда Кровожадности ценит свободу и лояльность, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном мире, названном Обезглавленной Землей.',
+    ru: 'Орда Кровожадности ценит свободу и лояльность, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном мире, названном Headless Landing.',
     es: 'A la horda Bloodlust le importan la libertad y lealtad. Con su guarida situado en un desierto llamado Headless Landing, ofrecen el lugar perfecto para expresión y caos.',
     fr: 'La faction des Bloodlusts valorise la liberté et la loyauté. Elle offre une place à l\'individualisme et même au chaos. Leurs défenses sont situées dans un royaume désertique appelé Headless Landing.',
     de: 'Die Horde der Bloodlust schätzt Freiheit und Loyalität, bietet einen Ort für Individualismus oder gar Chaos. Ihre Verteidigung befindet sich in einem Wüstenreich namens Headless Landing.',
@@ -16,11 +16,11 @@ export default {
     tr: 'Bloodlust kalabalığı özgürlüğe ve sadakate önem verir, bireyciliğe ve hatta kaosa yer verir. Savunmaları, Başsız İniş denilen bir çöl aleminde bulunur.',
     it: 'L\'orda degli Assetati di Sangue dà estrema importanza alla lealtà ed alla libertà, ed offre un posto ad individualisti e caotici. La loro roccaforte è situata in un regno desertico chiamato Headless Landing.',
     tl: 'Ang halaga ng Bloodlust kuyog ay nag-aalok ng kalayaan at katapatan ay nag-aalok ng isang lugar para sa individualism o kahit kaguluhan. Ang kanilang mga panlaban ay matatagpuan sa isang rehiyong disyerto na tinatawag na Headless Landing.',
-    pl: 'Horda Bloodlust ceni sobie wolność i lojalność, oferuje miejsce dla indywidualizmu, a nawet chaosu. Ich obrona znajduje się w pustynnym królestwie zwanym Landingiem bez głowy.',
+    pl: 'Horda Bloodlust ceni sobie wolność i lojalność, oferuje miejsce dla indywidualizmu, a nawet chaosu. Ich obrona znajduje się w pustynnym królestwie zwanym Headless Landing.',
     ht: 'Branch fanmi an Bloodlust gen ladan valè a nan libète ak lwayote. Li ofri yon kote pou endividyalis e menm dezòd. Defans yo sitiye nan yon peyi dezè ki rele Landing.',
-    el: 'Η ορχήστρα της πρωτοπορίας επιδιώκει την παράδοση τη δομή και την τάξη στην κοινωνία. Το κάστρο τους βρίσκεται σε μια καταπράσινη περιοχή γύρω από το Guardstone.',
-    bg: 'Ордата на Кръвожадните ценни свобода и лоялност, предлага място за индивидуализъм или дори хаос. Техните защити се намират в пустинния свят, наречен Безголовата Земя.',
-    cs: 'Horda Bloodlust hodnotí svobodu a loajalitu, nabízí místo pro individualismus nebo dokonce chaos. Jejich Obrany jsou umístěny v pouštní říši nazvané Bezhlavý Přistání.',
+    el: 'Η ορδή Bloodlust εκτιμά την ελευθερία και την πίστη, προσφέρει ένα χώρο για ατομικισμό ή και χάος.  Οι άμυνές τους βρίσκονται σε μια ερημική περιοχή αποκαλούμενη Headless Landing.',
+    bg: 'Ордата на Кръвожадните ценни свобода и лоялност, предлага място за индивидуализъм или дори хаос. Техните защити се намират в пустинния свят, наречен Headless Landing.',
+    cs: 'Horda Bloodlust hodnotí svobodu a loajalitu, nabízí místo pro individualismus nebo dokonce chaos. Jejich Obrany jsou umístěny v pouštní říši nazvané Headless Landing.',
     ja: 'ブラッドラストの大群は自由と忠誠心を重んじ、個人主義や混沌のための場所を提供します。彼らの防御は、ヘッドレスランディングと呼ばれる砂漠の王国にあります。',
     ro: 'Facțiunea Bloodlust valorizează libertatea și loialitatea, oferă un loc pentru individualism sau chiar haos. Apărările lor sunt situate într-o regiune a deșertului numită Headless Landing.'
 
