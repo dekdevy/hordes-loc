ebc0419f976c43a93c8c1906e232847998f6ac5d
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-25 03:05:40 +0100

    Update blacksmith.js

--- loc/npcs/blacksmith.js
+++ loc/npcs/blacksmith.js
@@ -1,7 +1,7 @@
 export default {
   interactions: [{
     text: {
-      en: 'Upgrading is risky without additional special items like a Blacksmith\'s Blessed Hammer, which can prevent items from breaking during upgrades. Without the hammer, we will be taking a risk. So what can I upgrade for you today?'
+      en: 'Upgrading is risky without additional special items like a Blacksmith’s Blessed Hammer, which can prevent items from breaking during upgrades. Without the hammer, we will be taking a risk. So what can I upgrade for you today?'
     },
     choices: [{
       en: 'blacksmith open interaction'
