5064b8a2c10687ba1de53a8465e979a1e066d365
dek <polydek@googlemail.com>
2019-05-25 18:54:23 +0200

    Update stats.js

--- loc/ui/stats.js
+++ loc/ui/stats.js
@@ -8,6 +8,31 @@ export default {
   },
 
   array: [
+
+    {
+      en: 'Strength'
+    },
+
+    {
+      en: 'Stamina'
+    },
+
+    {
+      en: 'Dexterity'
+    },
+
+    {
+      en: 'Intelligence'
+    },
+
+    {
+      en: 'Wisdom'
+    },
+
+    {
+      en: 'Luck'
+    },
+
     {
       en: 'HP'
     },
@@ -65,53 +90,7 @@ export default {
     },
 
     {
-      en: 'Dexterity'
-    },
-
-    {
-      en: 'Strength'
-    },
-
-    {
-      en: 'Intelligence'
-    },
-
-    {
-      en: 'Wisdom'
-    },
-
-    {
-      en: 'Stamina'
-    },
-
-    {
-      en: 'Luck'
-    },
-
-    {
       en: 'Bag Slots'
     }
   ]
 }
-
-// _STAT_HP = 0
-// _STAT_MP = 1
-// _STAT_HP_REG5 = 2
-// _STAT_MP_REG5 = 3
-// _STAT_DMG_MIN = 4
-// _STAT_DMG_MAX = 5
-// _STAT_DEFENSE = 6
-// _STAT_BLOCK = 7
-// _STAT_EVADE = 8
-// _STAT_ACCURACY = 9
-// _STAT_CRITICAL = 10
-// _STAT_MOVESPEED = 11
-// _STAT_HASTE = 12
-// _STAT_ATTACKSPEED = 13
-// _STAT_DEXTERITY = 14
-// _STAT_STRENGTH = 15
-// _STAT_INTELLIGENCE = 16
-// _STAT_WISDOM = 17
-// _STAT_STAMINA = 18
-// _STAT_LUCK = 19
-// _STAT_SLOT = 20
