249d86ac1803c7f210cbe9e11adc6cc7e33958db
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-26 21:14:11 +0100

    French update

--- loc/npcs/blacksmith.js
+++ loc/npcs/blacksmith.js
@@ -1,7 +1,8 @@
 export default {
   interactions: [{
     text: {
-      en: 'Upgrading is risky without additional special items like a Blacksmith’s Blessed Hammer, which can prevent items from breaking during upgrades. Without the hammer, we will be taking a risk. So what can I upgrade for you today?'
+      en: 'Upgrading is risky without additional special items like a Blacksmith’s Blessed Hammer, which can prevent items from breaking during upgrades. Without the hammer, we will be taking a risk. So what can I upgrade for you today?',
+      fr: 'C'est dangereux d'améliorer ton équipement sans objets spéciaux, comme un Blacksmith's Blessed Hammer, qui peut éviter ton équipement de casser pendant que tu les améliorations. Sans l'Hammer, tu cours un risque. Alors, que veux-tu que je t'améliore aujourd'hui?'
     },
     choices: [{
       en: 'blacksmith open interaction'
