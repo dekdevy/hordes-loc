b8fd6c9f935c60261edac0317408fa5dacf91eb8
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-05-27 09:48:46 +0100

    Danish Hammer -> English Hammer

--- loc/npcs/blacksmith.js
+++ loc/npcs/blacksmith.js
@@ -3,7 +3,7 @@ export default {
     text: {
       en: 'Upgrading is risky without additional special items like a Blacksmith’s Blessed Hammer, which can prevent items from breaking during upgrades. Without the hammer, we will be taking a risk. So what can I upgrade for you today?',
       fr: 'C\'est dangereux d\'améliorer ton équipement sans objets spéciaux, comme un Blacksmith\'s Blessed Hammer, qui peut éviter ton équipement de casser pendant que tu les améliorations. Sans l\'Hammer, tu cours un risque. Alors, que veux-tu que je t\'améliore aujourd\'hui?',
-      da: 'At opgradere er risikabelt uden specielle ting som smedens velsignet hammer, som kan forhindre ting i at gå i stykker under opgraderinger. Uden hammeren tager vi en risiko. Så hvad kan jeg opgradere for dig i dag? '
+      da: 'At opgradere er risikabelt uden specielle ting som Blacksmith's Blessed Hammer, som kan forhindre ting i at gå i stykker under opgraderinger. Uden hammeren tager vi en risiko. Så hvad kan jeg opgradere for dig i dag? '
     },
     choices: [{
       en: 'blacksmith open interaction'
