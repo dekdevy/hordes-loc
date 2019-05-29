836ab8a48ede7e473583f43ea7897277571648cc
dek <polydek@googlemail.com>
2019-05-29 15:03:06 +0200

    Update book.js

--- loc/items/book.js
+++ loc/items/book.js
@@ -1,5 +1,12 @@
 // --- DO NOT TRANSLATE UNTIL END OF BETA AND SKILLS HAVE BEEN DESIGNED ---
 export default [{
+  name: {$$: 'Melee attack'},
+
+  description: {
+    en: 'Perform automatic melee attacks with your weapon.'
+  }
+},
+{
   name: {$$: 'Slash'},
 
   description: {
