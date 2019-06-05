8c93e2e700ed2a8491a7b9e7ce834575ca3fe090
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-06-05 11:45:43 +0100

    Update merchant.js

--- loc/npcs/merchant.js
+++ loc/npcs/merchant.js
@@ -37,7 +37,8 @@ export default {
       bg: 'Да, покажи ми нещата за продажба..',
       cs: 'Ano, ukaž mi věci na prodej.',
       ja: 'はい、商品を見せてください。',
-      sv: 'Ja, visa mig objekten till salu.'
+      sv: 'Ja, visa mig objekten till salu.',
+      da: 'Ja, vis mig varerne til salg.'
     }]
   }]
 }
