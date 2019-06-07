4d810bf93a53a61522a779ad69559e3cb126c857
dek <polydek@googlemail.com>
2019-06-07 21:58:19 +0200

    death loc

---
+++ loc/ui/death.js
@@ -0,0 +1,13 @@
+export default {
+  death: {
+    en: 'You have died'
+  },
+
+  deathmsg: {
+    en: 'Press the button to be resurrected at the nearest conjurer.'
+  },
+
+  respawn: {
+    en: 'Respawn'
+  }
+}
