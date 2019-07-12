2bb68148e0e48113d3ab4769f06265bcaaa2c123
Lasui <52349863+Lasui@users.noreply.github.com>
2019-07-12 10:49:34 +0100

    portuguese update

--- loc/classes/warrior.js
+++ loc/classes/warrior.js
@@ -33,7 +33,7 @@ export default {
     fr: 'Les guerriers sont conçus pour encaisser plus de monstres que n\'importe quelle autre classe en ayant une défense élevée grâce à leurs équipements (Boucliers) et des buffs défensifs puissants.',
     hi: 'सर्वश्रेष्ठ रक्षा उपकरण और ताक़तवर कवच के उपयोग से सारी श्रेणियो में योधा सबसे अधिक मार रोक सकते हैं।',
     ko: '전사들은 장비 (방패)와 강력한 방어 숙련자를 통해 높은 방어력을 가짐으로써 다른 클래스보다 더 많은 몬스터를 견딜수 있습니다.',
-    pt: 'Os guerreiros são construídos para resistir a mais monstros do que qualquer outra classe, tendo alta defesa através de equipamentos (Escudos) e fortes buffs defensivos.',
+    pt: 'Os guerreiros são feitos de forma a resistir a mais monstros do que qualquer outra classe, tendo alta defesa através de equipamentos (Escudos) e fortes buffs defensivos.',
     nl: 'Krijgers zijn gebouwd nemenmeer schade dan enige andere klasse door het hebben van hoge verdediging door middel van apparatuur (Schilden) en sterke defensieve status.',
     tl: 'Ang mga Warrior ay binuo upang tangke ng higit pang mga monsters kaysa sa anumang iba pang mga klase sa pamamagitan ng pagkakaroon ng mataas na pagtatanggol sa pamamagitan ng kagamitan (Shields) at malakas na defensive buffs.',
     tr: 'Savaçlılar güçlü defansif ekipmanları (Kalkanlar) ve defansif güçlendirmeleri ile canavarları diğer sınıflardan daha çok üstüne çeker.',
