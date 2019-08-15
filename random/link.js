198456be6ec14f286d1f520ec63734749a0a65aa
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-08-15 17:53:20 +0100

    Update link.js

--- random/link.js
+++ random/link.js
@@ -3,11 +3,18 @@ import { rndArray } from 'core/math/rnd.js'
 // Words for the random party link generator.
 // The format is "adjective + noun + of + goal".
 
-const adjectives = []
+const adjectives = ['epic', 'rare', 'uncommon', 'common', 'trashy', 'magical', 'poisining', 'mystical', 'rusty', 'insane',
+  'smelly', 'moisturising', 'hilarious', 'sad', 'depressing', 'frozen', 'fresh', 'fast', 'slow', 'mean', 'nice', 'cold',
+  'hot', 'broken', 'refurbished', 'repaired', 'spooky', 'glowing', 'dying', 'furious', 'thunderous', 'glorious', 'impish',
+  'shiny', 'orcish', 'elven', 'dwarven', 'human', 'ghoulish', 'ghostly', 'terrifying', 'radiant', 'sparkling', 'wet',
+  'valiant']
 
-const nouns = []
+const nouns = ['sword', 'axe', 'potion', 'hammer', 'mace', 'coin', 'bag', 'archer', 'warrior', 'mage', 'unicorn',
+  'deer', 'dog', 'cat', 'staff']
 
-const goals = []
+const goals = ['slaying', 'wisdom', 'destruction', 'death', 'healing', 'luck', 'misfortune', 'rage', 'joy',
+  'leveling', 'vengance', 'resurrection', 'mastery', 'loss', 'flames', 'fire', 'frost', 'glory', 'war', 'sprinting',
+  'doom', 'love', 'archery', 'hunting', 'peace', 'failure', 'success', 'victory', 'denial', 'agility', 'despair']
 
 export const generate = ()=>{
   // generates a random party invite link name
