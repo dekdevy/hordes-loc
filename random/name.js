f6c64aa08250303e3a8b69bf92a042059c5b7158
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-08-15 13:30:17 +0100

    Update name.js

--- random/name.js
+++ random/name.js
@@ -3,7 +3,9 @@ import { rndArray, rndi } from 'core/math/rnd.js'
 // Syllables used for random player/monster name generation.
 // These are not part of the localisation, no more syllables are needed.
 
-const syllables = []
+const syllables = ['bow', 'hammer', 'sword', 'staff', 'doom', 'flesh', 'eye', 'soul', 'claw', 'lynx', 'vang', 'blood',
+  'lust', 'wing', 'strong', 'gem', 'rune', 'dhai', 'morgh', 'craft', 'owl', 'reap', 'glob', 'ice','flame', 'blade',
+  'ash', 'fang', 'scar', 'life', 'smith', 'drakh', 'bat', 'kar', 'daug', 'ghal']
 
 export const generate = (minSyl = 2, maxSyl = 6, maxLen = 8)=>{
   // generates a random name with a variable max length / syllable amount
