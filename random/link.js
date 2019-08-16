741cefb8fff6dca6228b358c1cd68abf011b70d9
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-08-16 13:43:07 +0100

    Reducing adjectives.

--- random/link.js
+++ random/link.js
@@ -4,27 +4,25 @@ import { rndArray } from 'core/math/rnd.js'
 // The format is "adjective + noun + of + goal".
 
 const adjectives = ['epic', 'rare', 'uncommon', 'common', 'trashy', 'magical', 'mystical', 'rusty', 'insane',
-  'smelly', 'hilarious', 'sad', 'depressed', 'frozen', 'dusty',
-  'broken', 'repaired', 'spooky', 'glowing', 'furious', 'thunderous', 'glorious',
+  'smelly', 'hilarious', 'sad', 'depressed', 'frozen', 'dusty', 'nepharious','swinging', 'parrying', 
+  'broken', 'repaired', 'spooky', 'glowing', 'furious', 'thunderous', 'glorious', 'swearing', 
   'shiny', 'orcish', 'elven', 'dwarven', 'human', 'terrifying', 'radiant', 'sparkling',
-  'valiant', 'brilliant', 'royal', 'legendary', 'divine', 'gloomy',
-  'funny', 'mighty', 'wild', 'hallowed', 'heavy', 'sly', 'sneaky', 'stretchy', 'slippery',
-  'young', 'ancient', 'elderly', 'poetic', 'oily', 'ageing', 'noble', 'bleeding', 'crude',
-  'growing', 'vile', 'disgusting', 'holy', 'radioactive', 'speedy', 'bright', 'possessed', 'teleporting', 'ghastly',
-  'augmeneted', 'embarrassing', 'cruel', 'cunning', 'glamorous', 'intricate', 'imperial', 'lethal',
-  'jolly', 'religious', 'drunk', 'elite', 'charismatic', 'agile', 'strong', 'stealthy', 'deadly', 'flying', 'brave', 'sturdy',
+  'valiant', 'brilliant', 'royal', 'legendary', 'divine', 'gloomy', 'scaly', 'massive', 'awkward'
+  'funny', 'mighty', 'wild', 'hallowed', 'heavy', 'sly', 'sneaky', 'stretchy', 'slippery', 'emerald', 'famous',
+  'young', 'ancient', 'elderly', 'poetic', 'oily', 'ageing', 'noble', 'bleeding', 'crude', 'ghastly', 'grim', 
+  'growing', 'vile', 'disgusting', 'holy', 'radioactive', 'speedy', 'bright', 'possessed', 'teleporting',
+  'augmeneted', 'embarrassing', 'cruel', 'cunning', 'glamorous', 'intricate', 'imperial', 'lethal', 'sturdy',
+  'jolly', 'religious', 'drunk', 'elite', 'charismatic', 'agile', 'strong', 'stealthy', 'deadly', 'flying', 'brave',
   'corrupted', 'decaying', 'soothing', 'smiling', 'butchering', 'smoking', 'spritual', 'godly', 'pixelated', 'grunting',
-  'polished', 'charming', 'hairy', 'berserk', 'mischievous', 'resurrecting', 'disguised', 'ragged', 'stinging', 'noisy',
+  'hairy', 'berserk', 'mischievous', 'resurrecting', 'ragged', 'stinging', 'noisy',
   'overpowered', 'murderous', 'victorious', 'violent', 'bloody', 'immortal','invisible', 'slicing',
   'piercing', 'poking', 'lazy', 'hot', 'warm', 'abusive', 'acrobatic', 'accursed', 'bewitched', 'seething', 
   'pale', 'singing', 'whispering', 'revolting', 'ripe', 'romantic', 'catastrophic', 'mad', 'stinking', 'puzzling'
   'sleek', 'skulking', 'smacking', 'spellbound', 'explosive', 'fantastic', 'hazardous', 'praying','frosted',
   'loud', 'evil', 'silken', 'humorous', 'medieval', 'metal', 'unlucky', 'chaotic', 'hollow', 'naive', 'swift', 'oozing',
-  'stingy', 'tipsy', 'priceless', 'tribal', 'thieving', 'slav', 'fake', 'coldforged', 'lavish', 'abandoned', 'gorgeous',
-  'cheap', 'expensive', 'benevolent', 'flourishing', 'supreme', 'superior', 'woody', 'peaceful'
-  'diamond', 'magnificent', 'bloodhungry', 'tearing', 'bursting', 'furry', 'powerful', 'dangerous', 'awkward',
-  'beautiful', 'sleeping', 'hideous', 'awful', 'humble', 'itchy', 'grim', 'emerald', 'famous', 'dark'
-  'swearing', 'swinging', 'oniony', 'parrying', 'nepharious', 'scaly', 'icy', 'snowy', 'spherical', 'massive']
+  'stingy', 'tipsy', 'priceless', 'tribal', 'slav', 'fake', 'coldforged', 'lavish', 'abandoned', 'gorgeous',
+  'cheap', 'supreme', 'peaceful', 'diamond', 'magnificent', 'bloodhungry', 'powerful', 'dangerous',
+  'beautiful', 'sleeping', 'hideous']
 
 const nouns = ['sword', 'axe', 'potion', 'hammer', 'mace', 'coin', 'bag', 'archer', 'warrior', 'mage', 'unicorn',
   'deer', 'dog', 'cat', 'staff', 'venom']
