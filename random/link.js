9cd4a0e6bb859624d8385f3ba83dcda2d4f6a3d8
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-08-24 09:17:25 +0100

    Atrocious grammar mistakes which need immediate help.

--- random/link.js
+++ random/link.js
@@ -25,7 +25,7 @@ const adjectives = ['epic', 'rare', 'uncommon', 'common', 'trashy', 'magical', '
 
 const nouns = ['sword', 'axe', 'potion', 'hammer', 'mace', 'coin', 'bag', 'archer', 'warrior', 'mage', 'unicorn',
   'deer', 'dog', 'cat', 'staff', 'venom', 'snake', 'literature', 'seed', 'fortress', 'lair', 'river', 'star',
-  'bear', 'banana', 'apple', 'boots', 'armor', 'helmet', 'tower', 'forge', 'castle', 'flesh', 'food', 'relic',
+  'bear', 'banana', 'apple', 'boots', 'armour', 'helmet', 'tower', 'forge', 'castle', 'flesh', 'food', 'relic',
   'town', 'smith', 'craftsman', 'bow', 'wolf', 'pear', 'orge', 'goblin', 'orc', 'skeleton', 'spider', 'boar', 'bone',
   'raid', 'group', 'party', 'owl', 'sparrow', 'crocodile', 'god', 'ant', 'lion', 'treasure', 'gem', 'adventure',
   'crown', 'swamp', 'forest', 'breakfast', 'meal', 'shaman', 'winter', 'summer', 'spring', 'gold', 'legacy', 'sea',
@@ -35,7 +35,7 @@ const nouns = ['sword', 'axe', 'potion', 'hammer', 'mace', 'coin', 'bag', 'arche
   'country', 'wasteland', 'behemoth', 'lich', 'gavel', 'club', 'morningstar', 'heart', 'tauren', 'manuscript',
   'pearl', 'ruby', 'python', 'nugget', 'obsidian', 'ghost', 'gauntlet', 'greaves', 'bracer', 'slingshot', 'sun',
   'voyage', 'odyssey', 'stronghold', 'fork', 'blight', 'book', 'script', 'scroll', 'leather', 'feather', 'spear',
-  'assault', 'throne', 'mockingbird', 'bowman', 'raider', 'scout', 'officer', 'lieutenant', 'general', 'armorer',
+  'assault', 'throne', 'mockingbird', 'bowman', 'raider', 'scout', 'officer', 'lieutenant', 'general', 'armourer',
   'ninja', 'assassin', 'tea', 'demon', 'angel', 'tooth', 'song', 'storm', 'warbow', 'missionary', 'prophet', 'tavern',
   'skirmish', 'mutant', 'warlock', 'beer', 'hurricane', 'salamander', 'hostage', 'bandit', 'outlaw', 'blacksmith',
   'earthquake', 'chainmail', 'cobbler', 'servant', 'loot', 'sabre', 'scythe', 'squire', 'journeyman', 'dragon',
@@ -52,9 +52,9 @@ const goals = ['slaying', 'wisdom', 'destruction', 'death', 'healing', 'luck', '
   'heresy', 'drifting', 'slipping', 'stumbling', 'falling', 'swordsmanship', 'bravery', 'slavery', 'crime', 'advice',
   'enlightenment', 'cowardice', 'blasphemy', 'ridiculing', 'hysteria', 'trolling', 'corruption', 'forging', 'skill',
   'survival', 'creativity', 'fame', 'wealth', 'prosperity', 'fighting', 'marriage', 'friendship', 'shame', 'scorn',
-  'magic', 'might', 'guidance', 'cruelty', 'kindness', 'youth', 'elderness', 'poetry', 'lust', 'bowmanship', 'valor',
+  'magic', 'might', 'guidance', 'cruelty', 'kindness', 'youth', 'elderness', 'poetry', 'lust', 'bowmanship', 'valour',
   'vengeance', 'experience', 'dominance', 'bliss', 'vanishing', 'success', 'failure', 'positivity', 'negativity',
-  'virtue', 'honor', 'foolishness', 'focusing', 'theft']
+  'virtue', 'honour', 'foolishness', 'focusing', 'theft']
 
 export const generate = ()=>{
   // generates a random party invite link name
