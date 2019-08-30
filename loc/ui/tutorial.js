87f145888e5e9b6059bca35a3dcdd9cad3164674
dek <polydek@googlemail.com>
2019-08-30 20:28:04 +0200

    tutorial steps 20-38

--- loc/ui/tutorial.js
+++ loc/ui/tutorial.js
@@ -21,10 +21,25 @@ export default {
     {en: 'You have leveled up and earned stat points. Open your character panel again.'},
     {en: 'Stat points can be used to increase your primary stats. Hover over the buttons to preview their effects, then use your stat points.'},
     {en: 'Next, we will teach you how to learn new skills. First, level up until you unlock a new skill.'},
-    {en: 'A new skill is available. You need to use books to learn new skills. Walk back and talk to the trader.'},
-    {en: 'The trader will sell books for Lv. 1 skills. Find the skill you unlocked and buy the book for it.'},
+    {en: 'A new skill is available. You need to use books to learn new skills.'},
+    {en: 'Collect some items and coins. You need 32 coins for your first book. You can sell items to the trader as well.'},
+    {en: 'You have enough coins. Talk to the trader in your base and buy your new book by right clicking it.'},
     {en: 'Once you have the book, open your inventory, right-click the book and press "use item" to learn it.'},
     {en: 'Skillful. Open your skill panel (K) to see an overview of your skills.'},
-    {en: 'You can click and drag skills into your skillbar, but we also do this automatically for you because we\'re nice.'}
+    {en: 'Here you can see your skills. Hover over them for additional information.'},
+    {en: 'You can click and drag a skill icon into your skillbar. We already did it for you, but try it anyways!'},
+    {en: 'You have learned a lot. You\'re almost ready to start your adventure.'},
+    {en: 'One last thing. Grouping up with others will help you on your path to greatness.'},
+    {en: 'Hordes has a party system. A party of players will share experience equally.'},
+    {en: 'You can create a party by clicking on the button in the upper left corner.'},
+    {en: 'Invite players to your party by right-clicking on their unitframe, or use the command /partyinvite.'},
+    {en: 'No, you don\'t have to. But with the power of friendship, you can do more damage.'},
+    {en: 'Friendship, amongst many other things, can be found in the chat. Press Enter.'},
+    {en: 'Type in your greatest battle shout. Then press enter to send your love letter to the world.'},
+    {en: 'Okay. One, final last thing, I promise. See that cogwheel button in the upper right corner? The game settings.'},
+    {en: 'You can change many things here, such as the language, or reducing the graphics.'},
+    {en: 'Alright, close the settings again.'},
+    {en: 'Thats it. You did it. You finally are ready to slay. Your faction depends on you.'},
+    {en: 'Good luck, champion. I\'ll be back.'}
   ]
 }
