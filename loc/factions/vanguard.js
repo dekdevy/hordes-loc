522e436d4a139f32d2e52955118cc16872d99df1
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-09-10 19:29:42 +0100

    "Guardstonr" -> "Guardstone"

--- loc/factions/vanguard.js
+++ loc/factions/vanguard.js
@@ -1,5 +1,5 @@
 export default {
-  // English Only
+  // English Only.
   name: {
     $$: 'Vanguard'
   },
@@ -10,7 +10,7 @@ export default {
     sv: 'Vanguard-horden eftersträvar ett ordnat och traditionsdrivet samhälle. Deras slott ligger i ett frodigt område kring Guardstone.',
     ru: 'Орда Vanguard стремится к традиции, структуру и порядок в обществе. Их крепость может быть найдена в пышной зеленой зоне около Guardstone.',
     es: 'La horda Vanguard busca la tradición, la estructura y el orden en la sociedad. Su castillo se puede encontrar en una desarrollada área verde alrededor de Guardstone.',
-    hi: 'Vanguard गिरोह समाज में परंपरा, संरचना और व्यवस्था चाहता है। उनका महल Guardstonr के आसपास हरे-भरे क्षेत्र में पाया जा सकता है।',
+    hi: 'Vanguard गिरोह समाज में परंपरा, संरचना और व्यवस्था चाहता है। उनका महल Guardstone के आसपास हरे-भरे क्षेत्र में पाया जा सकता है।',
     de: 'Die Vanguard Horde sucht Tradition, Struktur und Ordnung in der Gesellschaft. Ihre Burg befindet sich in einer üppigen grünen Umgebung rund um Guardstone.',
     fr: 'La faction des Vanguard respecte la tradition, la structure et l\'ordre dans la société. Leur palais se trouve dans une zone verdoyante autour de Guardstone.',
     ko: 'Vanguardの大群は、社会における伝統、構造、そして秩序を求めています。 彼らの城はGuardstone周辺の緑豊かな地域にあります。',
