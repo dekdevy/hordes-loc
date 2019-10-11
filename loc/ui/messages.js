0e706cf841287d32a077649eb4bb631280209e04
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-10-11 18:37:32 +0100

    Russian update for messages.js

--- loc/ui/messages.js
+++ loc/ui/messages.js
@@ -1,36 +1,47 @@
 export default {
   clanMemberJoin: {
-    en: '$1 has joined your clan.'
+    en: '$1 has joined your clan.',
+    ru: '$1 вступил в ваш клан'
   },
   clanMemberInvite: {
-    en: '$1 has been invited to your clan.'
+    en: '$1 has been invited to your clan.',
+    ru: '$1 был приглашен в ваш клан'
   },
   clanMemberLeave: {
-    en: '$1 has left your clan.'
+    en: '$1 has left your clan.',
+    ru: '$1 покинул ваш клан'
   },
   clanMemberApply: {
-    en: '$1 has applied to join your clan.'
+    en: '$1 has applied to join your clan.',
+    ru: '$1 подал заявку на вступление в ваш клан'
   },
   clanMemberRolePromote: {
-    en: '$1 was promoted in clan role.'
+    en: '$1 was promoted in clan role.',
+    ru: '$1 был повышен в должности'
   },
   clanMemberRoleDemote: {
-    en: '$1 was demoted in clan role.'
+    en: '$1 was demoted in clan role.',
+    ru: '$1 был понижен в должности'
   },
   clanKickOther: {
-    en: '$1 was kicked from your clan.'
+    en: '$1 was kicked from your clan.',
+    ru: '$1 Боб был выгнан из вашего клана'
   },
   clanKickYou: {
-    en: 'You were kicked from your clan.'
+    en: 'You were kicked from your clan.',
+    ru: '$1 выгнали из вашего клана'
   },
   partyMemberInvite: {
-    en: '$1 has been invited by $2 to join your party.'
+    en: '$1 has been invited by $2 to join your party.',
+    ru: '$1 пригласил $2 вступить в вашу Party'.
   },
   partyMemberJoin: {
-    en: '$1 has joined your party.'
+    en: '$1 has joined your party.',
+    ru: '$1 вступил в вашу Party'
   },
   partyMemberLeave: {
-    en: '$1 has left your party.'
+    en: '$1 has left your party.',
+    ru: '$1 вышел из Party'
   },
   partyInvitationDecline: {
     en: '$1 has declined to join your party.'
