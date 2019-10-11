2a03cbaf49260b59bb3290ec8a82f07858f15b08
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-10-11 18:47:01 +0100

    Russian update for messages.js

--- loc/ui/messages.js
+++ loc/ui/messages.js
@@ -1,73 +1,82 @@
 export default {
   clanMemberJoin: {
     en: '$1 has joined your clan.',
-    ru: '$1 вступил в ваш клан'
+    ru: '$1 вступил в ваш клан.'
   },
   clanMemberInvite: {
     en: '$1 has been invited to your clan.',
-    ru: '$1 был приглашен в ваш клан'
+    ru: '$1 был приглашен в ваш клан.'
   },
   clanMemberLeave: {
     en: '$1 has left your clan.',
-    ru: '$1 покинул ваш клан'
+    ru: '$1 покинул ваш клан.'
   },
   clanMemberApply: {
     en: '$1 has applied to join your clan.',
-    ru: '$1 подал заявку на вступление в ваш клан'
+    ru: '$1 подал заявку на вступление в ваш клан.'
   },
   clanMemberRolePromote: {
     en: '$1 was promoted in clan role.',
-    ru: '$1 был повышен в должности'
+    ru: '$1 был повышен в должности клана.'
   },
   clanMemberRoleDemote: {
     en: '$1 was demoted in clan role.',
-    ru: '$1 был понижен в должности'
+    ru: '$1 был понижен в должности клана.'
   },
   clanKickOther: {
     en: '$1 was kicked from your clan.',
-    ru: '$1 Боб был выгнан из вашего клана'
+    ru: '$1 Боб был выгнан из вашего клана.'
   },
   clanKickYou: {
     en: 'You were kicked from your clan.',
-    ru: '$1 выгнали из вашего клана'
+    ru: '$1 выгнали из вашего клана.'
   },
   partyMemberInvite: {
     en: '$1 has been invited by $2 to join your party.',
-    ru: '$1 пригласил $2 вступить в вашу Party'.
+    ru: '$1 пригласил $2 вступить в вашу Party.'.
   },
   partyMemberJoin: {
     en: '$1 has joined your party.',
-    ru: '$1 вступил в вашу Party'
+    ru: '$1 вступил в вашу Party.'
   },
   partyMemberLeave: {
     en: '$1 has left your party.',
-    ru: '$1 вышел из Party'
+    ru: '$1 вышел из Party.'
   },
   partyInvitationDecline: {
-    en: '$1 has declined to join your party.'
+    en: '$1 has declined to join your party.',
+    ru: '$1 отказался вступать в вашу Party.'
   },
   partyMemberPromote: {
-    en: '$1 has been promoted in party role.'
+    en: '$1 has been promoted in party role.',
+    ru: '$1 был повышен в должности Party.'
   },
   partyMemberDemote: {
-    en: '$1 has been demoted in party role.'
+    en: '$1 has been demoted in party role.',
+    ru: '$1 был понижен в должности Party'
   },
   partyInviteLink: {
-    en: '$1 has created a party invitation link: $2.'
+    en: '$1 has created a party invitation link: $2.',
+    ru: '$1 создал линк Party приглашения: $2.'
   },
   partyKickYou: {
-    en: 'You have been kicked from the party by $1.'
+    en: 'You have been kicked from the party by $1.',
+    ru: '$1 выгнал вас из Party.'
   },
   partyKickOther: {
     en: '$1 was kicked from your party by $2.'
+    ru: '$2 выгнал $1 из Party.'
   },
   auctionSold: {
-    en: 'Your merchant auction has been sold and $1 coins have been sent to your stash.'
+    en: 'Your merchant auction has been sold and $1 coins have been sent to your stash.',
+    ru: 'Ваш торговый аукцион был продан, и в ваш сундук было отправлено $1 монет.'
   },
   online: {
-    en: '$1 has come online.'
+    en: '$1 has come online.',
+    ru: '$1 сейчас онлайн.'
   },
   offline: {
-    en: '$1 has gone offline.'
+    en: '$1 has gone offline.',
+    ru: '$1 сейчас офлайн.'
   }
 }
