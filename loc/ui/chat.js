46b0b32626df2ca530fa5be25f5c11ba57154836
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-10-12 19:20:06 +0100

    Dutch chat types.

--- loc/ui/chat.js
+++ loc/ui/chat.js
@@ -31,21 +31,24 @@ export default {
       fr: 'Gain d\'xp',
       es: 'Exp ganado',
       pt: 'Exp Ganho',
-      tr: 'Exp Kazanımı'
+      tr: 'Exp Kazanımı',
+      nl: 'Exp Gekregen'
     },
     {
       en: 'Target Change',
       ru: 'Смена Цели',
       fr: 'Changement de Cible',
       es: 'Cambia objetivo',
-      pt: 'Mudança de Alvo'
+      pt: 'Mudança de Alvo',
+      nl: 'Verander Doelwit'
     },
     {
       en: 'Cast',
       ru: 'Каст',
       fr: 'Cast',
       es: 'Convocar',
-      pt: 'Lançar'
+      pt: 'Lançar',
+      nl: 'Uitspreken'
     },
     {
       en: 'Damage',
@@ -53,7 +56,8 @@ export default {
       fr: 'Damage',
       es: 'Daño',
       pt: 'Dano',
-      tr: 'Hasar'
+      tr: 'Hasar',
+      nl: 'Schade'
     },
     {
       en: 'Heal',
@@ -61,7 +65,8 @@ export default {
       fr: 'Heal',
       es: 'Curar',
       pt: 'Curar',
-      tr: 'İyileşme'
+      tr: 'İyileşme',
+      nl: 'Genezen:
     },
     {
       en: 'Death',
@@ -69,42 +74,48 @@ export default {
       fr: 'Death',
       es: 'Muerto',
       pt: 'Morte',
-      tr: 'Ölüm'
+      tr: 'Ölüm',
+      nl: 'Dood'
     },
     {
       en: 'Level Up',
       ru: 'Повышения Уровня',
       fr: 'Level Up',
       es: 'Subir nivel',
-      pt: 'Subir de nível'
+      pt: 'Subir de nível',
+      nl: 'Level Omhoog'
     },
     {
       en: 'Items',
       ru: 'Предметы',
       fr: 'Items',
       es: 'Items',
-      pt: 'Items'
+      pt: 'Items',
+      nl: 'Voorwerpen'
     },
     {
       en: 'Auction',
       ru: 'Аукцион',
       fr: 'Enchères',
       es: 'Subasta',
-      pt: 'Leilão'
+      pt: 'Leilão',
+      nl: 'Veiling'
     },
     {
       en: 'Pick Gold',
       ru: 'Собранное Золото',
       fr: 'Gold Ramassé',
       es: 'Oro agarrado',
-      pt: 'Pegar Ouro'
+      pt: 'Pegar Ouro',
+      nl: 'Goud Oprapen'
     },
     {
       en: 'Spent Gold',
       ru: 'Потраченное Золото',
       fr: 'Gold Utilisé',
       es: 'Oro gastado',
-      pt: 'Ouro gasto'
+      pt: 'Ouro gasto',
+      nl: 'Goud Besteden'
     },
     {
       en: 'Fame Gain',
@@ -112,7 +123,8 @@ export default {
       fr: 'Gain de Gloire',
       es: 'Fama ganada',
       pt: 'Fama ganha',
-      tr: 'Şöhret Kazancı'
+      tr: 'Şöhret Kazancı',
+      nl: 'Beroemdheid Gekregen'
     }
   ]
 }
