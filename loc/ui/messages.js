3ee4f1152665641319e02f3d2b200923f6097dc0
ZillZei <56450011+ZillZei@users.noreply.github.com>
2019-10-12 07:05:51 +0300

    Update messages.js

--- loc/ui/messages.js
+++ loc/ui/messages.js
@@ -1,15 +1,19 @@
 export default {
   clanMemberJoin: {
-    en: '$1 has joined your clan.'
+    en: '$1 has joined your clan.',
+    tr: '$1 klanınıza katıldı.'
   },
   clanMemberInvite: {
-    en: '$1 has been invited to your clan.'
+    en: '$1 has been invited to your clan.',
+    tr: '$1 klanınıza davet edildi.'
   },
   clanMemberLeave: {
-    en: '$1 has left your clan.'
+    en: '$1 has left your clan.',
+    tr: '$1 klanınızdan ayrıldı.'
   },
   clanMemberApply: {
-    en: '$1 has applied to join your clan.'
+    en: '$1 has applied to join your clan.',
+    tr: '$1 klanınıza katılmak için başvurdu.'
   },
   clanMemberRolePromote: {
     en: '$1 was promoted in clan role.'
@@ -18,10 +22,12 @@ export default {
     en: '$1 was demoted in clan role.'
   },
   clanKickOther: {
-    en: '$1 was kicked from your clan.'
+    en: '$1 was kicked from your clan.',
+    tr: '$1 klanınızdan atıldı.'
   },
   clanKickYou: {
-    en: 'You were kicked from your clan.'
+    en: 'You were kicked from your clan.',
+    tr: 'Klanınızdan atıldınız.'
   },
   partyMemberInvite: {
     en: '$1 has been invited by $2 to join your party.'
@@ -54,9 +60,11 @@ export default {
     en: 'Your merchant auction has been sold and $1 coins have been sent to your stash.'
   },
   online: {
-    en: '$1 has come online.'
+    en: '$1 has come online.',
+    tr: '$1 çevrimiçi oldu.'
   },
   offline: {
-    en: '$1 has gone offline.'
+    en: '$1 has gone offline.',
+    tr: '$1 çevrimdışı oldu.'
   }
 }
