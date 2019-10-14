2ff9e283fe0fb5edd1b4085aec87c78adec71818
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-10-14 20:11:24 +0100

    Hungarian Sage response fixed.

--- loc/npcs/sage.js
+++ loc/npcs/sage.js
@@ -7,7 +7,7 @@ export default {
     choices: [{
       en: 'Reset my stat points ( $g$1 ).',
       ru: 'Сброс мои очки статов ( $g$1 ).',
-      hu: 'Statpontok újraszervezése ( $g$1 ).',
+      hu: 'Statpont újraszervezés ( $g$1 ).',
       pt: 'Reinicie os meus pontos de stat ( $g$1 ).',
       fr: 'Réinitialisez mes points d\'aptitudes ( $g$1 ).',
       nl: 'Reset mijn status punten ( $g$1 ).',
