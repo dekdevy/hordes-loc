38182f3b4d68525f5df774a31848e4af53ea9252
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 11:54:39 +0000

    Updated item descriptions.

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -2,91 +2,91 @@ export default [
   {
     name       : {$$: 'Simple Wristbands' },
     description: {
-      en: 'ItemDescription'
+      en: ' These wristbands are leather strips that are wrapped in a simple formation.'
     }
   },
 
   {
     name       : {$$: 'Makeshift Armbands' },
     description: {
-      en: 'ItemDescription'
+      en: 'These armbands were created hastily. The rough battle markings on the sides of the leather do not inspire confidence of the previous owner\'s survival.'
     }
   },
 
   {
     name       : {$$: 'Leather Armlets' },
     description: {
-      en: 'ItemDescription'
+      en: 'Tough leather has been painstakingly crafted into fine armlets. On the side of the leather is the craftsman\'s signature: "Markay\'ak".'
     }
   },
 
   {
     name       : {$$: 'Bone Bracelets' },
     description: {
-      en: 'ItemDescription'
+      en: 'This type of bracelet is ornately crafted with thick monster bones. Many superstitious traditions claim that the wearer of such bracelets become permeated with the deceased monster\'s power; however, others claim that the reuse of bones prevents monsters from resurrecting.'
     }
   },
 
   {
     name       : {$$: 'Iron Vambraces' },
     description: {
-      en: 'ItemDescription'
+      en: 'This type of iron armor has been heavily used in war and remains dirty. The claw marks on the metal seem to be mostly superficial.'
     }
   },
 
   {
     name       : {$$: 'Imbued Bracers' },
     description: {
-      en: 'ItemDescription'
+      en: 'These steel bracers are marked with small runes that imbue extra protection. Imbued bracers are somewhat rare since the creation method of the runes are a secret.'
     }
   },
 
   {
     name       : {$$: 'Ember Cuffs' },
     description: {
-      en: 'ItemDescription'
+      en: 'These cuffs were forged with a special fire that can only be created through magic. In the past, fire elementals taught only a few special blacksmiths the secret to forging ember cuffs.'
     }
   },
 
   {
     name       : {$$: 'Mirror Armlets' },
     description: {
-      en: 'ItemDescription'
+      en: 'Mirrored armlets are made by exceptional blacksmiths with the skills necessary to make reflective armor for damage reduction. This protective quality inspired many Elvish wizards to often experiment on mirrored armlets.'
     }
   },
 
   {
     name       : {$$: 'Golem Fragments' },
     description: {
-      en: 'ItemDescription'
+      en: 'Golem fragments are painstakingly assembled by a small team of magic-using blacksmiths. Every pair of golem fragments is crafted over several hundred days using the husks of rock golems.'
     }
   },
 
   {
     name       : {$$: 'Coldforged Bracers' },
     description: {
-      en: 'ItemDescription'
+      en: 'Bracers such as this are comprised of a rare metal found in the mountains. Blacksmiths use enchanted hammers to forge the bracers without heat in order to retain the magicenhancing properties of the metal.'
     }
   },
 
   {
     name       : {$$: 'Blackstars' },
     description: {
-      en: 'ItemDescription'
+      en: 'These bracers were created by blacksmiths that hammered metal found within black meteorites. So far, the greatest concentration of black meteorites was found after a cataclysm that followed the Second Great War.'
     }
   },
 
   {
     name       : {$$: 'Mobiuses' },
     description: {
-      en: 'ItemDescription'
+      en: 'Ambitious wizards on the edge of madness discovered secrets to dimensional magic and formed bracers that transcend normal magical protection. These abnormal bracers required a special shape to maintain their properties.'
     }
   },
 
   {
     name       : {$$: 'Guardians' },
     description: {
-      en: 'ItemDescription'
+      en: 'Some legends say that these bracers only appear when the realms need a guardian. History shows that terrible tragedies occur shortly thereafter whenever these bracers are found; as such, their appearances are often seen as bad omens.'
     }
   }
 
