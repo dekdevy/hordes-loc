138d0f34dd4f8ce0af6c6347d63f725dc8abfa90
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 16:42:53 +0000

    Russian armour translations.

--- loc/items/armor.js
+++ loc/items/armor.js
@@ -2,77 +2,88 @@ export default [
   {
     name       : {$$: 'Potato Sack' },
     description: {
-      en: 'This is a surprisingly sturdy material that can be used as a protective clothing. The makeshift nature of this material is obvious, so many adventurers choose to replace the potato sack as quickly as possible.'
+      en: 'This is a surprisingly sturdy material that can be used as a protective clothing. The makeshift nature of this material is obvious, so many adventurers choose to replace the potato sack as quickly as possible.',
+      ru: 'Это удивительно прочный материал, который можно использовать в качестве защитной одежды. Самодельная природа этого материала очевидна, так что многие приключеники предпочитают заменить картофельный мешок как можно скорее.'
     }
   },
 
   {
     name       : {$$: 'Faded Garment' },
     description: {
-      en: 'This garment is a family heirloom that has been passed down from one adventuring generation to another.'
+      en: 'This garment is a family heirloom that has been passed down from one adventuring generation to another.',
+      ru: 'Это семейная реликвия, которая передавалась из поколения в поколение.'
     }
   },
 
   {
     name       : {$$: 'Adventurer\'s Tunic' },
     description: {
-      en: 'This tunic is crafted from robust fabrics that are designed to withstand wilderness survival. Some traditions involve giving tunics as gifts to adventurers that are deemed worthy by an organization.'
+      en: 'This tunic is crafted from robust fabrics that are designed to withstand wilderness survival. Some traditions involve giving tunics as gifts to adventurers that are deemed worthy by an organization.',
+      ru: 'Эта туника изготовлена из прочных тканей, которые разработаны, чтобы противостоять выживанию в дикой природе. Некоторые традиции включают в себя вручение туник в качестве подарка приключеникам, которые организация считает достойными.'
     }
   },
 
   {
     name       : {$$: 'Leather Jerkin' },
     description: {
-      en: 'This leather armor is a common choice for traveling adventurers. The trend of wearing leather jerkins originated around Yggdrasil when dangerous creatures began to lurk in the nearby jungles.'
+      en: 'This leather armor is a common choice for traveling adventurers. The trend of wearing leather jerkins originated around Yggdrasil when dangerous creatures began to lurk in the nearby jungles.',
+      ru: 'Эта кожаная броня является общим выбором для путешественников. Тенденция ношения кожаных джеркинов возникла вокруг Yggdrasil, когда в близлежащих джунглях начали таиться опасные существа.'
     }
   },
 
   {
     name       : {$$: 'Scaled Chestguard' },
     description: {
-      en: 'This armor is made of monster scales that were found on the ground. Monster scales are often crafted into respectable armor whenever metal is in short supply.'
+      en: 'This armor is made of monster scales that were found on the ground. Monster scales are often crafted into respectable armor whenever metal is in short supply.',
+      ru: 'Эта броня сделана из чудовищных чешуек, которые были найдены на земле. Чудовищные чешуя часто изготавливаются в респектабельные доспехи, когда металла не хватает.'
     }
   },
 
   {
     name       : {$$: 'Sky Mail' },
     description: {
-      en: 'Light Elvish metals are used to create divine chainmails. Old legends say that the first sky mails were originally created from discarded armor pieces once worn by divine creatures.'
+      en: 'Light Elvish metals are used to create divine chainmails. Old legends say that the first sky mails were originally created from discarded armor pieces once worn by divine creatures.',
+      ru: 'Легкие эльфийские металлы используются для создания божественных кольчуг. Старые легенды гласят, что первые небесные кольчуги изначально создавались из частей брони, которые когда-то носили божественные создания.'
     }
   },
 
   {
     name       : {$$: 'Shadow Cloak' },
     description: {
-      en: 'This cloak summons small shadows that envelop the wearer during battle to enhance protection. Cloaks such as these are crafted with strange shadow magic and then alchemically enhanced in secret ways.'
+      en: 'This cloak summons small shadows that envelop the wearer during battle to enhance protection. Cloaks such as these are crafted with strange shadow magic and then alchemically enhanced in secret ways.',
+      ru: 'Этот плащ вызывает небольшие тени, которые окружают хозяйна во время боя, чтобы усилить защиту. Такие плащи создаются с помощью странной теневой магии, а затем тайно обогащаются алхимическим путем.'
     }
   },
 
   {
     name       : {$$: 'Runic Halfplate' },
     description: {
-      en: 'Magic runes were etched into this armor to provide additional protection. Some scholars believe the runes on all runic halfplate armors are meant to interact together in some larger, undiscovered ritual.'
+      en: 'Magic runes were etched into this armor to provide additional protection. Some scholars believe the runes on all runic halfplate armors are meant to interact together in some larger, undiscovered ritual.',
+      ru: 'Волшебные руны были выгравированы на этой броне, чтобы обеспечить дополнительную защиту. Некоторые ученые полагают, что руны на всех доспехов предназначены для взаимодействия в более крупных, нераскрытых ритуалах.'
     }
   },
 
   {
     name       : {$$: 'Hellfire Fullplate' },
     description: {
-      en: 'Supernatural infernos were used in the making of this armor. Great sacrifices were made to appease the demands of devilish creatures so that a few blacksmiths could forge with the strange flames.'
+      en: 'Supernatural infernos were used in the making of this armor. Great sacrifices were made to appease the demands of devilish creatures so that a few blacksmiths could forge with the strange flames.',
+      ru: 'При изготовлении этих доспехов использовались сверхъестественные пламя. Крупные жертвы были принесены, для того чтобы успокоить требования дьявольских существ, чтобы несколько кузнецов могли ковать с этим странным огнем.'
     }
   },
 
   {
     name       : {$$: 'Soulkeeper' },
     description: {
-      en: 'Ancient legends say that each Soulkeeper armor harbors a soul that willingly sacrificed itself to enhance the armor’s protective power. In harder times, individuals made great sacrifices to ensure the survival of their clans and families.'
+      en: 'Ancient legends say that each Soulkeeper armor harbors a soul that willingly sacrificed itself to enhance the armor’s protective power. In harder times, individuals made great sacrifices to ensure the survival of their clans and families.',
+      ru: 'Древние легенды гласят, что в каждом доспехи хранится душа, которая добровольно пожертвовала собой, чтобы усилить защитную силу доспеха. В более трудные времена люди идут на большие жертвы, чтобы обеспечить выживание своих кланов и семей.'
     }
   },
 
   {
     name       : {$$: 'Deathless' },
     description: {
-      en: 'Deathless armors were created by the first kings and leaders as a means to prevent mortal wounds during the First Great War. True to the legend, those kings and leaders did not perish in battle; instead, they were each assassinated in their sleep and their armors were stolen. The forging techniques were lost to time, but massive fortunes were clearly necessary for the process.'
+      en: 'Deathless armors were created by the first kings and leaders as a means to prevent mortal wounds during the First Great War. True to the legend, those kings and leaders did not perish in battle; instead, they were each assassinated in their sleep and their armors were stolen. The forging techniques were lost to time, but massive fortunes were clearly necessary for the process.',
+      ru: 'Такие доспехи были созданы первыми царями и вождями, чтобы предотвратить смертельные ранения во время First Great War. По преданию легенды, эти цари и вожди не погибли в бою, а были убиты во сне, и их доспехи были украдены. Методы ковки были потеряны со временем, но для этого процесса явно требовались огромные силы и жертвы.'
     }
   }
 
