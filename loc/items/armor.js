8c96f8c89b7b5c3c2db0c15b961a7e6a076a3947
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 11:58:44 +0000

    Updated item descriptions.

--- loc/items/armor.js
+++ loc/items/armor.js
@@ -2,77 +2,77 @@ export default [
   {
     name       : {$$: 'Potato Sack' },
     description: {
-      en: 'ItemDescription'
+      en: 'This is a surprisingly sturdy material that can be used as a protective clothing. The makeshift nature of this material is obvious, so many adventurers choose to replace the potato sack as quickly as possible.'
     }
   },
 
   {
     name       : {$$: 'Faded Garment' },
     description: {
-      en: 'ItemDescription'
+      en: 'This garment is a family heirloom that has been passed down from one adventuring generation to another.'
     }
   },
 
   {
     name       : {$$: 'Adventurer\'s Tunic' },
     description: {
-      en: 'ItemDescription'
+      en: 'This tunic is crafted from robust fabrics that are designed to withstand wilderness survival. Some traditions involve giving tunics as gifts to adventurers that are deemed worthy by an organization.'
     }
   },
 
   {
     name       : {$$: 'Leather Jerkin' },
     description: {
-      en: 'ItemDescription'
+      en: 'This leather armor is a common choice for traveling adventurers. The trend of wearing leather jerkins originated around Yggdrasil when dangerous creatures began to lurk in the nearby jungles.'
     }
   },
 
   {
     name       : {$$: 'Scaled Chestguard' },
     description: {
-      en: 'ItemDescription'
+      en: 'This armor is made of monster scales that were found on the ground. Monster scales are often crafted into respectable armor whenever metal is in short supply.'
     }
   },
 
   {
     name       : {$$: 'Sky Mail' },
     description: {
-      en: 'ItemDescription'
+      en: 'Light Elvish metals are used to create divine chainmails. Old legends say that the first sky mails were originally created from discarded armor pieces once worn by divine creatures.'
     }
   },
 
   {
     name       : {$$: 'Shadow Cloak' },
     description: {
-      en: 'ItemDescription'
+      en: 'This cloak summons small shadows that envelop the wearer during battle to enhance protection. Cloaks such as these are crafted with strange shadow magic and then alchemically enhanced in secret ways.'
     }
   },
 
   {
     name       : {$$: 'Runic Halfplate' },
     description: {
-      en: 'ItemDescription'
+      en: 'Magic runes were etched into this armor to provide additional protection. Some scholars believe the runes on all runic halfplate armors are meant to interact together in some larger, undiscovered ritual.'
     }
   },
 
   {
     name       : {$$: 'Hellfire Fullplate' },
     description: {
-      en: 'ItemDescription'
+      en: 'Supernatural infernos were used in the making of this armor. Great sacrifices were made to appease the demands of devilish creatures so that a few blacksmiths could forge with the strange flames.'
     }
   },
 
   {
     name       : {$$: 'Soulkeeper' },
     description: {
-      en: 'ItemDescription'
+      en: 'Ancient legends say that each Soulkeeper armor harbors a soul that willingly sacrificed itself to enhance the armor’s protective power. In harder times, individuals made great sacrifices to ensure the survival of their clans and families.'
     }
   },
 
   {
     name       : {$$: 'Deathless' },
     description: {
-      en: 'ItemDescription'
+      en: 'Deathless armors were created by the first kings and leaders as a means to prevent mortal wounds during the First Great War. True to the legend, those kings and leaders did not perish in battle; instead, they were each assassinated in their sleep and their armors were stolen. The forging techniques were lost to time, but massive fortunes were clearly necessary for the process.'
     }
   }
 
