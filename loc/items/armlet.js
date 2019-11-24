77f912ca0fb7de83dcffb3bc62976a791859c06f
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 15:46:51 +0000

    Russian armlet translations.

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -2,91 +2,105 @@ export default [
   {
     name       : {$$: 'Simple Wristbands' },
     description: {
-      en: 'These wristbands are leather strips that are wrapped in a simple formation.'
+      en: 'These wristbands are leather strips that are wrapped in a simple formation.',
+      ru: 'Эти браслеты сделаны из кожаных полосок, которые были завернуты в простую форму'
     }
   },
 
   {
     name       : {$$: 'Makeshift Armbands' },
     description: {
-      en: 'These armbands were created hastily. The rough battle markings on the sides of the leather do not inspire confidence of the previous owner\'s survival.'
+      en: 'These armbands were created hastily. The rough battle markings on the sides of the leather do not inspire confidence of the previous owner\'s survival.',
+      ru: 'Эти браслеты были созданы поспешно. Грубая боевая разметка по бокам кожи не внушает уверенности в выживании предыдущего владельца.'
     }
   },
 
   {
     name       : {$$: 'Leather Armlets' },
     description: {
-      en: 'Tough leather has been painstakingly crafted into fine armlets. On the side of the leather is the craftsman\'s signature: "Markay\'ak".'
+      // Do NOT translate "Markay\'ak".
+      en: 'Tough leather has been painstakingly crafted into fine armlets. On the side of the leather is the craftsman\'s signature: "Markay\'ak".',
+      ru: 'Жесткая кожа была тщательно обработана в виде тонких браслетов. На боковой стороне кожи стоит подпись ремесленника: "Markay\'ak"'
     }
   },
 
   {
     name       : {$$: 'Bone Bracelets' },
     description: {
-      en: 'This type of bracelet is ornately crafted with thick monster bones. Many superstitious traditions claim that the wearer of such bracelets become permeated with the deceased monster\'s power; however, others claim that the reuse of bones prevents monsters from resurrecting.'
+      en: 'This type of bracelet is ornately crafted with thick monster bones. Many superstitious traditions claim that the wearer of such bracelets become permeated with the deceased monster\'s power; however, others claim that the reuse of bones prevents monsters from resurrecting.',
+      ru: 'Такой тип браслета сделан из толстых костей, принадлежащих мертвым монстрам. Многие суеверные традиции утверждают, что обладатель таких браслетов пронизан властью умершего монстра, однако другие утверждают, что повторное использование костей предотвращает воскрешение монстров.'
     }
   },
 
   {
     name       : {$$: 'Iron Vambraces' },
     description: {
-      en: 'This type of iron armor has been heavily used in war and remains dirty. The claw marks on the metal seem to be mostly superficial.'
+      en: 'This type of iron armor has been heavily used in war and remains dirty. The claw marks on the metal seem to be mostly superficial.',
+      ru: 'Такой тип железных доспехов широко использовался на войне и остается грязным. Следы от когтей на металле, кажется, в основном поверхностные.'
     }
   },
 
   {
     name       : {$$: 'Imbued Bracers' },
     description: {
-      en: 'These steel bracers are marked with small runes that imbue extra protection. Imbued bracers are somewhat rare since the creation method of the runes are a secret.'
+      en: 'These steel bracers are marked with small runes that imbue extra protection. Imbued bracers are somewhat rare since the creation method of the runes are a secret.',
+      ru: 'Эти стальные браслеты отмечены мелкими рунами, которые обеспечивают дополнительную защиту. Такие браслеты встречаются редко, так как метод создания рун является секретным.'
     }
   },
 
   {
     name       : {$$: 'Ember Cuffs' },
     description: {
-      en: 'These cuffs were forged with a special fire that can only be created through magic. In the past, fire elementals taught only a few special blacksmiths the secret to forging ember cuffs.'
+      en: 'These cuffs were forged with a special fire that can only be created through magic. In the past, fire elementals taught only a few special blacksmiths the secret to forging ember cuffs.',
+      ru: 'Эти браслеты были выкованы специальным огнем, который может быть создан только с помощью магии. В прошлом, огненные элементали научили только нескольких кузнецов секрету изготовления этих браслетов.'
     }
   },
 
   {
     name       : {$$: 'Mirror Armlets' },
     description: {
-      en: 'Mirrored armlets are made by exceptional blacksmiths with the skills necessary to make reflective armor for damage reduction. This protective quality inspired many Elvish wizards to often experiment on mirrored armlets.'
+      en: 'Mirrored armlets are made by exceptional blacksmiths with the skills necessary to make reflective armor for damage reduction. This protective quality inspired many Elvish wizards to often experiment on mirrored armlets.',
+      ru: 'Зеркальные браслеты изготавливаются чрезвычайно опытными кузнецами, обладающими навыками, необходимыми для изготовления брони для снижения урона. Это защитное качество вдохновило многих эльфийских волшебников часто экспериментировать на зеркальных браслетов.'
     }
   },
 
   {
     name       : {$$: 'Golem Fragments' },
     description: {
-      en: 'Golem fragments are painstakingly assembled by a small team of magic-using blacksmiths. Every pair of golem fragments is crafted over several hundred days using the husks of rock golems.'
+      en: 'Golem fragments are painstakingly assembled by a small team of magic-using blacksmiths. Every pair of golem fragments is crafted over several hundred days using the husks of rock golems.',
+      ru: 'Фрагменты големов тщательно собираются небольшой группой кузнецов, использующих магию. Каждая пара фрагментов голема изготавливается в течение нескольких сотен дней с использованием шелухи големов.'
     }
   },
 
   {
     name       : {$$: 'Coldforged Bracers' },
     description: {
-      en: 'Bracers such as this are comprised of a rare metal found in the mountains. Blacksmiths use enchanted hammers to forge the bracers without heat in order to retain the magicenhancing properties of the metal.'
+      en: 'Bracers such as this are comprised of a rare metal found in the mountains. Blacksmiths use enchanted hammers to forge the bracers without heat in order to retain the magicenhancing properties of the metal.',
+      ru: 'Такие браслеты состоят из редкого металла, встречающегося в горах. Кузнецы используют заколдованные молотки для ковки браслетов без нагрева, чтобы сохранить магические свойства металла.'
     }
   },
 
   {
     name       : {$$: 'Blackstars' },
     description: {
-      en: 'These bracers were created by blacksmiths that hammered metal found within black meteorites. So far, the greatest concentration of black meteorites was found after a cataclysm that followed the Second Great War.'
+      en: 'These bracers were created by blacksmiths that hammered metal found within black meteorites. So far, the greatest concentration of black meteorites was found after a cataclysm that followed the Second Great War.',
+      ru: 'Эти браслеты были созданы кузнецами, которые ковали металл, найденный в черных метеоритах. До сих пор наибольшая концентрация черных метеоритов была обнаружена после катаклизма, последовавшего за Second Great War.'
     }
   },
 
   {
     name       : {$$: 'Mobiuses' },
     description: {
-      en: 'Ambitious wizards on the edge of madness discovered secrets to dimensional magic and formed bracers that transcend normal magical protection. These abnormal bracers required a special shape to maintain their properties.'
+      en: 'Ambitious wizards on the edge of madness discovered secrets to dimensional magic and formed bracers that transcend normal magical protection. These abnormal bracers required a special shape to maintain their properties.',
+      ru: 'Амбициозные волшебники на грани безумия открыли для себя секреты размерной магии и сформировали браслеты, выходящие за рамки обычной магической защиты. Эти ненормальные браслеты требовали специальной формы для поддержания своих свойств.'
     }
   },
 
   {
     name       : {$$: 'Guardians' },
     description: {
-      en: 'Some legends say that these bracers only appear when the realms need a guardian. History shows that terrible tragedies occur shortly thereafter whenever these bracers are found; as such, their appearances are often seen as bad omens.'
+      en: 'Some legends say that these bracers only appear when the realms need a guardian. History shows that terrible tragedies occur shortly thereafter whenever these bracers are found; as such, their appearances are often seen as bad omens.',
+      ru: 'Некоторые легенды гласят, что эти браслеты появляются только тогда, когда королевства нуждаются в страже. История показывает, что ужасные трагедии происходят вскоре после этого, когда эти браслеты обнаруживаются; как таковые, их появление часто рассматривается как плохое предзнаменование.'
     }
   }
 
