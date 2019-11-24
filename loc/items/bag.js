97b15f86dc3d1f154927e58c0caf979aa626c2fa
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 11:59:42 +0000

    Updated item descriptions.

--- loc/items/bag.js
+++ loc/items/bag.js
@@ -2,35 +2,35 @@ export default [
   {
     name       : {$$: 'Linen Pouch' },
     description: {
-      en: 'ItemDescription'
+      en: ' This pouch is made from a delicate fabric. Pouches such as this are often owned by common people for carrying their belongings.'
     }
   },
 
   {
     name       : {$$: 'Adventurer\'s Rucksack' },
     description: {
-      en: 'ItemDescription'
+      en: 'This rugged sack is crafted from sturdy fabrics to hold more equipment than a linen pouch.'
     }
   },
 
   {
     name       : {$$: 'Purpur Duffel' },
     description: {
-      en: 'ItemDescription'
+      en: 'This robust sack is made from monster hides and permits a respectable inventory'
     }
   },
 
   {
     name       : {$$: 'Elven Saddlebag' },
     description: {
-      en: 'ItemDescription'
+      en: 'This fine bag was crafted from expensive Elvish fabrics and contains more pockets than most other bags.'
     }
   },
 
   {
     name       : {$$: 'Moss Enigma' },
     description: {
-      en: 'ItemDescription'
+      en: 'Bags such as this are a gift from nature to secret druid societies. Unfortunately, monsters have disposed of many druids over time and have stolen these sacred bags.'
     }
   }
 
