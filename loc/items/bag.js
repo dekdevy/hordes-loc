209056784316d288211ee41526252ae8c56d952a
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 16:54:00 +0000

    Russian bag translations.

--- loc/items/bag.js
+++ loc/items/bag.js
@@ -2,35 +2,40 @@ export default [
   {
     name       : {$$: 'Linen Pouch' },
     description: {
-      en: ' This pouch is made from a delicate fabric. Pouches such as this are often owned by common people for carrying their belongings.'
+      en: 'This pouch is made from a delicate fabric. Pouches such as this are often owned by common people for carrying their belongings.',
+      ru: 'Этот мешок сделан из нежной ткани. Мешки, подобные этому, часто принадлежат простым людям для перевозки своих вещей.'
     }
   },
 
   {
     name       : {$$: 'Adventurer\'s Rucksack' },
     description: {
-      en: 'This rugged sack is crafted from sturdy fabrics to hold more equipment than a linen pouch.'
+      en: 'This rugged sack is crafted from sturdy fabrics to hold more equipment than a linen pouch.',
+      ru: 'Этот прочный мешок сделан из прочных тканей и может держать в себя больше вещей, чем Linen Pouch.'
     }
   },
 
   {
     name       : {$$: 'Purpur Duffel' },
     description: {
-      en: 'This robust sack is made from monster hides and permits a respectable inventory'
+      en: 'This robust sack is made from monster hides and permits a respectable inventory.',
+      ru: 'Этот прочный мешок сделан из шкур монстров и предлагает респектабельный инвентарь.'
     }
   },
 
   {
     name       : {$$: 'Elven Saddlebag' },
     description: {
-      en: 'This fine bag was crafted from expensive Elvish fabrics and contains more pockets than most other bags.'
+      en: 'This fine bag was crafted from expensive Elvish fabrics and contains more pockets than most other bags.',
+      ru: 'Эта прекрасная сумка была изготовлена из дорогих эльфийских тканей и содержит больше карманов, чем большинство других сумок.'
     }
   },
 
   {
     name       : {$$: 'Moss Enigma' },
     description: {
-      en: 'Bags such as this are a gift from nature to secret druid societies. Unfortunately, monsters have disposed of many druids over time and have stolen these sacred bags.'
+      en: 'Bags such as this are a gift from nature to secret druid societies. Unfortunately, monsters have disposed of many druids over time and have stolen these sacred bags.',
+      ru: 'Такие сумки являются подарком от природы тайным друидным обществам. К сожалению, монстры со временем избавлялись от многих друидов и украли эти священные сумки.'
     }
   }
 
