96a08242885fd11249bfa978c0cd394243c0fe13
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 12:06:43 +0000

    Updated item descriptions.

--- loc/items/bow.js
+++ loc/items/bow.js
@@ -2,119 +2,119 @@ export default [
   {
     name       : {$$: 'Driftwood Shortbow' },
     description: {
-      en: 'ItemDescription'
+      en: 'This bow is a makeshift creation. Wood is not a plentiful resource in some regions, so denizens sometimes use the materials on hand to create weapons such as this.'
     }
   },
 
   {
     name       : {$$: 'Novice Shortbow' },
     description: {
-      en: 'ItemDescription'
+      en: 'Bows such as these are crafted for beginning archers to hone their skills.'
     }
   },
 
   {
     name       : {$$: 'Curved Shortbow'},
     description: {
-      en: 'ItemDescription'
+      en: 'Curved shortbows are mostly produced for hunting local wildlife.'
     }
   },
 
   {
     name       : {$$: 'Adventurer\'s Shortbow' },
     description: {
-      en: 'ItemDescription'
+      en: 'This type of bow is a standard weapon of choice for adventurers'
     }
   },
 
   {
     name       : {$$: 'Longbow' },
     description: {
-      en: 'ItemDescription'
+      en: 'Longbows are designed to be weapons of war'
     }
   },
 
   {
     name       : {$$: 'Bone Bow' },
     description: {
-      en: 'ItemDescription'
+      en: 'This bow is crafted from monster bones to produce extra resilience and power.'
     }
   },
 
   {
     name       : {$$: 'Elven Bow' },
     description: {
-      en: 'ItemDescription'
+      en: 'Elvish craftsmen put exquisite care in their bows during the crafting process, and Elvish archers have a proud reputation during times of war for a reason.'
     }
   },
 
   {
     name       : {$$: 'Ancient Bow' },
     description: {
-      en: 'ItemDescription'
+      en: 'Ancient bows are exceptional weapons that were designed by forgotten master craftsmen. These bows are often passed from one generation to another.'
     }
   },
 
   {
     name       : {$$: 'Iron Piercer' },
     description: {
-      en: 'ItemDescription'
+      en: 'These bows were developed in response to armor-wearing monsters.'
     }
   },
 
   {
     name       : {$$: 'Silver Recurve' },
     description: {
-      en: 'ItemDescription'
+      en: 'This bow design is more mechanically efficient than the longbow. The unique pattern of decorative silver is a testament to the craftsmanship required to make such a fine bow.'
     }
   },
 
   {
     name       : {$$: 'Assassin\'s Bow' },
     description: {
-      en: 'ItemDescription'
+      en: 'Bows such as this were previously owned by assassins that made modifications to drastically increase efficiency. Assassins regularly modify their weapons to reach optimal performance.'
     }
   },
 
   {
     name       : {$$: 'Skyfire Warbow' },
     description: {
-      en: 'ItemDescription'
+      en: 'This bow was bathed in the flames of a phoenix and cooled in the night sky. Bows created in this way are sometimes imbued with extra magical characteristics from phoenix fire.'
     }
   },
 
   {
     name       : {$$: 'Hellfire Warbow' },
     description: {
-      en: 'ItemDescription'
+      en: ' Bows such as this were made from hellfires that are provided by devilish creatures. These kinds of bows are rare because of the ultimate price required for such wicked transactions and acquisitions.'
     }
   },
 
   {
     name       : {$$: 'Widowmaker' },
     description: {
-      en: 'ItemDescription'
+      en: 'Widowmakers are living bows that bond with the user. Powerful monsters use a mystic process to create widowmakers; the resulting weapon can be remarkably jealous and entice the user not to use any other weapon by destroying foes with vast depths of battle experience.'
     }
   },
 
   {
     name       : {$$: 'Stormsong' },
     description: {
-      en: 'ItemDescription'
+      en: 'These bows are rare due to the length and method of the forging process; bows must be greatly elevated to ensure that every lightning strike can be used to gradually forge this bow into a Stormsong. The creation process can take ages.'
     }
   },
 
   {
     name       : {$$: 'Scarebow' },
     description: {
-      en: 'ItemDescription'
+      en: 'This malevolent bow eats the dreams and nightmares of the wielder. This living bow offers no loyalty and demand sacrifices in the form of battle victims.'
     }
   },
 
   {
     name       : {$$: 'Fury' },
     description: {
-      en: 'ItemDescription'
+      en: 'When a great tragedy befalls an entire civilization, the spirits of enraged victims pour their anger into a weapon to wreak vengeance upon the world. This weapon is only known as “Fury.”'
     }
   }
 
