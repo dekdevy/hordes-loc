451fa7a33e67222219560baea978b8fec809bd79
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 12:23:27 +0000

    Updated item descriptions.

--- loc/items/quiver.js
+++ loc/items/quiver.js
@@ -2,249 +2,70 @@ export default [
   {
     name       : {$$: 'Linen Quiver' },
     description: {
-      en: 'Soft, full of holes. Arrows keep falling out.',
-      ru: 'Мягкий, дырявый. Стрелы постоянно выпадают.',
-      de: 'Weich, voller Löcher. Pfeile fallen immer wieder heraus.',
-      es: 'Suave, lleno de huecos. Las flechas siguen cayendo.',
-      fr: 'Un carquois plein de trouvs dont les flèches n\'arrêtent pas de tomber.',
-      it: 'Morbido, pieno di buchi. Le frecce continuano a cadere.',
-      pt: 'Suave, cheio de buracos. As setas continuam caindo.',
-      tl: 'Malambot na puno ng mga butas. Ang mga arrow ay patuloy na bumagsak.',
-      bg: 'Мека, пълна с дупки. Стрелките продължават да падат.',
-      el: 'Μαλακό γεμάτο τρύπες. Τα βέλη συνεχίζουν να πέφτουν.',
-      hi: 'मुलायम और गद्दों से भारी हुई। सारे तीर गिरते रहते है।',
-      nl: 'Zacht, vol gaten. Pijlen blijven uitvallen.',
-      pl: 'Miękka, pełna dziur. Strzałki wciąż wypadają.',
-      ht: 'Mou, plen twou. Flèch yo kontinye tonbe.',
-      cs: 'Měkká, plná děr. Šipky stále vypadávají.',
-      ja: '柔らかい穴だらけ。矢印は抜け続けます。',
-      sv: 'Mjuk, full av hål. Pilarna fortsätter att falla ut.',
-      ko: '부드러운, 구멍 가득. 화살표는 계속 떨어지고 있습니다.',
-      ro: 'Moale, plină de găuri. Săgețile continuă să cadă.',
-      tr: 'Yumuşak ve delik dolu. Oklar dışarı düşmeye devam ediyor.',
-      da: 'Blød, fuld af huller. Pile fortsætter med at falde.'
+      en: 'This mass-produced quiver is made with Elvish linen for extra comfort.'
     }
   },
 
   {
     name       : {$$: 'Rustic Quiver' },
     description: {
-      en: 'Looks cheap and unreliable, yet sturdy enough for your arrows.',
-      ru: 'Выглядит дешевым и ненадежным, но достаточно прочный для стрел.',
-      nl: 'Ziet er goedkoop en onbetrouwbaar uit..., maar stevig genoeg voor uw pijlen.',
-      de: 'Sieht billig und unzuverlässig aus., aber dennoch robust genug für Ihre Pfeile.',
-      fr: 'Ça a l\'air bon marché et peu fiable., mais assez robuste pour vos flèches.',
-      es: 'Parece barato y poco fiable, pero lo suficientemente resistente para tus flechas.',
-      pt: 'Parece barato e não confiável, mas resistente o suficiente para as suas flechas.',
-      it: 'Sembra a buon mercato e inaffidabile, ma abbastanza robusto per le frecce.',
-      pl: 'Wygląda tanio i niewiarygodnie., a jednocześnie wystarczająco wytrzymała, aby można było używać strzałek.',
-      ko: '싸고 믿을 수 없지만 당신의 화살만큼 튼튼한 것 같습니다.',
-      sv: 'Ser billigt och opålitligt ut, men robust nog för dina pilar.',
-      ht: 'Sanble bon mache ak enfidèl, ankò solid ase pou flèch ou yo.',
-      hi: 'सस्ता और अविश्वसनीय लगता है, फिर भी अपने तीरों के लिए पर्याप्त है।',
-      tl: 'Mukhang mura at hindi kapani-paniwala, ngunit matibay sapat para sa iyong mga arrow.',
-      el: 'Φαίνεται φτηνό και αναξιόπιστο, αλλά αρκετά ανθεκτική για τα βέλη σας.',
-      bg: 'Изглежда евтино и ненадеждно, но достатъчно здрава за стрелите ви.',
-      cs: 'Vypadá to levné a nespolehlivé, ale dostatečně pevná pro vaše šípy.',
-      ja: '安くて信頼できないように見えますが、あなたの矢には十分頑丈です。',
-      tr: 'Ucuz ve güvenilmez görünüyor, ancak oklarınız için yeterince sağlam.',
-      ro: 'Pare ieftin și nedemn de încredere, dar destul de robust pentru săgețile tale.',
-      da: 'Ser billig og upålidelig, men robust nok til dine pile.'
+      en: 'Rustic Quivers are reliable and battle-tested. These quivers are often passed on to apprentice archers as they progress.'
     }
   },
 
   {
     name       : {$$: 'Snakeskin Quiver' },
     description: {
-      en: 'Every time you pull an arrow out, it hisses at you.',
-      ru: 'Каждый раз, когда вы вытаскиваете стрелу, она шипит на вас.',
-      de: 'Jedes Mal, wenn du einen Pfeil herausziehst, zischt er dich an.',
-      es: 'Cada vez que toma una flecha, el carcaj se sisea.',
-      tl: 'Sa bawat oras na hilahin mo ang isang arrow out ito heses sa iyo.',
-      bg: 'Всеки път, когато издърпате стрела, тя изсъхва.',
-      el: 'Κάθε φορά που τραβάς ένα βέλος, σου σου σου λέει.',
-      fr: 'Chaque fois que vous tirez une flèche de ce carquois, il hisse tel un serpent.',
-      it: 'Ogni volta che tiri fuori una freccia, ti sibila addosso.',
-      pt: 'Sempre que puxas uma flecha para fora, ela sibila para ti.',
-      hi: 'तीर निकालने पर साँप की तरह फूफकारती है।',
-      nl: 'Elke keer als je een pijl eruit trekt, sist het naar je.',
-      pl: 'Za każdym razem, gdy wyciągasz strzałę, syczysz na siebie.',
-      ht: 'Chak fwa ou rale yon flèch soti, li sifle nan ou.',
-      cs: 'Pokaždé, když vytáhnete šipku, zasyče na vás.',
-      ja: 'あなたが矢を引き出すたびにそれはあなたに向かっている。',
-      sv: 'Varje gång du drar en pil ut hissar den på dig.',
-      ko: '당신이 화살을 꺼낼 때마다, 그것은 당신에게 겁을줍니다.',
-      ro: 'De fiecare dată când scoți o săgeată, te sâsâie.',
-      tr: 'bir oku her çıkardığınızda, tıslama sesi duyarsınız.',
-      da: 'Hver gang du trækker en pil ud, hyser den på dig.'
+      en: 'These quivers are made from snake scales from special monsters. Typically, snake skins are found after the giant snakes have shed their skin; to attempt a snakeskin collection otherwise would be a high risk.'
     }
   },
 
   {
     name       : {$$: 'Reinforced Exemplar' },
     description: {
-      en: 'Linen sack covered in stiff leather.',
-      ru: 'Льняной мешочек обтянутый жесткой кожей.',
-      de: 'Leinensack mit steifem Leder bezogen.',
-      es: 'Un saco de lino cubierto de cuero rígido.',
-      fr: 'Sac en lin recouvert de cuir rigide.',
-      tl: 'Linyang lino na sakop sa matigas na katad.',
-      bg: 'Спално бельо, покрито с твърда кожа.',
-      el: 'Σάκος από λινάρι που καλύπτεται από σκληρό δέρμα.',
-      it: 'Sacco in lino rivestito in pelle rigida.',
-      pt: 'Saco de linho coberto de couro rígido.',
-      hi: 'सख़्त चमड़े से ढकी लिनन की पोती।',
-      nl: 'Linnenzak bedekt met stijf leer.',
-      pl: 'Lniany worek pokryty sztywną skórą.',
-      ht: 'Sak twal fin blan kouvri nan kwi rèd.',
-      cs: 'Plátěný pytel pokrytý tuhou kůží.',
-      ja: 'リネンの袋は硬い革で覆われています。',
-      sv: 'Linnesäck täckt av styvt läder.',
-      ko: '리넨 자루는 뻣뻣한 가죽으로 덮여 있습니다.',
-      ro: 'Sac din in acoperit cu piele rigidă.',
-      tr: 'Keten çuval sert deri kaplı.',
-      da: 'Linnesække dækket af stiv læder.'
+      en: 'The exquisite craftsmanship of Markay’ak resulted in the creation of Reinforced Exemplars. Quivers such as this are often a testament to the pride of any professional archer.'
     }
   },
 
   {
     name       : {$$: 'Lazarus\'s Revenge' },
     description: {
-      en: 'Finely crafted quiver made from a mysterious shimmering material.',
-      nl: 'Fijn gemaakt koker gemaakt van een mysterieus glinsterend materiaal.',
-      ru: 'Хорошо сделаный колчан произведённый из мистического блестящего материала.',
-      de: 'Fein gearbeiteter Köcher aus einem geheimnisvoll schimmernden Material.',
-      fr: 'Carquois finement fabriqué à partir d\'un mystérieux matériau scintillant.',
-      es: 'Un carcaj finamente hecho de un misterioso material resplandeciente.',
-      pt: 'Aljava finamente trabalhada feita de um misterioso material cintilante.',
-      it: 'Faretra finemente realizzata in un misterioso materiale scintillante.',
-      pl: 'Precyzyjnie wykonany kołczan wykonany z tajemniczego, błyszczącego materiału.',
-      ko: '신비한 반짝이는 소재로 만든 세 심하게 제작 된 떨림.',
-      sv: 'Finhantverkad quiver gjord av ett mystiskt glittrande material.',
-      ht: 'Tise byen fabrike flèch ki fèt ak yon materyèl misterye chatwayant.',
-      hi: 'रहस्यमय ढंग से टिमटिमाती हुई सामग्री से बना तरकश।',
-      tl: 'Ang pinong crafted quiver na ginawa mula sa isang mahiwagang shimmering na materyal.',
-      el: 'Λεπτώς φτιαγμένος από ένα μυστηριώδες γυαλιστερό υλικό.',
-      bg: 'Фино изработен колчан от мистериозен блестящ материал.',
-      cs: 'Jemně vytvořený toulec vyrobený z tajemného třpytivého materiálu.',
-      ja: '神秘的なきらめく材料で作られた細かく作られた震え。',
-      tr: 'Gizemli parıldayan metalden yapılmış ince hazırlanmış ok',
-      ro: 'O tolbă fină făcută dintr-un material strălucitor și misterios.'
+      en: 'An immortal from an elder race created quivers with an inhuman power in order to manifest his revenge upon the world.'
     }
   },
 
   {
     name       : {$$: 'Dragonhide Cynosure' },
     description: {
-      en: 'Item Description'
+      en: 'This quiver is made of dragon scales found on the ground. Dragon scales are a very difficult crafting material, so only the best craftsmen can use them.'
     }
   },
 
   {
     name       : {$$: 'Lotharien' },
     description: {
-      en: 'Elven quiver coated in shimmering diamond.',
-      ru: 'Эльфийский колчан, покрытый мерцающим бриллиантом.',
-      de: 'Elfenköcher, beschichtet mit schimmerndem Diamanten.',
-      es: 'Un carcaj cubrierto en diamante creado por los duendes.',
-      fr: 'Carquois elfique décoré de diamants brillants.',
-      tl: 'Si Elven quiver pinahiran sa shimmering diamond.',
-      bg: 'Елфическия колчан, покрит с блестящ диамант.',
-      el: 'Ελβετικό τρίχας επικαλυμμένο με λαμπερό διαμάντι.',
-      it: 'Faretra elfica rivestita di diamante luccicante.',
-      pt: 'Aljava elfica revestida com diamantes cintilantes.',
-      hi: 'एल्वेन पोटली, हीरों से जड़ी।',
-      nl: 'Elven koker gecoat in glinsterende diamant.',
-      pl: 'Elficki kołczan pokryty błyszczącym diamentem.',
-      ht: 'Flè Elven kouvwi ak dyaman chatwayant.',
-      cs: 'Elven potápěč potažený třpytivým diamantem.',
-      ja: 'きらめくダイヤモンドでコーティングされたエルフの矢筒。',
-      sv: 'Koger av älvor belagda i skimrande diamant.',
-      ko: '엘프 떨림이 반짝이는 다이아몬드로 코팅되었습니다.',
-      ro: 'Tolbă a elfilor acoperită cu diamante sclipitoare.',
-      tr: 'Parıldayan elmas ile kaplanmış Elfler tarafından yapılan ok',
-      da: 'flammer af elver overtrukket i glitrende diamant.'
+      en: 'Lothariens are quivers that are named after a people warred out of existence. The Lotharien people battled surrounding territories for power acquisition while using these quivers.'
     }
   },
 
   {
     name       : {$$: 'Treant\'s Gift' },
     description: {
-      en: 'A spectacular emerald green quiver filling your foes with guilt as they prepare to attack you.',
-      ru: 'Великолепный изумрудно-зеленый колчан наполняющий врагов чувством вины, когда те готовятся напасть на вас.',
-      de: 'Ein spektakulärer smaragdgrüner Köcher, der deine Feinde mit Schuldgefühlen erfüllt, während sie sich darauf vorbereiten, dich anzugreifen.',
-      fr: 'Un carquois vert émeraude spectaculaire qui remplit vos ennemis de culpabilité alors qu\'ils se préparent à vous attaquer.',
-      es: 'Un espectacular carcaj de color verde esmeralda llena de culpa a tus enemigos mientras se preparan para atacarte.',
-      pt: 'Uma espetacular aljava verde-esmeralda que enche os seus inimigos de culpa enquanto eles se preparam para atacar você.',
-      it: 'Una spettacolare faretra verde smeraldo che riempie i tuoi nemici di sensi di colpa mentre si preparano ad attaccarti.',
-      pl: 'Spektakularny szmaragdowo-zielony kołczan wypełniający twoich wrogów poczuciem winy, gdy przygotowują się do ataku.',
-      ko: '그들이 당신을 공격 할 준비를 할 때 죄책감으로 당신의 원수를 채우는 환상적인 에메랄드 빛 떨림.',
-      sv: 'En spektakulär smaragdgrön quiver fyller dina fiender med skuld när de förbereder sig för att attackera dig.',
-      ht: 'Yon espektakilè emwòd vèt flèch ranpli lènmi ou yo ak koupab menm jan yo prepare al atake ou.',
-      hi: 'एक शानदार पन्ना हरी तरकश अपराध के साथ अपने दुश्मनों को भरने के रूप में वे आप पर हमला करने के लिए तैयार करते हैं।',
-      tl: 'Isang kamangha-manghang emerald green quiver na pinupuno ang iyong mga kaaway sa pagkakasala habang naghahanda sila sa pag-atake sa iyo.',
-      el: 'Ένας θεαματικός σμαραγδένιος πράσινος φανατικός που γεμίζει τους εχθρούς σας με ενοχή, καθώς ετοιμάζονται να σας επιτεθούν.',
-      bg: 'Впечатляващ смарагдово зелен колчан, изпълващ враговете ви с вина, докато се подготвят да ви атакуват.',
-      cs: 'Velkolepý smaragdově zelený toulec naplňující své nepřátele vinou, když se na vás připravují k útoku.',
-      ja: '彼らがあなたを攻撃する準備をしている時に壮観なエメラルドグリーンの槍があなたの敵を罪悪感で満たしています',
-      nl: 'Een spectaculaire smaragdgroene koker vult je vijanden met schuldgevoelens als ze zich voorbereiden om je aan te vallen.',
-      tr: 'Size saldırmaya hazırlanırken düşmanlarınızı suçluluk duygusuyla dolduran muhteşem bir zümrüt yeşili ok',
-      ro: 'Tolba de un verde smarald spectaculos îți umple dușmanii cu vinovăție în timp ce se pregătesc să te atace.',
-      da: 'En spektakulær smaragdgrøn kvæler fylder dine fjender med skyld, da de forbereder sig på at angribe dig.'
+      en: 'This quiver is a gift from the forest as a means for nature spirits to reclaim their stolen power, territory, and respect.'
     }
   },
 
   {
     name       : {$$: 'Vodhrai' },
     description: {
-      en: 'A quiver is coated in a deep, shimmering navy material. Ancient stories state that such a quiver clears the mind of distractions during battle.',
-      ru: 'Колчан покрыт в глубоком, мерцающем металле. Древние истории говорят, что, во время битвы, такой колчан очищает разум от отвлечения.',
-      pt: 'Uma aljava é revestida num material marinho profundo e cintilante. Histórias antigas afirmam que tal aljava limpa a mente das distrações durante a batalha.',
-      es: 'Un carcaj está recubierto de un material marino profundo y brillante. Historias antiguas afirman que tal carcajada despeja la mente de distracciones durante la batalla.',
-      fr: 'Un carquois recouvert d\'une matière marine profonde et scintillante. Les histoires anciennes disent qu\'un tel carquois libère l\'esprit des distractions pendant les combats.',
-      de: 'Ein Köcher ist mit einem tiefen, schimmernden Marinematerial überzogen. Alte Geschichten besagen, dass ein solches Zittern den Geist von Ablenkungen im Kampf befreit.',
-      ko: '떨림은 깊고 반짝이는 해군 재료로 코팅됩니다. 고대의 이야기는 그러한 떨림이 전투 중 산만 함의 마음을 맑게한다고 말한다.',
-      nl: 'Een koker is gecoat in een diep, glinsterend marinemateriaal. Oude verhalen zeggen dat zo\'n koker de geest ontdoet van afleiding tijdens een gevecht.',
-      pl: 'Dzióbek jest pokryty głębokim, mieniącym się materiałem z marynarki wojennej. Starożytne historie mówią, że taki kołczan oczyszcza umysł z zakłóceń podczas walki.',
-      sv: 'En quiver är belagd i ett djupt, skimrande marinmaterial. Antika historier säger att en sådan dödare rensar sinnet av distraheringar under striden.',
-      ht: 'Yon flèch kouvwi nan yon gwo basen byen fon, chandèl materyèl marin. Istwa ansyen deklare ke tankou yon flèch netwaye lespri a nan distraksyon pandan batay.',
-      it: 'Una faretra è rivestita in un materiale navale profondo e scintillante. Antiche storie affermano che tale faretra libera la mente dalle distrazioni durante la battaglia.',
-      hi: 'एक तरकश एक गहरी, झिलमिलाता नौसेना सामग्री में लेपित है। प्राचीन कहानियों में कहा गया है कि इस तरह के एक तरकश लड़ाई के दौरान ध्यान भंग करने का मन साफ ​​करता है।',
-      tl: 'Ang isang engkanto ay pinahiran sa isang malalim, shimmering navy materyal. Sinasabi ng mga sinaunang istorya na ang ganitong pag-iikot ay nag-aalis ng isip ng mga kaguluhan sa panahon ng labanan.',
-      el: 'Ένα τρίχωμα είναι επικαλυμμένο σε ένα βαθύ, λαμπερό ναυτικό υλικό. Οι αρχαίες ιστορίες δηλώνουν ότι ένας τέτοιος φαύλος καθαρίζει το μυαλό των περισπασμών κατά τη διάρκεια της μάχης.',
-      bg: 'Колчанът е покрит с дълбок, блестящ материал. Древните истории заявяват, че такъв колчан изчиства ума на отвличанията по време на битка.',
-      cs: 'Toulec je potažen hlubokým, třpytivým námořnickým materiálem. Starověké příběhy říkají, že takový tulák vyčistí mysl rozptylování během bitvy.',
-      ja: '震えは深く、きらめく海軍材料でコーティングされています。古代の物語は、そのような震えが戦いの間に気晴らしの心をクリアすると述べています。',
-      tr: 'Derin parıldayan donanma malzeme ile kaplı bir ok. Antik hikayeler, böyle bir okun savaş sırasında dikkat dağıtıcı zihni temizlediğini belirtir.',
-      ro: 'O tolbă acoperită într-un material marinăresc adânc și sclipitor. Poveștile antice spun că o astfel de tolbă golește mintea de distrageri în timpul luptei.',
-      da: 'En kvæler er belagt i et dybt skinnende marinemateriale. Gamle historier siger, at en sådan kvæler rydder tankerne om distraktioner under kamp.'
+      en: 'The Vodhrai are the embodiment of vengeance, anger, wrath, and determination. The unusually strong emotions from this living quiver might serve to corrupt the user.'
     }
   },
 
   {
     name       : {$$: 'Pompeii' },
     description: {
-      en: 'Crumbling scrolls that are stained with blood read that chaos fills the mind of those that have been touched by arrows held in such a quiver.',
-      ru: 'Хрупкие свитки, испачканные кровью, говорят, что разумы тех, кто коснется стрел, находящихся в этом колчане, поселит хаос.',
-      pt: 'Pergaminhos desmoronando que estão manchados de sangue lido que faz o caos encher a mente daqueles que foram tocados por setas seguradas em tal aljava.',
-      es: 'Pergaminos que se desmoronan y están manchados de sangre leen que el caos llena la mente de aquellos que han sido tocados por flechas sostenidas en tal carcajada.',
-      fr: 'Des parchemins émiettés et tachés de sang disent que le chaos remplit l\'esprit de ceux qui ont été touchés par les flèches contenues dans un tel carquois.',
-      de: 'Zerbröckelnde Schriftrollen, die mit Blut befleckt sind, lesen, dass das Chaos den Geist derer erfüllt, die von Pfeilen berührt wurden, die in einem solchen Köcher gehalten werden.',
-      ko: '피로 얼룩진 악화 된 두루마리는 혼돈이 그 떨림 속에 든 화살에 닿은 사람들의 마음을 채우는 것을 읽습니다.',
-      nl: 'Afbrokkelende krullen die bevlekt zijn met bloed lezen dat de chaos de geest vult van degenen die zijn aangeraakt door pijlen die in zo\'n koker worden gehouden',
-      pl: 'Kruszące się zwoje poplamione krwią czytają, że chaos wypełnia umysł tych, których dotknęły strzały trzymane w takim kołczanie.',
-      sv: 'Förnedrande rullar som är färgade med blod läser att kaos fyller tankarna hos dem som har blivit rörda av pilar som hålls i ett sådant peka',
-      ht: 'Tonbe woulo ki gen tache ak san li ke dezòd ranpli lespri a nan moun ki te manyen ak flèch ki te fèt nan yon tèlman fou',
-      it: 'Pergamene sbriciolanti che sono macchiate di sangue leggiamo che il caos riempie la mente di coloro che sono stati toccati da frecce tenute in una tale faretra',
-      hi: 'रक्त के साथ दागने वाले क्रम्बल स्क्रॉल पढ़ते हैं कि अराजकता उन लोगों के दिमाग को भर देती है जिन्हें इस तरह के तरकश में रखे गए तीर द्वारा छुआ गया है',
-      tl: 'Ang mabagsik na mga balumbon na nabaho sa dugo ay nagbabasa na ang mga kaguluhan ay pumupuno sa isip ng mga naantig sa mga arrow na gaganapin sa naturang isang paliwanagan',
-      el: 'Τραγανισμένοι κύλινδροι που είναι χρωματισμένοι με αίμα διαβάζουν ότι το χάος γεμίζει το μυαλό εκείνων που έχουν αγγιχτεί από τα βέλη που κρατιούνται σε ένα τέτοιο φαρέντο',
-      bg: 'Разрушаващи се свитъци, зацапани с кръв, че хаосът изпълва съзнанието на онези, които са били докоснати от стрели, държани в такъв колчан',
-      cs: 'Rozpadající se svitky, které jsou obarveny krví, čtou, že chaos naplňuje mysl těch, kteří se dotkli šípů v takovém toulci',
-      ja: '血で染まっている崩壊している巻物は、そのような矢筒の中に握られた矢によって触れられたそれらの心を混乱が満たすことを読みます',
-      tr: 'Kanla lekelenmiş tomurcuklanan parşömenler, kaosun bu oklarla tutulanların aklını doldurduğunu oku.',
-      ro: 'Pergamentele sfărâmicioase care sunt pătate de sânge citesc că haosul umple mintea celor care au fost atinși de săgețile ținute într-o astfel de tolbă.',
-      da: 'Smuldrende ruller, der er farvet med blod, læser, at kaos fylder tankerne hos dem, der er blevet rørt ved hjælp af pile, der holdes i en sådan kvæler.'
+      en: 'This quiver was once found near the ruins of an entire civilization. There are no surviving records about the quiver in the region from this time period.'
     }
   }
 
