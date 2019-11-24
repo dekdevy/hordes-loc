7f67c9d40366407500d2bee677b0f3efaf374345
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 12:36:26 +0000

    Updated item descriptions.

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -2,119 +2,119 @@ export default [
   {
     name       : {$$: 'Broken Twig' },
     description: {
-      en: 'ItemDescription'
+      en: ' Sticks and broken twigs are used by apprentice mages for educational purposes.'
     }
   },
 
   {
     name       : {$$: 'Cracked Stick' },
     description: {
-      en: 'ItemDescription'
+      en: 'This is a damaged battlestaff that still has some use for beginning magic users'
     }
   },
 
   {
     name       : {$$: 'Gnarled Broomstick' },
     description: {
-      en: 'ItemDescription'
+      en: 'ome of the oldest trees are used to create Gnarled Broomsticks. These weapons produce spells to sweep enemies off the battlefield.'
     }
   },
 
   {
     name       : {$$: 'Oak Staff' },
     description: {
-      en: 'ItemDescription'
+      en: 'This is a sturdy oak weapon for casting intermediate magic.'
     }
   },
 
   {
     name       : {$$: 'Mystic Wand' },
     description: {
-      en: 'ItemDescription'
+      en: 'This wand is a gift from the Mystics that is embedded with strange shards that enhance magical power.'
     }
   },
 
   {
     name       : {$$: 'Bone Staff' },
     description: {
-      en: 'ItemDescription'
+      en: 'This is made from the bones of monster mages to augment magical power.'
     }
   },
 
   {
     name       : {$$: 'Encrusted Rod' },
     description: {
-      en: 'ItemDescription'
+      en: 'This rod is encrusted with gems that store magical power.'
     }
   },
 
   {
     name       : {$$: 'Imbued Staff' },
     description: {
-      en: 'ItemDescription'
+      en: 'The etched runes on this staff faintly glow when spells are cast. Although several mage guilds clearly believe that the runes augment spell casting, the true nature of the runes is unknown.'
     }
   },
 
   {
     name       : {$$: 'Emerald Staff' },
     description: {
-      en: 'ItemDescription'
+      en: 'This staff uses an enchanted stone to channel and focus magical energies. The process of finding such a special emerald, along with the expensive enchanting process, makes this staff somewhat difficult to craft.'
     }
   },
 
   {
     name       : {$$: 'Sapphire Staff' },
     description: {
-      en: 'ItemDescription'
+      en: 'This staff is made of Dragonwood and uses stones that were originally dragon gifts. The residual traces of draconic power contained in the stones compound magic’s potency.'
     }
   },
 
   {
     name       : {$$: 'Frozen Greatstaff' },
     description: {
-      en: 'ItemDescription'
+      en: 'The Frozen Greatstaff is made by mortals with ice found only in the Underworld. Although the construction methods are flawed, the results can still be quite powerful.'
     }
   },
 
   {
     name       : {$$: 'Infernal Staff' },
     description: {
-      en: 'ItemDescription'
+      en: 'This staff is made of Underwood that underwent a magical tempering process using Underworld Flames. Although the staffs were undoubtedly crafted by mortal hands, the method to obtain Underworld Flames seems to be a secret lost to time.'
     }
   },
 
   {
     name       : {$$: 'Hellfire Greatstaff' },
     description: {
-      en: 'ItemDescription'
+      en: 'Hellfire Greatstaffs are constructed with hellfires provided by mischievous devils. Although the process to create a staff such as this is mysterious, the price exacted by the devils involved is not.'
     }
   },
 
   {
     name       : {$$: 'Divine Staff' },
     description: {
-      en: 'ItemDescription'
+      en: 'This kind of staff is modeled from ancient texts. Part of the crafting process requires hundreds of devout beings to request blessings upon the staff by specific deities.'
     }
   },
 
   {
     name       : {$$: 'Crystal Core' },
     description: {
-      en: 'ItemDescription'
+      en: 'This strange staff utilizes crystals from the Realm of Madness in order to enhance wild magics in nature. The crystals are the central component of this staff.'
     }
   },
 
   {
     name       : {$$: 'Witch\'s Heart' },
     description: {
-      en: 'ItemDescription'
+      en: 'The heart of an elder witch powers this staff’s magic. Forbidden powers reanimated the beating heart, which causes the staff to symbolically embody a representation of the undead.'
     }
   },
 
   {
     name       : {$$: 'Deathweaver' },
     description: {
-      en: 'ItemDescription'
+      en: 'Deathweavers are relics that have survived the creation of the great barrier. These strange staffs are not of this world.'
     }
   }
 
