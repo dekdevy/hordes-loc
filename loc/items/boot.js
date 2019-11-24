c6c67696b1cb1d1b5000327a41504a4f76fc2891
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-24 17:41:07 +0000

    Russian boot translations.

--- loc/items/boot.js
+++ loc/items/boot.js
@@ -2,91 +2,107 @@ export default [
   {
     name       : {$$: 'Sandals' },
     description: {
-      en: 'This footwear is primarily made for comfort rather than running.'
+      en: 'This footwear is primarily made for comfort rather than running.',
+      ru: 'Эта обувь больше предназначена для комфорта, чём для бега.'
     }
   },
 
   {
     name       : {$$: 'Cloth Footpads' },
     description: {
-      en: 'These footpads are wrapped in a makeshift material to increase running stability.'
+      en: 'These footpads are wrapped in a makeshift material to increase running stability.',
+      ru: 'Эта обувь обернута грубом материалом для повышения устойчивости бега.'
     }
   },
 
   {
     name       : {$$: 'Leather Shoes' },
     description: {
-      en: 'Leather shoes are created with fine craftsmanship for moderate battle durability.'
+      en: 'Leather shoes are created with fine craftsmanship for moderate battle durability.',
+      ru: 'Эта кожаная обувь сделана с тонким мастерством для умеренной боевой прочности.'
     }
   },
 
   {
     name       : {$$: 'Bone Brogans' },
     description: {
-      en: 'The monster bones used in the creation of footwear such as this seem to enhance movement speed, but whether the enhancement is through magic or the lightness of the material is not known.'
+      en: 'The monster bones used in the creation of footwear such as this seem to enhance movement speed, but whether the enhancement is through magic or the lightness of the material is not known.',
+      ru: 'Кости монстров, используемые при изготовлении обуви, похоже, увеличивают скорость передвижения, но вопрос в том, происходит ли это с помощью магии или легкость материала, остается неизвестным.'
     }
   },
 
   {
     name       : {$$: 'Scaled Treads' },
     description: {
-      en: 'This footwear is carefully crafted with monster scales that are both light and robust for battle.'
+      en: 'This footwear is carefully crafted with monster scales that are both light and robust for battle.',
+      ru: 'Эта обувь тщательно изготовлена с использованием чудовищных чешуек, которые одновременно легки и прочны в бою.'
     }
   },
 
   {
     name       : {$$: 'Shadow Shoes' },
     description: {
-      en: ' Enchanted shoes such as this push the wearer\'s feet with shadow magic to augment speed.'
+      en: 'Enchanted shoes such as this push the wearer\'s feet with shadow magic to augment speed.',
+      ru: 'Волшебный ботинки, как эти, толкают ноги владельца с помощью теневой магии, чтобы увеличить скорость.'
     }
   },
 
   {
     name       : {$$: 'Wartorn Boots' },
     description: {
-      en: 'These boots are crafted from sturdy iron and have been used in many wars.'
+      en: 'These boots are crafted from sturdy iron and have been used in many wars.',
+      ru: 'Эти ботинки изготовлены из прочной железы и использовались во многих войнах.'
     }
   },
 
   {
     name       : {$$: 'Imbued Treads' },
     description: {
-      en: ' Footwear such as this is etched with runes that increase the wearer’s speed. However, scholars are hesitant about the use of these runes since their true purpose and nature is unclear.'
+      en: 'Footwear such as this is etched with runes that increase the wearer’s speed. 
+However, scholars are hesitant about the use of these runes since their true purpose and nature is 
+unclear.
+',
+      ru: 'Обувь, подобная этой, выгравирована рунами, которые увеличивают скорость владелеца. Однако ученые неуверенные по поводу использования этих рун, поскольку их истинное назначение и природа неясны.'
     }
   },
 
   {
     name       : {$$: 'Skyswift Boots' },
     description: {
-      en: ' Elvish magic is assisted by wind spirits to create Skyswift Boots. Although Elves are very close to nature, even they know little about the reclusive wind spirits.'
+      en: 'Elvish magic is assisted by wind spirits to create Skyswift Boots. Although Elves are very close to nature, even they know little about the reclusive wind spirits.',
+      ru: 'Эльфийская магия создаёт такие ботинки при помощи духовых ветров. Хотя эльфы очень близки к природе, даже они мало знают о духах ветра.'
     }
   },
 
   {
     name       : {$$: 'Coldforged Greaves' },
     description: {
-      en: 'These boots are forged without heat and with enchanted hammers to keep the innate properties of the special metal. This rare metal is most often found in the mountains.'
+      en: 'These boots are forged without heat and with enchanted hammers to keep the innate properties of the special metal. This rare metal is most often found in the mountains.',
+      ru: 'Эти ботинки выкованы без нагрева, с волшебными молотками, чтобы сохранить врожденные свойства специального металла. Этот редкий металл чаще всего встречается в горах.'
     }
   },
 
   {
     name       : {$$: 'Cloudrunners' },
     description: {
-      en: 'These boots are created by monster craftsmen with the help of sky spirits. Some monster factions have a close connection with sky spirits and nature; however, monsters do not frequently talk about this relationship.'
+      en: 'These boots are created by monster craftsmen with the help of sky spirits. Some monster factions have a close connection with sky spirits and nature; however, monsters do not frequently talk about this relationship.',
+      ru: 'Эти ботинки были созданы мастерами-монстрами с помощью небесных духов. Некоторые фракции монстров имеют тесную связь с небесными духами и природой, однако, монстры не часто говорят об этих отношениях.'
     }
   },
 
   {
     name       : {$$: 'Talaria' },
     description: {
-      en: 'These boots are named after a famous relic legend. The namesake of these boots were supposedly the fast footwear of a famous deity, but no one seems to know where the story originates.'
+      en: 'These boots are named after a famous relic legend. The namesake of these boots were supposedly the fast footwear of a famous deity, but no one seems to know where the story originates.',
+      ru: 'Эти ботинки названы в честь знаменитой реликтовой легенды. Тезка этих ботинок предположительно была быстрой обувью знаменитого божества, но, похоже, никто не знает, откуда взялась эта история.'
     }
   },
 
   {
     name       : {$$: 'Starshards' },
     description: {
-      en: 'This footwear is composed of meteorite pieces. Black meteorites were found during a terrible catastrophe where fire rained down from the sky and destroyed much of the landscape.'
+      en: 'This footwear is composed of meteorite pieces. Black meteorites were found during a terrible catastrophe where fire rained down from the sky and destroyed much of the landscape.',
+      ru: 'Эта обувь состоит из осколков метеорита. Черные метеориты были обнаружены во время страшной катастрофы, когда огонь падал с небес и уничтожил большую часть ландшафта.'
     }
   }
 
