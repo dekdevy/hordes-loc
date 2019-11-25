d17810d7c122d9172671995374c53b67c5f4a7e7
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-25 11:42:22 +0000

    Russian glove translations.

--- loc/items/glove.js
+++ loc/items/glove.js
@@ -26,14 +26,16 @@ export default [
   {
     name       : {$$: 'Bone Grips' },
     description: {
-      en: 'Gloves such as this are made from monster bones. Monster bones provide extra resilience and, occasionally, residual magic.'
+      en: 'Gloves such as this are made from monster bones. Monster bones provide extra resilience and, occasionally, residual magic.',
+      ru: 'Такие перчатки сделаны из костей чудовищей. Кости чудовищ обеспечивают дополнительную упругость и, иногда, остаточную магию.'
     }
   },
 
   {
     name       : {$$: 'Iron Gauntlets' },
     description: {
-      en: ' Iron gauntlets are made by master craftsmen that have spent decades working with metal.'
+      en: 'Iron gauntlets are made by master craftsmen that have spent decades working with metal.',
+      ru: 'Эти рукавицы изготавливаются мастерами, десятилетиями работавшими с металлом.'
     }
   },
 
@@ -41,55 +43,63 @@ export default [
     name       : {$$: 'Imbued Battlefists' },
     description: {
       en: 'Mage guilds often create experimental gloves such as this to increase magic power against monster factions. Some experimental gloves are adorned with runes that were copied from ancient sources.'
+      ru: 'Магические гильдии часто создают такие экспериментальные перчатки, как эти, чтобы увеличить магическую силу против фракций монстров. Некоторые экспериментальные перчатки украшены рунами, которые были скопированы из древних источников.'
     }
   },
 
   {
     name       : {$$: 'Wartorn Mitts' },
     description: {
-      en: 'This armor was battle-tested during the Third Great War. Although these mitts are old, armor of this quality was once necessary for survival.'
+      en: 'This armor was battle-tested during the Third Great War. Although these mitts are old, armor of this quality was once necessary for survival.',
+      ru: 'Эта броня прошла боевые испытания во время Third Great War. Хотя эти перчатки старые, броня такого качества когда-то была необходима для выживания.'
     }
   },
 
   {
     name       : {$$: 'Fiery Handguards' },
     description: {
-      en: 'These are experimental gloves created by fire mages that used techniques and methods taught by fire elementals.'
+      en: 'These are experimental gloves created by fire mages that used techniques and methods taught by fire elementals.',
+      ru: 'Это экспериментальные перчатки, созданные огняными магами, использующими методы и приемы, которым обучают элементы огня.'
     }
   },
 
   {
     name       : {$$: 'Empowerment Gloves' },
     description: {
-      en: 'Monster kings paid for expensive enhancements to these gloves. These enhancements were meant to gain an advantage against certain monster factions in battles for territory.'
+      en: 'Monster kings paid for expensive enhancements to these gloves. These enhancements were meant to gain an advantage against certain monster factions in battles for territory.',
+      ru: 'Короли-монстры заплатили за дорогие улучшение этих перчаток. Целью этих усовершенствований было получить преимущество над некоторыми фракциями монстров в боях за территорию.'
     }
   },
 
   {
     name       : {$$: 'Coldforged Fists' },
     description: {
-      en: 'This armor is forged without fire or heat in the hopes of retaining the magical properties of the special metal found in the mountains. Blacksmiths require enchanted hammers to forge gloves such as this.'
+      en: 'This armor is forged without fire or heat in the hopes of retaining the magical properties of the special metal found in the mountains. Blacksmiths require enchanted hammers to forge gloves such as this.',
+      ru: 'Эта броня выкована без огня и тепла в надежде сохранить магические свойства специального металла, найденного в горах. Для изготовления таких перчаток, кузнецам требуются волшебные молотки.'
     }
   },
 
   {
     name       : {$$: 'Eternals' },
     description: {
-      en: 'An elder race of immortals wore this armor and established great empires. The race disappeared suddenly and mysteriously, but some of their armor was left behind.'
+      en: 'An elder race of immortals wore this armor and established great empires. The race disappeared suddenly and mysteriously, but some of their armor was left behind.',
+      ru: 'Старейшая раса бессмертных носила эту броню и учредила великие империи. Раса внезапно и загадочно исчезла, но часть их доспехов осталась позади.'
     }
   },
 
   {
     name       : {$$: 'Phrygians' },
     description: {
-      en: 'Phrygians were developed by a long-lost civilization destroyed by war. Most of the ruins of these lost cities contained fantastic treasures that have long since been plundered.'
+      en: 'Phrygians were developed by a long-lost civilization destroyed by war. Most of the ruins of these lost cities contained fantastic treasures that have long since been plundered.',
+      ru: 'Эти магуществиные перчатки были созданы давно потерянной цивилизацией, уничтоженной войной. Большинство руин этих потерянных городов содержали огромные сокровище, которые уже давно были разграблены.'
     }
   },
 
   {
     name       : {$$: 'Titans' },
     description: {
-      en: 'Powerful armor such as this was necessary to slay titanic monster overlords that tried to take control of the realms after the initial formation of the Great Barrier. The Great Barrier was created to protect the realms from dark and forgotten enemies that rivaled the power of deities.'
+      en: 'Powerful armor such as this was necessary to slay titanic monster overlords that tried to take control of the realms after the initial formation of the Great Barrier. The Great Barrier was created to protect the realms from dark and forgotten enemies that rivaled the power of deities.',
+      ru: 'Мощные доспехи, подобные этих, были необходимы для уничтожения надзирателей титанических монстров, которые пытались захватить контроль над царствами первоначального образования "The Great Barrier". Этот барьер был создан для защиты царств от темных и забытых врагов, которые соперничали с силой богов.'
     }
   }
 
