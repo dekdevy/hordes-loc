da737897487352c726ecd747e911b690b849d1e8
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-25 11:16:54 +0000

    Russian bow translations.

--- loc/items/bow.js
+++ loc/items/bow.js
@@ -2,119 +2,136 @@ export default [
   {
     name       : {$$: 'Driftwood Shortbow' },
     description: {
-      en: 'This bow is a makeshift creation. Wood is not a plentiful resource in some regions, so denizens sometimes use the materials on hand to create weapons such as this.'
+      en: 'This bow is a makeshift creation. Wood is not a plentiful resource in some regions, so denizens sometimes use the materials on hand to create weapons such as this.',
+      ru: 'Этот лук - самодельное конструкция. Древесина не является богатым ресурсом в некоторых регионах, поэтому жители иногда используют имеющиеся материалы для создания такого оружия, как это.'
     }
   },
 
   {
     name       : {$$: 'Novice Shortbow' },
     description: {
-      en: 'Bows such as these are crafted for beginning archers to hone their skills.'
+      en: 'Bows such as these are crafted for beginning archers to hone their skills.',
+      ru: 'Такие луки созданы для начинающих лучников, чтобы улучшать свои навыки.'
     }
   },
 
   {
     name       : {$$: 'Curved Shortbow'},
     description: {
-      en: 'Curved shortbows are mostly produced for hunting local wildlife.'
+      en: 'Curved shortbows are mostly produced for hunting local wildlife.',
+      ru: 'Такие луки в основном производятся для охоты на местных диких животных.'
     }
   },
 
   {
     name       : {$$: 'Adventurer\'s Shortbow' },
     description: {
-      en: 'This type of bow is a standard weapon of choice for adventurers'
+      en: 'This type of bow is a standard weapon of choice for adventurers.',
+      ru: 'Этот тип лука является стандартным оружием выбора для искателей путешественников.'
     }
   },
 
   {
     name       : {$$: 'Longbow' },
     description: {
-      en: 'Longbows are designed to be weapons of war'
+      en: 'Longbows are designed to be weapons of war.',
+      ru: 'Этот лук задуман как оружие войны.'
     }
   },
 
   {
     name       : {$$: 'Bone Bow' },
     description: {
-      en: 'This bow is crafted from monster bones to produce extra resilience and power.'
+      en: 'This bow is crafted from monster bones to produce extra resilience and power.',
+      ru: 'Этот лук сделан из чудовищных костей для обеспечения дополнительной упругости и силы.'
     }
   },
 
   {
     name       : {$$: 'Elven Bow' },
     description: {
-      en: 'Elvish craftsmen put exquisite care in their bows during the crafting process, and Elvish archers have a proud reputation during times of war for a reason.'
+      en: 'Elvish craftsmen put exquisite care in their bows during the crafting process, and Elvish archers have a proud reputation during times of war for a reason.',
+      ru: 'Эльфийские мастера проявляют исключительную осторожность во время изготовления своих луков. Эльфийские лучники имеют восхитительнаю репутацию во время боя.'
     }
   },
 
   {
     name       : {$$: 'Ancient Bow' },
     description: {
-      en: 'Ancient bows are exceptional weapons that were designed by forgotten master craftsmen. These bows are often passed from one generation to another.'
+      en: 'Ancient bows are exceptional weapons that were designed by forgotten master craftsmen. These bows are often passed from one generation to another.',
+      ru: 'Древние луки, как этот, исключительное оружие, созданное забытыми мастерами. Эти луки часто передаются из поколения в поколение как фамильная ценность.'
     }
   },
 
   {
     name       : {$$: 'Iron Piercer' },
     description: {
-      en: 'These bows were developed in response to armor-wearing monsters.'
+      en: 'These bows were developed in response to armor-wearing monsters.',
+      ru: 'Эти луки были разработаны чтобы легче справляться с броней монстров.'
     }
   },
 
   {
     name       : {$$: 'Silver Recurve' },
     description: {
-      en: 'This bow design is more mechanically efficient than the longbow. The unique pattern of decorative silver is a testament to the craftsmanship required to make such a fine bow.'
+      en: 'This bow design is more mechanically efficient than the longbow. The unique pattern of decorative silver is a testament to the craftsmanship required to make such a fine bow.',
+      ru: 'Эта конструкция лука более эффективна с механической точки зрения, чем длинный лук. Уникальный узор из декоративного серебра является свидетельством мастерства, необходимого для изготовления такого вылечаятвинова лука.'
     }
   },
 
   {
     name       : {$$: 'Assassin\'s Bow' },
     description: {
-      en: 'Bows such as this were previously owned by assassins that made modifications to drastically increase efficiency. Assassins regularly modify their weapons to reach optimal performance.'
+      en: 'Bows such as this were previously owned by assassins that made modifications to drastically increase efficiency. Assassins regularly modify their weapons to reach optimal performance.',
+      ru: 'Такие луки раньше принадлежали убийцам, которые вносили изменения для резкого повышения эффективности. Убийцы регулярно модифицируют свое оружие для достижения оптимальных результатов.'
     }
   },
 
   {
     name       : {$$: 'Hellfire Warbow' },
     description: {
-      en: ' Bows such as this were made from hellfires that are provided by devilish creatures. These kinds of bows are rare because of the ultimate price required for such wicked transactions and acquisitions.'
+      en: 'Bows such as this were made from hellfires that are provided by devilish creatures. These kinds of bows are rare because of the ultimate price required for such wicked transactions and acquisitions.',
+      ru: 'Такие луки были сделаны из адского огня, который обеспечивают дьявольские существа. Луки, как этот, встречаются редко из-за конечной цены, необходимой для таких злонамеренных сделок и приобретений.'
     }
   },
 
   {
     name       : {$$: 'Skyfire Warbow' },
     description: {
-      en: 'This bow was bathed in the flames of a phoenix and cooled in the night sky. Bows created in this way are sometimes imbued with extra magical characteristics from phoenix fire.'
+      en: 'This bow was bathed in the flames of a phoenix and cooled in the night sky. Bows created in this way are sometimes imbued with extra magical characteristics from phoenix fire.',
+      ru: 'Этот лук раскален в пламя феникса и охлажден в ночном небе. Созданные таким образом, луки иногда наделяются дополнительными магическими свойствами от огня фениксов.'
     }
   },
 
   {
     name       : {$$: 'Widowmaker' },
     description: {
-      en: 'Widowmakers are living bows that bond with the user. Powerful monsters use a mystic process to create widowmakers; the resulting weapon can be remarkably jealous and entice the user not to use any other weapon by destroying foes with vast depths of battle experience.'
+      en: 'Widowmakers are living bows that bond with the user. Powerful monsters use a mystic process to create widowmakers; the resulting weapon can be remarkably jealous and entice the user not to use any other weapon by destroying foes with vast depths of battle experience.',
+      ru: 'Эту луки живые конструкции, которые привязаны к пользователю. Мощные чудовища используют загадочный процесс создания этих луков; полученное оружие может вызывать удивительную ревность и побуждать пользователя не применять никакое другое оружие, уничтожая противников с огромным боевым опытом.'
     }
   },
 
   {
     name       : {$$: 'Stormsong' },
     description: {
-      en: 'These bows are rare due to the length and method of the forging process; bows must be greatly elevated to ensure that every lightning strike can be used to gradually forge this bow into a Stormsong. The creation process can take ages.'
+      en: 'These bows are rare due to the length and method of the forging process; bows must be greatly elevated to ensure that every lightning strike can be used to gradually forge this bow into a Stormsong. The creation process can take ages.',
+      ru: 'Эти луки редки из-за длинного процесса ковки; луки должны быть сильно приподняты, чтобы гарантировать, что каждый удар молнии может быть использован для постепенной ковки этого лука в Stormsong. Процесс создания может занять годы.'
     }
   },
 
   {
     name       : {$$: 'Scarebow' },
     description: {
-      en: 'This malevolent bow eats the dreams and nightmares of the wielder. This living bow offers no loyalty and demand sacrifices in the form of battle victims.'
+      en: 'This malevolent bow eats the dreams and nightmares of the wielder. This living bow offers no loyalty and demand sacrifices in the form of battle victims.',
+      ru: 'Этот злобный лук съедает сны и кошмары владелеца. Этот живой лук не предлагает верности и требует требует жертв в виде солдат противоборствующих армий.'
     }
   },
 
   {
     name       : {$$: 'Fury' },
     description: {
-      en: 'When a great tragedy befalls an entire civilization, the spirits of enraged victims pour their anger into a weapon to wreak vengeance upon the world. This weapon is only known as “Fury.”'
+      en: 'When a great tragedy befalls an entire civilization, the spirits of enraged victims pour their anger into a weapon to wreak vengeance upon the world. This weapon is only known as ”Fury.”',
+      ru: 'Когда целая цивилизация переживает великую трагедию, духи разгневанных жертв изливают свой гнев в оружие, чтобы отомстить миру. Это оружие известно только как "Fury".'
     }
   }
 
