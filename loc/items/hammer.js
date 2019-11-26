a940d524b9f5c7111df52f6d0d53e1f997231ed8
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-26 18:33:26 +0000

    Russian hammer translations.

--- loc/items/hammer.js
+++ loc/items/hammer.js
@@ -2,119 +2,136 @@ export default [
   {
     name       : {$$: 'Splintered Club' },
     description: {
-      en: 'This is a makeshift weapon that is often created by farmers.'
+      en: 'This is a makeshift weapon that is often created by farmers.',
+      ru: 'Это временное оружие, которое часто создают фермеры.'
     }
   },
 
   {
     name       : {$$: 'Wooden Mallet' },
     description: {
-      en: 'This is a mallet normally used for carpentry; however, wooden mallets are often used for combat out of necessity when resources and materials are scarce.'
+      en: 'This is a mallet normally used for carpentry; however, wooden mallets are often used for combat out of necessity when resources and materials are scarce.',
+      ru: 'Этот молоток, обычно используется для столярных работ; однако деревянные молоточки по необходимости часто используются для борьбы, когда ресурсов и материалов не хватает.'
     }
   },
 
   {
     name       : {$$: 'Primal Mace' },
     description: {
-      en: ' Primal maces are battle-tested and have legends heaped upon them; however, their effectiveness has diminished over time.'
+      en: 'Primal maces are battle-tested and have legends heaped upon them; however, their effectiveness has diminished over time.',
+      ru: 'Первоклассные булавы проходят боевые испытания и на них накапливаются легенды, однако их эффективность со временем снижается.'
     }
   },
 
   {
     name       : {$$: 'Orcish Bludgeon' },
     description: {
-      en: 'This is an orc\'s preferred tool for interrogations and battle.'
+      en: 'This is an orc\'s preferred tool for interrogations and battle.',
+      ru: 'Это предпочтительный инструмент орков для допросов и ведения боя.'
     }
   },
 
   {
     name       : {$$: 'Heavy Mace' },
     description: {
-      en: 'Heavy maces are made for war, infiltration, and charisma.'
+      en: 'Heavy maces are made for war, infiltration, and charisma.',
+      ru: 'Тяжелые булавы созданы для войны, вторжений и харизмы.'
     }
   },
 
   {
     name       : {$$: 'Iron Basher' },
     description: {
-      en: ' Iron bashers are typically made by expert craftsmen such as the monster Markay\'ak. Craftsmen almost always leave their mark on the iron as a symbol of pride.'
+      en: 'Iron bashers are typically made by expert craftsmen such as the monster Markay\'ak. Craftsmen almost always leave their mark on the iron as a symbol of pride.',
+      ru: 'Железные башеры обычно изготавливаются опытными мастерами, такими как монстр Markay\'ak. Ремесленники почти всегда оставляют свой след на железе как символ гордости.'
     }
   },
 
   {
     name       : {$$: 'Darkmetal Maul' },
     description: {
-      en: 'These mauls are made with a special metal that has distinct colors and properties. The secrets of this darkmetal were stolen from monster craftsmen and popularized.'
+      en: 'These mauls are made with a special metal that has distinct colors and properties. The secrets of this darkmetal were stolen from monster craftsmen and popularized.',
+      ru: 'Эти булавы сделаны из специального металла, который имеет различные цвета и свойства. Секреты этого тёмного металла были украдены у монстров-ремесленников и популяризованы.'
     }
   },
 
   {
     name       : {$$: 'Divine Gavel' },
     description: {
-      en: 'These gavels are blessed by a church. Divine gavels sometimes acquire power through the devotion, achievement, and sacrifice of previous owners.'
+      en: 'These gavels are blessed by a church. Divine gavels sometimes acquire power through the devotion, achievement, and sacrifice of previous owners.',
+      ru: 'Эти молоты благословлены церковью. Божественные молоты иногда приобретают силу благодаря преданности, достижениям и самопожертвованию предыдущих владельцев.'
     }
   },
 
   {
     name       : {$$: 'Hallowed Hammer' },
     description: {
-      en: ' Hammers such as this are often created by famous blacksmiths like Markay\'ak with the help of religious monster priests. Hallowed hammers are sometimes endowed with sacred powers.'
+      en: 'Hammers such as this are often created by famous blacksmiths like Markay\'ak with the help of religious monster priests. Hallowed hammers are sometimes endowed with sacred powers.',
+      ru: 'Такие молоты часто создаются известными кузнецами, такими как Markay\'ak, с помощью священников - религиозных монстров. Священные молоты иногда наделены святыми способностями.'
     }
   },
 
   {
     name       : {$$: 'Dwarven Maul' },
     description: {
-      en: ' Dwarven mauls are robust and powerful enough to withstand Dwarven life in the mines and ground. Dwarven blacksmiths have vast experience in making hammers.'
+      en: 'Dwarven mauls are robust and powerful enough to withstand Dwarven life in the mines and ground. Dwarven blacksmiths have vast experience in making hammers.',
+      ru: 'Булавы гномов достаточно прочны и мощны, чтобы сохранять жизни гномов в шахтах и на земле. Гномы кузнецы имеют огромный опыт изготовления булав.'
     }
   },
 
   {
     name       : {$$: 'Coldforged Gavel' },
     description: {
-      en: 'These gavels are made from a special metal and forged without heat. The process of forging without heat allows innate properties of the special metal to remain.'
+      en: 'These gavels are made from a special metal and forged without heat. The process of forging without heat allows innate properties of the special metal to remain.',
+      ru: 'Эти молотки изготавливаются из специального металла и куются без нагрева. Процесс ковки без нагрева позволяет сохранить врожденные свойства специального металла.'
     }
   },
 
   {
     name       : {$$: 'Skullshatterer' },
     description: {
-      en: 'Skullshatterer hammers were originally created to combat the undead. The hammers that remain are often considered prized possessions by many clerics and sacred beings.'
+      en: 'Skullshatterer hammers were originally created to combat the undead. The hammers that remain are often considered prized possessions by many clerics and sacred beings.',
+      ru: 'Первоначально эти молоты использовались для разбивания черепов и борьбы с нежитью. Оставшиеся молоты часто считаются ценным достоянием для многих священнослужителей и святых.'
     }
   },
 
   {
     name       : {$$: 'Amboss' },
     description: {
-      en: 'These hammers are known for breaking anvil heads during the process of forging. They are rarely created and typically fetch a substantial price.'
+      en: 'These hammers are known for breaking anvil heads during the process of forging. They are rarely created and typically fetch a substantial price.',
+      ru: 'Эти молоты известны тем, что они ломают поверхность наковальни в процессе ковки. Они редко создаются и, как правило, имеют существенную цену.'
     }
   },
 
   {
     name       : {$$: 'Benevolence' },
     description: {
-      en: 'Spiritual masters often spread the message of their spirituality in a variety of ways. This hammer has the power to give the gift of peace to enemies — one way or another.'
+      en: 'Spiritual masters often spread the message of their spirituality in a variety of ways. This hammer has the power to give the gift of peace to enemies — one way or another.',
+      ru: 'Духовные мастера часто распространяют весть о своей духовности различными способами. Этот молот обладает способностью так или иначе дарить врагам дар мира.'
     }
   },
 
   {
     name       : {$$: 'Hammer of Gaia' },
     description: {
-      en: 'These hammers were owned and used by titanic monster overlords after the initial formation of the Great Barrier. These titans wrecked the land and ruled the realms for a time.'
+      en: 'These hammers were owned and used by titanic monster overlords after the initial formation of the Great Barrier. These titans wrecked the land and ruled the realms for a time.',
+      ru: 'Эти молоты принадлежали Титанам-повелителям и использовались ими после первоначального образования "Great Barrier". Эти титаны разрушили землю и некоторое время правили королевством.'
     }
   },
 
   {
     name       : {$$: 'Worldender' },
     description: {
-      en: ' According to historians, this hammer is said to have once caused an earthquake massive enough to topple a city.'
+      en: ' According to historians, this hammer is said to have once caused an earthquake massive enough to topple a city.',
+      ru: 'По словам историков, этот молот, как говорят, однажды вызвал землетрясение, достаточно мощное, чтобы свергнуть город.'
     }
   },
 
   {
     name       : {$$: 'Nightmare' },
     description: {
-      en: 'An ancient legend claims that a strange hammer fell through a Great Barrier fissure into the realms; hammers such as this are simply not of this world.'
+      en: 'An ancient legend claims that a strange hammer fell through a Great Barrier fissure into the realms; hammers such as this are simply not of this world.',
+      ru: 'Древняя легенда гласит, что странный молот упал через трещину "Great Barrie" в царства; молотки, подобные этому, просто не относятся к этому миру.'
     }
   }
 
