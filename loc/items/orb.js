f1445be329f21875787935dffaafc7e129446489
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-26 18:38:26 +0000

    Russian orb translations.

--- loc/items/orb.js
+++ loc/items/orb.js
@@ -3,21 +3,24 @@ export default [
   {
     name       : {$$: 'Rat Skull' },
     description: {
-      en: 'This rat skull is a useful tool for beginning mages. Mages sometimes experiment on the bones of long-dead creatures to produce a focusing vessel.'
+      en: 'This rat skull is a useful tool for beginning mages. Mages sometimes experiment on the bones of long-dead creatures to produce a focusing vessel.',
+      ru: 'Это череп крысы - полезный инструмент для начинающих магов. Иногда маги экспериментируют с костями долгоживущих существ, чтобы создать фокусирующийся сосуд.'
     }
   },
 
   {
     name       : {$$: 'Crystal Globe' },
     description: {
-      en: 'These globes are used for a variety of magical tasks. This is a classic device for magic users.'
+      en: 'These globes are used for a variety of magical tasks. This is a classic device for magic users.',
+      ru: 'Эти сферы используются для различных магических задач. Это классический инструмент для волшебников.'
     }
   },
 
   {
     name       : {$$: 'Seer\'s Stone' },
     description: {
-      en: 'The internal magic of these stones allow wise men to protect themselves during scrying and other seer activities.'
+      en: 'The internal magic of these stones allow wise men to protect themselves during scrying and other seer activities.',
+      ru: 'Внутренняя магия этих камней позволяет мудрецам защитить себя во время охоты и других провидческих действий.'
     }
   },
 
@@ -31,42 +34,48 @@ export default [
   {
     name       : {$$: 'Cyclops\'s Eye' },
     description: {
-      en: 'This is a fantastic magical component that is easily enchanted. Handy items such as this often come with innate powers and can frequently be found in trade shipments between merchants.'
+      en: 'This is a fantastic magical component that is easily enchanted. Handy items such as this often come with innate powers and can frequently be found in trade shipments between merchants.',
+      ru: 'Это фантастический магический компонент, который легко очаровывает. Такие удобные предметы, как этот, часто имеют врожденные силы и часто встречаются в товарах у торговцев.'
     }
   },
 
   {
     name       : {$$: 'Nüwa\'s Rondure' },
     description: {
-      en: 'This powerful magical object is often used to control the elements and inspire fear in the hearts of enemies. Nüwa, a legendary magic user from a monster faction, made many objects such as this.'
+      en: 'This powerful magical object is often used to control the elements and inspire fear in the hearts of enemies. Nüwa, a legendary magic user from a monster faction, made many objects such as this.',
+      ru: 'Этот мощный магический объект часто используется для управления стихией и внушает страх в сердцах врагов. Nüwa\'s, легендарный волшебник из фракции монстров, создал множество подобных предметов.'
     }
   },
 
   {
     name       : {$$: 'Baetylus\'s Eye' },
     description: {
-      en: 'Objects like this were created from meteorites and misused as holy symbols. Although they are no longer used as holy symbols, Baetylus’s Eyes radiate substantial magical power.'
+      en: 'Objects like this were created from meteorites and misused as holy symbols. Although they are no longer used as holy symbols, Baetylus’s Eyes radiate substantial magical power.',
+      ru: 'Подобные объекты были созданы из метеоритов и использовались как священные символы. Хотя они больше не используются в качестве священных символов, Baetylus\'s Eyes излучают значительную магическую силу.'
     }
   },
 
   {
     name       : {$$: 'Benben Stone' },
     description: {
-      en: 'Benben Stones were placed on top of the pyramids of an ancient, dead race. The stones absorbed thousands of years of solar energy, which was said to be a gift from a sun deity.'
+      en: 'Benben Stones were placed on top of the pyramids of an ancient, dead race. The stones absorbed thousands of years of solar energy, which was said to be a gift from a sun deity.',
+      ru: 'Benben Камни были установлены на вершине пирамид древней, мертвой расы. Камни впитали в себя тысячелетнюю солнечную энергию, которая, как говорят, была подарком солнечного божества.'
     }
   },
 
   {
     name       : {$$: 'Silthrim' },
     description: {
-      en: 'Ancient snake stones such as this were found on forgotten altars inside ancient ruins. Scholars say that the stones were probably used in worshipping a snake deity, but the texts found in the ruins have not been successfully translated yet.'
+      en: 'Ancient snake stones such as this were found on forgotten altars inside ancient ruins. Scholars say that the stones were probably used in worshipping a snake deity, but the texts found in the ruins have not been successfully translated yet.',
+      ru: 'Древние змеиные камни, подобные этому, были найдены на забытых алтарях внутри древних руин. Ученые говорят, что камни, вероятно, использовались для поклонения божеству змеи, но тексты, найденные в руинах, еще не были успешно переведены.'
     }
   },
 
   {
     name       : {$$: 'Lich\'s Phylactery' },
     description: {
-      en: 'A lich’s phylactery contains the soul of a powerful undead creature. Older phylacteries come with powerful enchantments to protect the wearer — and the soul contained within.'
+      en: 'A lich’s phylactery contains the soul of a powerful undead creature. Older phylacteries come with powerful enchantments to protect the wearer — and the soul contained within.',
+      ru: 'Филактерия лича содержит душу могущественного существа-нежити. Старые филактерии обладают мощным завораживающим действием для защиты владельца и души, содержащейся в них.'
     }
   }
 ]
