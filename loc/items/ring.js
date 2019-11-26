73007107ecd29da9773aef349ebe5220209586af
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-26 18:56:52 +0000

    Russian ring translations.

--- loc/items/ring.js
+++ loc/items/ring.js
@@ -2,91 +2,104 @@ export default [
   {
     name       : {$$: 'Woven Band' },
     description: {
-      en: 'This strange band allows a small part of the wearer’s life essence to be stored.'
+      en: 'This strange band allows a small part of the wearer’s life essence to be stored.',
+      ru: 'Эта странная повязка позволяет хранить небольшую часть жизни владельца.'
     }
   },
 
   {
     name       : {$$: 'Ironbark Loop' },
     description: {
-      en: 'This ring is made from a special wood that is as tough as iron. Ironbark is often used as an alternative to metal.'
+      en: 'This ring is made from a special wood that is as tough as iron. Ironbark is often used as an alternative to metal.',
+      ru: 'Это кольцо изготовлено из специальной древесины, прочной как железо. Железная кора часто используется в качестве альтернативы металлу.'
     }
   },
 
   {
     name       : {$$: 'Brass Unity' },
     description: {
-      en: 'Two kingdoms that were joined by royal marriage had their armies wear these rings as a symbol of the newly allied nations.'
+      en: 'Two kingdoms that were joined by royal marriage had their armies wear these rings as a symbol of the newly allied nations.',
+      ru: 'Два королевства, которые присоединились королевским бракосочетаниям, носили эти кольца в качестве символа новых союзнических народов.'
     }
   },
 
   {
     name       : {$$: 'Hollowed Bone' },
     description: {
-      en: 'This ring is made from the bones of a monster husk. Monster bone can be used as a sturdy material when no other resources are available.'
+      en: 'This ring is made from the bones of a monster husk. Monster bone can be used as a sturdy material when no other resources are available.',
+      ru: 'Это кольцо изготовлено из костей монстров шелухи. Кость монстра может быть использована как прочный материал, когда нет других ресурсов.'
     }
   },
 
   {
     name       : {$$: 'Elven Heirloom' },
     description: {
-      en: 'Rings are often handed down from generation to generation in the Elvish tradition.'
+      en: 'Rings are often handed down from generation to generation in the Elvish tradition.',
+      ru: 'В эльфийской традиции кольца часто передаются из поколения в поколение.'
     }
   },
 
   {
     name       : {$$: 'Imbued Union' },
     description: {
-      en: 'This ring has tiny runes carved into the inner band. Some historians believe these rings were the last effort of a lost kingdom to fortify its population from disease.'
+      en: 'This ring has tiny runes carved into the inner band. Some historians believe these rings were the last effort of a lost kingdom to fortify its population from disease.',
+      ru: 'Крошечные руны вырезаны на внутренней части кольца. Некоторые историки считают, что эти кольца были последними попытками потерянного королевства укрепить здоровье своего населения от болезней.'
     }
   },
 
   {
     name       : {$$: 'Arcane Ring' },
     description: {
-      en: 'Arcane rings are the magical experiments of intermediate mages. Some rings are crafted and enchanted to substantially enhance magic-casting abilities.'
+      en: 'Arcane rings are the magical experiments of intermediate mages. Some rings are crafted and enchanted to substantially enhance magic-casting abilities.',
+      ru: 'Arcane Ring- это магические эксперименты промежуточных магов. Некоторые кольца изготовлены и зачарованы, чтобы существенно повысить магические способности.'
     }
   },
 
   {
     name       : {$$: 'Emerald Band' },
     description: {
-      en: ' These rings once belonged to the “Cult of the Emerald Spiders,” a gang of bandits that were among the first to ride spiders.'
+      en: 'These rings once belonged to the “Cult of the Emerald Spiders,” a gang of bandits that were among the first to ride spiders.',
+      ru: 'Эти кольца когда-то принадлежали банде "Культ изумрудных пауков", которые одни из первых ездили на пауках.'
     }
   },
 
   {
     name       : {$$: 'Infernal Ring' },
     description: {
-      en: ' Infernal rings are gifts from devilish creatures. They entice the wearer to desire more power. This desire is often followed by ill-advised offers by more devils.'
+      en: 'Infernal rings are gifts from devilish creatures. They entice the wearer to desire more power. This desire is often followed by ill-advised offers by more devils.',
+      ru: 'Infernal Ring - это подарок от дьявольских существ. Они соблазняют владельца желать больше силы. За этим желанием часто следуют неразумные предложения от большого количества дьяволов.'
     }
   },
 
   {
     name       : {$$: 'Ancient Signet' },
     description: {
-      en: 'Nature spirits originally gave these rings as gifts of appreciation to primal monsters. Many of these rings have been stolen or lost over thousands of years.'
+      en: 'Nature spirits originally gave these rings as gifts of appreciation to primal monsters. Many of these rings have been stolen or lost over thousands of years.',
+      ru: 'Природные духи изначально дарили эти кольца в знак благодарности первобытным монстрам. Многие из этих колец были украдены или потеряны за тысячи лет.'
     }
   },
 
   {
     name       : {$$: 'Reverence' },
     description: {
-      en: ' Many religious factions claim that individuals wearing this ring have the favor of a deity; however, the specific deity is not known. Many religious figureheads believe that the divine anonymity is intentional.'
+      en: 'Many religious factions claim that individuals wearing this ring have the favor of a deity; however, the specific deity is not known. Many religious figureheads believe that the divine anonymity is intentional.',
+      ru: 'Многие религиозные фракции утверждают, что люди, носящие это кольцо, имеют благосклонность к божеству; однако конкретное божество не известно. Многие религиозные деятели считают, что божественная анонимность носит преднамеренный характер.'
     }
   },
 
   {
     name       : {$$: 'Gyges' },
     description: {
-      en: 'Ancient scrolls claim that these legendary rings make the wearer nearly invincible. The scrolls also mention stories of wearers becoming corrupt and profoundly unhappy.'
+      en: 'Ancient scrolls claim that these legendary rings make the wearer nearly invincible. The scrolls also mention stories of wearers becoming corrupt and profoundly unhappy.',
+      ru: 'Древние свитки утверждают, что эти легендарные кольца делают владельца почти непобедимым. В свитках также упоминаются истории о том, как носители становятся сломлены и глубоко несчастными.'
     }
   },
 
   {
     name       : {$$: 'Peacekeeper' },
     description: {
-      en: 'Once every age, a Peacekeeper is found and marks the wearer as a special protector of the realms. In a past age, the bearer of the Peacekeeper failed and the Third Great War began.'
+      en: 'Once every age, a Peacekeeper is found and marks the wearer as a special protector of the realms. In a past age, the bearer of the Peacekeeper failed and the Third Great War began.',
+      ru: 'Один раз за век, находят Peacekeeper и отмечают его носителя как особого защитника царств. В прошлом веке носительница Peacekeeper потерпела неудачу, и началась Third Great War.'
     }
   }
 
