07c625aae76c4184df2ec01282964697ed86faba
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-26 19:05:17 +0000

    Russian shield translations.

--- loc/items/shield.js
+++ loc/items/shield.js
@@ -2,70 +2,79 @@ export default [
   {
     name       : {$$: 'Wooden Shield' },
     description: {
-      en: 'This is a makeshift shield that seems to have been produced out of necessity.'
+      en: 'This is a makeshift shield that seems to have been produced out of necessity.',
+      ru: 'Это самодельный щит, который, кажется, был создан по необходимости.'
     }
   },
 
   {
     name       : {$$: 'Buckler' },
     description: {
-      en: 'Bucklers can be useful protection against small and light weapons. Pirates often use bucklers for maneuverability.'
+      en: 'Bucklers can be useful protection against small and light weapons. Pirates often use bucklers for maneuverability.',
+      ru: 'Bucklers могут быть полезной защитой от стрелкового оружия и легких вооружений. Пираты часто используют Buckler для маневренности.'
     }
   },
 
   {
     name       : {$$: 'Old Bulwark' },
     description: {
-      en: 'This is a shield type that is often made during war times. Although cheap, the shield is made of sturdy ironbark.'
+      en: 'This is a shield type that is often made during war times. Although cheap, the shield is made of sturdy ironbark.',
+      ru: 'Такой тип щитов часто делают во время войны. Хотя щит и дешевый, он изготовлен из прочной железной коры.'
     }
   },
 
   {
     name       : {$$: 'Metal Guard' },
     description: {
-      en: 'Kingdom sentries often use shields such as this to reduce received damage as well as protect citizens.'
+      en: 'Kingdom sentries often use shields such as this to reduce received damage as well as protect citizens.',
+      ru: 'Королевствские часовые часто используют такие щиты, как этот, для уменьшения получаемого ущерба, а также для защиты граждан.'
     }
   },
 
   {
     name       : {$$: 'Darkmetal Shield' },
     description: {
-      en: 'This shield is made of darkmetal and anointed with a black oil. Adventurers with this kind of shield are certain to stand out from the rest.'
+      en: 'This shield is made of darkmetal and anointed with a black oil. Adventurers with this kind of shield are certain to stand out from the rest.',
+      ru: 'Этот щит изготовлен из черного металла и помазан черным маслом. Искатели приключений с таким щитом обязательно будут выделяться среди остальных.'
     }
   },
 
   {
     name       : {$$: 'Spiked Warshield' },
     description: {
-      en: ' Spiked Warshields are robust in battle and fitting for orcish culture. Veteran orc warriors display their victory notches on the sides of their shields.'
+      en: ' Spiked Warshields are robust in battle and fitting for orcish culture. Veteran orc warriors display their victory notches on the sides of their shields.',
+      ru: 'Spiked Warshield сильны в битве и приспособлены для оркской культуры. Ветераны оркской войны выставляют свои победные метки по бокам щитов.'
     }
   },
 
   {
     name       : {$$: 'Protecteron' },
     description: {
-      en: ' Paladins acquire this shield when they attain a certain level of spiritual devotion. However, these paladin shields are sometimes seen without their owners...'
+      en: 'Paladins acquire this shield when they attain a certain level of spiritual devotion. However, these paladin shields are sometimes seen without their owners...',
+      ru: 'Паладины приобретают этот щит, когда достигают определенного уровня духовной преданности. Однако, эти паладинские щиты иногда видны без их владельцев...'
     }
   },
 
   {
     name       : {$$: 'Svalinn' },
     description: {
-      en: 'This type of shield is made by mortals with ice that is found only in the Underworld. Although the shield is powerful, the unusual properties and rarity of the Underworld Ice make mortal attempts at reconstructing these shields inadequate at best.'
+      en: 'This type of shield is made by mortals with ice that is found only in the Underworld. Although the shield is powerful, the unusual properties and rarity of the Underworld Ice make mortal attempts at reconstructing these shields inadequate at best.',
+      ru: 'Этот тип щита, смертными создаётся со льдом, и встречается только в Underworld. Хотя щит является мощным, необычные свойства и редкость подводного льда делают для смертных попытки реконструкции этих щитов в лучшем случае неадекватными.'
     }
   },
 
   {
     name       : {$$: 'Ancile' },
     description: {
-      en: ' This sacred shield is wrapped in legend and rumors. One ancient scroll tells how the shield created false copies of the user to confuse opponents. Another scroll alternatively explains that the shield was modeled after the very shield wielded by a deity'
+      en: 'This sacred shield is wrapped in legend and rumors. One ancient scroll tells how the shield created false copies of the user to confuse opponents. Another scroll alternatively explains that the shield was modeled after the very shield wielded by a deity',
+      ru: 'Этот священный щит окружён легендами и слухами. Один древний свиток рассказывает о том, как щит создавал ложные копии война, чтобы запутать противников. Другой свиток объясняет, что щит был смоделирован по образцу того самого щита, которым обладало божество.'
     }
   },
 
   {
     name       : {$$: 'Aegis' },
     description: {
-      en: 'The beholder’s eye in the center of this shield has instilled fear in the hearts of many opponents that claimed to be fearless. The preservation method that allows the organic eye to function is still a mystery.'
+      en: 'ItemDescription'
     }
   }
 
