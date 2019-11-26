f8f174edff0e5a9f808068ed54c660f796b3a4c1
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-26 18:44:48 +0000

    Russian quiver translations.

--- loc/items/quiver.js
+++ loc/items/quiver.js
@@ -2,70 +2,80 @@ export default [
   {
     name       : {$$: 'Linen Quiver' },
     description: {
-      en: 'This mass-produced quiver is made with Elvish linen for extra comfort.'
+      en: 'This mass-produced quiver is made with Elvish linen for extra comfort.',
+      ru: 'Это массовая продукция колчанов была сделана с Эльфийским льном для дополнительного комфорта'
     }
   },
 
   {
     name       : {$$: 'Rustic Quiver' },
     description: {
-      en: 'Rustic Quivers are reliable and battle-tested. These quivers are often passed on to apprentice archers as they progress.'
+      en: 'Rustic Quivers are reliable and battle-tested. These quivers are often passed on to apprentice archers as they progress.',
+      ru: 'Деревенские колчаны надежны и проверены в бою. Эти колчаны часто передаются ученикам лучников по мере их развития.'
     }
   },
 
   {
     name       : {$$: 'Snakeskin Quiver' },
     description: {
-      en: 'These quivers are made from snake scales from special monsters. Typically, snake skins are found after the giant snakes have shed their skin; to attempt a snakeskin collection otherwise would be a high risk.'
+      en: 'These quivers are made from snake scales from special monsters. Typically, snake skins are found after the giant snakes have shed their skin; to attempt a snakeskin collection otherwise would be a high risk.',
+      ru: 'Эти колчаны сделаны из змеиной чешуи из особых монстров. Обычно эти шкуры ищут после того, как гигантские змеи сбросили свою кожу; в противном случае сбор кожи будет иметь большой риск.'
     }
   },
 
   {
     name       : {$$: 'Reinforced Exemplar' },
     description: {
-      en: 'The exquisite craftsmanship of Markay’ak resulted in the creation of Reinforced Exemplars. Quivers such as this are often a testament to the pride of any professional archer.'
+      en: 'The exquisite craftsmanship of Markay’ak resulted in the creation of Reinforced Exemplars. Quivers such as this are often a testament to the pride of any professional archer.',
+      ru: 'Изысканное мастерство Markay\’ak привело к созданию Reinforced Exemplars. Колчаны, подобные этому, часто являются свидетельством гордости любого профессионального лучника.'
     }
   },
 
   {
     name       : {$$: 'Lazarus\'s Revenge' },
     description: {
-      en: 'An immortal from an elder race created quivers with an inhuman power in order to manifest his revenge upon the world.'
+      en: 'An immortal from an elder race created quivers with an inhuman power in order to manifest his revenge upon the world.',
+      ru: 'Бессмертный из древней расы создал колчан с нечеловеческой силой, чтобы явить свою месть миру.'
     }
   },
 
   {
     name       : {$$: 'Dragonhide Cynosure' },
     description: {
-      en: 'This quiver is made of dragon scales found on the ground. Dragon scales are a very difficult crafting material, so only the best craftsmen can use them.'
+      en: 'This quiver is made of dragon scales found on the ground. Dragon scales are a very difficult crafting material, so only the best craftsmen can use them.',
+      ru: 'Этот колчан сделан из драконьей чешуи, найденной на земле. Чешуя дракона - очень сложный материал для изготовления, поэтому использовать её могут только лучшие мастера.'
     }
   },
 
   {
     name       : {$$: 'Lotharien' },
     description: {
-      en: 'Lothariens are quivers that are named after a people warred out of existence. The Lotharien people battled surrounding territories for power acquisition while using these quivers.'
+      en: 'Lothariens are quivers that are named after a people warred out of existence. The Lotharien people battled surrounding territories for power acquisition while using these quivers.',
+      ru: 'Lothariens - это колчан, названный в честь народа, вытесненного из жизни. Lothariens боролись за власть на близлежащих территориях, используя эти колчаны.'
     }
   },
 
   {
     name       : {$$: 'Treant\'s Gift' },
     description: {
-      en: 'This quiver is a gift from the forest as a means for nature spirits to reclaim their stolen power, territory, and respect.'
+      en: 'This quiver is a gift from the forest as a means for nature spirits to reclaim their stolen power, territory, and respect.',
+      ru: 'Этот колчан - подарок от леса, как средство для духов природы, чтобы вернуть себе украденную власть, территорию и уважение.'
     }
   },
 
   {
     name       : {$$: 'Vodhrai' },
     description: {
-      en: 'The Vodhrai are the embodiment of vengeance, anger, wrath, and determination. The unusually strong emotions from this living quiver might serve to corrupt the user.'
+      en: 'The Vodhrai are the embodiment of vengeance, anger, wrath, and determination. The unusually strong emotions from this living quiver might serve to corrupt the user.',
+      ru: 'Vodhrai являются воплощением мести, гнева, ярости и решимости. Необычайно сильные эмоции от этого живого колчана могут повредить обладателя.'
     }
   },
 
   {
     name       : {$$: 'Pompeii' },
     description: {
-      en: 'This quiver was once found near the ruins of an entire civilization. There are no surviving records about the quiver in the region from this time period.'
+      en: 'This quiver was once found near the ruins of an entire civilization. There are no surviving records about the quiver in the region from this time period.',
+      ru: 'Этот колчан однажды был обнаружен рядом с руинами целой цивилизации. Нет сохранившихся записей о колчане в регионе за этот период времени.'
     }
   }
 
