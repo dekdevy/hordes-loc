ba0355e6ca415b2ea18ab0329a976287646b2823
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-26 19:00:39 +0000

    Russian rune translations.

--- loc/items/rune.js
+++ loc/items/rune.js
@@ -2,77 +2,88 @@ export default [
   {
     name       : {$$: 'Lucid' },
     description: {
-      en: 'This flat material is commonly found in mines; monsters often mine for Lucid to support their economies.'
+      en: 'This flat material is commonly found in mines; monsters often mine for Lucid to support their economies.',
+      ru: 'Этот плоский материал часто встречается в шахтах; монстры часто добывают Lucid, чтобы поддержать свою экономику.'
     }
   },
 
   {
     name       : {$$: 'Melant' },
     description: {
-      en: 'The curves of Melant give the appearance of writing or symbols, but blacksmiths believe these natural formations reveal a powerful internal structure.'
+      en: 'The curves of Melant give the appearance of writing or symbols, but blacksmiths believe these natural formations reveal a powerful internal structure.',
+      ru: 'Изгибы Melant создают видимость письма или символов, но кузнецы считают, что эти природные образования открывающие мощную внутреннюю структуру.'
     }
   },
 
   {
     name       : {$$: 'Turim' },
     description: {
-      en: 'This useful material originates from a great volcano and the surrounding region. Turim were once frequently traded between caravans.'
+      en: 'This useful material originates from a great volcano and the surrounding region. Turim were once frequently traded between caravans.',
+      ru: 'Этот полезный материал происходит из большого вулкана и окружающего региона. Turim когда-то часто торговали между караванами.'
     }
   },
 
   {
     name       : {$$: 'Fundo' },
     description: {
-      en: 'This robust material was recently found to be useful for upgrades when blacksmiths began experimenting with "worthless Fundo." Since that time, Fundo have been considered a valuable resource.'
+      en: 'This robust material was recently found to be useful for upgrades when blacksmiths began experimenting with "worthless Fundo." Since that time, Fundo have been considered a valuable resource.',
+      ru: 'Этот прочный материал недавно был признан полезным для улучшения, когда кузнецы начали экспериментировать с "бесполезным Fundo". С тех пор Fundo считается ценным ресурсом.'
     }
   },
 
   {
     name       : {$$: 'Amari' },
     description: {
-      en: ' Amari are often given as gifts from overseas kingdoms. Scholars say that Amari were first discovered when a race of sea creatures made first contact with several land kingdoms and brought Amari from the sea floor.'
+      en: 'Amari are often given as gifts from overseas kingdoms. Scholars say that Amari were first discovered when a race of sea creatures made first contact with several land kingdoms and brought Amari from the sea floor.',
+      ru: 'Amari часто дарят в качестве подарков от заморских королевств. Ученые говорят, что Amari были впервые обнаружены, когда раса морских существ впервые вступила в контакт с несколькими королевствами суши и привезла Amari с морского дна.'
     }
   },
 
   {
     name       : {$$: 'Purum' },
     description: {
-      en: 'Often mistaken for gold, this material was once thought to be the same material as dragon scales. Many ancient decorations and weapons are adorned with Purum.'
+      en: 'Often mistaken for gold, this material was once thought to be the same material as dragon scales. Many ancient decorations and weapons are adorned with Purum.',
+      ru: 'Часто ошибочно принимаемый за золото, этот материал когда-то считался тем же самым материалом, что и чешуя дракона. Многие древние украшения и оружие украшены Purum.'
     }
   },
 
   {
     name       : {$$: 'Royal' },
     description: {
-      en: 'Royals are often owned and collected by kings and queens to be used as ceremonial gifts during arranged marriages.'
+      en: 'Royals are often owned and collected by kings and queens to be used as ceremonial gifts during arranged marriages.',
+      ru: 'Royals зачастую пренадлежит и собирается королями и королевами что бы исользовать в качестве подарков на церемониях браков по договорённости.'
     }
   },
 
   {
     name       : {$$: 'Tara' },
     description: {
-      en: ' These formations naturally occur most often near Yggdrasil, the World Tree. The beauty and power rarity of Tara make them equally suited for either jewelry heirlooms or item upgrades.'
+      en: 'These formations naturally occur most often near Yggdrasil, the World Tree. The beauty and power rarity of Tara make them equally suited for either jewelry heirlooms or item upgrades.',
+      ru: 'Эти образования естественным образом чаще всего встречаются вблизи Yggdrasil, the World Tree. Красота и редкость Tara делает их одинаково подходящими как для ювелирных изделий, так и для улучшения.'
     }
   },
 
   {
     name       : {$$: 'Gloom' },
     description: {
-      en: 'Once the secret methods of enhancing equipment using the power of Gloom were discovered, those methods were shortly sold to an opposing kingdom as one of the biggest betrayals in the history of the realms.'
+      en: 'Once the secret methods of enhancing equipment using the power of Gloom were discovered, those methods were shortly sold to an opposing kingdom as one of the biggest betrayals in the history of the realms.',
+      ru: 'Как только были обнаружены секретные методы улучшения экипировки с использованием силы Gloom, эти методы тут же были проданы в противоборствующее королевство. Это считается одним из самых больших предательств в истории королевства.'
     }
   },
 
   {
     name       : {$$: 'Plurae' },
     description: {
-      en: 'Unusual crystalline properties in Plurae make it an exceptional material. Although Plurae were only recently rediscovered in the forgotten depths of the world, the applications of Plurae have already been substantial in turning the tides of war.'
+      en: 'Unusual crystalline properties in Plurae make it an exceptional material. Although Plurae were only recently rediscovered in the forgotten depths of the world, the applications of Plurae have already been substantial in turning the tides of war.',
+      ru: 'Необычные кристаллические свойства Plurae делают его исключительным материалом. Хотя Plurae только недавно были вновь открыты в забытых глубинах мира, применение Plurae уже сыграло существенную роль в изменении течения войны.'
     }
   },
 
   {
     name       : {$$: 'Aeter' },
     description: {
-      en: 'Although Aeter were once considered gifts from the gods by queens and kings of the past, written histories claim that Aeter were found inside black meteorites.'
+      en: 'Although Aeter were once considered gifts from the gods by queens and kings of the past, written histories claim that Aeter were found inside black meteorites.',
+      ru: 'Хоть королями и королевами прошлого Aeter когда-то и считался дарами богов, письменные истории утверждают, что ветер был найден внутри черных метеоритов.'
     }
   }
 ]
