eb532bfcd3379e7ce70a5450a749ab63901475e5
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2019-11-27 20:44:24 +0000

    Turkish bag translations.

--- loc/items/bag.js
+++ loc/items/bag.js
@@ -3,7 +3,8 @@ export default [
     name       : {$$: 'Linen Pouch' },
     description: {
       en: 'This pouch is made from a delicate fabric. Pouches such as this are often owned by common people for carrying their belongings.',
-      ru: 'Этот мешок сделан из нежной ткани. Мешки, подобные этому, часто принадлежат простым людям для перевозки своих вещей.'
+      ru: 'Этот мешок сделан из нежной ткани. Мешки, подобные этому, часто принадлежат простым людям для перевозки своих вещей.',
+      tr: 'Bu torba narin bir kumaştan yapıldı. Bunun gibi torbalar, sıradan insanlar tarafından eşyalarını taşımak için sıklıkla kullanılır.'
     }
   },
 
@@ -11,7 +12,8 @@ export default [
     name       : {$$: 'Adventurer\'s Rucksack' },
     description: {
       en: 'This rugged sack is crafted from sturdy fabrics to hold more equipment than a linen pouch.',
-      ru: 'Этот прочный мешок сделан из крепких тканей и может содержать в себе больше вещей, чем Linen Pouch.'
+      ru: 'Этот прочный мешок сделан из крепких тканей и может содержать в себе больше вещей, чем Linen Pouch.',
+      tr: 'Bu engebeli çanta, bir Linen Pouch\'tan daha fazla ekipman taşıyabilmesi için sağlam kumaşlardan yapıldı.'
     }
   },
 
@@ -19,7 +21,8 @@ export default [
     name       : {$$: 'Purpur Duffel' },
     description: {
       en: 'This robust sack is made from monster hides and permits a respectable inventory.',
-      ru: 'Этот прочный мешок сделан из шкур монстров и предлагает респектабельный инвентарь.'
+      ru: 'Этот прочный мешок сделан из шкур монстров и предлагает респектабельный инвентарь.',
+      tr: 'Canavar kalıntılarından yapılmış bu sağlam çanta gayet büyük bir envanter sağlıyor.'
     }
   },
 
@@ -27,7 +30,8 @@ export default [
     name       : {$$: 'Elven Saddlebag' },
     description: {
       en: 'This fine bag was crafted from expensive Elvish fabrics and contains more pockets than most other bags.',
-      ru: 'Эта прекрасная сумка была изготовлена из дорогих эльфийских тканей и содержит больше карманов, чем большинство других сумок.'
+      ru: 'Эта прекрасная сумка была изготовлена из дорогих эльфийских тканей и содержит больше карманов, чем большинство других сумок.',
+      tr: 'Bu güzel çanta, Elf kumaşlarından yapıldı ve diğer çantalardan daha fazla cebe sahip.'
     }
   },
 
@@ -35,7 +39,8 @@ export default [
     name       : {$$: 'Moss Enigma' },
     description: {
       en: 'Bags such as this are a gift from nature to secret druid societies. Unfortunately, monsters have disposed of many druids over time and have stolen these sacred bags.',
-      ru: 'Такие сумки являются подарком от природы тайным обществом друидов. К сожалению, монстры со временем избавлялись от многих друидов и украли эти священные сумки.'
+      ru: 'Такие сумки являются подарком от природы тайным обществом друидов. К сожалению, монстры со временем избавлялись от многих друидов и украли эти священные сумки.',
+      tr: 'Bunun gibi çantalar, gizli rahip topluluklarından doğal bir armağandır. Ne yazık ki, canavarlar birçok rahipten kurtulup bu kutsal çantaları çalmışlardır.'
     }
   }
 
