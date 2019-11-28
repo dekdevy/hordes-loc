a96380934ec48b58e3f17dd9c133838d7bd5dd62
qdysfqrek <58287757+qdysfqrek@users.noreply.github.com>
2019-11-28 09:52:46 +0200

    Update bloodlust.js

--- loc/factions/bloodlust.js
+++ loc/factions/bloodlust.js
@@ -7,7 +7,7 @@ export default {
   // Do NOT translate "Bloodlust" or "Headless Landing".
   description: {
     en: 'The Bloodlust horde values freedom and loyalty, offers a place for individualism or even chaos. Their defenses are located in a desert realm called Headless Landing.',
-    ru: 'Орда Bloodlust ценит свободу и лояльность, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном королевстве, названном Headless Landing.',
+    ru: 'Фракция Bloodlust ценит свободу и лояльность, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном королевстве, названном Headless Landing.',
     es: 'La horda Bloodlust valora la libertad y lealtad, ofrece un lugar para el individualismo o incluso el caos. Sus defensas están ubicadas en un reino del desierto llamado Headless Landing.',
     fr: 'La faction des Bloodlust valorise la liberté et la loyauté. Elle offre une place à l\'individualisme et même au chaos. Leurs défenses sont situées dans un royaume désertique appelé Headless Landing.',
     de: 'Die Horde der Bloodlust schätzt Freiheit und Loyalität, bietet einen Ort für Individualismus oder gar Chaos. Ihre Verteidigung befindet sich in einem Wüstenreich namens Headless Landing.',
