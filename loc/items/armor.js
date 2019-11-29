5768edc3343d0a05e3cfe226567eb3fd5776b972
Egafgo <56559297+Egafgo@users.noreply.github.com>
2019-11-29 17:34:38 +0100

    Hungarian armor transalations

--- loc/items/armor.js
+++ loc/items/armor.js
@@ -3,6 +3,7 @@ export default [
     name       : {$$: 'Potato Sack' },
     description: {
       en: 'This is a surprisingly sturdy material that can be used as a protective clothing. The makeshift nature of this material is obvious, so many adventurers choose to replace the potato sack as quickly as possible.',
+      hu: 'Ez a meglepően erős anyag képes tompítani a csapások erejét. A készítő képzetlensége egyértelmű, nem csoda hogy a legtöbb kalandor minél hamarabb le akarja cserélni a krumplizsákot.',
       ru: 'Это удивительно прочный материал, который можно использовать в качестве защитной одежды. Самодельная природа этого материала очевидна, так что многие приключеники предпочитают заменить картофельный мешок как можно скорее.'
     }
   },
@@ -11,6 +12,7 @@ export default [
     name       : {$$: 'Faded Garment' },
     description: {
       en: 'This garment is a family heirloom that has been passed down from one adventuring generation to another.',
+      hu: 'Ez a garnaméta egy családi kincs, amely nemzedékről nemzedékre szállva kalandozott a világban.',
       ru: 'Это семейная реликвия, которая передавалась из поколения в поколение.'
     }
   },
@@ -19,6 +21,7 @@ export default [
     name       : {$$: 'Adventurer\'s Tunic' },
     description: {
       en: 'This tunic is crafted from robust fabrics that are designed to withstand wilderness survival. Some traditions involve giving tunics as gifts to adventurers that are deemed worthy by an organization.',
+      hu: 'Ez a tunika vastag szálakból van elkészítve, hogy segítséget nyújtson a vadonban is a túléléshez. Egyesek az ilyen tunikát a klánja által reá méltónak tartott harcosoknak ajándékképp adják.',
       ru: 'Эта туника изготовлена из прочных тканей, которые разработаны, чтобы противостоять выживанию в дикой природе. Некоторые традиции включают в себя вручение туник в качестве подарка искателям приключений, которые организация считает достойными.'
     }
   },
@@ -27,6 +30,7 @@ export default [
     name       : {$$: 'Leather Jerkin' },
     description: {
       en: 'This leather armor is a common choice for traveling adventurers. The trend of wearing leather jerkins originated around Yggdrasil when dangerous creatures began to lurk in the nearby jungles.',
+      hu: 'Ez a bőrvért a nagy utazók legnagyobb kedveltje. A használatuk akkor terjedt el, amikor a Yggdrasil körüli esőerdőkbe veszedelmes bestiák áramlottak be.',
       ru: 'Эта коженая броня является общим выбором для путешественников. Тенденция ношения козлиной кожи возникла вокруг Yggdrasil, когда в близлежащих джунглях начали таиться опасные существа.'
     }
   },
@@ -35,6 +39,7 @@ export default [
     name       : {$$: 'Scaled Chestguard' },
     description: {
       en: 'This armor is made of monster scales that were found on the ground. Monster scales are often crafted into respectable armor whenever metal is in short supply.',
+      hu: 'Ezt a páncélt pikkelyek alkotják, amik vérmes szörnyetegekről estek a földre. A pikkelyeket gyakran gyűjtik páncélba, fémszegénység idején.',
       ru: 'Эта броня сделана из чешуек монстров, которые были найдены на земле. Чешуя монстров часто изготавливается в респектабельные доспехи, когда не хватает металла.'
     }
   },
@@ -43,6 +48,7 @@ export default [
     name       : {$$: 'Sky Mail' },
     description: {
       en: 'Light Elvish metals are used to create divine chainmails. Old legends say that the first sky mails were originally created from discarded armor pieces once worn by divine creatures.',
+      hu: 'A könnyű Elf fémek áldott láncingek kovácsolására kiválóan alkalmasak. Egyesek szerint az első áldott ingek eredetileg szentek eldobott páncélszilánkjaiból álltak.',
       ru: 'Легкие эльфийские металлы используются для создания божественных кольчуг. Старые легенды гласят, что первые небесные кольчуги изначально создавались из частей брони, которые когда-то носили божественные создания.'
     }
   },
@@ -51,6 +57,7 @@ export default [
     name       : {$$: 'Shadow Cloak' },
     description: {
       en: 'This cloak summons small shadows that envelop the wearer during battle to enhance protection. Cloaks such as these are crafted with strange shadow magic and then alchemically enhanced in secret ways.',
+      hu: 'Ez a köpeny kis árnyakat varázsol elő, amik elfedik a viselőjét, hogy megvédjék a csatákban. Ilyen köpönyegek különös árnyékmágiával készülnek, azután pedig titkos alkímiával bűvölik el őket.',
       ru: 'Этот плащ вызывает небольшие тени, которые окружают хозяйна во время боя, чтобы усилить защиту. Такие плащи создаются с помощью странной теневой магии, а затем тайно обогащаются алхимическим путем.'
     }
   },
@@ -59,6 +66,7 @@ export default [
     name       : {$$: 'Runic Halfplate' },
     description: {
       en: 'Magic runes were etched into this armor to provide additional protection. Some scholars believe the runes on all runic halfplate armors are meant to interact together in some larger, undiscovered ritual.',
+      hu: 'Rúnaköveket vertek a páncélba megnövelt védelemért. A varázslótanoncok úgy hiszik, minden rúnakő a vérten egymással egyesülhet egy különös és veszedelmes, fel nem fedezett vérszerződés során.',
       ru: 'Волшебные руны были выгравированы на этой броне, чтобы обеспечить дополнительную защиту. Некоторые ученые полагают, что руны на всех доспехах предназначены для взаимодействия в более крупных, нераскрытых ритуалах.'
     }
   },
@@ -67,6 +75,7 @@ export default [
     name       : {$$: 'Hellfire Fullplate' },
     description: {
       en: 'Supernatural infernos were used in the making of this armor. Great sacrifices were made to appease the demands of devilish creatures so that a few blacksmiths could forge with the strange flames.',
+      hu: 'Természetfeletti pokoltűz koholta ezt a vértet. Az ördögi szörnyetegek alkuját pedig csak hatalmas emberáldozatokkal lehetett teljesíteni, hogy néhány kovács megtanulhasson egy ilyet elkészíteni.',
       ru: 'При изготовлении этих доспехов использовались сверхъестественное пламя. Крупные жертвы были принесены, для того чтобы успокоить требования дьявольских существ, чтобы несколько кузнецов могли ковать с этим странным огнем.'
     }
   },
@@ -75,6 +84,7 @@ export default [
     name       : {$$: 'Soulkeeper' },
     description: {
       en: 'Ancient legends say that each Soulkeeper armor harbors a soul that willingly sacrificed itself to enhance the armor’s protective power. In harder times, individuals made great sacrifices to ensure the survival of their clans and families.',
+      hu: 'Az Ősi Legendák szerint a Lélekőrző páncél magához vonzza a lelket, amely feláldozta magát, hogy megnövelje a vért erejét. Nehéz időkben sokan ezt az áldozatot hozták, hogy biztosítsák klánjuk, törzsük, vagy családjuk túlélését.',
       ru: 'Древние легенды гласят, что в каждом доспехе хранится душа, которая добровольно пожертвовала собой, чтобы усилить защитную силу доспеха. В более трудные времена люди идут на большие жертвы, чтобы обеспечить выживание своих кланов и семей.'
     }
   },
@@ -83,6 +93,7 @@ export default [
     name       : {$$: 'Deathless' },
     description: {
       en: 'Deathless armors were created by the first kings and leaders as a means to prevent mortal wounds during the First Great War. True to the legend, those kings and leaders did not perish in battle; instead, they were each assassinated in their sleep and their armors were stolen. The forging techniques were lost to time, but massive fortunes were clearly necessary for the process.',
+      hu: 'Ezek a vértek ősi császárok készíttették kovácsaikkal, hogy megvédjék magukat a halandó sebektől a Nagy Háború során. A legenda szerint, azok az uralkodók nem harcban estek el; az ágyukban ölték meg őket bérgyilkosok, akik lehúzták róluk előtte a vértet. A kovácsolási módszer ismeretlen, de annyi bizonyos, hogy óriási vagyon volt szügséges hozzá.',
       ru: 'Такие доспехи были созданы первыми царями и вождями, чтобы предотвратить смертельные ранения во время "The First Great War". По преданию легенды, эти цари и вожди не погибли в бою, а были убиты во сне, и их доспехи были украдены. Методы ковки были потеряны со временем, но для этого процесса явно требовались огромные силы и жертвы.'
     }
   }
