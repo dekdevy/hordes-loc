8db233b722ce4fc76beddad01e4dddcd561a74a0
dek <polydek@googlemail.com>
2019-12-03 21:16:12 +0100

    Update mount.js

--- loc/items/mount.js
+++ loc/items/mount.js
@@ -12,5 +12,54 @@ export default [
     description: {
       en: 'A domesticated spider.'
     }
+  },
+  {
+    name: {$$: 'Leafspider'},
+
+    description: {
+      en: 'A domesticated spider.'
+    }
+  },
+  {
+    name: {$$: 'Ruby Crawler'},
+
+    description: {
+      en: 'A domesticated spider.'
+    }
+  },
+  {
+    name: {$$: 'Tick'},
+
+    description: {
+      en: 'A domesticated spider.'
+    }
+  },
+  {
+    name: {$$: 'Shadowstrider'},
+
+    description: {
+      en: 'Roaming the shadows.'
+    }
+  },
+  {
+    name: {$$: 'Pebbles'},
+
+    description: {
+      en: 'These rocks have a soul.'
+    }
+  },
+  {
+    name: {$$: 'Ember Pebbles'},
+
+    description: {
+      en: 'Extremely hot rocks.'
+    }
+  },
+  {
+    name: {$$: 'Ether Wyrm'},
+
+    description: {
+      en: 'Peaceful, transcendental creature.'
+    }
   }
 ]
