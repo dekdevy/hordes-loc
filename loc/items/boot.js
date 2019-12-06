7cccd25d878de3f82c8dfb2ad0c8227408541421
Egafgo <56559297+Egafgo@users.noreply.github.com>
2019-12-06 19:44:36 +0100

    Hungarian Boots transaltions

--- loc/items/boot.js
+++ loc/items/boot.js
@@ -3,6 +3,7 @@ export default [
     name       : {$$: 'Sandals' },
     description: {
       en: 'This footwear is primarily made for comfort rather than running.',
+      hu: 'Ez a lábbeli inkább a kényelmet mintsem a védelmet szolgálja.',
       ru: 'Эта обувь больше предназначена для комфорта, чём для бега.'
     }
   },
@@ -11,6 +12,7 @@ export default [
     name       : {$$: 'Cloth Footpads' },
     description: {
       en: 'These footpads are wrapped in a makeshift material to increase running stability.',
+      hu: 'Ezeket a bocskorokat erős szövetbe csavarták, hogy megkönnyítsék a futást.',
       ru: 'Эта обувь обернута грубым материалом для повышения стабильности бега.'
     }
   },
@@ -19,6 +21,7 @@ export default [
     name       : {$$: 'Leather Shoes' },
     description: {
       en: 'Leather shoes are created with fine craftsmanship for moderate battle durability.',
+      hu: 'Az erős bőr megfelelő megmunkálás után akár harci védelmet is nyújthat.',
       ru: 'Эта кожаная обувь сделана с тонким мастерством для умеренной боевой прочности.'
     }
   },
@@ -27,6 +30,7 @@ export default [
     name       : {$$: 'Bone Brogans' },
     description: {
       en: 'The monster bones used in the creation of footwear such as this seem to enhance movement speed, but whether the enhancement is through magic or the lightness of the material is not known.',
+      hu: 'A szörnyetegek a lábbeli alkotásakor a futási képességre is ki akartak hatni. Hogy ezt egyszerűen a csizma könnyedsége okozza, vagy valamiféle bűbály; nem tudni egyenlőre.',
       ru: 'При изготовлении обуви использовались кости монстров, похоже, увеличивают скорость передвижения, но вопрос в том, происходит ли это с помощью магии или из-за легкости материала, остается неизвестным.'
     }
   },
@@ -35,6 +39,7 @@ export default [
     name       : {$$: 'Scaled Treads' },
     description: {
       en: 'This footwear is carefully crafted with monster scales that are both light and robust for battle.',
+      hu: 'Ezek a bocskorokat nagy körültekintéssel készítették, így elég könnyűek és robosztusok is a csatára.',
       ru: 'Эта обувь тщательно изготовлена с использованием чешуек монстров, которые одновременно легки и прочны в бою.'
     }
   },
@@ -43,6 +48,7 @@ export default [
     name       : {$$: 'Shadow Shoes' },
     description: {
       en: 'Enchanted shoes such as this push the wearer\'s feet with shadow magic to augment speed.',
+      hu: 'Az ehhez hasonlatos bűvös lábbelik az árnyékmágiával irányítják viselőjük lábát, nagy sebességet biztosítva.',
       ru: 'Волшебный ботинки, как эти, толкают ноги владельца с помощью теневой магии, чтобы увеличить скорость.'
     }
   },
@@ -51,6 +57,7 @@ export default [
     name       : {$$: 'Wartorn Boots' },
     description: {
       en: 'These boots are crafted from sturdy iron and have been used in many wars.',
+      hu: 'Ezek kemény, tömör vascsizmák, amiket számos nagy háborúban viseltek.',
       ru: 'Эти ботинки изготовлены из прочного железа и использовались во многих войнах.'
     }
   },
@@ -59,6 +66,7 @@ export default [
     name       : {$$: 'Imbued Treads' },
     description: {
       en: 'Footwear such as this is etched with runes that increase the wearer’s speed. However, scholars are hesitant about the use of these runes since their true purpose and nature is unclear.',
+      hu: 'Az ilyen bocskorokat rúnákkal erősítették meg, amik megnövelik a viselő mozgékonyságát. Ám a mágus társaságok óvakodnak ezektől, mondván, a rúnák mibenléte és működése ismeretlen számukra.',
       ru: 'Обувь, подобная этой, выгравирована рунами, которые увеличивают скорость владелеца. Однако ученые неуверенные по поводу использования этих рун, поскольку их истинное назначение и природа неясны.'
     }
   },
@@ -67,6 +75,7 @@ export default [
     name       : {$$: 'Skyswift Boots' },
     description: {
       en: 'Elvish magic is assisted by wind spirits to create Skyswift Boots. Although Elves are very close to nature, even they know little about the reclusive wind spirits.',
+      hu: 'Az Elf mágia a szelek szellemeivel összefogva fonja az Égsebes Csizmákat. Habár az Elfek igen közel állanak efféle jószágokhoz, elég keveset tudnak a haragos viharszellemről.',
       ru: 'Эльфийская магия создаёт такие ботинки при помощи духов ветра. Хоть эльфы и очень близки к природе, но даже они мало знают о духах ветра.'
     }
   },
@@ -75,6 +84,7 @@ export default [
     name       : {$$: 'Coldforged Greaves' },
     description: {
       en: 'These boots are forged without heat and with enchanted hammers to keep the innate properties of the special metal. This rare metal is most often found in the mountains.',
+      hu: 'A híres hő nélküli kovácsolás termékei ezek a lábbelik is; bűvölt kalapácsokkal tartják meg a ritka fém veleszületett varázserejét. Ez a fém leggyakrabban a hegyekben fordul elő.',
       ru: 'Эти ботинки выкованы без нагрева, с волшебными молотками, чтобы сохранить врожденные свойства специального металла. Этот редкий металл чаще всего встречается в горах.'
     }
   },
@@ -83,6 +93,7 @@ export default [
     name       : {$$: 'Cloudrunners' },
     description: {
       en: 'These boots are created by monster craftsmen with the help of sky spirits. Some monster factions have a close connection with sky spirits and nature; however, monsters do not frequently talk about this relationship.',
+      hu: 'A bocskorok égi szellemek és szörnyetegek művei. Néhány bestiaszövetség szoros kapcsolatokat ápol a légi lényekkel és a természettel; habár, nem kürtölik ezt sem világgá. Különös jószágok már csak ezek.',
       ru: 'Эти ботинки были созданы мастерами-монстрами с помощью небесных духов. Некоторые фракции монстров имеют тесную связь с небесными духами и природой, однако, монстры не часто говорят об этих отношениях.'
     }
   },
@@ -91,6 +102,7 @@ export default [
     name       : {$$: 'Talaria' },
     description: {
       en: 'These boots are named after a famous relic legend. The namesake of these boots were supposedly the fast footwear of a famous deity, but no one seems to know where the story originates.',
+      hu: 'Egy híres leganda után nevezték el a csizmákat. A lábbelik névadója egy ismert istenség gyors saruja volt, de azt senki nem tudja, honnan származik a történet, és ki is volt ez az istenség.',
       ru: 'Эти ботинки названы в честь знаменитой реликтовой легенды. Тезка этих ботинок предположительно была быстрой обувью знаменитого божества, но, похоже, никто не знает, откуда взялась эта история.'
     }
   },
@@ -99,6 +111,7 @@ export default [
     name       : {$$: 'Starshards' },
     description: {
       en: 'This footwear is composed of meteorite pieces. Black meteorites were found during a terrible catastrophe where fire rained down from the sky and destroyed much of the landscape.',
+      en: 'Ezt a lábbelit meteoritszilánkok alkotják. Fekete kövek a borzalmas katasztrófák során kerülnek a földre, amikor az égbolt meggyullad, és a kénköves pokol elszabadul mindenfelé.',
       ru: 'Эта обувь состоит из осколков метеорита. Черные метеориты были обнаружены во время страшной катастрофы, когда огонь падал с небес и уничтожил большую часть ландшафта.'
     }
   }
