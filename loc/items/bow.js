de94516529bfd2391ee7ae49f62ece4698cca0e4
Malfrador <malfrador@gmail.com>
2019-12-12 22:55:00 +0100

    Added german descriptions for all bows
    Tried to keep them short, was hard ^^

--- loc/items/bow.js
+++ loc/items/bow.js
@@ -4,7 +4,8 @@ export default [
     description: {
       en: 'This bow is a makeshift creation. Wood is not a plentiful resource in some regions, so denizens sometimes use the materials on hand to create weapons such as this.',
       hu: 'Ez az íj egy egyszerű barkács; a fa nem nő ki mindenhol, ne csudáljuk, hogy egyes népek megpróbálják a legtöbbet a kéznél lévő anyagokból kicsikarni.',
-      ru: 'Этот лук - самоделка. Древесина не является богатым ресурсом в некоторых регионах, поэтому жители иногда используют имеющиеся материалы для создания такого оружия, как это.'
+      ru: 'Этот лук - самоделка. Древесина не является богатым ресурсом в некоторых регионах, поэтому жители иногда используют имеющиеся материалы для создания такого оружия, как это.',
+      de: 'Dieser Bogen ist eine provisorische Kreation. Holz ist eine seltene Ressource in manchen Regionen, daher nutzen Bewohner manchmal vorhandene Materialien, um solche Waffen herzustellen.'
     }
   },
 
@@ -13,7 +14,8 @@ export default [
     description: {
       en: 'Bows such as these are crafted for beginning archers to hone their skills.',
       hu: 'Az ilyen íjakat fiatal lövészek kapják, hogy legyen mivel gyakorolniuk.',
-      ru: 'Такие луки созданы для начинающих лучников, чтобы улучшать свои навыки.'
+      ru: 'Такие луки созданы для начинающих лучников, чтобы улучшать свои навыки.',
+      de: 'Bögen wie dieser werden von Anfänger-Bogenschützen zum Üben genutzt.'
     }
   },
 
@@ -22,7 +24,8 @@ export default [
     description: {
       en: 'Curved shortbows are mostly produced for hunting local wildlife.',
       hu: 'A kézíjakkal remekül lehet vadászni, ezért is olyan elterjedtek.',
-      ru: 'Такие луки в основном производятся для охоты на местных диких животных.'
+      ru: 'Такие луки в основном производятся для охоты на местных диких животных.',
+      de: 'Gekrümmte Kurzbögen werden hauptsächlich zur Jagd hergestellt.'
     }
   },
 
@@ -31,7 +34,8 @@ export default [
     description: {
       en: 'This type of bow is a standard weapon of choice for adventurers.',
       hu: 'Ez az íj az általános fegyverválasztása a legtöbb kalandozónak.',
-      ru: 'Этот тип лука является стандартным выбором оружия для искателей приключений.'
+      ru: 'Этот тип лука является стандартным выбором оружия для искателей приключений.',
+      de: 'Diese Sorte Bogen ist die Standardwaffe für Abenteurer.'
     }
   },
 
@@ -40,7 +44,8 @@ export default [
     description: {
       en: 'Longbows are designed to be weapons of war.',
       hu: 'A hosszúíjakat egyetlen célra szerkesztették, mégpedig háborúra.',
-      ru: 'Длинный лук задуман как оружие войны.'
+      ru: 'Длинный лук задуман как оружие войны.',
+      de: 'Langbögen sind als Kriegswaffen entwickelt worden.'
     }
   },
 
@@ -49,7 +54,8 @@ export default [
     description: {
       en: 'This bow is crafted from monster bones to produce extra resilience and power.',
       hu: 'Ez az íj csontokból áll, hogy magas szintű rugalmasságot és erőt biztosítson.',
-      ru: 'Этот лук сделан из костей монстров для обеспечения дополнительной упругости и силы.'
+      ru: 'Этот лук сделан из костей монстров для обеспечения дополнительной упругости и силы.',
+      de: 'Dieser Bogen wurde aus Monster-Knochen hergestellt um sehr belastbar und stark zu sein.' 
     }
   },
 
@@ -58,7 +64,8 @@ export default [
     description: {
       en: 'Elvish craftsmen put exquisite care in their bows during the crafting process, and Elvish archers have a proud reputation during times of war for a reason.',
       hu: 'Az Elf mesteremberek kivételes gondot fektetnek az íjaikba, és az íjászaik mindig kitüntetik magukat a harci időszakokban, mégpedig nem akármiért.',
-      ru: 'Эльфийские мастера проявляют исключительную осторожность во время изготовления своих луков. Эльфийские лучники имеют восхитительную военную репутацию.'
+      ru: 'Эльфийские мастера проявляют исключительную осторожность во время изготовления своих луков. Эльфийские лучники имеют восхитительную военную репутацию.',
+      de: 'Elfische Handwerker sind während des Herstellungsprozesses besondersd sorgfältig, und elfische Bogenschützen haben in Kriegszeiten aus gutem Grund einen guten Ruf.'
     }
   },
 
@@ -67,7 +74,8 @@ export default [
     description: {
       en: 'Ancient bows are exceptional weapons that were designed by forgotten master craftsmen. These bows are often passed from one generation to another.',
       hu: 'Az ősi íjak még vándorló ősmagyar törzsek keze munkái. Nemzedékről nemzedékre, apáról fiúra szálltak a fegyverek, és a készítésük titkai egyaránt.',
-      ru: 'Древние луки, как этот, исключительное оружие, созданное забытыми мастерами. Эти луки часто передаются из поколения в поколение как фамильная ценность.'
+      ru: 'Древние луки, как этот, исключительное оружие, созданное забытыми мастерами. Эти луки часто передаются из поколения в поколение как фамильная ценность.',
+      de: 'Altertümliche Bögen sind außergewöhnliche Waffen die von vergessenen Meisterhandwerken hergestellt wurden. Diese Bögen werden über Generationen weitergegeben.'
     }
   },
 
@@ -76,7 +84,8 @@ export default [
     description: {
       en: 'These bows were developed in response to armor-wearing monsters.',
       hu: 'Amint megjelentek a szörnyetegek talpig vasban, felütötték a fejüket efféle anarmnensek is.',
-      ru: 'Эти луки были разработаны чтобы легче справляться с броней монстров.'
+      ru: 'Эти луки были разработаны чтобы легче справляться с броней монстров.',
+      de: 'Diese Bögen wurden als Antwort auf gerüstete Monster entwickelt.'
     }
   },
 
@@ -85,7 +94,8 @@ export default [
     description: {
       en: 'This bow design is more mechanically efficient than the longbow. The unique pattern of decorative silver is a testament to the craftsmanship required to make such a fine bow.',
       hu: 'Ez az íjforma sokkal hatékonyabb, mint a hosszúíj. A dístítő ezüst különleges mintája tanúbizonsága annak a mesteri tudásnak, ami képes volt egy ilyen fegyver elkészítésére.',
-      ru: 'Эта конструкция лука более эффективна с механической точки зрения, чем длинный лук. Уникальный узор из декоративного серебра является свидетельством мастерства, необходимого для изготовления такого величественного лука.'
+      ru: 'Эта конструкция лука более эффективна с механической точки зрения, чем длинный лук. Уникальный узор из декоративного серебра является свидетельством мастерства, необходимого для изготовления такого величественного лука.',
+      de: 'Das Design dieses Bogens ist effizienter als das eines Langbogens. Das einmalige dekorative Silbermuster ist ein Zeugnis der Handwerkskunst, die für einen solchen Bogen benötigt wird.' 
     }
   },
 
@@ -94,7 +104,8 @@ export default [
     description: {
       en: 'Bows such as this were previously owned by assassins that made modifications to drastically increase efficiency. Assassins regularly modify their weapons to reach optimal performance.',
       hu: 'Az ehhez hasonló íjakat régebben képzett bérgyilkosok használták, akiknek a módosításai megnövelték az íj erejét. A bérgyilkosok szinte mindig feljavítják az íjukat a megfelelő teljesítményért.',
-      ru: 'Такие луки раньше принадлежали ассасинам , которые вносили изменения для резкого повышения эффективности. Ассасины регулярно модифицируют свое оружие для достижения оптимальных результатов.'
+      ru: 'Такие луки раньше принадлежали ассасинам , которые вносили изменения для резкого повышения эффективности. Ассасины регулярно модифицируют свое оружие для достижения оптимальных результатов.',
+      de: 'Bögen wie dieser befanden sich im Besitz von Assasinen, die Modifikationen vornahmen, um die Effizienz zu erhöhen. Assassinen modifizieren oft ihre Waffen, um optimale Leistung zu erlangen.'
     }
   },
 
@@ -103,7 +114,8 @@ export default [
     description: {
       en: 'Bows such as this were made from hellfires that are provided by devilish creatures. These kinds of bows are rare because of the ultimate price required for such wicked transactions and acquisitions.',
       hu: 'Ezeket a hatalmas íjakat ördögi jószágok pokoltüze formálta. Igen ritkák ezek az íjak, lévén az ár túlságosan hatalmas, hogy nyélbe üssék ezeket a megátalkodott, ördögi üzleteket.',
-      ru: 'Такие луки были сделаны из адского пламени, которое создают дьявольские существа. Луки, как этот, встречается редко из-за конечной цены, необходимой для таких злонамеренных сделок и приобретений.'
+      ru: 'Такие луки были сделаны из адского пламени, которое создают дьявольские существа. Луки, как этот, встречается редко из-за конечной цены, необходимой для таких злонамеренных сделок и приобретений.',
+      de: 'Solche Bögen wurden aus Höllenfeuer gemacht, das von teuflischen Kreaturen gespeist wurde. Diese Bögen sind da der Höchstpreis für solche bösen Transaktionen und Übernahmen erforderlich ist.'
     }
   },
 
@@ -112,7 +124,8 @@ export default [
     description: {
       en: 'This bow was bathed in the flames of a phoenix and cooled in the night sky. Bows created in this way are sometimes imbued with extra magical characteristics from phoenix fire.',
       hu: 'Ez az íj a Főnix lángjaiban fürdött, és az éjszakai ég hűtötte le. Azok az íjak, amiket így készítenek, gyakran vannak a Főnix lángjának sajátosságaival is elbűvölve.',
-      ru: 'Этот лук раскален в пламени феникса и охлажден в ночном небе. Созданные таким образом луки иногда наделяются дополнительными магическими свойствами от огня фениксов.'
+      ru: 'Этот лук раскален в пламени феникса и охлажден в ночном небе. Созданные таким образом луки иногда наделяются дополнительными магическими свойствами от огня фениксов.',
+      de: 'Dieser Bogen wurde im Feuer eines Phönix gebadet und im Nachthimmel gekühlt. Bögen die so hergestellt wurden besitzen manchmal magische Eigenschaften des Phönixfeuers.'
     }
   },
 
@@ -121,7 +134,8 @@ export default [
     description: {
       en: 'Widowmakers are living bows that bond with the user. Powerful monsters use a mystic process to create widowmakers; the resulting weapon can be remarkably jealous and entice the user not to use any other weapon by destroying foes with vast depths of battle experience.',
       hu: 'Az Özvegyítők élő íjak, amik egybefonódnak a használóval. A legerősebb szörnyetegek egy misztikus folyamattal készítik őket; az eredmény olyan remek lehet, hogy a használó soha többé nem vet szemet más anarnmentumra, mert a harci tapaszlatok éteri magasságba emelték gyilkoló művészetét.',
-      ru: 'Widowmakers - живые луки, которые привязаны к обладателю. Мощные монстры используют загадочный процесс создания этих луков; полученное оружие имеет удивительную ревность и побуждает обладателя не применять никакое другое оружие, уничтожая противников с огромным боевым опытом.'
+      ru: 'Widowmakers - живые луки, которые привязаны к обладателю. Мощные монстры используют загадочный процесс создания этих луков; полученное оружие имеет удивительную ревность и побуждает обладателя не применять никакое другое оружие, уничтожая противников с огромным боевым опытом.',
+      de: 'Widowmakers sind lebendige Bögen, die einen Bund mit ihrem Besitzer eingehen. Mächtige Monster stellen diese Bögen in einem mystischen Prozess her.  Die resultierende Waffe kann bemerkenswert eifersüchtig sein und den Benutzer dazu verleiten, keine andere Waffe zu benutzen, indem er Feinde mit großer Kampferfahrung zerstört.'
     }
   },
 
@@ -130,7 +144,8 @@ export default [
     description: {
       en: 'These bows are rare due to the length and method of the forging process; bows must be greatly elevated to ensure that every lightning strike can be used to gradually forge this bow into a Stormsong. The creation process can take ages.',
       hu: 'Efféle íjak ritkák a készítési folyamat hossza miatt; az íjakat a magasba kell emelni, hogy a villámok ereje kovácsolja egybe őket- csak így válhatnak valódi Viharvésszé. A folyamat pedig évekig eltarthat.',
-      ru: 'Эти луки редки из-за длинного процесса ковки; луки должны быть сильно приподняты, чтобы гарантировать, что каждый удар молнии может быть использован для постепенной ковки этого лука в Stormsong. Процесс создания может занять годы.'
+      ru: 'Эти луки редки из-за длинного процесса ковки; луки должны быть сильно приподняты, чтобы гарантировать, что каждый удар молнии может быть использован для постепенной ковки этого лука в Stormsong. Процесс создания может занять годы.',
+      de: 'Diese Bögen sind aufgrund der Methodik und Länge des Schmiedeprozesses selten: Die Bögen müssen in großer Höhe hergestellt werden, damit jeder Blitz genutzt werden kann, um sie zu schmieden. Dies kann Ewigkeiten dauern.'
     }
   },
 
@@ -139,7 +154,8 @@ export default [
     description: {
       en: 'This malevolent bow eats the dreams and nightmares of the wielder. This living bow offers no loyalty and demands sacrifices in the form of battle victims.',
       hu: 'Ez a rémületes íj felzabálja a viselő álmait és rémálmait. Az élő íj nem tanusít hűséget, és áldozatot követel, mégpedig halott ellenségek formájában.',
-      ru: 'Этот злобный лук съедает сны и кошмары владелеца. Этот живой лук не предлагает верности и требует жертв в виде солдат противоборствующих армий.'
+      ru: 'Этот злобный лук съедает сны и кошмары владелеца. Этот живой лук не предлагает верности и требует жертв в виде солдат противоборствующих армий.',
+      de: 'Dieser bösartige Bogen frisst die Träume und Alpträume seines Nutzers. Dieser lebendige Bogen ist illoyal und fordert Opfer in Form von Kampfgegnern.'
     }
   },
 
@@ -148,7 +164,8 @@ export default [
     description: {
       en: 'When a great tragedy befalls an entire civilization, the spirits of enraged victims pour their anger into a weapon to wreak vengeance upon the world. This weapon is only known as "Fury."',
       hu: 'Amikor nagy tragédia zuhan egy világra, a dühödt áldozatok szellemei gyűlöletüket egy fegyverbe gyűjtik, hogy bosszút álljanak a világon. Ezt a fegyvert úgy ismerik: "Fúria".',
-      ru: 'Когда целая цивилизация переживает великую трагедию, духи разгневанных жертв изливают свою злость в оружие, чтобы отомстить миру. Это оружие известно только как "Fury".'
+      ru: 'Когда целая цивилизация переживает великую трагедию, духи разгневанных жертв изливают свою злость в оружие, чтобы отомстить миру. Это оружие известно только как "Fury".',
+      de: 'Wenn großes Unheil eine gesamte Zivilisation befällt, lassen die Geister der Opfer ihren Zorn in eine Waffe strömen, um Rache an der Welt zu üben. Dieses Waffe ist nur als "Fury" bekannt.'
     }
   }
 
