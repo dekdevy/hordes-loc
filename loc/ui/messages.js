4288e81f40417ce0df1b3ee7d8c538e4921f8c96
Baptiste Chiocca <baptiste.chiocca@lemonlearning.fr>
2019-12-13 00:27:23 +0100

    french chat messages

--- loc/ui/messages.js
+++ loc/ui/messages.js
@@ -4,148 +4,171 @@ export default {
     de: '$1 ist deinem Clan beigetreten.',
     ru: '$1 вступил в ваш клан.',
     pt: '$1 entrou no teu clã',
-    hu: '$1 belépett a klánodba.'
+    hu: '$1 belépett a klánodba.',
+    fr: '$1 a rejoint votre clan.'
   },
   clanMemberInvite: {
     en: 'A clan invitation has been sent to $1.',
     de: '$1 wurde in deinen Clan eingeladen.',
     ru: 'Выслано приглашение игроку $1 для вступления в ваш клан.',
     pt: '$1 foi convidado para o teu clã.',
-    hu: '$1 meghívást kapott a klánodba.'
+    hu: '$1 meghívást kapott a klánodba.',
+    fr: 'Une invitation de clan a été envoyée à $1.'
   },
   clanMemberLeave: {
     en: '$1 has left your clan.',
     de: '$1 ist aus deinem Clan ausgetreten.',
     ru: 'Игрок $1 покинул ваш клан.',
     pt: '$1 saiu do teu clã.',
-    hu: '$1 elhagyta a klánodat.'
+    hu: '$1 elhagyta a klánodat.',
+    fr: '$1 a quitté votre clan.'
   },
   clanMemberApply: {
     en: '$1 has applied to join your clan.',
     de: '$1 hat sich bei deinem Clan beworben',
     ru: 'Игрок $1 подал заявку на вступление в ваш клан.',
     pt: '$1 solicitou para entrar no teu clã.',
-    hu: '$1 jelentkezett a klánodba.'
+    hu: '$1 jelentkezett a klánodba.',
+    fr: '$1 a postulé pour rejoindre votre clan.'
   },
   clanMemberRolePromote: {
     en: '$1 was promoted in clan role.',
     de: '$1 hat eine Clan Beförderung erhalten.',
     ru: 'Игрок $1 был повышен на новую должность.',
     pt: '$1 foi promovido na função do clã.',
-    hu: '$1 előléptetéshez jutott a klánodban.'
+    hu: '$1 előléptetéshez jutott a klánodban.',
+    fr: '$1 a été promu dans le clan.'
   },
   clanMemberRoleDemote: {
     en: '$1 was demoted in clan role.',
     de: '$1 hat eine Clan degradierung erhalten.',
     ru: 'Игрок $1 был понижен в должности.',
     pt: '$1 foi rebaixado na função do clã.',
-    hu: '$1 viselkedése visszabuktatásához vezetett.'
+    hu: '$1 viselkedése visszabuktatásához vezetett.',
+    fr: '$1 a été rétrogradé dans le clan.'
   },
   clanKickOther: {
     en: '$1 was kicked from your clan.',
     de: '$1 wurde aus dem Clan geworfen.',
     ru: 'Игрок $1 был исключен из вашего клана.',
     pt: '$1 foi expulso do teu clã.',
-    hu: 'A klánodból kirúgták: $1.'
+    hu: 'A klánodból kirúgták: $1.',
+    fr: '$1 a été expulsé du clan.'
   },
   clanKickYou: {
     en: 'You were kicked from your clan.',
     de: 'Du wurdest aus deinem Clan geworfen',
     ru: 'Вы были исключены из вашего клана.',
     pt: 'Você foi expulso do clã.',
-    hu: 'Kirúgtak a klánodból.'
+    hu: 'Kirúgtak a klánodból.',
+    fr: 'Vous avez été expulsé du clan.'
   },
   partyMemberInvite: {
     en: '$1 has been invited by $2 to join your party.',
     de: '$1 wurde von $2 eingeladen um deiner Party beizutreten.',
     ru: '$1 пригласил $2 вступить в вашу Party.',
     pt: '$1 foi convidado por $2 para se juntar à tua party.',
-    hu: '$2 a Partydba meghívta: $1.'
+    hu: '$2 a Partydba meghívta: $1.',
+    fr: '$1 a été invité par $2 à rejoindre votre party.'
   },
   partyMemberJoin: {
     en: '$1 has joined your party.',
     de: '$1 ist deiner Party beigetreten.',
     ru: 'Игрок $1 вступил в вашу Party.',
     pt: '$1 juntou-se à sua party.',
-    hu: '$1 belépett a Partydba.'
+    hu: '$1 belépett a Partydba.',
+    fr: '$1 a rejoint votre party.'
   },
   partyMemberLeave: {
     en: '$1 has left your party.',
     de: '$1 hat deine Party verlassen',
     ru: 'Игрок $1 вышел из Party.',
     pt: '$1 saiu da sua party.',
-    hu: '$1 elhagyta a Partydat.'
+    hu: '$1 elhagyta a Partydat.',
+    fr: '$1 a quitté votre party.'
   },
   partyInvitationDecline: {
     en: '$1 has declined to join your party.',
     de: '$1 hat abgelehnt deiner Party beizutreten.',
     ru: 'Игрок $1 отказался вступать в вашу Party.',
     pt: '$1 rejeitou juntar-se à sua party.',
-    hu: '$1 elutasította a meghívást a Partydba.'
+    hu: '$1 elutasította a meghívást a Partydba.',
+    fr: '$1 a refusé l\'invitation à rejoindre votre party.'
   },
   partyMemberPromote: {
     en: '$1 has been promoted in party role.',
     de: '$1 wurde in der Party befördert.',
     ru: 'Игрок $1 был повышен в должности Party.',
     pt: '$1 foi promovido na função da party.',
-    hu: '$1 előléptetéshez jutott a Partydban.'
+    hu: '$1 előléptetéshez jutott a Partydban.',
+    fr: '$1 a été promu dans la party.'
   },
   partyMemberFound: {
-    en: 'Found $1 new party members.'
+    en: 'Found $1 new party members.',
+    fr: '$1 nouveaux équipiers trouvés.'
   },
   partyMemberDemote: {
     en: '$1 has been demoted in party role.',
     de: '$1 wurde in der Party degradiert.',
     ru: 'Игрок $1 был понижен в должности Party',
     pt: '$1 foi rebaixado na função da party.',
-    hu: '$1 viselkedése visszabuktatásához vezetett.'
+    hu: '$1 viselkedése visszabuktatásához vezetett.',
+    fr: '$1 a été rétrogradé dans la party.'
   },
   partyInviteLink: {
     en: '$1 has created a party invitation link: $2.',
     de: '$1 hat einen Einladungslink für die Party erstellt.',
     ru: 'Игрок $1 создал ссылку для присоединения к вашей Party: $2.',
     pt: '$1 criou um link de convite da party: $2.',
-    hu: '$1 létrehozta a következő Partymeghívó linket: $2.'
+    hu: '$1 létrehozta a következő Partymeghívó linket: $2.',
+    fr: '$1 a créé un lien d\'invitation pour la party: $2'
   },
   partyKickYou: {
     en: 'You have been kicked from the party by $1.',
     de: 'Du wurdest von $1 aus deiner Party geworfen.',
     ru: '$1 исключил вас из Party.',
     pt: 'Você foi expulso da Party pelo $1.',
-    hu: '$1 kirúgott a Partydból.'
+    hu: '$1 kirúgott a Partydból.',
+    fr: 'Vous avez été expulsé de la party par $1'
   },
   partyKickOther: {
     en: '$1 was kicked from your party by $2.',
     de: '$1 wurde von $2 aus der Party entfernt.',
     ru: '$2 исключил $1 из Party.',
     pt: '$1 foi expulso da sua party pelo $2.',
-    hu: '$2 kirúgta a Partydból: $1.'
+    hu: '$2 kirúgta a Partydból: $1.',
+    fr: '$1 a été expulsé de votre party par $2.'
   },
   partyQueueStart: {
-    en: 'Your party has started queue type: $1.'
+    en: 'Your party has started queue type: $1.',
+    fr: 'Votre party a rejoint la file d\'attente de type: $1'
   },
   partyQueueStop: {
-    en: 'Your party has stopped queue type: $1.'
+    en: 'Your party has stopped queue type: $1.',
+    fr: 'Votre party a quitté la file d\'attente de type: $1'
   },
   auctionSold: {
     en: 'Your merchant auction has been sold and $g$1 has been sent to your Stash.',
     de: 'Dein Angebot im Auktionshaus wurde erfolgreich verkauft und $g$1 wurden in deine Truhe gelegt.',
     ru: 'Ваш лот был продан на аукционе. В Stash добавлено: $g$1.',
     pt: 'O seu leilão do mercado foi vendido e $g$1 foram enviadas para o seu Stash.',
-    hu: 'A Merchantnál feladott aukciódat eladták ennyiért: $g$1. Ez a Stash fiókodba került.'
+    hu: 'A Merchantnál feladott aukciódat eladták ennyiért: $g$1. Ez a Stash fiókodba került.',
+    fr: 'Votre ventre aux enchères a été vendue et $g$1 ont été ajoutés dans votre Stash.'
   },
   online: {
     en: '$1 has come online.',
     de: '$1 ist nun Online',
     ru: '$1 в игре.',
     pt: '$1 está online.',
-    hu: '$1 belépett a játékba.'
+    hu: '$1 belépett a játékba.',
+    fr: '$1 est en ligne.'
   },
   offline: {
     en: '$1 has gone offline.',
     de: '$1 ist nun Offline.',
     ru: '$1 вышел из игры.',
     pt: '$1 ficou offline.',
-    hu: '$1 kilépett a játékból.'
+    hu: '$1 kilépett a játékból.',
+    fr: '$1 s\'est déconnecté.'
   }
 }
