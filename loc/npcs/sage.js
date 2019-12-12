9d9c51821d9f186fa21437342b0d3cfa5f1e6c8e
Malfrador <malfrador@gmail.com>
2019-12-12 23:40:53 +0100

    German: Added Translations

--- loc/npcs/sage.js
+++ loc/npcs/sage.js
@@ -3,7 +3,8 @@ export default {
     text: {
       en: 'Burdened minds are filled with opinions aplenty; a cup can be quite useful if first found empty. How may I help you on your path toward enlightenment?',
       hu: 'A megtisztított tudatokban lehetőségek végtelenje rejlik; egy üres csésze csupán akkor hasznos, ha teljesen üres. Hogy segélhetlek a megvilágosodás felé?',
-      ru: 'Загруженные умы полны мнений; чаша может оказаться весьма полезной, если впервые обнаружить ее пустой. Как я могу помочь тебе на пути к просветлению?'
+      ru: 'Загруженные умы полны мнений; чаша может оказаться весьма полезной, если впервые обнаружить ее пустой. Как я могу помочь тебе на пути к просветлению?',
+      de: 'Belastete Gemüter sind reichlich mit Ansichten gefüllt. Ein Becher kann sehr nützlich sein, wenn er leer ist. Wie kann ich dir auf deinem Weg zur Erleuchtung helfen?'
     },
 
     choices: [{
@@ -13,7 +14,8 @@ export default {
       pt: 'Reinicie os meus pontos de stat ( $g$1 ).',
       fr: 'Réinitialisez mes points d\'aptitudes ( $g$1 ).',
       nl: 'Reset mijn status punten ( $g$1 ).',
-      es: 'Restablecer mis puntos de estadísticas ( $g$1 ).'
+      es: 'Restablecer mis puntos de estadísticas ( $g$1 ).',
+      de: 'Setze meine Stat-Punkte zurück. ( $g$1 ).'
     }]
   }]
 }
