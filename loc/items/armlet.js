0fa2dcf9ed912f9fe1533841c17a1fe830fd7540
Baptiste Chiocca <baptiste.chiocca@lemonlearning.fr>
2019-12-12 23:44:40 +0100

    Added french armlet translations

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -4,7 +4,8 @@ export default [
     description: {
       en: 'These wristbands are leather strips that are wrapped in a simple formation.',
       hu: 'Ezek a csuklóvédők bőrszalagok, melyek egyetlen csíkba lettek tekerve.',
-      ru: 'Эти браслеты сделаны из кожаных полосок, которые были завернуты в простую форму'
+      ru: 'Эти браслеты сделаны из кожаных полосок, которые были завернуты в простую форму',
+      fr: 'Ces bracelets sont des lanières de cuir qui sont enveloppées dans une formation simple.'
     }
   },
 
@@ -13,7 +14,8 @@ export default [
     description: {
       en: 'These armbands were created hastily. The rough battle markings on the sides of the leather do not inspire confidence of the previous owner\'s survival.',
       hu: 'Ezeket a karvédőket sietve készítették. A durva küzdelem jelei a bőrön nem éppen az előző tulajdonos túléléséről árulkodnak.',
-      ru: 'Эти браслеты были созданы поспешно. Грубая боевая разметка по бокам кожи не внушает уверенности в выживании предыдущего владельца.'
+      ru: 'Эти браслеты были созданы поспешно. Грубая боевая разметка по бокам кожи не внушает уверенности в выживании предыдущего владельца.',
+      fr: 'Ces brassards ont été créés à la va-vite. Les marques de combat rugueuses sur les côtés du cuir n\'inspirent pas confiance en la survie de l\'ancien propriétaire.'
     }
   },
 
@@ -23,7 +25,8 @@ export default [
       // Do NOT translate "Markay\'ak".
       en: 'Tough leather has been painstakingly crafted into fine armlets. On the side of the leather is the craftsman\'s signature: "Markay\'ak".',
       hu: 'A cserzett bőrt fáradtságos munkával formálták alkarvédőkké. A páncél oldalán ott a bőrműves névjegye: "Markay\'ak".',
-      ru: 'Жесткая кожа была тщательно обработана в виде тонких браслетов. На боковой стороне кожи стоит подпись ремесленника: "Markay\'ak"'
+      ru: 'Жесткая кожа была тщательно обработана в виде тонких браслетов. На боковой стороне кожи стоит подпись ремесленника: "Markay\'ak"',
+      fr: 'Le cuir robuste a été méticuleusement travaillé pour en faire des bracelets de qualité. Sur le côté du cuir se trouve la signature de l\'artisan : "Markay\'ak".'
     }
   },
 
@@ -32,7 +35,8 @@ export default [
     description: {
       en: 'This type of bracelet is ornately crafted with thick monster bones. Many superstitious traditions claim that the wearer of such bracelets become permeated with the deceased monster\'s power; however, others claim that the reuse of bones prevents monsters from resurrecting.',
       hu: 'Ez a fajta csuklóvédő díszesen faragott, vastag csontokból áll. A babonás népek úgy hiszik, ilyen csuklóvértek viselője bírni fog az elpusztított bestia erejével; mások úgy vélik, a csontok felhasználása megakadályozza a szörnyeteget újjászületésében.',
-      ru: 'Такой тип браслета сделан из толстых костей, принадлежащих мертвым монстрам. Многие суеверные традиции утверждают, что обладатель таких браслетов пронизан властью умершего монстра, однако другие утверждают, что повторное использование костей предотвращает воскрешение монстров.'
+      ru: 'Такой тип браслета сделан из толстых костей, принадлежащих мертвым монстрам. Многие суеверные традиции утверждают, что обладатель таких браслетов пронизан властью умершего монстра, однако другие утверждают, что повторное использование костей предотвращает воскрешение монстров.',
+      fr: 'Ce type de bracelet est orné d\'os de monstres épais. De nombreuses traditions superstitieuses prétendent que le porteur de ces bracelets s\'imprègne du pouvoir du monstre décédé ; cependant, d\'autres prétendent que la réutilisation des os empêche les monstres de ressusciter.'
     }
   },
 
@@ -41,7 +45,8 @@ export default [
     description: {
       en: 'This type of iron armor has been heavily used in war and remains dirty. The claw marks on the metal seem to be mostly superficial.',
       hu: 'Ez a fajta alkarvért messze elterjedt a háborúban, és ez hírnevet is csinált neki. A karomnyomok többnyire felszínesek a vason.',
-      ru: 'Такой тип железных доспехов широко использовался на войне и остается грязным. Следы от когтей на металле, кажется, в основном поверхностные.'
+      ru: 'Такой тип железных доспехов широко использовался на войне и остается грязным. Следы от когтей на металле, кажется, в основном поверхностные.',
+      fr: 'Ce type d\'armure de fer a été très utilisé en temps de guerre et reste encrassé. Les marques de griffes sur le métal semblent être superficielles.'
     }
   },
 
@@ -50,7 +55,8 @@ export default [
     description: {
       en: 'These steel bracers are marked with small runes that imbue extra protection. Imbued bracers are somewhat rare since the creation method of the runes are a secret.',
       hu: 'Az acél vértek rúnákkal vannak ellátva a teljes védelemért. Ilyen elbűvölt alkarvédők elég ritkák lettek, amióta a rúnák titkát elfeledték.',
-      ru: 'Эти стальные браслеты отмечены мелкими рунами, которые обеспечивают дополнительную защиту. Такие браслеты встречаются редко, так как метод создания рун является секретным.'
+      ru: 'Эти стальные браслеты отмечены мелкими рунами, которые обеспечивают дополнительную защиту. Такие браслеты встречаются редко, так как метод создания рун является секретным.',
+      fr: 'Ces brassards en acier sont marqués de petites runes qui leur confèrent une protection supplémentaire. Les brassard imprégnés sont quelque peu rares puisque la méthode de création des runes est un secret.',
     }
   },
 
@@ -59,7 +65,8 @@ export default [
     description: {
       en: 'These cuffs were forged with a special fire that can only be created through magic. In the past, fire elementals taught only a few special blacksmiths the secret to forging ember cuffs.',
       hu: 'Ezek a csuklóvértek egy ritka tűzzel lettek kovácsolva, mégpedig mágia segítségével. Régen a tűz elem szellemei csak kevés kiválasztottat avattak be a borostyánkő titkaiba.',
-      ru: 'Эти браслеты были выкованы специальным огнем, который может быть создан только с помощью магии. В прошлом, огненные элементали научили только нескольких кузнецов секрету изготовления этих браслетов.'
+      ru: 'Эти браслеты были выкованы специальным огнем, который может быть создан только с помощью магии. В прошлом, огненные элементали научили только нескольких кузнецов секрету изготовления этих браслетов.',
+      fr: 'Ces menottes ont été forgées avec un feu spécial qui ne peut être créé que par magie. Autrefois, les élémentaires de feu n\'enseignaient qu\'à quelques forgerons spéciaux le secret de la forge des menottes de braise.',
     }
   },
 
@@ -68,7 +75,8 @@ export default [
     description: {
       en: 'Mirrored armlets are made by exceptional blacksmiths with the skills necessary to make reflective armor for damage reduction. This protective quality inspired many Elvish wizards to often experiment on mirrored armlets.',
       hu: 'A tükrözött karvérteket különleges képességű kovácsok készítették, akik képesek voltak a támadást visszaverő acél létrehozására. Ez a védelem sok Elf varázslót arra késztetett, hogy kísérletezzenek a tükrös karvértekkel.',
-      ru: 'Зеркальные браслеты изготавливаются чрезвычайно опытными кузнецами, обладающими навыками, необходимыми для изготовления брони для снижения урона. Это защитное качество вдохновило многих эльфийских волшебников часто экспериментировать на зеркальных браслетах.'
+      ru: 'Зеркальные браслеты изготавливаются чрезвычайно опытными кузнецами, обладающими навыками, необходимыми для изготовления брони для снижения урона. Это защитное качество вдохновило многих эльфийских волшебников часто экспериментировать на зеркальных браслетах.',
+      fr: 'Les bracelets en miroir sont fabriqués par des forgerons exceptionnels possédant les compétences nécessaires pour fabriquer des armures réfléchissantes pour réduire les dégâts. Cette qualité protectrice a inspiré de nombreux sorciers Elfiques à expérimenter souvent sur des bracelets en miroir.'
     }
   },
 
@@ -77,7 +85,8 @@ export default [
     description: {
       en: 'Golem fragments are painstakingly assembled by a small team of magic-using blacksmiths. Every pair of golem fragments is crafted over several hundred days using the husks of rock golems.',
       hu: 'A Gólem szilánkok fáradságosan lettek összeillesztve egy mágus-kovács csoport által. Minden párját ezeknek a csuklóvédőknek napok százain át készítették kőgólemek holttesteiből.',
-      ru: 'Фрагменты големов тщательно собираются небольшой группой кузнецов, использующих магию. Каждая пара фрагментов голема изготавливается в течение нескольких сотен дней с использованием шелухи големов.'
+      ru: 'Фрагменты големов тщательно собираются небольшой группой кузнецов, использующих магию. Каждая пара фрагментов голема изготавливается в течение нескольких сотен дней с использованием шелухи големов.',
+      fr: 'Les fragments de Golem sont minutieusement assemblés par une petite équipe de forgerons magiciens. Chaque paire de fragments de golem est fabriquée sur plusieurs centaines de jours à l\'aide de coques de golems de roche.'
     }
   },
 
@@ -86,7 +95,8 @@ export default [
     description: {
       en: 'Bracers such as this are comprised of a rare metal found in the mountains. Blacksmiths use enchanted hammers to forge the bracers without heat in order to retain the magic-enhancing properties of the metal.',
       hu: 'A csuklóvértek mint ezek egy ritka hegyi fémet tartalmaznak. A kovácsok bűvölt kalapáccsal hozzák létre őket hevítés nélkül, hogy megőrizzék a fém csodálatos képességeit.',
-      ru: 'Такие браслеты состоят из редкого металла, встречающегося в горах. Кузнецы используют заколдованные молотки для ковки браслетов без нагрева, чтобы сохранить магические свойства металла.'
+      ru: 'Такие браслеты состоят из редкого металла, встречающегося в горах. Кузнецы используют заколдованные молотки для ковки браслетов без нагрева, чтобы сохранить магические свойства металла.',
+      fr: 'De tels brassard sont composés d\'un métal rare que l\'on trouve dans les montagnes. Les forgerons utilisent des marteaux enchanteurs pour forger les brassards sans chaleur afin de conserver les propriétés magiques du métal.'
     }
   },
 
@@ -95,7 +105,8 @@ export default [
     description: {
       en: 'These bracers were created by blacksmiths that hammered metal found within black meteorites. So far, the greatest concentration of black meteorites was found after a cataclysm that followed the Second Great War.',
       hu: 'Ezeket a karvédőket fekete meteorkövekből kalapálták ki. Eddig a legnagyobb mennyiségben fekete meteoritot azután a kataklizma után találtak, ami a Második Nagy Háborút követte.',
-      ru: 'Эти браслеты были созданы кузнецами, которые ковали металл, найденный в черных метеоритах. До сих пор наибольшая концентрация черных метеоритов была обнаружена после катаклизма, последовавшего за "The Second Great War".'
+      ru: 'Эти браслеты были созданы кузнецами, которые ковали металл, найденный в черных метеоритах. До сих пор наибольшая концентрация черных метеоритов была обнаружена после катаклизма, последовавшего за "The Second Great War".',
+      fr: 'Ces brassards ont été créés par des forgerons qui martelaient le métal trouvé dans les météorites noires. Jusqu\'à présent, la plus grande concentration de météorites noires a été trouvée après un cataclysme qui a suivi la Seconde Grande Guerre.'
     }
   },
 
@@ -104,7 +115,8 @@ export default [
     description: {
       en: 'Ambitious wizards on the edge of madness discovered secrets to dimensional magic and formed bracers that transcend normal magical protection. These abnormal bracers required a special shape to maintain their properties.',
       hu: 'Nagyravágyástól az őrületbe vezetett varázslók fedezték fel a dimenzionális mágiát, és formáltak vértet, amely túllép bármely elképzelhető védelmen. Egy különös formára volt szükség, hogy fenntartsák ezt a képességet.',
-      ru: 'Амбициозные волшебники на грани безумия открыли для себя секреты размерной магии и сформировали браслеты, выходящие за рамки обычной магической защиты. Эти ненормальные браслеты требовали специальной формы для поддержания своих свойств.'
+      ru: 'Амбициозные волшебники на грани безумия открыли для себя секреты размерной магии и сформировали браслеты, выходящие за рамки обычной магической защиты. Эти ненормальные браслеты требовали специальной формы для поддержания своих свойств.',
+      fr: 'Des sorciers ambitieux au bord de la folie ont découvert les secrets de la magie dimensionnelle et ont formé des brassards qui transcendent la protection magique normale. Ces brassards anormaux nécessitaient une forme spéciale pour conserver leurs propriétés.'
     }
   },
 
@@ -113,7 +125,8 @@ export default [
     description: {
       en: 'Some legends say that these bracers only appear when the realms need a guardian. History shows that terrible tragedies occur shortly thereafter whenever these bracers are found; as such, their appearances are often seen as bad omens.',
       hu: 'A Legendák szerint ott tűnnek föl ezek, ahol védelmezőre van szükség. Ám a történelem bebizonyította, hogy iszonyatos tragédiák következnek be, ha ilyet találnak. Így hát a feltűnésüket gyakran rossz előjelként látják.',
-      ru: 'Некоторые легенды гласят, что эти браслеты появляются только тогда, когда королевства нуждаются в страже. История показывает, что ужасные трагедии происходят вскоре после этого, когда эти браслеты обнаруживаются; как таковое, их появление часто рассматривается как плохое предзнаменование.'
+      ru: 'Некоторые легенды гласят, что эти браслеты появляются только тогда, когда королевства нуждаются в страже. История показывает, что ужасные трагедии происходят вскоре после этого, когда эти браслеты обнаруживаются; как таковое, их появление часто рассматривается как плохое предзнаменование.',
+      fr: 'Certaines légendes disent que ces brassards n\'apparaissent que lorsque les royaumes ont besoin d\'un gardien. L\'histoire montre que de terribles tragédies surviennent peu de temps après chaque fois que l\'on découvre ces brassards ; en tant que tel, leurs apparences sont souvent considérées comme de mauvais présages.'
     }
   }
 
