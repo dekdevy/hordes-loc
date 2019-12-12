a327aaa8507dc5a5d692d69ac423647a2287755f
Malfrador <malfrador@gmail.com>
2019-12-12 23:00:03 +0100

    German bag descriptions

--- loc/items/bag.js
+++ loc/items/bag.js
@@ -5,7 +5,8 @@ export default [
       en: 'This pouch is made from a delicate fabric. Pouches such as this are often owned by common people for carrying their belongings.',
       ru: 'Этот мешок сделан из нежной ткани. Мешки, подобные этому, часто принадлежат простым людям для перевозки своих вещей.',
       hu: 'Ez az erszény minőségi szövetből van. A köznép többnyire ilyenekben hordozza kisebb-nagyobb értékeit.',
-      tr: 'Bu torba narin bir kumaştan yapıldı. Bunun gibi torbalar, sıradan insanlar tarafından eşyalarını taşımak için sıklıkla kullanılır.'
+      tr: 'Bu torba narin bir kumaştan yapıldı. Bunun gibi torbalar, sıradan insanlar tarafından eşyalarını taşımak için sıklıkla kullanılır.',
+      de: 'Dieser Beutel ist aus feinem Stoff gefertigt. Beutel wie dieser werden oft von normalen Leutem zum Transport ihres Besitzes genutzt.'
     }
   },
 
@@ -15,7 +16,8 @@ export default [
       en: 'This rugged sack is crafted from sturdy fabrics to hold more equipment than a linen pouch.',
       ru: 'Этот прочный мешок сделан из крепких тканей и может содержать в себе больше вещей, чем Linen Pouch.',
       hu: 'Ez a gyűrött zsák erős anyagból van, hogy több felszerelést bírjon el egy erszénynél.',
-      tr: 'Bu engebeli çanta, bir Linen Pouch\'tan daha fazla ekipman taşıyabilmesi için sağlam kumaşlardan yapıldı.'
+      tr: 'Bu engebeli çanta, bir Linen Pouch\'tan daha fazla ekipman taşıyabilmesi için sağlam kumaşlardan yapıldı.',
+      de: 'Dieser robuste Sack ist aus stabilen Stoffen gefertigt, um mehr Ausrüstung aufzunehmen als ein Leinenbeutel.'
     }
   },
 
@@ -25,7 +27,8 @@ export default [
       en: 'This robust sack is made from monster hides and permits a respectable inventory.',
       ru: 'Этот прочный мешок сделан из шкур монстров и предлагает респектабельный инвентарь.',
       hu: 'Ez a robosztus hátizsákot szörnyetegek bőréből készítették, és bőséges tárolóhelyet nyújt.',
-      tr: 'Canavar kalıntılarından yapılmış bu sağlam çanta gayet büyük bir envanter sağlıyor.'
+      tr: 'Canavar kalıntılarından yapılmış bu sağlam çanta gayet büyük bir envanter sağlıyor.',
+      de: 'Dieser robuste Sack ist aus Monsterhaut gefertigt und erlaubt ein ansehnliches Inventar.'
     }
   },
 
@@ -35,7 +38,8 @@ export default [
       en: 'This fine bag was crafted from expensive Elvish fabrics and contains more pockets than most other bags.',
       ru: 'Эта прекрасная сумка была изготовлена из дорогих эльфийских тканей и содержит больше карманов, чем большинство других сумок.',
       hu: 'Ez a remek zsák drága Elf szövetből készült, és több zsebet varrtak rá, mint bármely más erszényre.',
-      tr: 'Bu güzel çanta, Elf kumaşlarından yapıldı ve diğer çantalardan daha fazla cebe sahip.'
+      tr: 'Bu güzel çanta, Elf kumaşlarından yapıldı ve diğer çantalardan daha fazla cebe sahip.',
+      de: 'Diese feine Tasche wurde aus teuren elfischen Stoffen gefertigt und enthält mehr Fächer als die meisten anderen Taschen.'
     }
   },
 
@@ -45,7 +49,8 @@ export default [
       en: 'Bags such as this are a gift from nature to secret druid societies. Unfortunately, monsters have disposed of many druids over time and have stolen these sacred bags.',
       ru: 'Такие сумки являются подарком от природы тайным обществом друидов. К сожалению, монстры со временем избавлялись от многих друидов и украли эти священные сумки.',
       hu: 'Az ilyen hátizsákokat remeteközösségek adományaiként kapták a hősök. Sajnos, a szörnyetegek addig háborgatták az erdei bölcseket, míg szert nem tettek a táskákra.',
-      tr: 'Bunun gibi çantalar, gizli rahip topluluklarından doğal bir armağandır. Ne yazık ki, canavarlar birçok rahipten kurtulup bu kutsal çantaları çalmışlardır.'
+      tr: 'Bunun gibi çantalar, gizli rahip topluluklarından doğal bir armağandır. Ne yazık ki, canavarlar birçok rahipten kurtulup bu kutsal çantaları çalmışlardır.',
+      de: 'Taschen wie diese sind ein Geschenk der Natur an geheime Druidengesellschaften. Leider haben Monster im Laufe der Zeit viele Druiden entsorgt und die heiligen Taschen gestohlen.'
     }
   }
 
