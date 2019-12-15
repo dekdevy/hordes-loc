c1304e9e89a75693a8a0e10bb8a010c0717182df
Malfrador <malfrador@gmail.com>
2019-12-15 17:40:42 +0100

    German: Added descriptions

--- loc/items/quiver.js
+++ loc/items/quiver.js
@@ -4,7 +4,8 @@ export default [
     description: {
       en: 'This mass-produced quiver is made with Elvish linen for extra comfort.',
       hu: 'Ez a tucatáru Elf fonállal van elkészítve a különleges kényelemért.',
-      ru: 'Это массовая продукция колчанов была сделана с Эльфийским льном для дополнительного комфорта'
+      ru: 'Это массовая продукция колчанов была сделана с Эльфийским льном для дополнительного комфорта',
+      de: 'Dieser massenhaft produzierte Köcher besteht aus Elfenleinen für extra Komfort.'
     }
   },
 
@@ -13,7 +14,8 @@ export default [
     description: {
       en: 'Rustic Quivers are reliable and battle-tested. These quivers are often passed on to apprentice archers as they progress.',
       hu: 'A durva tegezek megbízhatóak és kipróbáltak. A tegezt haladó képességű íjásztanítványok szokták kiérdemelni.',
-      ru: 'Деревенские колчаны надежны и проверены в бою. Эти колчаны часто передаются ученикам лучников по мере их развития.'
+      ru: 'Деревенские колчаны надежны и проверены в бою. Эти колчаны часто передаются ученикам лучников по мере их развития.',
+      de: 'Rustikale Köcher sind verlässlich und kampferprobt. Diese Köcher werden oft an lernende Bogenschützen vergeben.'
     }
   },
 
@@ -22,7 +24,8 @@ export default [
     description: {
       en: 'These quivers are made from snake scales from special monsters. Typically, snake skins are found after the giant snakes have shed their skin; to attempt a snakeskin collection otherwise would be a high risk.',
       hu: 'Ezek a tegezek különleges bestiák kígyópikkelyeiből lettek összeállítva. Általában a szörnyetegek levedlett bőrét vagdalják fel, egyébként igen kellemetlen volna a gyűjtögetés, már akinek.',
-      ru: 'Эти колчаны сделаны из змеиной чешуи из особых монстров. Обычно эти шкуры ищут после того, как гигантские змеи сбросили свою кожу; в противном случае сбор кожи будет иметь большой риск.'
+      ru: 'Эти колчаны сделаны из змеиной чешуи из особых монстров. Обычно эти шкуры ищут после того, как гигантские змеи сбросили свою кожу; в противном случае сбор кожи будет иметь большой риск.',
+      de: 'Diese Köcher werden aus den Schlangenschuppen spezieller Monster hergestellt. Typischerweise wird die Schlangenhaut eingesammelt nachdem riesige Schlangen sich gehäutet haben, andere Methoden wären ein hohes Risiko.'
     }
   },
 
@@ -31,7 +34,8 @@ export default [
     description: {
       en: 'The exquisite craftsmanship of Markay\'ak resulted in the creation of Reinforced Exemplars. Quivers such as this are often a testament to the pride of any professional archer.',
       hu: 'Markay\'ák mesterünk különleges, tapasztalt kezének munkája eredményezte a megerősített exampláriumokat. Az efféle kiváló tegezek minden íjászmester büszkeségei.',
-      ru: 'Изысканное мастерство Markay\'ak привело к созданию Reinforced Exemplars. Колчаны, подобные этому, часто являются свидетельством гордости любого профессионального лучника.'
+      ru: 'Изысканное мастерство Markay\'ak привело к созданию Reinforced Exemplars. Колчаны, подобные этому, часто являются свидетельством гордости любого профессионального лучника.',
+      de: 'Die exquisite Handwerkskunst von Markay\'ak führte zur Entwicklung von verstärkten Exemplaren. Solche Köcher sind oft ein Zeichen des Stolzes für jeden professionellen Bogenschützen.'
     }
   },
 
@@ -40,7 +44,8 @@ export default [
     description: {
       en: 'An immortal from an elder race created quivers with an inhuman power in order to manifest his revenge upon the world.',
       hu: 'Egy halhatatlan teremtette ezeket a tegezeket embertelen akaratával, hogy kinyilatkoztassa bosszúját eme világon.',
-      ru: 'Бессмертный из древней расы создал колчан с нечеловеческой силой, чтобы явить свою месть миру.'
+      ru: 'Бессмертный из древней расы создал колчан с нечеловеческой силой, чтобы явить свою месть миру.',
+      de: 'Ein Unsterblicher einer alten Rasse schuf Köcher mit einer unmenschlichen Kraft, um seine Rache an der Welt zu manifestieren.'
     }
   },
 
@@ -49,7 +54,8 @@ export default [
     description: {
       en: 'This quiver is made of dragon scales found on the ground. Dragon scales are a very difficult crafting material, so only the best craftsmen can use them.',
       hu: 'Ez a puzdra elhagyott sárkánypikkelyekből készült. Ezek a pikkelyek azonban igen makacs alapanyagok, és csak a legjobb kovácsok használhatják fel őket.',
-      ru: 'Этот колчан сделан из драконьей чешуи, найденной на земле. Чешуя дракона - очень сложный материал для изготовления, поэтому использовать её могут только лучшие мастера.'
+      ru: 'Этот колчан сделан из драконьей чешуи, найденной на земле. Чешуя дракона - очень сложный материал для изготовления, поэтому использовать её могут только лучшие мастера.',
+      de: 'Dieser Köcher besteht aus Drachenschuppen, die auf dem Boden gefunden wurden. Drachenschuppen sind ein sehr schwieriges Handwerksmaterial, so dass nur die besten Handwerker sie nutzen können.'
     }
   },
 
@@ -58,7 +64,8 @@ export default [
     description: {
       en: 'Lothariens are quivers that are named after a people warred out of existence. The Lotharien people battled surrounding territories for power acquisition while using these quivers.',
       hu: 'A Lotariánusok olyan tegezek, melyek egy kipusztult nép után lettek elnevezve. A Lotariének hosszan küzdöttek a környező fajokkal a mágikus tartalékokért, eme tegezeket használva.',
-      ru: 'Lothariens - это колчан, названный в честь народа, вытесненного из жизни. Lothariens боролись за власть на близлежащих территориях, используя эти колчаны.'
+      ru: 'Lothariens - это колчан, названный в честь народа, вытесненного из жизни. Lothariens боролись за власть на близлежащих территориях, используя эти колчаны.',
+      de: 'Lothariens sind Köcher, die nach einem Volk benannt sind, das aus der Welt getilgt wurde. Das Volk der Lotharien kämpfte gegen angrenzende Territorien um den Erwerb von Macht, während es diese Köcher benutzte.'
     }
   },
 
@@ -67,7 +74,8 @@ export default [
     description: {
       en: 'This quiver is a gift from the forest as a means for nature spirits to reclaim their stolen power, territory, and respect.',
       hu: 'Ez a tegez az erdőtől kapott ajándék, jelképe a természet visszahódításának, az erőért, területért, és a tiszteletért.',
-      ru: 'Этот колчан - подарок от леса, как средство для духов природы, чтобы вернуть себе украденную власть, территорию и уважение.'
+      ru: 'Этот колчан - подарок от леса, как средство для духов природы, чтобы вернуть себе украденную власть, территорию и уважение.',
+      de: 'Dieser Köcher ist ein Geschenk aus dem Wald als Mittel der Naturgeister, um ihre gestohlene Kraft, ihr Territorium und ihren Respekt zurückzugewinnen.'
     }
   },
 
@@ -76,7 +84,8 @@ export default [
     description: {
       en: 'The Vodhrai are the embodiment of vengeance, anger, wrath, and determination. The unusually strong emotions from this living quiver might serve to corrupt the user.',
       hu: 'A Voderáj a harag, a düh, a bosszú, és az akarat megnyilatkozása. Ezek a különösen hatalmas érzelmek a tegezből megronthatják annak jámbor viselőjét.',
-      ru: 'Vodhrai являются воплощением мести, гнева, ярости и решимости. Необычайно сильные эмоции от этого живого колчана могут повредить обладателя.'
+      ru: 'Vodhrai являются воплощением мести, гнева, ярости и решимости. Необычайно сильные эмоции от этого живого колчана могут повредить обладателя.',
+      de: 'Die Vodhrai sind die Verkörperung von Rache, Wut, Groll, Zorn und Entschlossenheit. Die ungewöhnlich starken Emotionen dieses lebenden Köcher könnten dazu dienen, den Besitzer zu verderben.'
     }
   },
 
@@ -85,7 +94,8 @@ export default [
     description: {
       en: 'This quiver was once found near the ruins of an entire civilization. There are no surviving records about the quiver in the region from this time period.',
       hu: 'Ez a tegez egy ősi civilizáció egyetlen hagyatéka. Nincsenek emlékeink, se feljegyzéseink az ekkor használt tegezekről, érdekes módon.',
-      ru: 'Этот колчан однажды был обнаружен рядом с руинами целой цивилизации. Нет сохранившихся записей о колчане в регионе за этот период времени.'
+      ru: 'Этот колчан однажды был обнаружен рядом с руинами целой цивилизации. Нет сохранившихся записей о колчане в регионе за этот период времени.',
+      de: 'Dieser Köcher wurde einst in der Nähe der Ruinen einer Zivilisation gefunden. Es gibt keine erhaltenen Aufzeichnungen über den Köcher aus diesem Zeitraum.'
     }
   }
 
