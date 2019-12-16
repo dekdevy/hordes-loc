0c3fa4c6aeef2c85f096d3b239804425bb2f3795
Kavalorn47 <58919442+Kavalorn47@users.noreply.github.com>
2019-12-16 21:30:16 +0100

    messages PL translation

--- loc/ui/messages.js
+++ loc/ui/messages.js
@@ -6,7 +6,8 @@ export default {
     es: '$1 se ha unido a tu clan.',
     pt: '$1 entrou no teu clã',
     hu: '$1 belépett a klánodba.',
-    fr: '$1 a rejoint votre clan.'
+    fr: '$1 a rejoint votre clan.',
+    pl: '$1 dołączył do twojego klanu.'
   },
   clanMemberInvite: {
     en: 'A clan invitation has been sent to $1.',
@@ -15,7 +16,8 @@ export default {
     es: 'Se ha enviado una invitación del clan a $1.',
     pt: '$1 foi convidado para o teu clã.',
     hu: '$1 meghívást kapott a klánodba.',
-    fr: 'Une invitation de clan a été envoyée à $1.'
+    fr: 'Une invitation de clan a été envoyée à $1.',
+    pl: 'Wysłano zaproszenie do klanu do $1.'
   },
   clanMemberLeave: {
     en: '$1 has left your clan.',
@@ -24,7 +26,8 @@ export default {
     es: '$1 ha abandonado tu clan.',
     pt: '$1 saiu do teu clã.',
     hu: '$1 elhagyta a klánodat.',
-    fr: '$1 a quitté votre clan.'
+    fr: '$1 a quitté votre clan.',
+    pl: '$1 opuścił twój klan.'
   },
   clanMemberApply: {
     en: '$1 has applied to join your clan.',
@@ -33,7 +36,8 @@ export default {
     es: '$1 ha solicitado entrar a tu clan.',
     pt: '$1 solicitou para entrar no teu clã.',
     hu: '$1 jelentkezett a klánodba.',
-    fr: '$1 a postulé pour rejoindre votre clan.'
+    fr: '$1 a postulé pour rejoindre votre clan.',
+    pl: '$1 aplikował aby dołączyć do twojego klanu.'
   },
   clanMemberRolePromote: {
     en: '$1 was promoted in clan role.',
@@ -42,7 +46,8 @@ export default {
     es: '$1 fue promovido en el rol del clan.',
     pt: '$1 foi promovido na função do clã.',
     hu: '$1 előléptetéshez jutott a klánodban.',
-    fr: '$1 a été promu dans le clan.'
+    fr: '$1 a été promu dans le clan.',
+    pl: '$1 otrzymał awans w klanie.'
   },
   clanMemberRoleDemote: {
     en: '$1 was demoted in clan role.',
@@ -51,7 +56,8 @@ export default {
     es: '$1 fue rebajado en el rol del clan.',
     pt: '$1 foi rebaixado na função do clã.',
     hu: '$1 viselkedése visszabuktatásához vezetett.',
-    fr: '$1 a été rétrogradé dans le clan.'
+    fr: '$1 a été rétrogradé dans le clan.',
+    pl: '$1 otrzymał degradację w klanie.'
   },
   clanKickOther: {
     en: '$1 was kicked from your clan.',
@@ -60,7 +66,8 @@ export default {
     es: '$1 fue expulsado de tu clan.',
     pt: '$1 foi expulso do teu clã.',
     hu: 'A klánodból kirúgták: $1.',
-    fr: '$1 a été expulsé du clan.'
+    fr: '$1 a été expulsé du clan.',
+    pl: 'Wyrzucono $1 z klanu.'
   },
   clanKickYou: {
     en: 'You were kicked from your clan.',
@@ -69,7 +76,8 @@ export default {
     es: 'Ha sido expulsado del clan.',
     pt: 'Você foi expulso do clã.',
     hu: 'Kirúgtak a klánodból.',
-    fr: 'Vous avez été expulsé du clan.'
+    fr: 'Vous avez été expulsé du clan.',
+    pl: 'Wyrzucono cię z klanu.'
   },
   partyMemberInvite: {
     en: '$1 has been invited by $2 to join your party.',
@@ -78,7 +86,8 @@ export default {
     es: '$1 ha sido invitado por $2 a unirse a tu party',
     pt: '$1 foi convidado por $2 para se juntar à tua party.',
     hu: '$2 a Partydba meghívta: $1.',
-    fr: '$1 a été invité par $2 à rejoindre votre party.'
+    fr: '$1 a été invité par $2 à rejoindre votre party.',
+    pl: '$1 został zaproszony przez $2 aby dołączyć do party.'
   },
   partyMemberJoin: {
     en: '$1 has joined your party.',
@@ -87,7 +96,8 @@ export default {
     es: '$1 se ha unido a tu party.',
     pt: '$1 juntou-se à sua party.',
     hu: '$1 belépett a Partydba.',
-    fr: '$1 a rejoint votre party.'
+    fr: '$1 a rejoint votre party.',
+    pl: '$1 dołącza do party.'
   },
   partyMemberLeave: {
     en: '$1 has left your party.',
@@ -96,7 +106,8 @@ export default {
     es: '$1 ha abandonado tu party.',
     pt: '$1 saiu da sua party.',
     hu: '$1 elhagyta a Partydat.',
-    fr: '$1 a quitté votre party.'
+    fr: '$1 a quitté votre party.',
+    pl: '$1 opuszcza party.'
   },
   partyInvitationDecline: {
     en: '$1 has declined to join your party.',
@@ -105,7 +116,8 @@ export default {
     es: '$1 ha rechazado unirse a tu party.',
     pt: '$1 rejeitou juntar-se à sua party.',
     hu: '$1 elutasította a meghívást a Partydba.',
-    fr: '$1 a refusé l\'invitation à rejoindre votre party.'
+    fr: '$1 a refusé l\'invitation à rejoindre votre party.',
+    pl: '$1 odmawia dołączenia do party.'
   },
   partyMemberPromote: {
     en: '$1 has been promoted in party role.',
@@ -114,13 +126,15 @@ export default {
     es: '$1 ha sido promovido en el rol de la party.',
     pt: '$1 foi promovido na função da party.',
     hu: '$1 előléptetéshez jutott a Partydban.',
-    fr: '$1 a été promu dans la party.'
+    fr: '$1 a été promu dans la party.',
+    pl: '$1 awansował w roli party.'
   },
   partyMemberFound: {
     en: 'Found $1 new party members.',
     de: '$1 neue Party-Mitglieder gefunden.',
     fr: '$1 nouveaux équipiers trouvés.',
-    es: '$1 encontró nuevos miembros de party.'
+    es: '$1 encontró nuevos miembros de party.',
+    pl: '$1 znalazł nowych członków party.'
   },
   partyMemberDemote: {
     en: '$1 has been demoted in party role.',
@@ -129,7 +143,8 @@ export default {
     es: '$1 ha sido rebajado en el rol de la party.',
     pt: '$1 foi rebaixado na função da party.',
     hu: '$1 viselkedése visszabuktatásához vezetett.',
-    fr: '$1 a été rétrogradé dans la party.'
+    fr: '$1 a été rétrogradé dans la party.',
+    pl: '$1 został degradowany w roli party.'
   },
   partyInviteLink: {
     en: '$1 has created a party invitation link: $2.',
@@ -138,7 +153,8 @@ export default {
     es: '$1 ha creado un link de invitación a la party: $2.',
     pt: '$1 criou um link de convite da party: $2.',
     hu: '$1 létrehozta a következő Partymeghívó linket: $2.',
-    fr: '$1 a créé un lien d\'invitation pour la party: $2'
+    fr: '$1 a créé un lien d\'invitation pour la party: $2',
+    pl: '$1 utworzył link zaproszenia do party: $2.'
   },
   partyKickYou: {
     en: 'You have been kicked from the party by $1.',
@@ -147,7 +163,8 @@ export default {
     es: 'Has sido expulsado de la party por $1.',
     pt: 'Você foi expulso da Party pelo $1.',
     hu: '$1 kirúgott a Partydból.',
-    fr: 'Vous avez été expulsé de la party par $1'
+    fr: 'Vous avez été expulsé de la party par $1',
+    pl: 'Usunięto cię z party przez $1.'
   },
   partyKickOther: {
     en: '$1 was kicked from your party by $2.',
@@ -156,19 +173,22 @@ export default {
     es: '$1 fue expulsado de tu party por $2.',
     pt: '$1 foi expulso da sua party pelo $2.',
     hu: '$2 kirúgta a Partydból: $1.',
-    fr: '$1 a été expulsé de votre party par $2.'
+    fr: '$1 a été expulsé de votre party par $2.',
+    pl: '$1 został usunięty z party przez $2.
   },
   partyQueueStart: {
     en: 'Your party has started queue type: $1.',
     de: 'Party-Warteschlange gestartet: $1.',
     fr: 'Votre party a rejoint la file d\'attente de type: $1',
-    es: 'Tu party empezo la fila tipo: $1.'
+    es: 'Tu party empezo la fila tipo: $1.',
+    pl: 'Twoje party rozpoczęło kolejkowanie w typie: $1.'
   },
   partyQueueStop: {
     en: 'Your party has stopped queue type: $1.',
     de: 'Party-Warteschlange gestoppt: $1.',
     fr: 'Votre party a quitté la file d\'attente de type: $1',
-    es: 'Tu party ha parado la fila tipo: $1.'
+    es: 'Tu party ha parado la fila tipo: $1.',
+    pl: 'Twoje party zakończyło kolejkowanie w typie: $1.'
   },
   auctionSold: {
     en: 'Your merchant auction has been sold and $g$1 has been sent to your Stash.',
@@ -177,7 +197,8 @@ export default {
     es: 'Su subasta mercantil ha sido vendida y $g$1 ha sido enviado a tu Stash.',
     pt: 'O seu leilão do mercado foi vendido e $g$1 foram enviadas para o seu Stash.',
     hu: 'A Merchantnál feladott aukciódat eladták ennyiért: $g$1. Ez a Stash fiókodba került.',
-    fr: 'Votre ventre aux enchères a été vendue et $g$1 ont été ajoutés dans votre Stash.'
+    fr: 'Votre ventre aux enchères a été vendue et $g$1 ont été ajoutés dans votre Stash.',
+    pl: 'Twoja aukcja u kupca została sprzedana i $g$1 zostało wysłane do twojego Stash.'
   },
   online: {
     en: '$1 has come online.',
@@ -186,7 +207,8 @@ export default {
     es: '$1 está online.',
     pt: '$1 está online.',
     hu: '$1 belépett a játékba.',
-    fr: '$1 est en ligne.'
+    fr: '$1 est en ligne.',
+    pl: '$1 jest teraz online.'
   },
   offline: {
     en: '$1 has gone offline.',
@@ -195,6 +217,7 @@ export default {
     es: '$1 está offline.',
     pt: '$1 ficou offline.',
     hu: '$1 kilépett a játékból.',
-    fr: '$1 s\'est déconnecté.'
+    fr: '$1 s\'est déconnecté.',
+    pl: '$1 jest teraz offline.'
   }
 }
