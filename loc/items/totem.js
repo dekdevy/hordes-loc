a4defa562238aead446199a9d2e1323fca6cc6dc
Kavalorn47 <58919442+Kavalorn47@users.noreply.github.com>
2019-12-17 21:49:47 +0100

    PL Translation added

--- loc/items/totem.js
+++ loc/items/totem.js
@@ -3,7 +3,8 @@ export default [
     name       : {$$: 'Dream Feathers'},
     description: {
       en: 'These feathers absorb the dreams and nightmares of those that sleep near it. The dreams and nightmares are stored and reused in the form of magic.',
-      de: 'Diese Federn absorbieren die Träume und Alpträume derjenigen, die in ihrer Nähe schlafen. Diese werden gespeichert und in Form von Magie wiederverwendet.'
+      de: 'Diese Federn absorbieren die Träume und Alpträume derjenigen, die in ihrer Nähe schlafen. Diese werden gespeichert und in Form von Magie wiederverwendet.',
+      pl: 'Te pióra absorbują marzenia i koszmary tych, którzy śpią w pobliżu. Marzenia i koszmary są przechowywane i ponownie wykorzystywane w formie magii.'
     }
   },
 
@@ -11,7 +12,8 @@ export default [
     name       : {$$: 'Voodoo Doll'},
     description: {
       en: 'A supernatural creature bound to this doll fuels magical power available to the user.',
-      de: 'Ein an diese Puppe gebundenes übernatürliches Wesen verstärkt die magischen Kräfte, die dem Benutzer zur Verfügung stehen.'
+      de: 'Ein an diese Puppe gebundenes übernatürliches Wesen verstärkt die magischen Kräfte, die dem Benutzer zur Verfügung stehen.',
+      pl: 'Supernaturalna istota powiązana z tą lalką wzmacnia jej użytkownika potęgą magii.'
     }
   },
 
@@ -19,7 +21,8 @@ export default [
     name       : {$$: 'Glowing Chicken'},
     description: {
       en: 'This is a vessel that emanates magic originating from nature.',
-      de: 'Dies ist ein Gefäß, das Naturmagie ausstrahlt.'
+      de: 'Dies ist ein Gefäß, das Naturmagie ausstrahlt.',
+      pl: 'To łącze eminujące magią pochodzącą z natury.'
     }
   },
 
@@ -27,7 +30,8 @@ export default [
     name       : {$$: 'Divine Beads'},
     description: {
       en: 'These beads were carried by holy priests as a means to store divine power and count their blessings. Some of the divine power still resides within each bead.',
-      de: 'Diese Perlen wurden von heiligen Priestern getragen, um die göttliche Kraft zu speichern und ihren Segen zu zählen. Ein Teil der göttlichen Kraft befindet sich noch in jeder Perle.'
+      de: 'Diese Perlen wurden von heiligen Priestern getragen, um die göttliche Kraft zu speichern und ihren Segen zu zählen. Ein Teil der göttlichen Kraft befindet sich noch in jeder Perle.',
+      pl: 'Te perły były noszone przez świętych kapłanów jako środek do gromadzenia boskiej mocy i liczenia ich błogosławieństw. Część boskiej mocy wciąż znajduje się w każdej perle.'
     }
   },
 
@@ -35,7 +39,8 @@ export default [
     name       : {$$: 'Templar\'s Resolve'},
     description: {
       en: 'When defeated templars fail a divine task, they pour spiritual devotion into a personal totem such as this to aid the side of good.',
-      de: 'Wenn besiegte Tempelritter eine göttliche Aufgabe nicht erfüllen, gießen sie spirituelle Hingabe in ein persönliches Totem wie dieses, um das Gute zu unterstützen.'
+      de: 'Wenn besiegte Tempelritter eine göttliche Aufgabe nicht erfüllen, gießen sie spirituelle Hingabe in ein persönliches Totem wie dieses, um das Gute zu unterstützen.',
+      pl: 'Kiedy pokonani templariusze nie wypełniają boskiego zadania, wlewają duchowe oddanie do osobistego totemu, takiego jak ten, aby pomóc stronie dobra.'
     }
   },
 
@@ -43,15 +48,17 @@ export default [
     name       : {$$: 'Holy Cricket'},
     description: {
       en: 'This is one of the many magical crickets that were once owned by the emperors and empresses of Tiger\'s Teeth. To ward against assassinations, magical crickets enhanced the supernatural power of the royalty that carried them.',
-      de: 'Dies ist eine der vielen magischen Grillen, die einst den Kaisern von Tiger\'s Teeth gehörten. Um gegen Attentate vorzugehen, verstärkten magische Grillen die übernatürliche Macht des Herrschers, der sie trug.'
-    }
+      de: 'Dies ist eine der vielen magischen Grillen, die einst den Kaisern von Tiger\'s Teeth gehörten. Um gegen Attentate vorzugehen, verstärkten magische Grillen die übernatürliche Macht des Herrschers, der sie trug.',
+      pl: 'Jest to jeden z wielu magicznych świerszczy, które niegdyś były własnością cesarzy i imperiów Zębów Tygrysa. Aby przeciwdziałać zabójstwom, magiczne świerszcze zwiększały nadnaturalną moc królewskiej rodziny, która je niosła.'
+         }
   },
 
   {
     name       : {$$: 'Laughing Skulls'},
     description: {
       en: 'This is a prison with supernatural creatures that are unwillingly bound together. They generate substantial magical power with their unnatural laughter.',
-      de: 'Dies ist ein Gefängnis gefüllt mit übernatürlichen Kreaturen, die unfreiwillig verbunden sind. Sie erzeugen mit ihrem unnatürlichen Lachen eine beträchtliche magische Kraft.'
+      de: 'Dies ist ein Gefängnis gefüllt mit übernatürlichen Kreaturen, die unfreiwillig verbunden sind. Sie erzeugen mit ihrem unnatürlichen Lachen eine beträchtliche magische Kraft.',
+      pl: 'To więzienie z nadprzyrodzonymi stworzeniami, które niechętnie się ze sobą wiążą. Generują pokaźną magiczną moc swoim nienaturalnym śmiechem.'
     }
   },
 
@@ -59,7 +66,8 @@ export default [
     name       : {$$: 'Hive Mind'},
     description: {
       en: 'A collective of supernatural minds that focus their magical power and bestow it upon the user of their totem. The Hive Mind believes that all creatures must submit to their power and tries to convince the user to join them.',
-      de: 'Ein Kollektiv übernatürlicher Geister, das seine magische Kraft konzentriert und sie dem Nutzer verleiht. Das Hive Mind glaubt, dass sich alle Kreaturen seiner Macht unterwerfen müssen und versucht, den Benutzer davon zu überzeugen sich anzuschließen.'
+      de: 'Ein Kollektiv übernatürlicher Geister, das seine magische Kraft konzentriert und sie dem Nutzer verleiht. Das Hive Mind glaubt, dass sich alle Kreaturen seiner Macht unterwerfen müssen und versucht, den Benutzer davon zu überzeugen sich anzuschließen.',
+      pl: 'Zbiór nadprzyrodzonych umysłów, które skupiają swoją magiczną moc i obdarzają nią użytkownika swojego totemu. Hive Mind wierzy, że wszystkie stworzenia muszą poddać się swojej mocy i próbuje przekonać użytkownika, aby do nich dołączył.'
     }
   },
 
@@ -67,7 +75,8 @@ export default [
     name       : {$$: 'Nganga\'s Serpent'},
     description: {
       en: 'This is an avatar of a supernatural creature. Some scholars say that Nganga’s Serpent is given to those that are chosen and deemed worthy for greatness; however, other scholars suggest that this is another means by which deities try to control and manipulate mortals.',
-      de: 'Dies ist ein Avatar einer übernatürlichen Kreatur. Einige Gelehrte sagen, dass die Schlange von Nganga denen gegeben wird, die für Großes bestimmt und für würdig befunden werden; andere sagen, dass dies ein weiteres Mittel ist mit dem Götter versuchen, Sterbliche zu manipulieren.'
+      de: 'Dies ist ein Avatar einer übernatürlichen Kreatur. Einige Gelehrte sagen, dass die Schlange von Nganga denen gegeben wird, die für Großes bestimmt und für würdig befunden werden; andere sagen, dass dies ein weiteres Mittel ist mit dem Götter versuchen, Sterbliche zu manipulieren.',
+      pl: 'To jest avatar nadprzyrodzonego stworzenia. Niektórzy uczeni twierdzą, że Wąż Nganga jest dany tym, którzy zostali wybrani i uznani za godnych wielkości; jednakże inni uczeni sugerują, że jest to kolejny sposób, w jaki bóstwa próbują kontrolować śmiertelników i nimi manipulować.'
     }
   },
 
@@ -75,7 +84,8 @@ export default [
     name       : {$$: 'Sangoma\'s Bones'},
     description: {
       en: 'These ancient bones are said to be small fragments of a forgotten monster deity. The bones radiate power beyond imagination.',
-      de: 'Diese alten Knochen sollen kleine Fragmente einer vergessenen Monstergottheit sein. Die Knochen unvorstellbare Kraft aus.'
+      de: 'Diese alten Knochen sollen kleine Fragmente einer vergessenen Monstergottheit sein. Die Knochen unvorstellbare Kraft aus.',
+      pl: 'Mówi się, że te starożytne kości to małe fragmenty zapomnianego bóstwa potworów. Kości promieniują mocą poza wyobraźnią.'
     }
   }
 
