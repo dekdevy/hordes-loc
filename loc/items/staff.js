97f9db26ba379b87f8b489736e8ea505d578356d
Kavalorn47 <58919442+Kavalorn47@users.noreply.github.com>
2019-12-17 22:30:33 +0100

    Added PL translation for staffs

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -2,119 +2,136 @@ export default [
   {
     name       : {$$: 'Broken Twig' },
     description: {
-      en: 'Sticks and broken twigs are used by apprentice mages for educational purposes.'
+      en: 'Sticks and broken twigs are used by apprentice mages for educational purposes.',
+      pl: 'Kije i połamane gałązki są używane przez aspirujących magów do celów edukacyjnych.'
     }
   },
 
   {
     name       : {$$: 'Cracked Stick' },
     description: {
-      en: 'This is a damaged battlestaff that still has some use for beginning magic users.'
+      en: 'This is a damaged battlestaff that still has some use for beginning magic users.',
+      pl: 'Jest to zniszczony kostur bojowy, który wciąż jest przydatny dla początkujących użytkowników magii.'
     }
   },
 
   {
     name       : {$$: 'Gnarled Broomstick' },
     description: {
-      en: 'Some of the oldest trees are used to create Gnarled Broomsticks. These weapons produce spells to sweep enemies off the battlefield.'
+      en: 'Some of the oldest trees are used to create Gnarled Broomsticks. These weapons produce spells to sweep enemies off the battlefield.',
+      pl: 'Niektóre z najstarszych drzew służą do tworzenia sękatych mioteł. Broń ta wytwarza zaklęcia, które dosłownie zmiatają wrogów z pola walki.'
     }
   },
 
   {
     name       : {$$: 'Oak Staff' },
     description: {
-      en: 'This is a sturdy oak weapon for casting intermediate magic.'
+      en: 'This is a sturdy oak weapon for casting intermediate magic.',
+      pl: 'To solidna broń dębowa do rzucania magii.'
     }
   },
 
   {
     name       : {$$: 'Mystic Wand' },
     description: {
-      en: 'This wand is a gift from the Mystics that is embedded with strange shards that enhance magical power.'
+      en: 'This wand is a gift from the Mystics that is embedded with strange shards that enhance magical power.',
+      pl: 'Ta różdżka jest darem od Mistyka. Ma w sobie osadzone dziwne odłamki, które zwiększają magiczną moc.'
     }
   },
 
   {
     name       : {$$: 'Bone Staff' },
     description: {
-      en: 'This is made from the bones of monster mages to augment magical power.'
+      en: 'This is made from the bones of monster mages to augment magical power.',
+      pl: 'Zrobione z kości magów-potworów, co pozwala zwiększyć magiczną moc.'
     }
   },
 
   {
     name       : {$$: 'Encrusted Rod' },
     description: {
-      en: 'This rod is encrusted with gems that store magical power.'
+      en: 'This rod is encrusted with gems that store magical power.',
+      pl: 'Ta wędka jest inkrustowana klejnotami, które przechowują w sobie magiczną moc.'
     }
   },
 
   {
     name       : {$$: 'Imbued Staff' },
     description: {
-      en: 'The etched runes on this staff faintly glow when spells are cast. Although several mage guilds clearly believe that the runes augment spell casting, the true nature of the runes is unknown.'
+      en: 'The etched runes on this staff faintly glow when spells are cast. Although several mage guilds clearly believe that the runes augment spell casting, the true nature of the runes is unknown.',
+      pl: 'Wytrawione runy na kiju świecą delikatnie, gdy rzucane są czary. Chociaż kilka gildii magów wierzy, że rzucanie zaklęć zwiększa siłę run, ich prawdziwa natura jest nieznana.'
     }
   },
 
   {
     name       : {$$: 'Emerald Staff' },
     description: {
-      en: 'This staff uses an enchanted stone to channel and focus magical energies. The process of finding such a special emerald, along with the expensive enchanting process, makes this staff somewhat difficult to craft.'
+      en: 'This staff uses an enchanted stone to channel and focus magical energies. The process of finding such a special emerald, along with the expensive enchanting process, makes this staff somewhat difficult to craft.',
+      pl: 'Ten kostur używa zaczarowanego kamienia do ukierunkowywania i skupiania magicznych energii. Proces znajdowania takiego specjalnego szmaragdu, wraz z kosztownym procesem zaklęć, sprawia, że ten kostur jest skomplikowany do wytworzenia.'
     }
   },
 
   {
     name       : {$$: 'Sapphire Staff' },
     description: {
-      en: 'This staff is made of Dragonwood and uses stones that were originally dragon gifts. The residual traces of draconic power contained in the stones compound magic’s potency.'
+      en: 'This staff is made of Dragonwood and uses stones that were originally dragon gifts. The residual traces of draconic power contained in the stones compound magic’s potency.',
+      pl: 'Ta laska wykonana jest z Dragonwood i używa kamieni, które niegdyś były smoczymi prezentami. Resztki smoczej mocy zawarte w kamieniach potęgują moc magii użytkownika.'
     }
   },
 
   {
     name       : {$$: 'Frozen Greatstaff' },
     description: {
-      en: 'The Frozen Greatstaff is made by mortals with ice found only in the Underworld. Although the construction methods are flawed, the results can still be quite powerful.'
+      en: 'The Frozen Greatstaff is made by mortals with ice found only in the Underworld. Although the construction methods are flawed, the results can still be quite powerful.',
+      pl: 'Frozen Greatstaff jest wytwarzany przez śmiertelników z lodu, który występuje tylko w Zaświatach. Mimo, iż metody tworzenia pozostawiają wiele do życzenia, wyniki mogą być dość obiecujące.'
     }
   },
 
   {
     name       : {$$: 'Infernal Staff' },
     description: {
-      en: 'This staff is made of Underwood that underwent a magical tempering process using Underworld Flames. Although the staffs were undoubtedly crafted by mortal hands, the method to obtain Underworld Flames seems to be a secret lost to time.'
+      en: 'This staff is made of Underwood that underwent a magical tempering process using Underworld Flames. Although the staffs were undoubtedly crafted by mortal hands, the method to obtain Underworld Flames seems to be a secret lost to time.',
+      pl: 'Laska ta wykonana jest z Podszycia, które przeszło magiczny proces utwardzania przy użyciu Płomieni Podziemia. Chociaż laska została bez wątpienia wykonana przez śmiertelne ręce, metoda zdobywania Płomieni Podziemia wydaje się być tajemnicą zagubioną w czasie.'
     }
   },
 
   {
     name       : {$$: 'Hellfire Greatstaff' },
     description: {
-      en: 'Hellfire Greatstaffs are constructed with hellfires provided by mischievous devils. Although the process to create a staff such as this is mysterious, the price exacted by the devils involved is not.'
+      en: 'Hellfire Greatstaffs are constructed with hellfires provided by mischievous devils. Although the process to create a staff such as this is mysterious, the price exacted by the devils involved is not.',
+      pl: 'Piekielne Kostury Bojowe są zbudowane z piekielnych ognisk dostarczanych przez psotne diabły. Choć proces tworzenia takiej laski jest tajemniczy, cena pobierana przez zaangażowane diabły jest dość znana.'
     }
   },
 
   {
     name       : {$$: 'Divine Staff' },
     description: {
-      en: 'This kind of staff is modeled from ancient texts. Part of the crafting process requires hundreds of devout beings to request blessings upon the staff by specific deities.'
+      en: 'This kind of staff is modeled from ancient texts. Part of the crafting process requires hundreds of devout beings to request blessings upon the staff by specific deities.',
+      pl: 'Ten rodzaj kostura wzorowany jest na starożytnych tekstach. Część procesu rzemieślniczego wymaga setek pobożnych istot, które proszą o wzmocnienie kostura poprzez boskie błogosławieństwa.'
     }
   },
 
   {
     name       : {$$: 'Crystal Core' },
     description: {
-      en: 'This strange staff utilizes crystals from the Realm of Madness in order to enhance wild magics in nature. The crystals are the central component of this staff.'
+      en: 'This strange staff utilizes crystals from the Realm of Madness in order to enhance wild magics in nature. The crystals are the central component of this staff.',
+      pl: 'Ta dziwna laska wykorzystuje kryształy z Królestwa Szaleństwa w celu wzmocnienia dzikiej magii w przyrodzie. Kryształy są głównym składnikiem tej laski.'
     }
   },
 
   {
     name       : {$$: 'Witch\'s Heart' },
     description: {
-      en: 'The heart of an elder witch powers this staff’s magic. Forbidden powers reanimated the beating heart, which causes the staff to symbolically embody a representation of the undead.'
+      en: 'The heart of an elder witch powers this staff’s magic. Forbidden powers reanimated the beating heart, which causes the staff to symbolically embody a representation of the undead.',
+      pl: 'Serce starszej wiedźmy napędza magię tego kostura. Zakazane moce ożywiły bijące serce, co powoduje, że laska symbolicznie ucieleśnia nieumarłych.'
     }
   },
 
   {
     name       : {$$: 'Deathweaver' },
     description: {
-      en: 'Deathweavers are relics that have survived the creation of the great barrier. These strange staffs are not of this world.'
+      en: 'Deathweavers are relics that have survived the creation of the great barrier. These strange staffs are not of this world.',
+      pl: 'Tkacze Śmierci to relikwie, które przetrwały powstanie wielkiej bariery. Te dziwne laski są nie z tego świata.'
     }
   }
 
