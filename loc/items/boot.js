300092984ee87f045156950b6706dd19ec544670
Aerics84 <58844333+Aerics84@users.noreply.github.com>
2019-12-18 14:28:19 +0100

    Added german translation.

--- loc/items/boot.js
+++ loc/items/boot.js
@@ -4,7 +4,8 @@ export default [
     description: {
       en: 'This footwear is primarily made for comfort rather than running.',
       hu: 'Ez a lábbeli inkább a kényelmet mintsem a védelmet szolgálja.',
-      ru: 'Эта обувь больше предназначена для комфорта, чём для бега.'
+      ru: 'Эта обувь больше предназначена для комфорта, чём для бега.',
+      de: 'Dieses Schuhwerk ist vor allem auf Komfort und nicht auf Laufen ausgelegt.'
     }
   },
 
@@ -13,7 +14,8 @@ export default [
     description: {
       en: 'These footpads are wrapped in a makeshift material to increase running stability.',
       hu: 'Ezeket a bocskorokat erős szövetbe csavarták, hogy megkönnyítsék a futást.',
-      ru: 'Эта обувь обернута грубым материалом для повышения стабильности бега.'
+      ru: 'Эта обувь обернута грубым материалом для повышения стабильности бега.',
+      de: 'Diese Fußpolster sind in ein provisorisches Material gehüllt, um die Laufstabilität zu erhöhen.'
     }
   },
 
@@ -22,7 +24,8 @@ export default [
     description: {
       en: 'Leather shoes are created with fine craftsmanship for moderate battle durability.',
       hu: 'Az erős bőr megfelelő megmunkálás után akár harci védelmet is nyújthat.',
-      ru: 'Эта кожаная обувь сделана с тонким мастерством для умеренной боевой прочности.'
+      ru: 'Эта кожаная обувь сделана с тонким мастерством для умеренной боевой прочности.',
+      de: 'Lederschuhe werden mit feiner Handwerkskunst für eine mäßige Haltbarkeit im Kampf hergestellt.'
     }
   },
 
@@ -31,7 +34,8 @@ export default [
     description: {
       en: 'The monster bones used in the creation of footwear such as this seem to enhance movement speed, but whether the enhancement is through magic or the lightness of the material is not known.',
       hu: 'A szörnyetegek a lábbeli alkotásakor a futási képességre is ki akartak hatni. Hogy ezt egyszerűen a csizma könnyedsége okozza, vagy valamiféle bűbály; nem tudni egyenlőre.',
-      ru: 'При изготовлении обуви использовались кости монстров, похоже, увеличивают скорость передвижения, но вопрос в том, происходит ли это с помощью магии или из-за легкости материала, остается неизвестным.'
+      ru: 'При изготовлении обуви использовались кости монстров, похоже, увеличивают скорость передвижения, но вопрос в том, происходит ли это с помощью магии или из-за легкости материала, остается неизвестным.',
+      de: 'Die Monsterknochen, die bei der Herstellung derartiger Schuhen verwendet werden, scheinen die Bewegungsgeschwindigkeit zu erhöhen. Ob die Verbesserung jedoch durch Magie oder die Leichtigkeit des Materials erfolgt, ist nicht bekannt.'
     }
   },
 
@@ -40,7 +44,8 @@ export default [
     description: {
       en: 'This footwear is carefully crafted with monster scales that are both light and robust for battle.',
       hu: 'Ezek a bocskorokat nagy körültekintéssel készítették, így elég könnyűek és robosztusok is a csatára.',
-      ru: 'Эта обувь тщательно изготовлена с использованием чешуек монстров, которые одновременно легки и прочны в бою.'
+      ru: 'Эта обувь тщательно изготовлена с использованием чешуек монстров, которые одновременно легки и прочны в бою.',
+      de: 'Dieses Schuhwerk wurde sorgfältig mit Monsterschuppen gefertigt, die sowohl leicht als auch robust für den Kampf geeignet sind.'
     }
   },
 
@@ -49,7 +54,8 @@ export default [
     description: {
       en: 'Enchanted shoes such as this push the wearer\'s feet with shadow magic to augment speed.',
       hu: 'Az ehhez hasonlatos bűvös lábbelik az árnyékmágiával irányítják viselőjük lábát, nagy sebességet biztosítva.',
-      ru: 'Волшебный ботинки, как эти, толкают ноги владельца с помощью теневой магии, чтобы увеличить скорость.'
+      ru: 'Волшебный ботинки, как эти, толкают ноги владельца с помощью теневой магии, чтобы увеличить скорость.',
+      de: 'Verzauberte Schuhe wie diese treiben die Füße des Trägers mit Schattenmagie an, um die Geschwindigkeit zu erhöhen.'
     }
   },
 
@@ -58,7 +64,8 @@ export default [
     description: {
       en: 'These boots are crafted from sturdy iron and have been used in many wars.',
       hu: 'Ezek kemény, tömör vascsizmák, amiket számos nagy háborúban viseltek.',
-      ru: 'Эти ботинки изготовлены из прочного железа и использовались во многих войнах.'
+      ru: 'Эти ботинки изготовлены из прочного железа и использовались во многих войнах.',
+      de: 'Diese Schuhe sind aus robustem Eisen gefertigt und wurden in vielen Kriegen getragen.'
     }
   },
 
@@ -67,7 +74,8 @@ export default [
     description: {
       en: 'Footwear such as this is etched with runes that increase the wearer’s speed. However, scholars are hesitant about the use of these runes since their true purpose and nature is unclear.',
       hu: 'Az ilyen bocskorokat rúnákkal erősítették meg, amik megnövelik a viselő mozgékonyságát. Ám a mágus társaságok óvakodnak ezektől, mondván, a rúnák mibenléte és működése ismeretlen számukra.',
-      ru: 'Обувь, подобная этой, выгравирована рунами, которые увеличивают скорость владелеца. Однако ученые неуверенные по поводу использования этих рун, поскольку их истинное назначение и природа неясны.'
+      ru: 'Обувь, подобная этой, выгравирована рунами, которые увеличивают скорость владелеца. Однако ученые неуверенные по поводу использования этих рун, поскольку их истинное назначение и природа неясны.',
+      de: 'Schuhe wie dieses sind mit Runen verziert, welche die Geschwindigkeit des Trägers erhöhen. Gelehrte zögern jedoch bei der Verwendung dieser Runen, da ihr wahrer Zweck und ihre Natur unklar ist.'
     }
   },
 
@@ -76,7 +84,8 @@ export default [
     description: {
       en: 'Elvish magic is assisted by wind spirits to create Skyswift Boots. Although Elves are very close to nature, even they know little about the reclusive wind spirits.',
       hu: 'Az Elf mágia a szelek szellemeivel összefogva fonja az Égsebes Csizmákat. Habár az Elfek igen közel állanak efféle jószágokhoz, elég keveset tudnak a haragos viharszellemről.',
-      ru: 'Эльфийская магия создаёт такие ботинки при помощи духов ветра. Хоть эльфы и очень близки к природе, но даже они мало знают о духах ветра.'
+      ru: 'Эльфийская магия создаёт такие ботинки при помощи духов ветра. Хоть эльфы и очень близки к природе, но даже они мало знают о духах ветра.',
+      de: 'Die elfische Magie wird von Windgeistern unterstützt, um Skyswift Boots zu erstellen. Obwohl die Elfen sehr naturverbunden sind, wissen selbst sie wenig über die zurückgezogenen Windgeister.'
     }
   },
 
@@ -85,7 +94,8 @@ export default [
     description: {
       en: 'These boots are forged without heat and with enchanted hammers to keep the innate properties of the special metal. This rare metal is most often found in the mountains.',
       hu: 'A híres hő nélküli kovácsolás termékei ezek a lábbelik is; bűvölt kalapácsokkal tartják meg a ritka fém veleszületett varázserejét. Ez a fém leggyakrabban a hegyekben fordul elő.',
-      ru: 'Эти ботинки выкованы без нагрева, с волшебными молотками, чтобы сохранить врожденные свойства специального металла. Этот редкий металл чаще всего встречается в горах.'
+      ru: 'Эти ботинки выкованы без нагрева, с волшебными молотками, чтобы сохранить врожденные свойства специального металла. Этот редкий металл чаще всего встречается в горах.',
+      de: 'Diese Schuhe wurden ohne Hitze und mit verzauberten Hämmern geschmiedet, um die natürlichen Eigenschaften des speziellen Metalls zu erhalten. Dieses seltene Metall ist am häufigsten in den Bergen zu finden.'
     }
   },
 
@@ -94,7 +104,8 @@ export default [
     description: {
       en: 'These boots are created by monster craftsmen with the help of sky spirits. Some monster factions have a close connection with sky spirits and nature; however, monsters do not frequently talk about this relationship.',
       hu: 'A bocskorok égi szellemek és szörnyetegek művei. Néhány bestiaszövetség szoros kapcsolatokat ápol a légi lényekkel és a természettel; habár, nem kürtölik ezt sem világgá. Különös jószágok már csak ezek.',
-      ru: 'Эти ботинки были созданы мастерами-монстрами с помощью небесных духов. Некоторые фракции монстров имеют тесную связь с небесными духами и природой, однако, монстры не часто говорят об этих отношениях.'
+      ru: 'Эти ботинки были созданы мастерами-монстрами с помощью небесных духов. Некоторые фракции монстров имеют тесную связь с небесными духами и природой, однако, монстры не часто говорят об этих отношениях.',
+      de: 'Diese Schuhe werden von monströsen Handwerkern mit Hilfe von Wolkengeistern hergestellt. Einige Monstervölker haben eine enge Verbindung zu den Geistern des Himmels und der Natur. Monster sprechen jedoch nicht oft über diese Beziehung.'
     }
   },
 
@@ -103,7 +114,8 @@ export default [
     description: {
       en: 'These boots are named after a famous relic legend. The namesake of these boots were supposedly the fast footwear of a famous deity, but no one seems to know where the story originates.',
       hu: 'Egy híres leganda után nevezték el a csizmákat. A lábbelik névadója egy ismert istenség gyors saruja volt, de azt senki nem tudja, honnan származik a történet, és ki is volt ez az istenség.',
-      ru: 'Эти ботинки названы в честь знаменитой реликтовой легенды. Тезка этих ботинок предположительно была быстрой обувью знаменитого божества, но, похоже, никто не знает, откуда взялась эта история.'
+      ru: 'Эти ботинки названы в честь знаменитой реликтовой легенды. Тезка этих ботинок предположительно была быстрой обувью знаменитого божества, но, похоже, никто не знает, откуда взялась эта история.',
+      de: 'Diese Schuhe sind nach einer berühmten Reliktlegende benannt. Der Namensgeber dieser Stiefel war angeblich das schnelle Schuhwerk einer berühmten Gottheit, aber niemand scheint zu wissen, woher die Geschichte kommt.'
     }
   },
 
@@ -112,7 +124,8 @@ export default [
     description: {
       en: 'This footwear is composed of meteorite pieces. Black meteorites were found during a terrible catastrophe where fire rained down from the sky and destroyed much of the landscape.',
       hu: 'Ezt a lábbelit meteoritszilánkok alkotják. Fekete kövek a borzalmas katasztrófák során kerülnek a földre, amikor az égbolt meggyullad, és a kénköves pokol elszabadul mindenfelé.',
-      ru: 'Эта обувь состоит из осколков метеорита. Черные метеориты были обнаружены во время страшной катастрофы, когда огонь падал с небес и уничтожил большую часть ландшафта.'
+      ru: 'Эта обувь состоит из осколков метеорита. Черные метеориты были обнаружены во время страшной катастрофы, когда огонь падал с небес и уничтожил большую часть ландшафта.',
+      de: 'Dieses Schuhwerk besteht aus Meteoritenstücken. Schwarze Meteoriten wurden während einer schrecklichen Katastrophe gefunden, bei der es vom Himmel Feuer regnete und einen Großteil der Landschaft zerstörte.'
     }
   }
 
