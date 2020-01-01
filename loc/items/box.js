c9b877d887b2e05ebbd867ac455f8ae335a5983f
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-01-01 14:51:43 +0000

    Update box.js

--- loc/items/box.js
+++ loc/items/box.js
@@ -2,7 +2,9 @@ export default [
   {
     name       : {$$: 'Mysterious Saddle Chest'},
     description: {
+      // Do NOT translate "Faivel"
       en: 'Shipped from unexplored regions of Faivel. The contents of this cage cannot be inspected until opened.',
+      ru: 'Доставлено из неисследованных районов Faivel. Содержимое этой клетки не может быть проверено, пока не будет открыто.',
       de: 'Aus unentdeckten Regionen von Faivel geliefert. Der Inhalt dieses Käfigs kann nicht inspiziert werden, ohne ihn zu öffnen.'
     }
   }
