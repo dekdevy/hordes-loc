3c1d87685096c805cb9dfbbf3adae5b489024ffc
EternalLume <58798429+EternalLume@users.noreply.github.com>
2020-01-01 15:40:36 -0500

    Polish Translation for Box.js

--- loc/items/box.js
+++ loc/items/box.js
@@ -3,6 +3,7 @@ export default [
     name       : {$$: 'Mysterious Saddle Chest'},
     description: {
       en: 'Shipped from unexplored regions of Faivel. The contents of this cage cannot be inspected until opened.',
+      pl: 'Wysyłane z niezbadanych regionów Faivel. Zawartość tej klatki nie może być sprawdzana aż do momentu jej otwarcia'
       de: 'Aus unentdeckten Regionen von Faivel geliefert. Der Inhalt dieses Käfigs kann nicht inspiziert werden, ohne ihn zu öffnen.'
     }
   }
