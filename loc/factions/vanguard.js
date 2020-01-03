323d9b9ba7569c23191295b088b55ef1b224ad7d
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-01-03 12:05:29 +0000

    Bengali Vanguard translation.

--- loc/factions/vanguard.js
+++ loc/factions/vanguard.js
@@ -19,6 +19,7 @@ export default {
     ro: 'Facțiunea Vanguard caută tradiția, structura și ordinea în societate. Castelul lor poate fi găsit într-o zonă verde luxuriantă din jurul Guardstone.',
     zh: 'Vanguard部落尋求社會的傳統，結構和秩序。 他們的城堡在Guardstone周圍鬱鬱蔥蔥的綠色區域。',
     hu: 'A Vanguard hordái a rendet és a szervezettséget kívánják megvalósítani, és erősen tisztelik a hagyományt. A büszke kastélyuk Guardstone buja zöld erdelyében húzódik meg.',
-    pl: 'Horda Vanguard poszukuje tradycji, struktury i porządku w społeczeństwie. Ich zamek znajduje się na zielonych włościach wokół Guardstone.'
+    pl: 'Horda Vanguard poszukuje tradycji, struktury i porządku w społeczeństwie. Ich zamek znajduje się na zielonych włościach wokół Guardstone.',
+    bn: 'Vanguard সৈন্যদল সমাজে ঐতিহ্য, কাঠামো এবং শৃঙ্খলা তালাশ করে। তাদের দুর্গ Guardstone এর চারপাশে একটি ঘন সবুজ অঞ্চলে পাওয়া যেতে পারে।'
   }
 }
