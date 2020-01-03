28043ccc5d03400abadaa317865e553cf7a45cef
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-01-03 12:11:33 +0000

    Bengali warrior translation.

--- loc/classes/warrior.js
+++ loc/classes/warrior.js
@@ -15,7 +15,8 @@ export default {
     ro: 'Războinic',
     zh: '戰士',
     hu: 'Harcos',
-    pl: 'Wojownik'
+    pl: 'Wojownik',
+    bn: 'যোদ্ধা'
   },
   // Class description, visible during character creation
   description: {
@@ -32,6 +33,7 @@ export default {
     ro: 'Războinicii sunt făcuți pentru a atrage mai mulți monștrii decât orice altă clasă, având o defensivă foarte ridicată datorită echipamentului (Scuturi) și buff-uri defensive puternice.',
     zh: '通過裝備（盾牌）和強大的防守增益效果，戰士可以打敗比其他角色更多的怪物。',
     hu: 'A harcosok hatalmas védekező képességükre és erejükre támaszkodva magukra vállalhatják a közelharcot számos ellenséggel, hogy a csapatjuk többi tagja zavartalanul használhassa mágiáját.',
-    pl: 'Wojownicy są stworzeni aby tankować więcej potworów niż jakakolwiek inna klasa, a to dzięki wysokiej obronie i pomocy wyposażenia (tarcze) oraz silnych buffów obronnych.'
+    pl: 'Wojownicy są stworzeni aby tankować więcej potworów niż jakakolwiek inna klasa, a to dzięki wysokiej obronie i pomocy wyposażenia (tarcze) oraz silnych buffów obronnych.',
+    bn: 'যোদ্ধারা অন্য যেকোনো শ্রেণী থেকে বেশী শত্রু যখম করতে পারে তাদের সরঞ্জামের (ঢাল) ও শক্তিশালী বাফের থেকে পাওয়া উচ্চমাত্রার প্রতিরক্ষা ব্যাবস্থার ফলে।'
   }
 }
