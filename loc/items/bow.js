2f46792a1334332cf255054397353256d36344f8
trieu1994 <nguyenquangtrieu1994@gmail.com>
2020-01-07 08:59:23 +0700

    Add Vietnamese bow translation

--- loc/items/bow.js
+++ loc/items/bow.js
@@ -6,7 +6,8 @@ export default [
       hu: 'Ez az íj egy egyszerű barkács; a fa nem nő ki mindenhol, ne csudáljuk, hogy egyes népek megpróbálják a legtöbbet a kéznél lévő anyagokból kicsikarni.',
       ru: 'Этот лук - самоделка. Древесина не является богатым ресурсом в некоторых регионах, поэтому жители иногда используют имеющиеся материалы для создания такого оружия, как это.',
       de: 'Dieser Bogen ist eine provisorische Kreation. Holz ist eine seltene Ressource in manchen Regionen, daher nutzen Bewohner manchmal vorhandene Materialien, um solche Waffen herzustellen.',
-      es: 'Este arco es una creación temporal. No hay mucha madera en algunas regiones, por lo tanto los Denizens usan el material que tienen a mano para crear armas como esta.'
+      es: 'Este arco es una creación temporal. No hay mucha madera en algunas regiones, por lo tanto los Denizens usan el material que tienen a mano para crear armas como esta.',
+      vi: 'Cung này là một sáng tạo tạm thời. Gỗ không phải là nguồn tài nguyên dồi dào ở một số vùng, do đó, người dân đôi khi sử dụng bất kỳ vật liệu sẵn có nào để tạo ra vũ khí như thế này.'
     }
   },
 
@@ -17,7 +18,8 @@ export default [
       hu: 'Az ilyen íjakat fiatal lövészek kapják, hogy legyen mivel gyakorolniuk.',
       ru: 'Такие луки созданы для начинающих лучников, чтобы улучшать свои навыки.',
       de: 'Bögen wie dieser werden von Anfänger-Bogenschützen zum Üben genutzt.',
-      es: 'Arcos como estos son manufacturados para arqueros iniciados de forma que estos puedan refinar sus habilidades.'
+      es: 'Arcos como estos son manufacturados para arqueros iniciados de forma que estos puedan refinar sus habilidades.',
+      vi: 'Những chiếc cung như thế này được tạo ra để các cung thủ bắt đầu trau dồi kỹ năng của họ'
     }
   },
 
@@ -28,7 +30,8 @@ export default [
       hu: 'A kézíjakkal remekül lehet vadászni, ezért is olyan elterjedtek.',
       ru: 'Такие луки в основном производятся для охоты на местных диких животных.',
       de: 'Gekrümmte Kurzbögen werden hauptsächlich zur Jagd hergestellt.',
-      es: 'Los Curved Shortbows son mayoritariamente producidos para cazar la fauna local.'
+      es: 'Los Curved Shortbows son mayoritariamente producidos para cazar la fauna local.',
+      vi: 'Cung ngắn cong chủ yếu được dùng để săn bắn động vật hoang dã.'
     }
   },
 
@@ -39,7 +42,8 @@ export default [
       hu: 'Ez az íj az általános fegyverválasztása a legtöbb kalandozónak.',
       ru: 'Этот тип лука является стандартным выбором оружия для искателей приключений.',
       de: 'Diese Sorte Bogen ist die Standardwaffe für Abenteurer.',
-      es: 'Este tipo de arco son el arma de elección estándar para los aventureros.'
+      es: 'Este tipo de arco son el arma de elección estándar para los aventureros.',
+      vi: 'Loại cung này là vũ khí tiêu chuẩn để các nhà thám hiểm lựa chọn.'
     }
   },
 
@@ -50,7 +54,8 @@ export default [
       hu: 'A hosszúíjakat egyetlen célra szerkesztették, mégpedig háborúra.',
       ru: 'Длинный лук задуман как оружие войны.',
       de: 'Langbögen sind als Kriegswaffen entwickelt worden.',
-      es: 'Los Longbows son diseñados para ser armas de guerra.'
+      es: 'Los Longbows son diseñados para ser armas de guerra.',
+      vi: 'Cung dài được thiết kế trở thành vũ khí trong các trận chiến.'
     }
   },
 
@@ -61,7 +66,8 @@ export default [
       hu: 'Ez az íj csontokból áll, hogy magas szintű rugalmasságot és erőt biztosítson.',
       ru: 'Этот лук сделан из костей монстров для обеспечения дополнительной упругости и силы.',
       de: 'Dieser Bogen wurde aus Monster-Knochen hergestellt um sehr belastbar und stark zu sein.',
-      es: 'Este arco es confeccionado con huesos de monstruo para que así tenga una resistencia y potencia mayor.'
+      es: 'Este arco es confeccionado con huesos de monstruo para que así tenga una resistencia y potencia mayor.',
+      vi: 'Cung này được chế tạo từ xương quái vật tăng thêm khả năng phục hồi và sức mạnh.'
     }
   },
 
@@ -72,7 +78,8 @@ export default [
       hu: 'Az Elf mesteremberek kivételes gondot fektetnek az íjaikba, és az íjászaik mindig kitüntetik magukat a harci időszakokban, mégpedig nem akármiért.',
       ru: 'Эльфийские мастера проявляют исключительную осторожность во время изготовления своих луков. Эльфийские лучники имеют восхитительную военную репутацию.',
       de: 'Elfische Handwerker sind während des Herstellungsprozesses besondersd sorgfältig, und elfische Bogenschützen haben in Kriegszeiten aus gutem Grund einen guten Ruf.',
-      es: 'Los artesanos Élficos ponen un cuidado exquisito en sus arcos durante el proceso de manufacturado, es por esto que los arqueros Elfos tienen una orgullosa reputación durante tiempos de guerra, y con motivos.'
+      es: 'Los artesanos Élficos ponen un cuidado exquisito en sus arcos durante el proceso de manufacturado, es por esto que los arqueros Elfos tienen una orgullosa reputación durante tiempos de guerra, y con motivos.',
+      vi: 'Thợ thủ công Elvish rất tỉ mỉ trong quá trình chế tạo cung của họ, và cung thủ Elvish có một danh tiếng đáng tự hào trong thời gian chiến tranh vì một lý do.'
     }
   },
 
@@ -83,7 +90,8 @@ export default [
       hu: 'Az ősi íjak még vándorló ősmagyar törzsek keze munkái. Nemzedékről nemzedékre, apáról fiúra szálltak a fegyverek, és a készítésük titkai egyaránt.',
       ru: 'Древние луки, как этот, исключительное оружие, созданное забытыми мастерами. Эти луки часто передаются из поколения в поколение как фамильная ценность.',
       de: 'Altertümliche Bögen sind außergewöhnliche Waffen die von vergessenen Meisterhandwerken hergestellt wurden. Diese Bögen werden über Generationen weitergegeben.',
-      es: 'Los Ancient Bows son armas excepcionales que fueron diseñadas por maestros artesanos ya olvidados. Estos arcos normalmente son heredados de generación en generación.'
+      es: 'Los Ancient Bows son armas excepcionales que fueron diseñadas por maestros artesanos ya olvidados. Estos arcos normalmente son heredados de generación en generación.',
+      vi: 'Cung cổ xưa là vũ khí đặc biệt được thiết kế bởi những nghệ nhân bậc thầy bị lãng quên. Những chiếc cung này thường được truyền từ thế hệ này sang thế hệ khác.'
     }
   },
 
@@ -94,7 +102,8 @@ export default [
       hu: 'Amint megjelentek a szörnyetegek talpig vasban, felütötték a fejüket efféle anarmnensek is.',
       ru: 'Эти луки были разработаны чтобы легче справляться с броней монстров.',
       de: 'Diese Bögen wurden als Antwort auf gerüstete Monster entwickelt.',
-      es: 'Estos arcos fueron desarrollados en respuesta a los monstruos que llevan armadura.'
+      es: 'Estos arcos fueron desarrollados en respuesta a los monstruos que llevan armadura.',
+      vi: 'Những chiếc cung này được thiết kế để chống lại những loại quái vật mang giáp.'
     }
   },
 
@@ -105,7 +114,8 @@ export default [
       hu: 'Ez az íjforma sokkal hatékonyabb, mint a hosszúíj. A dístítő ezüst különleges mintája tanúbizonsága annak a mesteri tudásnak, ami képes volt egy ilyen fegyver elkészítésére.',
       ru: 'Эта конструкция лука более эффективна с механической точки зрения, чем длинный лук. Уникальный узор из декоративного серебра является свидетельством мастерства, необходимого для изготовления такого величественного лука.',
       de: 'Das Design dieses Bogens ist effizienter als das eines Langbogens. Das einmalige dekorative Silbermuster ist ein Zeugnis der Handwerkskunst, die für einen solchen Bogen benötigt wird.',
-      es: 'El diseño de este arco es más mecánico y eficiente que el del Longbow. El patrón único decorativo de plata es un testamento al manufacturado requerido para confeccionar tal buen arco.'
+      es: 'El diseño de este arco es más mecánico y eficiente que el del Longbow. El patrón único decorativo de plata es un testamento al manufacturado requerido para confeccionar tal buen arco.',
+      vi: 'Thiết kế cung này là hiệu quả cơ học hơn so với cung dài. Hoa văn độc đáo được mạ bạc trên cung là một minh chứng cho sự khéo léo cần có để tạo ra một chiếc cung tinh xảo như vậy.'
     }
   },
 
@@ -116,7 +126,8 @@ export default [
       hu: 'Az ehhez hasonló íjakat régebben képzett bérgyilkosok használták, akiknek a módosításai megnövelték az íj erejét. A bérgyilkosok szinte mindig feljavítják az íjukat a megfelelő teljesítményért.',
       ru: 'Такие луки раньше принадлежали ассасинам , которые вносили изменения для резкого повышения эффективности. Ассасины регулярно модифицируют свое оружие для достижения оптимальных результатов.',
       de: 'Bögen wie dieser befanden sich im Besitz von Assasinen, die Modifikationen vornahmen, um die Effizienz zu erhöhen. Assassinen modifizieren oft ihre Waffen, um optimale Leistung zu erlangen.',
-      es: 'Arcos como este fueron previamente poseidos por asesinos que hicieron modificaciones para aumentar su eficiencia drásticamente. Los asesinos regularmente modificaban sus armas para obtener un rendimiento óptimo.'
+      es: 'Arcos como este fueron previamente poseidos por asesinos que hicieron modificaciones para aumentar su eficiencia drásticamente. Los asesinos regularmente modificaban sus armas para obtener un rendimiento óptimo.',
+      vi: 'Những cung như thế này trước đây thuộc sở hữu của những sát thủ đã thực hiện các sửa đổi để tăng mạnh hiệu quả. Sát thủ thường xuyên sửa đổi vũ khí của họ để đạt được hiệu suất tối ưu.'
     }
   },
 
@@ -127,7 +138,8 @@ export default [
       hu: 'Ezeket a hatalmas íjakat ördögi jószágok pokoltüze formálta. Igen ritkák ezek az íjak, lévén az ár túlságosan hatalmas, hogy nyélbe üssék ezeket a megátalkodott, ördögi üzleteket.',
       ru: 'Такие луки были сделаны из адского пламени, которое создают дьявольские существа. Луки, как этот, встречается редко из-за конечной цены, необходимой для таких злонамеренных сделок и приобретений.',
       de: 'Solche Bögen wurden aus Höllenfeuer gemacht, das von teuflischen Kreaturen gespeist wurde. Diese Bögen sind da der Höchstpreis für solche bösen Transaktionen und Übernahmen erforderlich ist.',
-      es: 'Arcos como este fueron hechos en los fuegos del infierno provisto por criaturas diabólicas. Este tipo de arcos son raros debido al precio extremo requerido para completar su adquisición.'
+      es: 'Arcos como este fueron hechos en los fuegos del infierno provisto por criaturas diabólicas. Este tipo de arcos son raros debido al precio extremo requerido para completar su adquisición.',
+      vi: 'Những chiếc cung như thế này được tạo ra từ lửa địa ngục được cung cấp bởi các sinh vật quỷ dữ. Những loại cung này rất hiếm vì cái giá cuối cùng phải trả cho nó rất cao.'
     }
   },
 
@@ -138,7 +150,8 @@ export default [
       hu: 'Ez az íj a Főnix lángjaiban fürdött, és az éjszakai ég hűtötte le. Azok az íjak, amiket így készítenek, gyakran vannak a Főnix lángjának sajátosságaival is elbűvölve.',
       ru: 'Этот лук раскален в пламени феникса и охлажден в ночном небе. Созданные таким образом луки иногда наделяются дополнительными магическими свойствами от огня фениксов.',
       de: 'Dieser Bogen wurde im Feuer eines Phönix gebadet und im Nachthimmel gekühlt. Bögen die so hergestellt wurden besitzen manchmal magische Eigenschaften des Phönixfeuers.',
-      es: 'Este arco fue bañado en las llamas de un fénix y enfriado en el cielo nocturno. Arcos creados de esta manera a veces son imbuidos con características mágicas extra fruto de las llamas del fénix.'
+      es: 'Este arco fue bañado en las llamas de un fénix y enfriado en el cielo nocturno. Arcos creados de esta manera a veces son imbuidos con características mágicas extra fruto de las llamas del fénix.',
+      vi: 'Cây cung này được tắm trong ngọn lửa của một con phượng hoàng và được làm nguội trên bầu trời đêm. Cung được tạo ra theo cách này đôi khi được thấm nhuần thêm các đặc tính ma thuật từ ngọn lửa phượng hoàng.'
     }
   },
 
@@ -149,7 +162,8 @@ export default [
       hu: 'Az Özvegyítők élő íjak, amik egybefonódnak a használóval. A legerősebb szörnyetegek egy misztikus folyamattal készítik őket; az eredmény olyan remek lehet, hogy a használó soha többé nem vet szemet más anarnmentumra, mert a harci tapaszlatok éteri magasságba emelték gyilkoló művészetét.',
       ru: 'Widowmakers - живые луки, которые привязаны к обладателю. Мощные монстры используют загадочный процесс создания этих луков; полученное оружие имеет удивительную ревность и побуждает обладателя не применять никакое другое оружие, уничтожая противников с огромным боевым опытом.',
       de: 'Widowmakers sind lebendige Bögen, die einen Bund mit ihrem Besitzer eingehen. Mächtige Monster stellen diese Bögen in einem mystischen Prozess her.  Die resultierende Waffe kann bemerkenswert eifersüchtig sein und den Benutzer dazu verleiten, keine andere Waffe zu benutzen, indem er Feinde mit großer Kampferfahrung zerstört.',
-      es: 'Los Widowmakers son arcos vivientes que crean un vínculo con el usuario. Monstruos poderosos usan un proceso místico para crear Widowmakers; el arma que esto da como resultado es notablemente celosa y persuade al usuario para no usar ninguna otra arma a través de la destrucción de enemigos con amplia experiencia en batalla.'
+      es: 'Los Widowmakers son arcos vivientes que crean un vínculo con el usuario. Monstruos poderosos usan un proceso místico para crear Widowmakers; el arma que esto da como resultado es notablemente celosa y persuade al usuario para no usar ninguna otra arma a través de la destrucción de enemigos con amplia experiencia en batalla.',
+      vi: 'Widowmakers đang trú ngụ trong cung gắn kết với người dùng. Quái vật mạnh mẽ sử dụng một quá trình thần bí để tạo ra những Widowmakers ; vũ khí thành phẩm có thể đáng ghen tị và lôi kéo người dùng không sử dụng bất kỳ vũ khí nào khác bằng cách tiêu diệt kẻ thù với kinh nghiệm chiến đấu sâu rộng'
     }
   },
 
@@ -160,7 +174,8 @@ export default [
       hu: 'Efféle íjak ritkák a készítési folyamat hossza miatt; az íjakat a magasba kell emelni, hogy a villámok ereje kovácsolja egybe őket- csak így válhatnak valódi Viharvésszé. A folyamat pedig évekig eltarthat.',
       ru: 'Эти луки редки из-за длинного процесса ковки; луки должны быть сильно приподняты, чтобы гарантировать, что каждый удар молнии может быть использован для постепенной ковки этого лука в Stormsong. Процесс создания может занять годы.',
       de: 'Diese Bögen sind aufgrund der Methodik und Länge des Schmiedeprozesses selten: Die Bögen müssen in großer Höhe hergestellt werden, damit jeder Blitz genutzt werden kann, um sie zu schmieden. Dies kann Ewigkeiten dauern.',
-      es: 'Estos arcos son raros debido a su longitud y al método de forjado. Los arcos deben ser enormemente elevados para asegurar que cada rayo pueda ser utilizado para ir forjando este arco gradualmente hasta llegar a ser un Stormsong. Este proceso de creación puede tomar siglos.'
+      es: 'Estos arcos son raros debido a su longitud y al método de forjado. Los arcos deben ser enormemente elevados para asegurar que cada rayo pueda ser utilizado para ir forjando este arco gradualmente hasta llegar a ser un Stormsong. Este proceso de creación puede tomar siglos.',
+      vi: 'Những cung này rất hiếm do độ dài và phương pháp của quá trình rèn; cung phải được nâng lên rất nhiều để đảm bảo rằng mọi tia sét đều có thể được sử dụng để dần dần rèn cung này thành Stormsong. Quá trình tạo ra nó có thể mất nhiều thời gian'
     }
   },
 
@@ -171,7 +186,8 @@ export default [
       hu: 'Ez a rémületes íj felzabálja a viselő álmait és rémálmait. Az élő íj nem tanusít hűséget, és áldozatot követel, mégpedig halott ellenségek formájában.',
       ru: 'Этот злобный лук съедает сны и кошмары владелеца. Этот живой лук не предлагает верности и требует жертв в виде солдат противоборствующих армий.',
       de: 'Dieser bösartige Bogen frisst die Träume und Alpträume seines Nutzers. Dieser lebendige Bogen ist illoyal und fordert Opfer in Form von Kampfgegnern.',
-      es: 'Este arco malévolo se come los sueños y pesadillas de su portador. Este arco viviente ofrece ninguna lealtad y demanda sacrificios en forma de víctimas en batalla.'
+      es: 'Este arco malévolo se come los sueños y pesadillas de su portador. Este arco viviente ofrece ninguna lealtad y demanda sacrificios en forma de víctimas en batalla.',
+      vi: 'Cây cung độc ác này gặm nhấm những giấc mơ và ác mộng của người mang nó. Cây cung sống này không có lòng trung thành và đòi hỏi sự hy sinh dưới hình thức nạn nhân chiến đấu'
     }
   },
 
@@ -182,7 +198,8 @@ export default [
       hu: 'Amikor nagy tragédia zuhan egy világra, a dühödt áldozatok szellemei gyűlöletüket egy fegyverbe gyűjtik, hogy bosszút álljanak a világon. Ezt a fegyvert úgy ismerik: "Fúria".',
       ru: 'Когда целая цивилизация переживает великую трагедию, духи разгневанных жертв изливают свою злость в оружие, чтобы отомстить миру. Это оружие известно только как "Fury".',
       de: 'Wenn großes Unheil eine gesamte Zivilisation befällt, lassen die Geister der Opfer ihren Zorn in eine Waffe strömen, um Rache an der Welt zu üben. Dieses Waffe ist nur als "Fury" bekannt.',
-      es: 'Cuando una gran trajedia recae sobre una civilización entera, los espíritus de las victimas iracundas canalizan su furia en un arma para desatar la venganza en contra del mundo. Este arma es únicamente conocida como "Fury".'
+      es: 'Cuando una gran trajedia recae sobre una civilización entera, los espíritus de las victimas iracundas canalizan su furia en un arma para desatar la venganza en contra del mundo. Este arma es únicamente conocida como "Fury".',
+      vi: 'Khi một thảm kịch lớn xảy ra trong toàn bộ nền văn minh, những linh hồn của những nạn nhân tức giận đã trút cơn giận dữ của họ vào một vũ khí để báo thù thế giới. Vũ khí này chỉ được gọi là "Fury."'
     }
   }
 
