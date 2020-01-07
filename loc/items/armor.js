cea2b661303c833a351d8e6b3090cf226f47600c
trieu1994 <nguyenquangtrieu1994@gmail.com>
2020-01-07 08:07:57 +0700

    Add Vietnamese armor translation

--- loc/items/armor.js
+++ loc/items/armor.js
@@ -6,7 +6,8 @@ export default [
       hu: 'Ez a meglepően erős anyag képes tompítani a csapások erejét. A készítő képzetlensége egyértelmű, nem csoda hogy a legtöbb kalandor minél hamarabb le akarja cserélni a krumplizsákot.',
       ru: 'Это удивительно прочный материал, который можно использовать в качестве защитной одежды. Самодельная природа этого материала очевидна, так что многие приключеники предпочитают заменить картофельный мешок как можно скорее.',
       de: 'Dies ist ein überraschend robustes Material, das als Schutzkleidung verwendet werden kann. Die provisorische Natur dieses Materials ist offensichtlich, so dass viele Abenteurer beschließen, sich so schnell wie möglich für den Austausch des Kartoffelsackes zu entscheiden.',
-      es: 'Este es un material sorprendentemente resistente que puede ser usado como ropa de protección. La naturaleza improvisada de este material es obvia, muchos aventureros eligen remplazar el Potato Sack tan rápido como sea posible.'
+      es: 'Este es un material sorprendentemente resistente que puede ser usado como ropa de protección. La naturaleza improvisada de este material es obvia, muchos aventureros eligen remplazar el Potato Sack tan rápido como sea posible.',
+      vi: 'Đây là một vật liệu đầy sức mạnh đáng ngạc nhiên có thể được sử dụng như một giáp bảo vệ. Bản chất của vật liệu này là rõ ràng, vì vậy nhiều nhà thám hiểm chọn thay thế bao tải khoai tây càng nhanh càng tốt.'
     }
   },
 
@@ -17,7 +18,8 @@ export default [
       hu: 'Ez a garnaméta egy családi kincs, amely nemzedékről nemzedékre szállva kalandozott a világban.',
       ru: 'Это семейная реликвия, которая передавалась из поколения в поколение.',
       de: 'Dieses Gewand ist ein Familienerbstück, das von einer abenteuerlustigen Generation an die nächste weitergegeben wurde.',
-      es: 'Esta prenda es una reliquia familiar que ha sido heredada de una generación aventurera a otra.'
+      es: 'Esta prenda es una reliquia familiar que ha sido heredada de una generación aventurera a otra.',
+      vi: 'Bộ quần áo này là một vật gia truyền được truyền từ thế hệ này sang thế hệ khác.'
     }
   },
 
@@ -28,7 +30,8 @@ export default [
       hu: 'Ez a tunika vastag szálakból van elkészítve, hogy segítséget nyújtson a vadonban is a túléléshez. Egyesek az ilyen tunikát a klánja által reá méltónak tartott harcosoknak ajándékképp adják.',
       ru: 'Эта туника изготовлена из прочных тканей, которые разработаны, чтобы противостоять выживанию в дикой природе. Некоторые традиции включают в себя вручение туник в качестве подарка искателям приключений, которые организация считает достойными.',
       de: 'Diese Tunika ist aus robusten Stoffen gefertigt, die so konzipiert ist, dass sie dem Überleben in der Wildnis standhält. In manchen Traditionen werden Tuniken als Geschenke für Abenteurer angeboten, die von einer Organisation als würdig erachtet werden.',
-      es: 'Esta túnica está hecha a mano con telas robustas que fueron diseñadas para resistir la supervivencia en la naturaleza. Algunas tradiciones acostumbran a dar túnicas como regalos a aventureros que son considerados dignos por su organización.'
+      es: 'Esta túnica está hecha a mano con telas robustas que fueron diseñadas para resistir la supervivencia en la naturaleza. Algunas tradiciones acostumbran a dar túnicas como regalos a aventureros que son considerados dignos por su organización.',
+      vi: 'Áo này được chế tạo từ các loại vải mạnh mẽ được thiết kế để có thể sống sót ngoài tự nhiên. Một số giáo hội coi việc tặng chiếc áo này cho các nhà thám hiểm là một nghi thức truyền thống.'
     }
   },
 
@@ -39,7 +42,8 @@ export default [
       hu: 'Ez a bőrvért a nagy utazók legnagyobb kedveltje. A használatuk akkor terjedt el, amikor a Yggdrasil körüli esőerdőkbe veszedelmes bestiák áramlottak be.',
       ru: 'Эта коженая броня является общим выбором для путешественников. Тенденция ношения козлиной кожи возникла вокруг Yggdrasil, когда в близлежащих джунглях начали таиться опасные существа.',
       de: 'Diese Lederrüstung ist eine beliebte Wahl für reisende Abenteurer. Der Trend zum Tragen von Leder-Rucksäcken entstand um Yggdrasil herum, als gefährliche Kreaturen im nahen Dschungel zu lauerten.',
-      es: 'Esta armadura de cuero es una opción común para los aventureros que viajan. La tendencia de usar estos Leather Jerkins se originó alrededor de Yggdrasil cuando peligrosas criaturas comenzaron a acechar en las selvas cercanas.'
+      es: 'Esta armadura de cuero es una opción común para los aventureros que viajan. La tendencia de usar estos Leather Jerkins se originó alrededor de Yggdrasil cuando peligrosas criaturas comenzaron a acechar en las selvas cercanas.',
+      vi: 'Áo giáp da này là một lựa chọn phổ biến cho các nhà thám hiểm du hành. Xu hướng mặc đồ da có nguồn gốc từ Yggdrasil khi những sinh vật nguy hiểm bắt đầu ẩn nấp trong những khu rừng gần đó.'
     }
   },
 
@@ -50,7 +54,8 @@ export default [
       hu: 'Ezt a páncélt pikkelyek alkotják, amik vérmes szörnyetegekről estek a földre. A pikkelyeket gyakran gyűjtik páncélba, fémszegénység idején.',
       ru: 'Эта броня сделана из чешуек монстров, которые были найдены на земле. Чешуя монстров часто изготавливается в респектабельные доспехи, когда не хватает металла.',
       de: 'Diese Rüstung besteht aus Monsterschuppen, die auf dem Boden gefunden wurden. Monsterschuppen werden oft zu respektable Rüstungen gefertig, wenn Metall knapp ist.',
-      es: 'Esta armadura está hecha de escamas de monstruos que se encontraron en el suelo. Las escamas de montruo amenudo se forjaban y transformaban en armaduras respetables cuando el metal es escaso.'
+      es: 'Esta armadura está hecha de escamas de monstruos que se encontraron en el suelo. Las escamas de montruo amenudo se forjaban y transformaban en armaduras respetables cuando el metal es escaso.',
+      vi: 'Bộ giáp này được làm từ vảy quái vật được tìm thấy trên mặt đất. Vảy quái vật thường được chế tạo thành áo giáp mỗi khi thiếu kim loại.'
     }
   },
 
@@ -61,7 +66,8 @@ export default [
       hu: 'A könnyű Elf fémek áldott láncingek kovácsolására kiválóan alkalmasak. Egyesek szerint az első áldott ingek eredetileg szentek eldobott páncélszilánkjaiból álltak.',
       ru: 'Легкие эльфийские металлы используются для создания божественных кольчуг. Старые легенды гласят, что первые небесные кольчуги изначально создавались из частей брони, которые когда-то носили божественные создания.',
       de: 'Leichte elfische Metalle werden verwendet, um göttliche Kettenhemden herzustellen. Alte Legenden sagen, dass die ersten Sky Hemds ursprünglich aus weggeworfenen Rüstungsteilen entstanden sind, die früher von göttlichen Kreaturen getragen wurden.',
-      es: 'Los metales ligeros élficos se usan para crear cotas de malla divinas. Las viejas leyendas dicen que las primeras Sky Mails fueron creados a partir de piezas de armadura desechadas que alguna vez fueron usadas por criaturas divinas.'
+      es: 'Los metales ligeros élficos se usan para crear cotas de malla divinas. Las viejas leyendas dicen que las primeras Sky Mails fueron creados a partir de piezas de armadura desechadas que alguna vez fueron usadas por criaturas divinas.',
+      vi: 'Kim loại Elvish nhẹ được sử dụng để tạo ra áo giáp lưới thần thánh. Truyền thuyết xưa nói rằng những giáp lưới đầu tiên được tạo ra từ những mảnh áo giáp bị vứt bỏ từng được mặc bởi các sinh vật thần thánh.'
     }
   },
 
@@ -72,7 +78,8 @@ export default [
       hu: 'Ez a köpeny kis árnyakat varázsol elő, amik elfedik a viselőjét, hogy megvédjék a csatákban. Ilyen köpönyegek különös árnyékmágiával készülnek, azután pedig titkos alkímiával bűvölik el őket.',
       ru: 'Этот плащ вызывает небольшие тени, которые окружают хозяйна во время боя, чтобы усилить защиту. Такие плащи создаются с помощью странной теневой магии, а затем тайно обогащаются алхимическим путем.',
       de: 'Dieser Umhang beschwört kleine Schatten, welche den Träger im Kampf umgeben, um den Schutz zu erhöhen. Solche Mäntel werden mit sonderbarer Schattenmagie gefertigt und anschließend auf geheimnisvolle Weise alchemistisch aufgewertet.',
-      es: 'Esta capa invoca pequeñas sombras que envuelven al portador durante batalla para mejorar la protección. Las capas como estas son hechas a mano con una extraña sombra mágica y luego se la mejora con alquimia de forma secreta.'
+      es: 'Esta capa invoca pequeñas sombras que envuelven al portador durante batalla para mejorar la protección. Las capas como estas son hechas a mano con una extraña sombra mágica y luego se la mejora con alquimia de forma secreta.',
+      vi: 'Chiếc áo choàng này triệu hồi những bóng ma xung quanh người mặc trong trận chiến để tăng cường bảo vệ. Những chiếc áo choàng như thế này được chế tạo bằng ma thuật bóng tối kỳ lạ và sau đó được tăng cường hóa học theo những cách bí mật'
     }
   },
 
@@ -83,7 +90,8 @@ export default [
       hu: 'Rúnaköveket vertek a páncélba megnövelt védelemért. A varázslótanoncok úgy hiszik, minden rúnakő a vérten egymással egyesülhet egy különös és veszedelmes, fel nem fedezett vérszerződés során.',
       ru: 'Волшебные руны были выгравированы на этой броне, чтобы обеспечить дополнительную защиту. Некоторые ученые полагают, что руны на всех доспехах предназначены для взаимодействия в более крупных, нераскрытых ритуалах.',
       de: 'In diese Rüstung wurden magische Runen geätzt, um zusätzlichen Schutz zu bieten. Einige Gelehrte glauben, dass die Runen auf allen Runen-Halbplattenrüstungen dazu gedacht sind in einem größeren, unentdeckten Ritual, zusammenzuwirken.',
-      es: 'Runas mágicas fueron grabadas en esta armadura para proporcionar protección adicional. Algunos estudiosos creen que las runas en este tipo de armaduras rúnicas están destinadas a interactuar juntas en algún ritual más grande y aún por descubrir.'
+      es: 'Runas mágicas fueron grabadas en esta armadura para proporcionar protección adicional. Algunos estudiosos creen que las runas en este tipo de armaduras rúnicas están destinadas a interactuar juntas en algún ritual más grande y aún por descubrir.',
+      vi: 'Cổ ngữ ma thuật đã được khắc vào bộ giáp này để cung cấp thêm sự bảo vệ. Một số học giả tin rằng các cổ ngữ trên tất cả các áo giáp dùng để tương tác với nhau trong một số nghi lễ lớn hơn, chưa được khám phá.'
     }
   },
 
@@ -94,7 +102,8 @@ export default [
       hu: 'Természetfeletti pokoltűz koholta ezt a vértet. Az ördögi szörnyetegek alkuját pedig csak hatalmas emberáldozatokkal lehetett teljesíteni, hogy néhány kovács megtanulhasson egy ilyet elkészíteni.',
       ru: 'При изготовлении этих доспехов использовались сверхъестественное пламя. Крупные жертвы были принесены, для того чтобы успокоить требования дьявольских существ, чтобы несколько кузнецов могли ковать с этим странным огнем.',
       de: 'Übernatürliche Infernos wurden bei der Herstellung dieser Rüstung verwendet. Große Opfer wurden erbracht, um die Forderungen der teuflischen Kreaturen zu erfüllen, so dass einige wenige Schmiede mit den seltsamen Flammen schmieden konnten.',
-      es: 'Se usaron llamas sobrenaturales en la fabricación de esta armadura. Se hicieron grandes sacrificios para apaciguar las demandas de las criaturas diabólicas de forma que así, algunos herreros pudieran forjar a través de estas extrañas llamas.'
+      es: 'Se usaron llamas sobrenaturales en la fabricación de esta armadura. Se hicieron grandes sacrificios para apaciguar las demandas de las criaturas diabólicas de forma que así, algunos herreros pudieran forjar a través de estas extrañas llamas.',
+      vi: 'Một ngọn lửa siêu nhiên đã được sử dụng trong việc chế tạo áo giáp này. Những sự hiến tế to lớn đã được thực hiện để xoa dịu những yêu cầu của các sinh vật quỷ dữ để một vài thợ rèn có thể rèn được ngọn lửa kỳ lạ.'
     }
   },
 
@@ -106,7 +115,8 @@ export default [
       hu: 'Az Ősi Legendák szerint a Lélekőrző páncél magához vonzza a lelket, amely feláldozta magát, hogy megnövelje a vért erejét. Nehéz időkben sokan ezt az áldozatot hozták, hogy biztosítsák klánjuk, törzsük, vagy családjuk túlélését.',
       ru: 'Древние легенды гласят, что в каждом доспехе хранится душа, которая добровольно пожертвовала собой, чтобы усилить защитную силу доспеха. В более трудные времена люди идут на большие жертвы, чтобы обеспечить выживание своих кланов и семей.',
       de: 'Alte Legenden sagen, dass jede Soulkeeper-Rüstung eine Seele beherbergt, die sich bereitwillig geopfert hat, um die Schutzfunktion der Rüstung zu verstärken. In schwierigeren Zeiten brachten Einzelne große Opfer dar, um das Überleben ihrer Stämme und Familien zu sichern.',
-      es: 'Las leyendas antiguas dicen que cada armadura de Soulkeeper alberga un alma que se sacrificó voluntariamente para mejorar el poder protector de la armadura. En tiempos difíciles, los individuos hicieron grandes sacrificios para asegurar la supervivencia de sus clanes y familias.'
+      es: 'Las leyendas antiguas dicen que cada armadura de Soulkeeper alberga un alma que se sacrificó voluntariamente para mejorar el poder protector de la armadura. En tiempos difíciles, los individuos hicieron grandes sacrificios para asegurar la supervivencia de sus clanes y familias.',
+      vi: 'Truyền thuyết cổ xưa nói rằng mỗi bộ giáp Soulkeeper chứa một linh hồn sẵn sàng hy sinh bản thân để tăng cường sức mạnh bảo vệ của bộ giáp. Trong thời kỳ gian khó, các thành viên đã hy sinh rất nhiều để đảm bảo sự sống còn của gia tộc và gia đình của họ.'
     }
   },
 
@@ -118,7 +128,8 @@ export default [
       hu: 'Ezek a vértek ősi császárok készíttették kovácsaikkal, hogy megvédjék magukat a halandó sebektől a Nagy Háború során. A legenda szerint, azok az uralkodók nem harcban estek el; az ágyukban ölték meg őket bérgyilkosok, akik lehúzták róluk előtte a vértet. A kovácsolási módszer ismeretlen, de annyi bizonyos, hogy óriási vagyon volt szügséges hozzá.',
       ru: 'Такие доспехи были созданы первыми царями и вождями, чтобы предотвратить смертельные ранения во время "The First Great War". По преданию легенды, эти цари и вожди не погибли в бою, а были убиты во сне, и их доспехи были украдены. Методы ковки были потеряны со временем, но для этого процесса явно требовались огромные силы и жертвы.',
       de: 'Die Deathless-Rüstungen wurden von den ersten Königen und Anführern geschaffen, um tödliche Wunden während des ersten großen Krieg zu heilen. Der Legende nach starben diese Könige und Anführer nicht im Kampf, sondern wurden jeweils im Schlaf ermordet und ihrer Rüstungen bestohlen. Die Schmiedetechniken gingen mit der Zeit verloren, aber ein riesiges Vermögen war für den Herstellungsprozess offensichtlich notwendig.',
-      es: 'Las Deathless fueron creadas por los primeros reyes y líderes como un medio para prevenir heridas mortales durante la Primera Gran Guerra. Fiel a la leyenda, esos reyes y líderes no perecieron en la batalla; en cambio, fueron asesinados mientras dormían y sus armaduras fueron robadas. Las técnicas de forja se perdieron con el tiempo, pero fortunas masivas fueron claramente necesarias para el proceso.'
+      es: 'Las Deathless fueron creadas por los primeros reyes y líderes como un medio para prevenir heridas mortales durante la Primera Gran Guerra. Fiel a la leyenda, esos reyes y líderes no perecieron en la batalla; en cambio, fueron asesinados mientras dormían y sus armaduras fueron robadas. Las técnicas de forja se perdieron con el tiempo, pero fortunas masivas fueron claramente necesarias para el proceso.',
+      vi: 'Áo giáp Deathless được tạo ra bởi các vị vua và nhà lãnh đạo đầu tiên như một phương tiện để ngăn chặn vết thương chí mạng trong Đại chiến thứ nhất. Đúng như truyền thuyết, những vị vua và nhà lãnh đạo đã không bị diệt vong trong trận chiến; thay vào đó, họ từng bị ám sát khi ngủ và áo giáp của họ đã bị đánh cắp. Các kỹ thuật rèn đã bị mất theo thời gian, nhưng vận may khổng lồ rõ ràng là cần thiết cho quá trình này.'
     }
   }
 
