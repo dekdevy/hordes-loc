ed617d4384496b0d6bd6a297accf17809f9ae1f3
trieu1994 <nguyenquangtrieu1994@gmail.com>
2020-01-07 10:39:57 +0700

    Add Vietnamese ring translation

--- loc/items/ring.js
+++ loc/items/ring.js
@@ -4,7 +4,8 @@ export default [
     description: {
       en: 'This strange band allows a small part of the wearer’s life essence to be stored.',
       ru: 'Эта странная повязка позволяет хранить небольшую часть жизни владельца.',
-      de: 'Dieses seltsame Band erlaubt es dem Träger einen kleinen Teil der Lebensessenz zu speichern.'
+      de: 'Dieses seltsame Band erlaubt es dem Träger einen kleinen Teil der Lebensessenz zu speichern.',
+      vi: 'Nhẫn kỳ lạ này cho phép một phần nhỏ tinh chất sự sống của người mang được lưu trữ.'
     }
   },
 
@@ -13,7 +14,8 @@ export default [
     description: {
       en: 'This ring is made from a special wood that is as tough as iron. Ironbark is often used as an alternative to metal.',
       ru: 'Это кольцо изготовлено из специальной древесины, прочной как железо. Железная кора часто используется в качестве альтернативы металлу.',
-      de: 'Dieser Ring wurde aus Holz, stark wie Eisen, hergestellt. Eisenholz wird oft als eine Alternative zu Metall genutzt.'
+      de: 'Dieser Ring wurde aus Holz, stark wie Eisen, hergestellt. Eisenholz wird oft als eine Alternative zu Metall genutzt.',
+      vi: 'Chiếc nhẫn này được làm từ một loại gỗ đặc biệt cứng như sắt. Ironbark thường được sử dụng thay thế cho kim loại'
     }
   },
 
@@ -22,7 +24,8 @@ export default [
     description: {
       en: 'Two kingdoms that were joined by royal marriage had their armies wear these rings as a symbol of the newly allied nations.',
       ru: 'Два королевства, которые присоединились королевским бракосочетаниям, носили эти кольца в качестве символа новых союзнических народов.',
-      de: 'Zwei Königreiche, vereint durch Heirat, ließen ihre Armeen diese Ringe als Symbol für die Einheit der verbündeten Nationen tragen'
+      de: 'Zwei Königreiche, vereint durch Heirat, ließen ihre Armeen diese Ringe als Symbol für die Einheit der verbündeten Nationen tragen',
+      vi: 'Hai vương quốc được liên kết bởi hôn nhân chính trị có quân đội của họ đeo những chiếc nhẫn này như một biểu tượng của các quốc gia mới được liên minh.'
     }
   },
 
@@ -31,7 +34,8 @@ export default [
     description: {
       en: 'This ring is made from the bones of a monster husk. Monster bone can be used as a sturdy material when no other resources are available.',
       ru: 'Это кольцо изготовлено из костей монстров шелухи. Кость монстра может быть использована как прочный материал, когда нет других ресурсов.',
-      de: 'Dieser Ring wurde aus den Knochen eines riesigen Husks gefertigt. Monsterknochen sind ein stabiles Material, wenn kein anderes verfügbar ist.' // Husk is not translatable
+      de: 'Dieser Ring wurde aus den Knochen eines riesigen Husks gefertigt. Monsterknochen sind ein stabiles Material, wenn kein anderes verfügbar ist.', // Husk is not translatable
+      vi: 'Chiếc nhẫn này được làm từ xương của một con quái vật. Xương quái vật có thể được sử dụng như một vật liệu chắc chắn khi không có tài nguyên nào khác.'
     }
   },
 
@@ -40,7 +44,8 @@ export default [
     description: {
       en: 'Rings are often handed down from generation to generation in the Elvish tradition.',
       ru: 'В эльфийской традиции кольца часто передаются из поколения в поколение.',
-      de: 'Ringe werden nach elfischer Tradition oft von Generation zu Generation weitergegeben.'
+      de: 'Ringe werden nach elfischer Tradition oft von Generation zu Generation weitergegeben.',
+      vi: 'Nhẫn thường được lưu truyền từ thế hệ này sang thế hệ khác trong truyền thống Elvish.'
     }
   },
 
@@ -49,7 +54,8 @@ export default [
     description: {
       en: 'This ring has tiny runes carved into the inner band. Some historians believe these rings were the last effort of a lost kingdom to fortify its population from disease.',
       ru: 'Крошечные руны вырезаны на внутренней части кольца. Некоторые историки считают, что эти кольца были последними попытками потерянного королевства укрепить здоровье своего населения от болезней.',
-      de: 'Dieser Ring besitzt auf der Innenseite winzige, geschnitzte Runen. Manche Historiker glauben das diese Ringe der letzte Versuch eines verloren Königreiches waren, seine Bevölkerung vor Krankheit zu schützen.'
+      de: 'Dieser Ring besitzt auf der Innenseite winzige, geschnitzte Runen. Manche Historiker glauben das diese Ringe der letzte Versuch eines verloren Königreiches waren, seine Bevölkerung vor Krankheit zu schützen.',
+      vi: 'Chiếc nhẫn này có những cỗ ngữ nhỏ được khắc vào dải bên trong. Một số nhà sử học tin rằng những chiếc nhẫn này là nỗ lực cuối cùng của một vương quốc đã mất để củng cố dân số khỏi bệnh tật.'
     }
   },
 
@@ -58,7 +64,8 @@ export default [
     description: {
       en: 'Arcane rings are the magical experiments of intermediate mages. Some rings are crafted and enchanted to substantially enhance magic-casting abilities.',
       ru: 'Arcane Ring- это магические эксперименты промежуточных магов. Некоторые кольца изготовлены и зачарованы, чтобы существенно повысить магические способности.',
-      de: 'Arkane Ringe sind die magischen Experimente von fortgeschrittenen Magiern. Einige Ringe wurden hergestellt, um die Magie-Fähigkeiten signifikant zu erhöhen.'
+      de: 'Arkane Ringe sind die magischen Experimente von fortgeschrittenen Magiern. Einige Ringe wurden hergestellt, um die Magie-Fähigkeiten signifikant zu erhöhen.',
+      vi: 'Nhẫn Arcane là thí nghiệm ma thuật của các pháp sư. Một số chiếc nhẫn được chế tác và phù phép để tăng cường đáng kể khả năng thi triển phép thuật'
     }
   },
 
@@ -67,7 +74,8 @@ export default [
     description: {
       en: 'These rings once belonged to the “Cult of the Emerald Spiders,” a gang of bandits that were among the first to ride spiders.',
       ru: 'Эти кольца когда-то принадлежали банде "Культ изумрудных пауков", которые одни из первых ездили на пауках.',
-      de: 'Diese Ringe gehörten einst dem "Kult der Smaragdspinnen", einer Gruppe von Banditen, die als eine der ersten auf Spinnen ritten.'
+      de: 'Diese Ringe gehörten einst dem "Kult der Smaragdspinnen", einer Gruppe von Banditen, die als eine der ersten auf Spinnen ritten.',
+      vi: 'Những chiếc nhẫn này từng thuộc về“Cult of the Emerald Spiders”, một nhóm cướp đầu tiên cưỡi nhện.'
     }
   },
 
@@ -76,7 +84,8 @@ export default [
     description: {
       en: 'Infernal rings are gifts from devilish creatures. They entice the wearer to desire more power. This desire is often followed by ill-advised offers by more devils.',
       ru: 'Infernal Ring - это подарок от дьявольских существ. Они соблазняют владельца желать больше силы. За этим желанием часто следуют неразумные предложения от большого количества дьяволов.',
-      de: 'Höllenringe sind Geschenke von teuflischen Kreaturen. Sie verleiten den Träger, sich mehr Macht zu wünschen. Diesem Wunsch folgen oft Angebote von mehr Teufeln.'
+      de: 'Höllenringe sind Geschenke von teuflischen Kreaturen. Sie verleiten den Träger, sich mehr Macht zu wünschen. Diesem Wunsch folgen oft Angebote von mehr Teufeln.',
+      vi: 'Infernal rings là quà tặng từ các sinh vật quỷ dữ. Họ lôi kéo người mang nó mong muốn quyền lực hơn. Mong muốn này thường được theo sau bởi những lời đề nghị thiếu sáng suốt của nhiều ác quỷ.'
     }
   },
 
@@ -85,7 +94,8 @@ export default [
     description: {
       en: 'Nature spirits originally gave these rings as gifts of appreciation to primal monsters. Many of these rings have been stolen or lost over thousands of years.',
       ru: 'Природные духи изначально дарили эти кольца в знак благодарности первобытным монстрам. Многие из этих колец были украдены или потеряны за тысячи лет.',
-      de: 'Naturgeister gaben diese Ringe als Anerkennungsgeschenk an Ur-Monster. Über tausende von Jahren gingen viele dieser Ringe verloren oder wurden gestohlen.'
+      de: 'Naturgeister gaben diese Ringe als Anerkennungsgeschenk an Ur-Monster. Über tausende von Jahren gingen viele dieser Ringe verloren oder wurden gestohlen.',
+      vi: 'Nature spirits ban đầu đã tặng những chiếc nhẫn này như một món quà đánh giá cao những con quái vật nguyên thủy. Nhiều chiếc nhẫn trong số này đã bị đánh cắp hoặc mất trong hàng ngàn năm.'
     }
   },
 
@@ -94,7 +104,8 @@ export default [
     description: {
       en: 'Many religious factions claim that individuals wearing this ring have the favor of a deity; however, the specific deity is not known. Many religious figureheads believe that the divine anonymity is intentional.',
       ru: 'Многие религиозные фракции утверждают, что люди, носящие это кольцо, имеют благосклонность к божеству; однако конкретное божество не известно. Многие религиозные деятели считают, что божественная анонимность носит преднамеренный характер.',
-      de: 'Viele religiöse Gruppen behaupten das Personen, die diesen Ring tragen den Segen eines Gottes erhalten. Welcher Gott ist jedoch unbekannt. Viele religiöse Figuren glauben, das diese Anonymität Absicht ist.'
+      de: 'Viele religiöse Gruppen behaupten das Personen, die diesen Ring tragen den Segen eines Gottes erhalten. Welcher Gott ist jedoch unbekannt. Viele religiöse Figuren glauben, das diese Anonymität Absicht ist.',
+      vi: 'Nhiều phe phái tôn giáo cho rằng các cá nhân đeo chiếc nhẫn này có sự ưu ái của một vị thần; tuy nhiên, vị thần cụ thể không được biết đến. Nhiều nhân vật tôn giáo tin rằng sự ẩn danh của Thiên Chúa là có chủ ý'
     }
   },
 
@@ -103,7 +114,8 @@ export default [
     description: {
       en: 'Ancient scrolls claim that these legendary rings make the wearer nearly invincible. The scrolls also mention stories of wearers becoming corrupt and profoundly unhappy.',
       ru: 'Древние свитки утверждают, что эти легендарные кольца делают владельца почти непобедимым. В свитках также упоминаются истории о том, как носители становятся сломлены и глубоко несчастными.',
-      de: 'Uralte Schriften behaupten das diese legendären Ringe den Träger fast unverwundbar machen. Die Schriften also berichten von Trägern die korrumpiert und unglücklich wurden.'
+      de: 'Uralte Schriften behaupten das diese legendären Ringe den Träger fast unverwundbar machen. Die Schriften also berichten von Trägern die korrumpiert und unglücklich wurden.',
+      vi: 'Các cuộn giấy cổ xưa cho rằng những chiếc nhẫn huyền thoại này khiến người đeo gần như bất khả chiến bại. Các cuộn sách cũng đề cập đến những câu chuyện về người mặc trở nên tha hóa và vô cùng bất hạnh.'
     }
   },
 
@@ -112,7 +124,8 @@ export default [
     description: {
       en: 'Once every age, a Peacekeeper is found and marks the wearer as a special protector of the realms. In a past age, the bearer of the Peacekeeper failed and the Third Great War began.',
       ru: 'Один раз за век, находят Peacekeeper и отмечают его носителя как особого защитника царств. В прошлом веке носительница Peacekeeper потерпела неудачу, и началась Third Great War.',
-      de: 'Einmal in jedem Zeitalter wird ein Peacekeeper gefunden und markiert den Träger als besonderen Beschützer der Reiche. Im letzten Zeitalter scheiterte der Ringträger und der große dritte Krieg begann.'
+      de: 'Einmal in jedem Zeitalter wird ein Peacekeeper gefunden und markiert den Träger als besonderen Beschützer der Reiche. Im letzten Zeitalter scheiterte der Ringträger und der große dritte Krieg begann.',
+      vi: 'Mỗi thời đại, một Peacekeeper  được tìm thấy và đánh dấu người mặc là người bảo vệ đặc biệt của các cõi. Trong một thời đại đã qua, Peacekeeper đã thất bại và Đại chiến thứ ba bắt đầu.'
     }
   }
 
