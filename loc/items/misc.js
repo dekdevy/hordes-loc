162d26e7cf02470d1d51c9a0a906acb249467120
sanchez matias <matias-leandro@outlook.com>
2020-01-10 10:23:31 -0300

    Spanish: added misc's descriptions

--- loc/items/misc.js
+++ loc/items/misc.js
@@ -7,7 +7,8 @@ export default [
       ru: 'Фляжка, содержащее красную жидкость, которая лечит тебя, когда ты ее пьете.',
       hu: 'Ez az üveg vörös folyadékot tartalmaz, ami enyhíti a fájdalmad.',
       de: 'Ein kleiner Trank, der eine rote Flüssigkeit beinhaltet und dich heilt, während du ihn trinkst.',
-      vi: 'Một lọ thuốc chứa chất lỏng màu đỏ, chữa lành cho bạn khi bạn uống nó'
+      vi: 'Một lọ thuốc chứa chất lỏng màu đỏ, chữa lành cho bạn khi bạn uống nó',
+      es: 'Un frasco de poción que contiene un líquido rojo que te cura mientras lo bebes.'
     }
   },
   {
@@ -18,7 +19,8 @@ export default [
       ru: 'Фляжка, содержащее голубую жидкость, которая восстанавливает твою ману, когда ты ее пьешь.',
       hu: 'Ez az üveg azúr folyadékot tartalmaz, ami felfrissíti a manádat.',
       de: 'Ein kleiner Trank, der eine blaue Flüssigkeit beinhaltet und dein Mana wiederherstellt, während du ihn trinkst.',
-      vi: 'Một lọ thuốc chứa chất lỏng màu xanh, hồi mana khi bạn uống nó.'
+      vi: 'Một lọ thuốc chứa chất lỏng màu xanh, hồi mana khi bạn uống nó.',
+      es: 'Un frasco de poción que contiene un líquido azul, recupera maná mientras lo bebes.'
     }
   },
   {
@@ -29,7 +31,8 @@ export default [
       ru: 'Фляжка, содержащее красную жидкость, которая лечит тебя, когда ты ее пьете.',
       hu: 'Ez az üveg vörös folyadékot tartalmaz, ami enyhíti a fájdalmad.',
       de: 'Ein mittlerer Trank, der eine rote Flüssigkeit beinhaltet und dich heilt, während du ihn trinkst.',
-      vi: 'Một lọ thuốc chứa chất lỏng màu đỏ, chữa lành cho bạn khi bạn uống nó'
+      vi: 'Một lọ thuốc chứa chất lỏng màu đỏ, chữa lành cho bạn khi bạn uống nó',
+      es: 'Un frasco de poción que contiene un líquido rojo que te cura mientras lo bebes.'
     }
   },
   {
@@ -40,7 +43,8 @@ export default [
       ru: 'Фляжка, содержащее голубую жидкость, которая восстанавливает твою ману, когда ты ее пьешь.',
       hu: 'Ez az üveg azúr folyadékot tartalmaz, ami felfrissíti a manádat.',
       de: 'Ein mittlerer Trank, der eine blaue Flüssigkeit beinhaltet und dein Mana wiederherstellt, während du ihn trinkst.',
-      vi: 'Một lọ thuốc chứa chất lỏng màu xanh, hồi mana khi bạn uống nó.'
+      vi: 'Một lọ thuốc chứa chất lỏng màu xanh, hồi mana khi bạn uống nó.',
+      es: 'Un frasco de poción que contiene un líquido azul, recupera maná mientras lo bebes.'
     }
   },
   {
@@ -51,7 +55,8 @@ export default [
       ru: 'Фляжка, содержащее красную жидкость, которая лечит тебя, когда ты ее пьете.',
       hu: 'Ez az üveg vörös folyadékot tartalmaz, ami enyhíti a fájdalmad.',
       de: 'Ein großer Trank, der eine rote Flüssigkeit beinhaltet und dich heilt, während du ihn trinkst.',
-      vi: 'Một lọ thuốc chứa chất lỏng màu đỏ, chữa lành cho bạn khi bạn uống nó'
+      vi: 'Một lọ thuốc chứa chất lỏng màu đỏ, chữa lành cho bạn khi bạn uống nó',
+      es: 'Un frasco de poción que contiene un líquido rojo que te cura mientras lo bebes.'
     }
   },
   {
@@ -62,7 +67,8 @@ export default [
       ru: 'Фляжка, содержащее голубую жидкость, которая восстанавливает твою ману, когда ты ее пьешь.',
       hu: 'Ez az üveg azúr folyadékot tartalmaz, ami felfrissíti a manádat.',
       de: 'Ein großer Trank, der eine blaue Flüssigkeit beinhaltet und dein Mana wiederherstellt, während du ihn trinkst.',
-      vi: 'Một lọ thuốc chứa chất lỏng màu xanh, hồi mana khi bạn uống nó.'
+      vi: 'Một lọ thuốc chứa chất lỏng màu xanh, hồi mana khi bạn uống nó.',
+      es: 'Un frasco de poción que contiene un líquido azul, recupera maná mientras lo bebes.'
     }
   }
 ]
