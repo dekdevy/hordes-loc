a3954e5d8a46218625b82f3a6e1cdfa734bf7ce6
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-02-12 00:23:41 +0000

    Dutch armlet translations.

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -8,7 +8,8 @@ export default [
       fr: 'Ces bracelets sont des lanières de cuir qui sont enveloppées dans une formation simple.',
       es: 'Estas pulseras estan hechas de tiras de cuero que están envueltas en una formación simple.',
       de: 'Diese Armbänder sind Lederstreifen, die in einer einfachen Anordnung gewickelt sind.',
-      vi: 'Những vòng tay này là những miếng da được bọc thành một hình đơn giản'
+      vi: 'Những vòng tay này là những miếng da được bọc thành một hình đơn giản.'
+      nl: 'Dit armbanden zijn leren stroken die verwikkeld zijn in een simpele formatie.'
     }
   },
 
@@ -21,7 +22,8 @@ export default [
       fr: 'Ces brassards ont été créés à la va-vite. Les marques de combat rugueuses sur les côtés du cuir n\'inspirent pas confiance en la survie de l\'ancien propriétaire.',
       es: 'Estos brazaletes fueron creados precipitadamente. Las marcas de las brutales batallas a los lados del cuero no inspiran confianza sobre la supervivencia de su dueño anterior.',
       de: 'Diese Armbänder wurden in kürzester Zeit erstellt. Die rauen Schlachtmarkierungen an den Seiten des Leders wecken kein Vertrauen in das Überleben des Vorbesitzers.',
-      vi: 'Những chiếc băng tay này được tạo ra một cách vội vã. Các vết tích chiến đấu trên các mặt da không tạo sự tự tin về sự sống sót của chủ sở hữu trước đó'
+      vi: 'Những chiếc băng tay này được tạo ra một cách vội vã. Các vết tích chiến đấu trên các mặt da không tạo sự tự tin về sự sống sót của chủ sở hữu trước đó.'
+      nl: 'Deze armbanden waren haastig gecreëerd. De ruwe gevecht tekens op de zijkanten van het leer inspireren niet het zelfvertrouwen van de vorige eigenaars overleven.'
     }
   },
 
@@ -35,7 +37,8 @@ export default [
       fr: 'Le cuir robuste a été méticuleusement travaillé pour en faire des bracelets de qualité. Sur le côté du cuir se trouve la signature de l\'artisan : "Markay\'ak".',
       es: 'Cuero resistente que ha sido cuidadosamente transformado a mano en finos brazaletes. Al costado del cuero está la firma del artesano: "Markay\'ak".',
       de: 'Strapazierfähiges Leder wurde sorgfältig zu feinen Armbändern verarbeitet. Auf der Seite des Leders befindet sich die Handschrift des Handwerkers: "Markay\'ak".',
-      vi: 'Tấm da cứng đã được chế tạo thành các vòng tay tốt. Trên mặt da là chữ ký của người thợ: "Markay\'ak".'
+      vi: 'Tấm da cứng đã được chế tạo thành các vòng tay tốt. Trên mặt da là chữ ký của người thợ: "Markay\'ak".',
+      nl: 'Stevig leer is zorgvuldig vervaardigd tot fijne armbanden. Op de zijkanten van het leer staat het ambachtsman z’n handtekening: "Markay\'ak".'
     }
   },
 
@@ -48,7 +51,10 @@ export default [
       fr: 'Ce type de bracelet est orné d\'os de monstres épais. De nombreuses traditions superstitieuses prétendent que le porteur de ces bracelets s\'imprègne du pouvoir du monstre décédé ; cependant, d\'autres prétendent que la réutilisation des os empêche les monstres de ressusciter.',
       es: 'Este tipo de brazalete está adornado a mano con huesos de monstruo gruesos. Muchas tradiciones supersticiosas afirman que el portador de dichos brazaletes se impregnará con el poder del monstruo fallecido; Sin embargo, otros afirman que la reutilización de huesos impide que los monstruos resuciten.',
       de: 'Diese Art von Armband ist kunstvoll mit dicken Monsterknochen verziert. Viele abergläubische Traditionen behaupten, dass der Träger solcher Armbänder von der Macht des verstorbenen Monsters durchdrungen wird. Andere behaupten jedoch, dass die Wiederverwendung von Knochen das Wiederaufleben von Monstern verhindert.',
-      vi: 'Loại vòng tay này được chế tạo công phu với xương quái vật dày. Nhiều truyền thống mê tín cho rằng người đeo những chiếc vòng như vậy trở nên thấm đẫm sức mạnh của quái vật đã chết; tuy nhiên, những người khác cho rằng việc tái sử dụng xương sẽ ngăn quái vật hồi sinh.'
+      vi: 'Loại vòng tay này được chế tạo công phu với xương quái vật dày. Nhiều truyền thống mê tín cho rằng người đeo những chiếc vòng như vậy trở nên thấm đẫm sức mạnh của quái vật đã chết; tuy nhiên, những người khác cho rằng việc tái sử dụng xương sẽ ngăn quái vật hồi sinh.',
+      nl: 'Dit type armbanden is sierlijk vervaardigd met dikke monsterbotten. Velen bijgelovige tradities beweren dat de drager van zulke armbanden doorgedrongen worden met de krachten van het 
+overleden monster; maar, anderen beweren dat het hergebruiken van botten voorkomt dat 
+monsters weer oprakelen.'
     }
   },
 
@@ -61,7 +67,8 @@ export default [
       fr: 'Ce type d\'armure de fer a été très utilisé en temps de guerre et reste encrassé. Les marques de griffes sur le métal semblent être superficielles.',
       es: 'Este tipo de armadura de hierro ha sido muy utilizada en la guerra y permanece sucia. La marcas de garras en el metal parecen ser mayoritariamente superficiales.',
       de: 'Diese Art von Eisenrüstung wurde im Krieg häufig verwendet und ist bis heute schmutzig. Die Kratzer auf dem Metall scheinen meist oberflächlich zu sein.',
-      vi: 'Loại áo giáp sắt này đã được sử dụng rất nhiều trong chiến tranh và vẫn còn bẩn. Các dấu móng vuốt trên kim loại dường như chủ yếu là bề ngoài.'
+      vi: 'Loại áo giáp sắt này đã được sử dụng rất nhiều trong chiến tranh và vẫn còn bẩn. Các dấu móng vuốt trên kim loại dường như chủ yếu là bề ngoài.',
+      nl: 'Dit type van ijzeren harnassen werd hevig gebruikt tijdens oorlogen and blijft vuil. De klauwafdrukkenop het metaal lijkt grotendeels oppervlakkig.'
     }
   },
 
@@ -74,7 +81,8 @@ export default [
       fr: 'Ces brassards en acier sont marqués de petites runes qui leur confèrent une protection supplémentaire. Les brassard imprégnés sont quelque peu rares puisque la méthode de création des runes est un secret.',
       es: 'Estos brazales de acero están marcados con pequeñas runas que imbuyen protección adicional. Los brazales imbuidos son algo raros ya que el método de creación de las runas es un secreto.',
       de: 'Diese Stahlarmschienen sind mit kleinen Runen versehen, die zusätzlichen Schutz bieten. Eingebettete Armschienen sind etwas seltener, da die Erstellungsmethode der Runen ein Geheimnis ist.',
-      vi: 'Những bao tay thép này được đánh dấu bằng các cổ ngữ nhỏ có thêm khả năng bảo vệ. Bao tay được thấm nhuần là hơi hiếm vì phương pháp tạo ra cổ ngữ là một bí mật.'
+      vi: 'Những bao tay thép này được đánh dấu bằng các cổ ngữ nhỏ có thêm khả năng bảo vệ. Bao tay được thấm nhuần là hơi hiếm vì phương pháp tạo ra cổ ngữ là một bí mật.',
+      nl: 'Deze stalen armbanden zijn gemarkeerd met kleine runen die extra bescherming doordringen. Imbued bracers zijn nogal zeldzaam aangezien de creatie methode van de runen een geheim zijn.'
     }
   },
 
@@ -87,7 +95,9 @@ export default [
       fr: 'Ces menottes ont été forgées avec un feu spécial qui ne peut être créé que par magie. Autrefois, les élémentaires de feu n\'enseignaient qu\'à quelques forgerons spéciaux le secret de la forge des menottes de braise.',
       es: 'Estas esposas fueron forjadas con un fuego especial que solo pueden ser creados a través de la magia. En el pasado, los elementales de fuego solo enseñaban a unos pocos herreros especiales el secreto de forjar esposas de ascuas.',
       de: 'Diese Manschetten wurden mit einem speziellen Feuer geschmiedet, das nur durch Magie erzeugt werden kann. In der Vergangenheit lehrten Feuerelementarier nur wenige spezielle Schmiede das Geheimnis des Schmiedens von Glimmerfesseln.',
-      vi: 'Những chiếc còng này được rèn bằng một ngọn lửa đặc biệt chỉ có thể được tạo ra thông qua phép thuật. Trong quá khứ, các nguyên tố lửa chỉ dạy cho một vài thợ rèn đặc biệt bí mật để rèn còng này.'
+      vi: 'Những chiếc còng này được rèn bằng một ngọn lửa đặc biệt chỉ có thể được tạo ra thông qua phép thuật. Trong quá khứ, các nguyên tố lửa chỉ dạy cho một vài thợ rèn đặc biệt bí mật để rèn còng này.',
+      nl: 'Deze manchetten waren gesmeed met een speciaal vuur die alleen gecreëerd kunnen worden door magie. In het verleden, vuur elementalen leerden alleen een paar speciale hoefsmeden het geheim 
+van hoe je de ember cuffs moet smeden.'
     }
   },
 
@@ -100,7 +110,9 @@ export default [
       fr: 'Les bracelets en miroir sont fabriqués par des forgerons exceptionnels possédant les compétences nécessaires pour fabriquer des armures réfléchissantes pour réduire les dégâts. Cette qualité protectrice a inspiré de nombreux sorciers Elfiques à expérimenter souvent sur des bracelets en miroir.',
       es: 'Los brazaletes espejados están hechos por herreros excepcionales con las habilidades necesarias para hacer armadura reflectante que reduce el daño. Esta cualidad protectora inspiró a muchos magos élficos a experimentar a menudo en brazaletes espejados.',
       de: 'Gespiegelte Armbänder werden von ausgezeichneten Schmieden hergestellt, die über die notwendigen Fähigkeiten verfügen reflektierende Rüstungen zur Schadensreduzierung herzustellen. Diese schützende Eigenschaft inspirierte viele elfische Zauberer dazu, oft an verspiegelten Armbändern zu experimentieren.',
-      vi: 'Vòng tay phản chiếu được chế tạo bởi thợ rèn đặc biệt với các kỹ năng cần thiết để chế tạo áo giáp phản chiếu để giảm sát thương. Chất lượng bảo vệ này đã truyền cảm hứng cho nhiều pháp sư Elvish thường thử nghiệm trên các vòng tay được phản chiếu.'
+      vi: 'Vòng tay phản chiếu được chế tạo bởi thợ rèn đặc biệt với các kỹ năng cần thiết để chế tạo áo giáp phản chiếu để giảm sát thương. Chất lượng bảo vệ này đã truyền cảm hứng cho nhiều pháp sư Elvish thường thử nghiệm trên các vòng tay được phản chiếu.',
+      nl: 'Mirror armlets zijn gemaakt door buitengewone hoefsmeden met de vaardigheid noodzakelijk om reflecterende harnassen te maken voor schade vermindering. Deze beschermende kwaliteit 
+inspireerde vele elven tovenaars om vaak te experimenteren op mirror armlets.'
     }
   },
 
@@ -113,7 +125,9 @@ export default [
       fr: 'Les fragments de Golem sont minutieusement assemblés par une petite équipe de forgerons magiciens. Chaque paire de fragments de golem est fabriquée sur plusieurs centaines de jours à l\'aide de coques de golems de roche.',
       es: 'Los fragmentos de golem están cuidadosamente ensamblados por un pequeño equipo de herreros usuarios de magia. Cada par de fragmentos de golems son elaborados durante cientos de días usando la cáscara de los golems de roca.',
       de: 'Golem-Fragmente werden von einem kleinen Team von Magie-Schmieden sorgfältig zusammengestellt. Jedes Paar von Golem-Fragmenten wird über mehrere hundert Tage hinweg aus den Schalen von Felsengolems hergestellt.',
-      vi: 'Các mảnh của Người đá được lắp ráp một cách tỉ mỉ bởi một nhóm nhỏ thợ rèn sử dụng ma thuật. Mỗi cặp mảnh golem được tạo ra trong vài trăm ngày bằng cách sử dụng vỏ của người đá.'
+      vi: 'Các mảnh của Người đá được lắp ráp một cách tỉ mỉ bởi một nhóm nhỏ thợ rèn sử dụng ma thuật. Mỗi cặp mảnh golem được tạo ra trong vài trăm ngày bằng cách sử dụng vỏ của người đá.',
+      nl: 'Golem fragments zijn nauw in elkaar gezet door een kleine team van magie-gebruikende hoefsmeden. Elk paar golem fragments zijn gemaakt over honderden dagen met gebruik van de 
+omhulsels van rots golems.'
     }
   },
 
@@ -126,7 +140,9 @@ export default [
       fr: 'De tels brassard sont composés d\'un métal rare que l\'on trouve dans les montagnes. Les forgerons utilisent des marteaux enchanteurs pour forger les brassards sans chaleur afin de conserver les propriétés magiques du métal.',
       es: 'Los brazaletes como este se componen de un metal raro que se encuentra en las montañas. Los herreros usan martillos encantados para forjar los brazaletes sin calor y así retener las propiedades mágicas del metal.',
       de: 'Solche Armschienen bestehen aus einem seltenen Metall, das in den Bergen gefunden wird. Schmiede verwenden verzauberte Hämmer, mit denen sie die Armschienen ohne Hitze schmieden, um die magieverstärkenden Eigenschaften des Metalls zu erhalten.',
-      vi: 'Những bao tay như thế này bao gồm một kim loại quý hiếm được tìm thấy ở vùng núi. Thợ rèn sử dụng búa mê hoặc để rèn các bao tay mà không cần nhiệt để giữ lại các đặc tính tăng cường ma thuật của kim loại.'
+      vi: 'Những bao tay như thế này bao gồm một kim loại quý hiếm được tìm thấy ở vùng núi. Thợ rèn sử dụng búa mê hoặc để rèn các bao tay mà không cần nhiệt để giữ lại các đặc tính tăng cường ma thuật của kim loại.',
+      nl: 'Armbanden zoals deze zijn gemaakt uit zeldzame metalen gevonden in de bergen. Hoefsmeden gebruiken betoverde hamers om de armbanden te smeden zonder hitte zodat de magie 
+verbeteringen op het metaal blijft.'
     }
   },
 
@@ -139,7 +155,10 @@ export default [
       fr: 'Ces brassards ont été créés par des forgerons qui martelaient le métal trouvé dans les météorites noires. Jusqu\'à présent, la plus grande concentration de météorites noires a été trouvée après un cataclysme qui a suivi la Seconde Grande Guerre.',
       es: 'Estos brazaletes fueron creados por herreros que martillaron metales encontrados en meteoritos negros. Hasta ahora, la mayor concentración de meteoritos negros se encontró despues de un cataclismo que desencadenó en la Segunda Gran Guerra.',
       de: 'Diese Armschienen wurden von Schmieden hergestellt, die Metall aus schwarzen Meteoriten verwendeten. Die bisher größte Konzentration an schwarzen Meteoriten wurde in der Folgezeit einer Katastrophe nach dem Zweiten Weltkrieg gefunden.',
-      vi: 'Những chiếc bao tay này được tạo ra bởi những người thợ rèn chế tạo từ kim loại tìm thấy trong các thiên thạch đen. Cho đến nay, nơi tập trung lớn nhất của các thiên thạch đen đã được tìm thấy sau một trận đại hồng thủy sau Đại chiến thứ hai.'
+      vi: 'Những chiếc bao tay này được tạo ra bởi những người thợ rèn chế tạo từ kim loại tìm thấy trong các thiên thạch đen. Cho đến nay, nơi tập trung lớn nhất của các thiên thạch đen đã được tìm thấy sau một trận đại hồng thủy sau Đại chiến thứ hai.',
+      nl: 'Deze armbanden werden gemaakt door hoefsmeden die hebben gehamerd op metaal gevonden in 
+zwarte meteotieten. Tot nu toe, de grootste concentratie van zwarte meteoren waren gevonden na 
+een natuurramp die de tweede grootte oorlog volgde.'
     }
   },
 
