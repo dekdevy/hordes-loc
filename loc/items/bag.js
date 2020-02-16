b21a9ea04143a5989f1447b58bf05b7b4c9a71fe
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-02-16 11:04:49 +0000

    Dutch bag translations.

--- loc/items/bag.js
+++ loc/items/bag.js
@@ -9,7 +9,8 @@ export default [
       de: 'Dieser Beutel ist aus feinem Stoff gefertigt. Beutel wie dieser werden oft von normalen Leuten zum Transport ihres Besitzes genutzt.',
       fr: 'Ce sac est fait dans un un tissu délicat. appartiennent souvent à des gens ordinaires pour transporter leurs effets personnels.',
       es: 'Esta bolsa está hecha de una tela delicada. Las bolsas como esta a menudo son propiedad de personas comunes para llevar sus pertenencias.',
-      vi: 'Túi này được làm từ một loại vải tinh tế. Những chiếc túi như thế này thường thuộc sở hữu của những người bình thường để mang đồ đạc của họ'
+      vi: 'Túi này được làm từ một loại vải tinh tế. Những chiếc túi như thế này thường thuộc sở hữu của những người bình thường để mang đồ đạc của họ',
+      nl: 'Deze buidel is gemaakt van een fijne stof. Buidels zoals deze zijn vaak eigendom van gewone mensen voor het dragen van hun bezittingen.'
     }
   },
 
@@ -23,7 +24,8 @@ export default [
       de: 'Dieser robuste Sack ist aus stabilen Stoffen gefertigt, um mehr Ausrüstung aufzunehmen als ein Leinenbeutel.',
       fr: 'Ce sac solide est fabriqué à partir de tissus solides pour contenir plus d\'équipement qu\'un sac en lin.',
       es: 'Este saco está hecho de telas resistentes para sostener más equipo que una bolsa de lino.',
-      vi: 'Bao tải xấu xí này được chế tạo từ các loại vải chắc chắn để chứa nhiều thiết bị hơn túi vải lanh.'
+      vi: 'Bao tải xấu xí này được chế tạo từ các loại vải chắc chắn để chứa nhiều thiết bị hơn túi vải lanh.',
+      nl: 'Deze rugzak is gehandhaafd van stevige stoffen om meer uitrusting te dragen dan een "Linen Pouch".'
     }
   },
 
@@ -37,7 +39,8 @@ export default [
       de: 'Dieser robuste Sack ist aus Monsterhaut gefertigt und erlaubt ein ansehnliches Inventar.',
       fr: 'Ce sac robuste est fabriqué à partir de peaux de monstre et offre un espace de stockage respectable.',
       es: 'Este saco robusto está hecho a base de pieles de monstruo y permite un inventario respetable.',
-      vi: 'Chiếc túi chắc chắn này được làm từ da quái vật và là một hành trang đáng trân trọng.'
+      vi: 'Chiếc túi chắc chắn này được làm từ da quái vật và là một hành trang đáng trân trọng.',
+      nl: 'Deze krachtige zak is gemaakt van monsterhuid en gunt een fatsoenlijke inventaris.'
     }
   },
 
@@ -51,7 +54,8 @@ export default [
       de: 'Diese feine Tasche wurde aus teuren elfischen Stoffen gefertigt und enthält mehr Fächer als die meisten anderen Taschen.',
       fr: 'Ce beau sac a été fabriqué à partir de tissus elfiques coûteux et contient plus de poches que la plupart des autres sacs.',
       es: 'Esta bolsa fina fue hecha de costosas telas élficas y contiene más bolsillos que la mayoría de las otras bolsas.',
-      vi: 'Chiếc túi tốt này được chế tác từ vải Elvish đắt tiền và chứa nhiều hơn hầu hết các túi khác'
+      vi: 'Chiếc túi tốt này được chế tác từ vải Elvish đắt tiền và chứa nhiều hơn hầu hết các túi khác',
+      nl: 'Deze fijne tas is gehandhaafd van dure tover-stoffen en heeft meer zakken dan de meeste andere tassen.'
     }
   },
 
@@ -65,7 +69,8 @@ export default [
       de: 'Taschen wie diese sind ein Geschenk der Natur an geheime Druidengesellschaften. Leider haben Monster im Laufe der Zeit viele Druiden entsorgt und die heiligen Taschen gestohlen.',
       fr: 'Des sacs comme celui-ci sont un cadeau de la nature aux sociétés druidiques secrètes. Malheureusement, les monstres ont éliminé de nombreux druides au fil du temps et ont volé ces sacs sacrés.',
       es: 'Bolsas como estas son un regalo de la naturaleza para las sociedades secretas de druidas. Desafortunadamente, los monstruos han eliminado muchos druidas con el tiempo y han robado estas bolsas sagradas.',
-      vi: 'Những chiếc túi như thế này là một món quà từ thiên nhiên cho các bộ tộc druid bí mật. Thật không may, quái vật đã vứt bỏ nhiều druid theo thời gian và đã đánh cắp những chiếc túi thiêng liêng này.'
+      vi: 'Những chiếc túi như thế này là một món quà từ thiên nhiên cho các bộ tộc druid bí mật. Thật không may, quái vật đã vứt bỏ nhiều druid theo thời gian và đã đánh cắp những chiếc túi thiêng liêng này.',
+      nl: 'Tassen zoals deze zijn cadeaus van de natuur aan geheime druïde samenlevingen. Jammer genoeg, monsters hebben vele druïdes opgeruimd na een tijdje en hebben deze heilige tassen gestolen.'
     }
   }
 
