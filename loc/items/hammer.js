432188213bd57b672b0f5325e862bf16f831036f
Redly0n <41922752+Redly0n@users.noreply.github.com>
2020-02-29 17:31:10 +0100

    Added French Translations to hammer.js

--- loc/items/hammer.js
+++ loc/items/hammer.js
@@ -3,6 +3,7 @@ export default [
     name       : {$$: 'Splintered Club' },
     description: {
       en: 'This is a makeshift weapon that is often created by farmers.',
+      fr: 'Cette arme de fortune est souvent fabriquée par des fermiers.',
       hu: 'Ez az egyszerű szerszám szántóvetők keze munkája.',
       ru: 'Это временное оружие, которое часто создают фермеры.',
       de: 'Dies ist eine provisorische Waffe, die oft von Bauern hergestellt wird.',
@@ -15,6 +16,7 @@ export default [
     name       : {$$: 'Wooden Mallet' },
     description: {
       en: 'This is a mallet normally used for carpentry; however, wooden mallets are often used for combat out of necessity when resources and materials are scarce.',
+      fr: 'Il s\'agit d\'un maillet normalement utilisé pour la menuiserie; cependant, les maillets en bois sont souvent utilisés pour le combat par nécessité lorsque les ressources et les matériaux sont rares.',
       hu: 'Ez a pöröly az ácsok eszköze, de amikor az ellenség megmássza a falakat, és a családok a barlangokba menekülnek, a hősöknek felér ez a leghatalmasabb pallosokkal.',
       ru: 'Этот молоток, обычно используется для столярных работ; однако деревянные молоточки по необходимости часто используются для борьбы, когда ресурсов и материалов не хватает.',
       de: 'Dies ist ein Hammer, der normalerweise für die Tischlerei verwendet wird. Holzhämmer werden jedoch oft im Kampf eingesetzt, wenn Rohstoffe und Materialien knapp sind.',
@@ -27,6 +29,7 @@ export default [
     name       : {$$: 'Primal Mace' },
     description: {
       en: 'Primal maces are battle-tested and have legends heaped upon them; however, their effectiveness has diminished over time.',
+      fr: 'Les Primal maces ont été testées au combat et ont accumulé des tas de légendes; cependant, leur efficacité a diminué avec le temps.',
       hu: 'Ezekre a kipróbált ormótlan bunkókra számos legenda fonódott, de a hatékonyságuk csökkent a történelem századai során.',
       ru: 'Первоклассные булавы проходят боевые испытания и на них накапливаются легенды, однако их эффективность со временем снижается.',
       de: 'Urstreitkolben sind kampferprobt und haben Legenden, die auf sie gewirkt haben. Ihre Wirksamkeit hat jedoch mit der Zeit abgenommen.',
@@ -39,6 +42,7 @@ export default [
     name       : {$$: 'Orcish Bludgeon' },
     description: {
       en: 'This is an orc\'s preferred tool for interrogations and battle.',
+      fr: 'C\'est l\'arme préféré des orcs pour les interrogatoires et les batailles.',
       hu: 'Ez az orkok kedvelt eszköze csatára és kihallgatásokra.',
       ru: 'Это предпочтительный инструмент орков для допросов и ведения боя.',
       de: 'Dies ist das bevorzugte Werkzeug eines Orks für Verhöre und Kämpfe.',
@@ -51,6 +55,7 @@ export default [
     name       : {$$: 'Heavy Mace' },
     description: {
       en: 'Heavy maces are made for war, infiltration, and charisma.',
+      fr: 'Les massues lourdes sont faites pour la guerre, l\'infiltration, et le charisme.',
       hu: 'A nehéz pőrölyök háborúra és tetszelgésre készültek a jóképű tiszteknek.',
       ru: 'Тяжелые булавы созданы для войны, вторжений и харизмы.',
       de: 'Schwere Keulen sind für Krieg, Überfall und Charisma geschaffen.',
@@ -63,6 +68,7 @@ export default [
     name       : {$$: 'Iron Basher' },
     description: {
       en: 'Iron bashers are typically made by expert craftsmen such as the monster Markay\'ak. Craftsmen almost always leave their mark on the iron as a symbol of pride.',
+      fr: 'Les Iron Bashers sont généralement fabriqués par des artisans experts tels que le monstre Markay\'ak. Les artisans laissent presque toujours leur marque sur le fer comme symbole de fierté.',
       hu: 'A vasbuzogányok általában mesteremberek munkái, például a híres Markay\'ak is efféléket készít. Szinte mindíg otthagyják a jelzésüket a vason, a büszkeség jelképeként.',
       ru: 'Железные башеры обычно изготавливаются опытными мастерами, такими как монстр Markay\'ak. Ремесленники почти всегда оставляют свой след на железе как символ гордости.',
       de: 'Eisenschläger werden typischerweise von erfahrenen Handwerkern wie dem Monster Markay\'ak hergestellt. Handwerker hinterlassen fast immer ihre Spuren auf dem Eisen als Symbol des Stolzes.',
@@ -75,6 +81,7 @@ export default [
     name       : {$$: 'Darkmetal Maul' },
     description: {
       en: 'These mauls are made with a special metal that has distinct colors and properties. The secrets of this darkmetal were stolen from monster craftsmen and popularized.',
+      fr: 'Ces marteaux sont fabriqués avec un métal spécial qui possède des couleurs et des propriétés distinctes. Les secrets de ce métal sombre ont été volés par des artisans monstres et furent popularisés.',
       hu: 'Ezek a kalapácsok kivételes fémből készültek, ami elnyeli a színeket, de a tulajdonságokat is. A varázslat titkai szörnyeteg kovácsoktól származik, és később terjedt el.',
       ru: 'Эти булавы сделаны из специального металла, который имеет различные цвета и свойства. Секреты этого тёмного металла были украдены у монстров-ремесленников и популяризованы.',
       de: 'Diese Mauls werden aus einem speziellen Metall hergestellt, das unterschiedliche Farben und Eigenschaften aufweist. Die Geheimnisse dieses Dunkelmetalls wurden von Monster-Handwerkern gestohlen und verbreitet.',
@@ -87,6 +94,7 @@ export default [
     name       : {$$: 'Divine Gavel' },
     description: {
       en: 'These gavels are blessed by a church. Divine gavels sometimes acquire power through the devotion, achievement, and sacrifice of previous owners.',
+      fr: 'Ces marteaux sont bénis par une église. Les marteaux divins acquièrent parfois du pouvoir grâce à la dévotion, à la réussite et au sacrifice des propriétaires précédents.',
       hu: 'Ezek a gavélusok az egyház áldását viselik. A szent pörölyök gyakran a hit, önfeláldozás, és a tisztaság erejével erősödnek, gyakran az előző viselőiével.',
       ru: 'Эти молоты благословлены церковью. Божественные молоты иногда приобретают силу благодаря преданности, достижениям и самопожертвованию предыдущих владельцев.',
       de: 'Diese Hämmer werden von einer Kirche gesegnet. Göttliche Hämmer erwerben manchmal Macht durch die Hingabe, Errungenschaft und Opferung früherer Besitzer.',
@@ -99,6 +107,7 @@ export default [
     name       : {$$: 'Hallowed Hammer' },
     description: {
       en: 'Hammers such as this are often created by famous blacksmiths like Markay\'ak with the help of religious monster priests. Hallowed hammers are sometimes endowed with sacred powers.',
+      fr: 'Des marteaux comme celui-ci sont souvent créés par des forgerons célèbres comme Markay\'ak avec l\'aide de prêtres monstres religieux. Les marteaux sanctifiés sont parfois dotés de pouvoirs sacrés.',
       hu: 'Az efféle kalapácsok híres mesterek remekei. Markay\'ak maga is kovácsolt ilyet, egy garmada szörnyetegpap segedelmével. Az üreges kalapácsok szentséges erővel vannak megtöltve.',
       ru: 'Такие молоты часто создаются известными кузнецами, такими как Markay\'ak, с помощью священников - религиозных монстров. Священные молоты иногда наделены святыми способностями.',
       de: 'Hämmer wie dieser werden oft von berühmten Schmieden wie Markay\'ak mit Hilfe von religiösen Monsterpriestern hergestellt. Heilige Hämmer sind manchmal mit heiligen Kräften versehen.',
@@ -111,6 +120,7 @@ export default [
     name       : {$$: 'Dwarven Maul' },
     description: {
       en: 'Dwarven mauls are robust and powerful enough to withstand Dwarven life in the mines and ground. Dwarven blacksmiths have vast experience in making hammers.',
+      fr: 'Les Dwarven mauls sont suffisamment robustes et puissants pour résister à la vie des nains dans les mines et à la surface. Les forgerons nains ont une vaste expérience dans la fabrication de marteaux.',
       hu: 'A törp pörölyök robosztus termetükkel megállják a helyüket a törpék földalatti életében is. A törp kovácsoknak hatalmas tapasztalatuk van efféle anarnmnensek készítésében.',
       ru: 'Булавы гномов достаточно прочны и мощны, чтобы сохранять жизни гномов в шахтах и на земле. Гномы кузнецы имеют огромный опыт изготовления булав.',
       de: 'Die Mauls der Zwerge sind robust und stark genug, um dem Leben der Zwerge in den Minen und auf der Oberfläche standzuhalten. Zwergenschmiede haben große Erfahrung in der Herstellung von Hämmern.',
@@ -123,6 +133,7 @@ export default [
     name       : {$$: 'Coldforged Gavel' },
     description: {
       en: 'These gavels are made from a special metal and forged without heat. The process of forging without heat allows innate properties of the special metal to remain.',
+      fr: 'Ces marteaux sont fabriqués à partir d\'un métal spécial et forgés sans chaleur. Le processus de forgeage sans chaleur permet aux propriétés innées du métal spécial de rester.',
       hu: 'A gavélusok különleges fémből, hő felhasználása nélkül születtek. Mint tudhatjuk, a folyamat lehetővé teszi a fém eredeti tulajdonságainak megőrzését.',
       ru: 'Эти молотки изготавливаются из специального металла и куются без нагрева. Процесс ковки без нагрева позволяет сохранить врожденные свойства специального металла.',
       de: 'Diese Hämmer sind aus einem speziellen Metall gefertigt und ohne Hitze geschmiedet. Der Prozess des Schmiedens ohne Hitze lässt die inneren Eigenschaften des speziellen Metalls bestehen.',
@@ -135,6 +146,7 @@ export default [
     name       : {$$: 'Skullshatterer' },
     description: {
       en: 'Skullshatterer hammers were originally created to combat the undead. The hammers that remain are often considered prized possessions by many clerics and sacred beings.',
+      fr: 'Les marteaux Skullshatterer ont été créés à l\'origine pour combattre les morts-vivants. Les marteaux restant sont souvent considérés comme des biens précieux par de nombreux religieux et êtres sacrés.',
       hu: 'A Koponyazúzó kalapácsok a csontos élőholt elleni küzdelemre készültek. A megmaradt pőrölyöket szent papok és áldott lovagok hátrahagyott ereklyéiképp őrzik.',
       ru: 'Первоначально эти молоты использовались для разбивания черепов и борьбы с нежитью. Оставшиеся молоты часто считаются ценным достоянием для многих священнослужителей и святых.',
       de: 'Skullshatterer Hämmer wurden ursprünglich zur Bekämpfung der Untoten entwickelt. Die verbleibenden Hämmer werden von vielen Geistlichen und Heiligen oft als wertvoller Besitz angesehen.',
@@ -147,6 +159,7 @@ export default [
     name       : {$$: 'Amboss' },
     description: {
       en: 'These hammers are known for breaking anvil heads during the process of forging. They are rarely created and typically fetch a substantial price.',
+      fr: 'Ces marteaux sont connus pour briser les têtes d\'enclume pendant le processus de forgeage. Ils sont rarement créés et coûtent généralement un prix substantiel.',
       hu: 'Ezek a pörölyök arról ismertek, hogy a kovácsolás során letörik vagy öt-hat üllő fejét. Már csak ezért is ritkák, és az áruk megfizethetetlen.',
       ru: 'Эти молоты известны тем, что они ломают поверхность наковальни в процессе ковки. Они редко создаются и, как правило, имеют существенную цену.',
       de: 'Diese Hämmer sind dafür bekannt, dass sie während des Schmiedeprozesses Ambossköpfe zerbrechen. Sie werden selten hergestellt und erzielen in der Regel einen hohen Preis.',
@@ -159,6 +172,7 @@ export default [
     name       : {$$: 'Benevolence' },
     description: {
       en: 'Spiritual masters often spread the message of their spirituality in a variety of ways. This hammer has the power to give the gift of peace to enemies — one way or another.',
+      fr: 'Les maîtres spirituels diffusent souvent le message de leur spiritualité de diverses manières. Ce marteau a le pouvoir d\'offrir le don de la paix aux ennemis - d\'une manière ou d\'une autre.',
       hu: 'A nagy misztikusok sokféleképp adják át tanaikat. Ez a pöröly olyan erővel rendelkezik, hogy mindig a béke ajándéka lesz ellenfeleidnek. Többféle mód van ezt megvalósítani.',
       ru: 'Духовные мастера часто распространяют весть о своей духовности различными способами. Этот молот обладает способностью так или иначе дарить врагам дар мира.',
       de: 'Spirituelle Meister verbreiten oft die Botschaft ihrer Geistigkeit auf vielfältige Weise. Dieser Hammer hat die Kraft, Feinden das Geschenk des Friedens zu geben - auf die eine oder andere Weise.',
@@ -171,6 +185,7 @@ export default [
     name       : {$$: 'Hammer of Gaia' },
     description: {
       en: 'These hammers were owned and used by titanic monster overlords after the initial formation of the Great Barrier. These titans wrecked the land and ruled the realms for a time.',
+      fr: 'Ces marteaux appartenaient et étaient utilisés par des suzerains de monstres titanesques après la formation initiale de la Grande Barrière. Ces titans ont détruit la terre et gouverné les royaumes pendant un certain temps.',
       hu: 'Ezek a kalapácsok titáni szörnyetegek anarnmnentumai, amiket a Nagy Fedezék konstrukciója óta használtak. Ezek a titánusok felhasították a földeket, és terrorjuk alatt bírták a birodalmakat.',
       ru: 'Эти молоты принадлежали Титанам-повелителям и использовались ими после первоначального образования "Great Barrier". Эти титаны разрушили землю и некоторое время правили королевством.',
       de: 'Diese Hämmer wurden nach der ersten Errichtung der großen Barriere von titanischen Monsteroberherren besessen und benutzt. Diese Titanen zerstörten das Land und beherrschten die Reiche für eine bestimmte Zeit.',
@@ -183,6 +198,7 @@ export default [
     name       : {$$: 'Worldender' },
     description: {
       en: 'According to historians, this hammer is said to have once caused an earthquake massive enough to topple a city.',
+      fr: 'Selon les historiens, ce marteau aurait provoqué un tremblement de terre suffisamment massif pour renverser une ville.',
       hu: 'A nagy mágusakadémia történészei szerint a pöröly egyetlen csapása egy teljes város vesztét okozhatja.',
       ru: 'По словам историков, этот молот, как говорят, однажды вызвал землетрясение, достаточно мощное, чтобы свергнуть город.',
       de: 'Nach Angaben von Historikern soll dieser Hammer einmal ein Erdbeben verursacht haben, das massiv genug war, um eine Stadt zu stürzen.',
@@ -195,6 +211,7 @@ export default [
     name       : {$$: 'Nightmare' },
     description: {
       en: 'An ancient legend claims that a strange hammer fell through a Great Barrier fissure into the realms; hammers such as this are simply not of this world.',
+      fr: 'Une ancienne légende prétend qu\'un étrange marteau est tombé à travers une fissure de la Grande Barrière dans les royaumes; des marteaux comme celui-ci ne sont tout simplement pas de ce monde.',
       hu: 'Egy ősi legenda azt állítja, hogy ez a kalapács a Nagy Fedezék egy repedésén át hullott a világunkba; az efféle pőrölyök nem evilági jószágok.',
       ru: 'Древняя легенда гласит, что странный молот упал через трещину "Great Barrie" в царства; молотки, подобные этому, просто не относятся к этому миру.',
       de: 'Eine alte Legende besagt, dass ein seltsamer Hammer durch eine Spalte der großen Barriere in die Reiche fiel. Solche Hämmer sind einfach nicht von dieser Welt.',
