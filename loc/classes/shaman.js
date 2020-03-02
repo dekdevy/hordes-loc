b94f88a4b2b713ec23805f6a3e93aa209de34b59
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-03-02 18:23:46 +0000

    Update shaman.js

--- loc/classes/shaman.js
+++ loc/classes/shaman.js
@@ -17,7 +17,7 @@ export default {
     hu: 'Táltos',
     pl: 'Szaman',
     bn: 'ওঝা',
-    vi: 'Vu thuật sư'
+    vi: 'Vu thuật sư',
     cs: 'Šaman'
   },
 
@@ -38,7 +38,7 @@ export default {
     hu: 'A táltosok feladata a csapat támogatása, mégpedig gyógyítóerejük segítségével, amit legjobb a sebzést felfogó harcosokra irányítaniuk. Egy táltos képes csapatára különböző áldásokat, ellenfeleire pedig átkokat szórni.',
     pl: 'Rolą szamana jest wsparcie grupy poprzez leczenie tanka i oferowanie ofensywnych buffów lub nakładanie okaleczających debuffów.',
     bn: 'ওঝার কাজ হলো ট্যাঙ্কদের নিরাময় করার মাধ্যমে পুরো দল কে সাহায্য করা এবং আক্রমণাত্মক বাফ অথবা বিপজ্জনক ডিবাফ সরবরাহ করা।',
-    vi: 'Vu thuật sư có vai trò hỗ trợ trong nhóm bằng cách hồi máu và cung cấp các Buff tấn công hoặc Debuff gây tê liệt'
+    vi: 'Vu thuật sư có vai trò hỗ trợ trong nhóm bằng cách hồi máu và cung cấp các Buff tấn công hoặc Debuff gây tê liệt',
     cs: 'Šaman je podpůrná role, která poskytuje skupině léčení, převážně Tankovi. Zároveň poskytuje ofenzivní Buffy či zpomalovací Debuffy.'
   }
 }
