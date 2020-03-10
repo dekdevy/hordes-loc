9a6b43e4f341e125407ada36efc74b56f8b33107
Redly0n <41922752+Redly0n@users.noreply.github.com>
2020-03-10 20:54:43 +0100

    Added French translations orb.js

--- loc/items/orb.js
+++ loc/items/orb.js
@@ -4,6 +4,7 @@ export default [
     name       : {$$: 'Rat Skull' },
     description: {
       en: 'This rat skull is a useful tool for beginning mages. Mages sometimes experiment on the bones of long-dead creatures to produce a focusing vessel.',
+      fr: 'Ce crâne de rat est un outil utile pour les mages débutants. Les mages expérimentent parfois sur les os de créatures mortes depuis longtemps pour produire un récipient de concentration.',
       hu: 'Ez a patkánykoponya kiválóan alkalmas kezdő mágusok számára. A varázslók olykor régi csontok szemgödrein kísérleteznek, hogy jobban összpontosíthassanak.',
       ru: 'Это череп крысы - полезный инструмент для начинающих магов. Иногда маги экспериментируют с костями долгоживущих существ, чтобы создать фокусирующийся сосуд.',
       de: 'Dieser Rattenschädel ist ein nützliches Hilfsmittel für beginnende Magier. Magier experimentieren manchmal mit den Knochen von längst toten Kreaturen, um ein Fokussiergefäß zu erzeugen.',
@@ -16,6 +17,7 @@ export default [
     name       : {$$: 'Crystal Globe' },
     description: {
       en: 'These globes are used for a variety of magical tasks. This is a classic device for magic users.',
+      fr: 'Ces globes sont utilisés pour une variété de tâches magiques. Il s\'agit d\'un appareil classique pour les utilisateurs de la magie.',
       hu: 'A kristálygömb százféle mágikus munkára fölhasználható. Ez minden varázshasználók kedvenc eszköze.',
       ru: 'Эти сферы используются для различных магических задач. Это классический инструмент для волшебников.',
       de: 'Diese Kugeln werden für eine Vielzahl von magischen Aufgaben verwendet. Dies ist ein klassisches Hilfsmittel für magische Anwender.',
@@ -28,6 +30,7 @@ export default [
     name       : {$$: 'Seer\'s Stone' },
     description: {
       en: 'The internal magic of these stones allow wise men to protect themselves during scrying and other seer activities.',
+      fr: 'La magie intérieure de ces pierres permet aux sages de se protéger pendant les prédictions d\'avenirs et les autres activités des voyants.',
       hu: 'Ezen kövek pokoli ereje megvédi a bölcseket, amíg hatalmas erőfeszítéssel a mágiára megfogására összpontosítanak.',
       ru: 'Внутренняя магия этих камней позволяет мудрецам защитить себя во время охоты и других провидческих действий.',
       de: 'Die innere Magie dieser Steine erlaubt es Weisen, sich während des Scrying und anderer Seheraktivitäten zu schützen.',
@@ -40,6 +43,7 @@ export default [
     name       : {$$: 'Philosopher\'s Stone' },
     description: {
       en: 'Rumors about immortality and transmutation inspired magic users to experiment and create stones such as this. These orbs became useful in augmenting magical power despite the experimental results falling short of the original intentions.',
+      fr: 'Les rumeurs sur l\'immortalité et la transmutation ont inspiré les utilisateurs de la magie à expérimenter et à créer des pierres comme celle-ci. Ces orbes sont devenues utiles pour augmenter la puissance magique bien que les résultats expérimentaux n\'aient pas atteint leurs intentions originales.',
       hu: 'A halhatatlanság évezredes pletykája sok varázslót arra ösztönzött, hogy maguk is efféle eszközök elkészítésével kísérletezzenek. Ezek a kövek ugyan képesek a energiákat fókuszálni, de sajnálatos módon eredeti funkciójukat soha nem töltik be.',
       ru: 'Слухи о бессмертии и трансмутации вдохновляли волшебников экспериментировать и создавать такие камни. Эти шары стали полезны для увеличения магической силы, несмотря на экспериментальные результаты, не соответствующие первоначальным намерениям.',
       de: 'Gerüchte über Unsterblichkeit und Transmutation inspirierten magische Benutzer zum Experimentieren und Gestalten solcher Steine. Diese Kugeln wurden zur Verstärkung der magischen Kraft verwendet, obwohl die experimentellen Ergebnisse nicht den ursprünglichen Absichten entsprachen.',
@@ -52,6 +56,7 @@ export default [
     name       : {$$: 'Cyclops\'s Eye' },
     description: {
       en: 'This is a fantastic magical component that is easily enchanted. Handy items such as this often come with innate powers and can frequently be found in trade shipments between merchants.',
+      fr: 'Il s\'agit d\'un fantastique composant magique qui s\'enchante facilement. Des objets pratiques comme celui-ci sont souvent dotés de pouvoirs innés et peuvent souvent être trouvés dans les échanges commerciaux entre marchands.',
       hu: 'Elképesztően könnyű elbűvölni ezt a mágikus komponenst. Ezek a hasznos tárgyak gyakorta rendelkeznek önálló, veleszületett képességekkel, és áruk mérvadó a tengeri kereskedelemben.',
       ru: 'Это фантастический магический компонент, который легко очаровывает. Такие удобные предметы, как этот, часто имеют врожденные силы и часто встречаются в товарах у торговцев.',
       de: 'Dies ist eine fantastische magische Komponente, die leicht zu verzaubern ist. Nützliche Gegenstände wie diese haben oft natürliche Kräfte und sind häufig in Handelssendungen zwischen Händlern zu finden.',
@@ -64,6 +69,7 @@ export default [
     name       : {$$: 'Nüwa\'s Rondure' },
     description: {
       en: 'This powerful magical object is often used to control the elements and inspire fear in the hearts of enemies. Nüwa, a legendary magic user from a monster faction, made many objects such as this.',
+      fr: 'Ce puissant objet magique est souvent utilisé pour contrôler les éléments et inspirer la peur dans le cœur des ennemis. Nüwa, un légendaire utilisateur de la magie, provenant d\'une faction de monstre, a fabriqué de nombreux objets comme celui-ci.',
       hu: 'Eme erős varázstárgy képes az őselemek irányítására, és az ellenfeleid megfélemlítésére. Nüva, a legendás szörnyeteg sámán számtalan efféle rondúrát készített tanítványai számára.',
       ru: 'Этот мощный магический объект часто используется для управления стихией и внушает страх в сердцах врагов. Nüwa\'s, легендарный волшебник из фракции монстров, создал множество подобных предметов.',
       de: 'Dieses mächtige magische Gegenstand wird oft verwendet, um die Elemente zu kontrollieren und Angst in den Herzen der Feinde zu wecken. Nüwa, ein legendärer magischer Benutzer aus einer Monsterfraktion, fertigte viele solcher Gegenstände an.',
@@ -76,6 +82,7 @@ export default [
     name       : {$$: 'Baetylus\'s Eye' },
     description: {
       en: 'Objects like this were created from meteorites and misused as holy symbols. Although they are no longer used as holy symbols, Baetylus’s Eyes radiate substantial magical power.',
+      fr: 'Des objets comme celui-ci ont été créés à partir de météores et furent utilisés à mauvais escient comme symboles sacrés. Bien qu\'ils ne soient plus utilisés comme symboles sacrés, les yeux de Baetylus dégagent un pouvoir magique considérable.',
       hu: 'Az ehhez hasonló eszközök meteoritokból származnak, és egykor ereklyeképp tisztelték őket. Bár mára nem díszítik falusi templomok oltárait, Betíleusz Szeme emberfeletti erőt sugároz.',
       ru: 'Подобные объекты были созданы из метеоритов и использовались как священные символы. Хотя они больше не используются в качестве священных символов, Baetylus\'s Eyes излучают значительную магическую силу.',
       de: 'Solche Gegenstände wurden aus Meteoriten erschaffen und als heilige Symbole missbraucht. Obwohl sie nicht mehr als heilige Symbole verwendet werden, strahlen Baetylus Augen eine enorme magische Kraft aus.',
@@ -88,6 +95,7 @@ export default [
     name       : {$$: 'Benben Stone' },
     description: {
       en: 'Benben Stones were placed on top of the pyramids of an ancient, dead race. The stones absorbed thousands of years of solar energy, which was said to be a gift from a sun deity.',
+      fr: 'Les pierres Benben ont été placées au sommet des pyramides d\'une ancienne race morte. Les pierres ont absorbé des milliers d\'années d\'énergie solaire, qui serait un cadeau d\'une divinité solaire.',
       hu: 'Benben Köve egy ősi, halott faj rabszolgák emelte gúlájának csúcsát díszítette. A kövek több ezer év napfényét nyelték el, melyet egy napisten ajándékaképp fogadtak a buzgó hívek.',
       ru: 'Benben Камни были установлены на вершине пирамид древней, мертвой расы. Камни впитали в себя тысячелетнюю солнечную энергию, которая, как говорят, была подарком солнечного божества.',
       de: 'Benben Steine wurden auf die Pyramiden einer alten, toten Spezies gelegt. Die Steine absorbierten Tausende von Jahren Sonnenenergie, die als Geschenk einer Sonnengottheit galt.',
@@ -100,6 +108,7 @@ export default [
     name       : {$$: 'Silthrim' },
     description: {
       en: 'Ancient snake stones such as this were found on forgotten altars inside ancient ruins. Scholars say that the stones were probably used in worshipping a snake deity, but the texts found in the ruins have not been successfully translated yet.',
+      fr: 'De vieilles pierres de serpent comme celle-ci ont été trouvées sur des autels oubliés à l\'intérieur de ruines antiques. Les savants disent que les pierres ont probablement été utilisées pour adorer une divinité de serpent, mais les textes trouvés dans les ruines n\'ont pas encore été traduits avec succès.',
       hu: 'Az ősi kígyóköveket eltemetett romok oltáraiból emelték ki. A mágusiskola növendékei egy kígyóistenséggel kapcsolják össze a köveket, de a mohó indák által benőtt, töredezett kőtáblákat eddig még senkinek sem sikerült lefordítani.',
       ru: 'Древние змеиные камни, подобные этому, были найдены на забытых алтарях внутри древних руин. Ученые говорят, что камни, вероятно, использовались для поклонения божеству змеи, но тексты, найденные в руинах, еще не были успешно переведены.',
       de: 'Solche alten Schlangensteine wurden auf längst vergessenen Altären in alten Ruinen gefunden. Wissenschaftler sagen, dass die Steine wahrscheinlich bei der Anbetung einer Schlangengottheit verwendet wurden. Die in den Ruinen gefundenen Texte sind jedoch noch nicht vollständig übersetzt worden.',
@@ -112,6 +121,7 @@ export default [
     name       : {$$: 'Lich\'s Phylactery' },
     description: {
       en: 'A lich’s phylactery contains the soul of a powerful undead creature. Older phylacteries come with powerful enchantments to protect the wearer — and the soul contained within.',
+      fr: 'Le phylactère d\'une liche contient l\'âme d\'une puissante créature morte-vivante. Les phylactères plus anciennes sont accompagnées de puissants enchantements pour protéger le porteur - et l\'âme contenue à l\'intérieur.',
       hu: 'A Boszorkánymester fülaktériuma egy élőholt teremtmény lelkét őrzi magában. Az idősebb fülaktériumok erősebb varázserővel védik a viselőt, és a bezárt lelket egyaránt.',
       ru: 'Филактерия лича содержит душу могущественного существа-нежити. Старые филактерии обладают мощным завораживающим действием для защиты владельца и души, содержащейся в них.',
       de: 'Die Pflanzenzucht eines Lichs enthält die Seele einer mächtigen untoten Kreatur. Ältere Pflanzenarten haben mächtige Verzauberungen, um den Träger und die darin enthaltene Seele zu schützen.',
