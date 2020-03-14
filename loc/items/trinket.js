2eb4c9e525b5b1455ee0d38caebac6d13da20faa
dek <polydek@googlemail.com>
2020-03-14 16:22:51 +0100

    Update trinket.js

--- loc/items/trinket.js
+++ loc/items/trinket.js
@@ -1,8 +1,8 @@
 export default [
   {
-    name       : {$$: 'Test'},
+    name       : {$$: 'Little Bell'},
     description: {
-      en: 'Test trinket.'
+      en: 'Bells are often used as a symbol of devotion toward a chosen deity. The interior of this bell has an inlaid silver inscription, but the translation of the inscription has been lost to time.'
     }
   }
 ]
