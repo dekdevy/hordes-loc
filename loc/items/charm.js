05670873e60aeab4424233004e0b9a2350e0f65a
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-03-17 12:31:51 +0000

    Russian charm translation.

--- loc/items/charm.js
+++ loc/items/charm.js
@@ -2,7 +2,8 @@ export default [
   {
     name       : {$$: 'Little Bell'},
     description: {
-      en: 'Bells are often used as a symbol of devotion toward a chosen deity. The interior of this bell has an inlaid silver inscription, but the translation of the inscription has been lost to time.'
+      en: 'Bells are often used as a symbol of devotion toward a chosen deity. The interior of this bell has an inlaid silver inscription, but the translation of the inscription has been lost to time.',
+      ru: 'Колокола часто используются как символ преданности избранному богу. Внутри этого колокола есть серебряная надпись, но перевод этой надписи потерян во времени.'
     }
   }
 ]
