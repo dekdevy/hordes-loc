8543259d0a7c7ed7f56f018a281bcc2d93f5c7cc
dek <polydek@googlemail.com>
2020-03-24 21:56:09 +0100

    Update charm.js

--- loc/items/charm.js
+++ loc/items/charm.js
@@ -5,5 +5,29 @@ export default [
       en: 'Bells are often used as a symbol of devotion toward a chosen deity. The interior of this bell has an inlaid silver inscription, but the translation of the inscription has been lost to time.',
       ru: 'Колокола часто используются как символ преданности избранному богу. Внутри этого колокола есть серебряная надпись, но перевод этой надписи потерян во времени.'
     }
+  },
+  {
+    name       : {$$: 'Hardened Egg'},
+    description: {
+      en: 'Hardened Egg lore.'
+    }
+  },
+  {
+    name       : {$$: 'Tattooed Skull'},
+    description: {
+      en: 'Charm 3 lore.'
+    }
+  },
+  {
+    name       : {$$: 'Ship Pennant'},
+    description: {
+      en: 'Charm 4 lore.'
+    }
+  },
+  {
+    name       : {$$: 'Blue Marble'},
+    description: {
+      en: 'Charm 5 lore.'
+    }
   }
 ]
