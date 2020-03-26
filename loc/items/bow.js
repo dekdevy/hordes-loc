1fc466f688b8f02c8d85f68e8e89dbad632fb3cf
isBre <d.brescia3@studenti.uniba.it>
2020-03-26 13:35:48 +0100

    added italian language in bow.js

--- loc/items/bow.js
+++ loc/items/bow.js
@@ -9,6 +9,7 @@ export default [
       de: 'Dieser Bogen ist eine provisorische Kreation. Holz ist eine seltene Ressource in manchen Regionen, daher nutzen Bewohner manchmal vorhandene Materialien, um solche Waffen herzustellen.',
       es: 'Este arco es una creación temporal. No hay mucha madera en algunas regiones, por lo tanto los Denizens usan el material que tienen a mano para crear armas como esta.',
       vi: 'Cung này là một sáng tạo tạm thời. Gỗ không phải là nguồn tài nguyên dồi dào ở một số vùng, do đó, người dân đôi khi sử dụng bất kỳ vật liệu sẵn có nào để tạo ra vũ khí như thế này.'
+	  it: 'Questo arco è una creazione improvvisata. Il legno non è una risorsa abbondante in alcune regioni, quindi gli abitanti a volte usano i materiali disponibili per creare armi come questa.'
     }
   },
 
@@ -22,6 +23,7 @@ export default [
       de: 'Bögen wie dieser werden von Anfänger-Bogenschützen zum Üben genutzt.',
       es: 'Arcos como estos son manufacturados para arqueros iniciados de forma que estos puedan refinar sus habilidades.',
       vi: 'Những chiếc cung như thế này được tạo ra để các cung thủ bắt đầu trau dồi kỹ năng của họ'
+	  it: 'Archi come questi sono realizzati per gli arcieri principianti per affinare le loro abilità.'
     }
   },
 
@@ -35,6 +37,7 @@ export default [
       de: 'Gekrümmte Kurzbögen werden hauptsächlich zur Jagd hergestellt.',
       es: 'Los Curved Shortbows son mayoritariamente producidos para cazar la fauna local.',
       vi: 'Cung ngắn cong chủ yếu được dùng để săn bắn động vật hoang dã.'
+	  it: 'Gli shortbows curvi sono principalmente prodotti per la caccia alla fauna selvatica locale.'
     }
   },
 
@@ -48,6 +51,7 @@ export default [
       de: 'Diese Sorte Bogen ist die Standardwaffe für Abenteurer.',
       es: 'Este tipo de arco son el arma de elección estándar para los aventureros.',
       vi: 'Loại cung này là vũ khí tiêu chuẩn để các nhà thám hiểm lựa chọn.'
+	  it: 'Questo tipo di arco è un\'arma standard per gli avventurieri.'
     }
   },
 
@@ -61,6 +65,7 @@ export default [
       de: 'Langbögen sind als Kriegswaffen entwickelt worden.',
       es: 'Los Longbows son diseñados para ser armas de guerra.',
       vi: 'Cung dài được thiết kế trở thành vũ khí trong các trận chiến.'
+	  it: 'I longbows sono progettati per essere armi di guerra.'
     }
   },
 
@@ -74,6 +79,7 @@ export default [
       de: 'Dieser Bogen wurde aus Monster-Knochen hergestellt um sehr belastbar und stark zu sein.',
       es: 'Este arco es confeccionado con huesos de monstruo para que así tenga una resistencia y potencia mayor.',
       vi: 'Cung này được chế tạo từ xương quái vật tăng thêm khả năng phục hồi và sức mạnh.'
+	  it: 'Questo arco è realizzato con ossa di mostri per produrre maggiore capacità di recupero e potenza.'
     }
   },
 
@@ -87,6 +93,7 @@ export default [
       de: 'Elfische Handwerker sind während des Herstellungsprozesses besondersd sorgfältig, und elfische Bogenschützen haben in Kriegszeiten aus gutem Grund einen guten Ruf.',
       es: 'Los artesanos Élficos ponen un cuidado exquisito en sus arcos durante el proceso de manufacturado, es por esto que los arqueros Elfos tienen una orgullosa reputación durante tiempos de guerra, y con motivos.',
       vi: 'Thợ thủ công Elvish rất tỉ mỉ trong quá trình chế tạo cung của họ, và cung thủ Elvish có một danh tiếng đáng tự hào trong thời gian chiến tranh vì một lý do.'
+	  it: 'Gli artigiani elfici prestano una cura maniacale ai loro archi durante il processo di fabbricazione e gli arcieri elfi sono orgogliosi durante i periodi di guerra per questa ragione.'
     }
   },
 
@@ -100,6 +107,7 @@ export default [
       de: 'Altertümliche Bögen sind außergewöhnliche Waffen die von vergessenen Meisterhandwerken hergestellt wurden. Diese Bögen werden über Generationen weitergegeben.',
       es: 'Los Ancient Bows son armas excepcionales que fueron diseñadas por maestros artesanos ya olvidados. Estos arcos normalmente son heredados de generación en generación.',
       vi: 'Cung cổ xưa là vũ khí đặc biệt được thiết kế bởi những nghệ nhân bậc thầy bị lãng quên. Những chiếc cung này thường được truyền từ thế hệ này sang thế hệ khác.'
+	  it: 'Gli antichi archi sono armi eccezionali progettate da maestri artigiani dimenticati. Questi archi sono spesso passati da una generazione all\'altra.'
     }
   },
 
@@ -113,6 +121,7 @@ export default [
       de: 'Diese Bögen wurden als Antwort auf gerüstete Monster entwickelt.',
       es: 'Estos arcos fueron desarrollados en respuesta a los monstruos que llevan armadura.',
       vi: 'Những chiếc cung này được thiết kế để chống lại những loại quái vật mang giáp.'
+	  it: 'Questi archi furono sviluppati in risposta a mostri che indossavano armature.'
     }
   },
 
@@ -126,6 +135,7 @@ export default [
       de: 'Das Design dieses Bogens ist effizienter als das eines Langbogens. Das einmalige dekorative Silbermuster ist ein Zeugnis der Handwerkskunst, die für einen solchen Bogen benötigt wird.',
       es: 'El diseño de este arco es más mecánico y eficiente que el del Longbow. El patrón único decorativo de plata es un testamento al manufacturado requerido para confeccionar tal buen arco.',
       vi: 'Thiết kế cung này là hiệu quả cơ học hơn so với cung dài. Hoa văn độc đáo được mạ bạc trên cung là một minh chứng cho sự khéo léo cần có để tạo ra một chiếc cung tinh xảo như vậy.'
+	  it: 'Questo design dell\'arco è meccanicamente più efficiente del longbow. La decorazione in argento è una testimonianza dell\'abilità richiesta per realizzare un arco così fine.'
     }
   },
 
@@ -139,6 +149,7 @@ export default [
       de: 'Bögen wie dieser befanden sich im Besitz von Assasinen, die Modifikationen vornahmen, um die Effizienz zu erhöhen. Assassinen modifizieren oft ihre Waffen, um optimale Leistung zu erlangen.',
       es: 'Arcos como este fueron previamente poseidos por asesinos que hicieron modificaciones para aumentar su eficiencia drásticamente. Los asesinos regularmente modificaban sus armas para obtener un rendimiento óptimo.',
       vi: 'Những cung như thế này trước đây thuộc sở hữu của những sát thủ đã thực hiện các sửa đổi để tăng mạnh hiệu quả. Sát thủ thường xuyên sửa đổi vũ khí của họ để đạt được hiệu suất tối ưu.'
+	  it: 'Archi come questo erano precedentemente di proprietà di assassini che avevano apportato modifiche per aumentare drasticamente l\'efficienza. Gli assassini modificano regolarmente le loro armi per raggiungere prestazioni ottimali'
     }
   },
 
@@ -152,6 +163,7 @@ export default [
       de: 'Solche Bögen wurden aus Höllenfeuer gemacht, das von teuflischen Kreaturen gespeist wurde. Diese Bögen sind da der Höchstpreis für solche bösen Transaktionen und Übernahmen erforderlich ist.',
       es: 'Arcos como este fueron hechos en los fuegos del infierno provisto por criaturas diabólicas. Este tipo de arcos son raros debido al precio extremo requerido para completar su adquisición.',
       vi: 'Những chiếc cung như thế này được tạo ra từ lửa địa ngục được cung cấp bởi các sinh vật quỷ dữ. Những loại cung này rất hiếm vì cái giá cuối cùng phải trả cho nó rất cao.'
+	  it: 'Archi come questo sono stati fatti da incendi infernali forniti da creature diaboliche. Questi tipi di archi sono rari a causa del prezzo finale richiesto per tali operazioni e acquisizioni malvagie.'
     }
   },
 
@@ -165,6 +177,7 @@ export default [
       de: 'Dieser Bogen wurde im Feuer eines Phönix gebadet und im Nachthimmel gekühlt. Bögen die so hergestellt wurden besitzen manchmal magische Eigenschaften des Phönixfeuers.',
       es: 'Este arco fue bañado en las llamas de un fénix y enfriado en el cielo nocturno. Arcos creados de esta manera a veces son imbuidos con características mágicas extra fruto de las llamas del fénix.',
       vi: 'Cây cung này được tắm trong ngọn lửa của một con phượng hoàng và được làm nguội trên bầu trời đêm. Cung được tạo ra theo cách này đôi khi được thấm nhuần thêm các đặc tính ma thuật từ ngọn lửa phượng hoàng.'
+	  it: 'Questo arco era inondato dalle fiamme di una fenice e raffreddato nel cielo notturno. Gli archi creati in questo modo sono talvolta impregnati di caratteristiche magiche extra dal fuoco della fenice.'
     }
   },
 
@@ -178,6 +191,7 @@ export default [
       de: 'Widowmakers sind lebendige Bögen, die einen Bund mit ihrem Besitzer eingehen. Mächtige Monster stellen diese Bögen in einem mystischen Prozess her.  Die resultierende Waffe kann bemerkenswert eifersüchtig sein und den Benutzer dazu verleiten, keine andere Waffe zu benutzen, indem er Feinde mit großer Kampferfahrung zerstört.',
       es: 'Los Widowmakers son arcos vivientes que crean un vínculo con el usuario. Monstruos poderosos usan un proceso místico para crear Widowmakers; el arma que esto da como resultado es notablemente celosa y persuade al usuario para no usar ninguna otra arma a través de la destrucción de enemigos con amplia experiencia en batalla.',
       vi: 'Widowmakers đang trú ngụ trong cung gắn kết với người dùng. Quái vật mạnh mẽ sử dụng một quá trình thần bí để tạo ra những Widowmakers ; vũ khí thành phẩm có thể đáng ghen tị và lôi kéo người dùng không sử dụng bất kỳ vũ khí nào khác bằng cách tiêu diệt kẻ thù với kinh nghiệm chiến đấu sâu rộng'
+	  it: 'I Widowmakers sono archi viventi che legano con l\'utente. I mostri potenti usano un processo mistico per creare i Widowmakers; l\'arma risultante può essere notevolmente gelosa e invogliare l\'utente a non usare nessun\'altra arma distruggendo i nemici con una vasta esperienza di battaglia.'
     }
   },
 
@@ -191,6 +205,7 @@ export default [
       de: 'Diese Bögen sind aufgrund der Methodik und Länge des Schmiedeprozesses selten: Die Bögen müssen in großer Höhe hergestellt werden, damit jeder Blitz genutzt werden kann, um sie zu schmieden. Dies kann Ewigkeiten dauern.',
       es: 'Estos arcos son raros debido a su longitud y al método de forjado. Los arcos deben ser enormemente elevados para asegurar que cada rayo pueda ser utilizado para ir forjando este arco gradualmente hasta llegar a ser un Stormsong. Este proceso de creación puede tomar siglos.',
       vi: 'Những cung này rất hiếm do độ dài và phương pháp của quá trình rèn; cung phải được nâng lên rất nhiều để đảm bảo rằng mọi tia sét đều có thể được sử dụng để dần dần rèn cung này thành Stormsong. Quá trình tạo ra nó có thể mất nhiều thời gian'
+	  it: 'Questi archi sono rari a causa della lunghezza e del metodo del processo di forgiatura; gli archi devono essere notevolmente rialzato per garantire che ogni fulmine possa essere utilizzato per forgiare gradualmente questo arco in un canto da tempesta. Il processo di creazione può richiedere secoli.'
     }
   },
 
@@ -204,6 +219,7 @@ export default [
       de: 'Dieser bösartige Bogen frisst die Träume und Alpträume seines Nutzers. Dieser lebendige Bogen ist illoyal und fordert Opfer in Form von Kampfgegnern.',
       es: 'Este arco malévolo se come los sueños y pesadillas de su portador. Este arco viviente ofrece ninguna lealtad y demanda sacrificios en forma de víctimas en batalla.',
       vi: 'Cây cung độc ác này gặm nhấm những giấc mơ và ác mộng của người mang nó. Cây cung sống này không có lòng trung thành và đòi hỏi sự hy sinh dưới hình thức nạn nhân chiến đấu'
+	  it: 'Questo arco malevolo mangia i sogni e gli incubi del possessore. Questo arco vivente non offre lealtà e richiede sacrifici sotto forma di vittime della battaglia.'
     }
   },
 
@@ -217,6 +233,7 @@ export default [
       de: 'Wenn großes Unheil eine gesamte Zivilisation befällt, lassen die Geister der Opfer ihren Zorn in eine Waffe strömen, um Rache an der Welt zu üben. Dieses Waffe ist nur als "Fury" bekannt.',
       es: 'Cuando una gran trajedia recae sobre una civilización entera, los espíritus de las victimas iracundas canalizan su furia en un arma para desatar la venganza en contra del mundo. Este arma es únicamente conocida como "Fury".',
       vi: 'Khi một thảm kịch lớn xảy ra trong toàn bộ nền văn minh, những linh hồn của những nạn nhân tức giận đã trút cơn giận dữ của họ vào một vũ khí để báo thù thế giới. Vũ khí này chỉ được gọi là "Fury."'
+	  it: 'Quando una grande tragedia colpisce un\'intera civiltà, gli spiriti delle vittime infuriate riversano la loro rabbia in un\'arma per scatenare vendetta sul mondo. Quest\'arma è conosciuta solo come "Fury".'
     }
   }
 
