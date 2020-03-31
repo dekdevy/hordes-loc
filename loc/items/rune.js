cd19f3ab1ab500697e7a29b76827209ae3730f0e
isBre <d.brescia3@studenti.uniba.it>
2020-03-31 12:24:39 +0200

    added italian language to rune.js

--- loc/items/rune.js
+++ loc/items/rune.js
@@ -7,7 +7,8 @@ export default [
       hu: 'Ez az anyag sűrűn előfordul a bányákban; sörnyetegek gyakran ásnak utána hogy megalapozzák a gazdaságaikat.',
       ru: 'Этот плоский материал часто встречается в шахтах; монстры часто добывают Lucid, чтобы поддержать свою экономику.',
       de: 'Dieses flache Material findet sich oft in Minen. Monsters bauen es gerne ab, um ihrer Wirtschaft zu helfen.',
-      vi: 'Vật liệu phẳng này thường được tìm thấy trong các mỏ; quái vật thường khai thác Lucid để hỗ trợ nền kinh tế của họ.'
+      vi: 'Vật liệu phẳng này thường được tìm thấy trong các mỏ; quái vật thường khai thác Lucid để hỗ trợ nền kinh tế của họ.',
+      it: 'Questo materiale piatto si trova comunemente nelle miniere; i mostri spesso minano per i Lucid per sostenere le loro economie.'
     }
   },
 
@@ -19,7 +20,8 @@ export default [
       hu: 'A Melant ívei írások látszatát keltik, de a kovácsok szerint ezek természetes formációk, melyek belső szerkezetről árulkodnak.',
       ru: 'Изгибы Melant создают видимость письма или символов, но кузнецы считают, что эти природные образования открывающие мощную внутреннюю структуру.',
       de: 'Die Kurven von Melant wirken wie Schrift oder Symbole, aber Schmiede glauben, dass diese natürlichen Formationen eine starke innere Struktur aufweisen.',
-      vi: 'Các đường cong của Melant mang đến sự xuất hiện của chữ viết hoặc biểu tượng, nhưng thợ rèn tin rằng những thành phần tự nhiên này cho thấy một cấu trúc bên trong mạnh mẽ.'
+      vi: 'Các đường cong của Melant mang đến sự xuất hiện của chữ viết hoặc biểu tượng, nhưng thợ rèn tin rằng những thành phần tự nhiên này cho thấy một cấu trúc bên trong mạnh mẽ.',
+      it: 'Le curve di Melant hanno la forma di parole o simboli, ma i fabbri credono che queste formazioni naturali rivelino una potente struttura interna.'
     }
   },
 
@@ -31,7 +33,8 @@ export default [
       hu: 'Ez a hasznos alapanyag egy tűzhányóból származik, és az azt környező földekről. Egy időben sűrűn adták-vették a karavánok.',
       ru: 'Этот полезный материал происходит из большого вулкана и окружающего региона. Turim когда-то часто торговали между караванами.',
       de: 'Dieses nützliche Material stammt von einem großen Vulkan und seiner Umgebung. Turim wurde früher rege zwischen Karawanen gehandelt.',
-      vi: 'Vật liệu hữu ích này bắt nguồn từ một ngọn núi lửa lớn và khu vực xung quanh. Turim đã từng được giao dịch thường xuyên giữa các đoàn lữ hành.'
+      vi: 'Vật liệu hữu ích này bắt nguồn từ một ngọn núi lửa lớn và khu vực xung quanh. Turim đã từng được giao dịch thường xuyên giữa các đoàn lữ hành.',
+      it: 'Questo materiale utile proviene da un grande vulcano e dalla regione circostante. Un tempo Turim veniva scambiato frequentemente tra le carovane.'
     }
   },
 
@@ -43,7 +46,8 @@ export default [
       hu: 'Ezekről a robosztus kövekről csak nemrég derült ki hogy milyen hasznosak, amikor a kovácsok elkezdtek kísérletezni a "csónaknehezék Fundoval". Azóta már értékes nyersanyag.',
       ru: 'Этот прочный материал недавно был признан полезным для улучшения, когда кузнецы начали экспериментировать с "бесполезным Fundo". С тех пор Fundo считается ценным ресурсом.',
       de: 'Vor kurzem wurde festgestellt das dieses robuste Material nützlich ist um Upgrades vorzunehmen, als Schmiede mit dem "wertlosen Fundo" experimentierten. Seitdem wird es als wertvoll betrachtet.',
-      vi: 'Vật liệu mạnh mẽ này gần đây đã được tìm thấy là hữu ích để nâng cấp khi thợ rèn bắt đầu thử nghiệm "Fundo vô giá trị". Kể từ đó, Fundo đã được coi là một nguồn tài nguyên có giá trị.'
+      vi: 'Vật liệu mạnh mẽ này gần đây đã được tìm thấy là hữu ích để nâng cấp khi thợ rèn bắt đầu thử nghiệm "Fundo vô giá trị". Kể từ đó, Fundo đã được coi là một nguồn tài nguyên có giá trị.',
+      it: 'Questo materiale robusto è stato recentemente trovato utile per gli aggiornamenti quando i fabbri hanno iniziato a sperimentare "Fundo senza valore". Da quel momento, Fundo è stata considerata una risorsa preziosa.' //I'm not sure about that
     }
   },
 
@@ -55,7 +59,8 @@ export default [
       hu: 'Az Amarit gyakran ajándékozzák vízalatti királyságokból. A tanoncok szerint az Amari felfedezése egy tengeri néphez köthető, akik kapcsolatukat a szárazföldi birodalmakkal tengerfenékről hozott Amarival kötötték meg.',
       ru: 'Amari часто дарят в качестве подарков от заморских королевств. Ученые говорят, что Amari были впервые обнаружены, когда раса морских существ впервые вступила в контакт с несколькими королевствами суши и привезла Amari с морского дна.',
       de: 'Amari wird häufig als Geschenk aus Übersee-Königreichen genutzt. Gelehrte sagen das Amari entdeckt wurde als eine Spezies von Seekreaturen ersten Kontakt mit verschiedenen Landkönigreichen hatte und Amari vom Meeresboden mitbrachten.',
-      vi: 'Amari thường được tặng làm quà từ các vương quốc ngoài biển. Các học giả nói rằng Amari lần đầu tiên được phát hiện khi một chủng tộc sinh vật biển tiếp xúc lần đầu với một số vương quốc trên đất liền và đưa Amari từ đáy biển.'
+      vi: 'Amari thường được tặng làm quà từ các vương quốc ngoài biển. Các học giả nói rằng Amari lần đầu tiên được phát hiện khi một chủng tộc sinh vật biển tiếp xúc lần đầu với một số vương quốc trên đất liền và đưa Amari từ đáy biển.',
+      it: 'Gli amari sono spesso dati in dono da regni d\'oltremare. Gli studiosi affermano che gli Amari furono scoperti per la prima volta quando una razza di creature marine prese il primo contatto con diversi regni terrestri e portò gli Amari dal fondo del mare.'
     }
   },
 
@@ -67,7 +72,8 @@ export default [
       hu: 'Gyakran összetévesztik az arannyal, de kiderült, hogy a sárkánypikkelyek anyagából van. Sok ősi díszítést és fegyert borítottak be Purummal.',
       ru: 'Часто ошибочно принимаемый за золото, этот материал когда-то считался тем же самым материалом, что и чешуя дракона. Многие древние украшения и оружие украшены Purum.',
       de: 'Oft mit Gold verwechselt oder für Drachenschuppen gehalten. Viele uralte Dekorationen und Waffen sind mit Purum verziert.',
-      vi: 'Thường bị nhầm với vàng, vật liệu này từng được cho là cùng chất liệu với vảy rồng. Nhiều đồ trang trí và vũ khí cổ xưa được trang trí bằng Purum.'
+      vi: 'Thường bị nhầm với vàng, vật liệu này từng được cho là cùng chất liệu với vảy rồng. Nhiều đồ trang trí và vũ khí cổ xưa được trang trí bằng Purum.',
+      it: 'Spesso scambiato per oro, un tempo si pensava che questo materiale fosse lo stesso materiale delle squame di drago. Molte antiche decorazioni e armi sono adornate con il Purum.'
     }
   },
 
@@ -79,7 +85,8 @@ export default [
       hu: 'A Royalokat királyok és királynék is gyűjtik, hogy szertartási ajándékok lehessenek a dinasztikus házasságaiknál.',
       ru: 'Royals зачастую пренадлежит и собирается королями и королевами что бы исользовать в качестве подарков на церемониях браков по договорённости.',
       de: 'Royals werden oft von Königen und Königinnen gesammelt und besessen, um als zeremonielles Gift bei Zwangsheiraten zu dienen.',
-      vi: 'Royals  thường được sở hữu và thu thập bởi các vị vua và hoàng hậu để được sử dụng làm quà tặng trong nghi lễ trong các cuộc hôn nhân sắp đặt.'
+      vi: 'Royals  thường được sở hữu và thu thập bởi các vị vua và hoàng hậu để được sử dụng làm quà tặng trong nghi lễ trong các cuộc hôn nhân sắp đặt.',
+      it: 'I Royals sono spesso posseduti e raccolti da re e regine per essere usati come regali cerimoniali durante i matrimoni combinati.'
     }
   },
 
@@ -91,7 +98,8 @@ export default [
       hu: 'Ezek a kristályok leggyakrabban Yggdrasil, a Világfa közelében fordulnak elő. A Tarát szépsége és ereje egyaránt alkalmassá teszik ékszerek készítéséhez és fegyverek fejlesztéséhez.',
       ru: 'Эти образования естественным образом чаще всего встречаются вблизи Yggdrasil, the World Tree. Красота и редкость Tara делает их одинаково подходящими как для ювелирных изделий, так и для улучшения.',
       de: 'Diese Formationen kommen oft natürliche nahe Yggdrasil, dem Weltenbaum, vor. Die Schönheit und seltene Kraft von Tara ist sowohl für juwelenverzierten Schmuck als auch für Item-Upgrades geeignet.',
-      vi: 'Những thành phần này thường xuất hiện ở gần Yggdrasil, World Tree. Vẻ đẹp và sự hiếm có của Tara khiến chúng phù hợp như nhau cho cả đồ trang sức gia truyền hoặc nâng cấp vật phẩm.'
+      vi: 'Những thành phần này thường xuất hiện ở gần Yggdrasil, World Tree. Vẻ đẹp và sự hiếm có của Tara khiến chúng phù hợp như nhau cho cả đồ trang sức gia truyền hoặc nâng cấp vật phẩm.',
+      it: 'Queste formazioni si verificano naturalmente molto spesso vicino a Yggdrasil, l\'albero del mondo. La bellezza e la rarità del potere di Tara li rendono ugualmente adatti sia per i cimeli di gioielli che per gli aggiornamenti degli oggetti.'
     }
   },
 
@@ -103,7 +111,8 @@ export default [
       hu: 'Amint a Gloom hasznát a harci felszerelés feljesztésére felfedezték, a módszereket rögtön eladták egy ellenséges királyságnak, ami a birodalmak történetének egyik legnagyobb árulása volt.',
       ru: 'Как только были обнаружены секретные методы улучшения экипировки с использованием силы Gloom, эти методы тут же были проданы в противоборствующее королевство. Это считается одним из самых больших предательств в истории королевства.',
       de: 'Nachdem die geheimen Methoden der Ausrüstungs-Verbesserung mit Gloom entdeckt wurden, wurden diese Techniken schnell an ein gegnerisches Königreich verkauft. Dies stellt einen der größten Verratsfälle in der Geschichte der Reihe dar.',
-      vi: 'Một khi các phương pháp tăng cường bí mật sử dụng sức mạnh của Gloom đã được phát hiện, những phương pháp đó đã sớm được bán cho một vương quốc đối địch như một trong những sự phản bội lớn nhất trong lịch sử của các vương quốc.'
+      vi: 'Một khi các phương pháp tăng cường bí mật sử dụng sức mạnh của Gloom đã được phát hiện, những phương pháp đó đã sớm được bán cho một vương quốc đối địch như một trong những sự phản bội lớn nhất trong lịch sử của các vương quốc.',
+      it: 'Una volta scoperti i metodi segreti per potenziare l\'equipaggiamento usando il potere del Gloom, quei metodi furono presto venduti a un regno opposto come uno dei più grandi tradimenti nella storia dei regni.'
     }
   },
 
@@ -115,7 +124,8 @@ export default [
       hu: 'Szokatlan kristályos tulajdonságai kivételes nyersanyaggá teszik a Pluraeét. Habár csupán nemrég fedezték fel újra a világ elfeledett mélységeiben, a Plurae hamar felforgatta a háborúk erőviszonyait.',
       ru: 'Необычные кристаллические свойства Plurae делают его исключительным материалом. Хотя Plurae только недавно были вновь открыты в забытых глубинах мира, применение Plurae уже сыграло существенную роль в изменении течения войны.',
       de: 'Plurae ist ein außergewöhnliches Material aufgrund seiner ungewöhnlichen Kristallstrukturen. Obwohl Plurae erst vor kurzem in den vergessenen Tiefen der Welt entdeckt wurde, ist sein Einsatz schon für eine Wende des Kriegsglücks verantwortlich gewesen.',
-      vi: 'Tính chất kết tinh bất thường trong Plurae làm cho nó trở thành một vật liệu đặc biệt. Mặc dù Plurae chỉ mới được khám phá gần đây ở vực thẳm bị lãng quên trên thế giới, nhưng các ứng dụng của Plurae đã rất quan trọng trong việc xoay chuyển dòng chiến tranh.'
+      vi: 'Tính chất kết tinh bất thường trong Plurae làm cho nó trở thành một vật liệu đặc biệt. Mặc dù Plurae chỉ mới được khám phá gần đây ở vực thẳm bị lãng quên trên thế giới, nhưng các ứng dụng của Plurae đã rất quan trọng trong việc xoay chuyển dòng chiến tranh.',
+      it: 'Le insolite proprietà cristalline del Plurae lo rendono un materiale eccezionale. Sebbene il Plurae sia stata riscoperta solo di recente nelle profondità dimenticate del mondo, le applicazioni del Plurae sono già state sostanziali nel invertire le sorti della guerra.'
     }
   },
 
@@ -127,7 +137,8 @@ export default [
       hu: 'Az Aeter egykoron a múlt isteni uralkodóinak ajándékaként volt ismert, de az írott források szerint fekete meteoritokban lelhatő fel ez az anyag.',
       ru: 'Хоть королями и королевами прошлого Aeter когда-то и считался дарами богов, письменные истории утверждают, что ветер был найден внутри черных метеоритов.',
       de: 'Aether wurde früheren Königinnen und Königen als Geschenk der Götter angesehen, auch wenn schriftliche Überlieferungen behaupten, es sei in schwarzen Meteoriten gefunden worden.',
-      vi: 'Mặc dù Aeter từng được coi là quà tặng từ các vị thần bởi các nữ hoàng và các vị vua trong quá khứ, lịch sử bằng văn bản cho rằng Aeter đã được tìm thấy bên trong các thiên thạch đen.'
+      vi: 'Mặc dù Aeter từng được coi là quà tặng từ các vị thần bởi các nữ hoàng và các vị vua trong quá khứ, lịch sử bằng văn bản cho rằng Aeter đã được tìm thấy bên trong các thiên thạch đen.',
+      it: 'Sebbene gli Aeter una volta fossero considerati regali dagli dei alle regine e ai re del passato, storie scritte affermano che l\'Aeter fu trovato all\'interno di meteoriti neri.'
     }
   }
 ]
