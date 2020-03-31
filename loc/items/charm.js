432d0d9700544b575726df243c10e2da9e91c316
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-03-31 16:34:52 +0100

    Russian charm translations.

--- loc/items/charm.js
+++ loc/items/charm.js
@@ -9,25 +9,29 @@ export default [
   {
     name       : {$$: 'Hardened Egg'},
     description: {
-      en: 'Fossilized monster eggs are often interpreted as symbols of longevity. However, no written records of these creatures exist beyond recent times.'
+      en: 'Fossilized monster eggs are often interpreted as symbols of longevity. However, no written records of these creatures exist beyond recent times.',
+      Ru: 'Окаменевшие яйца монстров часто интерпретируются как символы долголетия. Однако письменных записей об этих существах больше не существует.'
     }
   },
   {
     name       : {$$: 'Tattooed Skull'},
     description: {
-      en: 'Orc tribes often keep the skulls of worthy foes as a sign of respect. This skull is adorned with tattoos that symbolize the power the foe had during life.'
+      en: 'Orc tribes often keep the skulls of worthy foes as a sign of respect. This skull is adorned with tattoos that symbolize the power the foe had during life.',
+      ru: 'Оркские племена часто держат черепа достойных врагов в знак уважения. Этот череп украшают татуировки, символизирующие силу, которой враг обладал в течение жизни.'
     }
   },
   {
     name       : {$$: 'Ship Pennant'},
     description: {
-      en: 'Flags flown from the tops of warships were called pennants. This ship pennant was among the wrecked warships that first traveled to Headless Landing.'
+      en: 'Flags flown from the tops of warships were called pennants. This ship pennant was among the wrecked warships that first traveled to Headless Landing.',
+      ru: 'Флаги, поднятые с высот военных кораблей, назывались вымпелами. Этот вымпел был среди разбитых военных кораблей, которые первыми отправились в Headless Landing.'
     }
   },
   {
     name       : {$$: 'Blue Marble'},
     description: {
-      en: 'Mages created alternative methods of mana acquisition during times of scarcity. This marble was created during the Arcane Crisis, a time when mana networks were under extreme stress.'
+      en: 'Mages created alternative methods of mana acquisition during times of scarcity. This marble was created during the Arcane Crisis, a time when mana networks were under extreme stress.',
+      ru: 'Магов часто создавали альтернативные методы приобретения маны во времена нехватки. Этот камень был создан во время Аrcane Crisis - времена, когда сети маны находились в состоянии крайнего стресса.'
     }
   }
 ]
