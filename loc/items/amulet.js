8a4fd6f70ee037ff5b2b10d51a03005467be7afc
Redly0n <41922752+Redly0n@users.noreply.github.com>
2020-04-17 19:33:42 +0200

    test

--- loc/items/amulet.js
+++ loc/items/amulet.js
@@ -3,6 +3,7 @@ export default [
     name       : {$$: 'Rotten Talisman'},
     description: {
       en: 'Burned ingredients have been twisted and tied together to form an amulet of meager protection. Creating simple amulets like this has been a common survival tactic since ancient times.',
+      sr: 'your translation in serbian',
       ru: 'Материалы собраны в лесу были сожженны и скрученные чтобы создать амулет слабой защиты. Такое искусство было популярной тактикой выжевания в древние времена.',
       hi: 'जले पदार्थों को तोड़ मरोड़ कर इस्स ताबीज़ को बनाया है। ऐसे ताबीज़ बनाना प्राचीन समय से ज़िंदा रहने की आम युक्ति रही है।',
       de: 'Verbrannte Zutaten wurden verdreht und zu einem Amulett des mageren Schutzes zusammengebunden. Die Herstellung einfacher Amulette wie dieses ist seit der Antike eine gängige Überlebenstaktik.',
