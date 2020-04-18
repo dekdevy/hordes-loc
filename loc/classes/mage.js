80e27cad6dd2c9d6e5c2298496e989510b4b9be0
Filip12408 <63874372+Filip12408@users.noreply.github.com>
2020-04-18 12:31:53 +0200

    Updated serbian translation
    Updated serbian translation to letters which native speakers will more easily understand

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -19,7 +19,7 @@ export default {
     bn: 'যাদুকর',
     vi: 'Pháp sư',
     cs: 'Mág',
-    sr: 'Čarobnjak',
+    sr: 'Чаробњак',
     it: 'Mago'
   },
 
@@ -42,7 +42,7 @@ export default {
     bn: 'যাদুকরেরা বড় মাপের এলাকায় ক্ষতিসাধন করতে পারে, একইসাথে তারা তাদের শত্রুদের বরফপূর্ণ যাদু দ্বারা ধীরগতিসম্পন্ন করতে পারে।',
     vi: 'Pháp sư gây sát thương trên một vùng rộng lớn, đồng thời cũng rất hữu dụng thông qua việc gây làm chậm các mục tiêu bằng các phép thuật băng của họ.',
     cs: 'Mágové dávají velké AoE poškození mezitím co nabízí zpomalení nepřátel s jejich ledovou magií.',
-    sr: 'Čarobnjaci nanose veliku štetu u velikoj meri u svom području dejstva, i istovremeno pružaju pomoć usporavajući mete svojom magijom mraza.',
+    sr: 'Чаробњаци наносе велику штету у великој мери у свом подручју дејства, и истовремено пружају помоћ успоравањем мете својом магијом мраза.',
     it: 'I Maghi infliggono enormi danni ad area, e assicurano una grande utilità rallentando i bersagli con i loro incantesimi glaciali.'
   }
 }
