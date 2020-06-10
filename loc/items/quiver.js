3e65a99043b9875b0722d299a6fd345c4fbe6022
Pisha99 <55296646+Pisha99@users.noreply.github.com>
2020-06-10 21:18:36 +0200

    Added ES rectifications

--- loc/items/quiver.js
+++ loc/items/quiver.js
@@ -36,7 +36,7 @@ export default [
       ru: 'Эти колчаны сделаны из змеиной чешуи из особых монстров. Обычно эти шкуры ищут после того, как гигантские змеи сбросили свою кожу; в противном случае сбор кожи будет иметь большой риск.',
       de: 'Diese Köcher werden aus den Schlangenschuppen spezieller Monster hergestellt. Typischerweiwird die Schlangenhaut eingesammelt nachdem riesige Schlangen sich gehäutet haben, andere Methoden wären ein hohes Risiko.',
       vi: 'Những ống tên này được làm từ vảy rắn từ những con quái vật đặc biệt. Thông thường, da rắn được tìm thấy sau khi những con se rắn khổng lồ lột da; để thử một bộ sưu tập da rắn nếu không sẽ có nguy cơ cao.',
-      es: 'Estos quivers estan hechos de escamas de serpiente de monstruos especiales. Por lo general, las pieles de serpiente se encuentran después de que las serpientes gigantes han mudado su piel;de lo contrario, la recolección de la piel seria un gran riesgo.',
+      es: 'Estos quivers estan hechos de escamas de serpiente de monstruos especiales. Por lo general, las pieles de serpiente se encuentran después de que las serpientes gigantes hayan mudado su piel; de lo contrario, la recolección de la piel sería un gran riesgo.',
       it: 'Queste faretre sono fatte con scaglie di serpenti di mostri speciali. In genere, le pelli di serpente si trovano dopo che i serpenti giganti hanno perso la pelle; tentare una collezione di pelle di serpente altrimenti sarebbe un rischio elevato.'
     }
   },
@@ -50,7 +50,7 @@ export default [
       ru: 'Изысканное мастерство Markay\'ak привело к созданию Reinforced Exemplars. Колчаны, подобные этому, часто являются свидетельством гордости любого профессионального лучника.',
       de: 'Die exquisite Handwerkskunst von Markay\'ak führte zur Entwicklung von verstärkten Exemplaren. Solche Köcher sind oft ein Zeichen des Stolzes für jeden professionellen Bogenschützen.',
       vi: 'Sự khéo léo tinh tế của Markay\'ak đã dẫn đến việc tạo ra các Reinforced Exemplars. Những ống tên như thế này thường là một minh chứng cho niềm tự hào của bất kỳ cung thủ chuyên nghiệp nào.',
-      es: 'La exquisita artesanía de Markay\'ak resultó en la creacion de Reinforced Exemplars. Estos quivers son a menudo un testamento del orgullo de cualquier arquero profesional.',
+      es: 'La exquisita artesanía de Markay\'ak resultó en la creación de Reinforced Exemplars. Estos Quivers son a menudo un testamento del orgullo de cualquier arquero profesional.',
       it: 'Lo squisito artigianato di Markay\'ak ha portato alla creazione di esemplari rinforzati. Faretra come questa sono spesso una testimonianza dell\'orgoglio di ogni arciere professionista.'
     }
   },
@@ -78,7 +78,7 @@ export default [
       ru: 'Этот колчан сделан из драконьей чешуи, найденной на земле. Чешуя дракона - очень сложный материал для изготовления, поэтому использовать её могут только лучшие мастера.',
       de: 'Dieser Köcher besteht aus Drachenschuppen, die auf dem Boden gefunden wurden. Drachenschuppen sind ein sehr schwieriges Handwerksmaterial, so dass nur die besten Handwerker sie nutzen können.',
       vi: 'Ống tên này được làm bằng vảy rồng được tìm thấy trên mặt đất. c, vì vậy chỉ những thợ thủ công giỏi nhất mới có thể sử dụng chúng.',
-      es: 'Este quiver esta hecho de escamas de dragón que se encuentran en el suelo. Las escamas de dragón son un material de fabricación muy difícil, por lo que solo los mejores artesanos pueden usarlas.',
+      es: 'Este quiver está hecho de escamas de dragón que se encuentran en el suelo. Las escamas de dragón son un material de fabricación muy difícil, por lo que sólo los mejores artesanos pueden usarlas.',
       it: 'Questa faretra è composta da squame di drago trovate sul terreno. Le squame di drago sono un materiale di fabbricazione molto difficile, quindi solo i migliori artigiani possono usarle.'
     }
   },
@@ -92,7 +92,7 @@ export default [
       ru: 'Lothariens - это колчан, названный в честь народа, вытесненного из жизни. Lothariens боролись за власть на близлежащих территориях, используя эти колчаны.',
       de: 'Lothariens sind Köcher, die nach einem Volk benannt sind, das aus der Welt getilgt wurde. Das Volk der Lotharien kämpfte gegen angrenzende Territorien um den Erwerb von Macht, während es diese Köcher benutzte.',
       vi: 'Lothariens là những ống tên được đặt theo tên của một bộ tộc bị biến mất. Người Lotharien đã chiến đấu với các vùng lãnh thổ xung quanh để giành quyền lực trong khi sử dụng những người bỏ cuộc này.',
-      es: 'Los Lothariens son quivers que llevan el nombre de un pueblo que ya no existe. La gente de Lotharien luchó contra los territorios circundantes para adquirir poder mientras usaba estos quivers.',
+      es: 'Los Lothariens son quivers que llevan el nombre de un pueblo que ya no existe. La gente de Lotharien luchó contra los territorios circundantes para adquirir poder mientras usaban estos quivers.',
       it: 'Le lothariens sono faretre che prendono il nome da un popolo guastato dalla vita. Il popolo Lotario combatté contro i territori circostanti per l\'acquisizione di potere mentre utilizzava queste faretre.'
     }
   },
@@ -120,7 +120,7 @@ export default [
       ru: 'Vodhrai являются воплощением мести, гнева, ярости и решимости. Необычайно сильные эмоции от этого живого колчана могут повредить обладателя.',
       de: 'Die Vodhrai sind die Verkörperung von Rache, Wut, Groll, Zorn und Entschlossenheit. Die ungewöhnlich starken Emotionen dieses lebenden Köcher könnten dazu dienen, den Besitzer zu verderben.',
       vi: 'Vodhrai là hiện thân của sự báo thù, giận dữ, phẫn nộ và quyết tâm. Những cảm xúc mạnh mẽ khác thường từ người run rẩy sống này có thể phục vụ cho người dùng tha hóa.',
-      es: 'Los Vodhrai son la encarnación de la venganza, la colera, la ira y la determinación. Las emociones inusualmente fuertes de este estremecimiento vivo podrían servir para corromper al portador.',
+      es: 'Los Vodhrai son la encarnación de la venganza, la cólera, la ira y la determinación. Las emociones inusualmente fuertes de este estremecimiento vivo podrían servir para corromper al portador.',
       it: 'I Vodhrai sono l\'incarnazione di vendetta, rabbia, ira e determinazione. Le emozioni insolitamente forti di questa faretra vivente potrebbero servire a corrompere l\'utente.'
     }
   },
