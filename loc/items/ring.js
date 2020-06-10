6149f293e5f83f50a5e76ba7a054f62db0d8d586
Pisha99 <55296646+Pisha99@users.noreply.github.com>
2020-06-10 21:15:26 +0200

    Added ES translation

--- loc/items/ring.js
+++ loc/items/ring.js
@@ -7,7 +7,8 @@ export default [
       ru: 'Эта странная повязка позволяет хранить небольшую часть жизни владельца.',
       de: 'Dieses seltsame Band erlaubt es dem Träger einen kleinen Teil der Lebensessenz zu speichern.',
       vi: 'Nhẫn kỳ lạ này cho phép một phần nhỏ tinh chất sự sống của người mang được lưu trữ.',
-      it: 'Questa strana fascia consente di conservare una piccola parte dell\'essenza vitale di chi la indossa.'
+      it: 'Questa strana fascia consente di conservare una piccola parte dell\'essenza vitale di chi la indossa.',
+      es: 'Esta extraña banda permite almacenar una pequeña parte de la esencia vital del usuario.'
     }
   },
 
@@ -19,7 +20,8 @@ export default [
       ru: 'Это кольцо изготовлено из специальной древесины, прочной как железо. Железная кора часто используется в качестве альтернативы металлу.',
       de: 'Dieser Ring wurde aus Holz, stark wie Eisen, hergestellt. Eisenholz wird oft als eine Alternative zu Metall genutzt.',
       vi: 'Chiếc nhẫn này được làm từ một loại gỗ đặc biệt cứng như sắt. Ironbark thường được sử dụng thay thế cho kim loại',
-      it: 'Questo anello è realizzato in un legno speciale resistente come il ferro. Ironbark è spesso usato come alternativa al metallo.'
+      it: 'Questo anello è realizzato in un legno speciale resistente come il ferro. Ironbark è spesso usato come alternativa al metallo.',
+      es: 'Este anillo está hecho de una madera especial que es tan resistente como el hierro. El Ironbark se usa a menudo como una alternativa al metal.'
     }
   },
 
@@ -31,7 +33,8 @@ export default [
       ru: 'Два королевства, которые присоединились королевским бракосочетаниям, носили эти кольца в качестве символа новых союзнических народов.',
       de: 'Zwei Königreiche, vereint durch Heirat, ließen ihre Armeen diese Ringe als Symbol für die Einheit der verbündeten Nationen tragen',
       vi: 'Hai vương quốc được liên kết bởi hôn nhân chính trị có quân đội của họ đeo những chiếc nhẫn này như một biểu tượng của các quốc gia mới được liên minh.',
-      it: 'Due regni a cui si unirono il matrimonio reale fecero indossare ai loro eserciti questi anelli come simbolo delle nazioni appena alleate.'
+      it: 'Due regni a cui si unirono il matrimonio reale fecero indossare ai loro eserciti questi anelli come simbolo delle nazioni appena alleate.',
+      es: 'Dos reinos a los que se unió el matrimonio real hicieron que sus ejércitos llevaran estos anillos como símbolo de las nuevas naciones aliadas.'
     }
   },
 
@@ -43,7 +46,8 @@ export default [
       ru: 'Это кольцо изготовлено из костей монстров шелухи. Кость монстра может быть использована как прочный материал, когда нет других ресурсов.',
       de: 'Dieser Ring wurde aus den Knochen eines riesigen Husks gefertigt. Monsterknochen sind ein stabiles Material, wenn kein anderes verfügbar ist.', // Husk is not translatable
       vi: 'Chiếc nhẫn này được làm từ xương của một con quái vật. Xương quái vật có thể được sử dụng như một vật liệu chắc chắn khi không có tài nguyên nào khác.',
-      it: 'Questo anello è composto dalle ossa dei gusci dei mostri. L\'osso dei mostri può essere usato come materiale robusto quando non sono disponibili altre risorse.'
+      it: 'Questo anello è composto dalle ossa dei gusci dei mostri. L\'osso dei mostri può essere usato come materiale robusto quando non sono disponibili altre risorse.',
+      es: 'Este anillo está hecho de los huesos de la cáscara de un monstruo. El hueso de monstruo puede usarse como material resistente cuando no hay otros recursos disponibles.'
     }
   },
 
@@ -55,7 +59,8 @@ export default [
       ru: 'В эльфийской традиции кольца часто передаются из поколения в поколение.',
       de: 'Ringe werden nach elfischer Tradition oft von Generation zu Generation weitergegeben.',
       vi: 'Nhẫn thường được lưu truyền từ thế hệ này sang thế hệ khác trong truyền thống Elvish.',
-      it: 'Gli anelli vengono spesso tramandati di generazione in generazione nella tradizione elfica.'
+      it: 'Gli anelli vengono spesso tramandati di generazione in generazione nella tradizione elfica.',
+      es: 'Los anillos a menudo se transmiten de generación en generación en la tradición élfica.'
     }
   },
 
@@ -67,7 +72,8 @@ export default [
       ru: 'Крошечные руны вырезаны на внутренней части кольца. Некоторые историки считают, что эти кольца были последними попытками потерянного королевства укрепить здоровье своего населения от болезней.',
       de: 'Dieser Ring besitzt auf der Innenseite winzige, geschnitzte Runen. Manche Historiker glauben das diese Ringe der letzte Versuch eines verloren Königreiches waren, seine Bevölkerung vor Krankheit zu schützen.',
       vi: 'Chiếc nhẫn này có những cỗ ngữ nhỏ được khắc vào dải bên trong. Một số nhà sử học tin rằng những chiếc nhẫn này là nỗ lực cuối cùng của một vương quốc đã mất để củng cố dân số khỏi bệnh tật.',
-      it: 'Questo anello ha piccole rune scolpite nella fascia interna. Alcuni storici ritengono che questi anelli siano stati l\'ultimo sforzo di un regno perduto per fortificare la sua popolazione dalle malattie.'
+      it: 'Questo anello ha piccole rune scolpite nella fascia interna. Alcuni storici ritengono che questi anelli siano stati l\'ultimo sforzo di un regno perduto per fortificare la sua popolazione dalle malattie.',
+      es: 'Este anillo tiene pequeñas runas talladas en la banda interior. Algunos historiadores creen que estos anillos fueron el último esfuerzo de un reino perdido para fortalecer a su población de la enfermedad.'
     }
   },
 
@@ -79,7 +85,8 @@ export default [
       ru: 'Arcane Ring- это магические эксперименты промежуточных магов. Некоторые кольца изготовлены и зачарованы, чтобы существенно повысить магические способности.',
       de: 'Arkane Ringe sind die magischen Experimente von fortgeschrittenen Magiern. Einige Ringe wurden hergestellt, um die Magie-Fähigkeiten signifikant zu erhöhen.',
       vi: 'Nhẫn Arcane là thí nghiệm ma thuật của các pháp sư. Một số chiếc nhẫn được chế tác và phù phép để tăng cường đáng kể khả năng thi triển phép thuật',
-      it: 'Gli anelli arcani sono gli esperimenti magici dei maghi intermedi. Alcuni anelli sono realizzati e incantati per migliorare sostanzialmente le abilità di lancio di magia.'
+      it: 'Gli anelli arcani sono gli esperimenti magici dei maghi intermedi. Alcuni anelli sono realizzati e incantati per migliorare sostanzialmente le abilità di lancio di magia.',
+      es: 'Los Arcane Rings son los experimentos mágicos de los magos intermedios. Algunos anillos están diseñados y encantados para mejorar sustancialmente las habilidades de lanzamiento de magia.'
     }
   },
 
@@ -91,7 +98,8 @@ export default [
       ru: 'Эти кольца когда-то принадлежали банде "Культ изумрудных пауков", которые одни из первых ездили на пауках.',
       de: 'Diese Ringe gehörten einst dem "Kult der Smaragdspinnen", einer Gruppe von Banditen, die als eine der ersten auf Spinnen ritten.',
       vi: 'Những chiếc nhẫn này từng thuộc về“Cult of the Emerald Spiders”, một nhóm cướp đầu tiên cưỡi nhện.',
-      it: 'Questi anelli un tempo appartenevano al "Culto dei ragni di smeraldo", una banda di banditi che furono tra i primi a cavalcare i ragni.'
+      it: 'Questi anelli un tempo appartenevano al "Culto dei ragni di smeraldo", una banda di banditi che furono tra i primi a cavalcare i ragni.',
+      es: 'Estos anillos pertenecieron una vez al "Culto de las arañas esmeralda", una banda de bandidos que fueron de los primeros en montar arañas.'
     }
   },
 
@@ -103,7 +111,8 @@ export default [
       ru: 'Infernal Ring - это подарок от дьявольских существ. Они соблазняют владельца желать больше силы. За этим желанием часто следуют неразумные предложения от большого количества дьяволов.',
       de: 'Höllenringe sind Geschenke von teuflischen Kreaturen. Sie verleiten den Träger, sich mehr Macht zu wünschen. Diesem Wunsch folgen oft Angebote von mehr Teufeln.',
       vi: 'Infernal rings là quà tặng từ các sinh vật quỷ dữ. Họ lôi kéo người mang nó mong muốn quyền lực hơn. Mong muốn này thường được theo sau bởi những lời đề nghị thiếu sáng suốt của nhiều ác quỷ.',
-      it: 'Gli anelli infernali sono doni di creature diaboliche. Attirano chi lo indossa a desiderare più potere. Questo desiderio è spesso seguito da offerte sconsiderate da più diavoli.'
+      it: 'Gli anelli infernali sono doni di creature diaboliche. Attirano chi lo indossa a desiderare più potere. Questo desiderio è spesso seguito da offerte sconsiderate da più diavoli.',
+      es: 'Los anillos infernales son regalos de criaturas diabólicas. Atraen al usuario a desear más poder. Este deseo a menudo es seguido por ofertas desacertadas de más demonios.'
     }
   },
 
@@ -115,7 +124,8 @@ export default [
       ru: 'Природные духи изначально дарили эти кольца в знак благодарности первобытным монстрам. Многие из этих колец были украдены или потеряны за тысячи лет.',
       de: 'Naturgeister gaben diese Ringe als Anerkennungsgeschenk an Ur-Monster. Über tausende von Jahren gingen viele dieser Ringe verloren oder wurden gestohlen.',
       vi: 'Nature spirits ban đầu đã tặng những chiếc nhẫn này như một món quà đánh giá cao những con quái vật nguyên thủy. Nhiều chiếc nhẫn trong số này đã bị đánh cắp hoặc mất trong hàng ngàn năm.',
-      it: 'Gli spiriti della natura originariamente davano questi anelli come doni di apprezzamento per i mostri primordiali. Molti di questi anelli sono stati rubati o persi nel corso di migliaia di anni.'
+      it: 'Gli spiriti della natura originariamente davano questi anelli come doni di apprezzamento per i mostri primordiali. Molti di questi anelli sono stati rubati o persi nel corso di migliaia di anni.',
+      es: 'Los espíritus de la naturaleza originalmente dieron estos anillos como regalos de agradecimiento a los monstruos primitivos. Muchos de estos anillos han sido robados o perdidos durante miles de años.'
     }
   },
 
@@ -127,7 +137,8 @@ export default [
       ru: 'Многие религиозные фракции утверждают, что люди, носящие это кольцо, имеют благосклонность к божеству; однако конкретное божество не известно. Многие религиозные деятели считают, что божественная анонимность носит преднамеренный характер.',
       de: 'Viele religiöse Gruppen behaupten das Personen, die diesen Ring tragen den Segen eines Gottes erhalten. Welcher Gott ist jedoch unbekannt. Viele religiöse Figuren glauben, das diese Anonymität Absicht ist.',
       vi: 'Nhiều phe phái tôn giáo cho rằng các cá nhân đeo chiếc nhẫn này có sự ưu ái của một vị thần; tuy nhiên, vị thần cụ thể không được biết đến. Nhiều nhân vật tôn giáo tin rằng sự ẩn danh của Thiên Chúa là có chủ ý',
-      it: 'Molte fazioni religiose affermano che alle persone che indossano questo anello viene concesso il favore di una divinità; tuttavia, la divinità specifica non è nota. Molte figure religiose credono che l\'anonimato divino sia intenzionale.'
+      it: 'Molte fazioni religiose affermano che alle persone che indossano questo anello viene concesso il favore di una divinità; tuttavia, la divinità specifica non è nota. Molte figure religiose credono che l\'anonimato divino sia intenzionale.',
+      es: 'Muchas facciones religiosas afirman que las personas que usan este anillo tienen el favor de una deidad; sin embargo, la deidad específica no se conoce. Muchos mascarones religiosos creen que el anonimato divino es intencional.'
     }
   },
 
@@ -139,7 +150,8 @@ export default [
       ru: 'Древние свитки утверждают, что эти легендарные кольца делают владельца почти непобедимым. В свитках также упоминаются истории о том, как носители становятся сломлены и глубоко несчастными.',
       de: 'Uralte Schriften behaupten das diese legendären Ringe den Träger fast unverwundbar machen. Die Schriften also berichten von Trägern die korrumpiert und unglücklich wurden.',
       vi: 'Các cuộn giấy cổ xưa cho rằng những chiếc nhẫn huyền thoại này khiến người đeo gần như bất khả chiến bại. Các cuộn sách cũng đề cập đến những câu chuyện về người mặc trở nên tha hóa và vô cùng bất hạnh.',
-      it: 'Le antiche pergamene affermano che questi anelli leggendari rendono chi lo indossa quasi invincibile. I rotoli menzionano anche storie di portatori che diventano corrotti e profondamente infelici.'
+      it: 'Le antiche pergamene affermano che questi anelli leggendari rendono chi lo indossa quasi invincibile. I rotoli menzionano anche storie di portatori che diventano corrotti e profondamente infelici.',
+      es: 'Los antiguos pergaminos afirman que estos anillos legendarios hacen que el usuario sea casi invencible. Los pergaminos también mencionan historias de usuarios que se vuelven corruptos y profundamente infelices.'
     }
   },
 
@@ -151,7 +163,8 @@ export default [
       ru: 'Один раз за век, находят Peacekeeper и отмечают его носителя как особого защитника царств. В прошлом веке носительница Peacekeeper потерпела неудачу, и началась Third Great War.',
       de: 'Einmal in jedem Zeitalter wird ein Peacekeeper gefunden und markiert den Träger als besonderen Beschützer der Reiche. Im letzten Zeitalter scheiterte der Ringträger und der große dritte Krieg begann.',
       vi: 'Mỗi thời đại, một Peacekeeper  được tìm thấy và đánh dấu người mặc là người bảo vệ đặc biệt của các cõi. Trong một thời đại đã qua, Peacekeeper đã thất bại và Đại chiến thứ ba bắt đầu.',
-      it: 'Una volta ogni età, viene trovato un Peacekeeper che segna chi lo indossa come uno speciale protettore dei regni. In un\'epoca passata, il portatore del Peacekeeper fallì e iniziò la Terza Grande Guerra.'
+      it: 'Una volta ogni età, viene trovato un Peacekeeper che segna chi lo indossa come uno speciale protettore dei regni. In un\'epoca passata, il portatore del Peacekeeper fallì e iniziò la Terza Grande Guerra.',
+      es: 'Una vez por época, se encuentra un Pacificador y marca al usuario como un protector especial de los reinos. En una época pasada, el portador del Pacificador falló y comenzó la Tercera Gran Guerra.'
     }
   }
 
