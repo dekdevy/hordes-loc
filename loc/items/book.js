086e6a83f640e1d2ddf2d5b14594225d64c09fee
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2020-09-21 17:44:53 +0100

    Update book.js

--- loc/items/book.js
+++ loc/items/book.js
@@ -314,7 +314,7 @@ export default [{
   name: {$$: 'Mount Riding'},
 
   description: {
-    en: 'Allows you to ride ground mounts.'
+    en: 'Allows you to ride ground mounts. Mounts are bound to your account.'
   }
 },
 {
