117f8a4680fe8fdfe80c26ae8ec3b4bda0629ab9
Peter Mikola <mikopet@gmail.com>
2020-09-25 19:09:20 +0200

    add HU translation for charms

--- loc/items/charm.js
+++ loc/items/charm.js
@@ -6,7 +6,8 @@ export default [
       fr: 'Les cloches sont souvent utilisées comme symbole de dévotion envers une divinité choisie. L\'intérieur de cette cloche porte une inscription en argent incrustée, mais la traduction de l\'inscription a été perdue au fil du temps.',
       ru: 'Колокола часто используются как символ преданности избранному богу. Внутри этого колокола есть серебряная надпись, но перевод этой надписи потерян во времени.',
       it: 'Le campane sono spesso usate come simbolo di devozione verso una divinità scelta. L\'interno di questa campana ha un\'iscrizione d\'argento intarsiata, ma la traduzione dell\'iscrizione è andata perduta nel tempo.',
-      ro: 'Clopotele sunt deseori văzute ca pe un simbol al dedicării față de o zeitate aleasă. Interiorul acestui clopot are o inscripție de argint gravată, dar traducerea acesteia a fost pierdută în timp.'
+      ro: 'Clopotele sunt deseori văzute ca pe un simbol al dedicării față de o zeitate aleasă. Interiorul acestui clopot are o inscripție de argint gravată, dar traducerea acesteia a fost pierdută în timp.',
+      hu: 'A kis harangokot gyakran használják a lelkesedés szimbólumaiként egy választott istenség felé. Ennek a csengőnek ezüsttel berakott felirat van a belsején, de a fordítás már rég az enyészeté.'
     }
   },
   {
@@ -15,7 +16,8 @@ export default [
       en: 'Fossilized monster eggs are often interpreted as symbols of longevity. However, no written records of these creatures exist beyond recent times.',
       fr: 'Les œufs de monstres fossilisés sont souvent interprétés comme des symboles de longévité. Cependant, il n\'existe aucune trace écrite de ces créatures au-delà de l\'époque récente.',
       ru: 'Окаменевшие яйца монстров часто интерпретируются как символы долголетия. Однако письменных записей об этих существах больше не существует.',
-      ro: 'Ouăle de monstru fosilizate sunt deseori considerate un simbol al longevității. În orice caz, nu există dovezi scrise că aceste creaturi ar fi existat și în timpuri de demult.'
+      ro: 'Ouăle de monstru fosilizate sunt deseori considerate un simbol al longevității. În orice caz, nu există dovezi scrise că aceste creaturi ar fi existat și în timpuri de demult.',
+      hu: 'A megkövült szörnytojásokat gyakran a hosszú élet szimbólumaiként értelmezik. Másfelől viszont, nincsen írásos bizonyíték ezen lények létezésére a régmúltban.'
     }
   },
   {
@@ -24,7 +26,8 @@ export default [
       en: 'Orc tribes often keep the skulls of worthy foes as a sign of respect. This skull is adorned with tattoos that symbolize the power the foe had during life.',
       fr: 'Les tribus orques gardent souvent le crâne de leurs dignes ennemis en signe de respect. Ce crâne est orné de tatouages qui symbolisent le pouvoir que possédait l\'ennemi de son vivant.',
       ru: 'Оркские племена часто держат черепа достойных врагов в знак уважения. Этот череп украшают татуировки, символизирующие силу, которой враг обладал в течение жизни.',
-      ro: 'Triburile de orci deseori păstrează craniile celor vretnici ca pe un semn de respect. Acest craniu este decorat cu tatuaje ce simbolizează puterea pe care individul a avut-o în timpul vieții.'
+      ro: 'Triburile de orci deseori păstrează craniile celor vretnici ca pe un semn de respect. Acest craniu este decorat cu tatuaje ce simbolizează puterea pe care individul a avut-o în timpul vieții.',
+      hu: 'Ork törzsek gyakran tartják meg méltó ellenfeleik koponyáit a tisztelet jeléül. Ez a tetoválásokkal díszített koponya szimbolizálja az ellenfél életében valaha volt erejét.'
     }
   },
   {
@@ -33,7 +36,8 @@ export default [
       en: 'Flags flown from the tops of warships were called pennants. This ship pennant was among the wrecked warships that first traveled to Headless Landing.',
       fr: 'Les drapeaux qui flottaient au sommet des navires de guerre étaient appelés fanions. Ce fanion figurait parmi les navires de guerre naufragés qui se sont rendus pour la première fois à Headless Landing.',
       ru: 'Флаги, поднятые с высот военных кораблей, назывались вымпелами. Этот вымпел был среди разбитых военных кораблей, которые первыми отправились в Headless Landing.',
-      ro: 'Steagurile atârnate în vârfurile navelor se numeau fanioane. Această navă fanion a fost printre primele nave de război care au călătorit spre Headless Landing.'
+      ro: 'Steagurile atârnate în vârfurile navelor se numeau fanioane. Această navă fanion a fost printre primele nave de război care au călătorit spre Headless Landing.',
+      hu: 'A hadihajók tetején lebegő zászlókat lobogóknak hívjuk. Ez a lobogó az első Headless Landingre utazott hajók roncsai között volt.'
     }
   },
   {
@@ -42,7 +46,8 @@ export default [
       en: 'Mages created alternative methods of mana acquisition during times of scarcity. This marble was created during the Arcane Crisis, a time when mana networks were under extreme stress.',
       fr: 'Les mages ont créé des méthodes alternatives pour acquérir du mana pendant les périodes de pénurie. Cette bille a été créée pendant la crise des Arcanes, une période où les réseaux de mana étaient soumis à un stress extrême.',
       ru: 'Магов часто создавали альтернативные методы приобретения маны во времена нехватки. Этот камень был создан во время Аrcane Crisis - времена, когда сети маны находились в состоянии крайнего стресса.',
-      ro: 'Magicienii au creat metode alternative de a achiziționa mană în timpul sărăciei. Această mărgea a fost creată în timpul Arcane Crisis, un timp în care rețelele de mană erau supuse unui stres extrem.'
+      ro: 'Magicienii au creat metode alternative de a achiziționa mană în timpul sărăciei. Această mărgea a fost creată în timpul Arcane Crisis, un timp în care rețelele de mană erau supuse unui stres extrem.',
+      hu: 'Mágusok alternatív módjait készítették el a mana beszerzésének hiány esetén. Ez az üveggolyó a Arcane Crisis idején készült, akkor mikor a mana hálózatok extrém terhelés alatt voltak.'
     }
   }
 ]
