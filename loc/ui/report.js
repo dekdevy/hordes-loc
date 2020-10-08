e4c10bdd24d131905998e4ca057d6eacc6161508
dek <polydek@googlemail.com>
2020-10-08 16:51:03 +0200

    basic report descriptions

--- loc/ui/report.js
+++ loc/ui/report.js
@@ -4,8 +4,16 @@ export default {
     {en: 'Offensive Chat'},
     {en: 'Multiboxing'},
     {en: 'Scamming'},
-    {en: 'Cheating / Hacking'},
+    {en: 'Cheating / Exploiting'},
     {en: 'Offensive Name'},
     {en: 'Botting'}
+  ],
+  info: [
+    { en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages, a single report will suffice.' },
+    { en: 'Playing multiple characters at the same time to gain unfair advantages by abusing game mechanics such as item find stacking, pre-buffing, healbotting or other PVE / PVP activities.' },
+    { en: 'Tricking players into dropping items or falsely advertising ingame services with the sole intention of stealing someones wealth or items.' },
+    { en: 'Using cheating tools for unfair advantages, or abusing severe bugs that bypass the regular gameplay for unintended advantages.' },
+    { en: 'Names containing offensive language, letters to obfuscate identity or impersonate staff members.' },
+    { en: 'Using automated tools to play the game while not present, or using tools to automate actions for unfair advantages.' }
   ]
 }
