d328c3e0985445cb8885b8aaf37d9c7b2d78bdd5
dek <polydek@googlemail.com>
2020-10-08 19:50:00 +0200

    Update report.js

--- loc/ui/report.js
+++ loc/ui/report.js
@@ -9,11 +9,11 @@ export default {
     {en: 'Botting'}
   ],
   info: [
-    { en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages, a single report will suffice.' },
+    { en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, NSFW, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages; a single report will suffice.' },
     { en: 'Playing multiple characters at the same time to gain unfair advantages by abusing game mechanics such as item find stacking, pre-buffing, healbotting or other PVE / PVP activities.' },
     { en: 'Tricking players into dropping items or falsely advertising ingame services with the sole intention of stealing someones wealth or items.' },
-    { en: 'Using cheating tools for unfair advantages, or abusing severe bugs that bypass the regular gameplay for unintended advantages.' },
-    { en: 'Names containing offensive language, letters to obfuscate identity or impersonate staff members.' },
-    { en: 'Using automated tools to play the game while not present, or using tools to automate actions for unfair advantages.' }
+    { en: 'Bypassing regular gameplay by using cheating tools for unfair advantages or abusing severe game bugs that give unintended advantages.' },
+    { en: 'Names containing offensive language, letters to obfuscate identity, or impersonation of staff members.' },
+    { en: 'Using automated tools to play the game while not being present, or using tools to automate actions for unfair advantages.' }
   ]
 }
