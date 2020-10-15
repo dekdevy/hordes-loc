ed14d56c50555ff0c62957f1cb3761b2158ee785
Pedro Roccon <pedro@pedroroccon.com.br>
2020-10-15 12:08:37 +0000

    Add pt support to items/rune

--- loc/items/rune.js
+++ loc/items/rune.js
@@ -9,7 +9,8 @@ export default [
       de: 'Dieses flache Material findet sich oft in Minen. Monsters bauen es gerne ab, um ihrer Wirtschaft zu helfen.',
       vi: 'Vật liệu phẳng này thường được tìm thấy trong các mỏ; quái vật thường khai thác Lucid để hỗ trợ nền kinh tế của họ.',
       it: 'Questo materiale piatto si trova comunemente nelle miniere; i mostri spesso minano per i Lucid per sostenere le loro economie.',
-      es: 'Este material plano se encuentra comúnmente en minas; los monstruos a menudo extraen a Lucid para apoyar sus economías.'
+      es: 'Este material plano se encuentra comúnmente en minas; los monstruos a menudo extraen a Lucid para apoyar sus economías.',
+      pt: 'Este material plano geralmente é encontrado em minas; monstros frequentemente procuram o Lucid para sustentar suas economias.'
     }
   },
 
@@ -23,7 +24,8 @@ export default [
       de: 'Die Kurven von Melant wirken wie Schrift oder Symbole, aber Schmiede glauben, dass diese natürlichen Formationen eine starke innere Struktur aufweisen.',
       vi: 'Các đường cong của Melant mang đến sự xuất hiện của chữ viết hoặc biểu tượng, nhưng thợ rèn tin rằng những thành phần tự nhiên này cho thấy một cấu trúc bên trong mạnh mẽ.',
       it: 'Le curve di Melant hanno la forma di parole o simboli, ma i fabbri credono che queste formazioni naturali rivelino una potente struttura interna.',
-      es: 'Las curvas de Melant dan la apariencia de escritura o símbolos, pero los herreros creen que estas formaciones naturales revelan una poderosa estructura interna.'
+      es: 'Las curvas de Melant dan la apariencia de escritura o símbolos, pero los herreros creen que estas formaciones naturales revelan una poderosa estructura interna.',
+      pt: 'As curvas de Melant dão a aparência de escrita ou símbolos, mas os ferreiros acreditam que essas formações naturais revelam uma poderosa estrutura interna.'
     }
   },
 
@@ -37,7 +39,8 @@ export default [
       de: 'Dieses nützliche Material stammt von einem großen Vulkan und seiner Umgebung. Turim wurde früher rege zwischen Karawanen gehandelt.',
       vi: 'Vật liệu hữu ích này bắt nguồn từ một ngọn núi lửa lớn và khu vực xung quanh. Turim đã từng được giao dịch thường xuyên giữa các đoàn lữ hành.',
       it: 'Questo materiale utile proviene da un grande vulcano e dalla regione circostante. Un tempo Turim veniva scambiato frequentemente tra le carovane.',
-      es: 'Este útil material proviene de un gran volcán y su región circundante. El Turim fue solía ser intercambiado con frecuencia entre caravanas.'
+      es: 'Este útil material proviene de un gran volcán y su región circundante. El Turim fue solía ser intercambiado con frecuencia entre caravanas.',
+      pt: 'Este material útil se origina de um grande vulcão e da região circundante. Os turim costumavam ser trocados com frequência entre caravanas.'
     }
   },
 
@@ -51,7 +54,8 @@ export default [
       de: 'Vor kurzem wurde festgestellt das dieses robuste Material nützlich ist um Upgrades vorzunehmen, als Schmiede mit dem "wertlosen Fundo" experimentierten. Seitdem wird es als wertvoll betrachtet.',
       vi: 'Vật liệu mạnh mẽ này gần đây đã được tìm thấy là hữu ích để nâng cấp khi thợ rèn bắt đầu thử nghiệm "Fundo vô giá trị". Kể từ đó, Fundo đã được coi là một nguồn tài nguyên có giá trị.',
       it: 'Questo materiale robusto è stato recentemente trovato utile per gli aggiornamenti quando i fabbri hanno iniziato a sperimentare "Fundo senza valore". Da quel momento, Fundo è stata considerata una risorsa preziosa.', //I'm not sure about that
-      es: 'Recientemente se descubrió que este material robusto era útil para mejoras cuando los herreros comenzaron a experimentar con "Fundo sin valor". Desde entonces, Fundo ha sido considerado un recurso valioso.'
+      es: 'Recientemente se descubrió que este material robusto era útil para mejoras cuando los herreros comenzaron a experimentar con "Fundo sin valor". Desde entonces, Fundo ha sido considerado un recurso valioso.',
+      pt: 'Recentemente, descobriu-se que este material robusto era útil para atualizações quando os ferreiros começaram a fazer experiências com "fundo sem valor". Desde então, o Fundo tem sido considerado um recurso valioso.'
     }
   },
 
@@ -65,7 +69,8 @@ export default [
       de: 'Amari wird häufig als Geschenk aus Übersee-Königreichen genutzt. Gelehrte sagen das Amari entdeckt wurde als eine Spezies von Seekreaturen ersten Kontakt mit verschiedenen Landkönigreichen hatte und Amari vom Meeresboden mitbrachten.',
       vi: 'Amari thường được tặng làm quà từ các vương quốc ngoài biển. Các học giả nói rằng Amari lần đầu tiên được phát hiện khi một chủng tộc sinh vật biển tiếp xúc lần đầu với một số vương quốc trên đất liền và đưa Amari từ đáy biển.',
       it: 'Gli amari sono spesso dati in dono da regni d\'oltremare. Gli studiosi affermano che gli Amari furono scoperti per la prima volta quando una razza di creature marine prese il primo contatto con diversi regni terrestri e portò gli Amari dal fondo del mare.',
-      es: 'Los Amari a menudo se dan como regalos de reinos en el extranjero. Los estudiosos dicen que el Amari se descubrió por primera vez cuando una raza de criaturas marinas hizo el primer contacto con varios reinos terrestres y trajeron Amari del fondo del mar.'
+      es: 'Los Amari a menudo se dan como regalos de reinos en el extranjero. Los estudiosos dicen que el Amari se descubrió por primera vez cuando una raza de criaturas marinas hizo el primer contacto con varios reinos terrestres y trajeron Amari del fondo del mar.',
+      pt: 'Os Amari costumam ser dados como presentes de reinos estrangeiros. Estudiosos dizem que Amari foi descoberto pela primeira vez quando uma raça de criaturas marinhas fez contato pela primeira vez com vários reinos terrestres e trouxe Amari do fundo do mar.'
     }
   },
 
@@ -79,7 +84,8 @@ export default [
       de: 'Oft mit Gold verwechselt oder für Drachenschuppen gehalten. Viele uralte Dekorationen und Waffen sind mit Purum verziert.',
       vi: 'Thường bị nhầm với vàng, vật liệu này từng được cho là cùng chất liệu với vảy rồng. Nhiều đồ trang trí và vũ khí cổ xưa được trang trí bằng Purum.',
       it: 'Spesso scambiato per oro, un tempo si pensava che questo materiale fosse lo stesso materiale delle squame di drago. Molte antiche decorazioni e armi sono adornate con il Purum.',
-      es: 'A menudo confundido con oro, este material alguna vez se pensó que era del mismo material que las escamas de dragón. Muchas decoraciones y armas antiguas están adornadas con Purum.'
+      es: 'A menudo confundido con oro, este material alguna vez se pensó que era del mismo material que las escamas de dragón. Muchas decoraciones y armas antiguas están adornadas con Purum.',
+      pt: 'Muitas vezes confundido com ouro, este material já foi considerado o mesmo material das escamas de dragão. Muitas decorações e armas antigas são adornadas com Purum.'
     }
   },
 
@@ -93,7 +99,8 @@ export default [
       de: 'Royals werden oft von Königen und Königinnen gesammelt und besessen, um als zeremonielles Gift bei Zwangsheiraten zu dienen.',
       vi: 'Royals  thường được sở hữu và thu thập bởi các vị vua và hoàng hậu để được sử dụng làm quà tặng trong nghi lễ trong các cuộc hôn nhân sắp đặt.',
       it: 'I Royals sono spesso posseduti e raccolti da re e regine per essere usati come regali cerimoniali durante i matrimoni combinati.',
-      es: 'A menudo, los Royals son apropiados y coleccionados por reyes y reinas para ser utilizados como regalos ceremoniales durante matrimonios concertados.'
+      es: 'A menudo, los Royals son apropiados y coleccionados por reyes y reinas para ser utilizados como regalos ceremoniales durante matrimonios concertados.',
+      pt: 'Royals costumam ser propriedade e recolhidos por reis e rainhas para serem usados como presentes cerimoniais durante casamentos arranjados.'
     }
   },
 
@@ -107,7 +114,8 @@ export default [
       de: 'Diese Formationen kommen oft natürliche nahe Yggdrasil, dem Weltenbaum, vor. Die Schönheit und seltene Kraft von Tara ist sowohl für juwelenverzierten Schmuck als auch für Item-Upgrades geeignet.',
       vi: 'Những thành phần này thường xuất hiện ở gần Yggdrasil, World Tree. Vẻ đẹp và sự hiếm có của Tara khiến chúng phù hợp như nhau cho cả đồ trang sức gia truyền hoặc nâng cấp vật phẩm.',
       it: 'Queste formazioni si verificano naturalmente molto spesso vicino a Yggdrasil, l\'albero del mondo. La bellezza e la rarità del potere di Tara li rendono ugualmente adatti sia per i cimeli di gioielli che per gli aggiornamenti degli oggetti.',
-      es: 'Estas formaciones ocurren naturalmente con mayor frecuencia cerca de Yggdrasil, el Árbol del Mundo. La belleza y la rareza del poder de Tara los hacen igualmente adecuados para reliquias de joyas o mejoras de items.'
+      es: 'Estas formaciones ocurren naturalmente con mayor frecuencia cerca de Yggdrasil, el Árbol del Mundo. La belleza y la rareza del poder de Tara los hacen igualmente adecuados para reliquias de joyas o mejoras de items.',
+      pt: 'Essas formações ocorrem naturalmente com mais frequência perto de Yggdrasil, a Árvore do Mundo. A beleza e a raridade do poder de Tara os tornam igualmente adequados para relíquias de joias ou atualizações de itens.'
     }
   },
 
@@ -121,7 +129,8 @@ export default [
       de: 'Nachdem die geheimen Methoden der Ausrüstungs-Verbesserung mit Gloom entdeckt wurden, wurden diese Techniken schnell an ein gegnerisches Königreich verkauft. Dies stellt einen der größten Verratsfälle in der Geschichte der Reihe dar.',
       vi: 'Một khi các phương pháp tăng cường bí mật sử dụng sức mạnh của Gloom đã được phát hiện, những phương pháp đó đã sớm được bán cho một vương quốc đối địch như một trong những sự phản bội lớn nhất trong lịch sử của các vương quốc.',
       it: 'Una volta scoperti i metodi segreti per potenziare l\'equipaggiamento usando il potere del Gloom, quei metodi furono presto venduti a un regno opposto come uno dei più grandi tradimenti nella storia dei regni.',
-      es: 'Una vez que se descubrieron los métodos secretos para mejorar el equipo utilizando el poder de Gloom, esos métodos se vendieron en breve a un reino contrario como una de las mayores traiciones en la historia de los reinos.'
+      es: 'Una vez que se descubrieron los métodos secretos para mejorar el equipo utilizando el poder de Gloom, esos métodos se vendieron en breve a un reino contrario como una de las mayores traiciones en la historia de los reinos.',
+      pt: 'Uma vez que os métodos secretos de aumentar o equipamento usando o poder das Trevas foram descobertos, esses métodos foram vendidos em breve a um reino oposto como uma das maiores traições da história dos reinos.'
     }
   },
 
@@ -135,7 +144,8 @@ export default [
       de: 'Plurae ist ein außergewöhnliches Material aufgrund seiner ungewöhnlichen Kristallstrukturen. Obwohl Plurae erst vor kurzem in den vergessenen Tiefen der Welt entdeckt wurde, ist sein Einsatz schon für eine Wende des Kriegsglücks verantwortlich gewesen.',
       vi: 'Tính chất kết tinh bất thường trong Plurae làm cho nó trở thành một vật liệu đặc biệt. Mặc dù Plurae chỉ mới được khám phá gần đây ở vực thẳm bị lãng quên trên thế giới, nhưng các ứng dụng của Plurae đã rất quan trọng trong việc xoay chuyển dòng chiến tranh.',
       it: 'Le insolite proprietà cristalline del Plurae lo rendono un materiale eccezionale. Sebbene il Plurae sia stata riscoperta solo di recente nelle profondità dimenticate del mondo, le applicazioni del Plurae sono già state sostanziali nel invertire le sorti della guerra.',
-      es: 'Propiedades cristalinas inusuales del Plurae lo convierten en un material excepcional. Aunque el Plurae fue redescubierto recientemente en las profundidades olvidadas del mundo, el uso del Plurae ya han sido importantes para cambiar el rumbo de la guerra.'
+      es: 'Propiedades cristalinas inusuales del Plurae lo convierten en un material excepcional. Aunque el Plurae fue redescubierto recientemente en las profundidades olvidadas del mundo, el uso del Plurae ya han sido importantes para cambiar el rumbo de la guerra.',
+      pt: 'Propriedades cristalinas incomuns em Plurae tornam-no um material excepcional. Embora o Plurae tenha sido redescoberto apenas recentemente nas profundezas esquecidas do mundo, as aplicações do Plurae já foram substanciais para virar a maré da guerra.'
     }
   },
 
@@ -149,7 +159,8 @@ export default [
       de: 'Aether wurde früheren Königinnen und Königen als Geschenk der Götter angesehen, auch wenn schriftliche Überlieferungen behaupten, es sei in schwarzen Meteoriten gefunden worden.',
       vi: 'Mặc dù Aeter từng được coi là quà tặng từ các vị thần bởi các nữ hoàng và các vị vua trong quá khứ, lịch sử bằng văn bản cho rằng Aeter đã được tìm thấy bên trong các thiên thạch đen.',
       it: 'Sebbene gli Aeter una volta fossero considerati regali dagli dei alle regine e ai re del passato, storie scritte affermano che l\'Aeter fu trovato all\'interno di meteoriti neri.',
-      es: 'Si bien las reinas y reyes del pasado consideraron el Aeter como regalos de los dioses, las historias escritas afirman que el Aeter se encontró dentro de meteoritos negros.'
+      es: 'Si bien las reinas y reyes del pasado consideraron el Aeter como regalos de los dioses, las historias escritas afirman que el Aeter se encontró dentro de meteoritos negros.',
+      pt: 'Embora Aeter já tenha sido considerado um presente dos deuses por rainhas e reis do passado, histórias escritas afirmam que Aeter foi encontrado dentro de meteoritos negros.'
     }
   }
 ]
