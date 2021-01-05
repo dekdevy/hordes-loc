8630547fdb6b930aad1c8a03f5c489f017badd8d
karasugai <77003092+karasugai@users.noreply.github.com>
2021-01-05 22:12:06 +0900

    Update archer.js

--- loc/classes/archer.js
+++ loc/classes/archer.js
@@ -47,6 +47,6 @@ export default {
     cs: 'Lučišník dává vysoké poškození na jednoho hráče a AoE poškození v rychlých intervalech. Dobrý lučišník si vždy bude držet odstup a bude střílet nepřátele z povzdálí.',
     sr: 'Стрелци су изузетно способни да концентришу своју моћ на једну мету и могу да делују на више противника делимично.Добар стрелаље из далека.',
     it: 'Gli Arcieri infliggono un danno elevato ad un singolo bersaglio e un danno esplosivo ad area. Un buon arciere mantiene sempre le distanze e colpisce i nemici da lontano.'
-    ja: '射手は単体の敵に対して高火力を発揮し、戦況に爆発的な影響を与えます。上手な射手は常に敵との距離を保ち、遠くから狙い撃ちします。'
+    ja: '射手は単体の敵に対して高火力を発揮し、戦況に爆発的な影響を与えます。上手な射手は常に敵との距離を保ち、遠くから狙い撃ちにします。'
   }
 }
