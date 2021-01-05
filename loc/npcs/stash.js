51d1814590145ab27df03b0b810fa95522787ffb
karasugai <77003092+karasugai@users.noreply.github.com>
2021-01-06 00:43:39 +0900

    Update stash.js

--- loc/npcs/stash.js
+++ loc/npcs/stash.js
@@ -15,7 +15,7 @@ export default {
       pl: 'Tak, jestem gadającą skrzynią, już widzę pytania formujące się na twoich ustach. Od wielu lat służę rodzinom królewskim, przechowując ich bezcenne klejnoty i drogocenności, tylko po to, żeby umieścić mnie w tym brudnym obozowisku bym zaspokajała twoje śmieszne potrzeby. Cóż, czy chciałbyś abym zaoferowała ci moje usługi?',
       bn: 'হ্যাঁ, আমি কথা বলা বাক্স, আমি ইতিমধ্যে আপনার ঠোঁটে থাকা প্রশ্নটি দেখতে পাচ্ছি। আমি বহু বছর রাজপরিবারের সেবা করেছি, তাদের অমূল্য রত্ন ও জহরত সংরক্ষণ করেছি, কেবলমাত্র নিজেদের ক্ষুদ্র প্রয়োজনগুলি পূরণ করার জন্য আমাকে এই নোংরা শিবিরে রাখা হয়েছে। যাইহোক, আপনি কি চান আমি আপনাকে আমার পরিষেবাগুলি অফার করি?',
       it: 'Sì, sono una cassa parlante, posso già vedere la domanda formarsi sulle tue labbra. Per molti anni ho servito famiglie reali, conservando le loro inestimabili gemme e gioielli, solo per me essere collocato in questo sudicio campeggio per soddisfare le vostre esigenze meschine. Ad ogni modo, vorresti che ti offrissi i miei servizi?',
-      ja: 'やぁ、僕はおしゃべりチェストさ。何か言いたさげだね？前は王室に仕えていて、お偉いさんの宝玉だの宝石だのを匿ってたんだけど、何だかんだあって君のちゃっちいニーズにお応えする為に小汚いキャンプに置かれる始末さ。まぁそれは置いといて、早速使ってみるかい？'
+      ja: 'やぁ、僕はおしゃべりチェストさ。あぁ質問は却下させて貰うよ。前は王室に仕えていて、お偉いさんの宝玉だの宝石だのを匿ってたんだけど、何だかんだあって君のちゃっちいニーズにお応えする為に小汚いキャンプに置かれる始末さ。まぁそれは置いといて、早速使ってみるかい？'
     },
 
     // Do not translate "Stash".
