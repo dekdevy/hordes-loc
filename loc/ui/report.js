6440574a92ed5548e37b19ff7cf13ccada9ebe3b
mdl <49496108+drifted-mdl@users.noreply.github.com>
2021-02-02 21:56:30 +0000

    Update report.js

--- loc/ui/report.js
+++ loc/ui/report.js
@@ -1,12 +1,12 @@
 // Work in progress, do not translate
 export default {
   reasons: [
-    { en: 'Offensive Chat', ru: 'Оскорбления в чате' },
-    { en: 'Multiboxing', ru: 'Множественные аккаунты' },
-    { en: 'Scamming', ru: 'Мошенничество' },
-    { en: 'Cheating / Exploiting', ru: 'Читы / Взлом' },
-    { en: 'Offensive Name',  ru: 'Оскорбительное имя' },
-    { en: 'Botting', ru: 'Бот' }
+    { en: 'Offensive Chat'},
+    { en: 'Multiboxing'},
+    { en: 'Scamming'},
+    { en: 'Cheating / Exploiting'},
+    { en: 'Offensive Name'},
+    { en: 'Botting'}
   ],
   info: [
     { en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, NSFW, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages; a single report will suffice.' },
