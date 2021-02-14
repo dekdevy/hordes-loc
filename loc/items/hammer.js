25def357eefab4e0911d0e87f4384036cbd1f3ae
mdl <49496108+drifted-mdl@users.noreply.github.com>
2021-02-14 14:13:28 +0000

    Dutch hammers.

--- loc/items/hammer.js
+++ loc/items/hammer.js
@@ -10,7 +10,8 @@ export default [
       vi: 'Đây là một vũ khí tạm thời thường được tạo ra bởi nông dân',
       es: 'Esta es un arma improvisada que a menudo es creada por los granjeros.',
       it: 'Questa è un\'arma improvvisata che viene spesso creata dagli agricoltori.',
-      ro: 'Această este o armă improvizată care este de obicei creată de fermieri.'
+      ro: 'Această este o armă improvizată care este de obicei creată de fermieri.',
+      nl: 'Dit in elkaar gezette wapen wordt vooral gemaakt door boeren.'
     }
   },
 
@@ -25,7 +26,8 @@ export default [
       vi: 'Đây là một cái vồ thường được sử dụng làm mộc; tuy nhiên, vồ gỗ thường được sử dụng để chiến đáua khi tài nguyên và vật liệu khan hiếm.',
       es: 'Este es un mazo normalmente usado para carpintería; sin embargo, los mazos de madera a menudo se usan para combatir por necesidad cuando los recursos y materiales son escasos.',
       it: 'Questo è un martello normalmente usato per la carpenteria; tuttavia, le mazze di legno sono spesso usate per combattere per necessità quando risorse e materiali sono scarsi.',
-      ro: 'Acesta este un ciocan de obicei folosit la dulgherie; în orice caz, ciocanele din lemn sunt des utilizate în luptă, din necesitate, când resursele și materialele nu sunt suficiente.'
+      ro: 'Acesta este un ciocan de obicei folosit la dulgherie; în orice caz, ciocanele din lemn sunt des utilizate în luptă, din necesitate, când resursele și materialele nu sunt suficiente.',
+      nl: 'Dit is een hamer die normaal gebruikt wordt voor timmerwerk, maar wooden mallets worden ook af en toe gebruikt als wapens wanneer materialen schaars zijn.'
     }
   },
 
@@ -40,7 +42,8 @@ export default [
       vi: 'Chùy nguyên thủy được thử nghiệm chiến đấu và có những huyền thoại chất đống trên chúng; tuy nhiên, hiệu quả của chúng đã giảm dần theo thời gian',
       es: 'Las Primal maces son probadas en batalla y tienen leyendas sobre ellas; sin embargo, su efectividad ha disminuido con el tiempo.',
       it: 'Le mazze primordiali sono testate in battaglia e hanno delle leggende ammucchiate; tuttavia, la loro efficacia è diminuita nel tempo.',
-      ro: 'Primal Maces sunt testate în luptă si au legende îngrămădite asupra lor; totuși, efectul lor a scăzut cu timpul.'
+      ro: 'Primal Maces sunt testate în luptă si au legende îngrămădite asupra lor; totuși, efectul lor a scăzut cu timpul.',
+      nl: 'Primal maces zijn gevecht getest en hebben legendes op hun afgekregen, maar hun efficiëntie is verergerd na verloop van tijd.'
     }
   },
 
@@ -55,7 +58,8 @@ export default [
       vi: 'Đây là một công cụ ưa thích của Orc để tra khảo và chiến đấu',
       es: 'Esta es la herramienta preferida de los orcos para los interrogatorios y las batallas.',
       it: 'Questo è lo strumento preferito dagli orchi per gli interrogatori e le battaglie.',
-      ro: 'Aceasta este o uneltă preferată de orci pentru interogări și bătălii.'
+      ro: 'Aceasta este o uneltă preferată de orci pentru interogări și bătălii.',
+      nl: 'Dit is een orc z\'n voor gekeurde gereedschap voor onderhandelingen tijdens gevechten.'
     }
   },
 
@@ -70,7 +74,8 @@ export default [
       vi: 'Các chùy nặng được tạo ra cho chiến trận, xâm lược và dụ hoặc',
       es: 'Las mazas pesadas están hechas para la guerra, la infiltración y el carisma.',
       it: 'Mazze pesanti sono fatte per la guerra, l\'infiltrazione e il carisma.',
-      ro: 'Heavy Maces sunt făcute pentru război, infiltrare, și carismă.'
+      ro: 'Heavy Maces sunt făcute pentru război, infiltrare, și carismă.',
+      nl: 'Heavy Maces zijn gemaakt voor oorlog, infiltratie en charisma.'
     }
   },
 
@@ -85,7 +90,8 @@ export default [
       vi: 'Bashers sắt thường được thực hiện bởi các thợ thủ công chuyên gia như quái vật Markay\'ak. Thợ thủ công hầu như luôn để lại dấu ấn của mình trên tấm sắt như một biểu tượng của niềm tự hào.',
       es: 'Los Iron bashers son hechos típicamente por artesanos expertos como el monstruo Markay \'ak. Los artesanos casi siempre dejan su marca en el hierro como símbolo de orgullo.',
       it: 'I basher di ferro sono in genere realizzati da esperti artigiani come il mostro Markay \'ak. Gli artigiani lasciano quasi sempre il segno sul ferro come simbolo di orgoglio.',
-      ro: 'Iron Bashers sunt de obicei făcute de un meșter expert ca monstrul Markay\'ak. Meșterii aproape de fiecare dată lasă semnătura lor pe fier ca un simbol de mândrie.'
+      ro: 'Iron Bashers sunt de obicei făcute de un meșter expert ca monstrul Markay\'ak. Meșterii aproape de fiecare dată lasă semnătura lor pe fier ca un simbol de mândrie.',
+      nl: 'Iron bashers zijn normaal gesproken gemaakt door deskundige vakmensen zoals de monster Markay\'ak. Handhavers laten bijna altijd hun sporen na op het ijzer als een symbool van trots. '
     }
   },
 
@@ -100,7 +106,8 @@ export default [
       vi: 'Những chùy lớn được làm bằng một kim loại đặc biệt có màu sắc và tính chất riêng biệt. Các bí mật của Vật chất tối này đã bị đánh cắp từ các thợ thủ công quái vật và trở nên phổ biến.',
       es: 'Estos mazos están hechos con un metal especial que tiene distintos colores y propiedades. Los secretos de este metal oscuro fueron robados de monstruos artesanos y popularizados.',
       it: 'Questi mauls sono realizzati con un metallo speciale che ha colori e proprietà distinti. I segreti di questo metallo scuro furono rubati da artigiani mostruosi e resi popolari.',
-      ro: 'Aceste ciocane sunt făcute dintr-un metal special care are culori și proprietăți distincte. Secretele acestui metal întunecat au fost furate de la un monstru meșteșugar și apoi popularizat.'
+      ro: 'Aceste ciocane sunt făcute dintr-un metal special care are culori și proprietăți distincte. Secretele acestui metal întunecat au fost furate de la un monstru meșteșugar și apoi popularizat.',
+      nl: 'Deze knotsen zijn gemaakt van een speciaal metaal dat unieke kleuren en eigenschappen bezit. De geheimen van dit darkmetal was gestolen van een monster handhaver en gepopulariseerd.'
     }
   },
 
@@ -115,7 +122,8 @@ export default [
       vi: 'Những búa nhỏ này được ban phước bởi một nhà thờ. Thần linh đôi khi có được sức mạnh thông qua sự tận tâm, thành tích và sự hy sinh của chủ sở hữu trước đó.',
       es: 'Estos martillos son bendecidos por una iglesia. Los Divine gavels a veces adquieren poder a través de la devoción, el logro y el sacrificio de los propietarios anteriores.',
       it: 'Questi martelletti sono benedetti da una chiesa. I martelletti divini a volte acquisiscono potere attraverso la devozione, la realizzazione e il sacrificio dei precedenti proprietari.',
-      ro: 'Aceste ciocănele sunt binecuvântate de o biserică. Ciocănelele divine dobândesc putere prin devotare, realizări, și sacrificiul proprietarului precedent.'
+      ro: 'Aceste ciocănele sunt binecuvântate de o biserică. Ciocănelele divine dobândesc putere prin devotare, realizări, și sacrificiul proprietarului precedent.',
+      nl: 'Deze voorzittershamers zijn gezegend door een kerk. Goddelijke voorzittershamers krijgen soms kracht door de doorzetting, prestaties en opofferingen van de vorige eigenaren.'
     }
   },
 
@@ -130,7 +138,8 @@ export default [
       vi: 'Những chiếc búa như thế này thường được tạo ra bởi những thợ rèn nổi tiếng như Markay\'ak với sự giúp đỡ của các linh mục quái vật. Búa thần thánh đôi khi được ban cho sức mạnh thiêng liêng.',
       es: 'Los martillos como este a menudo son creados por herreros famosos como Markay\'ak con la ayuda de sacerdotes religiosos monstruos. Los Hallowed hammers a veces están dotados de poderes sagrados.',
       it: 'Martelli come questo sono spesso creati da famosi fabbri come Markay\'ak con l\'aiuto di mostri religiosi. I martelli sacri sono talvolta dotati di poteri sacri.',
-      ro: 'Astfel de ciocane sunt deseori create de fierari faimoși precum Markay\'ak cu ajutorul preoților monștrii religioși. Ciocanele sfințite sunt uneori înzestrate cu putere sacră.'
+      ro: 'Astfel de ciocane sunt deseori create de fierari faimoși precum Markay\'ak cu ajutorul preoților monștrii religioși. Ciocanele sfințite sunt uneori înzestrate cu putere sacră.',
+      nl: 'Hamers zoals deze zijn vaak gemaakt door bekende smids zoals Markay’ak met behulp van religieuze monster priesters. Hallowed Hammers zijn soms voorzien van heilige krachten.'
     }
   },
 
@@ -145,7 +154,8 @@ export default [
       vi: 'Dwarven mauls đủ mạnh và đủ dai để chống chọi với cuộc sống của Dwarven trong hầm mỏ và mặt đất. Thợ rèn người lùn có kinh nghiệm rộng lớn trong việc làm búa.',
       es: 'Los Dwarven mauls son lo suficientemente robustos y poderosos como para resistir la vida de los enanos en las minas y el suelo. Los herreros enanos tienen una vasta experiencia en la fabricación de martillos.',
       it: 'I maul nanici sono abbastanza robusti e potenti da resistere alla vita nanica nelle miniere e nel terreno. I fabbri nani hanno una vasta esperienza nella fabbricazione di martelli.',
-      ro: 'Ciocanele gnomilor sunt suficient de robuste și puternice pentru a rezista unei vieți de gnom în mine și în pământ. Fierarii gnomi au o experiență vastă în crearea ciocanelor.'
+      ro: 'Ciocanele gnomilor sunt suficient de robuste și puternice pentru a rezista unei vieți de gnom în mine și în pământ. Fierarii gnomi au o experiență vastă în crearea ciocanelor.',
+      nl: 'Dwarven Mauls zijn robuust en krachtig genoeg om het leven van een dwerg te weerstaan in de mijnen en de grond. Dwerg smids hebben uitgebreide ervaring in het maken van hamers.'
     }
   },
 
@@ -160,7 +170,8 @@ export default [
       vi: 'Những búa nhỏ này được làm từ một kim loại đặc biệt và được rèn mà không cần nhiệt. Quá trình rèn mà không cần nhiệt cho phép các đặc tính bẩm sinh của kim loại đặc biệt vẫn còn.',
       es: 'Estos mazos están hechos de un metal especial y forjados sin calor. El proceso de forjado sin calor permite que permanezcan las propiedades innatas del metal especial.',
       it: 'Questi martelletti sono realizzati in un metallo speciale e forgiati senza calore. Il processo di forgiatura senza calore consente alle proprietà innate del metallo speciale di rimanere.',
-      ro: 'Aceste ciocane sunt făcute dintr-un metal special forjat fără căldură. Procesul forjării fără căldură permite proprietăților ereditate a metalului special să se mențină.'
+      ro: 'Aceste ciocane sunt făcute dintr-un metal special forjat fără căldură. Procesul forjării fără căldură permite proprietăților ereditate a metalului special să se mențină.',
+      nl: 'Deze voorzittershamers zijn gemaakt van een speciaal metaal en gesmeed zonder hitte. Het proces van het smeden zonder hitte zorgt ervoor dat de aangeboren eigenschappen van het speciale metaal behouden wordt.'
     }
   },
 
@@ -175,7 +186,8 @@ export default [
       vi: 'Búa Skullshatterer ban đầu được tạo ra để chống lại xác sống. Những cái búa còn lại thường được coi là tài sản quý giá của nhiều giáo sĩ và những sinh vật linh thiêng.',
       es: 'Los martillos Skullshatterer fueron creados originalmente para combatir a los muertos vivientes. Los martillos que quedan a menudo son considerados posesiones preciadas por muchos clérigos y seres sagrados.',
       it: 'I martelli Skullshatterer furono originariamente creati per combattere i non morti. I martelli che restano sono spesso considerati beni preziosi da molti chierici ed esseri sacri.',
-      ro: 'Ciocanele Skullshatterer au fost original create pentru a combate morții vii. Aceste ciocane care rămân sunt deseori considerate posesiuni apreciate de multe clerici și ființe sacre.'
+      ro: 'Ciocanele Skullshatterer au fost original create pentru a combate morții vii. Aceste ciocane care rămân sunt deseori considerate posesiuni apreciate de multe clerici și ființe sacre.',
+      nl: 'Skullshatterer hamers werden eerst gecreëerd om de ondoden te bevechten. De hamers die overblijven zijn vaak gewaardeerde bezittingen van vele geestelijke en heilige wezens.'
     }
   },
 
@@ -190,7 +202,8 @@ export default [
       vi: 'Những cái búa này được biết đến với việc phá vỡ đầu đe trong quá trình rèn. Chúng hiếm khi được tạo ra và thường có một mức giá đáng kể.',
       es: 'Estos martillos son conocidos por romper cabezas de yunque durante el proceso de forja. Raramente se crean y generalmente alcanzan un precio considerable.',
       it: 'Questi martelli sono noti per rompere le teste dell\'incudine durante il processo di forgiatura. Sono creati raramente e in genere richiedono un prezzo considerevole.',
-      ro: 'Aceste ciocane sunt cunoscute pentru a rupe capuri de nicovale în timpul procesului de forjare. Ele sunt rar create și aduc în mod tipic și prețuri substanțiale.'
+      ro: 'Aceste ciocane sunt cunoscute pentru a rupe capuri de nicovale în timpul procesului de forjare. Ele sunt rar create și aduc în mod tipic și prețuri substanțiale.',
+      nl: 'Deze hamers zijn bekend voor het breken vaan aanbeelden tijdens het proces van smeden. Zij zijn zelden gecreëerd en zijn hebben normaal gesproken een substantiële prijs.'
     }
   },
 
@@ -205,7 +218,8 @@ export default [
       vi: 'Các bậc thầy tâm linh thường truyền bá thông điệp về tâm linh của họ theo nhiều cách khác nhau. Cây búa này có sức mạnh ban tặng món quà hòa bình cho kẻ thù - bằng cách này hay cách khác.',
       es: 'Los maestros espirituales a menudo difunden el mensaje de su espiritualidad de varias maneras. Este martillo tiene el poder de dar el regalo de la paz a los enemigos, de una forma u otra.',
       it: 'I maestri spirituali spesso diffondono il messaggio della loro spiritualità in vari modi. Questo martello ha il potere di dare il dono della pace ai nemici - in un modo o nell\'altro.',
-      ro: 'Maeștrii spirituali deseori răspândesc mesajul spiritualității lor într-o mulțime de feluri. Acest ciocan are puterea de a dărui pace inamicilor — într-un fel sau altul.'
+      ro: 'Maeștrii spirituali deseori răspândesc mesajul spiritualității lor într-o mulțime de feluri. Acest ciocan are puterea de a dărui pace inamicilor — într-un fel sau altul.',
+      nl: 'Spirituele meesters verspreiden vaak het bericht van hun spirituele variatie van manieren. Deze hamer heeft de kracht om het geschenk van vrede te brengen naar de tegenstanders – op een manier of een andere.'
     }
   },
 
@@ -220,7 +234,8 @@ export default [
       vi: 'Những chiếc búa này được sở hữu và sử dụng bởi các lãnh chúa quái vật titanic sau khi hình thành ban đầu của Great Barrier. Những người khổng lồ này đã phá hủy đất đai và cai trị các vương quốc trong một thời gian',
       es: 'Estos martillos fueron poseídos y utilizados por los señores de los monstruos titánicos después de la formación inicial de la Gran Barrera. Estos titanes destruyeron la tierra y gobernaron los reinos por un tiempo.',
       it: 'Questi martelli erano usati e di proprietà dei signori dei mostri titanici dopo la formazione iniziale della Grande Barriera. Questi titani distrussero la terra e governarono i regni per un po.',
-      ro: 'Aceste ciocane au fost deținute și utilizate de monștrii suzerani titani după formarea inițială a "Great Barrier". Acești titani au distrus pământul și au condus peste tărâmuri pentru o perioadă de timp.'
+      ro: 'Aceste ciocane au fost deținute și utilizate de monștrii suzerani titani după formarea inițială a "Great Barrier". Acești titani au distrus pământul și au condus peste tărâmuri pentru o perioadă de timp.',
+      nl: 'Deze hamers waren eigendom en werden gebruikt door titanische monster overheersers na de initiële vorming van de Great Barrier. Deze titanen vernielden het land en heersten over de rijken voor een bepaalde tijd.'
     }
   },
 
@@ -235,7 +250,8 @@ export default [
       vi: 'Theo các nhà sử học, cây búa này được cho là đã từng gây ra một trận động đất đủ lớn để lật đổ một thành phố.',
       es: 'Según los historiadores, se dice que este martillo causó un terremoto lo suficientemente grande como para derribar una ciudad.',
       it: 'Secondo gli storici, questo martello avrebbe causato una volta un terremoto abbastanza grande da rovesciare una città.',
-      ro: 'Conform istoricilor, acest ciocan se presupune că a cauzat cândva un cutremur suficient de mare pentru a răsturna un oraș întreg.'
+      ro: 'Conform istoricilor, acest ciocan se presupune că a cauzat cândva un cutremur suficient de mare pentru a răsturna un oraș întreg.',
+      nl: 'Volgens historici wordt gezegd dat deze hamer ooit een aarbeving heeft laten ontstaan wat krachtig genoeg was om een stad gelijk met de grond te maken.'
     }
   },
 
@@ -250,7 +266,8 @@ export default [
       vi: 'Một truyền thuyết cổ xưa cho rằng một cây búa kỳ lạ rơi qua khe nứt Great Barrier vào cõi âm; những cái búa như thế này đơn giản không phải của thế giới này',
       es: 'Una antigua leyenda dice que un extraño martillo cayó a través de una fisura de la Gran Barrera en los reinos; martillos como este simplemente no son de este mundo.',
       it: 'Un\'antica leggenda afferma che uno strano martello cadde attraverso una fenditura della Grande Barriera nei regni; martelli come questo non sono semplicemente di questo mondo.',
-      ro: 'O legendă antică pretinde că un ciocan misterios a căzut dintr-o fisură a "Great Barrier" pe tărâmuri; astfel de ciocane pur și simplu nu sunt din acestă lume.'
+      ro: 'O legendă antică pretinde că un ciocan misterios a căzut dintr-o fisură a "Great Barrier" pe tărâmuri; astfel de ciocane pur și simplu nu sunt din acestă lume.',
+      nl: 'Een eeuwenoude legende beweert dat een vreemde hamer door een spleet in de Great Barrier de rijken in viel. Hamers zoals deze zijn simpelweg niet van deze wereld.'
     }
   }
 
