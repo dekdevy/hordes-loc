5f765cdf8ad24ba575d75cc4188144c3c126ef07
Redly0n <41922752+Redly0n@users.noreply.github.com>
2021-03-01 12:20:50 +0100

    Added french translation tutorial.js

--- loc/ui/tutorial.js
+++ loc/ui/tutorial.js
@@ -20,25 +20,25 @@ export default {
       en: 'To prepare you for combat, I will explain the UI elements.'
       // it: 'Per prepararti al combattimento ti mostrerò i vari menu.'
       // pt: 'Para preparar você para o combate, eu vou te explicar os elementos da interface do usuário (UI).'
-      // fr: 'Pour vous préparer au combat, je vais vous expliquer les éléments de l'interface utilisateur (UI).'
+      // fr: 'Pour vous préparer au combat, je vais vous expliquer les éléments de l\'interface utilisateur (UI).'
     },
     {
       en: 'This is your unit frame. You can see your own health and mana here.'
       // it: 'Da qui puoi vedere la tua salute e il tuo mana.'
       // pt: 'Nesse quadro, você pode ver informações da sua mana e vida.'
-      // fr: 'Ici, vous pouvez voir votre vie et votre mana.'
+      // fr: 'Voici votre cadre d\'unité. Vous pouvez y voir votre vie et votre mana.'
     },
     {
       en: 'Move forward and find a monster, then target it by clicking it with your mouse or pressing <kbd>TAB</kbd>.'
       // it: 'Vai avanti e trova un mostro, quindi aggancialo facendo clic con il mouse o premendo <kbd>TAB</kbd>.'
       // pt: 'Mova-se para frente e encontre algum mosntro, então selecione-o para o combate clicando com o mouse, ou pressionando <kbd>TAB</kbd>.'
-      // fr: 'Avancez et trouvez un monstre, puis ciblez-le en lui cliquant dessus avec votre souris ou en appuyant sur <kbd>TAB</kbd>'
+      // fr: 'Avancez et trouvez un monstre, puis ciblez-le en lui cliquant dessus avec votre souris ou en appuyant sur <kbd>TAB</kbd>.'
     },
     {
       en: 'This monster is now your target. Any spells you cast will be directed at your target.'
       // it: 'Adesso il mostro è agganciato, Qualunque attacco che lancierai sarà diretto verso il mostro.'
       // pt: 'Esse monstro agora é seu alvo. Qualquer magia que você conjurar irá ser direcionada para o seu alvo.'
-      // fr: 'Ce monstre est maintenant votre cible. Tout sort que vous lancerez sera maintenant dirigé vers lui.'
+      // fr: 'Ce monstre est maintenant votre cible. Tout sort que vous lancerez sera dirigé vers lui.'
     },
     {
       en: 'You can attack by pressing the keys on your skillbar. Press <kbd>1</kbd> on your keyboard to attack the monster!'
@@ -56,13 +56,13 @@ export default {
       en: 'Nice job. You have been rewarded with experience points.'
       // it: 'Bel lavoro. Sarai ricompensato con dei punti esperienza.'
       // pt: 'Ótimo trabalho. Você foi recompensado com pontos de experiência.'
-      // fr: 'Beau travail. Vous avez été récompensé avec des points d'expériences.'
+      // fr: 'Beau travail. Vous avez été récompensé avec des points d\'expériences.'
     },
     {
       en: 'Collect enough experience points and you will level up.'
       // it: 'Raccogli abbastanza punti esperienza e salirai di livello.'
       // pt: 'Colete pontos de experiência suficientes e você irá subir de nível.'
-      // fr: 'Collectez assez d'expérience et vous montrez de niveau.'
+      // fr: 'Collectez assez d\'expérience et vous montrez de niveau.'
     },
     {
       en: 'You can press <kbd>Shift</kbd> to see items and coins on the ground. Pick up an item! '
@@ -80,7 +80,7 @@ export default {
       en: 'Here you can see your items. The inventory size depends on the bag you have equipped.'
       // it: 'Qui puoi vedere i tuoi oggetti. Il numero di slot disponibili dipende dalla borsa che hai equipaggiato.'
       // pt: 'Aqui você pode vender os seus itens. O tamanho do seu inventário depende do tamanho da mochila que você tem equipada'
-      // fr: 'Ici vous pouvez voir vos items. La taille de l'inventaire dépend du sac que vous avez équipé.'
+      // fr: 'Ici vous pouvez voir vos items. La taille de l\'inventaire dépend du sac que vous avez équipé.'
     },
     {
       en: 'You can equip items by right clicking them and selecting "Equip item".'
@@ -116,13 +116,13 @@ export default {
       en: 'Stat points can be used to increase your primary stats. Hover over the buttons to preview their effects, then use your stat points.'
       // it: 'I punti stat possono essere utilizzati per aumentare le tue statistiche primarie. Passa il mouse sui pulsanti per visualizzare in anteprima i loro effetti, quindi usa i tuoi punti stat.'
       // pt: 'Os pontos de stat podem ser utilizado para aumentar os seus stats primários. Passe o mouse em cima dos botões para obter uma prévia dos seus efeitos, então use os pontos de stats.'
-      // fr: 'Les points stat peuvent être utilisés pour augmenter vos stats primaires. Passez votre souris au dessus des bouttons pour visualiser leurs effets, puis utilisez vos points stat.'
+      // fr: 'Les points stat peuvent être utilisés pour augmenter vos stats primaires. Passez votre souris au dessus des boutons pour visualiser leurs effets, puis utilisez vos points stat.'
     },
     {
       en: 'Next, we will teach you how to learn new skills. First, level up to level 3 to unlock a new skill.'
       // it: 'Adesso ti spiegherò come imparare nuove abilità. Primo, raggiungi il livello 3.'
       // pt: 'Seguindo, nós vamos ensiar como aprender novas habilidades. Primeiro, aumente o seu nível até o 3 para desbloquear uma nova habilidade.'
-      // fr: 'À présent, nous allons vous enseigner comment apprendre de nouvelles compétences. Tout d'abord, atteignez le niveau 3 pour débloquer une nouvelle compétence.'
+      // fr: 'À présent, nous allons vous enseigner comment apprendre de nouvelles compétences. Tout d\'abord, atteignez le niveau 3 pour débloquer une nouvelle compétence.'
     },
     {
       en: 'A new skill is available. You need to use books to learn new skills.'
@@ -146,13 +146,13 @@ export default {
       en: 'The trader sells skill books, potions, and mounts. Right-click the book to buy it.'
       // it: 'Il commerciante vende libri abilità, pozioni e armature. Click-Destro sul libro per comprarlo.'
       // pt: 'O vendedor vende livros de habilidades, potes e montarias. Clique com o botão direito do mouse no livro para comprá-lo.'
-      // fr: 'Le Trader vend des livre de compétence, et des montures. Faites un clic-droit sur le livre pour l'acheter.'
+      // fr: 'Le Trader vend des livres de compétences, et des montures. Faites un clic-droit sur le livre pour l\'acheter.'
     },
     {
       en: 'Once you have the book, open your inventory, right-click the book and press <kbd>Learn skill</kbd> to learn it.'
       // it: 'Adesso che hai un libro, apri il tuo inventario, Click-Destro sul libro e premi <kbd>Impara abilità</kbd> per impararlo.'
       // pt: 'Agora que você tem o livro, abra o seu inventário, clique com o botão direito e pressione <kbd>Learn skill</kbd> para aprender a nova habilidade.'
-      // fr: 'Une fois que vous avez le livre, ouvrez votre inventaire, faites un clic-droit sur le livre et cliquez sur <kbd>Learn skill</kbd> pour l'apprendre.'
+      // fr: 'Une fois que vous avez le livre, ouvrez votre inventaire, faites un clic-droit sur le livre et cliquez sur <kbd>Learn skill</kbd> pour l\'apprendre.'
     },
     {
       en: 'Close the Trader Window, then open your skill panel (<kbd>K</kbd>) to see an overview of your skills.'
@@ -170,7 +170,7 @@ export default {
       en: 'You can click and drag a skill into your skillbar. We already did it for you!'
       // it: 'Puoi fare clic e trascinare le abilità nella barra abilità. Fatto solo per te!'
       // pt: 'Você pode clicar e arrastas as habilidades na sua barra de habilidades. Nós já fizemos isso para você!'
-      // fr: 'Vous pouvez cliquer sur un icône de compétence et le faire glisser dans votre barre de compétence. Nous l'avons déjà fait pour vous !'
+      // fr: 'Vous pouvez cliquer sur un icône de compétence et le faire glisser dans votre barre de compétence. Nous l\'avons déjà fait pour vous !'
     },
     {
       en: 'You have learned a lot. You\'re almost ready to start your adventure.'
@@ -182,19 +182,19 @@ export default {
       en: 'Hordes is all about working in a party with other players. A party can help you level up.'
       // it: 'Hordes significa lavorare in un party con altri giocatori. Un party può aiutarti a salire di livello.'
       // pt: 'Hordes fica melhor quando trabalhamos em conjunto em uma party com outros jogadores. Uma party pode te ajudar a aumentar de nível.'
-      // fr: 'Hordes consiste en grande partie à travailler dans un groupe avec d'autres joueurs. Un groupe peut vous aider à monter de niveau.'
+      // fr: 'Hordes consiste en grande partie à travailler dans un groupe avec d\'autres joueurs. Un groupe peut vous aider à monter de niveau.'
     },
     {
       en: 'A party of players will share experience and items, allowing them to progress faster than solo players.'
       // it: 'Un gruppo di giocatori condividerà esperienza e oggetti, consentendo loro di progredire più velocemente rispetto ai giocatori solitari.'
       // pt: 'Uma party de jogadores irá dividir toda a experiência e itens, permitindo que todos progridam de maneira mais rápido do que individualmente.'
-      // fr: 'Un groupe de joueurs va partager l'expérience et les items, leurs permettant de progresser plus rapidement que les joueurs seul.'
+      // fr: 'Un groupe de joueurs va partager l\'expérience et les items, permettant aux joueurs en groupe de progresser plus rapidement que les joueurs seuls.'
     },
     {
       en: 'You can create or find a party with the party interface. Click on <kbd>Find Party</kbd> to automatically search one.'
       // it: 'Puoi creare o trovare un party con la relativa interfaccia. Clicca on <kbd>Trova Party</kbd> per cercarne uno.'
       // pt: 'Você pode criar ou encontrar uma party com a interface da party. Clique em <kbd>Find Party</kbd> para encontrar uma party automaticamente.'
-      // fr: 'Vous pouvez créer ou trouver un groupe avec l'interface de groupe. Cliquez sur <kbd>Find Party</kbd> pour en chercher un automatiquement.'
+      // fr: 'Vous pouvez créer ou trouver un groupe avec l\'interface de groupe. Cliquez sur <kbd>Find Party</kbd> pour en chercher un automatiquement.'
     },
     {
       en: 'You can also invite players to your party by right-clicking on their unitframe, or using the command <kbd>/partyinvite</kbd>.'
@@ -212,25 +212,25 @@ export default {
       en: 'Players of the other faction can be killed to earn fame and items (such as the crown) for your character.'
       // it: 'I giocatori della fazione avversaria possono essere uccisi per guadagnare fama e oggetti (come la corona) per il tuo personaggio.'
       // pt: 'Jogadores da outra facção podem ser mortos para ganhar fama e itens (como uma coroa) para o seu personagem.'
-      // fr: 'Les joueurs de la faction adverse peuvent être tués pour gagner de la Gloire et des items (comme les couronnes) pour votre personnage.'
+      // fr: 'Les joueurs de la faction adverse peuvent être tués pour gagner des points de Gloire et des items (comme les couronnes) pour votre personnage.'
     },
     {
       en: 'You can also sell items to other players. You can visit the Merchant to do so.'
       // it: 'Puoi anche vendere oggetti ai giocatori. Puoi visitare il Mercante per farlo.'
       // pt: 'Você também pode vender itens para outros jogadores. Visite o mercador para fazer a sua venda.'
-      // fr: 'Vous pouvez aussi vendre des items aux autres joueurs. Vous pouvez parler au Merchant pour faire cela.'
+      // fr: 'Vous pouvez aussi vendre des items aux autres joueurs. Pour cela vous devez parler au Merchant.'
     },
     {
       en: 'Any items you buy at the merchant will be sent to your stash - a storage for your items and gold. It is a brown box located next to the merchant.'
       // it: 'Tutti gli oggetti che acquisti dal mercante verranno inviati alla tua scorta, un deposito per i tuoi oggetti e monete. È una scatola marrone situata accanto al mercante.'
       // pt: 'Qualquer item que você comprar no mercador será enviado para o seu baú - um local de armazenamento de itens e ouro. O baú é uma caixa marron localizada próxima ao mercador.'
-      // fr: 'Tous les items que vous achetez au Merchant sont envoyés dans votre Stash - un coffre pour vos items et votre or. C'est une caisse marron située à côté du Merchant.'
+      // fr: 'Tous les items que vous achetez au Merchant sont envoyés dans votre Stash - un coffre pour vos items et vos pièces. C'est une caisse marron située à côté du Merchant.'
     },
     {
       en: 'You will find runes when slaying monsters. Runes will allow you to upgrade your items. Visit the blacksmith to do so.'
       // it: 'Troverai le rune quando uccidi i mostri. Le rune ti permetteranno di aggiornare i tuoi oggetti. Visita il fabbro per farlo.'
       // pt: 'Você irá encontrar runas quando estiver matando monstros. As runas permitem que você fortifique os seus itens. Visite o blacksmith para fortificar seus itens.'
-      // fr: 'Vous trouverez des runes en tuant des monstres. Les runes vous permettent d'améliorer vos items. Pour cela, allez voir le Blacksmith.'
+      // fr: 'Vous trouverez des runes en tuant des monstres. Les runes vous permettent d\'améliorer vos items. Pour faire cela, allez voir le Blacksmith.'
     },
     {
       en: 'Coordination with players is important. You can use <kbd>/global</kbd>, <kbd>/party</kbd>, <kbd>/faction</kbd> and <kbd>/clan</kbd>. Press Enter to chat.'
@@ -242,13 +242,13 @@ export default {
       en: 'Type in your greatest battle shout. Then press enter to send your love letter to the world.'
       // it: 'Digita il tuo più grande grido di battaglia. Quindi premi Invio per inviare la tua lettera d\'amore al mondo.'
       // pt: 'Digite o seu melhor grito de batalhaa. Então pressione enter para enviar sua carta de amor para o mundo.'
-      // fr: 'Envoyez votre meilleur cri de guerre. Appuyez ensuite sur entrer pour envoyer votre lettre d'amour au monde entier.'
+      // fr: 'Ecrivez votre meilleur cri de guerre. Appuyez ensuite sur entrer pour envoyer votre lettre d\'amour au monde entier.'
     },
     {
       en: 'See the cogwheel in the upper right corner? Open your game settings.'
       // it: 'Vedi la ruota dentata nell\'angolo in alto a destra? Apri le impostazioni di gioco.'
       // pt: 'Está vendo a engrenagem no canto superior direito? Abra as configurações do seu jogo'
-      // fr: 'Vous voyez la roue dentelée dans le coin supérieur droit ? Ouvrez les paramatrès du jeu.'
+      // fr: 'Vous voyez la roue dentelée dans le coin supérieur droit ? Ouvrez-y les paramatrès du jeu.'
     },
     {
       en: 'You can change many things here, such as the controls, language, or reducing the graphics.'
@@ -266,7 +266,7 @@ export default {
       en: 'That\'s it. You did it. You are ready to slay. Your faction depends on you.'
       // it: 'Fatto. ci sei riuscito. Sei pronto per uccidere. La tua fazione dipende da te.'
       // pt: 'Isso é tudo. Você completou! Você está pronto para arrasar! Sua facção depende de você.'
-      // fr: 'Voilà, nous y sommes. Vous l'avez fait. Vous êtres prêt à tuer. Votre faction dépend sur vous.'
+      // fr: 'Voilà, nous y sommes. Vous l\'avez fait. Vous êtres prêt à tuer. Votre faction dépend de vous.'
     },
     {
       en: 'Good luck, champion.'
