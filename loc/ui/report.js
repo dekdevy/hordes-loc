7333b1ee29b526dd1b5a554188191fa0d52b0776
Saymoin <saymoin@mail.ru>
2021-05-22 21:28:06 +0500

    RU for report.js
    Translate to russian language.

--- loc/ui/report.js
+++ loc/ui/report.js
@@ -1,19 +1,55 @@
 // Work in progress, do not translate
 export default {
-  reasons: [
-    { en: 'Offensive Chat'},
-    { en: 'Multiboxing'},
-    { en: 'Scamming'},
-    { en: 'Cheating / Exploiting'},
-    { en: 'Offensive Name'},
-    { en: 'Botting'}
+  reasons: reasons: [
+    {
+      en: 'Offensive Chat',
+      ru: 'Оскорбления в чате'
+    },
+    {
+      en: 'Multiboxing',
+      ru: 'Множественные аккаунты'
+    },
+    {
+      en: 'Scamming',
+      ru: 'Мошенничество'
+    },
+    {
+      en: 'Cheating / Exploiting',
+      ru: 'Читы / Взлом'
+    },
+    {
+      en: 'Offensive Name', 
+      ru: 'Оскорбительное имя'
+    },
+    {
+      en: 'Botting',
+      ru: 'Бот'
+    }
   ],
   info: [
-    { en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, NSFW, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages; a single report will suffice.' },
-    { en: 'Playing multiple characters at the same time to gain unfair advantages by abusing game mechanics such as item find stacking, pre-buffing, healbotting or other PVE / PVP activities.' },
-    { en: 'Tricking players into dropping items or falsely advertising ingame services with the sole intention of stealing someones wealth or items.' },
-    { en: 'Bypassing regular gameplay by using cheating tools for unfair advantages or abusing severe game bugs that give unintended advantages.' },
-    { en: 'Names containing offensive language, letters to obfuscate identity, or impersonation of staff members.' },
-    { en: 'Using automated tools to play the game while not being present, or using tools to automate actions for unfair advantages.' }
+    { 
+      en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, NSFW, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages; a single report will suffice.', 
+      ru: 'Спам, оскорбительные выражения (явные, ЛГБТ-фобические, расистские, и NSFW и др.), олицетворение, доксинг, рекламные вещи, которые нарушают TOS. Обратите внимание, что допускается легкое подшучивание. Не сообщайте о нескольких сообщениях; одного отчета будет достаточно.'
+    },
+    { 
+      en: 'Playing multiple characters at the same time to gain unfair advantages by abusing game mechanics such as item find stacking, pre-buffing, healbotting or other PVE / PVP activities.',
+      ru: 'Играя несколькими персонажами одновременно, получая несправедливые преимущества, злоупотребляя игровой механикой, такой как укладка предметов, предварительные бафы, исцеление или другие действия PVE / PVP.'
+    },
+    { 
+      en: 'Tricking players into dropping items or falsely advertising ingame services with the sole intention of stealing someones wealth or items.',
+      ru: 'Обманом заставляя игроков бросать предметы или ложно рекламировать игровые сервисы с единственным намерением украсть чье-то богатство или предметы.'
+    },
+    { 
+      en: 'Bypassing regular gameplay by using cheating tools for unfair advantages or abusing severe game bugs that give unintended advantages.',
+      ru: 'Обход обычного игрового процесса путем использования мошеннических инструментов для получения несправедливых преимуществ или злоупотребления серьезными ошибками в игре, которые дают непреднамеренные преимущества.'
+    },
+    { 
+      en: 'Names containing offensive language, letters to obfuscate identity, or impersonation of staff members.',
+      ru: 'Имена, содержащие оскорбительные выражения, письма, чтобы скрыть личность или выдавать себя за сотрудников проекта.'
+    },
+    { 
+      en: 'Using automated tools to play the game while not being present, or using tools to automate actions for unfair advantages.',
+      ru: 'Использование инструментов автоматизации игрового процесса, или использование инструментов для автоматизации действий для получения несправедливых преимуществ.'
+    }
   ]
 }
