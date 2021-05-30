6dd23e577da5ea6a92a941be4b0218250c02f63d
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2021-05-30 17:38:16 +0100

    Update report.js

--- loc/ui/report.js
+++ loc/ui/report.js
@@ -2,54 +2,54 @@
 export default {
   reasons: reasons: [
     {
-      en: 'Offensive Chat',
-      ru: 'Оскорбления в чате'
+      en: 'Offensive Chat'
+      // ru: 'Оскорбления в чате'
     },
     {
-      en: 'Multiboxing',
-      ru: 'Множественные аккаунты'
+      en: 'Multiboxing'
+      // ru: 'Множественные аккаунты'
     },
     {
-      en: 'Scamming',
-      ru: 'Мошенничество'
+      en: 'Scamming'
+      // ru: 'Мошенничество'
     },
     {
-      en: 'Cheating / Exploiting',
-      ru: 'Читы / Взлом'
+      en: 'Cheating / Exploiting'
+      // ru: 'Читы / Взлом'
     },
     {
-      en: 'Offensive Name', 
-      ru: 'Оскорбительное имя'
+      en: 'Offensive Name',
+      // ru: 'Оскорбительное имя'
     },
     {
-      en: 'Botting',
-      ru: 'Бот'
+      en: 'Botting'
+      // ru: 'Бот'
     }
   ],
   info: [
     { 
-      en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, NSFW, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages; a single report will suffice.', 
-      ru: 'Спам, оскорбительные выражения (явные, ЛГБТ-фобические, расистские, и NSFW и др.), олицетворение, доксинг, рекламные вещи, которые нарушают TOS. Обратите внимание, что допускается легкое подшучивание. Не сообщайте о нескольких сообщениях; одного отчета будет достаточно.'
+      en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, NSFW, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages; a single report will suffice.'
+      // ru: 'Спам, оскорбительные выражения (явные, ЛГБТ-фобические, расистские, и NSFW и др.), олицетворение, доксинг, рекламные вещи, которые нарушают TOS. Обратите внимание, что допускается легкое подшучивание. Не сообщайте о нескольких сообщениях; одного отчета будет достаточно.'
     },
     { 
-      en: 'Playing multiple characters at the same time to gain unfair advantages by abusing game mechanics such as item find stacking, pre-buffing, healbotting or other PVE / PVP activities.',
-      ru: 'Играя несколькими персонажами одновременно, получая несправедливые преимущества, злоупотребляя игровой механикой, такой как укладка предметов, предварительные бафы, исцеление или другие действия PVE / PVP.'
+      en: 'Playing multiple characters at the same time to gain unfair advantages by abusing game mechanics such as item find stacking, pre-buffing, healbotting or other PVE / PVP activities.'
+      // ru: 'Играя несколькими персонажами одновременно, получая несправедливые преимущества, злоупотребляя игровой механикой, такой как укладка предметов, предварительные бафы, исцеление или другие действия PVE / PVP.'
     },
     { 
-      en: 'Tricking players into dropping items or falsely advertising ingame services with the sole intention of stealing someones wealth or items.',
-      ru: 'Обманом заставляя игроков бросать предметы или ложно рекламировать игровые сервисы с единственным намерением украсть чье-то богатство или предметы.'
+      en: 'Tricking players into dropping items or falsely advertising ingame services with the sole intention of stealing someones wealth or items.'
+      // ru: 'Обманом заставляя игроков бросать предметы или ложно рекламировать игровые сервисы с единственным намерением украсть чье-то богатство или предметы.'
     },
     { 
-      en: 'Bypassing regular gameplay by using cheating tools for unfair advantages or abusing severe game bugs that give unintended advantages.',
-      ru: 'Обход обычного игрового процесса путем использования мошеннических инструментов для получения несправедливых преимуществ или злоупотребления серьезными ошибками в игре, которые дают непреднамеренные преимущества.'
+      en: 'Bypassing regular gameplay by using cheating tools for unfair advantages or abusing severe game bugs that give unintended advantages.'
+      // ru: 'Обход обычного игрового процесса путем использования мошеннических инструментов для получения несправедливых преимуществ или злоупотребления серьезными ошибками в игре, которые дают непреднамеренные преимущества.'
     },
     { 
-      en: 'Names containing offensive language, letters to obfuscate identity, or impersonation of staff members.',
-      ru: 'Имена, содержащие оскорбительные выражения, письма, чтобы скрыть личность или выдавать себя за сотрудников проекта.'
+      en: 'Names containing offensive language, letters to obfuscate identity, or impersonation of staff members.'
+      // ru: 'Имена, содержащие оскорбительные выражения, письма, чтобы скрыть личность или выдавать себя за сотрудников проекта.'
     },
     { 
-      en: 'Using automated tools to play the game while not being present, or using tools to automate actions for unfair advantages.',
-      ru: 'Использование инструментов автоматизации игрового процесса, или использование инструментов для автоматизации действий для получения несправедливых преимуществ.'
+      en: 'Using automated tools to play the game while not being present, or using tools to automate actions for unfair advantages.'
+      // ru: 'Использование инструментов автоматизации игрового процесса, или использование инструментов для автоматизации действий для получения несправедливых преимуществ.'
     }
   ]
 }
