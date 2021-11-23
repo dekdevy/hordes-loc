a2b9f4346640153abaeb5e1ed7d35302ffa3ebe0
Redly0n <41922752+Redly0n@users.noreply.github.com>
2021-11-23 22:13:54 +0100

    Added French Translations report.js (commentary only)

--- loc/ui/report.js
+++ loc/ui/report.js
@@ -4,26 +4,32 @@ export default {
     {
       en: 'Offensive Chat'
       // ru: 'Оскорбления в чате'
+      // fr: 'Langage offensant'
     },
     {
       en: 'Multiboxing'
       // ru: 'Множественные аккаунты'
+      // fr: 'Comptes multiples'
     },
     {
       en: 'Scamming'
       // ru: 'Мошенничество'
+      // fr: 'Escroquerie'
     },
     {
       en: 'Cheating / Exploiting'
       // ru: 'Читы / Взлом'
+      // fr: 'Triche / Exploitation de Bugs'
     },
     {
       en: 'Offensive Name'
       // ru: 'Оскорбительное имя'
+      // fr: 'Nom offensant'
     },
     {
       en: 'Botting'
       // ru: 'Бот'
+      // fr: 'Botting'
     }
   ],
   info: [
@@ -31,28 +37,34 @@ export default {
       en: 'Spam, offensive language (explicit, LGBTQ-phobic, racist, NSFW, etc.), impersonation, doxing, advertising things that break the TOS. Note that mild banter is allowed. Do not report multiple messages; a single report will suffice.'
       // ru: 'Спам, оскорбительные выражения (явные, ЛГБТ-фобические, расистские, NSFW, и др.), олицетворение, доксинг, рекламные вещи, которые нарушают TOS. Допускается легкое подшучивание.
       // Не сообщайте о нескольких сообщениях; одного отчета достаточно.'
+      // fr: 'Spam, langage offensant (explicite, LGBTQ-phobe, raciste, NSFW, etc.), usurpation d'identité, doxing, publicité qui enfreint les TOS. Notez que le badinage léger est autorisé. Ne signalez pas plusieurs messages; un seul rapport suffira.'
     },
     {
       en: 'Playing multiple characters at the same time to gain unfair advantages by abusing game mechanics such as item find stacking, pre-buffing, healbotting or other PVE / PVP activities.'
       // ru: 'Играя несколькими персонажами одновременно, получая несправедливые преимущества, злоупотребляя игровой механикой, такой как укладка предметов, предварительные бафы,
       // исцеление или другие действия PVE / PVP.'
+      // fr: 'Jouer plusieurs personnages en même temps pour obtenir des avantages injustes en abusant des mécanismes du jeu tels que l'accumulation de Trouvaille, le pré-buffing, le healbotting ou d'autres activités PVE / PVP.'
     },
     {
       en: 'Tricking players into dropping items or falsely advertising ingame services with the sole intention of stealing someones wealth or items.'
       // ru: 'Обманом заставляя игроков бросать предметы или ложно рекламировать игровые сервисы с единственным намерением украсть чье-то богатство или предметы.'
+      // fr: 'Tromper les joueurs pour qu'ils abandonnent des objets ou faire de la publicité mensongère pour des services en jeu dans le seul but de voler la richesse ou les objets de quelqu'un.'
     },
     {
       en: 'Bypassing regular gameplay by using cheating tools for unfair advantages or abusing severe game bugs that give unintended advantages.'
       // ru: 'Обход обычного игрового процесса путем использования мошеннических инструментов для получения несправедливых преимуществ или злоупотребления серьезными ошибками в игре,
       // которые дают непреднамеренные преимущества.'
+      // fr: 'Contourner le jeu normal en utilisant des outils de triche pour obtenir des avantages déloyaux ou en abusant de graves bugs de jeu qui donnent des avantages involontaires.'
     },
     {
       en: 'Names containing offensive language, letters to obfuscate identity, or impersonation of staff members.'
       // ru: 'Имена, содержащие оскорбительные выражения, письма, чтобы скрыть личность или выдавать себя за сотрудников проекта.'
+      // fr: 'Les noms contenant un langage offensant, des lettres pour masquer l'identité ou l'usurpation d'identité de membres du Staff.'
     },
     {
       en: 'Using automated tools to play the game while not being present, or using tools to automate actions for unfair advantages.'
       // ru: 'Использование инструментов автоматизации игрового процесса, или использование инструментов для автоматизации действий для получения несправедливых преимуществ.'
+      // fr: 'L'utilisation d'outils automatisés pour jouer au jeu sans être présent, ou l'utilisation d'outils pour automatiser des actions pour obtenir des avantages injustes.'
     }
   ]
 }
