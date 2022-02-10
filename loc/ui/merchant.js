9fa6ad35ba3ba3051edc72bc00d362461c121b3e
No898 <82420070+No898@users.noreply.github.com>
2022-02-11 00:11:58 +0100

    Update merchant.js

--- loc/ui/merchant.js
+++ loc/ui/merchant.js
@@ -209,9 +209,8 @@ export default {
     sr: 'Поставио си $1 на продају.',
     it: 'Hai pubblicato $1 in vendita',
     ja: '$1を出品しました。',
-    cs: 'Zveřejnil jsi $1 k prodeji'
-    cs: 'Zveřejnili jste $1 na prodej',
-
+    cs: 'Zveřejnil jsi $1 k prodeji',
+   
   },
 
   // Do not translate "Stash".
