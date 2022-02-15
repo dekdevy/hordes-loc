28c2c43e190523e71db6f0f951359ce8b8b4b5ec
dek <polydek@googlemail.com>
2022-02-15 14:32:14 +0100

    Update pet.js

--- loc/items/pet.js
+++ loc/items/pet.js
@@ -1,9 +1,17 @@
 export default [
   {
-    name: {$$: '?'},
+    name: {$$: 'Greedy Grub'},
 
     description: {
-      en: '?'
+      en: 'A tiny grub that will loot items for you.'
+    }
+  },
+
+  {
+    name: {$$: 'Greedy Goblin'},
+
+    description: {
+      en: 'A tiny goblin that will loot items for you.'
     }
   }
 ]
