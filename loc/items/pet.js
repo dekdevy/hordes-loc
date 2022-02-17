df9f1440e6a906b37f7b747aff728db5ac2817aa
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-02-17 16:43:30 +0000

    Update pet.js

--- loc/items/pet.js
+++ loc/items/pet.js
@@ -3,7 +3,8 @@ export default [
     name: {$$: 'Greedy Grub'},
 
     description: {
-      en: 'A tiny grub that will loot items for you.'
+      en: 'A tiny grub that will loot items for you.',
+      ru: 'Крошечная личинка, которая будет подбирать для вас предметы.'
     }
   },
 
@@ -11,7 +12,8 @@ export default [
     name: {$$: 'Greedy Goblin'},
 
     description: {
-      en: 'A tiny goblin that will loot items for you.'
+      en: 'A tiny goblin that will loot items for you.',
+      ru: 'Крошечный гоблин, который будет подбирать для вас предметы.'
     }
   }
 ]
