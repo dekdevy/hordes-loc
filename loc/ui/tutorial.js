83c15af38a3da95c8bf76133e58fad18f736e66c
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-02-23 18:47:48 +0000

    Whitespace fix.

--- loc/ui/tutorial.js
+++ loc/ui/tutorial.js
@@ -359,7 +359,6 @@ export default {
       // pt: 'Isso é tudo. Você completou! Você está pronto para arrasar! Sua facção depende de você.'
       // fr: 'Voilà, nous y sommes. Vous l\'avez fait. Vous êtres prêt à tuer. Votre faction dépend de vous.'
       // cs: 'A je to. Zvládl jsi to. Jsi připraven zabít. Tvá frakce zavísí na tobě.'
-
     },
     {
       en: 'Good luck, champion.'
