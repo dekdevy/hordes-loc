87098eb52b4c98dc7459fd8921353525fe988769
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-03-09 12:27:19 +0000

    Update elixir.js

--- loc/ui/elixir.js
+++ loc/ui/elixir.js
@@ -35,6 +35,7 @@ export default {
     en: '+ more to come (TBA)'
   },
 
+  // Do not translate "Hordes"
   development: {
     en: 'Support Hordes development for a low cost. Unlock additional bag slots, store more items in your stash, make use of enhanced merchant features and many more perks (TBA).'
   },
@@ -43,10 +44,12 @@ export default {
     en: 'You do not have enough Hordes Points to do this.'
   },
 
+  // As in "Buy Hordes Points"
   buypoints: {
-    en: 'Buy Hordes Points'
+    en: 'Buy'
   },
 
+  // Do not translate "Hordes Elixir"
   thankyou: {
     en: 'Hordes Elixir time added. Thank you for your support. Please re-log any active characters.'
   }
