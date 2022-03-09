db988bc48c910546c0ffde22b3cbb06ea8786aa0
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-03-09 19:14:34 +0000

    Create charpanel.js

---
+++ loc/ui/charpanel.js
@@ -0,0 +1,25 @@
+export default {
+  name: {
+    en: 'Name'
+  },
+
+  level: {
+    en: 'Level'
+  },
+
+  faction: {
+    en: 'Faction'
+  },
+
+  prestige: {
+    en: 'Prestige'
+  },
+
+  rating: {
+    en: 'Rating'
+  },
+
+  medals: {
+    en: 'Medals'
+  }
+}
