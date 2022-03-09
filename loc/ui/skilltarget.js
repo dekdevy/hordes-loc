60d607918564fd1f8c5670e0bc8ece72c70c6fa5
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-03-09 19:54:24 +0000

    Create skilltarget.js

---
+++ loc/ui/skilltarget.js
@@ -0,0 +1,17 @@
+export default {
+  targetNone: {
+    en: 'Requires no target'
+  },
+
+  targetSelf: {
+    en: 'Self cast'
+  },
+
+  targetFriendly: {
+    en: 'on Allies'
+  },
+
+  targetEnemy: {
+    en: 'on Enemy'
+  }
+}
