e0ef77ba74207690a09eba41d7caf91d830cf768
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-03-09 00:43:32 +0000

    Update elixir.js

--- loc/ui/elixir.js
+++ loc/ui/elixir.js
@@ -1,5 +1,29 @@
 export default {
   chatsupport: {
     en: 'Chat supporter icon'
+  },
+  
+  noads: {
+    en: 'No ads'
+  },
+  
+  bagslots: {
+    en: 'base bag slots'
+  },
+  
+  stashstorage: {
+    en: 'stash storage'
+  },
+  
+  merchantlimit: {
+    en: 'merchant limit'
+  },
+  
+  merchantduration: {
+    en: 'merchant duration'
+  },
+  
+  tba: {
+    en: '+ more to come (TBA)'
   }
 }
