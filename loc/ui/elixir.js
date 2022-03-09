02b7e739067b273ec7f34c20457dfee8a7b80258
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-03-09 00:48:46 +0000

    Update elixir.js

--- loc/ui/elixir.js
+++ loc/ui/elixir.js
@@ -6,23 +6,23 @@ export default {
   noads: {
     en: 'No ads'
   },
-  
+
   bagslots: {
     en: 'base bag slots'
   },
-  
+
   stashstorage: {
     en: 'stash storage'
   },
-  
+
   merchantlimit: {
     en: 'merchant limit'
   },
-  
+
   merchantduration: {
     en: 'merchant duration'
   },
-  
+
   tba: {
     en: '+ more to come (TBA)'
   }
