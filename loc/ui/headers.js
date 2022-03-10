9406550d2232899735195cf7bb5e7655f388b52a
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-03-10 00:30:30 +0000

    Create headers.js

---
+++ loc/ui/headers.js
@@ -0,0 +1,25 @@
+export default {
+  character: {
+    en: 'Character'
+  },
+
+  skills: {
+    en: 'Skills'
+  },
+
+  pvp: {
+    en: 'PvP'
+  },
+
+  dpsMeter: {
+    en: 'DPS Meter'
+  },
+
+  fullscreen: {
+    en: 'Fullscreen'
+  },
+
+  menuReturn: {
+    en: 'Back to Menu'
+  }
+}
