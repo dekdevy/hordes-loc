7af3c3d0de666813b286241a58d77087ebeff107
drifted-mdl <49496108+drifted-mdl@users.noreply.github.com>
2022-03-12 14:23:09 +0000

    Create skilldescription.js

---
+++ loc/ui/skilldescription.js
@@ -0,0 +1,62 @@
+export default {
+  targetNone: {
+    en: 'Requires no target'
+  },
+
+  targetSelf: {
+    en: 'Self cast'
+  },
+
+  targetFriendly: {
+    en: 'on Allies'
+  },
+
+  targetEnemy: {
+    en: 'on Enemy'
+  },
+
+  spellMelee: {
+    en: 'Melee attack'
+  },
+
+  spellMagic: {
+    en: 'Magic attack'
+  },
+
+  spellHeal: {
+    en: 'Heal'
+  },
+
+  spellBuff: {
+    en: 'Buff'
+  },
+
+  spellBuffStack: {
+    en: 'Stackable buff'
+  },
+
+  spellMissile: {
+    en: 'Ranged missile'
+  },
+
+  spellMissileBuff: {
+    en: 'Range effect'
+  },
+
+  spellCustom: {
+    en: 'Effect'
+  },
+  
+  // eg: For 1 Intelligence gain 0.4 Min Dmg.
+  statincrement: [
+    {
+      en: 'For 1'
+    },
+    {
+      en: 'gain'
+    },
+    {
+      en: ' '
+    }
+  ]
+}
