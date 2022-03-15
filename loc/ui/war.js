e5eb12d4655349af7a5ea9bd26f51c0f68f9fcf6
dek <polydek@googlemail.com>
2022-03-15 22:15:52 +0100

    War Status Array

--- loc/ui/war.js
+++ loc/ui/war.js
@@ -3,23 +3,19 @@ export default {
     en: 'War Status'
   },
 
-  statusEnded: {
-    en: 'Ended'
-  },
-
-  statusEnding: {
-    en: 'Ending Soon'
-  },
-
-  statusActive: {
-    en: 'Active'
-  },
-
   duration: {
     en: 'Duration'
   },
 
   player: {
     en: 'Player'
-  }
+  },
+
+  statustypes: [{
+    en: 'Ended'
+  }, {
+    en: 'Ending Soon'
+  }, {
+    en: 'Active'
+  }]
 }
