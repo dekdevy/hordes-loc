698551e05754191564c790e0f16883a6caccdac5
Redly0n <41922752+Redly0n@users.noreply.github.com>
2022-03-15 10:56:26 +0100

    Added french translations elixir.js

--- loc/ui/elixir.js
+++ loc/ui/elixir.js
@@ -8,53 +8,64 @@ export default {
   },
 
   chatsupport: {
-    en: 'Chat supporter icon'
+    en: 'Chat supporter icon',
+    fr: 'Icône de chat de supporter'
   },
 
   noads: {
-    en: 'No ads'
+    en: 'No ads',
+    fr: 'Pas de publicité'
   },
 
   // i.e. 25 base bag slots
   bagslots: {
-    en: 'base bag slots'
+    en: 'base bag slots',
+    fr: 'emplacements de sac de base'
   },
 
   // i.e. 100 stash storage
   stashstorage: {
-    en: 'stash storage'
+    en: 'stash storage',
+    fr: 'emplacements de réserve (Stash)'
   },
 
   // i.e. 100 merchant limit
   merchantlimit: {
-    en: 'merchant limit'
+    en: 'merchant limit',
+    fr: 'objets max. sur le marché'
   },
 
   // i.e. 24h merchant duration
   merchantduration: {
-    en: 'merchant duration'
+    en: 'merchant duration',
+    fr: 'avant expiration d'un objet sur le marché'
   },
 
   tba: {
-    en: '+ more to come (TBA)'
+    en: '+ more to come (TBA)',
+    fr: '+ plus à venir'
   },
 
   // Do not translate "Hordes"
   support: {
-    en: 'Support Hordes development for a low cost. Unlock additional bag slots, store more items in your stash, make use of enhanced merchant features and many more perks (TBA).'
+    en: 'Support Hordes development for a low cost. Unlock additional bag slots, store more items in your stash, make use of enhanced merchant features and many more perks (TBA).',
+    fr: 'Soutenez le développement de Hordes à moindre coût. Débloquez des emplacements de sacs supplémentaires, stockez plus d'objets dans votre réserve (stash), profitez des fonctions améliorées du marchand et de bien d'autres avantages.' 
   },
 
   pointserror: {
-    en: 'You do not have enough Hordes Points to do this.'
+    en: 'You do not have enough Hordes Points to do this.',
+    fr: 'Vous ne possédez pas assez de Hordes Points pour faire ceci.'
   },
 
   // As in "Buy Hordes Points"
   buypoints: {
-    en: 'Buy'
+    en: 'Buy',
+    fr: 'Acheter des'
   },
 
   notactive: {
-    en: 'Not active'
+    en: 'Not active',
+    fr: 'Non actif'
   },
 
   //---------------------------------------------------------------------------------------------
@@ -62,53 +73,64 @@ export default {
   // e.g. Would you like to gift one year of Elixir? This will cost 9000 Hordes Points
   //      Would you like to give yourself one month of Elixir? This will cost 900 Hordes Points
   willgift: {
-    en: 'Would you like to gift '
+    en: 'Would you like to gift ',
+    fr: 'Souhaitez-vous offrir '
   },
 
   willadd: {
-    en: 'Would you like to give yourself '
+    en: 'Would you like to give yourself ',
+    fr: 'Souhaitez-vous vous octroyer '
   },
 
   subduration: [
     {
-      en: 'one year of '
+      en: 'one year of ',
+      fr: 'un an d\''
     },
     {
-      en: 'one month of '
+      en: 'one month of ',
+      fr: 'un mois d\''
     }
   ],
 
   willcost: {
-    en: 'This will cost '
+    en: 'This will cost ',
+    fr: 'Cela coûtera '
   },
   //---------------------------------------------------------------------------------------------
 
   send: {
-    en: 'Send Gift'
+    en: 'Send Gift',
+    fr: 'Offrir un cadeau'
   },
 
   // e.g. Enables Elixir until Jan 29, 2024 13:00. Do not translate "Elixir"
   enable: {
-    en: 'Enables Elixir until'
+    en: 'Enables Elixir until',
+    fr: 'Active l\'Elixir jusqu\'au'
   },
 
   // Do not translate "Elixir"
   sendgift: {
-    en: 'Send Elixir Gift'
+    en: 'Send Elixir Gift',
+    fr: 'Offrir de l\'Elixir'
   },
 
   // Do not translate "Elixir"
   extend: {
-    en: 'Extend Elixir'
+    en: 'Extend Elixir',
+    fr: 'Prolonger l\'Elixir'
   },
 
   // Do not translate "Elixir"
   activate: {
-    en: 'Activate Elixir'
+    en: 'Activate Elixir',
+    fr: 'Activer l\'Elixir'
   },
 
   // Do not translate "Elixir"
   thankyou: {
-    en: 'Elixir time added. Thank you for your support. Please re-log any active characters.'
+    en: 'Elixir time added. Thank you for your support. Please re-log any active characters.',
+    fr: 'Période d\'Elixir ajoutée. Merci pour votre soutien. Merci de bien vouloir reconnecter tout personnage connecté.
   }
 }
