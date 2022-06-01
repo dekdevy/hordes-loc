734787056dfdbbac54c12b0d751085b92eb4bb6d
Cryspoeth47 <105221671+Cryspoeth47@users.noreply.github.com>
2022-06-01 12:49:41 +0300

    itemdescription turkish translation

--- loc/ui/itemdescription.js
+++ loc/ui/itemdescription.js
@@ -1,26 +1,31 @@
 export default {
   compare: {
     en: 'Press Shift to compare item.',
-    fr: 'Maintenez MAJ pour comparer l\'objet.'
+    fr: 'Maintenez MAJ pour comparer l\'objet.',
+    tr: 'Eşyaları karşılaştırmak için shifte bas.'
   },
 
   onuse: {
     en: 'on use.',
-    fr: 'après utilisation.'
+    fr: 'après utilisation.',
+    tr: 'Kullanıldığında.'
   },
 
   onpurchase: {
     en: 'on purchase.',
-    fr: 'après l\'achat.'
+    fr: 'après l\'achat.',
+    tr: 'Satın alındığında.'
   },
 
   onsale: {
     en: 'on merchant sale.',
-    fr: 'après achat au Merchant.'
+    fr: 'après achat au Merchant.',
+    tr: 'Pazar satışında.'
   },
 
   equipeffect: {
     en: 'Equipping this item will have these effects',
-    fr: 'Equiper cet objet va avoir ces effets'
+    fr: 'Equiper cet objet va avoir ces effets',
+    tr: 'Bu eşyayı kuşanmak bu etkileri sağlıyacak'
   }
 }
