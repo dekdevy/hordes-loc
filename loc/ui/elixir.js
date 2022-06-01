2e4ba2c30a1eb9f0cc6776f11e615ca612fbd855
Cryspoeth47 <105221671+Cryspoeth47@users.noreply.github.com>
2022-06-01 12:46:11 +0300

    elixir turkish translation

--- loc/ui/elixir.js
+++ loc/ui/elixir.js
@@ -9,63 +9,74 @@ export default {
 
   chatsupport: {
     en: 'Chat supporter icon',
-    fr: 'Icône de chat de supporter'
+    fr: 'Icône de chat de supporter',
+    tr: 'Sohbette destekçi simgesi'
   },
 
   noads: {
     en: 'No ads',
-    fr: 'Pas de publicité'
+    fr: 'Pas de publicité',
+    tr: 'Reklam yok'
   },
 
   // i.e. 25 base bag slots
   bagslots: {
     en: 'base bag slots',
-    fr: 'emplacements de sac de base'
+    fr: 'emplacements de sac de base',
+    tr: 'Temel çanta slotu'
   },
 
   // i.e. 100 stash storage
   stashstorage: {
     en: 'stash storage',
-    fr: 'emplacements de réserve'
+    fr: 'emplacements de réserve',
+    tr: 'Stash kapasitesi'
   },
 
   // i.e. 100 merchant limit
   merchantlimit: {
     en: 'merchant limit',
-    fr: 'objets max. sur le marché'
+    fr: 'objets max. sur le marché',
+    tr: 'Pazar sınırı'
   },
 
   // i.e. 24h merchant duration
   merchantduration: {
     en: 'merchant duration',
-    fr: 'avant expiration d\'un objet sur le marché'
+    fr: 'avant expiration d\'un objet sur le marché',
+    tr: 'Pazarda kalma süresi'
   },
 
   tba: {
     en: '+ more to come (TBA)',
-    fr: '+ plus à venir'
+    fr: '+ plus à venir',
+    tr: 'daha fazlası'
   },
 
   // Do not translate "Hordes"
   support: {
     en: 'Support Hordes development for a low cost. Unlock additional bag slots, store more items in your stash, make use of enhanced merchant features and many more perks (TBA).',
-    fr: 'Soutenez le développement de Hordes à moindre coût. Débloquez des emplacements de sacs supplémentaires, stockez plus d\'objets dans votre réserve, profitez des fonctionnalités améliorées du marchand et de bien d\'autres avantages.'
+    fr: 'Soutenez le développement de Hordes à moindre coût. Débloquez des emplacements de sacs supplémentaires, stockez plus d\'objets dans votre réserve, profitez des fonctionnalités améliorées du marchand et de bien d\'autres avantages.',
+    tr: 'Hordes\'in geliştirilmesini küçük bir bedele destekleyin. Fazladan çanta slotlarının, stash\'inde daha fazla depolamanın, geliştirilmiiş pazar yenilinklerinin ve daha fazla fırsatın kilidini aç.'
   },
 
   pointserror: {
     en: 'You do not have enough Hordes Points to do this.',
-    fr: 'Vous ne possédez pas assez de Hordes Points pour faire ceci.'
+    fr: 'Vous ne possédez pas assez de Hordes Points pour faire ceci.',
+    tr: 'Bunu yapmak için yeterli Hordes point\'in yok.'
   },
 
   // As in "Buy Hordes Points"
   buypoints: {
     en: 'Buy',
-    fr: 'Acheter des'
+    fr: 'Acheter des',
+    tr: 'Satın al'
   },
 
   notactive: {
     en: 'Not active',
-    fr: 'Non actif'
+    fr: 'Non actif',
+    tr: 'Aktif değil'
   },
 
   //---------------------------------------------------------------------------------------------
@@ -74,63 +85,74 @@ export default {
   //      Would you like to give yourself one month of Elixir? This will cost 900 Hordes Points
   willgift: {
     en: 'Would you like to gift ',
-    fr: 'Souhaitez-vous offrir '
+    fr: 'Souhaitez-vous offrir ',
+    tr: 'Hediye etmek ister misin '
   },
 
   willadd: {
     en: 'Would you like to give yourself ',
-    fr: 'Souhaitez-vous vous octroyer '
+    fr: 'Souhaitez-vous vous octroyer ',
+    tr: 'Kendine almak ister misin '
   },
 
   subduration: [
     {
       en: 'one year of ',
-      fr: 'un an d\''
+      fr: 'un an d\'',
+      tr: 'Bir yıllık '
     },
     {
       en: 'one month of ',
-      fr: 'un mois d\''
+      fr: 'un mois d\'',
+      tr: 'Bir aylık '
     }
   ],
 
   willcost: {
     en: 'This will cost ',
-    fr: 'Cela coûtera '
+    fr: 'Cela coûtera ',
+    tr: 'Bu sana şu kadar bedel olacak'
   },
   //---------------------------------------------------------------------------------------------
 
   send: {
     en: 'Send Gift',
-    fr: 'Offrir un cadeau'
+    fr: 'Offrir un cadeau',
+    tr: 'Hediye gönder'
   },
 
   // e.g. Enables Elixir until Jan 29, 2024 13:00. Do not translate "Elixir"
   enable: {
     en: 'Enables Elixir until',
-    fr: 'Active l\'Elixir jusqu\'au'
+    fr: 'Active l\'Elixir jusqu\'au',
+    tr: 'Şu tarihe kadar Elixir aktif eder'
   },
 
   // Do not translate "Elixir"
   sendgift: {
     en: 'Send Elixir Gift',
-    fr: 'Offrir de l\'Elixir'
+    fr: 'Offrir de l\'Elixir',
+    tr: 'Elixir hediyesi gönder'
   },
 
   // Do not translate "Elixir"
   extend: {
     en: 'Extend Elixir',
-    fr: 'Prolonger l\'Elixir'
+    fr: 'Prolonger l\'Elixir',
+    tr: 'Elixir\'in süresini uzat'
   },
 
   // Do not translate "Elixir"
   activate: {
     en: 'Activate Elixir',
-    fr: 'Activer l\'Elixir'
+    fr: 'Activer l\'Elixir',
+    tr: 'Elixir aktif et'
   },
 
   // Do not translate "Elixir"
   thankyou: {
     en: 'Elixir time added. Thank you for your support. Please re-log any active characters.',
-    fr: 'Période d\'Elixir ajoutée. Merci pour votre soutien. Merci de bien vouloir reconnecter tout personnage connecté.'
+    fr: 'Période d\'Elixir ajoutée. Merci pour votre soutien. Merci de bien vouloir reconnecter tout personnage connecté.',
+    tr: 'Elixir süresi eklendi. Desteğin için teşekkürler. Lütfen herhangi bir karakter ile yeniden giriş yap.'
   }
 }
