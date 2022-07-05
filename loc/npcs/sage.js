0210f0bfae122644d9a4428c68bff2b333b07fb8
Hatsuse_izuna <minku0430@naver.com>
2022-07-05 21:23:35 +0900

    add korean sage
    sage add korean

--- loc/npcs/sage.js
+++ loc/npcs/sage.js
@@ -15,7 +15,8 @@ export default {
       tr: 'Yüklü zihinler bol miktarda fikirle doludur; İlk önce boş bulunursa bir fincan oldukça faydalı olabilir. Aydınlanmaya giden yolda sana nasıl yardımcı olabilirim?',
       ja: '滾る熱意は数多くの声により満たされている。空の器はより良いものへとなり得るのだ。今、其方の悟りへの道は開かれる。その身を委ねるが良い。',
       cs: 'Zatížená mysl je plná názorů; šálek může být užitečný, pokud je nejprve nalezen prázdný. Jak ti mohu být nápomocen na tvé cestě k osvícení?',
-      zh: '若多物可擇，其腦重也。杯之所以為杯，空也。在你走向啟蒙的道路上，我該如何幫助你？'
+      zh: '若多物可擇，其腦重也。杯之所以為杯，空也。在你走向啟蒙的道路上，我該如何幫助你？',
+      ko: '무거운 마음은 많은 의견으로 채워져 있습니다, 처음으로 컵을 발견한다면 그것은 매우 유용할지도 모릅니다, 깨달음을 향한 길을 어떻게 도와드릴까요?'
     },
 
     choices: [{
@@ -35,7 +36,8 @@ export default {
       tr: 'İstatistik puanlarımı sıfırla ( $g$1 )',
       ja: 'ステータスポイントをリセットして下さい。 ( $g$1 )',
       cs: 'Resetuj moje statistické body ( $g$1 )',
-      zh: '重製我的屬性點 ( $g$1 )'
+      zh: '重製我的屬性點 ( $g$1 )',
+      ko: '스탯 포인트를 초기화 해주세요 ( $g$1 ).'
     }]
   }]
 }
