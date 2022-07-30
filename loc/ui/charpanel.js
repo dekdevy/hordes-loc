3709def97ddcb9beeac197b6ace37df8c26a260f
kittenowo <96604263+kittenowo@users.noreply.github.com>
2022-07-30 14:01:04 +0800

    chinese translation update

--- loc/ui/charpanel.js
+++ loc/ui/charpanel.js
@@ -3,53 +3,62 @@ export default {
     en: 'Name',
     fr: 'Nom',
     tr: 'İsim',
-    ko: '이름'
+    ko: '이름',
+    zh: '名字'
   },
 
   level: {
     en: 'Level',
     fr: 'Niveau',
     tr: 'Seviye',
-    ko: '레벨'
+    ko: '레벨',
+    zh: '等級'
   },
 
   class: {
     en: 'Class',
     fr: 'Classe',
     tr: 'Sınıf',
-    ko: '직업'
+    ko: '직업',
+    zh: '職業'
   },
 
   faction: {
     en: 'Faction',
-    ko: '파벌'
+    ko: '파벌',
+    zh: '陣營'
   },
 
   rank: {
     en: 'Rank',
     fr: 'Rang',
     tr: 'Rütbe',
-    ko: '계급'
+    ko: '계급',
+    zh: '排位'
+    
   },
 
   rating: {
     en: 'Rating',
     fr: 'Classement',
     tr: 'Derecelendirme',
-    ko: '평점'
+    ko: '평점',
+    zh: '積分'
   },
 
   medals: {
     en: 'Medals',
     fr: 'Médailles',
     tr: 'Madalyalar',
-    ko: '메달'
+    ko: '메달',
+    zh: '獎章'
   },
 
   perday: {
     en: 'per day',
     fr: 'par jour',
     tr: 'Hergün',
-    ko: '하루 동안에'
+    ko: '하루 동안에',
+    zh: '一天'
   }
 }
