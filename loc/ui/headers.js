46b47c856112c5d928fc826a34966735143d6821
kittenowo <96604263+kittenowo@users.noreply.github.com>
2022-07-30 15:40:19 +0800

    chinese translation updated

--- loc/ui/headers.js
+++ loc/ui/headers.js
@@ -3,14 +3,16 @@ export default {
     en: 'Character',
     fr: 'Personnage',
     tr: 'Karakter',
-    ko: '캐릭터'
+    ko: '캐릭터',
+    zh: '人物'
   },
 
   skills: {
     en: 'Skills',
     fr: 'Compétences',
     tr: 'Yetenekler',
-    ko: '스킬'
+    ko: '스킬',
+    zh: '技能'
   },
 
   pvp: {
@@ -19,39 +21,45 @@ export default {
 
   interaction: {
     en: 'Interaction',
-    ko: '상호 작용'
+    ko: '상호 작용',
+    zh: '對話'
   },
 
   request: {
     en: 'Request',
     fr: 'Requête',
     tr: 'Talep',
-    ko: '요구'
+    ko: '요구',
+    zh: '需求'
   },
 
   warstats: {
     en: 'War Statistics',
     fr: 'Statistiques de Guerre',
     tr: 'Savaş istatistikleri',
-    ko: '전쟁 통계'
+    ko: '전쟁 통계',
+    zh: '戰爭數據'
   },
 
   dpsMeter: {
     en: 'DPS Meter',
-    ko: 'DPS 측정기'
+    ko: 'DPS 측정기',
+    zh: 'DPS 表'
   },
 
   fullscreen: {
     en: 'Fullscreen',
     fr: 'Plein écran',
     tr: 'Tam ekran',
-    ko: '전체화면'
+    ko: '전체화면',
+    zh: '全螢幕'
   },
 
   menuReturn: {
     en: 'Back to Menu',
     fr: 'Retour au menu',
     tr: 'Menüye dön',
-    ko: '메뉴로 돌아가기'
+    ko: '메뉴로 돌아가기',
+    zh: '返回主頁'
   }
 }
