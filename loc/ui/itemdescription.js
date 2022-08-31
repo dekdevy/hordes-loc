4b125d5791f8e72416c8bd09bbb1b17702fb5939
Suggest Name <dudugt500@gmail.com>
2022-08-31 12:57:16 -0300

    Update itemdescripton.js
    Added and improvements pt-br translations

--- loc/ui/itemdescription.js
+++ loc/ui/itemdescription.js
@@ -4,7 +4,8 @@ export default {
     fr: 'Maintenez Maj pour comparer l\'objet.',
     tr: 'Eşyaları karşılaştırmak için shifte bas.',
     ko: 'Shift 를 눌러 아이템을 비교하세요',
-    zh: '按住 Shift 比對物品'
+    zh: '按住 Shift 比對物品',
+    pt: 'Pressione Shift para comparar o item.'
   },
 
   onuse: {
@@ -12,7 +13,8 @@ export default {
     fr: 'après utilisation.',
     tr: 'Kullanıldığında.',
     ko: '사용 중',
-    zh: '使用時'
+    zh: '使用時',
+    pt: 'em uso.'
   },
 
   onpurchase: {
@@ -20,7 +22,8 @@ export default {
     fr: 'après l\'achat.',
     tr: 'Satın alındığında.',
     ko: '구매 할 경우',
-    zh: '購買時'
+    zh: '購買時',
+    pt: 'na compra'
   },
 
   onsale: {
@@ -28,7 +31,8 @@ export default {
     fr: 'après la vente au marché.',
     tr: 'Pazar satışında.',
     ko: '시장 판매',
-    zh: '拍賣後'
+    zh: '拍賣後',
+    pt: 'na venda do Merchant.'
   },
 
   equipeffect: {
@@ -36,6 +40,7 @@ export default {
     fr: 'Equiper cet objet aura ces effets',
     tr: 'Bu eşyayı kuşanmak bu etkileri sağlıyacak',
     ko: '이 아이템을 착용한다면 해당 효과를 가질 수 있습니다.',
-    zh: '裝備這個物品會有這些效果'
+    zh: '裝備這個物品會有這些效果',
+    pt: 'Equipar este item dará esses efeitos'
   }
 }
