3da8597807717145dc88eec186954b83ec8d35d8
dek <polydek@googlemail.com>
2023-02-12 21:49:10 +0100

    condense mount structure

--- loc/items/mount.js
+++ loc/items/mount.js
@@ -1,107 +1,62 @@
 export default [
   {
-    name: {$$: 'Grubling'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Grubling'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Spider'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Spider'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Leafspider'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Leafspider'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Ruby Crawler'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Ruby Crawler'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Tick'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Tick'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Shadowstrider'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Shadowstrider'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Pebbles'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Pebbles'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Ember Pebbles'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Ember Pebbles'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Ether Wyrm'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Ether Wyrm'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Shadow Wyrm'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Shadow Wyrm'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Sand Scarab'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Sand Scarab'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Golden Scarab'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Golden Scarab'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Flaming Skullspider'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Flaming Skullspider'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Blacksmith\'s Grub'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Blacksmith\'s Grub'},
+    description: { en: ' ' }
   },
   {
-    name: {$$: 'Snowball'},
-
-    description: {
-      en: ' '
-    }
+    name       : {$$: 'Snowball'},
+    description: { en: ' ' }
   }
 ]
